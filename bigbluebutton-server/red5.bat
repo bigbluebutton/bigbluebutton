784c7e30255083c71c65d555084347407fe97795
Richard Alam <ritzalam@gmail.com>
2008-12-21 18:10:51 +0000

    Remove bigbluebutton apps from build and add deploy task.
    git-svn-id: http://bigbluebutton.googlecode.com/svn/trunk@798 af16638f-c34d-0410-8cfa-b39d5352b314

--- bigbluebutton-server/red5.bat
+++ bigbluebutton-server/red5.bat
@@ -3,7 +3,7 @@
 if not "%JAVA_HOME%" == "" goto launchRed5
 
 :launchRed5
-"%JAVA_HOME%/bin/java" -Djava.security.manager -Dred5.root=dist -Djava.security.policy=conf/red5.policy -cp red5.jar;conf;bin org.red5.server.Standalone
+"%JAVA_HOME%/bin/java" -Djava.security.manager -Dred5.root=. -Djava.security.policy=conf/red5.policy -cp red5.jar;conf;bin org.red5.server.Standalone
 goto finaly
 
 :err
