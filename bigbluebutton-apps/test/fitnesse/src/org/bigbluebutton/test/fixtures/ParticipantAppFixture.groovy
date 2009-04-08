7f09116ac165278d96e435dc6b687e458523c0b3
Richard Alam <ritzalam@gmail.com>
2009-04-08 20:15:33 +0000

    - create sample fixture
    git-svn-id: http://bigbluebutton.googlecode.com/svn/trunk@1220 af16638f-c34d-0410-8cfa-b39d5352b314

--- bigbluebutton-apps/test/fitnesse/src/org/bigbluebutton/test/fixtures/ParticipantAppFixture.groovy
+++ bigbluebutton-apps/test/fitnesse/src/org/bigbluebutton/test/fixtures/ParticipantAppFixture.groovy
@@ -1,17 +1,18 @@
 
 package org.bigbluebutton.test.fixtures
 
-import fit.ColumnFixture
-import org.bigbluebutton.conference.Room
+import fit.Fixture
+import org.bigbluebutton.conference.service.participants.ParticipantsApplication
+import org.bigbluebutton.conference.RoomsManager
 
-public class ParticipantAppFixture  extends ColumnFixture{
-	private Room room
+public class ParticipantAppFixture  extends Fixture{
+	public static ParticipantsApplication app
+	private RoomsManager manager
 	public String roomName;
 
-	public String checkRoomName() {
-	    room = new Room(roomName)
-	    return room.getName()
-	}
-	
-	
+	public ParticipantAppFixture() {
+		app = new ParticipantsApplication()
+		manager = new RoomsManager()
+		app.setRoomsManager(manager)
+	}	
 }
