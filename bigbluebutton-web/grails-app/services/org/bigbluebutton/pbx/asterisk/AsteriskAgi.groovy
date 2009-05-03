90d1d82098c07a893f8488e2e1fdf88736208130
Richard Alam <ritzalam@gmail.com>
2009-05-03 20:06:34 +0000

    - enable AgiService to be called from Asterisk in order to have dynamic voice conferencing - need to add this line to /etc/asterisk/extension.conf   exten => 1300,1,Agi(agi://192.168.0.100/meetme.groovy)
    git-svn-id: http://bigbluebutton.googlecode.com/svn/trunk@1367 af16638f-c34d-0410-8cfa-b39d5352b314

--- bigbluebutton-web/grails-app/services/org/bigbluebutton/pbx/asterisk/AsteriskAgi.groovy
+++ bigbluebutton-web/grails-app/services/org/bigbluebutton/pbx/asterisk/AsteriskAgi.groovy
@@ -2,93 +2,55 @@ package org.bigbluebutton.pbx.asterisk;
 
 import org.asteriskjava.fastagi.AgiChannel;
 import org.asteriskjava.fastagi.AgiException;
+import org.asteriskjava.fastagi.AgiHangupException;
 import org.asteriskjava.fastagi.AgiRequest;
 import org.asteriskjava.fastagi.AgiScript;
 
 import java.util.Calendar
 
-/*
-import groovy.lang.Binding;
-import groovy.sql.Sql;
-import groovy.util.GroovyScriptEngine;
-import groovy.util.ResourceException;
-import groovy.util.ScriptException;
-
-import java.io.IOException;
-import java.sql.SQLException;
-import javax.sql.DataSource;
-*/
-
-import org.bigbluebutton.web.domain.Conference;
+import org.bigbluebutton.web.domain.ScheduledSession;
 
 class AsteriskAgi implements AgiScript {
 
-//    private GroovyScriptEngine gse
-//    private Sql db
-    
-//    private DataSource dataSource
-    
-    	
-//    def void setDataSource(DataSource source) {
-//    	dataSource = source;
-//    	db = new Sql(dataSource);
-//    }
-    
     private int tries = 0
     private long _10_minutes = 10*60*1000
         
     def void service(AgiRequest request, AgiChannel channel)
             throws AgiException {
-        
-        tries = 0
-        boolean found = false
-        while ((tries < 3).and(!found)) {
-            
-			def number = channel.getData("conf-getconfno", 10000, 10)
-			println "you entered "
-			println "$number new"
-		
-			def conf = Conference.findByConferenceNumber(number)
-
-			if (conf) { 
-				println "found one! " + conf.conferenceName
-				
-				def startTime = conf.startDateTime.time - _10_minutes				
-				def endTime = conf.startDateTime.time + conf.lengthOfConference*60*60*1000 + _10_minutes				
-				def now = new Date().time
-				
-				if ((startTime < now) && (endTime > now)) {				
-					channel.streamFile("conf-placeintoconf")
-					channel.exec("Meetme", "$number|dMq")
-					found = true
-				} else {
-/*					if (now < startTime) {
-						def time = new Calendar(conf.startDateTime)
-						def hr = time.get(Calendar.HOUR)
-						def min = time.get(Calendar.MINUTE)
-						def am_pm = time.get(Calendar.AM_PM)
-						
+        try {
+	        tries = 0
+	        boolean found = false
+	        while ((tries < 3).and(!found)) {
+	            
+				def number = channel.getData("conf-getconfno", 10000, 10)
+				println "you entered "
+				println "$number new"
+			
+				def conf = ScheduledSession.findByVoiceConferenceBridge(number)
+	
+				if (conf) { 
+					def startTime = conf.startDateTime.time - _10_minutes				
+					def endTime = conf.endDateTime.time + _10_minutes				
+					def now = new Date().time
+					
+					if ((startTime < now) && (endTime > now)) {				
+						channel.streamFile("conf-placeintoconf")
+						channel.exec("Meetme", "$number|dMq")
+						found = true
+					} else {
 						channel.streamFile("conference")
-						channel.streamFile("is-at")
-						channel.streamFile("digits/" + hr)
-						channel.streamFile("digits/" + min)
-						if (am_pm == Calendar.AM) {
-							channel.streamFile("digits/a-m")
-						} else {
-							channel.streamFile("digits/p-m")
-						}
-*/						channel.streamFile("conference")
 						channel.streamFile("is")
 						channel.streamFile("unavailable")
-						break;
-//					}
+					}
+				} else {
+					channel.streamFile("conf-invalid")
 				}
-			} else {
-				channel.streamFile("conf-invalid")
+				tries++
 			}
-			tries++
-		}
-		channel.streamFile("goodbye")
+			channel.streamFile("goodbye")
+        } catch (AgiHangupException e) {
+        	log.debug "User has hangup"
+        }
     } 
 
 }
