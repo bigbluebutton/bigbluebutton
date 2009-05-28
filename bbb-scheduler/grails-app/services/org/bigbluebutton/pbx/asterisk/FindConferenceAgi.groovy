799d7dc9c93821c5a2fcc7a3874d8ae98e93750b
Richard Alam <ritzalam@gmail.com>
2009-05-28 15:22:54 +0000

    - prompt for conference in the dialplan instead of in the AGI
    git-svn-id: http://bigbluebutton.googlecode.com/svn/trunk@1466 af16638f-c34d-0410-8cfa-b39d5352b314

---
+++ bbb-scheduler/grails-app/services/org/bigbluebutton/pbx/asterisk/FindConferenceAgi.groovy
@@ -0,0 +1,46 @@
+package org.bigbluebutton.pbx.asterisk;
+
+import org.asteriskjava.fastagi.AgiChannel;
+import org.asteriskjava.fastagi.AgiException;
+import org.asteriskjava.fastagi.AgiRequest;
+import org.asteriskjava.fastagi.AgiScript;
+import org.asteriskjava.fastagi.AgiHangupException
+import org.bigbluebutton.domain.Conference
+
+class FindConferenceAgi implements AgiScript {
+
+    private long _10_minutes = 10*60*1000
+        
+    def void service(AgiRequest request, AgiChannel channel)
+            throws AgiException {
+
+       	def number = request.getParameter("conference")
+		println "you entered $number"
+		
+		def conf = Conference.findByConferenceNumber(number)
+
+		if (conf) { 
+			println "found one! " + conf.conferenceName
+					
+			def startTime = conf.startDateTime.time - _10_minutes				
+			def endTime = conf.startDateTime.time + conf.lengthOfConference*60*60*1000 + _10_minutes				
+			def now = new Date()
+					
+			println "startTime " + new Date(startTime)
+			println "endTime " + new Date(endTime)
+			println "now " + now
+					
+			if ((startTime < now.time) && (endTime > now.time)) {	
+				println "CONFERENCE_FOUND=$number"
+				channel.setVariable("CONFERENCE_FOUND", number)
+			} else {
+				println ("CONFERENCE_FOUND=")
+				channel.setVariable("CONFERENCE_FOUND", '')
+			}
+		} else {
+			println ("CONFERENCE_INVALID=TRUE")
+			channel.setVariable("CONFERENCE_FOUND", '')
+		}
+    } 
+
+}
