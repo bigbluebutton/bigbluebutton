cba76a4644767bb3fba103ec56f5844cc0dcb451
Fred Dixon <ffdixon@gmail.com>
2009-06-03 01:43:50 +0000

    updated build.sh helper script for BigBlueButton-client
    git-svn-id: http://bigbluebutton.googlecode.com/svn/trunk@1539 af16638f-c34d-0410-8cfa-b39d5352b314

--- bigbluebutton-client/build.sh
+++ bigbluebutton-client/build.sh
@@ -3,38 +3,43 @@
 #
 # BigBlueButton: Script to build/deploy the BigBlueButton client
 
-if dpkg-query -s bbb-client | grep installed > /dev/null 2>&1; then
-	sudo apt-get remove --force-yes bbb-client
+
+while [ $# -gt 0 ]; do    # Until you run out of parameters . . .
+  case "$1" in
+    -h|--help)
+                echo "
+Helper script to build and deploy the BigBlueButton client.
+
+Hint: To avoid entering your password with sudo, just type 
+
+	sudo ls
+	
+at the start of your development session to have sudo remember that you can become
+root.
+"
+		exit 0
+              ;;
+  esac
+  shift       # Check next set of parameters.
+done
+
+
+#
+# Remove the bbb-client package if its already installed
+if dpkg-query -s bbb-client | grep "install ok installed" > /dev/null 2>&1; then
+        sudo apt-get purge --yes bbb-client
 fi
+
+
+#
+# Assign the local IP address
+#
 
 ant cleanandmake
 
 if [ $? -ne 0 ]; then
 	exit 1
 fi
-
-#
-# Assign the local IP address
-#
-OLD_IP="$(cat ./client/conf/config.xml | sed 's/"//g' | sed -n '/<porttest /{s/.*host=//;s/ .*//;p}')"
-IP="$(ifconfig eth0 | sed -n '/inet /{s/.*addr://;s/ .*//;p}')"
-
-sed -i "s/$OLD_IP/$IP/g" ./client/conf/config.xml
-
-while [ $# -gt 0 ]; do    # Until you run out of parameters . . .
-  case "$1" in
-    -d|--drop)
-              # drop the database
-		DROP=1
-              ;;
-    -n)
-		# don't rebuild the war
-		echo "set"
-              BUILD=0
-              ;;
-  esac
-  shift       # Check next set of parameters.
-done
 
 
 #
@@ -45,4 +50,13 @@ sudo rm -rf /var/www/bigbluebutton
 sudo mkdir /var/www/bigbluebutton
 sudo cp -r ./client /var/www/bigbluebutton
 
+#
+# Adjust the IP for the config.xml to match local machine
+#
+
+OLD_IP="$(cat /var/www/bigbluebutton/client/conf/config.xml | sed 's/"//g' | sed -n '/<porttest /{s/.*host=//;s/ .*//;p}')"
+IP="$(ifconfig eth0 | sed -n '/inet /{s/.*addr://;s/ .*//;p}')"
+sudo sed -i "s/$OLD_IP/$IP/g" /var/www/bigbluebutton/client/conf/config.xml
+
 echo "New client installed in /var/www/bigbluebutton."
+echo "done"
