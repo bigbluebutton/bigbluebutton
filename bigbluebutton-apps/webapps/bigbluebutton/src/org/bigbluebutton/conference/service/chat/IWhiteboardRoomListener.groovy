04d7f2942e8de36657100d5bdd07a14031cb5ea4
Richard Alam <ritzalam@gmail.com>
2009-06-22 02:22:03 +0000

    - removing whiteboard update as it got checkedin to trunk
    git-svn-id: http://bigbluebutton.googlecode.com/svn/trunk@1741 af16638f-c34d-0410-8cfa-b39d5352b314

--- bigbluebutton-apps/webapps/bigbluebutton/src/org/bigbluebutton/conference/service/chat/IWhiteboardRoomListener.groovy
+++
@@ -1,7 +0,0 @@
-
-package org.bigbluebutton.conference.service.whiteboard
-
-interface IWhiteboardRoomListener {
-	def getName()
-	def newWhiteboardMessage(message)
-}
--- bigbluebutton-apps/webapps/bigbluebutton/src/org/bigbluebutton/conference/service/chat/WhiteboardService.groovy
+++
@@ -1,26 +0,0 @@
-package org.bigbluebutton.conference.service.whiteboard
-import org.slf4j.Loggerimport org.slf4j.LoggerFactory
-import org.red5.logging.Red5LoggerFactory
-import org.red5.server.api.Red5
-public class WhiteboardService {
-	
-	private static Logger log = Red5LoggerFactory.getLogger( WhiteboardService.class, "bigbluebutton" );
-	
-	private WhiteboardApplication application
-
-	public String getWhiteboardMessages() {
-		String roomName = Red5.connectionLocal.scope.name
-		return application.getWhiteboardMessages(roomName)
-	}
-	
-	public void sendMessage(String message) {
-		String roomName = Red5.connectionLocal.scope.name
-		application.sendMessage(roomName, message)
-	}
-
-	public void setWhiteboardApplication(WhiteboardApplication a) {
-		log.debug("Setting Whiteboard Applications")
-		application = a
-	}
-
-}
