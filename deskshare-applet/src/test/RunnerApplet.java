457ad87938444e343abbaa84e73560a879f6c2fc
Richard Alam <ritzalam@gmail.com>
2009-06-23 16:45:21 +0000

    - copy deskshare applet to trunk
    git-svn-id: http://bigbluebutton.googlecode.com/svn/trunk@1764 af16638f-c34d-0410-8cfa-b39d5352b314

---
+++ deskshare-applet/src/test/RunnerApplet.java
@@ -0,0 +1,46 @@
+package test;
+import javax.swing.JApplet;
+
+import screenshot.Capture;
+import screenshot.CaptureThread;
+
+public class RunnerApplet extends JApplet {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	CaptureThread capThread;
+	
+	public void init(){
+		int screenWidth = Integer.parseInt(getParameter("CAPTURE_WIDTH"));
+		int screenHeight = Integer.parseInt(getParameter("CAPTURE_HEIGHT"));
+		int x = Integer.parseInt(getParameter("X"));
+		int y = Integer.parseInt(getParameter("Y"));
+		Capture capture = new Capture(x, y, screenWidth, screenHeight);
+		String roomNumber = getParameter("ROOM");
+		String IP = getParameter("IP");
+		capThread = new CaptureThread(capture, IP, roomNumber);
+	}
+	
+	public void stop(){
+		
+	}
+	
+	public void start(){
+		Thread thread = new Thread(capThread);
+		thread.start();
+	}
+	
+	/**
+	 * This method is called when the user closes the browser window containing the applet
+	 */
+	public void destroy(){
+		capThread.closeConnection();
+	}
+	
+	public void setScreenCoordinates(int x, int y){
+		capThread.capture.setX(x);
+		capThread.capture.setY(y);
+	}
+}
