ddd055b72f71ed890b259159a271836ba5215ee0
Richard Alam <ritzalam@gmail.com>
2009-08-24 18:53:29 +0000

    - removing...need to sort out license with Xuggler
    git-svn-id: http://bigbluebutton.googlecode.com/svn/trunk@2327 af16638f-c34d-0410-8cfa-b39d5352b314

--- deskshare-app/src/main/java/org/bigbluebutton/deskshare/CaptureMessage.java
+++
@@ -1,7 +0,0 @@
-package org.bigbluebutton.deskshare;
-
-public enum CaptureMessage {
-	CAPTURE_START,
-	CAPTURE_UPDATE,
-	CAPTURE_END
-}
--- deskshare-app/src/main/java/org/bigbluebutton/deskshare/NewScreenListener.java
+++
@@ -1,8 +0,0 @@
-package org.bigbluebutton.deskshare;
-
-import java.awt.image.BufferedImage;
-
-public interface NewScreenListener {
-
-	public void onNewScreen(BufferedImage newScreen);
-}
