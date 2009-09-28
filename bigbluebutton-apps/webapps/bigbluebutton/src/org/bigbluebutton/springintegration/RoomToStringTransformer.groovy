889984c53020a3397491a547a48bb24f4e7738a8
Jeremy Thomerson <jeremy@thomersonfamily.com>
2009-09-28 21:55:51 +0000

    have prototype of end-to-end JMS messaging for conference events with spring integration
    forgot to svn add one new package
    
    git-svn-id: http://bigbluebutton.googlecode.com/svn/trunk@2658 af16638f-c34d-0410-8cfa-b39d5352b314

---
+++ bigbluebutton-apps/webapps/bigbluebutton/src/org/bigbluebutton/springintegration/RoomToStringTransformer.groovy
@@ -0,0 +1,33 @@
+/*
+ * BigBlueButton - http://www.bigbluebutton.org
+ * 
+ * Copyright (c) 2008-2009 by respective authors (see below). All rights reserved.
+ * 
+ * BigBlueButton is free software; you can redistribute it and/or modify it under the 
+ * terms of the GNU Lesser General Public License as published by the Free Software 
+ * Foundation; either version 3 of the License, or (at your option) any later 
+ * version. 
+ * 
+ * BigBlueButton is distributed in the hope that it will be useful, but WITHOUT ANY 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License along 
+ * with BigBlueButton; if not, If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author Jeremy Thomerson <jthomerson@genericconf.com>
+ * $Id: $
+ */
+package org.bigbluebutton.springintegration
+
+import org.bigbluebutton.conference.Room;
+import org.springframework.integration.core.Message;
+import org.springframework.integration.message.StringMessage;
+
+public class RoomToStringTransformer {
+
+	public Message<?> transform(Room room) {
+		return new StringMessage(room.getName());
+	}
+
+}
