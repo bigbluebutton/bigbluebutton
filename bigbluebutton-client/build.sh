d088d6f476d20b08ad2532fdcd2a44d96ac240d2
Fred Dixon <ffdixon@gmail.com>
2009-11-05 22:08:38 +0000

    depreciated bbb-client/build.sh
    git-svn-id: http://bigbluebutton.googlecode.com/svn/trunk@2840 af16638f-c34d-0410-8cfa-b39d5352b314

--- bigbluebutton-client/build.sh
+++ bigbluebutton-client/build.sh
@@ -2,61 +2,14 @@
 
 #
 # BigBlueButton: Script to build/deploy the BigBlueButton client
+#
+echo "
+This script is depreciated.  Please see
 
+    http://code.google.com/p/bigbluebutton/wiki/Developing_BigBlueButton
 
-while [ $# -gt 0 ]; do    # Until you run out of parameters . . .
-  case "$1" in
-    -h|--help)
-                echo "
-Helper script to build and deploy the BigBlueButton client.
+for how to setup your development environment.
 
-Hint: To avoid entering your password with sudo, just type 
-
-	sudo ls
-	
-at the start of your development session to have sudo remember that you can become
-root.
+-- BigBlueButton Team
 "
-		exit 0
-              ;;
-  esac
-  shift       # Check next set of parameters.
-done
 
-
-#
-# Remove the bbb-client package if its already installed
-if dpkg-query -s bbb-client | grep "install ok installed" > /dev/null 2>&1; then
-        sudo apt-get purge --yes bbb-client
-fi
-
-
-#
-# Assign the local IP address
-#
-
-ant cleanandmake
-
-if [ $? -ne 0 ]; then
-	exit 1
-fi
-
-
-#
-# Copy over new client
-#
-
-sudo rm -rf /var/www/bigbluebutton
-sudo mkdir /var/www/bigbluebutton
-sudo cp -r ./client /var/www/bigbluebutton
-
-#
-# Adjust the IP for the config.xml to match local machine
-#
-
-OLD_IP="$(cat /var/www/bigbluebutton/client/conf/config.xml | sed 's/"//g' | sed -n '/<porttest /{s/.*host=//;s/ .*//;p}')"
-IP="$(ifconfig eth0 | sed -n '/inet /{s/.*addr://;s/ .*//;p}')"
-sudo sed -i "s/$OLD_IP/$IP/g" /var/www/bigbluebutton/client/conf/config.xml
-
-echo "New client installed in /var/www/bigbluebutton."
-echo "done"
