22080fd3d52f16bfaca66684c669820eb3f01836
Richard Alam <ritzalam@gmail.com>
2009-11-05 21:07:32 +0000

    - cleanup
    git-svn-id: http://svn.bigbluebutton.org/project-bbb/trunk@66 6ac8b576-4aa1-4e98-a958-8badfeb98c9d

--- deskshare-applet/src/main/java/org/bigbluebutton/deskshare/client/tiles/ChangedTilesListener.java
+++
@@ -1,8 +0,0 @@
-package org.bigbluebutton.deskshare.client.tiles;
-
-import java.io.ByteArrayOutputStream;
-
-public interface ChangedTilesListener {
-
-	public void onChangedTiles(ByteArrayOutputStream pixelData, boolean isKeyFrame);
-}
