34fbff77d1684a638ca9e39f1bca28fb2a73f4da
Richard Alam <ritzalam@gmail.com>
2009-12-31 19:47:05 +0000

    - handle new message from bbb-web and send to client
    git-svn-id: http://bigbluebutton.googlecode.com/svn/trunk@3168 af16638f-c34d-0410-8cfa-b39d5352b314

--- bigbluebutton-apps/src/main/groovy/org/bigbluebutton/conference/service/presentation/ConversionUpdatesMessageListener.groovy
+++ bigbluebutton-apps/src/main/groovy/org/bigbluebutton/conference/service/presentation/ConversionUpdatesMessageListener.groovy
@@ -34,6 +34,17 @@ public class ConversionUpdatesMessageListener implements MessageListener{
 	private static Logger log = Red5LoggerFactory.getLogger(ConversionUpdatesMessageListener.class, "bigbluebutton");
     
     private ConversionUpdatesProcessor conversionUpdatesProcessor
+
+	private static final String OFFICE_DOC_CONVERSION_SUCCESS_KEY = "OFFICE_DOC_CONVERSION_SUCCESS";
+    private static final String OFFICE_DOC_CONVERSION_FAILED_KEY = "OFFICE_DOC_CONVERSION_FAILED";
+    private static final String SUPPORTED_DOCUMENT_KEY = "SUPPORTED_DOCUMENT";
+    private static final String UNSUPPORTED_DOCUMENT_KEY = "UNSUPPORTED_DOCUMENT";
+    private static final String PAGE_COUNT_FAILED_KEY = "PAGE_COUNT_FAILED";
+    private static final String PAGE_COUNT_EXCEEDED_KEY = "PAGE_COUNT_EXCEEDED";	
+    private static final String GENERATED_SLIDE_KEY = "GENERATED_SLIDE";
+    private static final String GENERATING_THUMBNAIL_KEY = "GENERATING_THUMBNAIL";
+    private static final String GENERATED_THUMBNAIL_KEY = "GENERATED_THUMBNAIL";
+    private static final String CONVERSION_COMPLETED_KEY = "CONVERSION_COMPLETED";
     
 	public void start() {
 		log.debug "Starting conversion updates receiver."
@@ -56,49 +67,52 @@ public class ConversionUpdatesMessageListener implements MessageListener{
     	def code = mapMessage.getString("returnCode")
     	def room = mapMessage.getString("room")
     	def presentationName = mapMessage.getString("presentationName")
+    	def conference = mapMessage.getString("conference")
+    	def messageKey = mapMessage.getString("messageKey")
     	
 		Map message = new HashMap()
+    	message.put('conference', conference)
 		message.put('room', room)
 		message.put('code', code)
 		message.put('presentationName', presentationName)
+		message.put('messageKey', messageKey)
 		
-		log.debug "JMS: ${code}[$presentationName]"
+		log.debug "JMS: ${messageKey}[$presentationName]"
 		
-		switch (code) {
-			case 'SUCCESS':
-				log.debug "JMS: SUCCESS[$presentationName]"
-												
-				message.put('message', mapMessage.getStringProperty("message"))
+		switch (messageKey) {
+			case OFFICE_DOC_CONVERSION_SUCCESS_KEY :
+			case OFFICE_DOC_CONVERSION_FAILED_KEY :
+			case SUPPORTED_DOCUMENT_KEY :
+			case UNSUPPORTED_DOCUMENT_KEY :
+			case GENERATING_THUMBNAIL_KEY :
+			case GENERATED_THUMBNAIL_KEY :
+			case PAGE_COUNT_FAILED_KEY :
+				log.debug "JMS: ${messageKey}[$presentationName]"
 				conversionUpdatesProcessor.process(message)
 				break
-			case 'THUMBNAILS':
-				log.debug "JMS: THUMBNAILS[$presentationName]"
+			
+			case PAGE_COUNT_EXCEEDED_KEY :
+				log.debug "JMS: ${messageKey}[$presentationName]"
+				def numberOfPages = mapMessage.getInt("numberOfPages")
+				def maxNumberPages = mapMessage.getInt("maxNumberPages")
+				message.put('numberOfPages', numberOfPages)
+				message.put('maxNumberPages', maxNumberPages)
 				conversionUpdatesProcessor.process(message)
 				break
-			case 'FAILED_CONVERT_FORMAT':
-			case 'FAILED_CONVERT_NOT_SUPPORTED':
-			case 'FAILED_CONVERT_SOFFICE':
-			case 'FAILED_CONVERT_NBPAGE':
-			case 'FAILED_CONVERT_MAXNBPAGE_REACH':
-			case 'FAILED_CONVERT_SWF':
-			case 'FAILED_CONVERT_SWF_IMAGE':
-			case 'FAILED_CONVERT_SWF_PDF':
-			case 'FAILED_CONVERT_THUMBNAIL':
-				log.debug "JMS: ${code}[$presentationName]"
-				conversionUpdatesProcessor.process(message)
-				break;
-			/* Obsolete */
-			case 'FAILED_CONVERT':
-				log.debug "JMS: FAILED_CONVERT[$presentationName]"
-				conversionUpdatesProcessor.process(message)
-				break
-			case 'CONVERT':
-				def totalSlides = mapMessage.getInt("totalSlides")
-				def completedSlides = mapMessage.getInt("slidesCompleted")
-				message.put('totalSlides', totalSlides)
-				message.put('completedSlides', completedSlides)
+
+			case GENERATED_SLIDE_KEY:
+				def numberOfPages = mapMessage.getInt("numberOfPages")
+				def pagesCompleted = mapMessage.getInt("pagesCompleted")
+				message.put('numberOfPages', numberOfPages)
+				message.put('pagesCompleted', pagesCompleted)
 				
-				log.debug "JMS: CONVERT[$presentationName, $completedSlides, $totalSlides]"
+				log.debug "JMS: ${messageKey}[$presentationName]"
+				conversionUpdatesProcessor.process(message)
+				break
+			case CONVERSION_COMPLETED_KEY:
+				def slidesInfo = mapMessage.getString("slidesInfo")
+				message.put('slidesInfo', slidesInfo)				
+				log.debug "JMS: ${messageKey}[$presentationName]"
 				conversionUpdatesProcessor.process(message)
 				break
 			default:
--- bigbluebutton-apps/src/main/groovy/org/bigbluebutton/conference/service/presentation/PresentationEventRecorder.groovy
+++ bigbluebutton-apps/src/main/groovy/org/bigbluebutton/conference/service/presentation/PresentationEventRecorder.groovy
@@ -31,7 +31,18 @@ import groovy.xml.MarkupBuilder
 
 public class PresentationEventRecorder implements IEventRecorder, IPresentationRoomListener {
 	private static Logger log = Red5LoggerFactory.getLogger( PresentationEventRecorder.class, "bigbluebutton" )
-	
+
+	private static final String OFFICE_DOC_CONVERSION_SUCCESS_KEY = "OFFICE_DOC_CONVERSION_SUCCESS";
+    private static final String OFFICE_DOC_CONVERSION_FAILED_KEY = "OFFICE_DOC_CONVERSION_FAILED";
+    private static final String SUPPORTED_DOCUMENT_KEY = "SUPPORTED_DOCUMENT";
+    private static final String UNSUPPORTED_DOCUMENT_KEY = "UNSUPPORTED_DOCUMENT";
+    private static final String PAGE_COUNT_FAILED_KEY = "PAGE_COUNT_FAILED";
+    private static final String PAGE_COUNT_EXCEEDED_KEY = "PAGE_COUNT_EXCEEDED";	
+    private static final String GENERATED_SLIDE_KEY = "GENERATED_SLIDE";
+    private static final String GENERATING_THUMBNAIL_KEY = "GENERATING_THUMBNAIL";
+    private static final String GENERATED_THUMBNAIL_KEY = "GENERATED_THUMBNAIL";
+    private static final String CONVERSION_COMPLETED_KEY = "CONVERSION_COMPLETED";
+    
 	IRecorder recorder
 	private ISharedObject so
 	private final Boolean record
@@ -59,45 +70,61 @@ public class PresentationEventRecorder implements IEventRecorder, IPresentationR
 	}
 	
 	def sendUpdateMessage = {message ->
-		Map <String, Object> update = new HashMap<String, Object>();
+		handleReceivedMessage(message)
+	}
 
-		switch (message['code']) {
-			case 'SUCCESS':
-				update.put('returnCode', message['code'])
-				update.put("presentationName", message['presentationName'])
-				update.put("message", message['message'])
+	private def handleReceivedMessage = { message ->
+    	def code = message['returnCode']
+    	def room = message['room']
+    	def presentationName = message['presentationName']
+    	def conference = message['conference']
+    	def messageKey = message['messageKey']
+    	
+    	List<Object> list = new ArrayList<Object>();
+		list.add(conference);
+		list.add(room);
+		list.add(code);
+		list.add(presentationName);
+		list.add(messageKey);
+					
+		switch (messageKey) {
+			case OFFICE_DOC_CONVERSION_SUCCESS_KEY :
+			case OFFICE_DOC_CONVERSION_FAILED_KEY :
+			case SUPPORTED_DOCUMENT_KEY :
+			case UNSUPPORTED_DOCUMENT_KEY :
+			case GENERATING_THUMBNAIL_KEY :
+			case GENERATED_THUMBNAIL_KEY :
+			case PAGE_COUNT_FAILED_KEY :
+				log.debug "${messageKey}[$presentationName]"
+				// no extra data to send
+				so.sendMessage("conversionUpdateMessageCallback", list)
 				break
-			case 'FAILED_CONVERT_FORMAT':
-			case 'FAILED_CONVERT_NOT_SUPPORTED':
-			case 'FAILED_CONVERT_SOFFICE':
-			case 'FAILED_CONVERT_NBPAGE':
-			case 'FAILED_CONVERT_MAXNBPAGE_REACH':
-			case 'FAILED_CONVERT_SWF':
-			case 'FAILED_CONVERT_SWF_IMAGE':
-			case 'FAILED_CONVERT_SWF_PDF':
-			case 'FAILED_CONVERT_THUMBNAIL':
-				update.put('returnCode', message['code'])
-				break;
-			/* Deprecated */
-			case 'UPDATE':
-			case 'FAILED':
-			case 'FAILED_CONVERT':
-				update.put('returnCode', message['code'])
-				update.put("message", message['message'])
+			
+			case PAGE_COUNT_EXCEEDED_KEY :
+				log.debug "${messageKey}[$presentationName]"
+				def numberOfPages = message['numberOfPages']
+				def maxNumberPages = message['maxNumberPages']
+				list.add(numberOfPages)
+				list.add(maxNumberPages)
+				so.sendMessage("pageCountExceededUpdateMessageCallback", list)
 				break
-			case 'THUMBNAILS':
-				update.put('returnCode', message['code'])
-				update.put("presentationName", message['presentationName'])
+			case GENERATED_SLIDE_KEY:
+				log.debug "${messageKey}[$presentationName]"
+				def numberOfPages = message['numberOfPages']
+				def pagesCompleted = message['pagesCompleted']
+				list.add(numberOfPages)
+				list.add(pagesCompleted)
+				so.sendMessage("generatedSlideUpdateMessageCallback", list)
 				break
-			case 'CONVERT':
-				update.put('returnCode', message['code'])
-				update.put("totalSlides", message['totalSlides'])
-				update.put("completedSlides", message['completedSlides'])
-				break		
-		}
-
-		log.debug "Send SO message $update"
-		so.setAttribute("updateMessage", update)
+			case CONVERSION_COMPLETED_KEY:
+				log.debug "${messageKey}[$presentationName]"
+				def slidesInfo = message['slidesInfo']
+				list.add(slidesInfo)								
+				so.sendMessage("conversionCompletedUpdateMessageCallback", list)
+				break
+			default:
+				log.error("Cannot handle recieved message.")
+		}			
 	}
 	
 	def assignPresenter = {userid, name, assignedBy ->	
