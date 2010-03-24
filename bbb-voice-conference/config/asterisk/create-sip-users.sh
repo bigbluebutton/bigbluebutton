1dbc14c930fc07066cd41f3166f4d1a12a30c6cd
Richard Alam <ritzalam@gmail.com>
2010-03-24 19:30:10 +0000

    - add script to create sip users
    git-svn-id: http://bigbluebutton.googlecode.com/svn/trunk@3997 af16638f-c34d-0410-8cfa-b39d5352b314

---
+++ bbb-voice-conference/config/asterisk/create-sip-users.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Create 100 users.
+for i in {34380..34479}
+do
+  echo "
+
+[$i]
+type=friend
+username=$i
+insecure=very
+qualify=no
+nat=yes
+host=dynamic
+canreinvite=no
+context=demo-bbb-voip
+disallow=all
+allow=ulaw
+" >>  bbb_sip.conf
+done
+
+
