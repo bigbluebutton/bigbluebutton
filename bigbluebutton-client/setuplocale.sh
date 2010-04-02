281c32bbbaedd3c587e8c3693588c92a199c0fec
Richard Alam <ritzalam@gmail.com>
2010-04-02 17:42:37 +0000

    - handle spaces properly when parsing language array
    git-svn-id: http://bigbluebutton.googlecode.com/svn/trunk@4094 af16638f-c34d-0410-8cfa-b39d5352b314

--- bigbluebutton-client/setuplocale.sh
+++ bigbluebutton-client/setuplocale.sh
@@ -3,7 +3,7 @@
 #
 # Setup the locale for BigBlueButton
 #
-NEW_LANG=$(cat ./src/org/bigbluebutton/util/i18n/ResourceUtil.as | tr -d '\r' | sed -n "/localeChain:Array/{s/.*\[ //g;s/\];//g;s/\"//g;s/,//g;p}")
+NEW_LANG=$(cat ./src/org/bigbluebutton/util/i18n/ResourceUtil.as | tr -d '\r' | sed -n "/localeChain:Array/{s/.*\[[ ]*//g;s/[ ]*\];//g;s/\"//g;s/,//g;p}")
 echo "Checking languages: $NEW_LANG"
 
 CUR_DIR=$(pwd)
