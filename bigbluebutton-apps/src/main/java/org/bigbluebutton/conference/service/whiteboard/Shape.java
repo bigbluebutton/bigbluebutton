f00f568200f5df104dc51349f9a376a4c0aa6726
Denis Zgonjanin <me.snap@gmail.com>
2010-05-12 16:27:12 +0000

    Optimization for whiteboard freehand shapes. Removed unnecessary data points. This improves bandwidth usage, and doesn't choke Flash Player as much when there are a lot of shapes drawn on the same page.
    git-svn-id: http://bigbluebutton.googlecode.com/svn/trunk@4301 af16638f-c34d-0410-8cfa-b39d5352b314

--- bigbluebutton-apps/src/main/java/org/bigbluebutton/conference/service/whiteboard/Shape.java
+++ bigbluebutton-apps/src/main/java/org/bigbluebutton/conference/service/whiteboard/Shape.java
@@ -1,5 +1,7 @@
 package org.bigbluebutton.conference.service.whiteboard;
 
+import java.util.ArrayList;
+
 import org.red5.compatibility.flex.messaging.io.ArrayCollection;
 
 public class Shape {
@@ -11,6 +13,10 @@ public class Shape {
 	private double parentHeight;
 	private double[] shape;
 	
+	public static final String PENCIL = "pencil";
+	public static final String RECTANGLE = "rectangle";
+	public static final String ELLIPSE = "ellipse";
+	
 	public Shape(double[] shape, String type, int color, int thickness, double parentWidth, double parentHeight){
 		this.shape = shape;
 		this.type = type;
@@ -18,6 +24,8 @@ public class Shape {
 		this.thickness = thickness;
 		this.parentWidth = parentWidth;
 		this.parentHeight = parentHeight;
+		
+		if(this.type.equalsIgnoreCase(PENCIL)) this.shape = optimizeFreeHand();
 	}
 	
 	public ArrayCollection<Object> toList(){
@@ -40,5 +48,55 @@ public class Shape {
 		objects[4] = parentWidth;
 		objects[5] = parentHeight;
 		return objects;
+	}
+	
+	private double[] optimizeFreeHand(){
+		if (shape.length < 10) return shape; //Don't do any optimization for very small shapes
+		
+		ArrayList<Double> newShape = new ArrayList<Double>();
+		
+		double x1 = shape[0];
+		double y1 = shape[1];
+		newShape.add(x1);
+		newShape.add(y1);
+		double stableSlope = 0;
+		double newSlope;
+		double lastStableX = x1;
+		double lastStableY = y1;
+
+		for (int i=2; i<shape.length; i= i+2){
+			double x2 = shape[i];
+			double y2 = shape[i+1];
+			
+			newSlope = (y2 - y1)/(x2 - x1);
+			//System.out.println("Slope change: " + (Math.abs(stableSlope / newSlope - 1)));
+			if (slopeDifference(stableSlope, newSlope) < 5){
+				lastStableX = x2;
+				lastStableY = y2;
+			} else{
+				stableSlope = newSlope;
+				x1 = x2;
+				y1 = y2;
+				newShape.add(x1);
+				newShape.add(y1);
+			}
+		}
+		newShape.add(shape[shape.length - 2]);
+		newShape.add(shape[shape.length - 1]);
+		
+		double[] returnArray = new double[newShape.size()];
+		for (int j= 0; j<newShape.size(); j++){
+			returnArray[j] = newShape.get(j);
+		}
+		
+		//System.out.println("Original: " + shape.length);
+		//System.out.println("Optimized: " + returnArray.length);
+		return returnArray;
+	}
+	
+	private double slopeDifference(double oldSlope, double newSlope){
+		double differenceInRad = Math.atan(oldSlope) - Math.atan(newSlope);
+		//System.out.println(Math.abs(Math.toDegrees(differenceInRad)));
+		return Math.abs(Math.toDegrees(differenceInRad));
 	}
 }
