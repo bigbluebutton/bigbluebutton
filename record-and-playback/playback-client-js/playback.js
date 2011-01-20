5f740739658c2cfd87deb234decc25dee146ddee
deniszgonjanin <deniszgonjanin@gmail.com>
2011-01-20 08:22:46 +0000

    Playback for slides and audio, for xml events.

---
+++ record-and-playback/playback-client-js/playback.js
@@ -0,0 +1,85 @@
+(function($){
+	
+	var slideTransitions = new Array();
+	var audio;
+	var meetingTime;
+	
+	var RECORDING = 'testmeeting'
+	var PRESENTATION = 'testmeeting/ecosystem'
+	
+	//$.fn.bbb.playback = function(){
+	//	
+	//};
+	
+	$(document).ready(function(){
+		$.ajax({
+			type: "GET",
+			url: "testmeeting/manifest.xml",
+			dataType: "xml",
+			success: parseXml
+		});
+		
+	});
+	
+	window.addEventListener('load', function(){
+		audio = document.getElementById('audioRecording');
+		audio.setAttribute('src', RECORDING + '/testmeeting.ogg');
+		audio.load();
+		
+		audio.addEventListener('load', function(){
+			audio.play();
+		}, true);
+		
+		audio.addEventListener('timeupdate', onTimeUpdate, false);
+	}, false);
+	
+	function parseXml(xml){
+		//Find the timestamp the meeting started, so we can calculate relative offsets of events.
+		var start;
+		$(xml).find("participants").each(function(){
+			var participant = $(this);
+			if (participant.attr('event') === 'join'){
+				start = participant.attr('timestamp');
+				return false; //Breaks the each() loop
+			}
+		});
+		
+		$(xml).find("participants").each(function(){
+			var participant = $(this);
+			if (participant.attr('event') === 'leave_all'){
+				meetingTime = participant.attr('timestamp') - start;
+			}
+		});
+		
+		var i = 0;
+		$(xml).find("presentation").each(function(){
+			var element = $(this);
+			if (element.attr('event') === 'update_slide'){
+				var transitionEvent = {
+					slide : element.attr('slide'),
+					time : (element.attr('timestamp') - start) / 1000
+				};
+				slideTransitions[i] = transitionEvent;
+				i++;
+			}
+		});
+		
+	}
+	
+	function onTimeUpdate(){
+		var now = audio.currentTime;
+		
+		$.each(slideTransitions, function(index, value){
+			var time = value.time;
+			
+			if (slideTransitions[index + 1] == null) return; //Break for last slide, to avoid null reference error
+			
+			if (time < now && slideTransitions[index + 1].time > now){
+				var slideIndex = parseInt(value.slide) + 1; //A workaround for Issue 821
+				var slideToShow = PRESENTATION + '/slide-' + slideIndex + '.png';
+				$('#imgSlide').attr('src', slideToShow);
+			} 
+		});
+	}
+	
+})(jQuery);
