43823b05c307fc380df8fe386620dbc701b50729
BigBlueButton <firstuser@bbb-vm-20110129-04.localdomain>
2011-01-30 20:34:32 +0000

    RTMPAdapter for red5. Initial commit

---
+++ bigbluebutton-apps/src/main/java/org/bigbluebutton/conference/service/rtmpadapter/RTMPAdapterApp.java
@@ -0,0 +1,109 @@
+/** 
+* ===License Header===
+*
+* BigBlueButton open source conferencing system - http://www.bigbluebutton.org/
+*
+* Copyright (c) 2010 BigBlueButton Inc. and by respective authors (see below).
+*
+* This program is free software; you can redistribute it and/or modify it under the
+* terms of the GNU Lesser General Public License as published by the Free Software
+* Foundation; either version 2.1 of the License, or (at your option) any later
+* version.
+*
+* BigBlueButton is distributed in the hope that it will be useful, but WITHOUT ANY
+* WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+* PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public License along
+* with BigBlueButton; if not, see <http://www.gnu.org/licenses/>.
+* 
+* ===License Header===
+*/
+package org.bigbluebutton.conference.service.whiteboard;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.red5.compatibility.flex.messaging.io.ArrayCollection;
+import org.red5.logging.Red5LoggerFactory;
+import org.red5.server.adapter.IApplication;
+import org.red5.server.adapter.MultiThreadedApplicationAdapter;
+import org.red5.server.api.IClient;
+import org.red5.server.api.IConnection;
+import org.red5.server.api.IScope;
+import org.red5.server.api.Red5;
+import org.red5.server.api.so.ISharedObject;
+import org.slf4j.Logger;
+import redis.clients.jedis.Jedis;
+
+public class RTMPAdapterApp extends MultiThreadedApplicationAdapter implements IApplication {
+	
+	private static Logger log = Red5LoggerFactory.getLogger(WhiteboardApplication.class, "bigbluebutton");
+	private static final String APP = "RTMPAdapter";
+	private Jedis jedis;	
+
+	@Override
+	public boolean appStart(IScope app){
+		log.info("Starting RTMPAdapterApp");
+		this.scope = app;
+	
+		jedis = new Jedis("localhost:6379");
+		return true;
+	}
+	
+	@Override
+	public void appStop(IScope scope){
+	}
+	
+	@Override
+	public boolean appConnect(IConnection conn, Object[] params) {
+		return true;
+	}
+
+	@Override
+	public void appDisconnect(IConnection conn) {
+	}
+
+	@Override
+	public boolean appJoin(IClient client, IScope scope) {
+		return true;
+	}
+
+	@Override
+	public void appLeave(IClient client, IScope scope) {
+
+	}
+
+	@Override
+	public boolean roomConnect(IConnection connection, Object[] params) {
+    	return true;
+	}
+
+	@Override
+	public void roomDisconnect(IConnection connection) {
+
+	}
+
+	@Override
+	public boolean roomJoin(IClient client, IScope scope) {
+		return true;
+	}
+
+	@Override
+	public void roomLeave(IClient client, IScope scope) {
+	}
+
+	@Override
+	public boolean roomStart(IScope scope) {
+    		return true;
+	}
+
+	@Override
+	public void roomStop(IScope scope) {
+	}
+	
+	private IScope getLocalScope(){
+		return Red5.getConnectionLocal().getScope();
+	}
+	
+}
