38af89450e4a46109f993a1ded35374fef7416cc
Richard Alam <ritzalam@gmail.com>
2011-02-02 11:41:31 -0500

    - changed to handle new event names  - modified so that it handles multiple presentations properly

--- record-and-playback/playback-client-js/playback.js
+++ record-and-playback/playback-client-js/playback.js
@@ -1,11 +1,13 @@
 (function($){
 	
 	var slideTransitions = new Array();
+    var presentations = new Array();
+    var presentationName;
 	var audio;
 	var meetingTime;
 	
-	var RECORDING = 'testmeeting'
-	var PRESENTATION = 'testmeeting/ecosystem'
+	var RECORDING = 'workspace'
+	var PRESENTATION = 'workspace/presentations/'
 	var LOGO = 'logo.png';
 	
 	//$.fn.bbb.playback = function(){
@@ -15,7 +17,7 @@
 	$(document).ready(function(){
 		$.ajax({
 			type: "GET",
-			url: "testmeeting/manifest.xml",
+			url: "workspace/event.xml",
 			dataType: "xml",
 			success: parseXml
 		});
@@ -24,7 +26,7 @@
 	
 	window.addEventListener('load', function(){
 		audio = document.getElementById('audioRecording');
-		audio.setAttribute('src', RECORDING + '/testmeeting.ogg');
+		audio.setAttribute('src', RECORDING + '/recording.ogg');
 		audio.load();
 		
 		audio.addEventListener('load', function(){
@@ -35,36 +37,50 @@
 	}, false);
 	
 	function parseXml(xml){
-		//Find the timestamp the meeting started, so we can calculate relative offsets of events.
+		//Find the timestamp the meeting started, so we can calculate relative offsets of events		
 		var start;
-		$(xml).find("participants").each(function(){
-			var participant = $(this);
-			if (participant.attr('event') === 'join'){
-				start = participant.attr('timestamp');
+		$(xml).find("event").each(function(){
+			var event = $(this);
+			if (event.attr('name') === 'ParticipantJoinEvent'){
+				start = event.attr('timestamp');
 				return false; //Breaks the each() loop
 			}
 		});
 		
-		$(xml).find("participants").each(function(){
-			var participant = $(this);
-			if (participant.attr('event') === 'leave_all'){
-				meetingTime = participant.attr('timestamp') - start;
+		$(xml).find("event").each(function(){
+			var event = $(this);
+			if (event.attr('name') === 'EndAndKickAllEvent'){
+				meetingTime = event.attr('timestamp') - start;
+			}
+		});
+
+        var p = 0;
+		$(xml).find("event").each(function(){
+			var event = $(this);
+			if (event.attr('name') === 'SharePresentationEvent'){
+				var sharePresentationEvent = {
+
+					name : event.find('presentationName').text(),                    
+					time : (event.attr('timestamp') - start) / 1000
+				};
+                alert('presentationName=' + event.find('presentationName').text());
+				presentations[p] = sharePresentationEvent;
+                p++;
 			}
 		});
 		
 		var i = 0;
-		$(xml).find("presentation").each(function(){
-			var element = $(this);
-			if (element.attr('event') === 'update_slide'){
+		$(xml).find("event").each(function(){
+			var event = $(this);
+			if (event.attr('name') === 'GotoSlideEvent'){
 				var transitionEvent = {
-					slide : element.attr('slide'),
-					time : (element.attr('timestamp') - start) / 1000
+					slide : event.find('slide').text(),                    
+					time : (event.attr('timestamp') - start) / 1000
 				};
 				slideTransitions[i] = transitionEvent;
 				i++;
 			}
 		});
-		
 	}
 	
 	function onTimeUpdate(){
@@ -76,6 +92,14 @@
 			return;
 		}
 		
+		$.each(presentations, function(index, value){
+			var time = value.time;
+			
+			if (time < now && presentations[index].time < now){
+				presentationName = presentations[index].name				
+			} 
+		});        
+        
 		$.each(slideTransitions, function(index, value){
 			var time = value.time;
 			
@@ -83,10 +107,14 @@
 			
 			if (time < now && slideTransitions[index + 1].time > now){
 				var slideIndex = parseInt(value.slide) + 1; //A workaround for Issue 821
-				var slideToShow = PRESENTATION + '/slide-' + slideIndex + '.png';
+				var slideToShow = PRESENTATION + presentationName + '/slide-' + slideIndex + '.png';
 				$('#imgSlide').attr('src', slideToShow);
+                $('#slide').text = slideToShow;
 			} 
 		});
+        
+        
 	}
 	
 })(jQuery);
+
