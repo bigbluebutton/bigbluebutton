0f0e02de713f1414ccfa25e4fc1cf6ff9920263d
Richard Alam <ritzalam@gmail.com>
2011-02-14 14:17:05 -0500

    - connect to Jedis for each call for now until we figure out how not to get disconnected from Redis after 300sec

--- bigbluebutton-apps/src/main/java/org/bigbluebutton/conference/service/recorder/RedisDispatcher.java
+++ bigbluebutton-apps/src/main/java/org/bigbluebutton/conference/service/recorder/RedisDispatcher.java
@@ -1,24 +1,57 @@
 package org.bigbluebutton.conference.service.recorder;
 
+import org.apache.commons.pool.impl.GenericObjectPool.Config;
+
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisException;
 import redis.clients.jedis.JedisPool;
 
 public class RedisDispatcher implements Recorder {
-
-	//private String server;
-	//private int port;
 	private static final String COLON=":";
-	Jedis jedis;
+	private String host;
+	private int port;
 	
+//	Jedis jedis;
+	JedisPool jpool;
 	public RedisDispatcher(String host, int port){
-		//JedisPool jpool=new JedisPool("localhost", 6379);
-		jedis = new Jedis(host, port);		
+		this.host = host;
+		this.port = port;
+		
+//		jedis = new Jedis(host, port);		
+//		Config poolConfig = new Config();
+//		jpool = new JedisPool(poolConfig, host, port);
 	}
 	
 	@Override
 	public void record(String session, RecordEvent message) {
+		Jedis jedis = new Jedis(host, port);
 		Long msgid = jedis.incr("global:nextRecordedMsgId");
 		jedis.hmset("recording" + COLON + session + COLON + msgid, message.toMap());
-		jedis.rpush("meeting" + COLON + session + COLON + "recordings", msgid.toString());
+		jedis.rpush("meeting" + COLON + session + COLON + "recordings", msgid.toString());						
 	}
+/*	
+	@Override
+	public void record(String session, RecordEvent message) {
+		boolean tryAgain = true;
+		int howMany = 10;
+		while (tryAgain) {
+			Jedis jedis = jpool.getResource();
+			try {
+				Long msgid = jedis.incr("global:nextRecordedMsgId");
+				jedis.hmset("recording" + COLON + session + COLON + msgid, message.toMap());
+				jedis.rpush("meeting" + COLON + session + COLON + "recordings", msgid.toString());
+				jpool.returnResource(jedis);				
+			} catch(JedisException e) {
+				System.out.println("Failed to get redis connection...trying again...");
+				if (howMany < 0) {
+					tryAgain = false;
+				} else {
+					howMany--;
+					jpool.returnBrokenResource(jedis);
+				}
+			}
+			
+		}
+	}
+*/
 }
