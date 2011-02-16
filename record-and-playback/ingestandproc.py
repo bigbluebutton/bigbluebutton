5f535d386fdc42c8181f34299d1ba187c05de4f3
Richard Alam <ritzalam@gmail.com>
2011-02-16 17:48:30 -0500

    - make location of scripts configurable and playback url

--- record-and-playback/ingestandproc.py
+++ record-and-playback/ingestandproc.py
@@ -32,9 +32,14 @@ def main():
     redisPort = 0
     ingestDir = ""
     publishDir = ""
+    scriptsDir = ""
+    playbackHost = ""
     
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "hm:a:p:r:e:o:i:b:", ["help", "meeting-id=", "audio-dir=", "presentation-dir=", "archive-dir=", "redis-host=", "redis-port=", "ingest-dir=", "publish-dir="])
+        opts, args = getopt.getopt(sys.argv[1:], "hm:a:p:r:e:o:i:b:s:k:", ["help", "meeting-id=", "audio-dir=", 
+                                                "presentation-dir=", "archive-dir=", "redis-host=", 
+                                                "redis-port=", "ingest-dir=", "publish-dir=",
+                                                "scripts-dir=", "playback-host="])
     except getopt.GetoptError, err:
         print str(err)
         usage()
@@ -59,6 +64,10 @@ def main():
             ingestDir = a
         elif o in ("-b", "--publish-dir"):
             publishDir = a
+        elif o in ("-s", "--scripts-dir"):
+            scriptsDir = a
+        elif o in ("-k", "--playback-host"):
+            playbackHost = a
         else:
             assert False, "unhandled option"
     
@@ -85,7 +94,13 @@ def main():
         print "Missing ingest dir."
         printUsage = True  
     if (publishDir == ""):
-        print "Missing publishDir."
+        print "Missing publish dir."
+        printUsage = True  
+    if (scriptsDir == ""):
+        print "Missing scripts dir."
+        printUsage = True  
+    if (playbackHost == ""):
+        print "Missing playback host."
         printUsage = True  
     if (printUsage):
         printUsageHelp()
@@ -98,25 +113,25 @@ def main():
 # python publish.py -m e0401b38-0ed1-4526-b7d0-50e28211c9c4 -i /var/bigbluebutton/ingest -p /var/bigbluebutton/recordings
 
  
-    command = "python /home/firstuser/python/src/archive.py -m " + meetingId + " -a " + audioSrcDir + " -p " + presentationSrcDir + " -r " + archiveDir
+    command = "python " + scriptsDir + "/archive.py -m " + meetingId + " -a " + audioSrcDir + " -p " + presentationSrcDir + " -r " + archiveDir
     print command
     
     proc = subprocess.Popen(command, shell=True)
     proc.wait()
     
-    command = "python /home/firstuser/python/src/get-events.py -m " + meetingId + " -r " + redisHost + " -p " + redisPort + " -a " + archiveDir
+    command = "python " + scriptsDir + "/get-events.py -m " + meetingId + " -r " + redisHost + " -p " + redisPort + " -a " + archiveDir
     print command
     
     proc = subprocess.Popen(command, shell=True)
     proc.wait()
 
-    command = "python /home/firstuser/python/src/ingest.py -m " + meetingId + " -i " + ingestDir + " -a " + archiveDir
+    command = "python " + scriptsDir + "/ingest.py -m " + meetingId + " -i " + ingestDir + " -a " + archiveDir
     print command
     
     proc = subprocess.Popen(command, shell=True)
     proc.wait()
     
-    command = "python /home/firstuser/python/src/process-audio.py -m " + meetingId + " -i " + ingestDir
+    command = "python " + scriptsDir + "/process-audio.py -m " + meetingId + " -i " + ingestDir
     print command
     proc = subprocess.Popen(command, shell=True)
     proc.wait()
@@ -129,7 +144,7 @@ def main():
         pname = p.find('presentationName').text
         presentationNames.append(pname)
     
-    command = "python /home/firstuser/python/src/gen-slides.py -p " + meetingArchiveDir + "/presentations/" 
+    command = "python " + scriptsDir + "/gen-slides.py -p " + meetingArchiveDir + "/presentations/" 
     
     for p in presentationNames:
         genSlidesCmd = command + p + " -f " + p + ".pdf"
@@ -137,7 +152,7 @@ def main():
         proc = subprocess.Popen(genSlidesCmd, shell=True)
         proc.wait()
 
-    command = "python /home/firstuser/python/src/publish.py -m " + meetingId + " -i " + ingestDir + " -p " + publishDir
+    command = "python " + scriptsDir + "/publish.py -m " + meetingId + " -i " + ingestDir + " -p " + publishDir + " -k " + playbackHost
     print command
     proc = subprocess.Popen(command, shell=True)
     proc.wait()
