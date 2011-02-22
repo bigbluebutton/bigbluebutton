3a19058d540b533368b97f92569dd0cf55804e62
Marco Calderon <mcmarkos86@gmail.com>
2011-02-22 21:45:20 +0000

    changes in bbb-apps redis pubsub

---
+++ bigbluebutton-apps/src/main/java/org/bigbluebutton/conference/service/recorder/RedisListener.java
@@ -0,0 +1,32 @@
+package org.bigbluebutton.conference.service.recorder;
+
+
+public class RedisListener extends RedisServer implements Runnable {
+
+	private PubsubListener pubsubListener;
+	
+	public RedisListener(String server, int port) {
+		super(server, port);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void subscribe(){
+		Thread t= new Thread(this);
+		t.start();
+	}
+	
+	@Override
+	public void run() {
+		jedis.subscribe(pubsubListener, "bbbConferenceEvents");
+	}
+	
+	public PubsubListener getPubsubListener() {
+		return pubsubListener;
+	}
+	public void setPubsubListener(PubsubListener pubsubListener) {
+		System.out.println("setting pubsub");
+		this.pubsubListener = pubsubListener;
+		
+	}
+
+}
