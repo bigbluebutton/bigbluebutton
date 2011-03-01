d2c54b90bdc43ffb833cdfeee8b89abd25ab84d1
Jeremy Thomerson <jeremy@thomersonfamily.com>
2011-03-01 14:49:30 -0600

    enhance vm-build-debug.sh to exit when there is a build failure
    rather than continuing to start red5 with a broken build

--- bigbluebutton-apps/vm-build-debug.sh
+++ bigbluebutton-apps/vm-build-debug.sh
@@ -5,11 +5,11 @@ sudo /etc/init.d/red5 stop
 
 # build
 cd ~/dev/source/bigbluebutton/bigbluebutton-apps
-gradle resolveDeps war deploy
+gradle resolveDeps war deploy || exit
 
 # run
 cd /usr/share/red5/
-sudo -u red5 ./red5.sh 
+sudo -u red5 ./red5.sh || exit
 
 #return to working dir
 popd
