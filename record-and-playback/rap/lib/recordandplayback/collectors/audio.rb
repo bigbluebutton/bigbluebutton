c5526c1df317e4f8376566ba90048d7bee67625d
Richard Alam <ritzalam@gmail.com>
2011-04-04 09:07:59 -0400

    - add video collector

--- record-and-playback/rap/lib/recordandplayback/collectors/audio.rb
+++ record-and-playback/rap/lib/recordandplayback/collectors/audio.rb
@@ -64,7 +64,35 @@ module Collector
             Dir.glob("#{from_dir}/*.flv").each { |file|
                 FileUtils.cp(file, to_dir)
             }         
+        end       
+    end
+    
+    class Deskshare             
+        def location_exist?(location)
+            FileTest.directory?(location)
         end
         
+        def video_present?(meeting_id, location)
+            Dir.glob("#{location}/*.flv").empty?
+        end
+        
+        
+        def collect_video(meeting_id, from_dir, to_dir)         
+            if not location_exist?(from_dir) 
+                raise NoSuchDirectoryException, "Directory not found #{from_dir}"
+            end
+            
+            if not location_exist?(to_dir)
+                raise NoSuchDirectoryException, "Directory not found #{to_dir}"
+            end
+            
+            if (video_present?(meeting_id, from_dir))
+                raise NoVideoFileException, "No video recording for #{meeting_id} in #{from_dir}"
+            end
+                       
+            Dir.glob("#{from_dir}/*.flv").each { |file|
+                FileUtils.cp(file, to_dir)
+            }         
+        end       
     end
 end
---
+++ record-and-playback/rap/spec/recordandplayback/video_stubbed_collector_spec.rb
@@ -0,0 +1,52 @@
+require 'spec_helper'
+require 'fileutils'
+
+module Collector
+    describe Video do     
+        context "#success" do
+            it "should copy audio recording to archive" do
+                FileTest.stub(:directory?).and_return(true)
+                FileUtils.stub(:cp)
+                Dir.stub(:glob).and_return(['file1.flv', 'file2.flv'])
+                from_dir = 'from'
+                to_dir = 'to'
+                meeting_id = 'meeting-id'
+                archiver = Collector::Video.new
+                expect { archiver.collect_video( meeting_id, from_dir, to_dir ) }.to_not raise_error   
+            end
+        end
+        
+        context "#fail" do
+            it "should raise from directory not found exception" do
+                FileTest.stub(:directory?).and_return(false)
+                FileUtils.stub(:cp)
+                Dir.stub(:glob).and_return(['file1.wav', 'file2.wav'])
+                from_dir = '/from-dir-not-found'
+                to_dir = 'resources/archive'
+                meeting_id = 'meeting-id'
+                archiver = Collector::Video.new
+                expect {archiver.collect_video( meeting_id, from_dir, to_dir )}.to raise_error(NoSuchDirectoryException)
+            end
+            it "should raise to directory not found exception" do
+                from_dir = 'resources/raw/audio'
+                to_dir = '/to-dir-not-found'
+                meeting_id = 'meeting-id'
+                FileTest.stub(:directory?).and_return(false)
+                FileUtils.stub(:cp)
+                Dir.stub(:glob).and_return(['file1.wav', 'file2.wav'])
+                archiver = Collector::Video.new
+                expect { archiver.collect_video( meeting_id, from_dir, to_dir ) }.to raise_error(NoSuchDirectoryException)
+            end
+            it "should raise audio files not found exception" do
+                from_dir = 'resources/raw/audio'
+                to_dir = '/to-dir-not-found'
+                meeting_id = 'meeting-id'
+                FileTest.stub(:directory?).and_return(true)
+                FileUtils.stub(:cp)
+                Dir.stub(:glob).and_return([])
+                archiver = Collector::Video.new
+                expect { archiver.collect_video( meeting_id, from_dir, to_dir ) }.to raise_error(NoVideoFileException)
+            end
+        end
+    end
+end
