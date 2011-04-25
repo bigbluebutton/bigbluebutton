02b5c9f7731371e607e504786a6f964e1deac1a9
Richard Alam <ritzalam@gmail.com>
2011-04-25 15:29:33 -0400

    - add logger

--- record-and-playback/rap/lib/recordandplayback.rb
+++ record-and-playback/rap/lib/recordandplayback.rb
@@ -7,3 +7,24 @@ require 'recordandplayback/generators/audio'
 require 'recordandplayback/generators/matterhorn_processor'
 require 'recordandplayback/generators/audio_processor'
 require 'recordandplayback/generators/deskshare'
+
+module BigBlueButton
+  # BigBlueButton logs information about its progress.
+  # Replace with your own logger if you desire.
+  #
+  # @param [Logger] log your own logger
+  # @return [Logger] the logger you set
+  def self.logger=(log)
+    @logger = log
+  end
+  
+  # Get BigBlueButton logger.
+  #
+  # @return [Logger]
+  def self.logger
+    return @logger if @logger
+    logger = Logger.new(STDOUT)
+    logger.level = Logger::INFO
+    @logger = logger
+  end
+end
