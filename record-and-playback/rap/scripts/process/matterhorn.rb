22204336b5e04b3c23dac09eeef7ee35f4727bba
Richard Alam <ritzalam@gmail.com>
2011-05-11 18:08:43 -0400

    - process deskstop sharing (still assumes there is only one raw media)

--- record-and-playback/rap/scripts/process/matterhorn.rb
+++ record-and-playback/rap/scripts/process/matterhorn.rb
@@ -28,6 +28,7 @@ FileUtils.cp_r(raw_archive_dir, temp_dir)
 
 BigBlueButton::AudioProcessor.process("#{temp_dir}/#{meeting_id}", "#{target_dir}/audio.ogg")
 
+# Process video
 video = Dir.glob("#{temp_dir}/#{meeting_id}/video/#{meeting_id}/*.flv")[0]
 stripped_flv = "#{temp_dir}/stripped.flv"
 BigBlueButton.strip_audio_from_video(video, stripped_flv)
@@ -47,8 +48,6 @@ start_evt = BigBlueButton::Events.get_start_video_events(events_xml)
 
 stop_evt = BigBlueButton::Events.get_stop_video_events(events_xml)
 
-
-        
 first_gap_duration = start_evt[0][:timestamp].to_i - first_timestamp.to_i
 puts "First gap = " + first_gap_duration.to_s
 end_gap_duration = last_timestamp.to_i - stop_evt[0][:timestamp].to_i
@@ -57,3 +56,33 @@ BigBlueButton.create_blank_video(first_gap_duration/1000, 1000, blank_canvas, bl
 BigBlueButton.create_blank_video(end_gap_duration/1000, 1000, blank_canvas, blank2)
 BigBlueButton.concatenate_videos([blank1, stripped_flv, blank2], concat_vid)
 BigBlueButton.multiplex_audio_and_video("#{target_dir}/audio.ogg", concat_vid, "#{target_dir}/muxed-audio-webcam.flv")
+
+# Process deskshare
+deskshare = Dir.glob("#{temp_dir}/#{meeting_id}/deskshare/*.flv")[0]
+ds_stripped_flv = "#{temp_dir}/ds-stripped.flv"
+BigBlueButton.strip_audio_from_video(deskshare, ds_stripped_flv)
+ds_width = BigBlueButton.get_video_width(deskshare)
+ds_height = BigBlueButton.get_video_height(deskshare)
+ds_blank_canvas = "#{temp_dir}/ds-canvas.jpg"
+BigBlueButton.create_blank_canvas(ds_width, ds_height, "white", ds_blank_canvas)
+
+dsblank1 = "#{temp_dir}/ds-blank1.flv"
+dsblank2 = "#{temp_dir}/ds-blank2.flv"
+dsconcat_vid = "#{target_dir}/deskshare.flv"
+               
+events_xml = "#{temp_dir}/#{meeting_id}/events.xml"
+first_timestamp = BigBlueButton::Events.first_event_timestamp(events_xml)
+last_timestamp = BigBlueButton::Events.last_event_timestamp(events_xml)
+start_evt = BigBlueButton::Events.get_start_deskshare_events(events_xml)
+
+stop_evt = BigBlueButton::Events.get_stop_deskshare_events(events_xml)
+
+first_gap_duration = start_evt[0][:timestamp].to_i - first_timestamp.to_i
+puts "First gap = " + first_gap_duration.to_s
+end_gap_duration = last_timestamp.to_i - stop_evt[0][:timestamp].to_i
+puts "End gap = " + end_gap_duration.to_s
+BigBlueButton.create_blank_deskshare_video(first_gap_duration/1000, 1000, ds_blank_canvas, dsblank1)
+BigBlueButton.create_blank_deskshare_video(end_gap_duration/1000, 1000, ds_blank_canvas, dsblank2)
+BigBlueButton.concatenate_videos([dsblank1, ds_stripped_flv, dsblank2], dsconcat_vid)
+
+
