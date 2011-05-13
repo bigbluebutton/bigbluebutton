b60952917abe9ba9ba1588b940cce2cde00ee4a2
Richard Alam <ritzalam@gmail.com>
2011-05-13 13:34:31 -0400

    - change from popen to open3 for executing external commnds

--- record-and-playback/rap/lib/recordandplayback/generators/presentation.rb
+++ record-and-playback/rap/lib/recordandplayback/generators/presentation.rb
@@ -19,28 +19,21 @@ module BigBlueButton
       presentations
     end
 
-    # Determine the number pages in a presentation by looking at the number of
-    # swf files in a directory.
+    # Determine the number pages in a presentation.
     def self.get_number_of_pages_for(presentation_dir)
-      Dir.glob("#{presentation_dir}/*.swf")
+      Dir.glob("#{presentation_dir}/*.swf").size
     end
 
     # Extract a page from the pdf file.    
     def self.extract_page_from_pdf(page_num, pdf_presentation, pdf_out)
         command = "ghostscript #{OPTIONS} #{FIRSTPAGE}=#{page_num} #{LASTPAGE}=#{page_num} #{OUTPUTFILE}=#{pdf_out} #{NO_PDF_MARK_WORKAROUND} #{pdf_presentation}"
-        puts command
-        IO.popen(command,"w+")
-        # Wait for the process to finish
-        Process.wait()
+        BigBlueButton.execute(command)
     end
     
     # Convert a pdf page to a png.
     def self.convert_pdf_to_png(pdf_page, png_out)
         command = "convert -density 600x600 -resize 800x560 -quality 90 #{pdf_page} #{png_out}"
-        IO.popen(command, "w+")
-        # Wait for the process to finish
-        Process.wait()    
+        BigBlueButton.execute(command)    
     end
-
   end
 end
