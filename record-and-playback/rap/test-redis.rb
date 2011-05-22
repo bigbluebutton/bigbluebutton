01fb6a1c9b49f77bab65058a168d43e6885bcb28
Richard Alam <ritzalam@gmail.com>
2011-05-22 14:25:19 -0400

    - cleanup

--- record-and-playback/rap/test-redis.rb
+++
@@ -1,9 +0,0 @@
-require 'lib/recordandplayback'
-require 'rubygems'
-require 'redis'
-require 'builder'
-
-redis = BigBlueButton::RedisWrapper.new("192.168.0.166", 6379)
-events_archiver = BigBlueButton::RedisEventsArchiver.new redis
-
-puts events_archiver.store_events('58f4a6b3-cd07-444d-8564-59116cb53974')
