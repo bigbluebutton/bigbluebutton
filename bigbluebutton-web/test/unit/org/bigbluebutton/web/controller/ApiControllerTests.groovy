63636da8dcf8bffed725ea9cbab8fab17664a286
Richard Alam <ritzalam@gmail.com>
2011-05-26 14:25:31 -0400

    - add api controller unit tests

---
+++ bigbluebutton-web/test/unit/org/bigbluebutton/web/controller/ApiControllerTests.groovy
@@ -0,0 +1,17 @@
+package org.bigbluebutton.web.controller
+
+import grails.test.*
+
+class ApiControllerTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
