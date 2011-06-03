1bb6e2029161f2b75efa806de871984e5c7da4cb
Richard Alam <ritzalam@gmail.com>
2011-06-03 11:15:24 -0400

    - renamed ingenst-and-processing script  - log when saving metadata

--- bigbluebutton-web/src/java/org/bigbluebutton/api/RedisDispatcherImp.java
+++ bigbluebutton-web/src/java/org/bigbluebutton/api/RedisDispatcherImp.java
@@ -1,12 +1,29 @@
 package org.bigbluebutton.api;
 
+import java.io.IOException;
+import java.net.UnknownHostException;
+
 import org.bigbluebutton.api.domain.DynamicConference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import redis.clients.jedis.Jedis;
 
 public class RedisDispatcherImp implements IRedisDispatcher {
+	private static Logger log = LoggerFactory.getLogger(RedisDispatcherImp.class);
+	
 	@Override
 	public void createConferenceRecord(DynamicConference conf, String redisHost, int redisPort) {
 		Jedis jedis = new Jedis(redisHost, redisPort);
-		jedis.hmset("meeting:" + conf.getMeetingToken() + ":metadata", conf.getMetadata());
+		try {
+			log.debug("Setting metada for " + "meeting:" + conf.getMeetingToken() + ":metadata");
+			jedis.connect();
+			jedis.hmset("meeting:" + conf.getMeetingToken() + ":metadata", conf.getMetadata());
+		} catch (UnknownHostException e) {
+			log.error("Cannot connect to " + redisHost + ":" + redisPort);
+		} catch (IOException e) {
+			log.error("Failed to connect to Redis on " + redisHost + ":" + redisPort);
+		}
+		
 	}
 }
