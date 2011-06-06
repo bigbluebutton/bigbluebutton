f2b6b07112ef20284f65584c4d4e27480a7d126b
Richard Alam <ritzalam@gmail.com>
2011-06-06 14:58:16 -0400

    - no need to "sudo -u tomcat6" as main script already runs as tomcat6

--- record-and-playback/core/scripts/rap-worker.rb
+++ record-and-playback/core/scripts/rap-worker.rb
@@ -5,42 +5,42 @@ require 'fileutils'
 
 
 def archive_recorded_meeting()
-	`sudo -u tomcat6 ruby archive/archive.rb`
+  `ruby archive/archive.rb`
 end
 
 def process_archived_meeting(recording_dir)
-	done_files = Dir.glob("#{recording_dir}/status/archived/*.done")	
-	done_files.each do |df|
-	  match = /(.*).done/.match df.sub(/.+\//, "")
-	  meeting_id = match[1]
-	  
-		# Execute all the scripts under the steps directory.
-		# This script must be invoked from the scripts directory for the PATH to be resolved.
-		Dir.glob("#{Dir.pwd}/process/*.rb").sort.each do |file|
-			#  BigBlueButton.logger.info("Executing #{file}\n")  
-		  IO.popen("sudo -u tomcat6 ruby #{file} -m #{meeting_id}")
-		  Process.wait
-		  puts "********** #{$?.exitstatus} #{$?.exited?} #{$?.success?}********************"
-		end
-	end	
+  done_files = Dir.glob("#{recording_dir}/status/archived/*.done")	
+  done_files.each do |df|
+    match = /(.*).done/.match df.sub(/.+\//, "")
+    meeting_id = match[1]
+    
+    # Execute all the scripts under the steps directory.
+    # This script must be invoked from the scripts directory for the PATH to be resolved.
+    Dir.glob("#{Dir.pwd}/process/*.rb").sort.each do |file|
+      #  BigBlueButton.logger.info("Executing #{file}\n")  
+      IO.popen("ruby #{file} -m #{meeting_id}")
+      Process.wait
+      puts "********** #{$?.exitstatus} #{$?.exited?} #{$?.success?}********************"
+    end
+  end	
 end
 
 def publish_processed_meeting(recording_dir)
-	done_files = Dir.glob("#{recording_dir}/status/processed/*.done")
-	
-	done_files.each do |df|
-	  match = /(.*).done/.match df.sub(/.+\//, "")
-	  meeting_id = match[1]
-	  
-		# Execute all the scripts under the steps directory.
-		# This script must be invoked from the scripts directory for the PATH to be resolved.
-		Dir.glob("#{Dir.pwd}/publish/*.rb").sort.each do |file|
-			#  BigBlueButton.logger.info("Executing #{file}\n")  
-		  IO.popen("sudo -u tomcat6 ruby #{file} -m #{meeting_id}")
-		  Process.wait
-		  puts "********** #{$?.exitstatus} #{$?.exited?} #{$?.success?}********************"
-		end
-	end	
+  done_files = Dir.glob("#{recording_dir}/status/processed/*.done")
+  
+  done_files.each do |df|
+    match = /(.*).done/.match df.sub(/.+\//, "")
+    meeting_id = match[1]
+    
+    # Execute all the scripts under the steps directory.
+    # This script must be invoked from the scripts directory for the PATH to be resolved.
+    Dir.glob("#{Dir.pwd}/publish/*.rb").sort.each do |file|
+      #  BigBlueButton.logger.info("Executing #{file}\n")  
+      IO.popen("ruby #{file} -m #{meeting_id}")
+      Process.wait
+      puts "********** #{$?.exitstatus} #{$?.exited?} #{$?.success?}********************"
+    end
+  end	
 end
 
 #  if not FileTest.directory?("#{archive_dir}/#{meeting_id}")
