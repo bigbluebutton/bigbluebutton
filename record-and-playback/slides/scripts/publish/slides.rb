5f075eb9d56e670264d96cc8b315b4f54b3b4070
gugat <guga.salazar.loor@gmail.com>
2011-09-05 21:43:58 -0700

    Set chat to go down

--- record-and-playback/slides/scripts/publish/slides.rb
+++ record-and-playback/slides/scripts/publish/slides.rb
@@ -107,7 +107,7 @@ if (playback == "slides")
           chat_sender = node.xpath(".//sender")[0].text()
           chat_message =  node.xpath(".//message")[0].text()
           chat_start = (chat_timestamp.to_i - meeting_start.to_i) / 1000
-          xml.timeline(:in => chat_start, :direction => "up",  :innerHTML => "<span><strong>#{chat_sender}:</strong> #{chat_message}</span>", :target => "chat" )
+          xml.timeline(:in => chat_start, :direction => "down",  :innerHTML => "<span><strong>#{chat_sender}:</strong> #{chat_message}</span>", :target => "chat" )
         end
       }
     end
