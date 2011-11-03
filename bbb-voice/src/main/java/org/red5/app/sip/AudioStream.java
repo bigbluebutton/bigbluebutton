b795d159f7da8064a658ca3e015eddce7c5c86a0
Richard Alam <ritzalam@gmail.com>
2011-11-03 20:24:50 +0000

    build bbb-voice with red5 r4293

--- bbb-voice/src/main/java/org/red5/app/sip/AudioStream.java
+++ bbb-voice/src/main/java/org/red5/app/sip/AudioStream.java
@@ -197,8 +197,10 @@ public class AudioStream implements IBroadcastStream, IProvider, IPipeConnection
 		if (event instanceof IRTMPEvent) {
 			IRTMPEvent rtmpEvent = (IRTMPEvent) event;
 			if (livePipe != null) {
-				RTMPMessage msg = RTMPMessage.build(rtmpEvent);
-          
+				//RTMPMessage msg = RTMPMessage.build(rtmpEvent);
+				RTMPMessage msg = new RTMPMessage();
+				msg.setBody(rtmpEvent);
+				
 				if (creationTime == null)
 					creationTime = (long)rtmpEvent.getTimestamp();
           
