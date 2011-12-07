77f664c6bd30a7f83f99f2cb290bc5328949e036
Markos Calderon <mcmarkos86@gmail.com>
2011-12-07 13:18:21 -0800

    checking dirs instead of done files

--- record-and-playback/core/scripts/rap-worker.rb
+++ record-and-playback/core/scripts/rap-worker.rb
@@ -9,13 +9,14 @@ BigBlueButton.logger = logger
 
 def archive_recorded_meeting(recording_dir)
   recorded_done_files = Dir.glob("#{recording_dir}/status/recorded/*.done")
-  archived_done_files = Dir.glob("#{recording_dir}/status/archived/*.done")
+  archived_dirs = Dir.entries("#{recording_dir}/raw/") - ['.','..']
   
   recorded_done_files.each do |df|
 	match = /(.*).done/.match df.sub(/.+\//, "")
 	meeting_id = match[1]
 	
-	is_archived = archived_done_files.any? { |s| s.include?(meeting_id)  }
+	is_archived = archived_dirs.any? { |s| s.include?(meeting_id)  }
+
 	if(!is_archived)
 		command = "ruby archive/archive.rb -m #{meeting_id}"
 	    BigBlueButton.execute(command)
@@ -26,7 +27,6 @@ end
 
 def process_archived_meeting(recording_dir)
   archived_done_files = Dir.glob("#{recording_dir}/status/archived/*.done")
-  processed_done_files = Dir.glob("#{recording_dir}/status/processed/*.done")
   
   archived_done_files.each do |df|
     match = /(.*).done/.match df.sub(/.+\//, "")
@@ -35,14 +35,22 @@ def process_archived_meeting(recording_dir)
 	# Execute all the scripts under the steps directory.
 	# This script must be invoked from the scripts directory for the PATH to be resolved.
 	Dir.glob("#{Dir.pwd}/process/*.rb").sort.each do |file|
-	
-	  is_processed = processed_done_files.any? { |s| s.include?(meeting_id + "-" + file)  }
+	  
+	  # Checking if the meeting hasn't been processed
+	  match2 = /(.*).rb/.match file.sub(/.+\//, "")
+	  process_type = match2[1]
+	  is_processed = false
+	  if File.directory?("#{recording_dir}/process/#{process_type}")
+		processed_dirs = Dir.entries("#{recording_dir}/process/#{process_type}") - ['.','..']
+		is_processed = processed_dirs.any? { |s| s.include?(meeting_id)  }
+	  end
+	  
 	  if(!is_processed)
 	     #  BigBlueButton.logger.info("Executing #{file}\n")  
 	     #IO.popen("ruby #{file} -m #{meeting_id}")
 	     #Process.wait
 	     #puts "********** #{$?.exitstatus} #{$?.exited?} #{$?.success?}********************"
-	     command = "ruby #{file} -m #{meeting_id}"
+		 command = "ruby #{file} -m #{meeting_id}"
 	     BigBlueButton.execute(command)
 	  end
 
@@ -52,8 +60,7 @@ end
 
 def publish_processed_meeting(recording_dir)
   processed_done_files = Dir.glob("#{recording_dir}/status/processed/*.done")
-  published_done_files = Dir.glob("#{recording_dir}/status/published/*.done")
-  
+ 
   processed_done_files.each do |df|
     match = /(.*).done/.match df.sub(/.+\//, "")
     meeting_id = match[1]
@@ -62,7 +69,19 @@ def publish_processed_meeting(recording_dir)
     # This script must be invoked from the scripts directory for the PATH to be resolved.
     Dir.glob("#{Dir.pwd}/publish/*.rb").sort.each do |file|
 	
-	  is_published = published_done_files.any? { |s| s.include?(meeting_id + "-" + file)  }
+	  # Checking if the meeting hasn't been processed
+	  match2 = /(.*).rb/.match file.sub(/.+\//, "")
+	  publish_type = match2[1]
+	  
+	  match2 = /(.*)-(.*)/.match meeting_id
+	  c_meeting_id = match2[1]
+	  
+	  is_published = false
+	  if File.directory?("#{recording_dir}/publish/#{publish_type}")
+		published_dirs = Dir.entries("#{recording_dir}/publish/#{publish_type}") - ['.','..']
+		is_published = published_dirs.any? { |s| s.include?(c_meeting_id)  }
+	  end
+	
 	  if(!is_published)
 		#  BigBlueButton.logger.info("Executing #{file}\n")  
 		#IO.popen("ruby #{file} -m #{meeting_id}")
