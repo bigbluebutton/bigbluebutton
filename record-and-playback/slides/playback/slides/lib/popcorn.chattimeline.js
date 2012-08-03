4c1353560716e78b0fe5bb050be8eb73ff8ccb89
Chad Pilkey <capilkey@gmail.com>
2012-08-03 20:10:54 +0000

    starting to make playback accessible

--- record-and-playback/slides/playback/slides/lib/popcorn.chattimeline.js
+++ record-and-playback/slides/playback/slides/lib/popcorn.chattimeline.js
@@ -26,23 +26,14 @@
 
   var i = 1;
 
-  Popcorn.plugin( "chat-timeline" , function( options ) {
+  Popcorn.plugin( "chattimeline" , function( options ) {
 
     var target = document.getElementById( options.target ),
         contentDiv = document.createElement( "div" ),
-        container,
         goingUp = true;
 
-    if ( target && !target.firstChild ) {
-      target.appendChild ( container = document.createElement( "div" ) );
-      container.style.width = "inherit";
-      container.style.height = "inherit";
-      container.style.overflow = "auto";
-    } else {
-      container = target.firstChild;
-    }
-
     contentDiv.style.display = "none";
+    contentDiv.setAttribute('aria-hidden', true);
     contentDiv.id = "timelineDiv" + i;
 
     //  Default to up if options.direction is non-existant or not up or down
@@ -52,15 +43,15 @@
       goingUp = false;
     }
 
-    if ( target && container ) {
+    if ( target ) {
       // if this isnt the first div added to the target div
       if( goingUp ){
         // insert the current div before the previous div inserted
-        container.insertBefore( contentDiv, container.firstChild );
+        target.insertBefore( contentDiv, target.firstChild );
       }
       else {
 
-        container.appendChild( contentDiv );
+        target.appendChild( contentDiv );
       }
 
     }
@@ -70,25 +61,26 @@
     //  Default to empty if not used
     //options.innerHTML = options.innerHTML || "";
 
-    contentDiv.innerHTML = "<span><strong>" + options.name + ":&nbsp;</strong>" + options.message + "</span>";
+    contentDiv.innerHTML = "<p><strong>" + options.name + ":</strong>" + options.message + "</p>";
 
     return {
 
       start: function( event, options ) {
         contentDiv.style.display = "block";
-
+        contentDiv.setAttribute('aria-hidden', false);
         if( options.direction === "down" ) {
-          container.scrollTop = container.scrollHeight;
+          target.scrollTop = target.scrollHeight;
         }
       },
 
       end: function( event, options ) {
         contentDiv.style.display = "none";
+        contentDiv.setAttribute('aria-hidden', true);
       },
 
       _teardown: function( options ) {
 
-        ( container && contentDiv ) && container.removeChild( contentDiv ) && !container.firstChild && target.removeChild( container );
+        ( target && contentDiv ) && target.removeChild( contentDiv ) && !target.firstChild
       }
     };
   },
