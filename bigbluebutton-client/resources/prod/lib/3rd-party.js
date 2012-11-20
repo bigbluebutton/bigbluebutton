200d770b4f7fddea50876123702e1bac098aa317
Richard Alam <ritzalam@gmail.com>
2012-11-20 20:20:12 +0000

    - add JS api to query user id

--- bigbluebutton-client/resources/prod/lib/3rd-party.js
+++ bigbluebutton-client/resources/prod/lib/3rd-party.js
@@ -35,6 +35,12 @@ var getMyRoleSynch = function() {
   });
 }
 
+var getMyUserID = function() {
+  BBB.getMyUserID(function(userID) {
+    console.log("My user ID  = [" + userID + "]");
+  });
+}
+
 var getMeetingID = function() {
   BBB.getMeetingID(function(meetingID) {
     console.log("Meeting ID  = [" + meetingID + "]");
