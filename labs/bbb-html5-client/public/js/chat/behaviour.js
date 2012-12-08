f70a48ad81a438b362d821dc843e2db249dfbc3f
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2012-12-08 20:54:05 -0200

    Remove js triggers from the chat view and refactor it to be a bit more readable
    Also refactored behaviour.js a bit.

--- labs/bbb-html5-client/public/js/chat/behaviour.js
+++ labs/bbb-html5-client/public/js/chat/behaviour.js
@@ -2,45 +2,79 @@ define(["jquery", "chat/whiteboard", "chat/connection", "chat/whiteboard", "chat
        function($, Whiteboard, Connection, Whiteboard, Chat) {
 
   var layout = $("#layout");
-  var chat = $("#chat-btn");
-  var users = $("#users-btn");
+  var chatToogleBtn = $("#chat-btn");
+  var usersToogleBtn = $("#users-btn");
   var logoutBtn = $("#logout-btn");
   var userclick, chatclick;
 
   /* Ensure the status is set right at page load. */
-  chat.toggleClass("active", layout.hasClass("chat-enabled"));
-  users.toggleClass("active", layout.hasClass("chat-enabled"));
+  chatToogleBtn.toggleClass("active", layout.hasClass("chat-enabled"));
+  usersToogleBtn.toggleClass("active", layout.hasClass("users-enabled"));
 
-  chat.click(function() {
+  chatToogleBtn.on("click", function() {
     if(chatclick) clearTimeout(chatclick);
     layout.toggleClass("chat-enabled");
-    chat.toggleClass("active", layout.hasClass("chat-enabled"));
+    chatToogleBtn.toggleClass("active", layout.hasClass("chat-enabled"));
     chatclick = setTimeout(function(){
       Whiteboard.windowResized();
     }, 1100);
   });
 
-  users.click(function() {
+  usersToogleBtn.on("click", function() {
     if(userclick) clearTimeout(userclick);
     layout.toggleClass("users-enabled");
-    users.toggleClass("active", layout.hasClass("users-enabled"));
+    usersToogleBtn.toggleClass("active", layout.hasClass("users-enabled"));
     userclick = setTimeout(function() {
       Whiteboard.windowResized('users');
     }, 1100);
   });
 
-  logoutBtn.click(function() { Connection.emitLogout() });
+  logoutBtn.on("click", function() {
+    Connection.emitLogout();
+  });
 
-  $("#prev-slide-btn").click(function() { Connection.emitPrevSlide(); });
-  $("#next-slide-btn").click(function() { Connection.emitNextSlide(); });
-  $("#tool-pan-btn").click(function() { Connection.emitChangeTool('panzoom'); });
-  $("#tool-line-btn").click(function() { Connection.emitChangeTool('line'); });
-  $("#tool-rect-btn").click(function() { Connection.emitChangeTool('rect'); });
-  $("#tool-ellipse-btn").click(function() { Connection.emitChangeTool('ellipse'); });
+  $("#prev-slide-btn").on("click", function() {
+    Connection.emitPrevSlide();
+    return false;
+  });
 
-  $("button#chat-send").on("click", function(e) {
-    e.preventDefault();
+  $("#next-slide-btn").on("click", function() {
+    Connection.emitNextSlide();
+    return false;
+  });
+
+  $("#tool-pan-btn").on("click", function() {
+    Connection.emitChangeTool('panzoom');
+    return false;
+  });
+
+  $("#tool-line-btn").on("click", function() {
+    Connection.emitChangeTool('line');
+    return false;
+  });
+
+  $("#tool-rect-btn").on("click", function() {
+    Connection.emitChangeTool('rect');
+    return false;
+  });
+
+  $("#tool-ellipse-btn").on("click", function() {
+    Connection.emitChangeTool('ellipse');
+    return false;
+  });
+
+  $("button#chat-send").on("click", function() {
     Chat.sendMessage();
+    return false;
+  });
+
+  $("#colourView").on("click", function() {
+    Whiteboard.toggleColourPicker();
+    return false;
+  });
+
+  $("#switch-presenter").on("click", function() {
+    Chat.switchPresenter();
     return false;
   });
 
