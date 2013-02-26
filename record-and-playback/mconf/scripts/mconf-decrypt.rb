744dfac69947a0c5a3782bb8332ffc20b0c186cc
Felipe Cecagno <fcecagno@gmail.com>
2013-02-26 17:42:41 -0300

    fixing the xpath stuff on mconf-decrypt

--- record-and-playback/mconf/scripts/mconf-decrypt.rb
+++ record-and-playback/mconf/scripts/mconf-decrypt.rb
@@ -47,15 +47,14 @@ if not get_recordings_url.nil? and not get_recordings_url.empty?
   end
 
   doc.xpath("//recording").each do |recording|
-    record_id = recording.xpath("recordID").text
-    recording.xpath("//download/format").each do |format|
-      type = format.xpath("type").text
-      BigBlueButton.logger.info("type = #{type}")
+    record_id = recording.xpath(".//recordID").text
+    recording.xpath(".//download/format").each do |format|
+      type = format.xpath(".//type").text
       if type == "encrypted"
         meeting_id = record_id
-        file_url = format.xpath("url").text
-        key_file_url = format.xpath("key").text
-        md5_value = format.xpath("md5").text
+        file_url = format.xpath(".//url").text
+        key_file_url = format.xpath(".//key").text
+        md5_value = format.xpath(".//md5").text
 
         encrypted_file = file_url.split("/").last
         decrypted_file = File.basename(encrypted_file, '.*') + ".zip"
