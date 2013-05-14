a1946007a6dd9f174c941a0ae997ee882155b0a1
Calvin Walton <calvin.walton@kepstin.ca>
2013-05-14 13:59:14 -0400

    RaP: Presentation: Don't process audio twice when no video.
    The audio is unconditionally processed earlier in the script. Remove the
    line that runs it again when there is no video in the recording.

--- record-and-playback/presentation/scripts/process/presentation.rb
+++ record-and-playback/presentation/scripts/process/presentation.rb
@@ -96,8 +96,6 @@ if not FileTest.directory?(target_dir)
   
   if !Dir["#{raw_archive_dir}/video/*"].empty?    
     BigBlueButton.process_multiple_videos(target_dir, temp_dir, meeting_id, presentation_props['video_output_width'], presentation_props['video_output_height'])
-  else    
-    BigBlueButton::AudioProcessor.process("#{temp_dir}/#{meeting_id}", "#{target_dir}/audio.ogg")
   end
 
   process_done = File.new("#{recording_dir}/status/processed/#{meeting_id}-presentation.done", "w")
