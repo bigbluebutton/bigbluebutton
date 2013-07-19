553875287a61c1ecfb5e65d366ca3d1360f94a2b
Jesus Federico <jesus@123it.ca>
2013-07-19 09:51:16 -0400

    bbb-lti: Restored bbb-lti.log file

--- bbb-lti/grails-app/conf/Config.groovy
+++ bbb-lti/grails-app/conf/Config.groovy
@@ -71,6 +71,7 @@ log4j = {
     appenders {
         rollingFile name:"logfile", maxFileSize:1000000, file:"/var/log/bigbluebutton/bbb-lti.log", layout:pattern(conversionPattern: '%d{[dd.MM.yy HH:mm:ss.SSS]} %-5p %c %x - %m%n')
         console name:'console', layout:pattern(conversionPattern: '%d{[dd.MM.yy HH:mm:ss.SSS]} %-5p %c %x - %m%n')
+        'null' name:'stacktrace'
     }
     debug logfile:"grails.app"
         
@@ -86,8 +87,6 @@ log4j = {
            'org.hibernate'
 
     warn   'org.mortbay.log'
-    
-    'null' name:'stacktrace'
 }
 
 
