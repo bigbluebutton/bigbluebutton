c2884bdb9d075725efb492d0dcdd9035ce780b77
Calvin Walton <calvin.walton@kepstin.ca>
2013-08-26 11:44:22 -0400

    Add missing license header to edl.rb

--- record-and-playback/core/lib/recordandplayback/edl.rb
+++ record-and-playback/core/lib/recordandplayback/edl.rb
@@ -1,5 +1,22 @@
 # encoding: UTF-8
 
+# BigBlueButton open source conferencing system - http://www.bigbluebutton.org/
+#
+# Copyright (c) 2013 BigBlueButton Inc. and by respective authors.
+#
+# BigBlueButton is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#
+# BigBlueButton is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with BigBlueButton.  If not, see <http://www.gnu.org/licenses/>. 
+
 require File.expand_path('../edl/video', __FILE__)
 require File.expand_path('../edl/audio', __FILE__)
 
