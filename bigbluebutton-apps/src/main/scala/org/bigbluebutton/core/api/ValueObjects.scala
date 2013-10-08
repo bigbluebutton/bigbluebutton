69feca2a07ebfd1cf319983f95edfdd401fcab76
Richard Alam <ritzalam@gmail.com>
2013-10-08 21:47:49 +0000

    - continue making changes to voice app

--- bigbluebutton-apps/src/main/scala/org/bigbluebutton/core/api/ValueObjects.scala
+++ bigbluebutton-apps/src/main/scala/org/bigbluebutton/core/api/ValueObjects.scala
@@ -6,14 +6,49 @@ object Role extends Enumeration {
 	val VIEWER = Value("VIEWER")
 }
 
-case class Presenter(presenterID: String, presenterName: String, assignedBy: String)
-case class UserVO(userID: String, externUserID: String, name: String, role: String, raiseHand: Boolean, presenter: Boolean, hasStream: Boolean)
-case class MeetingConfig(name: String, id: MeetingID, passwords: MeetingPasswords, welcomeMsg: String, logoutUrl: String,
-	maxUsers: Int, record: Boolean=false, duration: MeetingDuration,
-	defaultAvatarURL: String, defaultConfigToken: String)
+case class Presenter(
+    presenterID: String, 
+    presenterName: String, 
+    assignedBy: String)
+    
+case class UserVO(
+    userID: String, 
+    externUserID: String, 
+    name: String, 
+    role: String, 
+    raiseHand: Boolean, 
+    presenter: Boolean, 
+    hasStream: Boolean)
+    
+case class MeetingConfig(
+    name: String, 
+    id: MeetingID, 
+    passwords: MeetingPasswords, 
+    welcomeMsg: String, 
+    logoutUrl: String,
+	maxUsers: Int, 
+	record: Boolean=false, 
+	duration: MeetingDuration,
+	defaultAvatarURL: String, 
+	defaultConfigToken: String)
 
-case class MeetingName(name: String)
-case class MeetingID(internal:String, external: String)
-case class VoiceConfig(telVoice: String, webVoice: String, dialNumber: String)
-case class MeetingPasswords(moderatorPass: String, viewerPass: String)
-case class MeetingDuration(duration: Int = 0, createdTime: Long = 0, startTime: Long = 0, endTime: Long = 0)
+case class MeetingName(
+    name: String)
+    
+case class MeetingID(
+    internal:String, 
+    external: String)
+    
+case class VoiceConfig(
+    telVoice: String, 
+    webVoice: String, 
+    dialNumber: String)
+    
+case class MeetingPasswords(
+    moderatorPass: String, 
+    viewerPass: String)
+    
+case class MeetingDuration(duration: Int = 0, 
+    createdTime: Long = 0, 
+    startTime: Long = 0, 
+    endTime: Long = 0)
