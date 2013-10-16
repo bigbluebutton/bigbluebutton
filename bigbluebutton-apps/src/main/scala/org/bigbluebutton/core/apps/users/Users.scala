00b8ed22060ba1ee8e78323251cd8a8858482cf3
Richard Alam <ritzalam@gmail.com>
2013-10-16 18:28:43 +0000

    - add tests for users

--- bigbluebutton-apps/src/main/scala/org/bigbluebutton/core/apps/users/Users.scala
+++ bigbluebutton-apps/src/main/scala/org/bigbluebutton/core/apps/users/Users.scala
@@ -1,7 +1,8 @@
 package org.bigbluebutton.core.apps.users
 
-import org.bigbluebutton.core.models.UserV
+import org.bigbluebutton.core.models.{UserV, Voice}
 import scala.collection.mutable.ArrayBuffer
+import org.bigbluebutton.core.api.Role._
 
 class Users {
 
@@ -15,7 +16,7 @@ class Users {
   /**
    * Returns the number of users.
    */
-  def size = users.size
+  def count = users.size
   
   /**
    * Removes a user.
@@ -25,21 +26,92 @@ class Users {
   /**
    * Gets a user.
    */
-  def get(id: String):Option[UserV] = {
-    var user:Option[UserV] = None
-    users.get(id) match {
-      case Some(u) => user = Some(u.copy())
-      case None => // do nothing
-    }
-    user
-  }
+  def get(id: String):Option[UserV] = users.values.find(u => u.id == id)
   
   /**
    * Get all users.
    */
-  def all():Array[UserV] = {
-    val u = new ArrayBuffer[UserV]()
-    users.values.foreach(kv => u += kv.copy())
-    u.toArray
+  def all():Array[UserV] = users.values.toArray
+  
+  def getPresenter():Option[UserV] = users.values.find(u => u.status.isPresenter)
+  
+  def makeEveryoneNotPresenter():Unit = {
+    users.values map (u => {
+     val p = u.copy(status = u.status.copy(isPresenter=false))
+     users += p.id -> p
+    })
+  }
+  
+  def makePresenter(id:String):Option[UserV] = {
+    var newPresenter: Option[UserV] = None
+    get(id) match {
+      case Some(u) => {
+        val p = u.copy(status = u.status.copy(isPresenter=true))
+        newPresenter = Some(p)
+        users += p.id -> p
+      }
+      case None =>
+    }
+    newPresenter
+  }
+
+  def moderatorCount:Int = users.values.filter(u => u.role == MODERATOR).size
+ 
+  def getVoiceUser(id:String):Option[UserV] = users.values.find(u => u.voice.id == id)
+  
+  def mute(id:String):Option[UserV] = {
+    var user:Option[UserV] = None
+    getVoiceUser(id) match {
+      case Some(u) => {
+        val mutedUser = u.copy(voice=u.voice.copy(muted=true))
+        users += mutedUser.id -> mutedUser
+        user = Some(mutedUser)
+      }
+      case None =>
+    }
+    
+    user
+  }
+  
+  def unmute(id:String):Option[UserV] = {
+    var user:Option[UserV] = None
+    getVoiceUser(id) match {
+      case Some(u) => {
+        val unmutedUser = u.copy(voice=u.voice.copy(muted=false))
+        users += unmutedUser.id -> unmutedUser
+        user = Some(unmutedUser)
+      }
+      case None =>
+    }
+    
+    user
+  }
+  
+  def joinedVoice(id:String, voice: Voice):Option[UserV] = {
+    var user:Option[UserV] = None
+    get(id) match {
+      case Some(u) => {
+        val voiceUser = u.copy(voice=voice)
+        users += voiceUser.id -> voiceUser
+        user = Some(voiceUser)
+      }
+      case None =>
+    }
+    
+    user
+  }
+  
+  def leftVoice(id:String):Option[UserV] = {
+    var user:Option[UserV] = None
+    getVoiceUser(id) match {
+      case Some(u) => {
+        val voiceUser = u.copy(voice=Voice())
+        users += voiceUser.id -> voiceUser
+        user = Some(voiceUser)
+      }
+      case None =>
+    }
+    
+    user
   }
 }
--- bigbluebutton-apps/src/main/scala/org/bigbluebutton/core/models/Values.scala
+++ bigbluebutton-apps/src/main/scala/org/bigbluebutton/core/models/Values.scala
@@ -3,6 +3,21 @@ package org.bigbluebutton.core.models
 import org.bigbluebutton.core.api.Role._
 
 case class Status(isPresenter: Boolean = false, handRaised: Boolean = false)
-case class Voice(hasJoined: Boolean = false, id: String = "", 
-    muted: Boolean = false, talking: Boolean = false, locked: Boolean = false)
-case class UserV(id: String, extId: String, name: String, role: Role = VIEWER, status: Status = Status(), voice: Voice = Voice())
+
+case class CallerId(num: String="", name: String="")
+
+case class Voice(
+    hasJoined: Boolean = false, 
+    id: String = "", 
+    callerId: CallerId = CallerId(),
+    muted: Boolean = false, 
+    talking: Boolean = false, 
+    locked: Boolean = false)
+
+case class UserV(
+    id: String, 
+    extId: String, 
+    name: String, 
+    role: Role = VIEWER, 
+    status: Status = Status(), 
+    voice: Voice = Voice())
