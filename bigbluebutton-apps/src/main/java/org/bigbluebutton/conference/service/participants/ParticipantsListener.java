efc89524deb7143b18b61eea4cf0c46583b1e04c
Anton Georgiev <anto.georgiev@gmail.com>
2014-04-07 21:17:04 +0000

    user_joined_event in pubsub on channel bigbluebutton:meeting:anton results in the user added to the userlist in the flash client

--- bigbluebutton-apps/src/main/java/org/bigbluebutton/conference/service/participants/ParticipantsListener.java
+++ bigbluebutton-apps/src/main/java/org/bigbluebutton/conference/service/participants/ParticipantsListener.java
@@ -8,53 +8,22 @@ import org.apache.commons.lang.StringEscapeUtils;
 import org.bigbluebutton.conference.service.messaging.MessagingConstants;
 import org.bigbluebutton.conference.service.messaging.redis.MessageHandler;
 
-import org.bigbluebutton.conference.service.presentation.ConversionUpdatesProcessor;
+//import org.bigbluebutton.conference.service.participants.participantsApplication;
+import org.bigbluebutton.core.api.IBigBlueButtonInGW;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.google.gson.JsonParser;
 import com.google.gson.JsonObject;
 
-
 public class ParticipantsListener implements MessageHandler{
 	
-	private ConversionUpdatesProcessor conversionUpdatesProcessor;
+	private IBigBlueButtonInGW bbbGW;
 	
-	public void setConversionUpdatesProcessor(ConversionUpdatesProcessor p) {
-		conversionUpdatesProcessor = p;
-	}	
+	public void setBigBlueButtonInGW(IBigBlueButtonInGW bbbGW) {
+		this.bbbGW = bbbGW;
+	}
 
-	private void sendConversionUpdate(String messageKey, String conference, 
-			                          String code, String presId, String filename) {
-		conversionUpdatesProcessor.sendConversionUpdate(messageKey, conference,
-				code, presId, filename);
-	}
-	
-	public void sendPageCountError(String messageKey, String conference, 
-            String code, String presId, Integer numberOfPages,
-            Integer maxNumberPages, String filename) {
-		conversionUpdatesProcessor.sendPageCountError(messageKey, conference, 
-	            code, presId, numberOfPages,
-	            maxNumberPages, filename);
-	}
-	
-	private void sendSlideGenerated(String messageKey, String conference, 
-            String code, String presId, Integer numberOfPages,
-            Integer pagesCompleted, String filename) {
-		conversionUpdatesProcessor.sendSlideGenerated(messageKey, conference, 
-	            code, presId, numberOfPages,
-	            pagesCompleted, filename);
-	}
-		
-	private void sendConversionCompleted(String messageKey, String conference, 
-            String code, String presId, Integer numberOfPages,
-             String filename, String presBaseUrl) {
-		
-		conversionUpdatesProcessor.sendConversionCompleted(messageKey, conference,  
-	            code, presId, numberOfPages, filename, presBaseUrl);
-	}
-	
-	
 	@Override
 	public void handleMessage(String pattern, String channel, String message) {
 		if (channel.equalsIgnoreCase(MessagingConstants.ANTON_CHANNEL))
@@ -66,6 +35,7 @@ public class ParticipantsListener implements MessageHandler{
 			JsonObject obj = (JsonObject) parser.parse(message);
 			JsonObject header = (JsonObject) obj.get("header");
 			//System.out.println ("header="+header);
+			JsonObject payload = (JsonObject) obj.get("payload");
 
 			String eventName = (String) header.get("name").toString();
 			eventName = eventName.replace("\"", "");//strip off quotations
@@ -74,6 +44,23 @@ public class ParticipantsListener implements MessageHandler{
 			if(eventName.equalsIgnoreCase("user_joined_event")) //put this string into a constants file
 			{
 				System.out.println("I'm in the case for joined_event" );
+				
+				JsonObject user = (JsonObject) payload.get("user");
+				JsonObject meeting = (JsonObject) payload.get("meeting");
+				
+				String meetingID = (String) meeting.get("id").toString().replace("\"", "");
+				String userid = (String) user.get("id").toString().replace("\"", "");
+				String username = (String) user.get("name").toString().replace("\"", "");
+				String role = (String) user.get("role").toString().replace("\"", "");
+				String externUserID = (String) user.get("external_id").toString().replace("\"", "");
+
+				System.out.println("4");
+				System.out.println("\nmeetingID="+meetingID+", "+"userid = "+userid+", username="+username+", role="+role+
+					"external_id="+externUserID);
+
+				bbbGW.userJoin(meetingID, userid,  username, role, externUserID);
+
+				System.out.println("5");
 			}
 			else if(eventName.equalsIgnoreCase("user_left_event")) //put this string into a constants file
 			{
@@ -81,6 +68,8 @@ public class ParticipantsListener implements MessageHandler{
 			}
 		}
 	}
+
+	
 }
 
 
