87155023b82f800f2774b60610226f423b534549
Anton Georgiev <anto.georgiev@gmail.com>
2014-05-09 21:21:16 +0000

    asking for username, the username reaches the node app

--- labs/demos/app.js
+++ labs/demos/app.js
@@ -1,9 +1,15 @@
 var myModule = angular.module('landingPage', []);
 
 myModule.controller('MainCtrl', function($scope, $http) {
-	$scope.username = "sampleNickname";
-	$scope.postUsername = function() {
-		$http.post('http://192.168.0.203:4000/login', $scope.username);
+	//$scope.username = "antobinary";
+	/*$scope.postUsername = function(username) {
+		$http.post('http://192.168.0.203:4000/login?username=jojo', $scope.username);
 		//alert("Surprise!!");
-	} 
+	}*/
+	$scope.postUsername = function(username) {
+		console.log("the passed uname=" + this.username);
+		$http.post('http://192.168.0.203:4000/login?username=' + $scope.username).success(function(res){
+			console.log("SUCCESS," + JSON.stringify(res.hi));
+		});
+	}
 });
--- labs/demos/handlers.coffee
+++ labs/demos/handlers.coffee
@@ -2,8 +2,8 @@ index = (request, response) ->
   response.sendfile('./views/index.html')
   
 login = (req, resp) -> #not working yet
-  console.log "req=" + JSON.stringify req
-  resp "heya"
+  console.log "req=" + JSON.stringify req.query.username
+
 
 exports.index = index
 exports.login = login
