f4f9da85220c41270e86bc4ca240853f32d3fa9e
Anton Georgiev <anto.georgiev@gmail.com>
2014-05-12 15:40:48 +0000

    introducing config.json for IP:PORT

--- labs/demos/public/js/app.js
+++ labs/demos/public/js/app.js
@@ -1,10 +1,14 @@
 var myModule = angular.module('landingPage', []);
 
+
+
 myModule.controller('MainCtrl', function($scope, $http) {
 	$scope.postUsername = function(username) {
 		console.log("the passed uname=" + this.username);
-		$http.post('http://192.168.0.203:4000/login?username=' + $scope.username).success(function(res){
-			console.log("SUCCESS," + JSON.stringify(res.hi));
+		jQuery.getJSON("config.json", function (json) {
+			$http.post(json.settings.IP + ':' + json.settings.PORT + '/login?username=' + $scope.username).success(function(res){
+				console.log("SUCCESS," + JSON.stringify(res.hi));
+			});
 		});
 	}
 });
