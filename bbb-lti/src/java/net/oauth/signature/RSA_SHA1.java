12692e66cb04afcf2dfa16f38b5e9a9f0ff0ce69
Jesus Federico <jesus@123it.ca>
2014-08-29 10:47:46 -0400

    Updated OAuth library

--- bbb-lti/src/java/net/oauth/signature/RSA_SHA1.java
+++ bbb-lti/src/java/net/oauth/signature/RSA_SHA1.java
@@ -17,6 +17,8 @@
 package net.oauth.signature;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.security.GeneralSecurityException;
 import java.security.KeyFactory;
@@ -26,12 +28,15 @@ import java.security.Signature;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 import java.security.spec.EncodedKeySpec;
+import java.security.spec.KeySpec;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 
 import net.oauth.OAuth;
 import net.oauth.OAuthAccessor;
 import net.oauth.OAuthException;
+import net.oauth.signature.pem.PEMReader;
+import net.oauth.signature.pem.PKCS1EncodedKeySpec;
 
 /**
  * Class to handle RSA-SHA1 signatures on OAuth requests. A consumer
@@ -44,10 +49,9 @@ import net.oauth.OAuthException;
  * c.setProperty(RSA_SHA1.PRIVATE_KEY, consumer_privateRSAKey);
  *
  * consumer_privateRSAKey must be an RSA signing key and
- * of type java.security.PrivateKey, String, or byte[]. In the latter two
- * cases, the key must be PKCS#8-encoded (byte[]) or PKCS#8-encoded and
- * then Base64-encoded (String).
- *
+ * of type java.security.PrivateKey, String, byte[] or InputStream. 
+ * The key must either PKCS#1 or PKCS#8 encoded.
+ * 
  * A service provider that wishes to verify signatures made by such a
  * consumer does not need a shared secret with the consumer, but it needs
  * to know the consumer's public key. You create the necessary
@@ -87,71 +91,34 @@ public class RSA_SHA1 extends OAuthSignatureMethod {
 
     private PrivateKey privateKey = null;
     private PublicKey publicKey = null;
-
+    
     @Override
     protected void initialize(String name, OAuthAccessor accessor)
     throws OAuthException {
         super.initialize(name, accessor);
 
-        Object privateKeyObject = accessor.consumer.getProperty(PRIVATE_KEY);
+        // Due to the support of PEM input stream, the keys must be cached. 
+        // The stream may not be markable so it can't be read again.
         try {
+            Object privateKeyObject = accessor.consumer.getProperty(PRIVATE_KEY);
             if (privateKeyObject != null) {
-                if (privateKeyObject instanceof PrivateKey) {
-                    privateKey = (PrivateKey)privateKeyObject;
-                } else if (privateKeyObject instanceof String) {
-                    privateKey = getPrivateKeyFromPem((String)privateKeyObject);
-                } else if (privateKeyObject instanceof byte[]) {
-                    privateKey = getPrivateKeyFromDer((byte[])privateKeyObject);
-                } else {
-                    throw new IllegalArgumentException(
-                            "Private key set through RSA_SHA1.PRIVATE_KEY must be of " +
-                            "type PrivateKey, String, or byte[], and not " +
-                            privateKeyObject.getClass().getName());
-                }
+                privateKey = loadPrivateKey(privateKeyObject);
             }
 
             Object publicKeyObject = accessor.consumer.getProperty(PUBLIC_KEY);
             if (publicKeyObject != null) {
-                if (publicKeyObject instanceof PublicKey) {
-                    publicKey = (PublicKey)publicKeyObject;
-                } else if (publicKeyObject instanceof String) {
-                    publicKey = getPublicKeyFromPem((String)publicKeyObject);
-                } else if (publicKeyObject instanceof byte[]) {
-                    publicKey = getPublicKeyFromDer((byte[])publicKeyObject);
-                } else {
-                    throw new IllegalArgumentException(
-                            "Public key set through RSA_SHA1.PRIVATE_KEY must be of " +
-                            "type PublicKey, String, or byte[], and not " +
-                            publicKeyObject.getClass().getName());
-                }
+                publicKey = loadPublicKey(publicKeyObject, false);
             } else {  // public key was null. perhaps they gave us a X509 cert.
                 Object certObject = accessor.consumer.getProperty(X509_CERTIFICATE);
                 if (certObject != null) {
-                    if (certObject instanceof X509Certificate) {
-                        publicKey = ((X509Certificate) certObject).getPublicKey();
-                    } else if (certObject instanceof String) {
-                        publicKey = getPublicKeyFromPemCert((String)certObject);
-                    } else if (certObject instanceof byte[]) {
-                        publicKey = getPublicKeyFromDerCert((byte[])certObject);
-                    } else {
-                        throw new IllegalArgumentException(
-                                "X509Certificate set through RSA_SHA1.X509_CERTIFICATE" +
-                                " must be of type X509Certificate, String, or byte[]," +
-                                " and not " + certObject.getClass().getName());
-                    }
+                    publicKey = loadPublicKey(certObject, true);
                 }
             }
         } catch (GeneralSecurityException e) {
             throw new OAuthException(e);
+        } catch (IOException e) {
+            throw new OAuthException(e);
         }
-    }
-
-    private PublicKey getPublicKeyFromPemCert(String certObject)
-            throws GeneralSecurityException {
-        CertificateFactory fac = CertificateFactory.getInstance("X509");
-        ByteArrayInputStream in = new ByteArrayInputStream(certObject.getBytes());
-        X509Certificate cert = (X509Certificate)fac.generateCertificate(in);
-        return cert.getPublicKey();
     }
 
     private PublicKey getPublicKeyFromDerCert(byte[] certObject)
@@ -169,9 +136,28 @@ public class RSA_SHA1 extends OAuthSignatureMethod {
         return fac.generatePublic(pubKeySpec);
     }
 
-    private PublicKey getPublicKeyFromPem(String publicKeyObject)
-            throws GeneralSecurityException {
-        return getPublicKeyFromDer(decodeBase64(publicKeyObject));
+    private PublicKey getPublicKeyFromPem(String pem) 
+    throws GeneralSecurityException, IOException {
+
+        InputStream stream = new ByteArrayInputStream(
+                pem.getBytes("UTF-8"));
+
+        PEMReader reader = new PEMReader(stream);
+        byte[] bytes = reader.getDerBytes(); 	
+        PublicKey pubKey;
+
+        if (PEMReader.PUBLIC_X509_MARKER.equals(reader.getBeginMarker())) {
+            KeySpec keySpec = new X509EncodedKeySpec(bytes);
+            KeyFactory fac = KeyFactory.getInstance("RSA");
+            pubKey = fac.generatePublic(keySpec);
+        } else if (PEMReader.CERTIFICATE_X509_MARKER.equals(reader.getBeginMarker())) {
+            pubKey = getPublicKeyFromDerCert(bytes);
+        } else {
+            throw new IOException("Invalid PEM fileL: Unknown marker for " + 
+                    " public key or cert " + reader.getBeginMarker());
+        }
+
+        return pubKey;
     }
 
     private PrivateKey getPrivateKeyFromDer(byte[] privateKeyObject)
@@ -181,9 +167,27 @@ public class RSA_SHA1 extends OAuthSignatureMethod {
         return fac.generatePrivate(privKeySpec);
     }
 
-    private PrivateKey getPrivateKeyFromPem(String privateKeyObject)
-            throws GeneralSecurityException {
-        return getPrivateKeyFromDer(decodeBase64(privateKeyObject));
+    private PrivateKey getPrivateKeyFromPem(String pem)
+    throws GeneralSecurityException, IOException {
+
+        InputStream stream = new ByteArrayInputStream(
+                pem.getBytes("UTF-8"));
+
+        PEMReader reader = new PEMReader(stream);
+        byte[] bytes = reader.getDerBytes();
+        KeySpec keySpec;
+
+        if (PEMReader.PRIVATE_PKCS1_MARKER.equals(reader.getBeginMarker())) {
+            keySpec = (new PKCS1EncodedKeySpec(bytes)).getKeySpec();
+        } else if (PEMReader.PRIVATE_PKCS8_MARKER.equals(reader.getBeginMarker())) {
+            keySpec = new PKCS8EncodedKeySpec(bytes);
+        } else {
+            throw new IOException("Invalid PEM file: Unknown marker " +
+                    "for private key " + reader.getBeginMarker());
+        }
+
+        KeyFactory fac = KeyFactory.getInstance("RSA");
+        return fac.generatePrivate(keySpec);
     }
 
     @Override
@@ -234,5 +238,100 @@ public class RSA_SHA1 extends OAuthSignatureMethod {
         verifier.initVerify(publicKey);
         verifier.update(message);
         return verifier.verify(signature);
+    }
+    
+    /**
+     * Load private key from various sources, including
+     * <ul>
+     * <li>A PrivateKey object
+     * <li>A string buffer for PEM
+     * <li>A byte array with PKCS#8 encoded key
+     * </ul>
+     * @param privateKeyObject
+     * @return The private key
+     * @throws IOException
+     * @throws GeneralSecurityException
+     */
+    private PrivateKey loadPrivateKey(Object privateKeyObject) 
+    throws IOException, GeneralSecurityException {
+
+        PrivateKey privateKey;
+
+        if (privateKeyObject instanceof PrivateKey) {
+            privateKey = (PrivateKey)privateKeyObject;
+        } else if (privateKeyObject instanceof String) {
+            try {
+                // PEM Reader's native string constructor is for filename.
+                privateKey = getPrivateKeyFromPem((String)privateKeyObject);
+            } catch (IOException e) {
+                // Check if it's PEM with markers stripped
+                privateKey = getPrivateKeyFromDer(
+                        decodeBase64((String)privateKeyObject));
+            }
+        } else if (privateKeyObject instanceof byte[]) {
+            privateKey = getPrivateKeyFromDer((byte[])privateKeyObject);
+        } else {
+            throw new IllegalArgumentException(
+                    "Private key set through RSA_SHA1.PRIVATE_KEY must be of " +
+                    "type PrivateKey, String or byte[] and not " +
+                    privateKeyObject.getClass().getName());
+        }
+
+        return privateKey;
+    }
+
+    /**
+     * Load a public key from key file or certificate. It can load from
+     * different sources depending on the type of the input,
+     * <ul>
+     * <li>A PublicKey object
+     * <li>A X509Certificate object
+     * <li>A string buffer for PEM
+     * <li>A byte array with X509 encoded key or certificate
+     * </ul>
+     * 
+     * @param publicKeyObject The object for public key or certificate
+     * @param isCert True if this object is provided as Certificate
+     * @return The public key
+     * @throws IOException
+     * @throws GeneralSecurityException
+     */
+    private PublicKey loadPublicKey(Object publicKeyObject, boolean isCert) 
+    throws IOException, GeneralSecurityException {
+
+        PublicKey publicKey;
+
+        if (publicKeyObject instanceof PublicKey) {
+            publicKey = (PublicKey)publicKeyObject;
+        } else if (publicKeyObject instanceof X509Certificate) {
+            publicKey = ((X509Certificate) publicKeyObject).getPublicKey();
+        } else if (publicKeyObject instanceof String) {
+            try {
+                publicKey = getPublicKeyFromPem((String)publicKeyObject);
+            } catch (IOException e) {
+                // Check if it's marker-stripped PEM for public key
+                if (isCert) 
+                    throw e;
+                publicKey = getPublicKeyFromDer(
+                        decodeBase64((String)publicKeyObject));
+            }
+        } else if (publicKeyObject instanceof byte[]) { 
+            if (isCert)
+                publicKey = getPublicKeyFromDerCert((byte[])publicKeyObject);
+            else
+                publicKey = getPublicKeyFromDer((byte[])publicKeyObject);
+        } else {
+            String source;
+            if (isCert)
+                source = "RSA_SHA1.X509_CERTIFICATE";
+            else
+                source = "RSA_SHA1.PUBLIC_KEY";
+            throw new IllegalArgumentException(
+                    "Public key or certificate set through " + source + " must be of " +
+                    "type PublicKey, String or byte[], and not " +
+                    publicKeyObject.getClass().getName());
+        }
+
+        return publicKey;
     }
 }
