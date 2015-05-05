3f7f19fe6f8d67de906340a579f2b833e7259e67
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2015-05-05 12:55:21 -0400

    Fix "cannot find symbol Date" on bbb-web

--- bigbluebutton-web/src/java/org/bigbluebutton/api/MeetingService.java
+++ bigbluebutton-web/src/java/org/bigbluebutton/api/MeetingService.java
@@ -22,6 +22,7 @@ package org.bigbluebutton.api;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
