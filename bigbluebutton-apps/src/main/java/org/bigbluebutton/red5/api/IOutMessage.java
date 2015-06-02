4929d261cec966a4dd38f524274c9e12327bad21
Richard Alam <ritzalam@gmail.com>
2015-06-02 19:38:38 +0000

    - cleanup

--- bigbluebutton-apps/src/main/java/org/bigbluebutton/red5/api/IOutMessage.java
+++
@@ -1,5 +0,0 @@
-package org.bigbluebutton.red5.api;
-
-public interface IOutMessage {
-
-}
--- bigbluebutton-apps/src/main/java/org/bigbluebutton/red5/api/OutMessageListener2.java
+++
@@ -1,6 +0,0 @@
-package org.bigbluebutton.red5.api;
-
-public interface OutMessageListener2 {
-
-	void handleMessage(IOutMessage msg);
-}
