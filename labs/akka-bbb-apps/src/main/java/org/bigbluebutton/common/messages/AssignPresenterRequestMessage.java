8515d543083e7b848efb64211329245f67723051
Richard Alam <ritzalam@gmail.com>
2015-06-04 20:50:04 +0000

    - fix switch presenter message

--- labs/akka-bbb-apps/src/main/java/org/bigbluebutton/common/messages/AssignPresenterRequestMessage.java
+++ labs/akka-bbb-apps/src/main/java/org/bigbluebutton/common/messages/AssignPresenterRequestMessage.java
@@ -44,12 +44,11 @@ public class AssignPresenterRequestMessage implements IPublishedMessage {
 				String messageName = header.get("name").getAsString();
 				if (ASSIGN_PRESENTER_REQUEST.equals(messageName)) {
 					if (payload.has(Constants.MEETING_ID)
-							&& payload.has(Constants.USER_ID)
 							&& payload.has(Constants.NEW_PRESENTER_ID)
 							&& payload.has(Constants.NEW_PRESENTER_NAME)
 							&& payload.has(Constants.ASSIGNED_BY)) {
 						String meetingId = payload.get(Constants.MEETING_ID).getAsString();
-						String newPresenterId = payload.get(Constants.USER_ID).getAsString();
+						String newPresenterId = payload.get(Constants.NEW_PRESENTER_ID).getAsString();
 						String newPresenterName = payload.get(Constants.NEW_PRESENTER_NAME).getAsString();
 						String assignedBy = payload.get(Constants.ASSIGNED_BY).getAsString();
 
