2a93dbf4a787d24c028ef882de390d5723bcc3cb
Richard Alam <ritzalam@gmail.com>
2015-06-17 15:59:27 +0000

    - fix https://code.google.com/p/bigbluebutton/issues/detail?id=1953

--- bigbluebutton-client/resources/prod/lib/bbb_deskshare.js
+++ bigbluebutton-client/resources/prod/lib/bbb_deskshare.js
@@ -3,6 +3,9 @@ if (!window.console.log) window.console.log = function () { };
 
 function startApplet(IP, roomNumber, fullScreen, useSVC2)
 {
+    var deskshareElement = document.getElementById("deskshare");
+    if (deskshareElement == null) {
+
         console.log("Starting deskshare applet.");
         var div = document.createElement("div");
         div.id = "deskshare";
@@ -20,15 +23,40 @@ function startApplet(IP, roomNumber, fullScreen, useSVC2)
                         "<param name=\"permissions\" value=\"all-permissions\"/>" +
                 "</applet>";
         document.body.appendChild(div);
+    } else {
+      console.log("Deskshare applet element already exists.");
+      var div = document.getElementById("deskshare");
+      if (div.parentNode) {
+        // Just rewrite the applet tag to kick off the applet. We don't remove the applet tag
+        // when desktop sharing is stopped to prevent Firefox (38.0.5) from asking for user permissions
+        // again resulting in the page reloading. (ralam june 17, 2015)
+        // https://code.google.com/p/bigbluebutton/issues/detail?id=1953
+        div.innerHTML =
+                "<applet code=\"org.bigbluebutton.deskshare.client.DeskShareApplet.class\"" +
+                        "id=\"DeskShareApplet\" width=\"100\" height=\"10\" archive=\"bbb-deskshare-applet-0.9.0.jar\">" +
+                        "<param name=\"ROOM\" value=\"" + roomNumber  + "\"/>" +
+                        "<param name=\"IP\" value=\"" + IP + "\"/>" +
+                        "<param name=\"PORT\" value=\"9123\"/>" +
+                        "<param name=\"SCALE\" value=\"0.8\"/>" +
+                        "<param name=\"FULL_SCREEN\" value=\"" + fullScreen + "\"/>" +
+                        "<param name=\"SVC2\" value=\"" + useSVC2 + "\"/>" +
+                        "<param name=\"JavaVersion\" value=\"1.7.0_51\"/>" +
+                        "<param name=\"permissions\" value=\"all-permissions\"/>" +
+                "</applet>";
+      }
+    }
 }
+
 
 function removeFrame () {
         var div = document.getElementById("deskshare");
         if (div.parentNode) {
           // Need to set the innerHTML otherwise the applet will restart in IE.
           // see https://code.google.com/p/bigbluebutton/issues/detail?id=1776
-          div.innerHTML = "";
-          div.parentNode.removeChild(div);
+          // Do NOT remove the applet tag as it makes Firefox (38.0.5) prompt for
+          // permissions again resulting in the page reloading. (ralam june 17, 2015)
+ //         div.innerHTML = "";
+//          div.parentNode.removeChild(div);
         }
 }
 
