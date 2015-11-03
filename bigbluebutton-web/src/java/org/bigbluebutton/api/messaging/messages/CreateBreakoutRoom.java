5c6ab0422e445ba51b0851d08b38746a7c5cd1c3
Richard Alam <ritzalam@gmail.com>
2015-11-03 20:14:40 +0000

    - fix compile issue

--- bigbluebutton-web/src/java/org/bigbluebutton/api/messaging/messages/CreateBreakoutRoom.java
+++ bigbluebutton-web/src/java/org/bigbluebutton/api/messaging/messages/CreateBreakoutRoom.java
@@ -3,7 +3,8 @@ package org.bigbluebutton.api.messaging.messages;
 
 public class CreateBreakoutRoom implements IMessage {
 
-  public final String meetingId;      // The main meeting internal id
+  public final String breakoutId;
+  public final String parentId;      // The main meeting internal id
   public final String name;               // The name of the breakout room
   public final String voiceConfId;        // The voice conference id
   public final String viewerPassword;
@@ -11,10 +12,11 @@ public class CreateBreakoutRoom implements IMessage {
   public final Integer durationInMinutes; // The duration of the breakout room
   public final String defaultPresentationURL;
 	
-	public CreateBreakoutRoom(String meetingId, String name, 
+	public CreateBreakoutRoom(String breakoutId, String parentId, String name, 
       String voiceConfId, String viewerPassword, String moderatorPassword, 
       Integer duration, String defaultPresentationURL) {
-    this.meetingId = meetingId;
+	  this.breakoutId = breakoutId;
+    this.parentId = parentId;
     this.name = name;
     this.voiceConfId = voiceConfId;
     this.viewerPassword = viewerPassword;
