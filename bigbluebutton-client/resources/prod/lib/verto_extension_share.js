77a6dd586dc9263319d057b73b5b24ba657b1c7f
perroned <danny.perrone77@gmail.com>
2016-01-18 07:29:42 -0800

    Support for html5 and Flash

--- bigbluebutton-client/resources/prod/lib/verto_extension_share.js
+++ bigbluebutton-client/resources/prod/lib/verto_extension_share.js
@@ -11,11 +11,11 @@ function endScreenshare(loggingCallback, onSuccess) {
 	loggingCallback({'status':'success', 'message': 'screenshare ended'});
 }
 
-function startScreenshare(loggingCallback, videoTag, vertoServerCredentials, extensionId, onSuccess, onFail) {
+function startScreenshare(loggingCallback, videoTag, vertoServerCredentials, extensionId, modifyResolution, onSuccess, onFail) {
 	if(!isLoggedIntoVerto()) { // start the verto log in procedure
 		// runs when the websocket is successfully created
 		callbacks.onWSLogin = function(v, success) {
-			startScreenshare2(loggingCallback, videoTag, extensionId, onSuccess, onFail);
+			startScreenshare2(loggingCallback, videoTag, extensionId, modifyResolution, onSuccess, onFail);
 			loggingCallback({'status':'success', 'message': 'screenshare started'});
 			console.log("logged in. starting screenshare");
 		}
@@ -23,12 +23,12 @@ function startScreenshare(loggingCallback, videoTag, vertoServerCredentials, ext
 		init(window.videoTag, vertoServerCredentials);
 	} else {
 		console.log("already logged into verto, going straight to making a call");
-		startScreenshare2(loggingCallback, videoTag, extensionId, onSuccess, onFail);
+		startScreenshare2(loggingCallback, videoTag, extensionId, modifyResolution, onSuccess, onFail);
 		loggingCallback({'status':'success', 'message': 'screenshare started'});
 	}
 }
 
-function startScreenshare2(loggingCallback, videoTag, extensionId, onSuccess, onFail) {
+function startScreenshare2(loggingCallback, videoTag, extensionId, modifyResolution, onSuccess, onFail) {
 	if (share_call) {
 		return;
 	}
@@ -59,12 +59,10 @@ function startScreenshare2(loggingCallback, videoTag, extensionId, onSuccess, on
 	var callbacks = {
 		onError: function(vertoErrorObject, errorMessage) {
 			console.error("custom callback: onError");
-			//
 			onFail();
 		},
 		onICEComplete: function(self, candidate) { // ICE candidate negotiation is complete
 			console.log("custom callback: onICEComplete");
-			//
 			onSuccess();
 		}
 	};
@@ -80,27 +78,7 @@ function startScreenshare2(loggingCallback, videoTag, extensionId, onSuccess, on
 			frameRate : {min: 15, max: 30}
 		};
 
-		window.listenOnly = false;
-		window.watchOnly = false;
-		window.joinAudio = true;
-
-		BBB.getMyUserInfo(function (retData){
-			share_call = verto.newCall({
-				destination_number: retData.voiceBridge + "-screen",
-				caller_id_name: retData.myUsername + " (Screen)",
-				caller_id_number: retData.myUserID + " (screen)",
-				outgoingBandwidth: outgoingBandwidth,
-				incomingBandwidth: incomingBandwidth,
-				videoParams: screen_constraints,
-				useVideo: true,
-				screenShare: true,
-				dedEnc: false,
-				mirrorInput: false,
-				tag: "webcam"
-			});
-		});
-
-		share_call.rtc.options.callbacks = $.extend(share_call.rtc.options.callbacks, callbacks);
+		doCall(screen_constraints, videoTag, callbacks);
 	} else {
 		getChromeExtensionStatus(extensionId, function(status) {
 			getScreenConstraints(function(error, screen_constraints) {
@@ -111,57 +89,49 @@ function startScreenshare2(loggingCallback, videoTag, extensionId, onSuccess, on
 				console.log("status", status);
 				console.log('screen_constraints', screen_constraints);
 
-				var selectedDeskshareResolution = getChosenDeskshareResolution(); // this is the video profile the user chose
-				my_real_size(selectedDeskshareResolution);
-				var selectedDeskshareConstraints = getDeskshareConstraintsFromResolution(selectedDeskshareResolution, screen_constraints); // convert to a valid constraints object
-				console.log(selectedDeskshareConstraints);
+				if (modifyResolution) {
+					console.log("modifying video quality");
+					var selectedDeskshareResolution = getChosenDeskshareResolution(); // this is the video profile the user chose
+					my_real_size(selectedDeskshareResolution);
+					var selectedDeskshareConstraints = getDeskshareConstraintsFromResolution(selectedDeskshareResolution, screen_constraints); // convert to a valid constraints object
+					console.log(selectedDeskshareConstraints);
+					screen_constraints = selectedDeskshareConstraints.video.mandatory;
+				} else {
+					screen_constraints = screen_constraints.mandatory;
+					screen_constraints.minFrameRate = 10;
+				}
 
-				window.listenOnly = false;
-				window.watchOnly = false;
-				window.joinAudio = true;
-
-				BBB.getMyUserInfo(function (retData){
-					share_call = verto.newCall({
-						destination_number: retData.voiceBridge + "-screen",
-						caller_id_name: retData.myUsername + " (Screen)",
-						caller_id_number: retData.myUserID + " (screen)",
-						outgoingBandwidth: outgoingBandwidth,
-						incomingBandwidth: incomingBandwidth,
-						videoParams: selectedDeskshareConstraints.video.mandatory,
-						useVideo: true,
-						screenShare: true,
-						dedEnc: true,
-						mirrorInput: true,
-						tag: videoTag,
-					});
-				});
-
-				share_call.rtc.options.callbacks = $.extend(share_call.rtc.options.callbacks, callbacks);
+				doCall(screen_constraints, videoTag, callbacks);
 			});
 		});
 	}
 }
 
-// return the webcam resolution that the user has selected
-this.getChosenDeskshareResolution = function() {
-	var videoConstraints = getAllPresetVideoResolutions(); // retrieve all resolutions
-	var selectedVideo = videoConstraints[$("#deskshareResolutions").find(":selected").val()];
-	return selectedVideo;
-}
+function doCall(screen_constraints, videoTag, callbacks) {
+	console.log("\n\n\nhere are the screen_constraints\n\n\n");
+	console.log(screen_constraints);
+	window.listenOnly = false;
+	window.watchOnly = false;
+	window.joinAudio = true;
 
-// receives a video resolution profile, and converts it into a constraints format for getUserMedia
-this.getDeskshareConstraintsFromResolution = function(resolution, constraints) {
-	return {
-		"audio": false,
-		"video": {
-			"mandatory": {
-				"maxWidth": resolution.constraints.maxWidth,
-				"maxHeight": resolution.constraints.maxHeight,
-				"chromeMediaSource": constraints.mandatory.chromeMediaSource,
-				"chromeMediaSourceId": constraints.mandatory.chromeMediaSourceId,
-				"minFrameRate": resolution.constraints.minFrameRate
-			},
-			"optional": []
+	BBB.getMyUserInfo(function (retData){
+		var callParams = {
+			destination_number: retData.voiceBridge + "-screen",
+			caller_id_name: retData.myUsername + " (Screen)",
+			caller_id_number: retData.myUserID + " (screen)",
+			outgoingBandwidth: outgoingBandwidth,
+			incomingBandwidth: incomingBandwidth,
+			videoParams: screen_constraints,
+			useVideo: true,
+			screenShare: true,
+			dedEnc: true,
+			mirrorInput: true,
+		};
+
+		if (videoTag != null) {
+			callParams.tag = videoTag;
 		}
-	};
+		share_call = verto.newCall(callParams);
+		share_call.rtc.options.callbacks = $.extend(share_call.rtc.options.callbacks, callbacks);
+	});
 }
