de1ee8575d005a453b1b03456d134b0fcf1356ef
jfederico <jesus@123it.ca>
2016-01-28 14:03:57 -0500

    Reformatted files in conflict

--- bigbluebutton-web/src/java/org/bigbluebutton/api/RecordingService.java
+++ bigbluebutton-web/src/java/org/bigbluebutton/api/RecordingService.java
@@ -1,21 +1,21 @@
 /**
-* BigBlueButton open source conferencing system - http://www.bigbluebutton.org/
-* 
-* Copyright (c) 2012 BigBlueButton Inc. and by respective authors (see below).
-*
-* This program is free software; you can redistribute it and/or modify it under the
-* terms of the GNU Lesser General Public License as published by the Free Software
-* Foundation; either version 3.0 of the License, or (at your option) any later
-* version.
-* 
-* BigBlueButton is distributed in the hope that it will be useful, but WITHOUT ANY
-* WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
-* PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
-*
-* You should have received a copy of the GNU Lesser General Public License along
-* with BigBlueButton; if not, see <http://www.gnu.org/licenses/>.
-*
-*/
+ * BigBlueButton open source conferencing system - http://www.bigbluebutton.org/
+ * 
+ * Copyright (c) 2012 BigBlueButton Inc. and by respective authors (see below).
+ *
+ * This program is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free Software
+ * Foundation; either version 3.0 of the License, or (at your option) any later
+ * version.
+ * 
+ * BigBlueButton is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along
+ * with BigBlueButton; if not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
 package org.bigbluebutton.api;
 
@@ -33,7 +33,7 @@ import org.slf4j.LoggerFactory;
 
 public class RecordingService {
     private static Logger log = LoggerFactory.getLogger(RecordingService.class);
-	
+
     private String processDir = "/var/bigbluebutton/recording/process";
     private String publishedDir = "/var/bigbluebutton/published";
     private String unpublishedDir = "/var/bigbluebutton/unpublished";
@@ -58,28 +58,34 @@ public class RecordingService {
         }
     }
 
-    private boolean shouldIncludeState( ArrayList<String> states, String type ) {
+    private boolean shouldIncludeState(ArrayList<String> states, String type) {
         boolean r = false;
 
-        if ( !states.isEmpty() ) {
-            if ( states.contains("any") ) {
+        if (!states.isEmpty()) {
+            if (states.contains("any")) {
                 r = true;
             } else {
-                if ( type.equals(Recording.STATE_PUBLISHED) && states.contains(Recording.STATE_PUBLISHED) ) {
+                if (type.equals(Recording.STATE_PUBLISHED)
+                        && states.contains(Recording.STATE_PUBLISHED)) {
                     r = true;
-                } else if ( type.equals(Recording.STATE_UNPUBLISHED) && states.contains(Recording.STATE_UNPUBLISHED) ) {
+                } else if (type.equals(Recording.STATE_UNPUBLISHED)
+                        && states.contains(Recording.STATE_UNPUBLISHED)) {
                     r = true;
-                } else if ( type.equals(Recording.STATE_DELETED) && states.contains(Recording.STATE_DELETED) ) {
+                } else if (type.equals(Recording.STATE_DELETED)
+                        && states.contains(Recording.STATE_DELETED)) {
                     r = true;
-                } else if ( type.equals(Recording.STATE_PROCESSING) && states.contains(Recording.STATE_PROCESSING) ) {
+                } else if (type.equals(Recording.STATE_PROCESSING)
+                        && states.contains(Recording.STATE_PROCESSING)) {
                     r = true;
-                } else if ( type.equals(Recording.STATE_PROCESSED) && states.contains(Recording.STATE_PROCESSED) ) {
+                } else if (type.equals(Recording.STATE_PROCESSED)
+                        && states.contains(Recording.STATE_PROCESSED)) {
                     r = true;
                 }
             }
 
         } else {
-            if ( type.equals(Recording.STATE_PUBLISHED) || type.equals(Recording.STATE_UNPUBLISHED) ) {
+            if (type.equals(Recording.STATE_PUBLISHED)
+                    || type.equals(Recording.STATE_UNPUBLISHED)) {
                 r = true;
             }
         }
@@ -87,58 +93,64 @@ public class RecordingService {
         return r;
     }
 
-    public ArrayList<Recording> getRecordings(ArrayList<String> recordIDs, ArrayList<String> states) {
+    public ArrayList<Recording> getRecordings(ArrayList<String> recordIDs,
+            ArrayList<String> states) {
         ArrayList<Recording> recs = new ArrayList<Recording>();
 
-        if ( recordIDs.isEmpty() ) {
-            if ( shouldIncludeState(states, Recording.STATE_PUBLISHED) )
+        if (recordIDs.isEmpty()) {
+            if (shouldIncludeState(states, Recording.STATE_PUBLISHED))
                 recordIDs.addAll(getAllRecordingIds(publishedDir));
 
-            if ( shouldIncludeState(states, Recording.STATE_UNPUBLISHED) )
+            if (shouldIncludeState(states, Recording.STATE_UNPUBLISHED))
                 recordIDs.addAll(getAllRecordingIds(unpublishedDir));
 
-            if ( shouldIncludeState(states, Recording.STATE_DELETED) )
+            if (shouldIncludeState(states, Recording.STATE_DELETED))
                 recordIDs.addAll(getAllRecordingIds(deletedDir));
 
-            if ( shouldIncludeState(states, Recording.STATE_PROCESSING) )
+            if (shouldIncludeState(states, Recording.STATE_PROCESSING))
                 recordIDs.addAll(getAllRecordingIds(processDir));
 
-            if ( shouldIncludeState(states, Recording.STATE_PROCESSED) )
+            if (shouldIncludeState(states, Recording.STATE_PROCESSED))
                 recordIDs.addAll(getAllRecordingIds(processDir));
 
         }
 
-        for(String recordID : recordIDs){
-            if ( shouldIncludeState(states, Recording.STATE_PUBLISHED) ) {
-                ArrayList<Recording> published = getRecordingsForPath(recordID, publishedDir);
+        for (String recordID : recordIDs) {
+            if (shouldIncludeState(states, Recording.STATE_PUBLISHED)) {
+                ArrayList<Recording> published = getRecordingsForPath(recordID,
+                        publishedDir);
                 if (!published.isEmpty()) {
                     recs.addAll(published);
                 }
             }
 
-            if ( shouldIncludeState(states, Recording.STATE_UNPUBLISHED) ) {
-                ArrayList<Recording> unpublished = getRecordingsForPath(recordID, unpublishedDir);
+            if (shouldIncludeState(states, Recording.STATE_UNPUBLISHED)) {
+                ArrayList<Recording> unpublished = getRecordingsForPath(
+                        recordID, unpublishedDir);
                 if (!unpublished.isEmpty()) {
                     recs.addAll(unpublished);
                 }
             }
 
-            if ( shouldIncludeState(states, Recording.STATE_DELETED) ) {
-                ArrayList<Recording> deleted = getRecordingsForPath(recordID, deletedDir);
+            if (shouldIncludeState(states, Recording.STATE_DELETED)) {
+                ArrayList<Recording> deleted = getRecordingsForPath(recordID,
+                        deletedDir);
                 if (!deleted.isEmpty()) {
                     recs.addAll(deleted);
                 }
             }
 
-            if ( shouldIncludeState(states, Recording.STATE_PROCESSING) ) {
-                ArrayList<Recording> processing = getRecordingsForPath(recordID, processDir);
+            if (shouldIncludeState(states, Recording.STATE_PROCESSING)) {
+                ArrayList<Recording> processing = getRecordingsForPath(
+                        recordID, processDir);
                 if (!processing.isEmpty()) {
                     recs.addAll(processing);
                 }
             }
 
-            if ( shouldIncludeState(states, Recording.STATE_PROCESSED) ) {
-                ArrayList<Recording> processed = getRecordingsForPath(recordID, processDir);
+            if (shouldIncludeState(states, Recording.STATE_PROCESSED)) {
+                ArrayList<Recording> processed = getRecordingsForPath(recordID,
+                        processDir);
                 if (!processed.isEmpty()) {
                     recs.addAll(processed);
                 }
@@ -148,10 +160,12 @@ public class RecordingService {
         return recs;
     }
 
-    public boolean recordingMatchesMetadata(Recording recording, Map<String, String> metadataFilters) {
+    public boolean recordingMatchesMetadata(Recording recording,
+            Map<String, String> metadataFilters) {
         for (Map.Entry<String, String> filter : metadataFilters.entrySet()) {
             String metadataValue = recording.getMetadata().get(filter.getKey());
-            if (metadataValue != null && metadataValue.equals(filter.getValue())) {
+            if (metadataValue != null
+                    && metadataValue.equals(filter.getValue())) {
                 // the recording has the metadata specified
                 // AND the value is the same as the filter
             } else {
@@ -161,7 +175,9 @@ public class RecordingService {
         return true;
     }
 
-    public Map<String, Recording> filterRecordingsByMetadata(Map<String, Recording> recordings, Map<String, String> metadataFilters) {
+    public Map<String, Recording> filterRecordingsByMetadata(
+            Map<String, Recording> recordings,
+            Map<String, String> metadataFilters) {
         Map<String, Recording> resultRecordings = new HashMap<String, Recording>();
         for (Map.Entry<String, Recording> entry : recordings.entrySet()) {
             if (recordingMatchesMetadata(entry.getValue(), metadataFilters))
@@ -170,41 +186,45 @@ public class RecordingService {
         return resultRecordings;
     }
 
-    public boolean existAnyRecording(ArrayList<String> idList){
-        ArrayList<String> publishList=getAllRecordingIds(publishedDir);
-        ArrayList<String> unpublishList=getAllRecordingIds(unpublishedDir);
+    public boolean existAnyRecording(ArrayList<String> idList) {
+        ArrayList<String> publishList = getAllRecordingIds(publishedDir);
+        ArrayList<String> unpublishList = getAllRecordingIds(unpublishedDir);
 
-        for (String id:idList) {
-            if (publishList.contains(id)||unpublishList.contains(id)) {
+        for (String id : idList) {
+            if (publishList.contains(id) || unpublishList.contains(id)) {
                 return true;
             }
         }
         return false;
     }
 
-    private ArrayList<String> getAllRecordingIds(String path){
-        ArrayList<String> ids=new ArrayList<String>();
+    private ArrayList<String> getAllRecordingIds(String path) {
+        ArrayList<String> ids = new ArrayList<String>();
 
         String[] format = getPlaybackFormats(path);
         for (int i = 0; i < format.length; i++) {
-            File[] recordings = getDirectories(path + File.separatorChar + format[i]);
+            File[] recordings = getDirectories(path + File.separatorChar
+                    + format[i]);
             for (int f = 0; f < recordings.length; f++) {
-                if(!ids.contains(recordings[f].getName()))
+                if (!ids.contains(recordings[f].getName()))
                     ids.add(recordings[f].getName());
             }
         }
         return ids;
     }
 
-    private ArrayList<Recording> getRecordingsForPath(String recordingId, String path) {
+    private ArrayList<Recording> getRecordingsForPath(String recordingId,
+            String path) {
         ArrayList<Recording> recs = new ArrayList<Recording>();
 
         String[] format = getPlaybackFormats(path);
         for (int i = 0; i < format.length; i++) {
-            File[] recordings = getDirectories(path + File.separatorChar + format[i]);
+            File[] recordings = getDirectories(path + File.separatorChar
+                    + format[i]);
             for (int f = 0; f < recordings.length; f++) {
                 if (recordings[f].getName().startsWith(recordingId)) {
-                    Recording r = getRecordingInfo(path, recordings[f].getName(), format[i]);
+                    Recording r = getRecordingInfo(path,
+                            recordings[f].getName(), format[i]);
                     recs.add(r);
                 }
             }
@@ -212,20 +232,23 @@ public class RecordingService {
         return recs;
     }
 
-    private Recording getRecordingInfo(String path, String recordingId, String format) {
-        Recording rec = recordingServiceHelper.getRecordingInfo(recordingId, path, format);
+    private Recording getRecordingInfo(String path, String recordingId,
+            String format) {
+        Recording rec = recordingServiceHelper.getRecordingInfo(recordingId,
+                path, format);
         return rec;
     }
 
     public void changeState(String recordingId, String state) {
-        if ( state.equals(Recording.STATE_PUBLISHED) ) {
-        // It can only be published if it is unpublished
+        if (state.equals(Recording.STATE_PUBLISHED)) {
+            // It can only be published if it is unpublished
             changeState(unpublishedDir, recordingId, state);
-        } else if ( state.equals(Recording.STATE_UNPUBLISHED) ) {
-        // It can only be unpublished if it is published
+        } else if (state.equals(Recording.STATE_UNPUBLISHED)) {
+            // It can only be unpublished if it is published
             changeState(publishedDir, recordingId, state);
-        } if ( state.equals(Recording.STATE_DELETED) ) {
-        // It can be deleted from any state
+        }
+        if (state.equals(Recording.STATE_DELETED)) {
+            // It can be deleted from any state
             changeState(publishedDir, recordingId, state);
             changeState(unpublishedDir, recordingId, state);
             changeState(deletedDir, recordingId, state);
@@ -235,34 +258,45 @@ public class RecordingService {
     private void changeState(String path, String recordingId, String state) {
         String[] format = getPlaybackFormats(path);
         for (int i = 0; i < format.length; i++) {
-            File[] recordings = getDirectories(path + File.separatorChar + format[i]);
+            File[] recordings = getDirectories(path + File.separatorChar
+                    + format[i]);
             for (int f = 0; f < recordings.length; f++) {
                 if (recordings[f].getName().equalsIgnoreCase(recordingId)) {
                     Recording r = getRecordingInfo(path, recordingId, format[i]);
                     if (r != null) {
                         File dest;
                         if (state.equals(Recording.STATE_PUBLISHED)) {
-                            dest = new File(publishedDir + File.separatorChar + format[i]);
+                            dest = new File(publishedDir + File.separatorChar
+                                    + format[i]);
                         } else if (state.equals(Recording.STATE_UNPUBLISHED)) {
-                            dest = new File(unpublishedDir + File.separatorChar + format[i]);
+                            dest = new File(unpublishedDir + File.separatorChar
+                                    + format[i]);
                         } else if (state.equals(Recording.STATE_DELETED)) {
-                            dest = new File(deletedDir + File.separatorChar + format[i]);
+                            dest = new File(deletedDir + File.separatorChar
+                                    + format[i]);
                         } else {
-                            log.debug(String.format("State: %s, is not supported", state));
+                            log.debug(String.format(
+                                    "State: %s, is not supported", state));
                             return;
                         }
-                        if(!dest.exists()) dest.mkdir();
-                        boolean moved = recordings[f].renameTo(new File(dest, recordings[f].getName()));
+                        if (!dest.exists())
+                            dest.mkdir();
+                        boolean moved = recordings[f].renameTo(new File(dest,
+                                recordings[f].getName()));
                         if (moved) {
                             log.debug("Recording successfully moved!");
                             r.setState(state);
-                            r.setPublished(state.equals(Recording.STATE_PUBLISHED));
+                            r.setPublished(state
+                                    .equals(Recording.STATE_PUBLISHED));
                             if (state.equals(Recording.STATE_DELETED)) {
                                 r.setPlaybackFormat(null);
                                 deleteRecording(recordingId, deletedDir);
                             }
-                            recordingServiceHelper.writeRecordingInfo(dest.getAbsolutePath() + File.separatorChar + recordings[f].getName(), r);
-                            log.debug(String.format("Recording successfully %s!", state));
+                            recordingServiceHelper.writeRecordingInfo(
+                                    dest.getAbsolutePath() + File.separatorChar
+                                            + recordings[f].getName(), r);
+                            log.debug(String.format(
+                                    "Recording successfully %s!", state));
                         } else {
                             log.debug("Recording was not moved");
                         }
@@ -275,7 +309,8 @@ public class RecordingService {
     private void deleteRecording(String id, String path) {
         String[] format = getPlaybackFormats(path);
         for (int i = 0; i < format.length; i++) {
-            File[] recordings = getDirectories(path + File.separatorChar + format[i]);
+            File[] recordings = getDirectories(path + File.separatorChar
+                    + format[i]);
             for (int f = 0; f < recordings.length; f++) {
                 if (recordings[f].getName().equals(id)) {
                     deleteDirectory(recordings[f]);
@@ -286,25 +321,25 @@ public class RecordingService {
     }
 
     private void createDirectory(File directory) {
-        if(!directory.exists()) directory.mkdir();
+        if (!directory.exists())
+            directory.mkdir();
     }
 
     private void deleteDirectory(File directory) {
         /**
-         * Go through each directory and check if it's not empty.
-         * We need to delete files inside a directory before a
-         * directory can be deleted.
+         * Go through each directory and check if it's not empty. We need to
+         * delete files inside a directory before a directory can be deleted.
          **/
-		File[] files = directory.listFiles();				
-		for (int i = 0; i < files.length; i++) {
-		    if (files[i].isDirectory()) {
-		        deleteDirectory(files[i]);
-		    } else {
-		        files[i].delete();
-		    }
-		}
-		// Now that the directory is empty. Delete it.
-		directory.delete();	
+        File[] files = directory.listFiles();
+        for (int i = 0; i < files.length; i++) {
+            if (files[i].isDirectory()) {
+                deleteDirectory(files[i]);
+            } else {
+                files[i].delete();
+            }
+        }
+        // Now that the directory is empty. Delete it.
+        directory.delete();
     }
 
     private File[] getDirectories(String path) {
