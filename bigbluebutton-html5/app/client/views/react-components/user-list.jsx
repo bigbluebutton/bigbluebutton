272ce793f586b6afba807aae1749269b9eb245a6
gcampes <gabrieldecampes@gmail.com>
2016-02-02 17:01:15 -0200

    Change user object that goes into the components. feat. oswaldoacauan

--- bigbluebutton-html5/app/client/views/react-components/user-list.jsx
+++ bigbluebutton-html5/app/client/views/react-components/user-list.jsx
@@ -1,17 +1,36 @@
 UserList = React.createClass({
   mixins: [ReactMeteorData],
   getMeteorData() {
+    const currentUserId = BBB.getCurrentUser().userId;
+
     return {
-      users: Meteor.Users.find().fetch()
+      // All this mapping should be on a service and not on the component itself
+      users: Meteor.Users.find()
+        .fetch()
+        .map(u => u.user)
+        .map(u => {
+          return {
+            name: u.name,
+            isCurrent: u.userid === currentUserId,
+            isPresenter: u.presenter,
+            isModerator: u.role === "MODERATOR",
+            emoji: u.emoji_status,
+            sharingStatus: {
+              isLocked: true, //TODO: Migrate blaze logic
+              isWebcamOpen: u.webcam_stream.length,
+              isListenOnly: u.listenOnly,
+              isMuted: u.voiceUser.muted,
+              isTalking: u.voiceUser.talking,
+            }
+          };
+        })
     };
   },
 
   render() {
     return (
-      <div className="user-list ScrollableWindowY">
-        {this.data.users.map(function(user) {
-          return <UserItem user={user}/>
-        })}
+      <div className="user-list">
+        {this.data.users.map((user) => <UserItem user={user}/>)}
       </div>
     );
   }
