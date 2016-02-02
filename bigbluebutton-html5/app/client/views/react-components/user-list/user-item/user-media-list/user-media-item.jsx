5932cfa2de2e00811072294e6687f26205b3f251
Oswaldo Acauan <oswaldoacauan@gmail.com>
2016-02-02 17:10:51 -0200

    Changed boilerplate stuff to icon component

--- bigbluebutton-html5/app/client/views/react-components/user-list/user-item/user-media-list/user-media-item.jsx
+++ bigbluebutton-html5/app/client/views/react-components/user-list/user-item/user-media-list/user-media-item.jsx
@@ -1,9 +1,7 @@
 UserMediaItem = React.createClass({
   render() {
     return (
-      <span rel="tooltip" data-placement="bottom" title="is only listening">
-        <i className="icon fi-volume-none usericon"></i>
-      </span>
+      <Icon icon="volume-none" title="is only listening"/>
     );
   }
 })
