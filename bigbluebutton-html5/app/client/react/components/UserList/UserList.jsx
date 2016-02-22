bcae6e2051e004dc1aef71b9e2c20b66d9bbff1c
gcampes <gabrieldecampes@gmail.com>
2016-02-22 16:13:28 -0300

    Solve syntax issues

--- bigbluebutton-html5/app/client/react/components/UserList/UserList.jsx
+++ bigbluebutton-html5/app/client/react/components/UserList/UserList.jsx
@@ -1,8 +1,8 @@
-UserList UserListContainer = React.createClass({
+UserList = React.createClass({
   render() {
     return (
       <div className="userlist ScrollableWindowY">
-        {this.props.users.map((user) => <UserItem key={user.id} user={user} currentUserModerator={this.props.currentUserModerator}/>)}
+        {this.props.users.map((user) => <UserItem key={user.id} user={user} currentUserModerator={this.props.currentUser.isModerator}/>)}
       </div>
     );
   }
