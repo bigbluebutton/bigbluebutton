e14d02e0d1ee50109174c26eda955112d8aaef8a
gcampes <gabrieldecampes@gmail.com>
2016-02-22 17:17:24 -0300

    Adds the status icons

--- bigbluebutton-html5/app/client/react/components/UserList/UserList.jsx
+++ bigbluebutton-html5/app/client/react/components/UserList/UserList.jsx
@@ -2,7 +2,7 @@ UserList = React.createClass({
   render() {
     return (
       <div className="userlist ScrollableWindowY">
-        {this.props.users.map((user) => <UserItem key={user.id} user={user} currentUserModerator={this.props.currentUser.isModerator}/>)}
+        {this.props.users.map((user) => <UserItem key={user.id} user={user} currentUser={this.props.currentUser}/>)}
       </div>
     );
   }
