195df420cb20045034b3fe9a34f6cff231025aed
gcampes <gabrieldecampes@gmail.com>
2016-02-22 16:13:28 -0300

    Solve syntax issues

--- bigbluebutton-html5/app/client/react/components/UserList/UserListContainer.jsx
+++ bigbluebutton-html5/app/client/react/components/UserList/UserListContainer.jsx
@@ -5,27 +5,29 @@ UserListContainer = React.createClass({
     const isCurrentUserModerator = currentUser.user.role === "MODERATOR";
     const currentUserId = currentUser.userId;
 
+    const users = Meteor.Users.find().fetch().map(u => u.user).map(u => {
+      return {
+        id: u.userid,
+        name: u.name,
+        isCurrent: u.userid === currentUserId,
+        isPresenter: u.presenter,
+        isModerator: u.role === "MODERATOR",
+        emoji: u.emoji_status,
+        sharingStatus: {
+          isLocked: true, //TODO: Migrate blaze logic
+          isWebcamOpen: u.webcam_stream.length,
+          isListenOnly: u.listenOnly,
+          isMuted: u.voiceUser.muted,
+          isTalking: u.voiceUser.talking
+        }
+      };
+    });
+
     console.log(currentUser);
     return {
       // All this mapping should be on a service and not on the component itself
-      currentUserModerator: isCurrentUserModerator,
-      users: Meteor.Users.find().fetch().map(u => u.user).map(u => {
-        return {
-          id: u.userid,
-          name: u.name,
-          isCurrent: u.userid === currentUserId,
-          isPresenter: u.presenter,
-          isModerator: u.role === "MODERATOR",
-          emoji: u.emoji_status,
-          sharingStatus: {
-            isLocked: true, //TODO: Migrate blaze logic
-            isWebcamOpen: u.webcam_stream.length,
-            isListenOnly: u.listenOnly,
-            isMuted: u.voiceUser.muted,
-            isTalking: u.voiceUser.talking
-          }
-        };
-      })
+      currentUser: users.find(u => u.isCurrent),
+      users: users
     };
   },
 
@@ -34,7 +36,7 @@ UserListContainer = React.createClass({
       <div id="users" className="component">
         <h3 className="meetingTitle">DEMO</h3>
         <div id="user-contents">
-          <UserList users={this.data.users} currentUserModerator={this.data.currentUserModerator}/>
+          <UserList users={this.data.users} currentUser={this.data.currentUser}/>
         </div>
       </div>
     );
