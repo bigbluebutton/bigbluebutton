ab0cf8f8aa6da3d5e0f4d4c13383f46f67adcb0d
gcampes <gabrieldecampes@gmail.com>
2016-02-23 09:18:17 -0300

    Adds event handling for the useritem click, which opens the private chat

--- bigbluebutton-html5/app/client/react/components/UserList/UserItem.jsx
+++ bigbluebutton-html5/app/client/react/components/UserList/UserItem.jsx
@@ -1,8 +1,38 @@
 UserItem = React.createClass({
-  statusicons(user){
+  openPrivateChat(user) {
+    let userIdSelected = user.id;
+
+    if (userIdSelected !== null) {
+      if (userIdSelected === this.props.currentUser.id) {
+        setInSession("inChatWith", "PUBLIC_CHAT");
+      } else {
+        setInSession("inChatWith", userIdSelected);
+      }
+    }
+    if (isPortrait() || isPortraitMobile()) {
+      toggleUserlistMenu();
+      toggleShield();
+    }
+    return setTimeout(() => { // waits until the end of execution queue
+      return $("#newMessageInput").focus();
+    }, 0);
+  },
+
+  render() {
+    const user = this.props.user;
+    return (
+      <div onClick={this.openPrivateChat.bind(this, user)} id="content" className="userItem">
+        {this.renderStatusIcons(user)}
+        {this.renderUserName(user)}
+        {this.renderUnreadBadge(user.unreadMessagesCount)}
+      </div>
+    );
+  },
+
+  renderStatusIcons(user) {
     let statusIcons = [];
 
-    if(this.props.currentUser.isModerator && this.props.currentUser.id !== user.id && !user.isPresenter){
+    if (this.props.currentUser.isModerator && this.props.currentUser.id !== user.id && !user.isPresenter) {
       statusIcons.push((
         <Tooltip className="setPresenter" title={"set " + user.name + " as presenter"}>
           <Icon iconName="projection-screen" className="statusIcon"/>
@@ -10,15 +40,10 @@ UserItem = React.createClass({
       ));
     }
 
-    if(user.isPresenter){
-      statusIcons.push((
-        <Icon iconName="projection-screen" title={user.name + " is the presenter"} className="statusIcon"/>
-      ));
-    }
-    else if(user.isModerator){
-      statusIcons.push((
-        <Icon iconName="torso" title={user.name +  " is a moderator"} className="statusIcon"/>
-      ))
+    if (user.isPresenter) {
+      statusIcons.push((<Icon iconName="projection-screen" title={user.name + " is the presenter"} className="statusIcon"/>));
+    } else if (user.isModerator) {
+      statusIcons.push((<Icon iconName="torso" title={user.name + " is a moderator"} className="statusIcon"/>))
     }
 
     return (
@@ -28,27 +53,16 @@ UserItem = React.createClass({
     );
   },
 
-  render() {
-    const user = this.props.user;
-    return (
-      <div id="content" className="userItem">
-        {this.statusicons(user)}
-        {this.renderUserName(user)}
-        {this.renderUnreadBadge(user.unreadMessagesCount)}
-      </div>
-    );
-  },
-
   renderUserName(user) {
     let classes = ['usernameEntry'];
     let userName = user.name;
 
-    if(user.isCurrent) {
+    if (user.isCurrent) {
       userName = userName.concat(' (you)');
       classes.push('userCurrent');
     }
 
-    if(user.unreadMessagesCount) {
+    if (user.unreadMessagesCount) {
       classes.push('gotUnreadMail');
     }
 
@@ -60,15 +74,14 @@ UserItem = React.createClass({
   },
 
   renderUnreadBadge(unreadMessagesCount) {
-    if(!unreadMessagesCount) {
+    if (!unreadMessagesCount) {
       return;
     }
 
     return (
       <div className="unreadChatNumber">
-        {(unreadMessagesCount > 9) ? '9+' : unreadMessagesCount }
+        {(unreadMessagesCount > 9) ? '9+' : unreadMessagesCount}
       </div>
     );
-  },
-
+  }
 })
