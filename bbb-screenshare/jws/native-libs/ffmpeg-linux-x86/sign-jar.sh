db1427e606bad7e9ab1da7cc9d8f100161f87a76
Richard Alam <ritzalam@gmail.com>
2016-02-26 16:50:23 +0000

    - add ffmpeg linux x86 jar

--- bbb-screenshare/jws/native-libs/ffmpeg-linux-x86/sign-jar.sh
+++ bbb-screenshare/jws/native-libs/ffmpeg-linux-x86/sign-jar.sh
@@ -1,13 +1,7 @@
 FFMPEG=ffmpeg-2.8.5-1.2-SNAPSHOT-linux-x86.jar
+cp ffmpeg-2.8.5-1.2-SNAPSHOT-linux-x86.jar build/libs/ffmpeg-linux-x86-0.0.1.jar
 mkdir workdir
-cp $FFMPEG workdir/ffmpeg-linux-x86.jar
 rm -rf src
-mkdir -p src/main/resources
-cd workdir
-jar xvf ffmpeg-linux-x86.jar
-cp org/bytedeco/javacpp/linux-x86/* ../src/main/resources
-cd ..
 rm -rf workdir
-gradle jar
 ant sign-jar
 cp build/libs/ffmpeg-linux-x86-0.0.1.jar ../../../app/jws/lib/ffmpeg-linux-x86.jar
