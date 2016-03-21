197237cca2ab45beaa4f25297955a10b80f700da
Maxim Khlobystov <maxim.khlobystov@gmail.com>
2016-03-20 22:43:08 -0400

    Set Grunt to rerun the syntax check on any JS/JSX file change.

--- bigbluebutton-html5/Gruntfile.js
+++ bigbluebutton-html5/Gruntfile.js
@@ -8,6 +8,17 @@ module.exports = function (grunt) {
   // configure Grunt
   grunt.initConfig({
 
+    watch: {
+      scripts: {
+        files: ['**/*.js', '**/*.jsx'],
+        tasks: ['force:jscs:check'],
+        options: {
+          event: ['all'],
+          spawn: true
+        },
+      },
+    },
+
     jscs: {
       check: {
         src: ['**/*.js', '**/*.jsx'],
@@ -33,16 +44,29 @@ module.exports = function (grunt) {
         command: 'HOME=/usr/share/meteor JASMINE_SERVER_UNIT=0 JASMINE_SERVER_INTEGRATION=0 JASMINE_CLIENT_INTEGRATION=0 JASMINE_BROWSER=PhantomJS JASMINE_MIRROR_PORT=3000 ROOT_URL=http://127.0.0.1/html5client meteor',
       },
     },
+
+    concurrent: {
+      options: {
+        logConcurrentOutput: true,
+        limit: 3,
+      },
+      meteor_watch: {
+        tasks: ['shell:start_meteor', 'watch']
+      },
+    }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-coffeelint');
   grunt.loadNpmTasks('grunt-jscs');
   grunt.loadNpmTasks('grunt-force-task');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-concurrent');
 
   // sets the default task to run JSCS first (forcing our way past warnings) and then start Meteor:
-  grunt.registerTask('default', ['force:jscs:check', 'shell:start_meteor']);
+  grunt.registerTask('default', ['force:jscs:check', 'concurrent:meteor_watch']);
 
   // sets the autofix task to fix JSCS warning when possible and then start Meteor:
-  grunt.registerTask('autofix', ['force:jscs:fix', 'shell:start_meteor']);
+  grunt.registerTask('autofix', ['force:jscs:autofix', 'concurrent:meteor_watch']);
 };
