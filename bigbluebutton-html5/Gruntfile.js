f36b0512adc6aae9f577f91bda642f877aaf958e
Maxim Khlobystov <maxim.khlobystov@gmail.com>
2016-04-25 08:56:49 -0400

    Added options of running HTML5 client in development/production modes.

--- bigbluebutton-html5/Gruntfile.js
+++ bigbluebutton-html5/Gruntfile.js
@@ -40,9 +40,12 @@ module.exports = function (grunt) {
     },
 
     shell: {
-      start_meteor: {
-        command: 'HOME=/usr/share/meteor JASMINE_SERVER_UNIT=0 JASMINE_SERVER_INTEGRATION=0 JASMINE_CLIENT_INTEGRATION=0 JASMINE_BROWSER=PhantomJS JASMINE_MIRROR_PORT=3000 ROOT_URL=http://127.0.0.1/html5client meteor',
+      start_meteor_development: {
+        command: 'HOME=/usr/share/meteor JASMINE_SERVER_UNIT=0 JASMINE_SERVER_INTEGRATION=0 JASMINE_CLIENT_INTEGRATION=0 JASMINE_BROWSER=PhantomJS JASMINE_MIRROR_PORT=3000 ROOT_URL=http://127.0.0.1/html5client meteor --settings settings-development.json',
       },
+      start_meteor_production: {
+        command: 'HOME=/usr/share/meteor JASMINE_SERVER_UNIT=0 JASMINE_SERVER_INTEGRATION=0 JASMINE_CLIENT_INTEGRATION=0 JASMINE_BROWSER=PhantomJS JASMINE_MIRROR_PORT=3000 ROOT_URL=http://127.0.0.1/html5client meteor --settings settings-production.json',
+      }
     },
 
     concurrent: {
@@ -50,8 +53,11 @@ module.exports = function (grunt) {
         logConcurrentOutput: true,
         limit: 3,
       },
-      meteor_watch: {
-        tasks: ['shell:start_meteor', 'watch']
+      meteor_watch_development: {
+        tasks: ['shell:start_meteor_development', 'watch']
+      },
+      meteor_watch_production: {
+        tasks: ['shell:start_meteor_production', 'watch']
       },
     }
   });
@@ -64,9 +70,11 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-shell');
   grunt.loadNpmTasks('grunt-concurrent');
 
+  var mode = grunt.option('mode') || 'development';
+
   // sets the default task to run JSCS first (forcing our way past warnings) and then start Meteor:
-  grunt.registerTask('default', ['force:jscs:check', 'concurrent:meteor_watch']);
+  grunt.registerTask('default', ['force:jscs:check', 'concurrent:meteor_watch_' + mode]);
 
   // sets the autofix task to fix JSCS warning when possible and then start Meteor:
-  grunt.registerTask('autofix', ['force:jscs:autofix', 'concurrent:meteor_watch']);
+  grunt.registerTask('autofix', ['force:jscs:autofix', 'concurrent:meteor_watch_' + mode]);
 };
