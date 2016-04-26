3756db157551319f3d75e1226baae4ccb15e7371
Anton Georgiev <anto.georgiev@gmail.com>
2016-04-26 23:39:54 +0000

    do next() if the eventName not among handled

--- bigbluebutton-html5/server/server.js
+++ bigbluebutton-html5/server/server.js
@@ -19,6 +19,48 @@ Meteor.startup(() => {
   const eventEmitter = new (Npm.require('events').EventEmitter);
   registerHandlers(eventEmitter);
 
+  const handledMessageTypes = [
+    'get_users_reply',
+    'meeting_created_message',
+    'get_all_meetings_reply',
+    'user_left_voice_message',
+    'user_joined_voice_message',
+    'user_voice_talking_message',
+    'user_voice_muted_message',
+    'user_listening_only',
+    'user_left_message',
+    'validate_auth_token_reply',
+    'user_joined_message',
+    'presenter_assigned_message',
+    'user_emoji_status_message',
+    'user_locked_message',
+    'user_unlocked_message',
+    'meeting_ended_message',
+    'meeting_destroyed_event',
+    'end_and_kick_all_message',
+    'disconnect_all_users_message',
+    'get_chat_history_reply',
+    'send_public_chat_message',
+    'send_private_chat_message',
+    'presentation_shared_message',
+    'get_presentation_info_reply',
+    'presentation_page_changed_message',
+    'presentation_removed_message',
+    'get_whiteboard_shapes_reply',
+    'send_whiteboard_shape_message',
+    'presentation_cursor_updated_message',
+    'whiteboard_cleared_message',
+    'undo_whiteboard_request',
+    'user_eject_from_meeting',
+    'disconnect_user_message',
+    'presentation_page_resized_message',
+    'recording_status_changed_message',
+    'new_permission_settings',
+    'poll_show_result_message',
+    'poll_started_message',
+    'poll_stopped_message',
+    'user_voted_poll_message',
+  ];
 
   // create create a PubSub connection, start listening
   Meteor.redisPubSub = new Meteor.RedisPubSub(function () {
@@ -47,19 +89,29 @@ Meteor.startup(() => {
     console.log("in taskHandler:" + eventName);
 
     if (failures > 0) {
+      next();
       return Meteor.log.error(`got a failure on taskHandler ${eventName} ${failures}`);
       // TODO should we stop or instead return next?
     } else {
       logRedisMessage(eventName, data.jsonMsg);
-      return eventEmitter.emit(eventName, {
-        payload: parsedMsg.payload,
-        header: parsedMsg.header,
 
-        callback: () => {
-          console.log("ready for next message");
-          return next();
-        },
-      });
+      // note!! we first check if we handle the event type. The handled event types are listed in an array
+      // if NOT in the array, call arg.callback()
+      if (handledMessageTypes.indexOf(eventName) > -1) {
+        return eventEmitter.emit(eventName, {
+          payload: parsedMsg.payload,
+          header: parsedMsg.header,
+
+          callback: () => {
+            console.log("ready for next message");
+            return next();
+          },
+        });
+      } else {
+        Meteor.log.error("NOT HANDLING:" + eventName);
+        return next();
+      }
+
     }
   };
   
