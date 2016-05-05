1f3fcf611f8c959df2b76aedcc18c7a837d34773
perroned <danny.perrone77@gmail.com>
2016-05-05 16:15:32 -0500

    Fixes button import path

--- bigbluebutton-html5/imports/ui/components/polling/PollingComponent.jsx
+++ bigbluebutton-html5/imports/ui/components/polling/PollingComponent.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import {Button} from '../Button.jsx';
+import {Button} from '../shared/Button.jsx';
 
 export default class PollingComponent extends React.Component {
   render() {
