109a8da35045698e8c1c01a9e2b7f7c439d22c50
perroned <danny.perrone77@gmail.com>
2016-05-16 09:14:28 -0500

    Fixes whitespace

--- bigbluebutton-html5/imports/api/deskshare/server/modifiers/handleIncomingDeskshareMessage.js
+++ bigbluebutton-html5/imports/api/deskshare/server/modifiers/handleIncomingDeskshareMessage.js
@@ -1,14 +1,14 @@
 import Meetings from '/imports/api/meetings/collection';
 
 export function handleIncomingDeskshareMessage(arg) {
-    const payload = arg.payload;
-    const voiceBridge = Meetings.findOne({ meetingId: payload.meetingId }).voiceConf;
-    const thisMeetingId = payload.meetingId;
-    const deskShareInfo = {
-        vw: payload.vw,
-        vh: payload.vh,
-        voice_bridge: voiceBridge, // payload.voice_bridge
-        broadcasting: payload.broadcasting,
-    };
-    handleDeskShareChange(thisMeetingId, deskShareInfo);
+  const payload = arg.payload;
+  const voiceBridge = Meetings.findOne({ meetingId: payload.meetingId }).voiceConf;
+  const thisMeetingId = payload.meetingId;
+  const deskShareInfo = {
+    vw: payload.vw,
+    vh: payload.vh,
+    voice_bridge: voiceBridge, // payload.voice_bridge
+    broadcasting: payload.broadcasting,
+  };
+  handleDeskShareChange(thisMeetingId, deskShareInfo);
 }
