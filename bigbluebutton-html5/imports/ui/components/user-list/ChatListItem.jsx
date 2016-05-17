947721250d6af7fcfe9d8aaad7f46884a2604fdb
gcampes <gabrieldecampes@gmail.com>
2016-05-17 10:52:27 -0300

    Adds the new layout for the user-list

---
+++ bigbluebutton-html5/imports/ui/components/user-list/ChatListItem.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import styles from './styles.scss';
+import { Link } from 'react-router';
+import classNames from 'classnames';
+
+export default class ChatListItem extends Component {
+  render() {
+
+    return (
+      <li className={styles.chatListItem}>
+        <div className={styles.chatThumbnail}>
+          <i className="icon-bbb-group-chat"></i>
+        </div>
+        <div className={styles.chatName}>
+          <h3>Public Chat</h3>
+        </div>
+        <div className={styles.unreadMessages}>
+          <p>14</p>
+        </div>
+      </li>
+    );
+  }
+}
--- bigbluebutton-html5/imports/ui/components/user-list/UserList.jsx
+++ bigbluebutton-html5/imports/ui/components/user-list/UserList.jsx
@@ -1,23 +1,29 @@
 import React, { Component } from 'react';
 import styles from './styles.scss';
 import { Link } from 'react-router';
+import UserListItem from './UserListItem';
+import ChatListItem from './ChatListItem';
 
 export default class UserList extends Component {
   render() {
     return (
-      <div>
-        <ul>
-          <li><b>USER-LIST</b></li>
-          <li>
-            <Link to="/html5client/users/chat">Open Public chat</Link>
-          </li>
-          <li>
-            <Link to={`/html5client/users/chat/${'fred-dixon'}`}>Open Private Chat With Fred</Link>
-          </li>
-          <li>
-            <Link to={`/html5client/users/chat/${'tiago-jacobs'}`}>Open Private Chat With Tiago</Link>
-          </li>
-        </ul>
+      <div className={styles.userList}>
+        <div className={styles.header}>
+          <h2>Participants</h2>
+        </div>
+
+        <div className={styles.messages}>
+          <h3 className={styles.smallTitle}>Messages</h3>
+          <ul>
+            <ChatListItem />
+          </ul>
+        </div>
+        <div className={styles.participants}>
+          <h3 className={styles.smallTitle}>Participants <small>({this.props.users.length})</small></h3>
+          <ul className={styles.list}>
+            {this.props.users.map(user => <UserListItem accessibilityLabel={'Status abc'}  accessible={true} key={user.id} user={user}/>)}
+          </ul>
+        </div>
       </div>
     );
   }
