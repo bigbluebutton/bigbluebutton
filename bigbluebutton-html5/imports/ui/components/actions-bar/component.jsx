0e9b8f7effe2beb5fa63e0f0ea15fc803ad06892
Oswaldo Acauan <oswaldoacauan@gmail.com>
2016-05-20 13:17:21 -0300

    Add basic styles for actions-bar

--- bigbluebutton-html5/imports/ui/components/actions-bar/component.jsx
+++ bigbluebutton-html5/imports/ui/components/actions-bar/component.jsx
@@ -15,41 +15,44 @@ export default class ActionsBar extends Component {
   render() {
     return (
       <div className={styles.actionsbar}>
-        <Button
-          onClick={this.handleClick}
-          label={'Actions'}
-          color={'primary'}
-          icon={'circle-add'}
-          size={'lg'}
-          circle={true}
-        />
-        &nbsp;
-        <Button
-          onClick={this.handleClick}
-          label={'Mute'}
-          color={'primary'}
-          icon={'audio'}
-          size={'lg'}
-          circle={true}
-        />
-        &nbsp;
-        <Button
-          onClick={this.handleClick}
-          label={'Cam Off'}
-          color={'primary'}
-          icon={'video-off'}
-          size={'lg'}
-          circle={true}
-        />
-        &nbsp;
-        <Button
-          onClick={this.handleClick}
-          label={'Raise'}
-          color={'primary'}
-          icon={'hand'}
-          size={'lg'}
-          circle={true}
-        />
+        <div className={styles.left}>
+          <Button
+            onClick={this.handleClick}
+            label={'Actions'}
+            color={'primary'}
+            icon={'circle-add'}
+            size={'lg'}
+            circle={true}
+          />
+        </div>
+        <div className={styles.center}>
+          <Button
+            onClick={this.handleClick}
+            label={'Mute'}
+            color={'primary'}
+            icon={'audio'}
+            size={'lg'}
+            circle={true}
+          />
+          <Button
+            onClick={this.handleClick}
+            label={'Cam Off'}
+            color={'primary'}
+            icon={'video-off'}
+            size={'lg'}
+            circle={true}
+          />
+          <Button
+            onClick={this.handleClick}
+            label={'Raise'}
+            color={'primary'}
+            icon={'hand'}
+            size={'lg'}
+            circle={true}
+          />
+        </div>
+        <div className={styles.right}>
+        </div>
       </div>
     );
   }
