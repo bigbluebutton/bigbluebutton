ba7cdca6483cd0978245383efe6539df42f47ca0
Oleksandr Zhurbenko <xaerok.od.ua@gmail.com>
2016-05-30 11:12:02 -0700

    Added fade in for the slides and a default content

--- bigbluebutton-html5/imports/ui/components/whiteboard/component.jsx
+++ bigbluebutton-html5/imports/ui/components/whiteboard/component.jsx
@@ -3,6 +3,7 @@ import WhiteboardShapeModel from './shape-factory/component.jsx';
 import { createContainer } from 'meteor/react-meteor-data';
 import Slide from './slide/component.jsx';
 import styles from './styles.scss';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 export default class Whiteboard extends React.Component {
   constructor(props) {
@@ -11,14 +12,46 @@ export default class Whiteboard extends React.Component {
 
   render() {
     return (
-      <div id="whiteboard-paper" style={this.props.svgStyle} className={styles.whiteboard}>
+
+      <div
+        id="whiteboard-paper"
+        style={this.props.svgStyle}
+        className={styles.whiteboard}
+      >
         <div id="svggroup">
-          <svg version='1.1' xmlNS='http://www.w3.org/2000/svg' {...this.props.svgProps} style={this.props.svgStyle}>
-            <Slide current_slide={this.props.current_slide} />
-            { this.props.shapes ? this.props.shapes.map((shape) =>
-              <WhiteboardShapeModel shape={shape} key={shape.shape.id}/>
-            ) : null }
-          </svg>
+        { this.props.current_slide ?
+          <ReactCSSTransitionGroup
+            transitionName={ {
+              enter: styles.enter,
+              enterActive: styles.enterActive,
+              appear: styles.appear,
+              appearActive: styles.appearActive
+            } }
+            transitionAppear={true}
+            transitionEnter={true}
+            transitionLeave={false}
+            transitionAppearTimeout={400}
+            transitionEnterTimeout={400}
+            transitionLeaveTimeout={400}
+          >
+            <svg
+              {...this.props.svgProps}
+              version="1.1"
+              xmlNS="http://www.w3.org/2000/svg"
+              style={this.props.svgStyle}
+              key={this.props.current_slide.slide.id}
+            >
+              <Slide current_slide={this.props.current_slide} />
+              { this.props.shapes ? this.props.shapes.map((shape) =>
+                <WhiteboardShapeModel
+                  shape={shape}
+                  key={shape.shape.id}
+                />
+                )
+              : null }
+            </svg>
+          </ReactCSSTransitionGroup>
+        : null }
         </div>
       </div>
     );
