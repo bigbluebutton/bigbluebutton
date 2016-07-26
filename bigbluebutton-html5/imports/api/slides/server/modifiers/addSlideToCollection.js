860916efc1021f0326d80a3edacecaf041b3966e
Anton Georgiev <anto.georgiev@gmail.com>
2016-07-26 17:47:36 +0000

    refactor 2 cases

--- bigbluebutton-html5/imports/api/slides/server/modifiers/addSlideToCollection.js
+++ bigbluebutton-html5/imports/api/slides/server/modifiers/addSlideToCollection.js
@@ -11,83 +11,56 @@ export function addSlideToCollection(meetingId, presentationId, slideObject) {
   }) == null) {
     const options = url.parse(imageUri);
 
+    let addSlideHelper = function (response) {
+      let contentType = response.headers['content-type'];
+
+        if (contentType.match(/svg/gi) || contentType.match(/png/gi)) {
+          let chunks = [];
+          response.on('data', Meteor.bindEnvironment(function (chunk) {
+            chunks.push(chunk);
+          })).on('end', Meteor.bindEnvironment(function () {
+            let buffer = Buffer.concat(chunks);
+            const dimensions = sizeOf(buffer);
+            const entry = {
+              meetingId: meetingId,
+              presentationId: presentationId,
+              slide: {
+                height_ratio: slideObject.height_ratio,
+                y_offset: slideObject.y_offset,
+                num: slideObject.num,
+                x_offset: slideObject.x_offset,
+                current: slideObject.current,
+                img_uri: slideObject.svg_uri != null ? slideObject.svg_uri : slideObject.png_uri,
+                txt_uri: slideObject.txt_uri,
+                id: slideObject.id,
+                width_ratio: slideObject.width_ratio,
+                swf_uri: slideObject.swf_uri,
+                thumb_uri: slideObject.thumb_uri,
+                width: dimensions.width,
+                height: dimensions.height,
+              },
+            };
+            Slides.insert(entry);
+          }));
+        } else {
+          console.log(`Slide file is not accessible or not ready yet`);
+          console.log(`response content-type`, response.headers['content-type']);
+        }
+    };
+
+    // HTTP connection
     if (process.env.NODE_ENV === 'development') {
       const http = Npm.require('http');
 
       http.get(options, Meteor.bindEnvironment(function (response) {
-        let contentType = response.headers['content-type'];
-
-        if (contentType.match(/svg/gi) || contentType.match(/png/gi)) {
-          let chunks = [];
-          response.on('data', Meteor.bindEnvironment(function (chunk) {
-            chunks.push(chunk);
-          })).on('end', Meteor.bindEnvironment(function () {
-            let buffer = Buffer.concat(chunks);
-            const dimensions = sizeOf(buffer);
-            const entry = {
-              meetingId: meetingId,
-              presentationId: presentationId,
-              slide: {
-                height_ratio: slideObject.height_ratio,
-                y_offset: slideObject.y_offset,
-                num: slideObject.num,
-                x_offset: slideObject.x_offset,
-                current: slideObject.current,
-                img_uri: slideObject.svg_uri != null ? slideObject.svg_uri : slideObject.png_uri,
-                txt_uri: slideObject.txt_uri,
-                id: slideObject.id,
-                width_ratio: slideObject.width_ratio,
-                swf_uri: slideObject.swf_uri,
-                thumb_uri: slideObject.thumb_uri,
-                width: dimensions.width,
-                height: dimensions.height,
-              },
-            };
-            Slides.insert(entry);
-          }));
-        } else {
-          console.log(`Slide file is not accessible or not ready yet`);
-          console.log(`response content-type`, response.headers['content-type']);
-        }
+        addSlideHelper(response);
       }));
     } else {
+      // HTTPS connection
       const https = Npm.require('https');
 
       https.get(options, Meteor.bindEnvironment(function (response) {
-        let contentType = response.headers['content-type'];
-
-        if (contentType.match(/svg/gi) || contentType.match(/png/gi)) {
-          let chunks = [];
-          response.on('data', Meteor.bindEnvironment(function (chunk) {
-            chunks.push(chunk);
-          })).on('end', Meteor.bindEnvironment(function () {
-            let buffer = Buffer.concat(chunks);
-            const dimensions = sizeOf(buffer);
-            const entry = {
-              meetingId: meetingId,
-              presentationId: presentationId,
-              slide: {
-                height_ratio: slideObject.height_ratio,
-                y_offset: slideObject.y_offset,
-                num: slideObject.num,
-                x_offset: slideObject.x_offset,
-                current: slideObject.current,
-                img_uri: slideObject.svg_uri != null ? slideObject.svg_uri : slideObject.png_uri,
-                txt_uri: slideObject.txt_uri,
-                id: slideObject.id,
-                width_ratio: slideObject.width_ratio,
-                swf_uri: slideObject.swf_uri,
-                thumb_uri: slideObject.thumb_uri,
-                width: dimensions.width,
-                height: dimensions.height,
-              },
-            };
-            Slides.insert(entry);
-          }));
-        } else {
-          console.log(`Slide file is not accessible or not ready yet`);
-          console.log(`response content-type`, response.headers['content-type']);
-        }
+        addSlideHelper(response);
       }));
     }
 
