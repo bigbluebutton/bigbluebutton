76574be9c21206b5852eac0897ad068e6bbb28bb
Oleksandr Zhurbenko <xaerok.od.ua@gmail.com>
2016-08-09 12:25:49 -0700

    Fixed a bug with adding an empty initial captions object

--- bigbluebutton-html5/imports/api/captions/server/modifiers/addCaptionsToCollection.js
+++ bigbluebutton-html5/imports/api/captions/server/modifiers/addCaptionsToCollection.js
@@ -6,14 +6,18 @@ export function addCaptionsToCollection(meetingId, locale, captionHistory) {
   let captionsString = captionHistory[1].slice(0);
   let captions = [];
 
-  while (captionsString.length > 0) {
-    if (captionsString.length > 100) {
-      captions.push(captionsString.slice(0, 100));
-      captionsString = captionsString.slice(100);
-    } else {
-      captions.push(captionsString);
-      captionsString = captionsString.slice(captionsString.length);
+  if (captionsString.length > 0) {
+    while (captionsString.length > 0) {
+      if (captionsString.length > 100) {
+        captions.push(captionsString.slice(0, 100));
+        captionsString = captionsString.slice(100);
+      } else {
+        captions.push(captionsString);
+        captionsString = captionsString.slice(captionsString.length);
+      }
     }
+  } else {
+    captions.push('');
   }
 
   let i;
