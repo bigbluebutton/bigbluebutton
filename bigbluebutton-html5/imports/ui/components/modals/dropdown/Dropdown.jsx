a1137743694bd7d34313b4c9d1566761c1477240
Jaeeun Cho <anna.jecho@gmail.com>
2016-08-15 13:26:22 -0400

    Dropdown with dropdown_v2 branch

--- bigbluebutton-html5/imports/ui/components/modals/dropdown/Dropdown.jsx
+++ bigbluebutton-html5/imports/ui/components/modals/dropdown/Dropdown.jsx
@@ -2,58 +2,53 @@ import React, { Component, PropTypes } from 'react';
 import ReactDOM from 'react-dom';
 import Button from '/imports/ui/components/button/component';
 import classNames from 'classnames';
+import Icon from '/imports/ui/components/icon/component';
 import styles from './styles';
-import DropdownContent from './DropdownContent';
-
-
 export default class Dropdown extends Component {
-
   constructor(props) {
     super(props);
     this.state = { isMenuOpen: false, };
     this.showMenu = this.showMenu.bind(this);
     this.hideMenu = this.hideMenu.bind(this);
   }
-
-  componentDidMount () {
-    window.addEventListener( 'click', this.onWindowClick.bind(this) );
-  }
-
-  componentWillUnmount () {
-    window.removeEventListener( 'click', this.onWindowClick.bind(this) );
-  }
-
   showMenu() {
     this.setState({ isMenuOpen: !this.state.isMenuOpen, });
   }
-
   hideMenu() {
     this.setState({ isMenuOpen: false, });
   }
-
-  onWindowClick(event) {
-    const dropdown_element = ReactDOM.findDOMNode(this);
-
-    if(event.target!== dropdown_element && !dropdown_element.contains(event.target)) {
-      this.hideMenu();
-    }
-  }
-
+  // componentDidMount () {
+  //   window.addEventListener( 'click', this.onWindowClick.bind(this) );
+  // }
+  //
+  // componentWillUnmount () {
+  //   window.removeEventListener( 'click', this.onWindowClick.bind(this) );
+  // }
+  //
+  // onWindowClick(event) {
+  //   const dropdown_element = ReactDOM.findDOMNode(this);
+  //
+  //   if(!dropdown_element.contains(event.target)) {
+  //     this.hideMenu();
+  //   }
+  // }
   render() {
-    const { styleName, icon, label } = this.props;
+    const { styleName, icon, label, circle } = this.props;
     return (
-      <div>
+      <div className={styles.dropdown}>
         <Button className={styles.settingBtn}
-                role='button'
-                label={label}
-                icon={icon}
-                ghost={true}
-                circle={true}
-                hideLabel={true}
-                onClick={this.showMenu}
-                ref='menuBtn'/>
-        { this.state.isMenuOpen ?
-          <DropdownContent /> : null
+            role='button'
+            label={'settings'}
+            icon={'more'}
+            ghost={true}
+            circle={true}
+            hideLabel={true}
+            onClick={this.showMenu}
+            ref='menuBtn'/>
+        {
+          this.state.isMenuOpen ?
+          this.props.children
+          : null
         }
       </div>
     );
