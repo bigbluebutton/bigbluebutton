d29dd62801d9f49102604ff3b6938d6145961d25
jfederico <jesus@123it.ca>
2016-08-22 17:23:45 -0400

    bbb-lti: Removed non-used plugins

--- bbb-lti/grails-app/conf/BuildConfig.groovy
+++ bbb-lti/grails-app/conf/BuildConfig.groovy
@@ -82,10 +82,8 @@ grails.project.dependency.resolution = {
         compile ":asset-pipeline:2.5.7"
 
         // plugins needed at runtime but not for compilation
-        runtime ":hibernate4:4.3.10" // or ":hibernate:3.6.10.18"
         runtime ":database-migration:1.4.0"
         runtime ":jquery:1.11.1"
-        runtime ":resources:1.2.14"
         runtime ':twitter-bootstrap:3.3.4'
     }
 }
