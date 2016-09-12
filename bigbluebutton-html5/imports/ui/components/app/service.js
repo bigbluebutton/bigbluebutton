60d545d434443f68568d83e7c39f7e08cd536a89
Gabriel Carvalho de Campes <gabrieldecampes@gmail.com>
2016-09-12 17:18:36 -0300

    Removes comment from app service

--- bigbluebutton-html5/imports/ui/components/app/service.js
+++ bigbluebutton-html5/imports/ui/components/app/service.js
@@ -63,7 +63,7 @@ function redirectToLogoutUrl(reason) {
   console.error(reason);
   console.log('Redirecting user to the logoutURL...');
 
-  // document.location.href = Auth.logoutURL;
+  document.location.href = Auth.logoutURL;
 }
 
 let wasKicked = false;
