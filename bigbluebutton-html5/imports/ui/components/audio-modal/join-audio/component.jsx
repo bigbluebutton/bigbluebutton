77d76a3adc12e6fbab57cd5682974e363edfc365
Matthew Marangoni <matthew.marangoni@gmail.com>
2016-10-11 12:10:06 -0700

    added join audio files

---
+++ bigbluebutton-html5/imports/ui/components/audio-modal/join-audio/component.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import Icon from '/imports/ui/components/icon/component';
+import Button from '/imports/ui/components/button/component';
+import ModalBase from '../modal/base/component';
+import { clearModal } from '/imports/ui/components/app/service';
+import classNames from 'classnames';
+import ReactDOM from 'react-dom';
+import styles from '../styles.scss';
+
+export default class JoinAudio extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleClose = this.handleClose.bind(this);
+  }
+
+  handleClose() {
+    this.setState({ isOpen: false });
+    clearModal();
+
+  handleClick() {
+  }
+
+  render() {
+    return (
+      <div className={styles.center}>
+        <Button className={styles.closeBtn}
+          label={'Close'}
+          icon={'close'}
+          size={'lg'}
+          circle={true}
+          hideLabel={true}
+          onClick={this.handleClose}
+        />
+        <div>
+          How would you like to join the audio?
+        </div>
+      </div>
+      <div className={styles.center}>
+        <Button className={styles.audioBtn}
+          label={'Audio'}
+          icon={'audio'}
+          circle={true}
+          size={'jumbo'}
+          onClick={this.handleClick}
+        />
+        <Button className={styles.audioBtn}
+          label={'Listen Only'}
+          icon={'listen'}
+          circle={true}
+          size={'jumbo'}
+          onClick={this.handleClick}
+        />
+      </div>
+    );
+  }
+};
