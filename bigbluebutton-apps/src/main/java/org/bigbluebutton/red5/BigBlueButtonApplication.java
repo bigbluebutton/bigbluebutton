7ea0ab70374cab2b184301719e6bfbad3ed394e5
Richard Alam <ritzalam@gmail.com>
2016-10-20 19:05:11 +0000

    Track user connection
      Need to track user connections to prevent removing the user when the defuncted connection gets
      cleaned up by Red5.

--- bigbluebutton-apps/src/main/java/org/bigbluebutton/red5/BigBlueButtonApplication.java
+++ bigbluebutton-apps/src/main/java/org/bigbluebutton/red5/BigBlueButtonApplication.java
@@ -22,7 +22,6 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
-
 import org.bigbluebutton.red5.client.messaging.ConnectionInvokerService;
 import org.bigbluebutton.red5.pubsub.MessagePublisher;
 import org.red5.logging.Red5LoggerFactory;
@@ -41,7 +40,9 @@ public class BigBlueButtonApplication extends MultiThreadedApplicationAdapter {
 
 	private ConnectionInvokerService connInvokerService;
 	private MessagePublisher red5InGW;
-	
+
+	private final UserConnectionMapper userConnections = new UserConnectionMapper();
+
 	private final String APP = "BBB";
 	private final String CONN = "RED5-";
 	
@@ -192,6 +193,8 @@ public class BigBlueButtonApplication extends MultiThreadedApplicationAdapter {
 		
 		log.info("User joining bbb-apps: data={}", logStr);
 
+		userConnections.addUserConnection(userId, connId);
+
 		return super.roomConnect(connection, params);
         
 	}
@@ -236,9 +239,14 @@ public class BigBlueButtonApplication extends MultiThreadedApplicationAdapter {
 	    Gson gson = new Gson();
 	    String logStr =  gson.toJson(logData);
 
-		log.info("User leaving bbb-apps: data={}", logStr);
-		red5InGW.userLeft(bbbSession.getRoom(), getBbbSession().getInternalUserID(), sessionId);
-	    
+		boolean removeUser = userConnections.userDisconnected(userId, connId);
+		if (removeUser) {
+			log.info("User leaving bbb-apps: data={}", logStr);
+			red5InGW.userLeft(bbbSession.getRoom(), getBbbSession().getInternalUserID(), sessionId);
+		} else {
+			log.info("User not leaving bbb-apps but just disconnected: data={}", logStr);
+		}
+
 		super.roomDisconnect(conn);
 	}
 	
