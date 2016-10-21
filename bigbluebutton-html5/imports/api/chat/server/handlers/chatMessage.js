d1786d61a6e24f00fa528b4535f813d540ceb61f
Oswaldo Acauan <oswaldoacauan@gmail.com>
2016-10-21 17:08:43 +0000

    Revert wrong changes on chat handler name

--- bigbluebutton-html5/imports/api/chat/server/handlers/chatMessage.js
+++ bigbluebutton-html5/imports/api/chat/server/handlers/chatMessage.js
@@ -2,12 +2,16 @@ import Logger from '/imports/startup/server/logger';
 import { check } from 'meteor/check';
 import addChat from '../modifiers/addChat';
 
-export default function handleMeetingDestruction({ payload }) {
+export default function handleChatMessage({ payload }) {
   const message = payload.message;
   const meetingId = payload.meeting_id;
 
   check(meetingId, String);
   check(message, Object);
+
+  // use current_time instead of message.from_time so that the
+  // chats from Flash and HTML5 have uniform times
+  message.from_time = +(header.current_time);
 
   return addChat(meetingId, message);
 };
