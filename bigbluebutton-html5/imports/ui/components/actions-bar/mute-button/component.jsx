c9221c94f3ec5ad366d20bf807957981e62440b2
Matthew Marangoni <matthew.marangoni@gmail.com>
2016-11-18 13:35:02 -0800

    removed unecessary import statements

--- bigbluebutton-html5/imports/ui/components/actions-bar/mute-button/component.jsx
+++ bigbluebutton-html5/imports/ui/components/actions-bar/mute-button/component.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
-import {createContainer} from 'meteor/react-meteor-data';
-import {callServer} from '/imports/ui/services/api';
 import Button from '/imports/ui/components/button/component';
-import Users from '/imports/api/users/index';
-import Auth from '/imports/ui/services/auth/index';
 
 export default class MuteAudioComponent extends React.Component {
 
