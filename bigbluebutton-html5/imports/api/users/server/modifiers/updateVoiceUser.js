5635f947898297c5df748f0e5e62cbf1b33498fb
Anton Georgiev <anto.georgiev@gmail.com>
2016-11-22 16:15:42 +0000

    refactor updateVoiceUser.js

--- bigbluebutton-html5/imports/api/users/server/modifiers/updateVoiceUser.js
+++ bigbluebutton-html5/imports/api/users/server/modifiers/updateVoiceUser.js
@@ -1,5 +1,4 @@
 import Users from '/imports/api/users';
-import { logger } from '/imports/startup/server/logger';
 import Logger from '/imports/startup/server/logger';
 
 //update a voiceUser - a helper method
@@ -7,130 +6,42 @@ export function updateVoiceUser(meetingId, voiceUserObject, callback = () => {})
   check(meetingId, String);
   check(voiceUserObject, Object);
 
-  let userObject;
-  userObject = Users.findOne({
-    userId: voiceUserObject.web_userid,
+  const webUserId = voiceUserObject.web_userid;
+
+  check(webUserId, String);
+
+  let userObject = Users.findOne({
+    userId: webUserId,
   });
 
-  if (userObject) {
-    const isTalking = voiceUserObject.talking;
+  Logger.debug(`user ${userObject.userId} vu=${JSON.stringify(voiceUserObject)}`);
 
-    check(isTalking, Boolean);
+  check(userObject, Object);
 
-    // talking
-    //if (isTalking) {
-      const selector = {
-        meetingId,
-        userId: voiceUserObject.web_userid,
-      };
+  const selector = {
+    meetingId,
+    userId: webUserId,
+  };
 
-      const modifier = {
-        $set: {
-          'user.voiceUser.talking': isTalking,
-        },
-      };
+  const modifier = {
+    $set: {
+      'user.voiceUser.talking': voiceUserObject.talking || false,
+      'user.voiceUser.joined': voiceUserObject.joined || false,
+      'user.voiceUser.locked': voiceUserObject.locked || false,
+      'user.voiceUser.muted': voiceUserObject.muted || false,
+      'user.listenOnly': voiceUserObject.listen_only || false,
+    },
+  };
 
-      const cb = (err, numChanged) => {
-        if (err) {
-          logger.error(
-              `_unsucc update of voiceUser ${voiceUserObject.web_userid} ` +
-              `[talking] err=${JSON.stringify(err)}`
-          );
-        }
-
-        return callback();
-      };
-
-      Logger.info(`user ${userObject.userId} talking ${isTalking}`);
-      Users.update(selector, modifier, cb);
-    //}
-
-    // joined
-    if (voiceUserObject.joined != null) {
-      Users.update({
-        meetingId: meetingId,
-        userId: voiceUserObject.web_userid,
-      }, {
-        $set: {
-          'user.voiceUser.joined': voiceUserObject.joined,
-        },
-      }, (err, numChanged) => {
-        if (err != null) {
-          logger.error(
-            `_unsucc update of voiceUser ${voiceUserObject.web_userid} ` +
-            `[joined] err=${JSON.stringify(err)}`
-          );
-        }
-
-        return callback();
-      });
+  const cb = (err, numChanged) => {
+    if (err) {
+      Logger.error(
+        `failed to update voiceUser ${JSON.stringify(voiceUserObject)} err=${JSON.stringify(err)}`
+      );
     }
 
-    // locked
-    if (voiceUserObject.locked != null) {
-      Users.update({
-        meetingId: meetingId,
-        userId: voiceUserObject.web_userid,
-      }, {
-        $set: {
-          'user.voiceUser.locked': voiceUserObject.locked,
-        },
-      }, (err, numChanged) => {
-        if (err != null) {
-          logger.error(
-            `_unsucc update of voiceUser ${voiceUserObject.web_userid} ` +
-            `[locked] err=${JSON.stringify(err)}`
-          );
-        }
-
-        return callback();
-      });
-    }
-
-    // muted
-    if (voiceUserObject.muted != null) {
-      Users.update({
-        meetingId: meetingId,
-        userId: voiceUserObject.web_userid,
-      }, {
-        $set: {
-          'user.voiceUser.muted': voiceUserObject.muted,
-        },
-      }, (err, numChanged) => {
-        if (err != null) {
-          logger.error(
-            `_unsucc update of voiceUser ${voiceUserObject.web_userid} ` +
-            `[muted] err=${JSON.stringify(err)}`
-          );
-        }
-
-        return callback();
-      });
-    }
-
-    // listenOnly
-    if (voiceUserObject.listen_only != null) {
-      return Users.update({
-        meetingId: meetingId,
-        userId: voiceUserObject.web_userid,
-      }, {
-        $set: {
-          'user.listenOnly': voiceUserObject.listen_only,
-        },
-      }, (err, numChanged) => {
-        if (err != null) {
-          logger.error(
-            `_unsucc update of voiceUser ${voiceUserObject.web_userid} ` +
-            `[listenOnly] err=${JSON.stringify(err)}`
-          );
-        }
-
-        return callback();
-      });
-    }
-
-  } else {
-    logger.error('ERROR! did not find such voiceUser!');
     return callback();
-  }
+  };
+
+  Users.update(selector, modifier, cb);
 };
