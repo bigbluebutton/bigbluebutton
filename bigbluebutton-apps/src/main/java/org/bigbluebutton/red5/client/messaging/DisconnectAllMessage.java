65dd914f52389e0f8c0f000b1cf84e69cc19d169
Richard Alam <ritzalam@gmail.com>
2017-02-02 16:27:44 +0000

    Handle exception when sending message to client
     Handle exceptions properly when sending message to client. We were not handling the
     exception so when it occured, the thread stopped and we can't send messages from
     server to client anymore.

--- bigbluebutton-apps/src/main/java/org/bigbluebutton/red5/client/messaging/DisconnectAllMessage.java
+++ bigbluebutton-apps/src/main/java/org/bigbluebutton/red5/client/messaging/DisconnectAllMessage.java
@@ -2,4 +2,7 @@ package org.bigbluebutton.red5.client.messaging;
 
 public class DisconnectAllMessage implements ClientMessage {
 
+    public String getMessageName() {
+        return "DisconnectAllMessage";
+    }
 }
