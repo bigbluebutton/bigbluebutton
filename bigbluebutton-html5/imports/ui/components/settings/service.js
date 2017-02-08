5a24ef777c1924bc492ce5720aae70db45a46bb7
Anton Georgiev <anto.georgiev@gmail.com>
2017-02-08 14:18:41 -0500

    add default values for fields and check if user is null

--- bigbluebutton-html5/imports/ui/components/settings/service.js
+++ bigbluebutton-html5/imports/ui/components/settings/service.js
@@ -2,9 +2,13 @@ import Users from '/imports/api/users';
 import AuthSingleton from '/imports/ui/services/auth/index.js';
 
 checkUserRoles = () => {
-  const user = Users.findOne({
+  let user = Users.findOne({
      userId: AuthSingleton.getCredentials().requesterUserId,
-   }).user;
+   });
+
+  if (user != null && user.user != null) {
+    user = user.user;
+  }
 
   return {
     isPresenter: user.presenter,
