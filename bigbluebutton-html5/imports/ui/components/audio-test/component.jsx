b1f0282c52150337b2fe1cdef6bede67259f29d2
Anton Georgiev <anto.georgiev@gmail.com>
2017-02-16 14:47:12 -0500

    localize label for AudioTest component

--- bigbluebutton-html5/imports/ui/components/audio-test/component.jsx
+++ bigbluebutton-html5/imports/ui/components/audio-test/component.jsx
@@ -1,22 +1,36 @@
 import React from 'react';
 import Button from '/imports/ui/components/button/component';
 import styles from '../settings/styles.scss';
+import { defineMessages, injectIntl } from 'react-intl';
 
-export default class AudioTest extends React.Component {
+class AudioTest extends React.Component {
   constructor(props) {
     super(props);
   }
 
   render() {
+    const {
+      intl,
+    } = this.props;
+
     return (
-        <Button className={styles.testAudioBtn}
-          label={'Play sound'}
-          icon={'audio'}
-          size={'md'}
-          color={'primary'}
-          ghost={true}
-          onClick={this.props.handlePlayAudioSample}
-        />
+      <Button className={styles.testAudioBtn}
+        label={intl.formatMessage(intlMessages.playSoundLabel)}
+        icon={'audio'}
+        size={'md'}
+        color={'primary'}
+        ghost={true}
+        onClick={this.props.handlePlayAudioSample}
+      />
     );
   }
 };
+
+const intlMessages = defineMessages({
+  playSoundLabel: {
+    id: 'app.audio.playSoundLabel',
+    defaultMessage: 'Play Sound',
+  },
+});
+
+export default injectIntl(AudioTest);
