8511e72f06a39c948040c22c81d2241017262bef
Oswaldo Acauan <oswaldoacauan@gmail.com>
2017-02-22 12:10:27 +0000

    Refactor muteUser method

--- bigbluebutton-html5/imports/api/users/server/methods.js
+++ bigbluebutton-html5/imports/api/users/server/methods.js
@@ -2,9 +2,11 @@ import { Meteor } from 'meteor/meteor';
 import kickUser from './methods/kickUser';
 import listenOnlyToggle from './methods/listenOnlyToggle';
 import userLogout from './methods/userLogout';
+import muteUser from './methods/muteUser';
 
 Meteor.methods({
   kickUser,
   listenOnlyToggle,
   userLogout,
+  muteUser,
 });
--- bigbluebutton-html5/imports/api/users/server/methods/muteUser.js
+++ bigbluebutton-html5/imports/api/users/server/methods/muteUser.js
@@ -1,42 +1,34 @@
-import { publish } from '/imports/api/common/server/helpers';
+import { Meteor } from 'meteor/meteor';
+import { check } from 'meteor/check';
+import RedisPubSub from '/imports/startup/server/redis';
+import Logger from '/imports/startup/server/logger';
 import { isAllowedTo } from '/imports/startup/server/userPermissions';
-import { appendMessageHeader } from '/imports/api/common/server/helpers';
-import { updateVoiceUser } from '/imports/api/users/server/modifiers/updateVoiceUser';
-import { logger } from '/imports/startup/server/logger';
 
-Meteor.methods({
-  // meetingId: the meetingId of the meeting the user[s] is in
-  // toMuteUserId: the userId of the user to be muted
-  // requesterUserId: the userId of the requester
-  // requesterToken: the authToken of the requester
-  muteUser(credentials, toMuteUserId) {
-    const REDIS_CONFIG = Meteor.settings.redis;
-    const { meetingId, requesterUserId, requesterToken } = credentials;
-    const action = function () {
-      if (toMuteUserId === requesterUserId) {
-        return 'muteSelf';
-      } else {
-        return 'muteOther';
-      }
-    };
+export default function muteUser(credentials, userId) {
+  const REDIS_CONFIG = Meteor.settings.redis;
+  const CHANNEL = REDIS_CONFIG.channels.toBBBApps.users;
+  const EVENT_NAME = 'mute_user_request_message';
 
-    if (isAllowedTo(action(), credentials)) {
-      let message = {
-        payload: {
-          user_id: toMuteUserId,
-          meeting_id: meetingId,
-          mute: true,
-          requester_id: requesterUserId,
-        },
-      };
-      message = appendMessageHeader('mute_user_request_message', message);
-      logger.info(`publishing a user mute request for ${toMuteUserId}`);
-      publish(REDIS_CONFIG.channels.toBBBApps.users, message);
-      updateVoiceUser(meetingId, {
-        web_userid: toMuteUserId,
-        talking: false,
-        muted: true,
-      });
-    }
-  },
-});
+  const { meetingId, requesterUserId } = credentials;
+
+  check(meetingId, String);
+  check(requesterUserId, String);
+  check(userId, String);
+
+  const action = userId === requesterUserId ? 'muteSelf' : 'muteOther';
+
+  if (!isAllowedTo(action, credentials)) {
+    throw new Meteor.Error('not-allowed', `You are not allowed to ${action}`);
+  }
+
+  let payload = {
+    user_id: userId,
+    meeting_id: meetingId,
+    mute: true,
+    requester_id: requesterUserId,
+  };
+
+  Logger.verbose(`User '${userId}' was muted by '${requesterUserId}' from meeting '${meetingId}'`);
+
+  return RedisPubSub.publish(CHANNEL, EVENT_NAME, payload);
+};
