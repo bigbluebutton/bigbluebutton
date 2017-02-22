8abdd10393b3e55c3d05a26e323df20f4f3e80ab
Oswaldo Acauan <oswaldoacauan@gmail.com>
2017-02-22 18:59:47 +0000

    Refactor handler for emojiStatus

--- bigbluebutton-html5/imports/api/users/server/eventHandlers.js
+++ bigbluebutton-html5/imports/api/users/server/eventHandlers.js
@@ -1,8 +1,10 @@
 import RedisPubSub from '/imports/startup/server/redis';
 import handleRemoveUser from './handlers/removeUser';
 import handlePresenterAssigned from './handlers/presenterAssigned';
+import handleEmojiStatus from './handlers/emojiStatus';
 
 RedisPubSub.on('user_eject_from_meeting', handleRemoveUser);
 RedisPubSub.on('disconnect_user_message', handleRemoveUser);
 RedisPubSub.on('user_left_message', handleRemoveUser);
 RedisPubSub.on('presenter_assigned_message', handlePresenterAssigned);
+RedisPubSub.on('user_emoji_status_message', handleEmojiStatus);
