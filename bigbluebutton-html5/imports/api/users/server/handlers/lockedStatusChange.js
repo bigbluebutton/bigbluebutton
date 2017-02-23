6b020d7e3a079cff6e9788d04a370617e2b445ae
Oswaldo Acauan <oswaldoacauan@gmail.com>
2017-02-23 17:46:03 +0000

    WIP. Refactor of get_users_reply handler

---
+++ bigbluebutton-html5/imports/api/users/server/handlers/lockedStatusChange.js
@@ -0,0 +1,61 @@
+import Logger from '/imports/startup/server/logger';
+import { check } from 'meteor/check';
+import Users from '/imports/api/users';
+
+import muteToggle from '../methods/muteToggle';
+
+export default function handleLockedStatusChange({ payload }) {
+  const meetingId = payload.meeting_id;
+  const userId = payload.userid;
+  const isLocked = arg.payload.locked;
+
+  check(meetingId, String);
+  check(userId, String);
+  check(isLocked, String);
+
+  const selector = {
+    meetingId,
+    userId,
+  };
+
+  const User = Users.findOne({
+    meetingId,
+    userId,
+  });
+  if (!User) {
+    throw new Meteor.Error(
+      'user-not-found', `You need a valid user to be able to set presenter`);
+  }
+
+  const modifier = {
+    $set: {
+      'user.locked': isLocked,
+    },
+  };
+
+  const cb = (err, numChanged) => {
+    if (err) {
+      return Logger.error(`Assigning user locked status: ${err}`);
+    }
+
+    if (numChanged) {
+      if (User.user.role === 'VIEWER'
+          && !User.user.listenOnly
+          && !User.user.voiceUser.muted
+          && User.user.voiceUser.joined
+          && isLocked) {
+
+        const credentials = {
+          meetingId,
+          requesterUserId: userId,
+        };
+
+        muteToggle(credentials, userId, true);
+      }
+
+      return Logger.info(`Assigned locked status '${isLocked ? 'locked' : 'unlocked'}' id=${newPresenterId} meeting=${meetingId}`);
+    }
+  };
+
+  return Users.update(selector, modifier, cb);
+};
---
+++ bigbluebutton-html5/imports/api/users/server/modifiers/addUser.js
@@ -0,0 +1,96 @@
+import { check } from 'meteor/check';
+import Logger from '/imports/startup/server/logger';
+
+import Meetings from '/imports/api/meetings';
+import Users from '/imports/api/users';
+
+import addChat from '/imports/api/chat/server/modifiers/addChat';
+import requestStunTurn from '../methods/requestStunTurn';
+
+export default function addUser(meetingId, user) {
+  const userId = user.user_id;
+
+  check(user, Object);
+  check(meetingId, String);
+  check(userId, String);
+
+  const selector = {
+    meetingId,
+    userId,
+  };
+
+  const modifier = {
+    $set: {
+      meetingId,
+      userId,
+      'user.connection_status': 'online',
+      'user.userid': userId,
+      'user.extern_userid': user.extern_userid,
+      'user.role': user.role,
+      'user.name': user.name,
+      'user._sort_name': user.name.trim().toLowerCase(),
+      'user.avatarURL': user.avatarURL,
+      'user.set_emoji_time': user.set_emoji_time || new Date(),
+      'user.time_of_joining': new Date(),
+      'user.emoji_status': user.emoji_status,
+      'user.webcam_stream': user.webcam_stream,
+      'user.presenter': user.presenter,
+      'user.locked': user.locked,
+      'user.phone_user': user.phone_user,
+      'user.listenOnly': user.listenOnly,
+      'user.has_stream': user.has_stream,
+      'user.has_stream.voiceUser.web_userid': user.voiceUser.web_userid,
+      'user.has_stream.voiceUser.callernum': user.voiceUser.callernum,
+      'user.has_stream.voiceUser.userid': user.voiceUser.userid,
+      'user.has_stream.voiceUser.talking': user.voiceUser.talking,
+      'user.has_stream.voiceUser.joined': user.voiceUser.joined,
+      'user.has_stream.voiceUser.callername': user.voiceUser.callername,
+      'user.has_stream.voiceUser.locked': user.voiceUser.locked,
+      'user.has_stream.voiceUser.muted': user.voiceUser.muted,
+    },
+  };
+
+  const cb = (err, numChanged) => {
+    if (err) {
+      return Logger.error(`Adding user to collection: ${err}`);
+    }
+
+    // TODO: Do we really need to request the stun/turn everytime?
+    requestStunTurn(meetingId, userId);
+
+    const { insertedId } = numChanged;
+    if (insertedId) {
+      addWelcomeChatMessage(meetingId, userId);
+      return Logger.info(`Added user id=${userId} meeting=${meetingId}`);
+    }
+
+    if (numChanged) {
+      return Logger.info(`Upserted user id=${userId} meeting=${meetingId}`);
+    }
+  };
+
+  return Users.upsert(selector, modifier, cb);
+};
+
+const addWelcomeChatMessage = (meetingId, userId) => {
+  const APP_CONFIG = Meteor.settings.public.app;
+  const CHAT_CONFIG = Meteor.settings.public.chat;
+
+  const Meeting = Meetings.findOne({ meetingId });
+
+  let welcomeMessage = APP_CONFIG.defaultWelcomeMessage
+      .concat(APP_CONFIG.defaultWelcomeMessageFooter)
+      .replace(/%%CONFNAME%%/, Meeting.meetingName);
+
+  const message = {
+    chat_type: CHAT_CONFIG.type_system,
+    message: welcomeMessage,
+    from_color: '0x3399FF',
+    to_userid: userId,
+    from_userid: CHAT_CONFIG.type_system,
+    from_username: '',
+    from_time: new Date(),
+  };
+
+  return addChat(meetingId, message);
+};
