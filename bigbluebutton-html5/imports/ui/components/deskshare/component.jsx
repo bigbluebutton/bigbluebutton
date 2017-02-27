05d32bdbd31f2311ddad25379e43cd32862dacbd
perroned <danny.perrone77@gmail.com>
2017-02-27 18:17:57 +0000

    Removes html5 video player controls

--- bigbluebutton-html5/imports/ui/components/deskshare/component.jsx
+++ bigbluebutton-html5/imports/ui/components/deskshare/component.jsx
@@ -7,7 +7,7 @@ export default class DeskshareComponent extends React.Component {
 
   render() {
     return (
-      <video id="deskshareVideo" style={{ height: '100%', width: '100%', }} controls/>
+      <video id="deskshareVideo" style={{ height: '100%', width: '100%', }} />
     );
   }
 };
