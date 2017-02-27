f93b16434dd094a77b2ec8fbae1e2c557897db73
perroned <danny.perrone77@gmail.com>
2017-02-27 18:16:16 +0000

    Handle issue with undefined variable

--- bigbluebutton-html5/imports/api/deskshare/server/modifiers/modifyDeskshareStatus.js
+++ bigbluebutton-html5/imports/api/deskshare/server/modifiers/modifyDeskshareStatus.js
@@ -2,9 +2,18 @@ import Deskshare from '/imports/api/deskshare';
 import Meetings from '/imports/api/meetings';
 import Logger from '/imports/startup/server/logger';
 import Users from '/imports/api/users';
+import { Meteor } from 'meteor/meteor';
+import { Match } from 'meteor/check';
 
 export default function modifyDeskshareStatus(meetingId, deskshareInfo) {
-  const presenter = Users.findOne({ meetingId: meetingId, 'user.presenter':  true }).user.userid;
+  const presenter = Users.findOne({ meetingId: meetingId, 'user.presenter':  true });
+  let startedById = null;
+
+  if (Match.test(presenter, Object) && Match.test(presenter.user.userid, String)) {
+    startedById = presenter.user.userid;
+  } else {
+    throw new Meteor.Error('not-allowed', `Deskshare presenter does not exist in this meeting`);
+  }
 
   Deskshare.upsert({ meetingId: meetingId }, { $set: {
     broadcasting: deskshareInfo.broadcasting,
@@ -12,6 +21,7 @@ export default function modifyDeskshareStatus(meetingId, deskshareInfo) {
     vw: deskshareInfo.vw,
     vh: deskshareInfo.vh,
     voiceBridge: deskshareInfo.voiceBridge,
-    startedBy: presenter,
+    startedBy: startedById,
   }, });
 }
+
