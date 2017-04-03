84fd8d4101cfd56d85d9ef6fec25a71b2a14055a
Pedro Beschorner Marin <pedrobmarin@gmail.com>
2017-04-03 17:06:16 +0000

    Branding back to BigBlueButton and adding back some of the client images we removed. This need to be reviewed to avoid having all this unused images!

--- record-and-playback/deploy.sh
+++ record-and-playback/deploy.sh
@@ -38,16 +38,7 @@ function deploy_format() {
 	done
 }
 
-RECORDING_SERVER=true
-if $RECORDING_SERVER ; then
-	deploy_format "presentation"
-	deploy_format "presentation_export"
-	deploy_format "mconf_decrypter"
-	sudo mv /usr/local/bigbluebutton/core/scripts/mconf-recording-decrypter.initd /etc/init.d/mconf-recording-decrypter
-	sudo mv /usr/local/bigbluebutton/core/scripts/mconf-recording-decrypter.monit /etc/monit/conf.d/mconf-recording-decrypter
-else
-	deploy_format "mconf_encrypted"
-fi
+deploy_format "presentation"
 
 sudo mkdir -p /var/bigbluebutton/playback/
 sudo mkdir -p /var/bigbluebutton/recording/raw/
