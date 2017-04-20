0cc9ce4048356b5c744c2db66df8b649e82540e7
Anton Georgiev <anto.georgiev@gmail.com>
2017-04-20 15:57:35 -0400

    moved componentWillMount to component..

--- bigbluebutton-html5/imports/ui/components/audio/component.jsx
+++ bigbluebutton-html5/imports/ui/components/audio/component.jsx
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
 import { init } from './service';
+import { showModal } from '../app/service';
+import AudioModalContainer from './audio-modal/container';
 
 export default class Audio extends Component {
   constructor(props) {
     super(props);
+  }
+
+  componentWillMount() {
+    if (this.props.showJoinAudio) {
+      showModal(<AudioModalContainer />);
+    }
   }
 
   componentDidMount() {
--- bigbluebutton-html5/imports/ui/components/audio/container.jsx
+++ bigbluebutton-html5/imports/ui/components/audio/container.jsx
@@ -2,22 +2,13 @@ import React, { Component } from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
 import Audio from './component';
 
-import { showModal } from '../app/service';
-import AudioModalContainer from './audio-modal/container';
-
 class AudioContainer extends Component {
-  componentWillMount() {
-    if (this.props.showJoinAudio) {
-      showModal(<AudioModalContainer />);
-    }
-  }
-
   render() {
     return (
       <Audio
         {...this.props}>
         {this.props.children}
-      </ Audio>
+      </Audio>
     );
   }
 }
