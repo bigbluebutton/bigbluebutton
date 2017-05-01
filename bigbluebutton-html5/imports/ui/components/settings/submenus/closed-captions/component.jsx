80d4feedcc49046cd403e905c9b37f61207cec1d
KDSBrowne <kert.browne85@gmail.com>
2017-05-01 12:46:21 -0700

    fix tabIndex for the settings closed caption submenu

--- bigbluebutton-html5/imports/ui/components/settings/submenus/closed-captions/component.jsx
+++ bigbluebutton-html5/imports/ui/components/settings/submenus/closed-captions/component.jsx
@@ -280,7 +280,7 @@ class ClosedCaptionsMenu extends BaseMenu {
                 className={cx(styles.formElement, styles.pullContentRight)}
                 aria-label={intl.formatMessage(intlMessages.backgroundColorLabel)}>
                 <div
-                  tabIndex='12'
+                  tabIndex='0'
                   className={ styles.swatch }
                   onClick={ this.handleColorPickerClick.bind(this, 'displayBackgroundColorPicker') }>
                   <div
@@ -320,7 +320,7 @@ class ClosedCaptionsMenu extends BaseMenu {
                 className={cx(styles.formElement, styles.pullContentRight)}
                 aria-label={intl.formatMessage(intlMessages.fontColorLabel)}>
                 <div
-                  tabIndex='12'
+                  tabIndex='0'
                   className={ styles.swatch }
                   onClick={ this.handleColorPickerClick.bind(this, 'displayFontColorPicker') }>
                   <div
