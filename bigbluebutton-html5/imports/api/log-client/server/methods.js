bede8ab44782adc1bfd8b99a26cd4a4eb2bc7ea8
Klaus Klein <klauswk1@hotmail.com>
2017-05-03 09:27:33 -0300

    Fix missing line and tab problem

--- bigbluebutton-html5/imports/api/log-client/server/methods.js
+++ bigbluebutton-html5/imports/api/log-client/server/methods.js
@@ -2,5 +2,5 @@ import { Meteor } from 'meteor/meteor';
 import logClient from './methods/logClient';
 
 Meteor.methods({
-    logClient,
+	logClient,
 });
--- bigbluebutton-html5/imports/api/log-client/server/methods/logClient.js
+++ bigbluebutton-html5/imports/api/log-client/server/methods/logClient.js
@@ -3,7 +3,7 @@ import { check } from 'meteor/check';
 import Logger from '/imports/startup/server/logger';
 
 export default function logClient() {
-    const args = Array.prototype.slice.call(arguments, 1);
+	const args = Array.prototype.slice.call(arguments, 1);
 
-    Logger.log(arguments[0], `Client Log`, args);
+	Logger.log(arguments[0], `Client Log`, args);
 };
