2d8c5f26f1f86cc12a7e3ae86afbf37e3fba7f51
Matthew Marangoni <matthew.marangoni@gmail.com>
2017-05-05 14:39:16 -0700

    inital call manager code

--- bigbluebutton-html5/imports/api/audio/client/manager/index.js
+++ bigbluebutton-html5/imports/api/audio/client/manager/index.js
@@ -29,4 +29,74 @@ export default class AudioManager {
     }
   }
 
+  transferToConference() {
+    // TODO: transfer from initialized state
+    // TODO: transfer from echo test to conference
+    // this.bridge.transferToConference();
+  }
+
+  getMicId() {
+    // Placeholder, will get the microphone ID for switching input device
+    // this.bridge.getMicId();
+  }
+
+  setMicId() {
+    // Placeholder, will set the microphone ID for switching input device
+    // this.bridge.setMicId();
+  }
+
+  getSpeakerId() {
+    // Placeholder, will get the speaker ID for switching output device
+    // this.bridge.getSpeakerId();
+  }
+
+  setSpeakerId() {
+    // Placeholder, will set the speaker ID for switching output device
+    // this.bridge.setSpeakerId();
+  }
+
+  getActiveMic() {
+    // Placeholder, will detect active input hardware
+    // this.bridge.getActiveMic();
+  }
+
 }
+
+AudioManager.CallStates = class {
+  static get init() {
+    return "initialized state";
+  }
+  static get echo() {
+    return "do echo test state";
+  }
+  static get callIntoEcho() {
+    return "calling into echo test state";
+  }
+  static get inEchoTest() {
+    return "in echo test state";
+  }
+  static get joinVoiceConference() {
+    return "join voice conference state";
+  }
+  static get callIntoConference() {
+    return "calling into conference state";
+  }
+  static get inConference() {
+    return "in conference state";
+  }
+  static get transferToConference() {
+    return "joining from echo into conference state";
+  }
+  static get echoTestFailed() {
+    return "echo test failed state";
+  }
+  static get callToListenOnly() {
+    return "call to listen only state";
+  }
+  static get connectToListenOnly() {
+    return "connecting to listen only state";
+  }
+  static get inListenOnly() {
+    return "in listen only state";
+  }
+};
