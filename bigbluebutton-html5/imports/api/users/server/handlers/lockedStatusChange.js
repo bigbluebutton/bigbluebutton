ca808195f2eb067b1f197577f56083750fbe02b1
JaeeunCho <anna.jecho@gmail.com>
2017-05-10 14:02:50 -0700

    HTML5 - added locked status for users

--- bigbluebutton-html5/imports/api/users/server/handlers/lockedStatusChange.js
+++ bigbluebutton-html5/imports/api/users/server/handlers/lockedStatusChange.js
@@ -50,7 +50,7 @@ export default function handleLockedStatusChange({ payload }) {
         muteToggle(credentials, userId, true);
       }
 
-      return Logger.info(`Assigned locked status '${isLocked ? 'locked' : 'unlocked'}' id=${newPresenterId} meeting=${meetingId}`);
+      return Logger.info(`Assigned locked status '${isLocked ? 'locked' : 'unlocked'}' id=${userId} meeting=${meetingId}`);
     }
   };
 
