7e257f153600c3f3f52a8c59eca8d272ad9144bd
Richard Alam <ritzalam@gmail.com>
2017-05-15 14:32:49 -0700

    - load conf file

--- bbb-apps-common/src/main/scala/org/bigbluebutton/client/ClientGWApplication.scala
+++ bbb-apps-common/src/main/scala/org/bigbluebutton/client/ClientGWApplication.scala
@@ -6,10 +6,12 @@ import org.bigbluebutton.red5.client.messaging.IConnectionInvokerService
 import scala.concurrent.duration._
 
 class ClientGWApplication(val connectionInvokerGW: IConnectionInvokerService,
-                         val oldMessageReceivedGW: OldMessageReceivedGW) {
+                         val oldMessageReceivedGW: OldMessageReceivedGW) extends SystemConfiguration{
 
   implicit val system = ActorSystem("bbb-apps-common")
   implicit val timeout = akka.util.Timeout(3 seconds)
+
+  println("*********** meetingManagerChannel = " + meetingManagerChannel)
 
   def shutdown(): Unit = {
     system.terminate()
--- bbb-apps-common/src/main/scala/org/bigbluebutton/client/SystemConfiguration.scala
+++ bbb-apps-common/src/main/scala/org/bigbluebutton/client/SystemConfiguration.scala
@@ -4,12 +4,13 @@ import scala.util.Try
 import com.typesafe.config.ConfigFactory
 
 trait SystemConfiguration {
-  val config = ConfigFactory.load()
+  val config = ConfigFactory.load("bbb-app")
 
   lazy val redisHost = Try(config.getString("redis.host")).getOrElse("127.0.0.1")
   lazy val redisPort = Try(config.getInt("redis.port")).getOrElse(6379)
   lazy val redisPassword = Try(config.getString("redis.password")).getOrElse("")
 
   lazy val redisSubChannel = Try(config.getString("redis.subChannel")).getOrElse("FROM-AKKA-APPS")
+  lazy val meetingManagerChannel = Try(config.getString("eventBus.meetingManagerChannel")).getOrElse("FOOOOOOOOO")
 
 }
