1573d641a649a713e5e65f51ed599fc816d16652
Oswaldo Acauan <oswaldoacauan@gmail.com>
2017-06-02 19:16:30 +0000

    Change lint import configs to our pattern

--- bigbluebutton-html5/.eslintrc.js
+++ bigbluebutton-html5/.eslintrc.js
@@ -3,8 +3,11 @@ module.exports = {
     "plugins": [
         "react",
         "jsx-a11y",
-        "import"
+        "import",
     ],
     "rules": {
+      "import/extensions":  [2, "never"],
+      "import/no-absolute-path": 0,
+      "import/no-unresolved": 0,
     },
 };
