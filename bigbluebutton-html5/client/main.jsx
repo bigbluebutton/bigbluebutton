aecc55b87cdc9fcd1e354af8d7a7f148f15dfad3
gcampes <gabrieldecampes@gmail.com>
2017-06-26 12:18:17 +0000

    removes unused files

--- bigbluebutton-html5/client/main.jsx
+++ bigbluebutton-html5/client/main.jsx
@@ -5,21 +5,11 @@ import { renderRoutes } from '/imports/startup/client/routes';
 import 'whatwg-fetch'
 
 Meteor.startup(() => {
-  let now = new Date().valueOf();
-
   if((window.navigator.platform === 'iPhone' || window.navigator.platform === 'iPad') && window.navigator.userAgent !== 'BigBlueButton') {
     if(confirm('Do you want to join using the BBB iOS WebRTC Application?')) {
       window.location = 'bbb://' + document.location.href;
       return;
     }
   }
-  //setTimeout(function () {
-  //   if (new Date().valueOf() - now > 100) return;
-  //   window.fetch = fetch;
   render(renderRoutes(), document.getElementById('app'));
-  //}, 25);
-  //let iframe = document.getElementById('sorry-frame');
-  //iframe.setAttribute('src', 'bbb-webrtc-ios://' + document.location.href);
-  //window.location = 'bbb-webrtc-ios://' + document.location.href;
-  console.log('não me arrependo de nada', fetch);
 });
