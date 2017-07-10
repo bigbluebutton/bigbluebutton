f6d92e5a507fc6a3673ac61d90fd6a701ab60d1c
Chad Pilkey <capilkey@gmail.com>
2017-07-10 18:12:06 -0400

    add deskshare msg handlers to akka-apps

--- akka-bbb-apps/src/main/scala/org/bigbluebutton/core/apps/deskshare/DeskshareApp2x.scala
+++ akka-bbb-apps/src/main/scala/org/bigbluebutton/core/apps/deskshare/DeskshareApp2x.scala
@@ -6,7 +6,11 @@ import org.bigbluebutton.core.OutMessageGateway
 import org.bigbluebutton.core.running.LiveMeeting
 
 class DeskshareApp2x(val liveMeeting: LiveMeeting,
-    val outGW: OutMessageGateway)(implicit val context: ActorContext) {
+  val outGW: OutMessageGateway)(implicit val context: ActorContext)
+    extends DeskshareStartedVoiceConfEvtMsgHdlr
+    with DeskshareStoppedVoiceConfEvtMsgHdlr
+    with DeskshareRtmpBroadcastStartedVoiceConfEvtMsgHdlr
+    with DeskshareRtmpBroadcastStoppedVoiceConfEvtMsgHdlr {
 
   val log = Logging(context.system, getClass)
 
