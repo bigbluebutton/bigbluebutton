a7ca723fe62efb3e1d2c83202e7195c6bc733f64
Klaus <klauswk1@hotmail.com>
2017-07-18 11:59:05 -0300

    Audio only working on html5

--- bigbluebutton-html5/imports/ui/components/audio/service.js
+++ bigbluebutton-html5/imports/ui/components/audio/service.js
@@ -2,19 +2,20 @@ import Users from '/imports/api/2.0/users';
 import Auth from '/imports/ui/services/auth';
 
 import AudioManager from '/imports/api/1.1/audio/client/manager';
+import Meetings from '/imports/api/2.0/meetings';
 
 let audioManager;
+
 const init = () => {
   const userId = Auth.userID;
   const User = Users.findOne({ userId });
   const username = User.user.name;
-
   const turns = [];
-  const stuns = [];
+  const stuns = ['stun:stun.freeswitch.org'];
+  const Meeting = Meetings.findOne({ meetingId: User.meetingId });
+  const voiceBridge = Meeting.voiceProp.voiceConf;
   // FIX ME
-  const voiceBridge = 'Meeting.voiceConf';
-  // FIX ME
-  const microphoneLockEnforced = 'Meeting.roomLockSettings.disableMic';
+  const microphoneLockEnforced = false;
 
   const userData = {
     userId,
