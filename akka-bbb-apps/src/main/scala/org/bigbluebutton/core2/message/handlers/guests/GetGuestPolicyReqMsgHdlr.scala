905f1a8e7128fd8514f6ca1a30dd61cdd900e45d
Richard Alam <ritzalam@gmail.com>
2017-07-20 17:33:49 -0700

    - clean up

--- akka-bbb-apps/src/main/scala/org/bigbluebutton/core2/message/handlers/guests/GetGuestPolicyReqMsgHdlr.scala
+++ akka-bbb-apps/src/main/scala/org/bigbluebutton/core2/message/handlers/guests/GetGuestPolicyReqMsgHdlr.scala
@@ -13,20 +13,9 @@ trait GetGuestPolicyReqMsgHdlr {
   val outGW: OutMessageGateway
 
   def handleGetGuestPolicyReqMsg(msg: GetGuestPolicyReqMsg): Unit = {
-    val guests = GuestsWaiting.findAll(liveMeeting.guestsWaiting)
-    notifyModeratorsOfGuestWaiting(guests, liveMeeting.users2x, liveMeeting.props.meetingProp.intId)
-
     val event = buildGetGuestPolicyRespMsg(liveMeeting.props.meetingProp.intId, msg.body.requestedBy,
       liveMeeting.guestsWaiting.getGuestPolicy().policy)
     outGW.send(event)
-  }
-
-  def notifyModeratorsOfGuestWaiting(guests: Vector[GuestWaiting], users: Users2x, meetingId: String): Unit = {
-    val mods = Users2x.findAll(users).filter(p => p.role == Roles.MODERATOR_ROLE)
-    mods foreach { m =>
-      val event = MsgBuilder.buildGuestsWaitingForApprovalEvtMsg(meetingId, m.intId, guests)
-      Sender.send(outGW, event)
-    }
   }
 
   def buildGetGuestPolicyRespMsg(meetingId: String, userId: String, policy: String): BbbCommonEnvCoreMsg = {
