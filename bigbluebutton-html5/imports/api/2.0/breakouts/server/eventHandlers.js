17dce149d916f9886d32d50c3c6c847b5d56d911
Klaus <klauswk1@hotmail.com>
2017-08-11 15:02:26 -0300

    fix dropdown going missing

--- bigbluebutton-html5/imports/api/2.0/breakouts/server/eventHandlers.js
+++ bigbluebutton-html5/imports/api/2.0/breakouts/server/eventHandlers.js
@@ -3,10 +3,8 @@ import handleBreakoutJoinURL from './handlers/breakoutJoinURL';
 import handleBreakoutStarted from './handlers/breakoutStarted';
 import handleUpdateTimeRemaining from './handlers/updateTimeRemaining';
 import handleBreakoutClosed from './handlers/breakoutClosed';
-import handleUpdateUsersEvtMsg from './handlers/updateUsersEvtMsg';
 
 RedisPubSub.on('BreakoutRoomStartedEvtMsg', handleBreakoutStarted);
 RedisPubSub.on('BreakoutRoomJoinURLEvtMsg', handleBreakoutJoinURL);
 RedisPubSub.on('BreakoutRoomsTimeRemainingUpdateEvtMsg', handleUpdateTimeRemaining);
 RedisPubSub.on('EndBreakoutRoomEvtMsg', handleBreakoutClosed);
-RedisPubSub.on('UpdateBreakoutUsersEvtMsg', handleUpdateUsersEvtMsg);
