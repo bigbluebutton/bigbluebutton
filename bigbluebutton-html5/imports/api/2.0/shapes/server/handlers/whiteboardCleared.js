76d778d9e5d99ad8832e188ef277f6461aed5dbe
Oleksandr Zhurbenko <xaerok.od.ua@gmail.com>
2017-08-15 15:30:11 -0700

    Fixed 'Clear All' annotations for the multi-user mode

--- bigbluebutton-html5/imports/api/2.0/shapes/server/handlers/whiteboardCleared.js
+++ bigbluebutton-html5/imports/api/2.0/shapes/server/handlers/whiteboardCleared.js
@@ -3,9 +3,15 @@ import { check } from 'meteor/check';
 import clearShapesWhiteboard from '../modifiers/clearShapesWhiteboard';
 
 export default function handleWhiteboardCleared({ body }, meetingId) {
+  check(body, {
+    userId: String,
+    whiteboardId: String,
+    fullClear: Boolean,
+  });
+
   const whiteboardId = body.whiteboardId;
+  const fullClear = body.fullClear;
+  const userId = body.userId;
 
-  check(whiteboardId, String);
-
-  return clearShapesWhiteboard(meetingId, whiteboardId);
+  return clearShapesWhiteboard(meetingId, whiteboardId, userId, fullClear);
 }
