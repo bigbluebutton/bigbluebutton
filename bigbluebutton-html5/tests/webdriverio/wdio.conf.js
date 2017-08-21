7819f425d86eb908dc8192d303bef34c74f36779
Maxim Khlobystov <maxim.khlobystov@gmail.com>
2017-08-21 11:42:10 -0400

    Added Google Dev and Firefox Nightly to the browsers list.

--- bigbluebutton-html5/tests/webdriverio/wdio.conf.js
+++ bigbluebutton-html5/tests/webdriverio/wdio.conf.js
@@ -3,12 +3,26 @@ exports.config = {
   capabilities: {
     chromeBrowser: {
       desiredCapabilities: {
+        browserName: 'chrome'
+      }
+    },
+    chromeDevBrowser: {
+      desiredCapabilities: {
         browserName: 'chrome',
+        chromeOptions: {
+          binary: '/opt/google/chrome-unstable/google-chrome-unstable'
+        }
       }
     },
     firefoxBrowser: {
       desiredCapabilities: {
         browserName: 'firefox'
+      }
+    },
+    firefoxNightlyBrowser: {
+      desiredCapabilities: {
+        browserName: 'firefox',
+        firefox_binary: '/usr/lib/firefox-trunk/firefox-trunk'
       }
     }
   },
