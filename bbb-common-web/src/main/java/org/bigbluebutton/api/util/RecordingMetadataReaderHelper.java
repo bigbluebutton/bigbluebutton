e8127fdb61c352cfa7d0d52d1a57421863b69069
Richard Alam <ritzalam@gmail.com>
2017-08-31 08:06:15 -0700

    - do not include recordings with bad metadata.xml in the response for recording api calls

--- bbb-common-web/src/main/java/org/bigbluebutton/api/util/RecordingMetadataReaderHelper.java
+++ bbb-common-web/src/main/java/org/bigbluebutton/api/util/RecordingMetadataReaderHelper.java
@@ -49,11 +49,6 @@ public class RecordingMetadataReaderHelper {
       log.error("IOException on " + metadataXml.getAbsolutePath(), e);
     }
 
-    if (recMeta == null) {
-      recMeta = new RecordingMetadata();
-      recMeta.setMetadataXml(metadataXml.getParentFile().getName());
-      recMeta.setProcessingError(true);
-    }
     return recMeta;
   }
 
