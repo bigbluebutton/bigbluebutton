c14e779bb453708de241b078d760f347ab7cfa71
KDSBrowne <kert.browne85@gmail.com>
2017-09-05 08:53:02 -0700

    add check to payload

--- bigbluebutton-html5/imports/api/2.0/meetings/server/modifiers/changeLockSettings.js
+++ bigbluebutton-html5/imports/api/2.0/meetings/server/modifiers/changeLockSettings.js
@@ -1,7 +1,10 @@
 import Logger from '/imports/startup/server/logger';
 import Meetings from '/imports/api/2.0/meetings';
+import { check } from 'meteor/check';
 
 export default function changeLockSettings(meetingId, payload) {
+  check(meetingId, String);
+  check(payload, Object)
 
   const selector = {
     meetingId
