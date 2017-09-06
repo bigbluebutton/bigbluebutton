a7056469fb5ea6af1318bf56abc29cde051f2347
Richard Alam <ritzalam@gmail.com>
2017-09-05 18:11:43 -0700

    - implement models

--- akka-bbb-apps/src/main/scala/org/bigbluebutton/core/models/GroupChats.scala
+++ akka-bbb-apps/src/main/scala/org/bigbluebutton/core/models/GroupChats.scala
@@ -1,10 +1,24 @@
 package org.bigbluebutton.core.models
 
-case class GroupChats(chats: Vector[GroupChat]) {
+class GroupChats {
+  private var chats: collection.immutable.HashMap[String, GroupChat] = new collection.immutable.HashMap[String, GroupChat]
 
+  private def toVector: Vector[GroupChat] = chats.values.toVector
+
+  private def save(chat: GroupChat): GroupChat = {
+    chats += chat.id -> chat
+    chat
+  }
+
+  private def remove(id: String): Option[GroupChat] = {
+    for {
+      chat <- chats.get(id)
+    } yield {
+      chat
+    }
+  }
 }
 
-case class GroupChat(id: String, name: String, open: Boolean, users: Vector[String], messages: Vector[GroupChatMsg])
-case class GroupChatMsg(id: String, sender: String)
-case class GroupChatMessage(msgId: String, timestamp: Long, from: UserIdAndName,
+case class GroupChat(id: String, name: String, open: Boolean, users: Vector[String], messages: Vector[GroupChatMessage])
+case class GroupChatMessage(msgId: String, timestamp: Long, sender: String,
                             font: String, size: Int, color: String, message: String)
