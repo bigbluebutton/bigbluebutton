9a145c64a856baf871a363ef2f5ff8bb7e462638
KDSBrowne <kert.browne85@gmail.com>
2017-09-25 11:32:09 -0700

    fix set properties and client kicked condition

--- bigbluebutton-html5/imports/api/2.0/users/server/modifiers/userKicked.js
+++ bigbluebutton-html5/imports/api/2.0/users/server/modifiers/userKicked.js
@@ -13,8 +13,12 @@ export default function kickUser(meetingId, userId) {
 
   const modifier = {
     $set: {
-      kicked: true,
       connectionStatus: 'offline',
+      listenOnly: false,
+      validated: false,
+      emoji: 'none',
+      presenter: false,
+      role: 'VIEWER',
     },
   };
 
