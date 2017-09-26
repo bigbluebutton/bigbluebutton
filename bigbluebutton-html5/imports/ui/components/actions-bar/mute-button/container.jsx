32187d446a6873fdeb2a9f0c9bf5f92defc22d4f
KDSBrowne <kert.browne85@gmail.com>
2017-09-25 18:18:08 -0700

    rename service import and remove constants

--- bigbluebutton-html5/imports/ui/components/actions-bar/mute-button/container.jsx
+++ bigbluebutton-html5/imports/ui/components/actions-bar/mute-button/container.jsx
@@ -1,17 +1,11 @@
 import React from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
 import MuteAudio from './component';
-import Service from '../service';
+import MuteAudioService from '../service';
 
 const MuteAudioContainer = props => (<MuteAudio {...props} />);
 
-export default createContainer(() => {
-  const toggleSelfVoice = Service.toggleSelfVoice;
-  const voiceUserData = Service.getVoiceUserData();
-
-  const data = {
-    toggleSelfVoice,
-    voiceUserData,
-  };
-  return data;
-}, MuteAudioContainer);
+export default createContainer(() => ({
+  toggleSelfVoice: MuteAudioService.toggleSelfVoice,
+  voiceUserData: MuteAudioService.getVoiceUserData(),
+}), MuteAudioContainer);
