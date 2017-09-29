27b89361c074c73f1d213620cd56b45ea688f4e4
Oswaldo Acauan <oswaldoacauan@gmail.com>
2017-09-29 09:40:40 -0300

    Add missing check

--- bigbluebutton-html5/imports/api/2.0/presentations/server/handlers/presentationCurrentChange.js
+++ bigbluebutton-html5/imports/api/2.0/presentations/server/handlers/presentationCurrentChange.js
@@ -2,6 +2,8 @@ import { check } from 'meteor/check';
 import changeCurrentPresentation from '../modifiers/changeCurrentPresentation';
 
 export default function presentationCurrentChange({ body }, meetingId) {
+  check(body, Object);
+
   const { presentationId } = body;
 
   check(meetingId, String);
