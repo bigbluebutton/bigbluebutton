32632151d534c3eb4ec855f07320c187e3606476
Oswaldo Acauan <oswaldoacauan@gmail.com>
2017-10-03 16:22:26 -0300

    Fix bug when user wasnt in voiceConf

--- bigbluebutton-html5/imports/ui/services/user/mapUser.js
+++ bigbluebutton-html5/imports/ui/services/user/mapUser.js
@@ -4,10 +4,11 @@ import VoiceUsers from '/imports/api/2.0/voice-users';
 const USER_CONFIG = Meteor.settings.public.user;
 const ROLE_MODERATOR = USER_CONFIG.role_moderator;
 
+const getVoiceUser = userId => VoiceUsers.findOne({ intId: userId });
+
 const mapUser = (user) => {
   const userId = Auth.userID;
-  const voiceUser = VoiceUsers.findOne({ intId: user.userId });
-  const { muted, talking, listenOnly } = voiceUser;
+  const voiceUser = getVoiceUser(user.userId);
 
   const mappedUser = {
     id: user.userId,
@@ -21,17 +22,18 @@ const mapUser = (user) => {
     isPresenter: user.presenter,
     isModerator: user.role === ROLE_MODERATOR,
     isCurrent: user.userId === userId,
-    isVoiceUser: listenOnly || talking,
-    isMuted: muted,
-    isTalking: talking,
-    isListenOnly: listenOnly,
+    isVoiceUser: !!voiceUser,
+    isMuted: voiceUser ? voiceUser.muted : false,
+    isTalking: voiceUser ? voiceUser.talking : false,
+    isListenOnly: voiceUser ? voiceUser.listenOnly : false,
     isSharingWebcam: 0,
     isPhoneUser: user.phone_user,
     isOnline: user.connectionStatus === 'online',
-    isLocked: user.locked && !(user.isPresenter || user.isModerator),
+    isLocked: user.locked,
   };
 
   return mappedUser;
 };
+
 
 export default mapUser;
