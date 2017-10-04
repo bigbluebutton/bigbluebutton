e241d6bf68bd0c324047dc0bcff0677261eb0480
KDSBrowne <kert.browne85@gmail.com>
2017-10-04 07:39:13 -0700

    remove unused voice-user method

--- bigbluebutton-html5/imports/api/2.0/voice-users/server/methods.js
+++ bigbluebutton-html5/imports/api/2.0/voice-users/server/methods.js
@@ -2,12 +2,10 @@ import { Meteor } from 'meteor/meteor';
 import mapToAcl from '/imports/startup/mapToAcl';
 import listenOnlyToggle from './methods/listenOnlyToggle';
 import muteToggle from './methods/muteToggle';
-import kickVoiceUser from './methods/kickVoiceUser';
 
 Meteor.methods(mapToAcl(['methods.listenOnlyToggle', 'methods.toggleSelfVoice', 'methods.toggleVoice',
-  'methods.kickVoiceUser'], {
-    listenOnlyToggle,
-    toggleSelfVoice: (credentials) => { muteToggle(credentials, credentials.requesterUserId); },
-    toggleVoice: muteToggle,
-    kickVoiceUser,
-  }));
+], {
+  listenOnlyToggle,
+  toggleSelfVoice: (credentials) => { muteToggle(credentials, credentials.requesterUserId); },
+  toggleVoice: muteToggle,
+}));
