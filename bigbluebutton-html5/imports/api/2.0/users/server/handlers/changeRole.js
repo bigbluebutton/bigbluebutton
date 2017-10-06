584f7e4bc8c9262979d5912abd08010bcc88e8ad
KDSBrowne <kert.browne85@gmail.com>
2017-10-06 11:12:33 -0700

    add checks to handler

--- bigbluebutton-html5/imports/api/2.0/users/server/handlers/changeRole.js
+++ bigbluebutton-html5/imports/api/2.0/users/server/handlers/changeRole.js
@@ -1,5 +1,9 @@
+import { check } from 'meteor/check';
 import changeRole from '../modifiers/changeRole';
 
 export default function handleChangeRole({ body }, meetingId) {
+  check(body, Object);
+  check(meetingId, String);
+
   changeRole({ body }, meetingId);
 }
