59ce592ce9fe28289e401938081500dd12cc2852
Tainan Felipe <tainanf@imdt.com.br>
2017-10-09 15:15:40 -0300

    fix clearUsers error linter

--- bigbluebutton-html5/imports/api/2.0/users/server/modifiers/clearUsers.js
+++ bigbluebutton-html5/imports/api/2.0/users/server/modifiers/clearUsers.js
@@ -1,10 +1,12 @@
-import Users from './../../';
 import Logger from '/imports/startup/server/logger';
+import Users from './../../';
 
-export default function clearUsers(meetingId) {
+const clearUsers = (meetingId) => {
   if (meetingId) {
     return Users.remove({ meetingId }, Logger.info(`Cleared Users (${meetingId})`));
   }
 
   return Users.remove({}, Logger.info('Cleared Users (all)'));
-}
+};
+
+export default clearUsers;
