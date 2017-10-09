0741563a940e3350f773285393f0897e06c2bcc7
Tainan Felipe <tainanf@imdt.com.br>
2017-10-09 16:44:58 -0300

    fix separator error linter

--- bigbluebutton-html5/imports/ui/components/dropdown/list/separator/component.jsx
+++ bigbluebutton-html5/imports/ui/components/dropdown/list/separator/component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import cx from 'classnames';
 import styles from '../styles';
 
-const DropdownListSeparator = ({ style, className }) => (
+const DropdownListSeparator = (props, { style, className }) => (
   <li style={style} className={cx(styles.separator, className)} />);
 
 export default DropdownListSeparator;
