3b8c410a3c53d09e9036335dafcb09762d92aee9
Anton Georgiev <anto.georgiev@gmail.com>
2017-10-11 10:09:47 -0300

    rearrange normalized emojis to match order of emoji definition

--- bigbluebutton-html5/imports/utils/statuses.js
+++ bigbluebutton-html5/imports/utils/statuses.js
@@ -12,15 +12,15 @@ const EMOJI_STATUSES = {
 };
 
 const EMOJI_NORMALIZE = {
+  away: 'time',
+  raiseHand: 'hand',
+  neutral: 'undecided',
+  confused: 'confused',
   sad: 'sad',
   happy: 'happy',
   applause: 'applause',
-  confused: 'confused',
   thumbsUp: 'thumbs_up',
   thumbsDown: 'thumbs_down',
-  raiseHand: 'hand',
-  away: 'time',
-  neutral: 'undecided',
 };
 
 export { EMOJI_STATUSES, EMOJI_NORMALIZE };
