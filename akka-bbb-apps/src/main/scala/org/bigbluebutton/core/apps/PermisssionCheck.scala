4a48a69c7f420fa6d58b44434f9209bc1cb7d454
Richard Alam <ritzalam@gmail.com>
2017-10-17 11:41:05 -0700

    - start implementing rights management

--- akka-bbb-apps/src/main/scala/org/bigbluebutton/core/apps/PermisssionCheck.scala
+++ akka-bbb-apps/src/main/scala/org/bigbluebutton/core/apps/PermisssionCheck.scala
@@ -1,6 +1,8 @@
 package org.bigbluebutton.core.apps
 
-import org.bigbluebutton.core.models.{ Roles, UserState }
+import org.bigbluebutton.core.models.{Roles, UserState, Users2x}
+import org.bigbluebutton.core.running.OutMsgRouter
+import org.bigbluebutton.core2.message.senders.MsgBuilder
 
 object PermisssionCheck {
 
@@ -12,8 +14,10 @@ object PermisssionCheck {
   val VIEWER_LEVEL = 0
 
   private def permissionToLevel(user: UserState): Int = {
-    if (user.authed) {
-      if (user.role == Roles.MODERATOR_ROLE) MOD_LEVEL else AUTHED_LEVEL
+    if (user.role == Roles.MODERATOR_ROLE) {
+      MOD_LEVEL
+    } else if (user.authed) {
+        AUTHED_LEVEL
     } else {
       GUEST_LEVEL
     }
@@ -33,8 +37,17 @@ object PermisssionCheck {
    * @param roleLevel Lowest role needed to have access.
    * @return true allows API to execute, false denies executing API
    */
-  def isAllowed(permissionLevel: Int, roleLevel: Int, user: UserState): Boolean = {
-    (permissionLevel <= permissionToLevel(user) && roleLevel <= roleToLevel(user))
+  def isAllowed(permissionLevel: Int, roleLevel: Int, users: Users2x, userId: String): Boolean = {
+    Users2x.findWithIntId(users, userId) match {
+      case Some(user) => (permissionToLevel(user) >= permissionLevel && roleToLevel(user) >= roleLevel)
+      case None => false
+    }
+
   }
 
+  def ejectUserForFailedPermission(meetingId: String, userId: String, reason: String, outGW: OutMsgRouter):Unit = {
+    // send a system message to force disconnection
+    val ejectFromMeetingSystemEvent = MsgBuilder.buildDisconnectClientSysMsg(meetingId, userId, reason)
+    outGW.send(ejectFromMeetingSystemEvent)
+  }
 }
