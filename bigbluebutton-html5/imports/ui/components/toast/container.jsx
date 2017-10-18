114689d28464eef385515c07fddd47afd65297a0
Tainan Felipe <tainanf@imdt.com.br>
2017-10-18 17:24:26 -0200

    add toast notification for ended breakout room

--- bigbluebutton-html5/imports/ui/components/toast/container.jsx
+++ bigbluebutton-html5/imports/ui/components/toast/container.jsx
@@ -1,21 +1,50 @@
 import React from 'react';
-
-import { ToastContainer } from 'react-toastify';
-
+import Breakouts from '/imports/api/2.0/breakouts';
+import { ToastContainer as Toastify } from 'react-toastify';
+import { createContainer } from 'meteor/react-meteor-data';
+import { defineMessages, injectIntl } from 'react-intl';
+import { withToast } from '/imports/ui/components/toast/service';
 import Icon from '../icon/component';
 import styles from './styles';
 
-export default () => (
-  <ToastContainer
-    closeButton={<Icon className={styles.close} iconName="close" />}
-    autoClose={5000}
-    className={styles.container}
-    toastClassName={styles.toast}
-    bodyClassName={styles.body}
-    progressClassName={styles.progress}
-    newestOnTop={false}
-    hideProgressBar={false}
-    closeOnClick
-    pauseOnHover
-  />
-);
+const intlMessages = defineMessages({
+  toastBreakoutRoomEnded: {
+    id: 'app.toast.BreakoutRoomEnded',
+    description: 'message when the breakout room is ended',
+  },
+});
+
+class ToastContainer extends React.Component {
+  // we never want this component to update since will break Toastify
+  shouldComponentUpdate() {
+    return false;
+  }
+
+  render() {
+    return <Toastify {...this.props} />;
+  }
+}
+
+export default injectIntl(withToast(createContainer(({
+  toastNotify,
+  intl,
+}) => {
+  Breakouts.find().observeChanges({
+    removed() {
+      toastNotify(intl.formatMessage(intlMessages.toastBreakoutRoomEnded), 'info', 'application');
+    },
+  });
+
+  return {
+    closeButton: (<Icon className={styles.close} iconName="close" />),
+    autoClose: 5000,
+    className: styles.container,
+    toastClassName: styles.toast,
+    bodyClassName: styles.body,
+    progressClassName: styles.progress,
+    newestOnTop: false,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+  };
+}, ToastContainer)));
