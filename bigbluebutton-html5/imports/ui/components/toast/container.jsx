ebfed7795edddf0fb2fa33d0106d6527280ceb65
Oswaldo Acauan <oswaldoacauan@gmail.com>
2017-10-19 08:45:07 -0200

    Add notification when recording start/stop

--- bigbluebutton-html5/imports/ui/components/toast/container.jsx
+++ bigbluebutton-html5/imports/ui/components/toast/container.jsx
@@ -1,21 +1,62 @@
 import React from 'react';
+import { ToastContainer as Toastify } from 'react-toastify';
+import { createContainer } from 'meteor/react-meteor-data';
+import { defineMessages, injectIntl } from 'react-intl';
+import { withToast } from '/imports/ui/components/toast/service';
 
-import { ToastContainer } from 'react-toastify';
+import Auth from '/imports/ui/services/auth';
+import Meetings from '/imports/api/meetings';
 
 import Icon from '../icon/component';
 import styles from './styles';
 
-export default () => (
-  <ToastContainer
-    closeButton={<Icon className={styles.close} iconName="close" />}
-    autoClose={5000}
-    className={styles.container}
-    toastClassName={styles.toast}
-    bodyClassName={styles.body}
-    progressClassName={styles.progress}
-    newestOnTop={false}
-    hideProgressBar={false}
-    closeOnClick
-    pauseOnHover
-  />
-);
+const intlMessages = defineMessages({
+  notificationRecordingStart: {
+    id: 'app.notification.recordingStart',
+    description: 'Notification for when the recording start',
+  },
+  notificationRecordingStop: {
+    id: 'app.notification.recordingStop',
+    description: 'Notification for when the recording stpop',
+  },
+});
+
+class ToastContainer extends React.Component {
+  // we never want this component to update since will break Toastify
+  shouldComponentUpdate() {
+    return false;
+  }
+
+  render() {
+    return <Toastify {...this.props} />;
+  }
+}
+
+export default injectIntl(withToast(createContainer(({ toastNotify, intl }) => {
+  const meetingId = Auth.meetingID;
+
+  Meetings.find({ meetingId }).observeChanges({
+    changed: (id, fields) => {
+      if (fields.recordProp.record === true) {
+        toastNotify(intl.formatMessage(intlMessages.notificationRecordingStart), 'success', 'record');
+      }
+
+      if (fields.recordProp.record === false) {
+        toastNotify(intl.formatMessage(intlMessages.notificationRecordingStop), 'error', 'record');
+      }
+    },
+  });
+
+  return {
+    closeButton: (<Icon className={styles.close} iconName="close" />),
+    autoClose: 5000,
+    className: styles.container,
+    toastClassName: styles.toast,
+    bodyClassName: styles.body,
+    progressClassName: styles.progress,
+    newestOnTop: false,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+  };
+}, ToastContainer)));
