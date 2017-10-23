896883ecd9d466de2790b5e06597991ecc520846
Tainan Felipe <tainanf@imdt.com.br>
2017-10-23 12:01:00 -0200

    remove comment in recording-indicator component

--- bigbluebutton-html5/imports/ui/components/nav-bar/recording-indicator/component.jsx
+++ bigbluebutton-html5/imports/ui/components/nav-bar/recording-indicator/component.jsx
@@ -1,22 +1,6 @@
 import React from 'react';
 import styles from './styles';
 
-/* export default class RecordingIndicator extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    const { beingRecorded } = this.props;
-
-    if (!beingRecorded) {
-      return null;
-    }
-
-    return (<div className={styles.indicator} />);
-  }
-} */
-
 const RecordingIndicator = ({ beingRecorded }) => {
   if (!beingRecorded) return null;
   return <div className={styles.indicator} />;
