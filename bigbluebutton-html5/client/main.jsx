3bd0d349f87b7b910879c5b36c1e647563113f22
Oswaldo Acauan <oswaldoacauan@gmail.com>
2017-10-24 10:59:23 -0200

    Add handler to log all uncaught client exceptions

--- bigbluebutton-html5/client/main.jsx
+++ bigbluebutton-html5/client/main.jsx
@@ -2,7 +2,15 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 import { renderRoutes } from '/imports/startup/client/routes';
+import { log } from '/imports/ui/services/api';
+
 
 Meteor.startup(() => {
   render(renderRoutes(), document.getElementById('app'));
+
+  // Log all uncaught exceptions to the server
+  // TODO: There is no StackTrace on the ErrorEvent object
+  window.addEventListener('error', (e) => {
+    log('error', e);
+  });
 });
