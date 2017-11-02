4d44f09c847329a59828142234650f3c91131de7
Richard Alam <ritzalam@gmail.com>
2017-11-02 13:16:44 -0700

    - add permissions check on a few messages

--- akka-bbb-apps/src/main/scala/org/bigbluebutton/core/apps/presentationpod/RemovePresentationPodPubMsgHdlr.scala
+++ akka-bbb-apps/src/main/scala/org/bigbluebutton/core/apps/presentationpod/RemovePresentationPodPubMsgHdlr.scala
@@ -1,6 +1,7 @@
 package org.bigbluebutton.core.apps.presentationpod
 
 import org.bigbluebutton.common2.msgs._
+import org.bigbluebutton.core.apps.PermissionCheck
 import org.bigbluebutton.core.bus.MessageBus
 import org.bigbluebutton.core.domain.MeetingState2x
 import org.bigbluebutton.core.running.LiveMeeting
@@ -11,54 +12,61 @@ trait RemovePresentationPodPubMsgHdlr {
   def handle(msg: RemovePresentationPodPubMsg, state: MeetingState2x,
              liveMeeting: LiveMeeting, bus: MessageBus): MeetingState2x = {
 
-    def buildRemovePresentationPodEvtMsg(meetingId: String, ownerId: String, podId: String): BbbCommonEnvCoreMsg = {
-      val routing = Routing.addMsgToClientRouting(MessageTypes.BROADCAST_TO_MEETING, meetingId, ownerId)
-      val envelope = BbbCoreEnvelope(RemovePresentationPodEvtMsg.NAME, routing)
-      val header = BbbClientMsgHeader(RemovePresentationPodEvtMsg.NAME, meetingId, ownerId)
+    if (applyPermissionCheck && !PermissionCheck.isAllowed(PermissionCheck.MOD_LEVEL, PermissionCheck.PRESENTER_LEVEL, liveMeeting.users2x, msg.header.userId)) {
+      val meetingId = liveMeeting.props.meetingProp.intId
+      val reason = "No permission to remove presentation pod from meeting."
+      PermissionCheck.ejectUserForFailedPermission(meetingId, msg.header.userId, reason, bus.outGW)
+      state
+    } else {
+      def buildRemovePresentationPodEvtMsg(meetingId: String, ownerId: String, podId: String): BbbCommonEnvCoreMsg = {
+        val routing = Routing.addMsgToClientRouting(MessageTypes.BROADCAST_TO_MEETING, meetingId, ownerId)
+        val envelope = BbbCoreEnvelope(RemovePresentationPodEvtMsg.NAME, routing)
+        val header = BbbClientMsgHeader(RemovePresentationPodEvtMsg.NAME, meetingId, ownerId)
 
-      val body = RemovePresentationPodEvtMsgBody(ownerId, podId)
-      val event = RemovePresentationPodEvtMsg(header, body)
+        val body = RemovePresentationPodEvtMsgBody(ownerId, podId)
+        val event = RemovePresentationPodEvtMsg(header, body)
 
-      BbbCommonEnvCoreMsg(envelope, event)
+        BbbCommonEnvCoreMsg(envelope, event)
+      }
+
+      val requesterId = msg.body.requesterId // TODO -- use it
+
+      val newState = for {
+        pod <- PresentationPodsApp.getPresentationPod(state, msg.body.podId)
+      } yield {
+
+        val ownerId = pod.ownerId
+
+        val event = buildRemovePresentationPodEvtMsg(
+          liveMeeting.props.meetingProp.intId,
+          ownerId, pod.id
+        )
+
+        bus.outGW.send(event)
+
+        val pods = state.presentationPodManager.removePod(pod.id)
+        state.update(pods)
+      }
+
+      newState match {
+        case Some(ns) => ns
+        case None     => state
+      }
+
+      // TODO check if requesterId == ownerId
+      // TODO check about notifying only the list of authorized?
+
+      //    val respMsg = buildRemovePresentationPodEvtMsg(
+      //      liveMeeting.props.meetingProp.intId,
+      //      ownerId, pod.id
+      //    )
+      //    bus.outGW.send(respMsg)
+      //
+      //    log.warning("RemovePresentationPodPubMsgHdlr new podId=" + pod.id)
+      //
+      //    val pods = state.presentationPodManager.removePod(pod)
+      //    state.update(pods)
     }
-
-    val requesterId = msg.body.requesterId // TODO -- use it
-
-    val newState = for {
-      pod <- PresentationPodsApp.getPresentationPod(state, msg.body.podId)
-    } yield {
-
-      val ownerId = pod.ownerId
-
-      val event = buildRemovePresentationPodEvtMsg(
-        liveMeeting.props.meetingProp.intId,
-        ownerId, pod.id
-      )
-
-      bus.outGW.send(event)
-
-      val pods = state.presentationPodManager.removePod(pod.id)
-      state.update(pods)
-    }
-
-    newState match {
-      case Some(ns) => ns
-      case None     => state
-    }
-
-    // TODO check if requesterId == ownerId
-    // TODO check about notifying only the list of authorized?
-
-    //    val respMsg = buildRemovePresentationPodEvtMsg(
-    //      liveMeeting.props.meetingProp.intId,
-    //      ownerId, pod.id
-    //    )
-    //    bus.outGW.send(respMsg)
-    //
-    //    log.warning("RemovePresentationPodPubMsgHdlr new podId=" + pod.id)
-    //
-    //    val pods = state.presentationPodManager.removePod(pod)
-    //    state.update(pods)
 
   }
 }
--- akka-bbb-apps/src/main/scala/org/bigbluebutton/core/apps/presentationpod/RemovePresentationPubMsgHdlr.scala
+++ akka-bbb-apps/src/main/scala/org/bigbluebutton/core/apps/presentationpod/RemovePresentationPubMsgHdlr.scala
@@ -1,6 +1,7 @@
 package org.bigbluebutton.core.apps.presentationpod
 
 import org.bigbluebutton.common2.msgs._
+import org.bigbluebutton.core.apps.PermissionCheck
 import org.bigbluebutton.core.bus.MessageBus
 import org.bigbluebutton.core.domain.MeetingState2x
 import org.bigbluebutton.core.running.LiveMeeting
@@ -13,35 +14,42 @@ trait RemovePresentationPubMsgHdlr {
     liveMeeting: LiveMeeting, bus: MessageBus
   ): MeetingState2x = {
 
-    def broadcastRemovePresentationEvtMsg(podId: String, userId: String, presentationId: String): Unit = {
-      val routing = Routing.addMsgToClientRouting(
-        MessageTypes.BROADCAST_TO_MEETING,
-        liveMeeting.props.meetingProp.intId, userId
-      )
-      val envelope = BbbCoreEnvelope(RemovePresentationEvtMsg.NAME, routing)
-      val header = BbbClientMsgHeader(RemovePresentationEvtMsg.NAME, liveMeeting.props.meetingProp.intId, userId)
+    if (applyPermissionCheck && !PermissionCheck.isAllowed(PermissionCheck.MOD_LEVEL, PermissionCheck.PRESENTER_LEVEL, liveMeeting.users2x, msg.header.userId)) {
+      val meetingId = liveMeeting.props.meetingProp.intId
+      val reason = "No permission to remove presentation from meeting."
+      PermissionCheck.ejectUserForFailedPermission(meetingId, msg.header.userId, reason, bus.outGW)
+      state
+    } else {
+      def broadcastRemovePresentationEvtMsg(podId: String, userId: String, presentationId: String): Unit = {
+        val routing = Routing.addMsgToClientRouting(
+          MessageTypes.BROADCAST_TO_MEETING,
+          liveMeeting.props.meetingProp.intId, userId
+        )
+        val envelope = BbbCoreEnvelope(RemovePresentationEvtMsg.NAME, routing)
+        val header = BbbClientMsgHeader(RemovePresentationEvtMsg.NAME, liveMeeting.props.meetingProp.intId, userId)
 
-      val body = RemovePresentationEvtMsgBody(podId, presentationId)
-      val event = RemovePresentationEvtMsg(header, body)
-      val msgEvent = BbbCommonEnvCoreMsg(envelope, event)
-      bus.outGW.send(msgEvent)
-    }
+        val body = RemovePresentationEvtMsgBody(podId, presentationId)
+        val event = RemovePresentationEvtMsg(header, body)
+        val msgEvent = BbbCommonEnvCoreMsg(envelope, event)
+        bus.outGW.send(msgEvent)
+      }
 
-    val podId = msg.body.podId
-    val presentationId = msg.body.presentationId
+      val podId = msg.body.podId
+      val presentationId = msg.body.presentationId
 
-    val newState = for {
-      pod <- PresentationPodsApp.getPresentationPod(state, podId)
-    } yield {
-      broadcastRemovePresentationEvtMsg(pod.id, msg.header.userId, presentationId)
+      val newState = for {
+        pod <- PresentationPodsApp.getPresentationPod(state, podId)
+      } yield {
+        broadcastRemovePresentationEvtMsg(pod.id, msg.header.userId, presentationId)
 
-      val pods = state.presentationPodManager.removePresentationInPod(pod.id, presentationId)
-      state.update(pods)
-    }
+        val pods = state.presentationPodManager.removePresentationInPod(pod.id, presentationId)
+        state.update(pods)
+      }
 
-    newState match {
-      case Some(ns) => ns
-      case None     => state
+      newState match {
+        case Some(ns) => ns
+        case None     => state
+      }
     }
 
   }
