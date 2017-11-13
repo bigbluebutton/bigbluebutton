d5460854d8f9b2d01e9e9315c81855f855dda053
Oleksandr Zhurbenko <xaerok.od.ua@gmail.com>
2017-11-13 15:23:05 -0800

    Set the file structure for the html5 visual regression screenshots

--- bigbluebutton-html5/tests/webdriverio/wdio.vreg.conf.js
+++ bigbluebutton-html5/tests/webdriverio/wdio.vreg.conf.js
@@ -1,82 +1,68 @@
 var path = require('path');
 var VisualRegressionCompare = require('wdio-visual-regression-service/compare');
-function getCommonScreenshotName(basePath) {
-  return function(context) {
-    var type = context.type;
-    var testName = context.test.title;
-    var browserVersion = parseInt(context.browser.version, 10);
-    var browserName = context.browser.name;
-    var browserViewport = context.meta.viewport;
-    var browserWidth = browserViewport.width;
-    var browserHeight = browserViewport.height;
-    return path.join(basePath, `${testName}_${type}_${browserWidth}_${browserHeight}.png`);
-  };
-}
 
-function getScreenshotNameWithBrowser(basePath) {
+function getScreenshotName(basePath) {
   return function(context) {
     var type = context.type;
     var testName = context.test.title;
     var browserVersion = parseInt(context.browser.version, 10);
-    var browserName = process.env.BROWSER_NAME=='chrome_mobile' ? process.env.DEVICE_NAME : context.browser.name;
+    var browserName = process.env.BROWSER_NAME === 'chrome_mobile' ? process.env.DEVICE_NAME : context.browser.name;
     var browserViewport = context.meta.viewport;
     var browserWidth = browserViewport.width;
     var browserHeight = browserViewport.height;
-    return path.join(basePath, `${testName}_${type}_${browserName}_${browserVersion}_${browserWidth}_${browserHeight}.png`);
+    return path.join(
+      basePath,
+      browserName,
+      `${browserWidth}x${browserHeight}`,
+      `${testName}_${type}.png`
+    );
   };
 }
 
 exports.config = {
-   
-    specs: [
-        'tests/webdriverio/specs/visual-regression/**/*.spec.js'
-    ],
+  specs: [
+    'tests/webdriverio/specs/visual-regression/**/*.spec.js'
+  ],
 
-    capabilities: [process.env.BROWSER_NAME=='chrome_mobile' ? {
-        maxInstances: 5,
-        browserName: 'chrome',
-        chromeOptions: {
-            mobileEmulation: {
-                deviceName: process.env.DEVICE_NAME
-            }
-        }
-    } : {
-        maxInstances: 5,
-        browserName: process.env.BROWSER_NAME
-    }],
+  capabilities: [process.env.BROWSER_NAME=='chrome_mobile' ? {
+    maxInstances: 5,
+    browserName: 'chrome',
+    chromeOptions: {
+      mobileEmulation: {
+        deviceName: process.env.DEVICE_NAME
+      }
+    }
+  } : {
+    maxInstances: 5,
+    browserName: process.env.BROWSER_NAME
+  }],
 
-    sync: true,
-    logLevel: 'verbose',
-    bail: 0,
+  sync: true,
+  logLevel: 'silent',
+  bail: 0,
+  host: 'localhost',
+  port: 4444,
+  baseUrl: 'http://localhost:8080',
+  waitforTimeout: 30000,
+  connectionRetryTimeout: 90000,
+  connectionRetryCount: 3,
+  services: ['visual-regression'],
+  visualRegression: {
+    compare: new VisualRegressionCompare.LocalCompare({
+      referenceName: getScreenshotName(path.join(process.cwd(), 'tests/webdriverio/screenshots/reference')),
+      screenshotName: getScreenshotName(path.join(process.cwd(), 'tests/webdriverio/screenshots/screen')),
+      diffName: getScreenshotName(path.join(process.cwd(), 'tests/webdriverio/screenshots/diff')),
+      misMatchTolerance: 0.01,
+    }),
+    viewports: process.env.BROWSER_NAME === 'chrome_mobile' ? [] : [{ width: 1920, height: 1200 }, { width: 960, height: 1200 }],
+    viewportChangePause: 300,
+    orientations: ['landscape'],
+  },
 
-    host: 'localhost',
-    port: 4444,
+  framework: 'jasmine',
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 30000
+  },
 
-    baseUrl: 'http://localhost:8080',
-
-    waitforTimeout: 30000,
-    connectionRetryTimeout: 90000,
-    connectionRetryCount: 3,
-
-    services: ['visual-regression'],
-
-    visualRegression: {
-      compare: new VisualRegressionCompare.LocalCompare({
-        referenceName: getCommonScreenshotName(path.join(process.cwd(), 'tests/webdriverio/screenshots/reference')),
-        screenshotName: getScreenshotNameWithBrowser(path.join(process.cwd(), 'tests/webdriverio/screenshots/screen')),
-        diffName: getScreenshotNameWithBrowser(path.join(process.cwd(), 'tests/webdriverio/screenshots/diff')),
-        misMatchTolerance: 0.01,
-      }),
-      viewports: process.env.BROWSER_NAME=='chrome_mobile' ? [] : [{ width: 1920, height: 1200 }, { width: 960, height: 1200 }],
-      viewportChangePause: 300,
-      orientations: ['landscape'],
-    },
-
-    framework: 'jasmine',
-    jasmineNodeOpts: {
-      defaultTimeoutInterval: 30000
-    },
-
-    reporters: ['spec'],
+  reporters: ['spec'],
 }
-
