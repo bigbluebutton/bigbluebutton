bdc89e0830c38a0b192c111f0ee294a02b0bd4d9
prlanzarin <prlanzarin@inf.ufrgs.br>
2017-12-08 16:52:37 +0000

    Refactored webcam share/unshare handlers

--- bigbluebutton-html5/imports/api/video/server/handlers/userSharedHtml5Webcam.js
+++ bigbluebutton-html5/imports/api/video/server/handlers/userSharedHtml5Webcam.js
@@ -1,11 +1,11 @@
 import sharedWebcam from '../modifiers/sharedWebcam';
 import {check} from 'meteor/check';
 
-export default function handleUserSharedHtml5Webcam({ header, payload }) {
-  const meetingId = header.meetingId;
-  const userId = header.userId;
-
+export default function handleUserSharedHtml5Webcam({ header }, meetingId ) {
+  check(header, Object);
+  const { userId } = header;
   check(meetingId, String);
+  check(userId, String);
 
   return sharedWebcam(meetingId, userId);
 }
