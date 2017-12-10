8ef47154f7e51c9cca1d5e55db01b406ed9fc1a3
KDSBrowne <kert.browne85@gmail.com>
2017-12-09 21:39:17 -0800

    use alternate shortcut implimentation

--- bigbluebutton-html5/imports/ui/components/audio/audio-controls/component.jsx
+++ bigbluebutton-html5/imports/ui/components/audio/audio-controls/component.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Button from '/imports/ui/components/button/component';
-import styles from './styles';
 import cx from 'classnames';
+import Shortcut from '/imports/ui/components/shortcut/component';
+import styles from './styles';
 
 const propTypes = {
+  handleToggleMuteMicrophone: PropTypes.func.isRequired,
+  handleJoinAudio: PropTypes.func.isRequired,
+  handleLeaveAudio: PropTypes.func.isRequired,
   disable: PropTypes.bool.isRequired,
+  unmute: PropTypes.bool.isRequired,
   mute: PropTypes.bool.isRequired,
   join: PropTypes.bool.isRequired,
+  glow: PropTypes.bool.isRequired,
 };
 
 const AudioControls = ({
   handleToggleMuteMicrophone,
+  handleJoinAudio,
+  handleLeaveAudio,
   mute,
   unmute,
   disable,
@@ -20,19 +28,29 @@ const AudioControls = ({
 }) => (
   <span className={styles.container}>
     {mute ?
-      <Button
-        className={glow ? cx(styles.button, styles.glow) : styles.button}
-        onClick={handleToggleMuteMicrophone}
-        disabled={disable}
-        label={join ? 'Leave Audio' : 'Join Audio'}
-        color={join ? 'danger' : 'primary'}
-        icon={join ? 'audio_off' : 'audio_on'}
-        size={'lg'}
-        circle
-      /> : null
-    }
-  </span>
-);
+      <Shortcut shortcut="Control+Alt+M">
+        <Button
+          className={glow ? cx(styles.button, styles.glow) : styles.button}
+          onClick={handleToggleMuteMicrophone}
+          disabled={disable}
+          label={unmute ? 'Unmute' : 'Mute'}
+          color="primary"
+          icon={unmute ? 'mute' : 'unmute'}
+          size="lg"
+          circle
+        />
+      </Shortcut> : null}
+    <Button
+      className={styles.button}
+      onClick={join ? handleLeaveAudio : handleJoinAudio}
+      disabled={disable}
+      label={join ? 'Leave Audio' : 'Join Audio'}
+      color={join ? 'danger' : 'primary'}
+      icon={join ? 'audio_off' : 'audio_on'}
+      size="lg"
+      circle
+    />
+  </span>);
 
 AudioControls.propTypes = propTypes;
 
