7e75dc08f4c0b87745389f9ca5e90988b3f137bc
KDSBrowne <kert.browne85@gmail.com>
2018-01-10 09:07:40 -0800

    handle tooltip hiding with setTimeout

--- bigbluebutton-html5/imports/ui/components/tooltip/component.jsx
+++ bigbluebutton-html5/imports/ui/components/tooltip/component.jsx
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import Tippy from 'tippy.js';
 import _ from 'lodash';
 import cx from 'classnames';
-import { ESCAPE } from '/imports/utils/keyCodes';
 
 const propTypes = {
   title: PropTypes.string.isRequired,
@@ -23,9 +22,7 @@ class Tooltip extends Component {
 
     this.tippySelectorId = _.uniqueId('tippy-');
     this.onShow = this.onShow.bind(this);
-    this.onHide = this.onHide.bind(this);
-    this.handleEscapeHide = this.handleEscapeHide.bind(this);
-    this.delay = [250, 100];
+    this.delay = [150, 50];
     this.dynamicTitle = true;
   }
 
@@ -39,24 +36,15 @@ class Tooltip extends Component {
       dynamicTitle: this.dynamicTitle,
       delay: this.delay,
       onShow: this.onShow,
-      onHide: this.onHide,
     };
 
     this.tooltip = Tippy(`#${this.tippySelectorId}`, options);
   }
 
   onShow() {
-    document.addEventListener('keyup', this.handleEscapeHide);
-  }
-
-  onHide() {
-    document.removeEventListener('keyup', this.handleEscapeHide);
-  }
-
-  handleEscapeHide(e) {
-    if (e.keyCode !== ESCAPE) return;
-
-    this.tooltip.tooltips[0].hide();
+    window.setTimeout(() => {
+      this.tooltip.tooltips[0].hide();
+    }, 1500);
   }
 
   render() {
