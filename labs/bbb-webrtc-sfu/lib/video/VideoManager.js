b6abec579af0a32d72ee602610a81a5bf6ea84f7
Lucas Fialho Zawacki <lfzawacki@gmail.com>
2018-01-11 13:47:31 +0000

    Better clean up on the bbb-wbertc serverside to fix html5 video problems

--- labs/bbb-webrtc-sfu/lib/video/VideoManager.js
+++ labs/bbb-webrtc-sfu/lib/video/VideoManager.js
@@ -41,12 +41,10 @@ var _onMessage = function (_message) {
     sessions[sessionId] = {};
   }
 
-  logAvailableSessions();
-
   switch (role) {
     case 'share':
-      if (message.cameraId && typeof sessions[sessionId][message.cameraId+'shared'] !== 'undefined' &&  sessions[sessionId][message.cameraId+'shared']) {
-        video = sessions[sessionId][message.cameraId+'shared'];
+      if (message.cameraId && typeof sessions[sessionId][message.cameraId+'-shared'] !== 'undefined' &&  sessions[sessionId][message.cameraId+'-shared']) {
+        video = sessions[sessionId][message.cameraId+'-shared'];
       }
       break;
     case 'viewer':
@@ -54,8 +52,8 @@ var _onMessage = function (_message) {
         video = sessions[sessionId][message.cameraId];
       }
     case 'any':
-      if (message.cameraId && typeof sessions[sessionId][message.cameraId+'shared'] !== 'undefined' &&  sessions[sessionId][message.cameraId+'shared']) {
-        video = sessions[sessionId][message.cameraId+'shared'];
+      if (message.cameraId && typeof sessions[sessionId][message.cameraId+'-shared'] !== 'undefined' &&  sessions[sessionId][message.cameraId+'-shared']) {
+        video = sessions[sessionId][message.cameraId+'-shared'];
       }
       else if (message.cameraId && sessions[sessionId][message.cameraId]) {
         video = sessions[sessionId][message.cameraId];
@@ -80,7 +78,7 @@ var _onMessage = function (_message) {
 
       switch (role) {
         case 'share':
-          sessions[sessionId][message.cameraId+'shared']= video;
+          sessions[sessionId][message.cameraId+'-shared']= video;
           break;
         case 'viewer':
           sessions[sessionId][message.cameraId] = video;
@@ -117,7 +115,7 @@ var _onMessage = function (_message) {
       console.log('[' + message.id + '] connection ' + sessionId + " with message => " + JSON.stringify(message, null, 2));
 
       if (video) {
-        stopSession(sessionId, role, cameraId);
+        stopVideo(sessionId, role, cameraId);
       } else {
         console.log(" [stop] Why is there no video on STOP?");
       }
@@ -156,26 +154,39 @@ var _onMessage = function (_message) {
   }
 };
 
-let stopSession = async function(sessionId, role, cameraId) {
-  console.log('  [VideoManager/x] Stopping session ' + sessionId + " with role " + role + " for camera " + cameraId);
+let stopSession = async function(sessionId) {
 
   let videoIds = Object.keys(sessions[sessionId]);
 
+  for (let i=0; i < videoIds.length; i++) {
+    let camId = videoIds[i].split('-')[0], role = videoIds[i].split('-')[1];
+    await stopVideo(sessionId, role ? 'share' : 'viewer', camId);
+  }
+
+  delete sessions[sessionId];
+  logAvailableSessions();
+}
+
+let stopVideo = async function(sessionId, role, cameraId) {
+  console.log('  [VideoManager/x] Stopping session ' + sessionId + " with role " + role + " for camera " + cameraId);
+
   try {
     if (role === 'share') {
-      var sharedVideo = sessions[sessionId][cameraId+'shared'];
-      await sharedVideo.stop();
-      delete sessions[sessionId][cameraId+'shared'];
-      console.log('  [VideoManager] Stopping sharer [', sessionId, '][', cameraId,'] with IDs' , videoIds);
+      var sharedVideo = sessions[sessionId][cameraId+'-shared'];
+      if (sharedVideo) {
+        console.log('  [VideoManager] Stopping sharer [', sessionId, '][', cameraId,']');
+        await sharedVideo.stop();
+        delete sessions[sessionId][cameraId+'-shared'];
+      }
     }
     else if (role === 'viewer') {
       var video = sessions[sessionId][cameraId];
-      await video.stop();
-      delete sessions[sessionId][cameraId];
-      console.log('  [VideoManager] Stopping viewer [', sessionId, '][', cameraId,'] with IDs ', sessions[sessionId][cameraId]);
+      if (video) {
+        console.log('  [VideoManager] Stopping viewer [', sessionId, '][', cameraId,']');
+        await video.stop();
+        delete sessions[sessionId][cameraId];
+      }
     }
-
-    logAvailableSessions();
   }
   catch (err) {
     console.log("  [VideoManager] Stop error => ", err);
@@ -192,7 +203,6 @@ let stopAll = function() {
   let sessionIds = Object.keys(sessions);
 
   for (var i = 0; i < sessionIds.length; i++) {
-
     stopSession(sessionIds[i]);
   }
 
