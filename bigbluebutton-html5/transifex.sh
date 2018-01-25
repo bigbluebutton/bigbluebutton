b37d4528af2dcf86b412dd64c8fe660e28f34bf3
Anton Georgiev <anto.georgiev@gmail.com>
2018-01-25 11:02:15 -0500

    unify indentation in transifex.sh

--- bigbluebutton-html5/transifex.sh
+++ bigbluebutton-html5/transifex.sh
@@ -26,43 +26,43 @@ else
       for ARG in "$@"
       do
         if [ "$ARG" == "all"  ]
-	then
-	  AVAILABLE_TRANSLATIONS=$( echo "$PROJECT_INFO" | grep 'language_code' | cut -d':' -f2 | tr -d '[",]' )
+        then
+          AVAILABLE_TRANSLATIONS=$( echo "$PROJECT_INFO" | grep 'language_code' | cut -d':' -f2 | tr -d '[",]' )
 
           echo "$AVAILABLE_TRANSLATIONS" | while read l
-	  do
-	    LOCALE=$( echo "$l" | tr -d '[:space:]' )
-	    TRANSLATION=$(curl -L --user "$USER":"$PW" -X GET "https://www.transifex.com/api/2/project/bigbluebutton-html5/resource/enjson/translation/$LOCALE/?mode=onlytranslated&file")
-	    NO_EMPTY_STRINGS=$(echo "$TRANSLATION" | sed '/: *\"\"/D' | sed '/}$/D')
-	    if [ $(echo "$NO_EMPTY_STRINGS" | wc -l) == 1 ]
-	    then
-	      echo -e "${RED}WARN:${NC} translation file $LOCALE.json is empty\n${RED}WARN:${NC} $LOCALE.json not created"
-	      continue
-	    else
-	      NO_TRAILING_COMMA=$(echo "$NO_EMPTY_STRINGS" | sed  '$ s/,$//')
-	      echo "$NO_TRAILING_COMMA" > ./private/locales/"$LOCALE".json
-	      echo -e "\n}\n" >> ./private/locales/"$LOCALE".json
-	      echo -e "Added translation file $LOCALE.json : ${GREEN}✓${NC}"
-	    fi
-	  done
-	else
-	  TRANSLATION=$(curl -L --user "$USER":"$PW" -X GET "https://www.transifex.com/api/2/project/bigbluebutton-html5/resource/enjson/translation/$ARG/?mode=onlytranslated&file")
-	  if [ "$TRANSLATION" == "Not Found" ]
-	  then
-	    echo -e "${RED}Err${NC}: Translations not found for locale ->${RED}$ARG${NC}<-"
-	  else
-	    NO_EMPTY_STRINGS=$(echo "$TRANSLATION" | sed '/: *\"\"/D' | sed '/}$/D')
-	    if [ $(echo "$NO_EMPTY_STRINGS" | wc -l) == 1 ]
-	    then
-	      echo -e "${RED}WARN:${NC} translation file $ARG.json is empty\n${RED}WARN:${NC} $ARG.json not created"
-	    else
-	      NO_TRAILING_COMMA=$(echo "$NO_EMPTY_STRINGS" | sed  '$ s/,//')
-	      echo "$NO_TRAILING_COMMA" > ./private/locales/"$ARG".json
-	      echo -e "\n}\n" >> ./private/locales/"$ARG".json
-	      echo -e "Added translation file $ARG.json :${GREEN} ✓${NC}"
-	    fi
-	  fi
-	fi
+            do
+              LOCALE=$( echo "$l" | tr -d '[:space:]' )
+              TRANSLATION=$(curl -L --user "$USER":"$PW" -X GET "https://www.transifex.com/api/2/project/bigbluebutton-html5/resource/enjson/translation/$LOCALE/?mode=onlytranslated&file")
+              NO_EMPTY_STRINGS=$(echo "$TRANSLATION" | sed '/: *\"\"/D' | sed '/}$/D')
+              if [ $(echo "$NO_EMPTY_STRINGS" | wc -l) == 1 ]
+              then
+                echo -e "${RED}WARN:${NC} translation file $LOCALE.json is empty\n${RED}WARN:${NC} $LOCALE.json not created"
+                continue
+              else
+                NO_TRAILING_COMMA=$(echo "$NO_EMPTY_STRINGS" | sed  '$ s/,$//')
+                echo "$NO_TRAILING_COMMA" > ./private/locales/"$LOCALE".json
+                echo -e "\n}\n" >> ./private/locales/"$LOCALE".json
+                echo -e "Added translation file $LOCALE.json : ${GREEN}✓${NC}"
+              fi
+            done
+        else
+          TRANSLATION=$(curl -L --user "$USER":"$PW" -X GET "https://www.transifex.com/api/2/project/bigbluebutton-html5/resource/enjson/translation/$ARG/?mode=onlytranslated&file")
+          if [ "$TRANSLATION" == "Not Found" ]
+          then
+            echo -e "${RED}Err${NC}: Translations not found for locale ->${RED}$ARG${NC}<-"
+          else
+            NO_EMPTY_STRINGS=$(echo "$TRANSLATION" | sed '/: *\"\"/D' | sed '/}$/D')
+            if [ $(echo "$NO_EMPTY_STRINGS" | wc -l) == 1 ]
+            then
+              echo -e "${RED}WARN:${NC} translation file $ARG.json is empty\n${RED}WARN:${NC} $ARG.json not created"
+            else
+              NO_TRAILING_COMMA=$(echo "$NO_EMPTY_STRINGS" | sed  '$ s/,//')
+              echo "$NO_TRAILING_COMMA" > ./private/locales/"$ARG".json
+              echo -e "\n}\n" >> ./private/locales/"$ARG".json
+              echo -e "Added translation file $ARG.json :${GREEN} ✓${NC}"
+            fi
+          fi
+        fi
       done
     fi
   fi
