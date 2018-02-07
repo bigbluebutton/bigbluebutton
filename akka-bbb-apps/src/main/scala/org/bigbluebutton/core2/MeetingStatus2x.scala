47147056c4ec8f2441dc3934d712cd54dd1f4f10
Richard Alam <ritzalam@gmail.com>
2018-02-07 07:30:33 -0800

    - handle properly out of order recording started/stopped events from freeswitch

--- akka-bbb-apps/src/main/scala/org/bigbluebutton/core2/MeetingStatus2x.scala
+++ akka-bbb-apps/src/main/scala/org/bigbluebutton/core2/MeetingStatus2x.scala
@@ -17,18 +17,12 @@ case class MeetingExtensionProp(maxExtensions: Int = 2, numExtensions: Int = 0, 
                                 sent10MinNotice: Boolean = false, sent5MinNotice: Boolean = false)
 
 object MeetingStatus2x {
-  def startRecordingVoice(status: MeetingStatus2x): Boolean = {
-    status.recordingVoice = true
-    status.recordingVoice
-  }
-
-  def stopRecordingVoice(status: MeetingStatus2x): Boolean = {
-    status.recordingVoice = false
-    status.recordingVoice
-  }
 
   def isVoiceRecording(status: MeetingStatus2x): Boolean = {
-    status.recordingVoice
+    status.voiceRecordings.values.find(s => s.recording) match {
+      case Some(rec) => true
+      case None      => false
+    }
   }
 
   def isExtensionAllowed(status: MeetingStatus2x): Boolean = status.extension.numExtensions < status.extension.maxExtensions
@@ -50,8 +44,41 @@ object MeetingStatus2x {
   def recordingStarted(status: MeetingStatus2x) = status.recording = true
   def recordingStopped(status: MeetingStatus2x) = status.recording = false
   def isRecording(status: MeetingStatus2x): Boolean = status.recording
-  def setVoiceRecordingFilename(status: MeetingStatus2x, path: String) = status.voiceRecordingFilename = path
-  def getVoiceRecordingFilename(status: MeetingStatus2x): String = status.voiceRecordingFilename
+
+  def voiceRecordingStart(status2x: MeetingStatus2x, stream: String): VoiceRecordingStream = {
+    val vrs = new VoiceRecordingStream(stream, recording = false, createdOn = System.currentTimeMillis, ackedOn = None, stoppedOn = None)
+    status2x.voiceRecordings += vrs.stream -> vrs
+    vrs
+  }
+
+  def voiceRecordingStarted(status2x: MeetingStatus2x, stream: String): Option[VoiceRecordingStream] = {
+    val vrs = for {
+      recStream <- status2x.voiceRecordings.values find (s => s.stream == stream)
+    } yield {
+      val rs = recStream.copy(recording = true, ackedOn = Some(System.currentTimeMillis))
+      status2x.voiceRecordings += rs.stream -> rs
+      rs
+    }
+
+    vrs
+  }
+
+  def voiceRecordingStopped(status2x: MeetingStatus2x, stream: String): Option[VoiceRecordingStream] = {
+    val vrs = for {
+      recStream <- status2x.voiceRecordings.values find (s => s.stream == stream)
+    } yield {
+      val rs = recStream.copy(recording = false, stoppedOn = Some(System.currentTimeMillis))
+      status2x.voiceRecordings += rs.stream -> rs
+      rs
+    }
+
+    vrs
+  }
+
+  def getVoiceRecordingStreams(status2x: MeetingStatus2x): Vector[VoiceRecordingStream] = {
+    status2x.voiceRecordings.values.filter(s => s.recording).toVector
+  }
+
   def setWebcamsOnlyForModerator(status: MeetingStatus2x, value: Boolean) = status.webcamsOnlyForModerator = value
   def webcamsOnlyForModeratorEnabled(status: MeetingStatus2x): Boolean = status.webcamsOnlyForModerator
   def permisionsInitialized(status: MeetingStatus2x): Boolean = status.permissionsInited
@@ -73,8 +100,8 @@ object MeetingStatus2x {
 }
 
 class MeetingStatus2x {
+  private var voiceRecordings: collection.immutable.HashMap[String, VoiceRecordingStream] = new collection.immutable.HashMap[String, VoiceRecordingStream]
 
-  private var recordingVoice = false
   private var audioSettingsInited = false
   private var permissionsInited = false
   private var permissionsChangedOn: Long = System.currentTimeMillis()
@@ -84,10 +111,10 @@ class MeetingStatus2x {
   private var meetingEnded = false
   private var meetingMuted = false
 
-  private var voiceRecordingFilename: String = ""
-
   private var extension = new MeetingExtensionProp
 
   private var webcamsOnlyForModerator = false
 
 }
+
+case class VoiceRecordingStream(stream: String, recording: Boolean, createdOn: Long, ackedOn: Option[Long], stoppedOn: Option[Long])
