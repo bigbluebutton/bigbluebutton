86993144cff8dce9971429b6268dc6664adb2ea5
João Francisco Siebel <jfsiebel@gmail.com>
2018-02-26 09:46:27 -0300

    Change recording indicator to css

--- bigbluebutton-html5/imports/ui/components/nav-bar/recording-indicator/component.jsx
+++ bigbluebutton-html5/imports/ui/components/nav-bar/recording-indicator/component.jsx
@@ -1,19 +1,15 @@
 import React from 'react';
-import Icon from '/imports/ui/components/icon/component';
 import { styles } from './styles';
 
-const RecordingIndicator = ({ record, allowStartStopRecording, title, recording }) => {
-
+const RecordingIndicator = ({
+  record, allowStartStopRecording, title, recording,
+}) => {
   if (!record || !allowStartStopRecording) return null;
 
   return (
-    <div className={recording ? styles.recording : styles.recordIndicator}>
-      <Icon
-        iconName="record"
-      />
-      <span>
-        {title}
-      </span>
+    <div>
+      <div className={recording ? styles.recordIndicator : styles.notRecording} />
+      <span className={recording ? styles.recordingLabel : styles.notRecordingLabel}>{title}</span>
     </div>
   );
 };
