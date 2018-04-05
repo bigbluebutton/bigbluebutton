f4ab6b76f37536c0f8152d393de5a7901c874f2a
Oleksandr Zhurbenko <xaerok.od.ua@gmail.com>
2018-04-05 15:31:39 -0700

    Corrected handlers.js

--- bigbluebutton-html5/imports/api/presentation-pods/server/eventHandlers.js
+++ bigbluebutton-html5/imports/api/presentation-pods/server/eventHandlers.js
@@ -1,12 +1,10 @@
 import RedisPubSub from '/imports/startup/server/redis';
 import handleCreateNewPresentationPod from './handlers/createNewPresentationPod';
 import handleRemovePresentationPod from './handlers/removePresentationPod';
-import handleGetAllPresentationPods from './handlers/getAllPresentationPods';
+import handleSyncGetPresentationPods from './handlers/syncGetPresentationPods';
 import handleSetPresenterInPod from './handlers/setPresenterInPod';
-// import handleSyncGetPresentationPods from './handlers/syncGetPresentationPods';
 
 RedisPubSub.on('CreateNewPresentationPodEvtMsg', handleCreateNewPresentationPod);
 RedisPubSub.on('RemovePresentationPodEvtMsg', handleRemovePresentationPod);
-// RedisPubSub.on('GetAllPresentationPodsRespMsg', handleGetAllPresentationPods);
 RedisPubSub.on('SetPresenterInPodRespMsg', handleSetPresenterInPod);
-RedisPubSub.on('SyncGetPresentationPodsRespMsg', handleGetAllPresentationPods);
+RedisPubSub.on('SyncGetPresentationPodsRespMsg', handleSyncGetPresentationPods);
