d259a875f603cf9c20685847cdeae60f50fd0982
Tainan Felipe <tainanfelipe214@gmail.com>
2018-04-11 16:37:46 -0300

    remove duplicated code in whiteboard-multi-user api

--- bigbluebutton-html5/imports/api/whiteboard-multi-user/server/eventHandlers.js
+++ bigbluebutton-html5/imports/api/whiteboard-multi-user/server/eventHandlers.js
@@ -1,7 +1,6 @@
 import RedisPubSub from '/imports/startup/server/redis';
 import handleGetWhiteboardAccess from './handlers/getWhiteboardAccess';
-import handleModifyWhiteboardAccess from './handlers/modifyWhiteboardAccess';
 
 RedisPubSub.on('GetWhiteboardAccessRespMsg', handleGetWhiteboardAccess);
 RedisPubSub.on('SyncGetWhiteboardAccessRespMsg', handleGetWhiteboardAccess);
-RedisPubSub.on('ModifyWhiteboardAccessEvtMsg', handleModifyWhiteboardAccess);
+RedisPubSub.on('ModifyWhiteboardAccessEvtMsg', handleGetWhiteboardAccess);
