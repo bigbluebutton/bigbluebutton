45213008f4d5d3e1af1bdc05b2871756faf44c42
KDSBrowne <kert.browne85@gmail.com>
2018-04-17 08:06:19 -0700

    add comment

--- bigbluebutton-html5/imports/ui/components/settings/component.jsx
+++ bigbluebutton-html5/imports/ui/components/settings/component.jsx
@@ -218,6 +218,9 @@ class Settings extends Component {
         confirm={{
           callback: () => {
             this.updateSettings(this.state.current);
+            /* We need to use mountModal(null) here to prevent submenu state updates,
+            *  from re-opening the modal.
+            */
             this.props.mountModal(null);
           },
           label: intl.formatMessage(intlMessages.SaveLabel),
