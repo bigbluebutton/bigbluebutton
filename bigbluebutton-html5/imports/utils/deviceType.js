599ba78b72e7776663fa4128b5e088753a561208
KDSBrowne <kert.browne85@gmail.com>
2018-04-21 13:19:42 -0700

    add comment

--- bigbluebutton-html5/imports/utils/deviceType.js
+++ bigbluebutton-html5/imports/utils/deviceType.js
@@ -1,4 +1,6 @@
 const deviceType = () => {
+  // Listing of Device Viewport sizes, Updated : March 25th, 2018
+  // http://mediag.com/news/popular-screen-resolutions-designing-for-all/
   const MAX_PHONE_SHORT_SIDE = 480;
 
   const smallSide = window.screen.width < window.screen.height
