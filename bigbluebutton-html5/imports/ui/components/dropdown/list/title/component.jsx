ccff25ded6650c2eb801a58adc875d824f6ffac7
KDSBrowne <kert.browne85@gmail.com>
2018-04-24 08:07:04 -0700

    prevent SR from announcing duplicate user name

--- bigbluebutton-html5/imports/ui/components/dropdown/list/title/component.jsx
+++ bigbluebutton-html5/imports/ui/components/dropdown/list/title/component.jsx
@@ -17,7 +17,7 @@ export default class DropdownListTitle extends Component {
     const { className, description } = this.props;
 
     return (
-      <li className={cx(styles.title, className)} aria-describedby={this.labelID}>
+      <li className={cx(styles.title, className)} aria-describedby={this.labelID} aria-hidden={true}>
         {this.props.children}
         <div id={this.labelID} aria-label={description} />
       </li>
