9ada59d1c44e36ea649ba41028a01b4af5d538d7
Calvin Walton <calvin.walton@kepstin.ca>
2018-04-25 15:04:01 -0400

    Fix more variable references in screenshare events

--- record-and-playback/core/lib/recordandplayback/generators/events.rb
+++ record-and-playback/core/lib/recordandplayback/generators/events.rb
@@ -219,7 +219,7 @@ module BigBlueButton
           filename = start_event.at_xpath('file').text
           filename = "#{archive_dir}/deskshare/#{File.basename(filename)}"
         when 'StartWebRTCDesktopShareEvent'
-          uri = event.at_xpath('filename').text
+          uri = start_event.at_xpath('filename').text
           filename = "#{archive_dir}/deskshare/#{File.basename(uri)}"
         else
           next
@@ -240,10 +240,10 @@ module BigBlueButton
       events.xpath('/recording/event[@module="Deskshare" or @module="bbb-webrtc-sfu"]').each do |stop_event|
         case stop_event['eventname']
         when 'DeskshareStoppedEvent'
-          filename = start_event.at_xpath('file').text
+          filename = stop_event.at_xpath('file').text
           filename = "#{archive_dir}/deskshare/#{File.basename(filename)}"
         when 'StopWebRTCDesktopShareEvent'
-          uri = event.at_xpath('filename').text
+          uri = stop_event.at_xpath('filename').text
           filename = "#{archive_dir}/deskshare/#{File.basename(uri)}"
         else
           next
