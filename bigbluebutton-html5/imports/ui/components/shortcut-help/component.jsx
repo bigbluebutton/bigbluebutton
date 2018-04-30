819839cb4e4a091186f36a16bc23c69d1c718b2b
KDSBrowne <kert.browne85@gmail.com>
2018-04-30 12:18:29 -0700

    add shotcuts keys to dev-config

--- bigbluebutton-html5/imports/ui/components/shortcut-help/component.jsx
+++ bigbluebutton-html5/imports/ui/components/shortcut-help/component.jsx
@@ -44,24 +44,17 @@ const intlMessages = defineMessages({
   leaveAudio: {
     id: 'app.audio.leaveAudio',
   },
+  accesskeyDesc: {
+    id: 'app.shortcut-help.accessKeyDesc',
+  },
 });
 
 class ShortcutHelpComponent extends Component {
   render() {
     const { intl } = this.props;
 
-    const shortcuts = [
-      { keys: 'Acess Key + O', function: intl.formatMessage(intlMessages.openOptions),},
-      { keys: 'Acess Key + U', function: intl.formatMessage(intlMessages.toggleUserList),},
-      { keys: 'Acess Key + M', function: intl.formatMessage(intlMessages.toggleMute),},
-      { keys: 'Acess Key + J', function: intl.formatMessage(intlMessages.joinAudio),},
-      { keys: 'Acess Key + L', function: intl.formatMessage(intlMessages.leaveAudio),},
-      { keys: 'Acess Key + P', function: intl.formatMessage(intlMessages.togglePublicChat),},
-      { keys: 'Acess Key + H', function: intl.formatMessage(intlMessages.hidePrivateChat),},
-      { keys: 'Acess Key + G', function: intl.formatMessage(intlMessages.closePrivateChat),},
-      { keys: 'Acess Key + A', function: intl.formatMessage(intlMessages.openActions),},
-      { keys: 'Acess Key + S', function: intl.formatMessage(intlMessages.openStatus),},
-    ];
+    const SHORTCUTS_CONFIG = Meteor.settings.public.app.shortcuts;
+    const shortcuts = Object.values(SHORTCUTS_CONFIG);
 
     return (
       <Modal
@@ -71,33 +64,32 @@ class ShortcutHelpComponent extends Component {
           description: intl.formatMessage(intlMessages.closeDesc),
         }}
       >
-        <span className={styles.span}>The operation to activate the Hotkeys depends on the browser and its platform.</span>
-        <p className={styles.p}><b>Windows / Linux : Acess Key</b></p>
+        <span className={styles.span}>{intl.formatMessage(intlMessages.accesskeyDesc)}</span>
         <p className={styles.p}>
-          FireFox : Alt + Shift<br></br>
-          Chrome  : Alt
+          <br /><b>Windows / Linux </b><br />
+          <b><i>FireFox</i></b> = Alt + Shift &nbsp;
+          <b><i>Chrome</i></b>  = Alt
+          <br /><br /><b>Mac</b><br />
+          <b><i>Safari</i></b>  = Control + Alt
         </p>
-        <p className={styles.p}><b>Mac : Acess Key</b></p>
-        <p className={styles.p}>
-          Control + Alt
-        </p>
+        <br />
         <table className={styles.shortcutTable}>
-            <tbody>
-                <tr>
-                    <th>{'Combo'}</th>
-                    <th>{'Function'}</th>
-                </tr>
-                {shortcuts.map(shortcut => (
-                    <tr key={_.uniqueId('hotkey-item-')}>
-                        <td className={styles.keyCell}>{shortcut.keys}</td>
-                        <td className={styles.descCell}>{shortcut.function}</td>
-                    </tr>
+          <tbody>
+            <tr>
+              <th>Combo</th>
+              <th>Function</th>
+            </tr>
+            {shortcuts.map(shortcut => (
+              <tr key={_.uniqueId('hotkey-item-')}>
+                <td className={styles.keyCell}>{`Access key + ${shortcut.accesskey}`}</td>
+                <td className={styles.descCell}>{intl.formatMessage(intlMessages[`${shortcut.descId}`])}</td>
+              </tr>
                 ))}
-            </tbody>
+          </tbody>
         </table>
       </Modal>
     );
   }
 }
-  
+
 export default injectIntl(ShortcutHelpComponent);
