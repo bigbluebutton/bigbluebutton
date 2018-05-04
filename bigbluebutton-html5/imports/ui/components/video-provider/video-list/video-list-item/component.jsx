44a5d6c600a5f879129bf8c3ad08e51e02e2592c
Tainan Felipe <tainanfelipe214@gmail.com>
2018-05-04 16:44:30 -0300

    add voice user icon for webcam status

--- bigbluebutton-html5/imports/ui/components/video-provider/video-list/video-list-item/component.jsx
+++ bigbluebutton-html5/imports/ui/components/video-provider/video-list/video-list-item/component.jsx
@@ -22,7 +22,6 @@ class VideoListItem extends Component {
 
   render() {
     const { user, actions } = this.props;
-
     return (
       <div className={cx({
         [styles.content]: true,
@@ -45,7 +44,9 @@ class VideoListItem extends Component {
             <DropdownContent placement="top left">
               <DropdownList className={styles.dropdownList}>
                 {[
-                  <DropdownListTitle className={styles.hiddenDesktop} key="name">{user.name}</DropdownListTitle>,
+                  <DropdownListTitle className={styles.hiddenDesktop} key="name">
+                    {user.name}
+                  </DropdownListTitle>,
                   <DropdownListSeparator className={styles.hiddenDesktop} key="sep" />,
                   ...actions.map(action => (<DropdownListItem key={user.id} {...action} />)),
                 ]}
@@ -53,6 +54,7 @@ class VideoListItem extends Component {
             </DropdownContent>
           </Dropdown>
           { user.isMuted ? <Icon className={styles.muted} iconName="unmute_filled" /> : null }
+          { user.isListenOnly ? <Icon className={styles.voice} iconName="listen" /> : null }
         </div>
       </div>
     );
