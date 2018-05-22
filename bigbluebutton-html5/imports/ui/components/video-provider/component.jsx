5433049ab7d651b08d153f71546501433088e3ca
Lucas Fialho Zawacki <lfzawacki@gmail.com>
2018-05-22 17:03:08 +0000

    Better handle visibility events and paused streams

--- bigbluebutton-html5/imports/ui/components/video-provider/component.jsx
+++ bigbluebutton-html5/imports/ui/components/video-provider/component.jsx
@@ -83,7 +83,7 @@ class VideoProvider extends Component {
 
 
     Object.keys(this.webRtcPeers).forEach((id) => {
-      if (this.props.userId !== id) {
+      if (this.props.userId !== id && this.webRtcPeers[id].started) {
         this._sendPauseStream(id, 'viewer', true);
       }
     });
@@ -93,7 +93,7 @@ class VideoProvider extends Component {
     log("debug", "Calling un-pause in viewer streams");
 
     Object.keys(this.webRtcPeers).forEach((id) => {
-      if (id !== this.props.userId) {
+      if (id !== this.props.userId && this.webRtcPeers[id].started) {
         this._sendPauseStream(id, 'viewer', false);
       }
     });
