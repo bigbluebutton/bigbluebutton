97b5d4dbc93cddec5d6295ede8f23861049d97d0
KDSBrowne <kert.browne85@gmail.com>
2018-05-29 07:48:53 -0700

    remove cx() and its import

--- bigbluebutton-html5/imports/ui/components/user-list/user-list-content/user-participants/user-list-item/user-list-content/component.jsx
+++ bigbluebutton-html5/imports/ui/components/user-list/user-list-content/user-participants/user-list-item/user-list-content/component.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { defineMessages } from 'react-intl';
-import cx from 'classnames';
 import PropTypes from 'prop-types';
 import { findDOMNode } from 'react-dom';
 import UserAvatar from '/imports/ui/components/user-avatar/component';
@@ -221,7 +220,7 @@ class UserListContent extends Component {
 
     const contents = (
       <div
-        className={!actions.length ? cx(styles.userListItem) : null}
+        className={!actions.length ? styles.userListItem : null}
       >
         <div className={styles.userItemContents}>
           <div className={styles.userAvatar}>
@@ -266,7 +265,7 @@ class UserListContent extends Component {
         isOpen={this.state.isActionsOpen}
         onShow={this.onActionsShow}
         onHide={this.onActionsHide}
-        className={cx(userItemContentsStyle)}
+        className={userItemContentsStyle}
         autoFocus={false}
         aria-haspopup="true"
         aria-live="assertive"
