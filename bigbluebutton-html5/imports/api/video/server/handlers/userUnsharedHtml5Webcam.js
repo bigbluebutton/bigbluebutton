9e9e85cddbedbac76465e946dfd9b81fc4d4b1f7
Tainan Felipe <tainanfelipe214@gmail.com>
2018-06-18 15:35:27 -0300

    skip share/unshare webcam using if not check

--- bigbluebutton-html5/imports/api/video/server/handlers/userUnsharedHtml5Webcam.js
+++ bigbluebutton-html5/imports/api/video/server/handlers/userUnsharedHtml5Webcam.js
@@ -3,17 +3,18 @@ import { check } from 'meteor/check';
 
 export default function handleUserUnsharedHtml5Webcam({ header, body }, meetingId) {
   const { userId, stream } = body;
-  const isValidStream = Match.Where((stream) => {
-    check(stream, String);
+  const isValidStream = (match) => {
     // Checking if the stream name is a flash one
     const regexp = /^([A-z0-9]+)-([A-z0-9]+)-([A-z0-9]+)(-recorded)?$/;
-    return !regexp.test(stream);
-  });
+    return !regexp.test(match);
+  };
 
   check(header, Object);
   check(meetingId, String);
   check(userId, String);
-  check(stream, isValidStream);
+  check(stream, String);
+
+  if (!isValidStream(stream)) return false;
 
   return unsharedWebcam(meetingId, userId);
 }
