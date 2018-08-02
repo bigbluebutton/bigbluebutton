140968571ea56617d17dce8c6f5404807fc3e8ca
Joao Siebel <joaos_desenv@imdt.com.br>
2018-08-02 14:23:20 -0300

    handle clear public chat for group chat

--- bigbluebutton-html5/imports/api/group-chat-msg/server/modifiers/clearGroupChatMsg.js
+++ bigbluebutton-html5/imports/api/group-chat-msg/server/modifiers/clearGroupChatMsg.js
@@ -2,12 +2,12 @@ import GroupChatMsg from '/imports/api/group-chat-msg';
 import Logger from '/imports/startup/server/logger';
 
 export default function clearGroupChatMsg(meetingId, chatId) {
-  if (meetingId) {
-    return GroupChatMsg.remove({ meetingId }, Logger.info(`Cleared GroupChat (${meetingId})`));
-  }
-
   if (chatId) {
     return GroupChatMsg.remove({ meetingId, chatId }, Logger.info(`Cleared GroupChat (${meetingId}, ${chatId})`));
+  }
+
+  if (meetingId) {
+    return GroupChatMsg.remove({ meetingId }, Logger.info(`Cleared GroupChat (${meetingId})`));
   }
 
   return GroupChatMsg.remove({}, Logger.info('Cleared GroupChat (all)'));
