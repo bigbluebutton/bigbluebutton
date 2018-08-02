48573829695522dba09abdba920fdaec46c74353
Felipe Cecagno <fcecagno@gmail.com>
2018-08-02 17:51:38 -0300

    fsesl wait for freeswitch; change links by depends_on; remove freeswitch ipv6 config files; enable wss-binding element on freeswitch; fix kurento listen only

--- labs/bbb-webrtc-sfu/docker-entrypoint.sh
+++ labs/bbb-webrtc-sfu/docker-entrypoint.sh
@@ -4,4 +4,8 @@ CONTAINER_IP=$(hostname -I | awk '{print $1}')
 
 sed -i "s|^\(localIpAddress\):.*|\1: \"$CONTAINER_IP\"|g" config/production.yml
 
+if [ ! -z "$KURENTO_NAME" ]; then
+    export KURENTO_IP=$(getent hosts $KURENTO_NAME | awk '{ print $1 }')
+fi
+
 npm start
