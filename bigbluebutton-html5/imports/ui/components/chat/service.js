e756ce15489d56cd2f3aaa360093a92d3cf6b77d
Anton Georgiev <anto.georgiev@gmail.com>
2018-08-29 16:00:53 +0000

    comply to lock settings for chat

--- bigbluebutton-html5/imports/ui/components/chat/service.js
+++ bigbluebutton-html5/imports/ui/components/chat/service.js
@@ -105,15 +105,15 @@ const isChatLocked = (receiverID) => {
   const isPublic = receiverID === PUBLIC_CHAT_ID;
 
   const meeting = Meetings.findOne({});
-  const user = Users.findOne({});
+  const user = Users.findOne({ userId: Auth.userID });
 
   if (meeting.lockSettingsProp !== undefined) {
     const isPubChatLocked = meeting.lockSettingsProp.disablePubChat;
     const isPrivChatLocked = meeting.lockSettingsProp.disablePrivChat;
     const isViewer = user.role === 'VIEWER';
 
-    return (isPublic && isPubChatLocked && isViewer && user.locked)
-      || (!isPublic && isPrivChatLocked && isViewer && user.locked);
+    return isViewer && user.locked &&
+      ((isPublic && isPubChatLocked) || (!isPublic && isPrivChatLocked));
   }
 
   return false;
