41f5249f4e62d004e7c00aa6c1137409010202e8
Ghazi Triki <ghazi.triki@riadvice.tn>
2018-10-04 23:52:57 +0100

    Updated SVG conversion to simplify complex files.

---
+++ bbb-common-web/src/main/java/org/bigbluebutton/presentation/handlers/AddNamespaceToSvgHanlder.java
@@ -0,0 +1,9 @@
+package org.bigbluebutton.presentation.handlers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AddNamespaceToSvgHanlder extends AbstractCommandHandler {
+    private static Logger log = LoggerFactory.getLogger(AddNamespaceToSvgHanlder.class);
+
+}
---
+++ bbb-common-web/src/main/java/org/bigbluebutton/presentation/handlers/Png2SvgConvertionHandler.java
@@ -0,0 +1,8 @@
+package org.bigbluebutton.presentation.handlers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Png2SvgConvertionHandler extends AbstractCommandHandler {
+    private static Logger log = LoggerFactory.getLogger(Png2SvgConvertionHandler.class);
+}
