94cdc0aee3130681e761254df333b686fff33b3f
KDSBrowne <kert.browne85@gmail.com>
2018-10-16 22:06:50 +0000

    add sound notification when poll starts

--- bigbluebutton-html5/imports/ui/components/polling/component.jsx
+++ bigbluebutton-html5/imports/ui/components/polling/component.jsx
@@ -18,49 +18,53 @@ const intlMessages = defineMessages({
   },
 });
 
-const Polling = ({ intl, poll, handleVote }) => (
-  <div className={styles.pollingContainer} role="alert">
-    <div className={styles.pollingTitle}>
-      {intl.formatMessage(intlMessages.pollingTitleLabel)}
-    </div>
-    <div className={styles.pollingAnswers}>
-      {poll.answers.map(pollAnswer => (
-        <div
-          key={pollAnswer.id}
-          className={styles.pollButtonWrapper}
-        >
-          <Tooltip
+const Polling = ({ intl, poll, handleVote }) => {
+  this.alert = new Audio(`${Meteor.settings.public.app.basename}/resources/sounds/Poll.mp3`);
+  this.alert.play();
+
+  return (
+    <div className={styles.pollingContainer} role="alert">
+      <div className={styles.pollingTitle}>
+        {intl.formatMessage(intlMessages.pollingTitleLabel)}
+      </div>
+      <div className={styles.pollingAnswers}>
+        {poll.answers.map(pollAnswer => (
+          <div
             key={pollAnswer.id}
-            title={pollAnswer.key}
+            className={styles.pollButtonWrapper}
           >
-            <Button
-              className={styles.pollingButton}
-              color="primary"
-              size="md"
-              label={pollAnswer.key}
-              key={pollAnswer.key}
-              onClick={() => handleVote(poll.pollId, pollAnswer)}
-              aria-labelledby={`pollAnswerLabel${pollAnswer.key}`}
-              aria-describedby={`pollAnswerDesc${pollAnswer.key}`}
-            />
-          </Tooltip>
-          <div
-            className={styles.hidden}
-            id={`pollAnswerLabel${pollAnswer.key}`}
-          >
-            {intl.formatMessage(intlMessages.pollAnswerLabel, { 0: pollAnswer.key })}
+            <Tooltip
+              key={pollAnswer.id}
+              title={pollAnswer.key}
+            >
+              <Button
+                className={styles.pollingButton}
+                color="primary"
+                size="md"
+                label={pollAnswer.key}
+                key={pollAnswer.key}
+                onClick={() => handleVote(poll.pollId, pollAnswer)}
+                aria-labelledby={`pollAnswerLabel${pollAnswer.key}`}
+                aria-describedby={`pollAnswerDesc${pollAnswer.key}`}
+              />
+            </Tooltip>
+            <div
+              className={styles.hidden}
+              id={`pollAnswerLabel${pollAnswer.key}`}
+            >
+              {intl.formatMessage(intlMessages.pollAnswerLabel, { 0: pollAnswer.key })}
+            </div>
+            <div
+              className={styles.hidden}
+              id={`pollAnswerDesc${pollAnswer.key}`}
+            >
+              {intl.formatMessage(intlMessages.pollAnswerDesc, { 0: pollAnswer.key })}
+            </div>
           </div>
-          <div
-            className={styles.hidden}
-            id={`pollAnswerDesc${pollAnswer.key}`}
-          >
-            {intl.formatMessage(intlMessages.pollAnswerDesc, { 0: pollAnswer.key })}
-          </div>
-        </div>
       ))}
-    </div>
-  </div>
-);
+      </div>
+    </div>);
+};
 
 export default injectIntl(injectWbResizeEvent(Polling));
 
