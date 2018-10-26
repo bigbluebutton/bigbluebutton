0551691684887d50616bfbd63775565676ca912a
KDSBrowne <kert.browne85@gmail.com>
2018-10-26 01:40:15 +0000

    edit css styles

--- bigbluebutton-html5/imports/ui/components/user-list/user-list-content/user-polls/container.jsx
+++ bigbluebutton-html5/imports/ui/components/user-list/user-list-content/user-polls/container.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import Users from '/imports/api/users';
-import Polls from '/imports/api/polls';
 import Auth from '/imports/ui/services/auth';
 import { withTracker } from 'meteor/react-meteor-data';
 import UserPolls from './component';
@@ -9,15 +7,4 @@ const UserPollsContainer = ({ ...props }) => <UserPolls {...props} />;
 
 export default withTracker(({ }) => {
   Meteor.subscribe('results', Auth.meetingID);
-
-  // const getUser = userId => Users.findOne({ userId });
-
-  // const currentUser = Users.findOne({ userId: Auth.userID });
-
-  // const currentPoll = Polls.findOne({ meetingId: Auth.meetingID });
-
-  return {
-    // currentUser,
-    // currentPoll,
-  };
 })(UserPollsContainer);
