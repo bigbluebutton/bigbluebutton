ecf0304a010cdb4eccb794b7ca7a285c0e147e2f
KDSBrowne <kert.browne85@gmail.com>
2018-10-31 19:13:19 +0000

    move poll container functions to service

--- bigbluebutton-html5/imports/ui/components/poll/container.jsx
+++ bigbluebutton-html5/imports/ui/components/poll/container.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { makeCall } from '/imports/ui/services/api';
 import { withTracker } from 'meteor/react-meteor-data';
-import Users from '/imports/api/users';
 import Auth from '/imports/ui/services/auth';
 import Presentations from '/imports/api/presentations';
 import PresentationAreaService from '/imports/ui/components/presentation/service';
-import Polls from '/imports/api/polls';
 import Poll from './component';
+import Service from './service';
 
 const PollContainer = ({ ...props }) => <Poll {...props} />;
 
@@ -19,37 +18,19 @@ export default withTracker(({ }) => {
 
   const currentSlide = PresentationAreaService.getCurrentSlide(currentPresentation.podId);
 
-  const currentUser = Users.findOne({ userId: Auth.userID });
-
-  // 'YN' = Yes,No
-  // 'TF' = True,False
-  // 'A-2' = A,B
-  // 'A-3' = A,B,C
-  // 'A-4' = A,B,C,D
-  // 'A-5' = A,B,C,D,E
-  const pollTypes = ['YN', 'TF', 'A-2', 'A-3', 'A-4', 'A-5', 'custom'];
-
   const startPoll = type => makeCall('startPoll', type, currentSlide.id);
 
   const startCustomPoll = (type, answers) => makeCall('startPoll', type, currentSlide.id, answers);
 
-  const stopPoll = () => makeCall('stopPoll', Auth.userId);
-
-  const publishPoll = () => makeCall('publishPoll');
-
-  const currentPoll = Polls.findOne({ meetingId: Auth.meetingID });
-
-  const getUser = userId => Users.findOne({ userId });
-
   return {
     currentSlide,
-    currentUser,
-    pollTypes,
+    currentUser: Service.currentUser(),
+    pollTypes: Service.pollTypes,
     startPoll,
     startCustomPoll,
-    stopPoll,
-    publishPoll,
-    currentPoll,
-    getUser,
+    stopPoll: Service.stopPoll,
+    publishPoll: Service.publishPoll,
+    currentPoll: Service.currentPoll(),
+    getUser: Service.getUser,
   };
 })(PollContainer);
