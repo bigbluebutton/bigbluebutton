acd42835b466f67ce619e66c7ba3402c059b4a1a
Ghazi Triki <ghazi.triki@riadvice.tn>
2018-11-05 21:11:58 +0100

    Updated bbb-common-message project to use sby 1.2.6 and scala 2.12.7

---
+++ bbb-common-message/project/Dependencies.scala
@@ -0,0 +1,55 @@
+package org.bigbluebutton.build
+
+import sbt._
+import Keys._
+
+object Dependencies {
+
+  object Versions {
+    // Scala
+    val scala = "2.12.7"
+    val junit = "4.12"
+    val junitInterface = "0.11"
+    val scalactic = "3.0.3"
+    val pegdown = "1.6.0"
+
+    // Libraries
+    val gson = "2.8.5"
+    val jackson = "2.9.7"
+    val lettuce = "5.1.2.RELEASE"
+
+    // Test
+    val scalaTest = "3.0.5"
+  }
+
+  object Compile {
+    val scalaLibrary = "org.scala-lang" % "scala-library" % Versions.scala
+    val scalaCompiler = "org.scala-lang" % "scala-compiler" % Versions.scala
+
+    val googleGson = "com.google.code.gson" % "gson" % Versions.gson
+    val jacksonModule = "com.fasterxml.jackson.module" %% "jackson-module-scala" % Versions.jackson
+    val lettuceCore = "io.lettuce" % "lettuce-core" % Versions.lettuce
+  }
+
+  object Test {
+    val scalaTest = "org.scalatest" %% "scalatest" % Versions.scalaTest % "test"
+    val junit = "junit" % "junit" % Versions.junit % "test"
+    val junitInteface = "com.novocode" % "junit-interface" % Versions.junitInterface % "test"
+    val scalactic = "org.scalactic" % "scalactic_2.12" % Versions.scalactic % "test"
+    val pegdown = "org.pegdown" % "pegdown" % Versions.pegdown % "test"
+  }
+
+  val testing = Seq(
+    Test.scalaTest,
+    Test.junit,
+    Test.junitInteface,
+    Test.scalactic,
+    Test.pegdown)
+
+  val runtime = Seq(
+    Compile.scalaLibrary,
+    Compile.scalaCompiler,
+    Compile.googleGson,
+    Compile.jacksonModule,
+    Compile.lettuceCore) ++ testing
+}
