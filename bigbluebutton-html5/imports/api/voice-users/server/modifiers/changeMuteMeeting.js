bafa4c15cca8ad97e3ecb224d7b7504f3d5808d8
Bobak Oftadeh <bobakoftadeh@outlook.com>
2018-11-15 14:07:47 -0800

    Added log message when meeting is muted

--- bigbluebutton-html5/imports/api/voice-users/server/modifiers/changeMuteMeeting.js
+++ bigbluebutton-html5/imports/api/voice-users/server/modifiers/changeMuteMeeting.js
@@ -1,5 +1,6 @@
 import Meetings from '/imports/api/meetings';
 import { check } from 'meteor/check';
+import Logger from '/imports/startup/server/logger';
 
 export default function changeMuteMeeting(meetingId, payload) {
   check(meetingId, String);
@@ -18,5 +19,17 @@ export default function changeMuteMeeting(meetingId, payload) {
     },
   };
 
-  return Meetings.upsert(selector, modifier);
+  const cb = (err, numChanged) => {
+    if (err) {
+      Logger.error(`Changing meeting mute status meeting={${meetingId}} ${err}`);
+      return;
+    }
+
+    if (numChanged) {
+      Logger.info(`Changed meeting mute status meeting=${meetingId}`);
+    }
+  };
+
+
+  return Meetings.upsert(selector, modifier, cb);
 }
