dbb2a63d5666ef03e2d62dbf1908b527ee5af56b
Anton Georgiev <anto.georgiev@gmail.com>
2018-12-05 21:38:19 +0000

    handle logs that cannot be stringified/parsed

--- bigbluebutton-html5/imports/ui/components/join-handler/component.jsx
+++ bigbluebutton-html5/imports/ui/components/join-handler/component.jsx
@@ -101,7 +101,14 @@ class JoinHandler extends Component {
         .then((resp) => {
           setLogoutURL(resp.logoutURL);
           if (resp.returncode !== 'FAILED') {
-            logger.info(`User successfully went through main.joinRouteHandler with [${JSON.stringify(resp)}].`);
+            let logString;
+            try {
+              logString = JSON.stringify(resp);
+            } catch (e) {
+              logger.error(`Could not stringify object ${resp}`)
+              logString = resp;
+            }
+            logger.info(`User successfully went through main.joinRouteHandler with [${logString}].`);
             return resolve(resp);
           }
           const e = new Error('Session not found');
