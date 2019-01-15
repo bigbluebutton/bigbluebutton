6231a3a1c55331ace14333b74df7511fa7f48191
Maxim Khlobystov <maxim.khlobystov@gmail.com>
2019-01-15 16:31:36 -0500

    Fix the tooltip error when icon is a target

--- bigbluebutton-html5/imports/ui/components/tooltip/component.jsx
+++ bigbluebutton-html5/imports/ui/components/tooltip/component.jsx
@@ -19,7 +19,8 @@ const defaultProps = {
 
 class Tooltip extends Component {
   static wait(tip, event) {
-    const tooltipTarget = event.target;
+    const tooltipTarget = event.target.lastChild
+      ? event.target : event.target.parentElement.parentElement;
     const expandedEl = tooltipTarget.parentElement.querySelector('[aria-expanded="true"]');
     const isTarget = expandedEl === tooltipTarget;
     if (expandedEl && !isTarget) return;
