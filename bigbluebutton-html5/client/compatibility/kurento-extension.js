544d2ad38ba034ce7c13b84f64ac38b1d80f8cce
prlanzarin <prlanzarin@inf.ufrgs.br>
2019-01-21 15:02:57 -0300

    Improved getDisplayMedia detection

--- bigbluebutton-html5/client/compatibility/kurento-extension.js
+++ bigbluebutton-html5/client/compatibility/kurento-extension.js
@@ -3,7 +3,8 @@ const isOpera = !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
 const isChrome = !!window.chrome && !isOpera;
 const isSafari = navigator.userAgent.indexOf('Safari') >= 0 && !isChrome;
 const isElectron = navigator.userAgent.toLowerCase().indexOf(' electron/') > -1;
-const hasDisplayMedia = typeof navigator.getDisplayMedia === 'function';
+const hasDisplayMedia = (typeof navigator.getDisplayMedia === 'function'
+  || typeof navigator.mediaDevices.getDisplayMedia === 'function');
 const kurentoHandler = null;
 
 Kurento = function (
