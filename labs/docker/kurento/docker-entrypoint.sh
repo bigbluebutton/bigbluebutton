2faf521cd28e2b0ae9389b8fed8891dbf922006e
Felipe Cecagno <fcecagno@gmail.com>
2019-02-01 17:29:27 -0300

    update docker files for kurento

--- labs/docker/kurento/docker-entrypoint.sh
+++ labs/docker/kurento/docker-entrypoint.sh
@@ -3,20 +3,21 @@
 apt-get install -y openh264-gst-plugins-bad-1.5
 
 rm -f /etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini
-touch /etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini
+# Generate WebRtcEndpoint configuration
+echo "stunServerAddress=$STUN_IP" >> /etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini
+echo "stunServerPort=$STUN_PORT" >> /etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini
 
-if [ -n "$KMS_TURN_URL" ]; then
-  echo "turnURL=$KMS_TURN_URL" >> /etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini
+if [ "$TURN_URL" != "" ]; then
+  echo "turnURL=$TURN_URL" >> /etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini
 fi
 
-if [ -n "$KMS_STUN_IP" -a -n "$KMS_STUN_PORT" ]; then
-  # Generate WebRtcEndpoint configuration
-  echo "stunServerAddress=$KMS_STUN_IP" >> /etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini
-  echo "stunServerPort=$KMS_STUN_PORT" >> /etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini
-fi
+rm -f /etc/kurento/modules/kurento/BaseRtpEndpoint.conf.ini
+# Generate BaseRtpEndpoint configuration
+echo "minPort=$RTP_MIN_PORT" >> /etc/kurento/modules/kurento/BaseRtpEndpoint.conf.ini
+echo "maxPort=$RTP_MAX_PORT" >> /etc/kurento/modules/kurento/BaseRtpEndpoint.conf.ini
 
-KMS_CONFIG=$(cat /etc/kurento/kurento.conf.json | sed '/^[ ]*\/\//d' | jq ".mediaServer.net.websocket.port = $PORT")
-echo $KMS_CONFIG > /etc/kurento/kurento.conf.json
+CONFIG=$(cat /etc/kurento/kurento.conf.json | sed '/^[ ]*\/\//d' | jq ".mediaServer.net.websocket.port = $PORT")
+echo $CONFIG > /etc/kurento/kurento.conf.json
 
 # Remove ipv6 local loop until ipv6 is supported
 cat /etc/hosts | sed '/::1/d' | tee /etc/hosts > /dev/null
