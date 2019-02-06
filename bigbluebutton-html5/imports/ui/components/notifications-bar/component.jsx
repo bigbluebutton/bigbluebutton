af3fcb81c95615bcf5ca1add1f8f06d423eea8ad
KDSBrowne <kert.browne85@gmail.com>
2019-02-06 18:38:38 +0000

    prevent SR announcing timer notifications

--- bigbluebutton-html5/imports/ui/components/notifications-bar/component.jsx
+++ bigbluebutton-html5/imports/ui/components/notifications-bar/component.jsx
@@ -17,15 +17,22 @@ const defaultProps = {
   color: 'default',
 };
 
+const isTimedAlert = (alertString) => {
+  // checks if the notification string contains a timer
+  const endsWithTime = alertString.match(/[0-9]*:[0-9]{2}$/i);
+  const hasCounter = alertString.match(/[ ][0-9]*[ ]/i);
+  return !!(hasCounter || endsWithTime);
+};
+
 const NotificationsBar = (props) => {
-  const { color } = props;
+  const { color, children } = props;
 
   return (
     <div
-      role="alert"
+      role={isTimedAlert(children) ? '' : 'alert'}
       className={cx(styles.notificationsBar, styles[color])}
     >
-      {props.children}
+      {children}
     </div>
   );
 };
