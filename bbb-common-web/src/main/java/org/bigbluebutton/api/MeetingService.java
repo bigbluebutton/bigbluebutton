bd3fb3f9f74438a8a2108aa340b6d0bdc23e40cd
Richard Alam <ritzalam@gmail.com>
2019-02-10 10:37:01 -0800

    - retry end meeting callback in 30ms, 1min, 2min, and 5min if failing

--- bbb-common-web/src/main/java/org/bigbluebutton/api/MeetingService.java
+++ bbb-common-web/src/main/java/org/bigbluebutton/api/MeetingService.java
@@ -671,7 +671,7 @@ public class MeetingService implements MessageListener {
         } catch (URISyntaxException e) {
             log.error("URI Syntax error in callback url=[{}]", callbackUrl, e);
         }
-        callbackUrlService.handleMessage(new MeetingEndedEvent(callbackUrl));
+        callbackUrlService.handleMessage(new MeetingEndedEvent(m.getInternalId(), m.getExternalId(), m.getName(), callbackUrl));
       }
 
       processRemoveEndedMeeting(message);
