9ecdff3d810fe95d08e09095fcfd89cf3b876fc9
Richard Alam <ritzalam@gmail.com>
2019-02-26 20:27:52 -0800

    - track if user joins within a certain period of time. Sometimes, nodejs takes    most of the CPU that users takes a long time to load the client and join. This    way, we can detect if there are a number of users failing to join the meeting.

--- akka-bbb-apps/src/main/scala/org/bigbluebutton/core/util/TimeUtil.scala
+++ akka-bbb-apps/src/main/scala/org/bigbluebutton/core/util/TimeUtil.scala
@@ -20,6 +20,10 @@ object TimeUtil {
     TimeUnit.MINUTES.toSeconds(minutes)
   }
 
+  def secondsToMillis(seconds: Long): Long = {
+    TimeUnit.SECONDS.toMillis(seconds)
+  }
+
   def timeNowInMinutes(): Long = TimeUnit.NANOSECONDS.toMinutes(System.nanoTime())
   def timeNowInSeconds(): Long = TimeUnit.NANOSECONDS.toSeconds(System.nanoTime())
   def timeNowInMs(): Long = TimeUnit.NANOSECONDS.toMillis(System.nanoTime())
