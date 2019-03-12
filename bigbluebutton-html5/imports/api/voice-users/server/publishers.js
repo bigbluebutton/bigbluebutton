8ddf7c2b309255d9e8caf2905d998df6687d1e8e
Tainan Felipe <tainanfelipe214@gmail.com>
2019-03-12 09:11:30 -0300

    Fix other users doesn't change talking state

--- bigbluebutton-html5/imports/api/voice-users/server/publishers.js
+++ bigbluebutton-html5/imports/api/voice-users/server/publishers.js
@@ -11,7 +11,12 @@ function voiceUser(credentials) {
 
   Logger.debug(`Publishing Voice User for ${meetingId} ${requesterUserId}`);
 
-  return VoiceUsers.find({ intId: requesterUserId });
+  return VoiceUsers.find({
+    $or: [
+      { intId: requesterUserId },
+      { meetingId },
+    ],
+  });
 }
 
 function publish(...args) {
