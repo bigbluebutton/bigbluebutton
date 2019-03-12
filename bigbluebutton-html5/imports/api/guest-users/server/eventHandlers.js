ba35ab6cc321004a2a542d5d0d9cbca0257c14a7
Tainan Felipe <tainanfelipe214@gmail.com>
2019-03-12 15:39:50 -0300

    Allow only html5 calls responses

--- bigbluebutton-html5/imports/api/guest-users/server/eventHandlers.js
+++ bigbluebutton-html5/imports/api/guest-users/server/eventHandlers.js
@@ -1,7 +1,8 @@
 import RedisPubSub from '/imports/startup/server/redis';
 import handleGuestApproved from './handlers/guestApproved';
+import { processForHTML5ServerOnly } from '/imports/api/common/server/helpers';
 import handleGuestsWaitingForApproval from './handlers/guestsWaitingForApproval';
 
-RedisPubSub.on('GetGuestsWaitingApprovalRespMsg', handleGuestsWaitingForApproval);
+RedisPubSub.on('GetGuestsWaitingApprovalRespMsg', processForHTML5ServerOnly(handleGuestsWaitingForApproval));
 RedisPubSub.on('GuestsWaitingForApprovalEvtMsg', handleGuestsWaitingForApproval);
 RedisPubSub.on('GuestsWaitingApprovedEvtMsg', handleGuestApproved);
