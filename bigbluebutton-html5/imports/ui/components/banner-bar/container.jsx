6d730b700a7f2461677925f2114b6f519b41e1a0
Tainan Felipe <tainanfelipe214@gmail.com>
2019-03-18 15:10:27 -0300

    improve code quality

--- bigbluebutton-html5/imports/ui/components/banner-bar/container.jsx
+++ bigbluebutton-html5/imports/ui/components/banner-bar/container.jsx
@@ -3,6 +3,6 @@ import { withTracker } from 'meteor/react-meteor-data';
 import BannerComponent from './component';
 
 export default withTracker(() => ({
-  color: Session.get('bannerColor') || undefined,
-  text: Session.get('bannerText') || undefined,
+  color: Session.get('bannerColor') || '#0F70D7',
+  text: Session.get('bannerText') || '',
 }))(BannerComponent);
