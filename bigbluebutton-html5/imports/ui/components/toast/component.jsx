be1fc2e840caafdecab835cf879f0087b42ce6df
KDSBrowne <kert.browne85@gmail.com>
2019-04-04 15:29:46 +0000

    improve toast styles

--- bigbluebutton-html5/imports/ui/components/toast/component.jsx
+++ bigbluebutton-html5/imports/ui/components/toast/component.jsx
@@ -23,92 +23,33 @@ const defaultIcons = {
   [toast.TYPE.DEFAULT]: 'about',
 };
 
-const getContentStrings = (msgContent) => {
-  const MIN_LENGTH = 2;
-  const strings = [];
-  if (typeof msgContent === 'string') strings.push(msgContent);
-  if (typeof msgContent === 'object') {
-    const { children } = msgContent.props;
-    if (Object.keys(children).length === MIN_LENGTH) {
-      return getContentStrings(children[1]);
-    }
-    if (Object.keys(children).length > MIN_LENGTH) {
-      Object.entries(children).reduce((acc, [key]) => {
-        if (typeof children[key] === 'string') {
-          strings.push(children[key]);
-        }
-        return null;
-      });
-    }
-  }
-  return strings;
-};
-
-const formatMessageStrings = (messages) => {
-  const _messages = messages;
-  const MAX_MESSAGE_CHARS = 40;
-
-  return _messages.map((msg) => {
-    let formattedMsg = msg;
-    if (msg.length > MAX_MESSAGE_CHARS) {
-      formattedMsg = `${msg.substring(0, MAX_MESSAGE_CHARS)}...`;
-    }
-    return formattedMsg;
-  });
-};
-
-const setContentStrings = (msgContent, msgStrings) => {
-  const _msgContent = msgContent;
-  const msgObj = _msgContent.props.children[1];
-  let stringIndex = 0;
-  Object.entries(msgObj.props.children).reduce((acc, [key]) => {
-    if (typeof msgObj.props.children[key] === 'string') {
-      msgObj.props.children[key] = msgStrings[stringIndex];
-      stringIndex += 1;
-    }
-    return null;
-  });
-  _msgContent.props.children[1] = msgObj;
-  return _msgContent;
-};
-
 const Toast = ({
   icon,
   type,
   message,
   content,
   small,
-}) => {
-  if (!content) return null;
-  const formattedContent = content;
-  const { children } = formattedContent.props;
-
-  let strings = getContentStrings(children);
-  strings = formatMessageStrings(strings);
-  formattedContent.props.children = setContentStrings(children, strings);
-
-  return (
-    <div
-      className={cx(styles.toastContainer, small ? styles.smallToastContainer : null)}
-      role="alert"
-    >
-      <div className={styles[type]}>
-        <div className={cx(styles.icon, small ? styles.smallIcon : null)}>
-          <Icon iconName={icon || defaultIcons[type]} />
-        </div>
-        <div className={cx(styles.message, small ? styles.smallMessage : null)}>
-          <span>{message}</span>
-        </div>
+}) => (
+  <div
+    className={cx(styles.toastContainer, small ? styles.smallToastContainer : null)}
+    role="alert"
+  >
+    <div className={styles[type]}>
+      <div className={cx(styles.icon, small ? styles.smallIcon : null)}>
+        <Icon iconName={icon || defaultIcons[type]} />
       </div>
-      <div className={styles.backgroundColorInherit}>
-        <div className={styles.separator} />
-        <div className={styles.backgroundColorInherit}>
-          {formattedContent}
-        </div>
+      <div className={cx(styles.message, small ? styles.smallMessage : null)}>
+        <span>{message}</span>
       </div>
     </div>
-  );
-};
+    <div className={styles.backgroundColorInherit}>
+      <div className={styles.separator} />
+      <div className={styles.backgroundColorInherit}>
+        {content}
+      </div>
+    </div>
+  </div>
+);
 
 export default Toast;
 
