17b8229afc548355aeed2612008252c961e2141a
Richard Alam <ritzalam@gmail.com>
2019-04-05 11:35:05 -0700

    - add redis password to fsesl

--- akka-bbb-fsesl/src/main/scala/org/bigbluebutton/SystemConfiguration.scala
+++ akka-bbb-fsesl/src/main/scala/org/bigbluebutton/SystemConfiguration.scala
@@ -1,10 +1,17 @@
 package org.bigbluebutton
 
 import scala.util.Try
+import com.typesafe.config.ConfigFactory
 
-import org.bigbluebutton.common2.redis.RedisConfiguration
+trait SystemConfiguration {
+  val config = ConfigFactory.load()
 
-trait SystemConfiguration extends RedisConfiguration {
+  // Redis server configuration
+  lazy val redisHost = Try(config.getString("redis.host")).getOrElse("127.0.0.1")
+  lazy val redisPort = Try(config.getInt("redis.port")).getOrElse(6379)
+  lazy val redisPassword = Try(config.getString("redis.password")).getOrElse("")
+  lazy val redisExpireKey = Try(config.getInt("redis.keyExpiry")).getOrElse(1209600)
+
   lazy val eslHost = Try(config.getString("freeswitch.esl.host")).getOrElse("127.0.0.1")
   lazy val eslPort = Try(config.getInt("freeswitch.esl.port")).getOrElse(8021)
   lazy val eslPassword = Try(config.getString("freeswitch.esl.password")).getOrElse("ClueCon")
@@ -12,4 +19,6 @@ trait SystemConfiguration extends RedisConfiguration {
 
   lazy val toFsAppsJsonChannel = Try(config.getString("eventBus.toFsAppsChannel")).getOrElse("to-fs-apps-json-channel")
   lazy val fromFsAppsJsonChannel = Try(config.getString("eventBus.fromFsAppsChannel")).getOrElse("from-fs-apps-json-channel")
+  lazy val toVoiceConfRedisChannel = Try(config.getString("redis.toVoiceConfRedisChannel")).getOrElse("to-voice-conf-redis-channel")
+  lazy val fromVoiceConfRedisChannel = Try(config.getString("redis.fromVoiceConfRedisChannel")).getOrElse("from-voice-conf-redis-channel")
 }
