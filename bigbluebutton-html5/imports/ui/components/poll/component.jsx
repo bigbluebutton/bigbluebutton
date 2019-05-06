adea7b90e6207b5ff5169c81980f65a0d07eaab8
KDSBrowne <kert.browne85@gmail.com>
2019-05-06 20:18:23 +0000

    move focus to poll panel when open

--- bigbluebutton-html5/imports/ui/components/poll/component.jsx
+++ bigbluebutton-html5/imports/ui/components/poll/component.jsx
@@ -6,6 +6,7 @@ import { withModalMounter } from '/imports/ui/components/modal/service';
 import _ from 'lodash';
 import { Session } from 'meteor/session';
 import Button from '/imports/ui/components/button/component';
+import { findDOMNode } from 'react-dom';
 import LiveResult from './live-result/component';
 import { styles } from './styles.scss';
 
@@ -105,6 +106,10 @@ class Poll extends Component {
     this.renderInputFields = this.renderInputFields.bind(this);
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleBackClick = this.handleBackClick.bind(this);
+  }
+
+  componentDidMount() {
+    findDOMNode(this.hideBtn).focus();
   }
 
   componentDidUpdate() {
@@ -321,6 +326,7 @@ class Poll extends Component {
       <div>
         <header className={styles.header}>
           <Button
+            ref={(node) => { this.hideBtn = node; }}
             tabIndex={0}
             label={intl.formatMessage(intlMessages.pollPaneTitle)}
             icon="left_arrow"
