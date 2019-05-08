77bc6aa9dc577ab909b6b40ef2f26592a909f508
Diego Benetti <diegob@imdt.com.br>
2019-05-08 14:15:25 -0300

    Fixes the duplicated messages when meeting is ended

--- bigbluebutton-html5/imports/api/meetings/server/handlers/meetingEnd.js
+++ bigbluebutton-html5/imports/api/meetings/server/handlers/meetingEnd.js
@@ -4,28 +4,23 @@ import Meetings from '/imports/api/meetings';
 import Breakouts from '/imports/api/breakouts';
 import setConnectionStatus from '/imports/api/users/server/modifiers/setConnectionStatus';
 import Users from '/imports/api/users/';
+import Logger from '/imports/startup/server/logger';
 
 export default function handleMeetingEnd({ body }, meetingId) {
   check(meetingId, String);
 
-  const cb = () => {
-    Users.find({ meetingId }).fetch().map(user => setConnectionStatus(user.meetingId, user.userId, 'offline'));
-    return Meteor.setTimeout(() => { meetingHasEnded(meetingId); }, 10000);
+  const cb = (err, num) => {
+    if (err) {
+      Logger.error(`Meeting endind error: ${err}`);
+      return;
+    }
+    if (num) {
+      Users.find({ meetingId })
+        .fetch().map(user => setConnectionStatus(user.meetingId, user.userId, 'offline'));
+      Meteor.setTimeout(() => { meetingHasEnded(meetingId); }, 10000);
+    }
   };
 
-  Meetings.update({
-    meetingId,
-  }, {
-    $set: {
-      meetingEnded: true,
-    },
-  }, cb);
-
-  Breakouts.update({
-    parentMeetingId: meetingId,
-  }, {
-    $set: {
-      meetingEnded: true,
-    },
-  }, cb);
+  Meetings.update({ meetingId }, { $set: { meetingEnded: true } }, cb);
+  Breakouts.update({ parentMeetingId: meetingId }, { $set: { meetingEnded: true } }, cb);
 }
