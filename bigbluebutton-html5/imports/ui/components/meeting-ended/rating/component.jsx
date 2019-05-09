c98c1ad5a7b7e8b669e7e332d84cb694a95feb69
KDSBrowne <kert.browne85@gmail.com>
2019-05-09 15:35:52 +0000

    add formatted message

--- bigbluebutton-html5/imports/ui/components/meeting-ended/rating/component.jsx
+++ bigbluebutton-html5/imports/ui/components/meeting-ended/rating/component.jsx
@@ -9,6 +9,10 @@ const intlMessages = defineMessages({
     id: 'app.meeting-ended.rating.legendLabel',
     description: 'label for star feedback legend',
   },
+  starLabel: {
+    id: 'app.meeting-ended.rating.starLabel',
+    description: 'label for feedback stars',
+  },
 });
 
 class Rating extends Component {
@@ -34,12 +38,27 @@ class Rating extends Component {
         <fieldset>
           <legend>{intl.formatMessage(intlMessages.legendTitle)}</legend>
           {
-              _.range(num)
-                .map(i => [(<input type="radio" id={`${i + 1}star`} name="rating" value={i + 1} key={_.uniqueId('star-')} onChange={() => this.clickStar(i + 1)} />),
-                  (<label htmlFor={`${i + 1}star`} title={`${i + 1} star`} key={_.uniqueId('star-')}>
-                    {`${i + 1} star`}
-                   </label>),
-                ]).reverse()
+            _.range(num)
+              .map(i => [
+                (
+                  <input
+                    type="radio"
+                    id={`${i + 1}star`}
+                    name="rating"
+                    value={i + 1}
+                    key={_.uniqueId('star-')}
+                    onChange={() => this.clickStar(i + 1)}
+                  />
+                ),
+                (
+                  <label
+                    htmlFor={`${i + 1}star`}
+                    key={_.uniqueId('star-')}
+                  >
+                    {`${i + 1} ${intl.formatMessage(intlMessages.starLabel)}`}
+                  </label>
+                ),
+              ]).reverse()
           }
         </fieldset>
       </div>
