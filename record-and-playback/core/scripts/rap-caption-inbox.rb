c6e09f52a1c45f91b348dc84cd7f4837c9e7ce56
Calvin Walton <calvin.walton@kepstin.ca>
2019-05-10 14:06:23 -0400

    RaP Caption inbox: read uploaded caption json file
    Some additional validation/normalization is added for the language tag,
    and some cleanup has been done for logging.

--- record-and-playback/core/scripts/rap-caption-inbox.rb
+++ record-and-playback/core/scripts/rap-caption-inbox.rb
@@ -18,24 +18,22 @@
 # along with BigBlueButton.  If not, see <http://www.gnu.org/licenses/>.
 
 require "rubygems"
+require "bundler/setup"
+
+require File.expand_path("../../lib/recordandplayback", __FILE__)
+
+require "journald-logger"
+require "locale"
 require "rb-inotify"
 require "yaml"
-require "logger"
 
-props = YAML::load(File.open("bigbluebutton.yml"))
+# Read configuration and set up logger
+
+props = YAML::load(File.open(File.expand_path("../bigbluebutton.yml", __FILE__)))
 
 log_dir = props["log_dir"]
-logger = Logger.new(STDERR)
-logger.level = Logger::INFO
-# TODO: I need to set the Bigbluebutton logger here if I load the other rap code
-
-def handle_caption_file(filename)
-  # There's a possible race condition where we can be notified twice for a new
-  # file. That's fine, just do nothing the second time.
-  return unless File.exist?(filename)
-
-  logger.info("Found new caption index file #{filename}")
-end
+logger = Journald::Logger.new("bbb-rap-caption-inbox")
+BigBlueButton.logger = logger
 
 captions_dir = props["captions_dir"]
 unless captions_dir
@@ -43,6 +41,64 @@ unless captions_dir
   exit(1)
 end
 captions_inbox_dir = File.join(captions_dir, "inbox")
+
+# Internal error classes
+
+# Base class for internal errors
+class CaptionError < StandardError
+end
+
+# Indicates that uploaded caption files are invalid (unrecoverable)
+class InvalidCaptionError < CaptionError
+end
+
+# Implementation
+
+def caption_file_notify(json_filename)
+  # There's a possible race condition where we can be notified twice for a new
+  # file. That's fine, just do nothing the second time.
+  return unless File.exist?(json_filename)
+
+  logger.info("Found new caption index file #{json_filename}")
+
+  # TODO: Rather than do anything directly in this script, it should create a
+  # queue job (resque?) that does the actual work.
+
+  new_caption_info = File.open(json_filename) { |file| JSON.parse(file) }
+  logger.tag(record_id: new_caption_info["record_id"]) do
+
+    # TODO: This is racy if multiple tools are editing the captions.json file
+    captions_info = begin
+      File.open(
+        File.join(captions_dir, new_caption_info["record_id"], "captions.json")
+      ) do |file|
+        JSON.parse(file)
+      end
+    rescue
+      # No captions file or cannot be read, assume none present
+      []
+    end
+
+    langtag = Locale::Tag::Rfc.parse(new_caption_info["lang"])
+    raise InvalidCaptionError, "Language tag is not well-formed" unless langtag
+
+    # Remove the info for an existing matching track
+    captions_info.delete_if do |caption_info|
+      caption_info["lang"] == new_caption_info["lang"] &&
+        caption_info["kind"] == new_caption_info["kind"]
+    end
+
+    captions_info << {
+      "kind"   => new_caption_info["kind"],
+      "label"  => new_caption_info["label"],
+      "lang"   => langtag.to_s,
+      "source" => "upload",
+    }
+
+    dest_filename = "#{captions_info["kind"]}_#{captions_info["lang"]}.vtt"
+
+  end
+end
 
 logger.info("Setting up inotify watch on #{captions_inbox_dir}")
 notifier = INotify::Notifier.new
@@ -54,7 +110,7 @@ end
 
 logger.info("Checking for missed/skipped caption files")
 Dir.glob(File.join(captions_inbox_dir, "*-track.json")).each do |filename|
-  handle_caption_file(filename)
+  caption_file_notify(filename)
 end
 
 logger.info("Waiting for new caption files...")
