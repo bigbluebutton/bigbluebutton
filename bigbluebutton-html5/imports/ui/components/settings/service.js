89898ae09ee121c93844c290929e6b8b0e9badc4
KDSBrowne <kert.browne85@gmail.com>
2019-05-22 18:54:30 +0000

    fix console error from state update on unmounted component

--- bigbluebutton-html5/imports/ui/components/settings/service.js
+++ bigbluebutton-html5/imports/ui/components/settings/service.js
@@ -3,6 +3,7 @@ import Captions from '/imports/api/captions';
 import Auth from '/imports/ui/services/auth';
 import _ from 'lodash';
 import Settings from '/imports/ui/services/settings';
+import { notify } from '/imports/ui/services/notification';
 
 const getClosedCaptionLocales = () => {
   // list of unique locales in the Captions Collection
@@ -22,9 +23,20 @@ const getUserRoles = () => {
   return user.role;
 };
 
-const updateSettings = (obj) => {
+const updateSettings = (obj, msg) => {
   Object.keys(obj).forEach(k => (Settings[k] = obj[k]));
   Settings.save();
+
+  if (msg) {
+    // prevents React state update on unmounted component
+    setTimeout(() => {
+      notify(
+        msg,
+        'info',
+        'settings',
+      );
+    }, 0);
+  }
 };
 
 const getAvailableLocales = () => fetch('/html5client/locales').then(locales => locales.json());
