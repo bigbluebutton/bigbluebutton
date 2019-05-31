c4ac5ef7c91ea33aef03eb163b7cfea8134c7aa0
Pedro Beschorner Marin <pedrobmarin@gmail.com>
2019-05-31 20:02:33 +0000

    Shared notes unread indicator

--- bigbluebutton-html5/imports/api/captions/server/modifiers/updatePad.js
+++ bigbluebutton-html5/imports/api/captions/server/modifiers/updatePad.js
@@ -3,7 +3,7 @@ import Logger from '/imports/startup/server/logger';
 import editCaptions from '/imports/api/captions/server/methods/editCaptions';
 import { check } from 'meteor/check';
 
-export default function padUpdate(padId, data, revs) {
+export default function updatePad(padId, data, revs) {
   check(padId, String);
   check(data, String);
   check(revs, Number);
--- bigbluebutton-html5/imports/api/note/server/helpers.js
+++ bigbluebutton-html5/imports/api/note/server/helpers.js
@@ -4,6 +4,7 @@ import { hashFNV32a } from '/imports/api/common/server/helpers';
 const ETHERPAD = Meteor.settings.private.etherpad;
 const NOTE_CONFIG = Meteor.settings.public.note;
 const BASE_URL = `http://${ETHERPAD.host}:${ETHERPAD.port}/api/${ETHERPAD.version}`;
+const TOKEN = '_';
 
 const createPadURL = padId => `${BASE_URL}/createPad?apikey=${ETHERPAD.apikey}&padID=${padId}`;
 
@@ -26,10 +27,26 @@ const getDataFromResponse = (data, key) => {
   return null;
 };
 
+const isNotePad = (padId) => {
+  return padId.search(TOKEN);
+};
+
+const processForNotePadOnly = fn => (message, ...args) => {
+  const { body } = message;
+  const { pad } = body;
+  const { id } = pad;
+
+  check(id, String);
+
+  if (isNotePad(id)) return fn(message, ...args);
+  return () => {};
+};
+
 export {
   generateNoteId,
   createPadURL,
   getReadOnlyIdURL,
   isEnabled,
   getDataFromResponse,
+  processForNotePadOnly,
 };
