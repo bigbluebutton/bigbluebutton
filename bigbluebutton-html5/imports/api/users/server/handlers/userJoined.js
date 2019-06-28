0de1d1d1297d4dbc60f8e630f29b69d991d5e329
Chad Pilkey <capilkey@gmail.com>
2019-06-28 18:50:57 +0000

    fix regression from #7606, handle roles correctly

--- bigbluebutton-html5/imports/api/users/server/handlers/userJoined.js
+++ bigbluebutton-html5/imports/api/users/server/handlers/userJoined.js
@@ -5,7 +5,6 @@ import addUser from '../modifiers/addUser';
 export default function handleUserJoined({ body }, meetingId) {
   const user = body;
 
-  user.moderator = user.moderator || false;
   check(user, Object);
 
   return addUser(meetingId, user);
