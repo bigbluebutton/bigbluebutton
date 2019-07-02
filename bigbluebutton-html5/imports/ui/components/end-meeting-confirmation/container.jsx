b01c0485ef7172837187139f55ed4fc04cc8f9f4
Maxim Khlobystov <maxim.khlobystov@gmail.com>
2019-07-02 16:51:41 -0400

    Make sure only moderators can do Meteor.call to end meeting when confirming in the modal

--- bigbluebutton-html5/imports/ui/components/end-meeting-confirmation/container.jsx
+++ bigbluebutton-html5/imports/ui/components/end-meeting-confirmation/container.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 import { withTracker } from 'meteor/react-meteor-data';
 import { withModalMounter } from '/imports/ui/components/modal/service';
 import { makeCall } from '/imports/ui/services/api';
+import Auth from '/imports/ui/services/auth';
+import Users from '/imports/api/users';
 import EndMeetingComponent from './component';
 
 const EndMeetingContainer = props => <EndMeetingComponent {...props} />;
+
+const ROLE_MODERATOR = Meteor.settings.public.user.role_moderator;
 
 export default withModalMounter(withTracker(({ mountModal }) => ({
   closeModal() {
@@ -12,7 +16,8 @@ export default withModalMounter(withTracker(({ mountModal }) => ({
   },
 
   endMeeting: () => {
-    makeCall('endMeeting');
+    const isUserModerator = Users.findOne({ userId: Auth.userID }).role === ROLE_MODERATOR;
+    if (isUserModerator) makeCall('endMeeting');
     mountModal(null);
   },
 
