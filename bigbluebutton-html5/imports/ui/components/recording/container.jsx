61aa1f6d7b4c4f33bbc266c44d5371bca199ac77
Maxim Khlobystov <maxim.khlobystov@gmail.com>
2019-07-02 16:50:42 -0400

    Make sure only moderators can do Meteor.call to toggle recordings when confirming the toggle in the modal

--- bigbluebutton-html5/imports/ui/components/recording/container.jsx
+++ bigbluebutton-html5/imports/ui/components/recording/container.jsx
@@ -4,9 +4,12 @@ import { withModalMounter } from '/imports/ui/components/modal/service';
 import { makeCall } from '/imports/ui/services/api';
 import Meetings from '/imports/api/meetings';
 import Auth from '/imports/ui/services/auth';
+import Users from '/imports/api/users';
 import RecordingComponent from './component';
 
 const RecordingContainer = props => <RecordingComponent {...props} />;
+
+const ROLE_MODERATOR = Meteor.settings.public.user.role_moderator;
 
 export default withModalMounter(withTracker(({ mountModal }) => {
   const { recording, time } = Meetings.findOne({ meetingId: Auth.meetingID }).recordProp;
@@ -15,7 +18,9 @@ export default withModalMounter(withTracker(({ mountModal }) => {
     closeModal: () => mountModal(null),
 
     toggleRecording: () => {
-      makeCall('toggleRecording');
+      const isUserModerator = Users.findOne({ userId: Auth.userID }).role === ROLE_MODERATOR;
+      if (isUserModerator) makeCall('toggleRecording');
+
       mountModal(null);
     },
 
