e22d61b2e122e68c0400b81ad769aefbceaacfb1
Anton Georgiev <anto.georgiev@gmail.com>
2019-08-01 12:47:45 -0400

    removed .log() function in favour of logger

--- bigbluebutton-html5/imports/ui/services/api/index.js
+++ bigbluebutton-html5/imports/ui/services/api/index.js
@@ -1,24 +1,6 @@
 import Auth from '/imports/ui/services/auth';
 import { check } from 'meteor/check';
-import { notify } from '/imports/ui/services/notification';
 import logger from '/imports/startup/client/logger';
-
-export function log(type = 'error', message, ...args) {
-  const logContents = { ...args };
-  const topic = logContents[0] ? logContents[0].topic : null;
-
-  const messageOrStack = message.stack || message.message || JSON.stringify(message);
-  console.debug(`CLIENT LOG (${topic ? `${type.toUpperCase()}.${topic}` : type.toUpperCase()}): `, messageOrStack, ...args);
-
-  logger[type]({
-    logCode: 'services_api_log',
-    extraInfo: {
-      message,
-      logContents,
-      attemptForUserInfo: Auth.fullInfo,
-    },
-  }, 'Client log from ui/services/api/index.js');
-}
 
 /**
  * Send the request to the server via Meteor.call and don't treat errors.
@@ -43,24 +25,14 @@ export function makeCall(name, ...args) {
         resolve(result);
       });
     } else {
-      const error = new Error('Meteor was not connected');
-      log('error', error);
-      reject(error);
+      reject(() => logger.warn({
+        logCode: 'servicesapiindex_makeCall',
+        extraInfo: {
+          attemptForUserInfo: Auth.fullInfo,
+          name,
+          ...args,
+        },
+      }, 'Connection to Meteor was interrupted.'));
     }
-  });
-}
-
-/**
- * Send the request to the server via Meteor.call and treat the error to a default callback.
- *
- * @param {string} name
- * @param {any} args
- * @see https://docs.meteor.com/api/methods.html#Meteor-call
- * @return {Promise}
- */
-export function call(name, ...args) {
-  return makeCall(name, ...args).catch((e) => {
-    notify(`Ops! Error while executing ${name}`, 'error');
-    throw e;
   });
 }
