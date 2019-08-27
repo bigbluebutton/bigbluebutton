ef29a56ed2b64d90bc911768f55bd315719d5035
Lucas Zawacki <lfzawacki@gmail.com>
2019-08-27 16:11:59 -0300

    Call arcPlayer getCurrentTime promise everytime as a hacky way to prevent player desync

--- bigbluebutton-html5/imports/ui/components/external-video-player/custom-players/arc-player.jsx
+++ bigbluebutton-html5/imports/ui/components/external-video-player/custom-players/arc-player.jsx
@@ -158,6 +158,10 @@ export class ArcPlayer extends Component {
   }
 
   getCurrentTime () {
+    this.player.getCurrentTime().then((t) => {
+      this.updateCurrentTime(t.data);
+    });
+
     return this.currentTime;
   }
 
