9e41cbf877a958b1b247306475393e767c396f27
Anton Georgiev <anto.georgiev@gmail.com>
2019-09-06 18:08:37 -0400

    remove incorrectly duplicated import

--- bigbluebutton-html5/imports/ui/components/user-list/user-list-content/user-participants/user-list-item/container.jsx
+++ bigbluebutton-html5/imports/ui/components/user-list/user-list-content/user-participants/user-list-item/container.jsx
@@ -5,7 +5,6 @@ import Meetings from '/imports/api/meetings';
 import Auth from '/imports/ui/services/auth';
 import UserListItem from './component';
 import UserListService from '/imports/ui/components/user-list/service';
-import Service from '../../../service';
 
 const UserListItemContainer = props => <UserListItem {...props} />;
 const isMe = intId => intId === Auth.userID;
@@ -31,10 +30,10 @@ export default withTracker(({ user }) => {
     changeRole: UserListService.changeRole,
     assignPresenter: UserListService.assignPresenter,
     getAvailableActions: UserListService.getAvailableActions,
-    normalizeEmojiName: Service.normalizeEmojiName,
-    getGroupChatPrivate: Service.getGroupChatPrivate,
-    getEmojiList: Service.getEmojiList(),
-    getEmoji: Service.getEmoji(),
-    hasPrivateChatBetweenUsers: Service.hasPrivateChatBetweenUsers,
+    normalizeEmojiName: UserListService.normalizeEmojiName,
+    getGroupChatPrivate: UserListService.getGroupChatPrivate,
+    getEmojiList: UserListService.getEmojiList(),
+    getEmoji: UserListService.getEmoji(),
+    hasPrivateChatBetweenUsers: UserListService.hasPrivateChatBetweenUsers,
   };
 })(UserListItemContainer);
