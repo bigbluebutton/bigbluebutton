5686bb223ae8050c42a2524283fee753d1dc90a4
Vitor Mateus de Almeida <vitormateusalmeida@gmail.com>
2019-10-31 18:11:26 -0300

    Fix webcam size when minimize presentation

--- bigbluebutton-html5/imports/ui/components/media/container.jsx
+++ bigbluebutton-html5/imports/ui/components/media/container.jsx
@@ -15,6 +15,7 @@ import ScreenshareContainer from '../screenshare/container';
 import DefaultContent from '../presentation/default-content/component';
 import ExternalVideoContainer from '../external-video-player/container';
 import { getVideoId } from '../external-video-player/service';
+import Storage from '../../services/storage/session';
 
 const LAYOUT_CONFIG = Meteor.settings.public.layout;
 const KURENTO_CONFIG = Meteor.settings.public.kurento;
@@ -147,6 +148,8 @@ export default withModalMounter(withTracker(() => {
       />
     );
   }
+
+  data.webcamPlacement = Storage.getItem('webcamPlacement');
 
   MediaContainer.propTypes = propTypes;
   return data;
