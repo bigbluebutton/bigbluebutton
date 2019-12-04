d94cc24fca2436470f72347703a94e21caef0557
Ghazi Triki <ghazi.triki@riadvice.tn>
2019-12-04 17:18:49 -0300

    Upgrade akka-bbb-apps build tool and dependencies versions.

--- akka-bbb-apps/project/Dependencies.scala
+++ akka-bbb-apps/project/Dependencies.scala
@@ -7,30 +7,29 @@ object Dependencies {
 
   object Versions {
     // Scala
-    val scala = "2.12.8"
-    val junit = "4.12"
-    val junitInterface = "0.11"
-    val scalactic = "3.0.3"
+    val scala = "2.12.10"
+    val junit = "5.6.0-M1"
+    val scalactic = "3.1.0"
 
     // Libraries
-    val akkaVersion = "2.5.19"
-    val gson = "2.8.5"
-    val jackson = "2.9.7"
+    val akkaVersion = "2.6.0"
+    val gson = "2.8.6"
+    val jackson = "2.10.1"
     val logback = "1.2.3"
-    val quicklens = "1.4.11"
-    val spray = "1.3.4"
+    val quicklens = "1.4.12"
+    val spray = "1.3.5"
 
     // Apache Commons
-    val lang = "3.8.1"
-    val codec = "1.11"
+    val lang = "3.9"
+    val codec = "1.13"
 
     // BigBlueButton
     val bbbCommons = "0.0.20-SNAPSHOT"
 
     // Test
-    val scalaTest = "3.0.5"
-    val mockito = "2.23.0"
-    val akkaTestKit = "2.5.18"
+    val scalaTest = "3.1.0"
+    val mockito = "3.2.0"
+    val akkaTestKit = "2.6.0"
   }
 
   object Compile {
@@ -49,13 +48,12 @@ object Dependencies {
 
     val apacheLang = "org.apache.commons" % "commons-lang3" % Versions.lang
 
-    val bbbCommons = "org.bigbluebutton" % "bbb-common-message_2.12" % Versions.bbbCommons excludeAll (
-      ExclusionRule(organization = "org.red5"))
+    val bbbCommons = "org.bigbluebutton" % "bbb-common-message_2.12" % Versions.bbbCommons
   }
 
   object Test {
     val scalaTest = "org.scalatest" %% "scalatest" % Versions.scalaTest % "test"
-    val junit = "junit" % "junit" % Versions.junit % "test"
+    val junit = "org.junit.jupiter" % "junit-jupiter-api" % Versions.junit % "test"
     val mockitoCore = "org.mockito" % "mockito-core" % Versions.mockito % "test"
     val scalactic = "org.scalactic" % "scalactic_2.12" % Versions.scalactic % "test"
     val akkaTestKit = "com.typesafe.akka" %% "akka-testkit" % Versions.akkaTestKit % "test"
