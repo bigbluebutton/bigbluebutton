9b2240ae0b62f5f7d3eb6ca7a4aaa0f3587bd217
Ghazi Triki <ghazi.triki@riadvice.tn>
2019-12-05 18:17:51 -0300

    Minor code improvements.

--- bbb-common-web/src/main/java/org/bigbluebutton/common/messages/BbbAppsIsAliveMessage.java
+++ bbb-common-web/src/main/java/org/bigbluebutton/common/messages/BbbAppsIsAliveMessage.java
@@ -30,8 +30,7 @@ public class BbbAppsIsAliveMessage {
 	}
 
 	public static BbbAppsIsAliveMessage fromJson(String message) {
-		JsonParser parser = new JsonParser();
-		JsonObject obj = (JsonObject) parser.parse(message);
+		JsonObject obj = (JsonObject) JsonParser.parseString(message);
 		if (obj.has("header") && obj.has("payload")) {
 			JsonObject header = (JsonObject) obj.get("header");
 			JsonObject payload = (JsonObject) obj.get("payload");
