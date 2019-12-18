4e1d55efd2726e527626ed612b7e5872c00bf605
Anton Georgiev <anto.georgiev@gmail.com>
2019-12-18 16:40:04 -0500

    Prevent parallel screenshare and external-video

--- bigbluebutton-html5/imports/api/screenshare/server/handlers/screenshareStarted.js
+++ bigbluebutton-html5/imports/api/screenshare/server/handlers/screenshareStarted.js
@@ -1,9 +1,16 @@
 import { check } from 'meteor/check';
+import Meetings from '/imports/api/meetings';
 import addScreenshare from '../modifiers/addScreenshare';
+import Logger from '/imports/startup/server/logger';
 
 export default function handleScreenshareStarted({ body }, meetingId) {
   check(meetingId, String);
   check(body, Object);
 
+  const meeting = Meetings.findOne({ meetingId });
+  if (meeting && meeting.externalVideoUrl) {
+    Logger.info(`ScreenshareStarted: There is external video being shared. Stopping it due to presenter change, ${meeting.externalVideoUrl}`);
+    Meetings.update({ meetingId }, { $set: { externalVideoUrl: null } });
+  }
   return addScreenshare(meetingId, body);
 }
