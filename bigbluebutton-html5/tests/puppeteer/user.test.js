c306034403082017cc7264e3e72594bf314c76c6
Mohamed Amine Ben Salah <damineone@gmail.com>
2020-02-13 17:00:33 -0300

    return commented code

--- bigbluebutton-html5/tests/puppeteer/user.test.js
+++ bigbluebutton-html5/tests/puppeteer/user.test.js
@@ -3,19 +3,19 @@ const Status = require('./user/status');
 const MultiUsers = require('./user/multiusers');
 
 describe('User', () => {
-  // test('Change status', async () => {
-  //   const test = new Status();
-  //   let response;
-  //   try {
-  //     await test.init();
-  //     response = await test.test();
-  //   } catch (e) {
-  //     console.log(e);
-  //   } finally {
-  //     await test.close();
-  //   }
-  //   expect(response).toBe(true);
-  // });
+  test('Change status', async () => {
+    const test = new Status();
+    let response;
+    try {
+      await test.init();
+      response = await test.test();
+    } catch (e) {
+      console.log(e);
+    } finally {
+      await test.close();
+    }
+    expect(response).toBe(true);
+  });
 
   test('Multi user presence check', async () => {
     const test = new MultiUsers();
