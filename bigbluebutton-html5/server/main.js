ecc02915d69a82f4b6bac07f48d919f4658fcb99
Anton Georgiev <anto.georgiev@gmail.com>
2020-02-26 10:24:38 -0500

    remove meteor package jquery and lodash

--- bigbluebutton-html5/server/main.js
+++ bigbluebutton-html5/server/main.js
@@ -32,3 +32,14 @@ import '/imports/api/local-settings/server';
 import '/imports/api/log-client/server';
 import '/imports/api/common/server/helpers';
 import '/imports/startup/server/logger';
+
+// Needed for Atmosphere package RocketChat/meteor-streamer
+// It is out of date and was written when Meteor contained lodash
+// package. However, we now import lodash as an npm package
+// in order to control versions, update flexibly, etc..
+// Setting the global._ to utilize the npm lodash package is an interim fix
+// and its introduction was inspired by
+// https://github.com/RocketChat/meteor-streamer/issues/40#issuecomment-497627893
+import _ from 'lodash';
+
+global._ = _;
