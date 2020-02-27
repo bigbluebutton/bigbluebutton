92a1fba0aececd6e4d8d4e083b9334f2b496d51b
Lucas Zawacki <lfzawacki@gmail.com>
2020-02-27 16:42:53 -0300

    Use extractCredentials in stopWatchingExternalVideo thus fixing it

--- bigbluebutton-html5/imports/api/external-videos/server/methods/stopWatchingExternalVideo.js
+++ bigbluebutton-html5/imports/api/external-videos/server/methods/stopWatchingExternalVideo.js
@@ -2,11 +2,14 @@ import { Meteor } from 'meteor/meteor';
 import Logger from '/imports/startup/server/logger';
 import Meetings from '/imports/api/meetings';
 import RedisPubSub from '/imports/startup/server/redis';
+import { extractCredentials } from '/imports/api/common/server/helpers';
 
-export default function stopWatchingExternalVideo(meetingId, requesterUserId) {
+export default function stopWatchingExternalVideo(options) {
   const REDIS_CONFIG = Meteor.settings.private.redis;
   const CHANNEL = REDIS_CONFIG.channels.toAkkaApps;
   const EVENT_NAME = 'StopExternalVideoMsg';
+
+  const { meetingId, requesterUserId } = extractCredentials(this.userId);
 
   const meeting = Meetings.findOne({ meetingId });
   if (!meeting || meeting.externalVideoUrl === null) return;
