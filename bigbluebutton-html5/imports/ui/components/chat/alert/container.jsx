cc07bfff20211f6b7bae92076293fb804bbf05da
Chad Pilkey <capilkey@gmail.com>
2020-03-05 15:14:22 -0800

    fixed inconsitencies and usage of the idChatOpen Session variable

--- bigbluebutton-html5/imports/ui/components/chat/alert/container.jsx
+++ bigbluebutton-html5/imports/ui/components/chat/alert/container.jsx
@@ -14,12 +14,23 @@ export default withTracker(() => {
   const AppSettings = Settings.application;
   const activeChats = UserListService.getActiveChats();
   const { loginTime } = Users.findOne({ userId: Auth.userID }, { fields: { loginTime: 1 } });
+
+  const openPanel = Session.get('openPanel');
+  let idChatOpen = Session.get('idChatOpen');
+
+  // Currently the panel can switch from the chat panel to something else and the idChatOpen won't
+  // always reset. A better solution would be to make the openPanel Session variable an
+  // Object { panelType: <String>, panelOptions: <Object> } and then get rid of idChatOpen
+  if (openPanel !== 'chat') {
+    idChatOpen = '';
+  }
+
   return {
     audioAlertDisabled: !AppSettings.chatAudioAlerts,
     pushAlertDisabled: !AppSettings.chatPushAlerts,
     activeChats,
     publicUserId: Meteor.settings.public.chat.public_group_id,
     joinTimestamp: loginTime,
-    idChatOpen: Session.get('idChatOpen'),
+    idChatOpen,
   };
 })(memo(ChatAlertContainer));
