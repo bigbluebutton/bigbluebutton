bd5efdcbfbc5de96803a50f5cc5fc460bdd950dd
sravani <sravani@bbb.pipitacademy.com>
2020-03-20 13:06:00 +0530

    chatfile font stylings

--- bigbluebutton-html5/imports/ui/components/chat/message-list/message-list-item/chat-file/component.jsx
+++ bigbluebutton-html5/imports/ui/components/chat/message-list/message-list-item/chat-file/component.jsx
@@ -41,7 +41,7 @@ export default class ChatFileUploaded extends PureComponent {
       <div className={(id == Auth.userID) ? styles.senderFileWrapper : styles.fileWrapper}>
         <div className={styles.wrapper}>
           <div className={styles.extensionBox}>
-            <Icon iconName="file" />
+            <img src="https://s3-alpha-sig.figma.com/img/0b43/a68b/f84eb6dd7e520d32d5c236c818153032?Expires=1585526400&Signature=OseYmvn1yyoFzQko1sxJPnOaexa5n0ITL56DScZxD2LN3P0Mhme78aum~v5rezadI0SWRB404ijdi~0JEvNmdysPI0l2igF~p50-CBAyqiqeE8alEQlsACtnhFSSsttfcK-mdivOp78We0mBC5i5A~YtmGWaat58DQmDHVwixkBO1htg4WtPfTN5mUw80Row9UEYndOWs18EuVapD5cXJUxIWznkQelnWyvT0piBUbB3mmca8w5-AUycwwvsF3rSpWAcbqL6hzbk70XJH63g7zlPM9KDLfcO9tEksgH5lB28Nk62ZvhvENNLSoub2-RFiUN6KIAKNdCM-qRgIjBBgg__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA" alt=""/>
           </div>
           <span className={styles.fileName}>{file.fileName}</span>
           <Button
