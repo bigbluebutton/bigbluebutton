1717732d955ba260216bbc91f810779366d4331f
Georg Wechslberger <=>
2020-03-24 14:11:18 +0100

    update html5-client config using yq

--- bigbluebutton-html5/docker-entrypoint.sh
+++ bigbluebutton-html5/docker-entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash -e
 
-export METEOR_SETTINGS=` jq "${METEOR_SETTINGS_MODIFIER}" ./programs/server/assets/app/config/settings-production.json `
+# Apply settings in environment.
+yq write -i ./programs/server/assets/app/config/settings.yml "public.kurento.wsUrl" "${KURENTO_WSURL}"
+yq write -i ./programs/server/assets/app/config/settings.yml "public.kurento.enableVideo" "${KURENTO_VIDEO}"
+yq write -i ./programs/server/assets/app/config/settings.yml "public.kurento.enableListenOnly" "${KURENTO_LISTEN_ONLY}"
+yq write -i ./programs/server/assets/app/config/settings.yml "public.kurento.enableScreensharing" "${KURENTO_SCREENSHARING}"
+yq write -i ./programs/server/assets/app/config/settings.yml "public.kurento.chromeExtensionKey" "${SCREENSHARING_EXTENSION_KEY}"
+yq write -i ./programs/server/assets/app/config/settings.yml "public.kurento.chromeExtensionLink" "${SCREENSHARING_EXTENSION_LINK}"
 
 node main.js
