b74bd0283ead5a2f865c91342666a8225c52dd6d
Pedro Beschorner Marin <pedrobmarin@gmail.com>
2020-03-26 15:17:59 -0300

    Fix webcamsOnlyForModerator

--- bigbluebutton-html5/imports/ui/components/video-provider/service.js
+++ bigbluebutton-html5/imports/ui/components/video-provider/service.js
@@ -153,7 +153,7 @@ class VideoService {
   }
 
   getVideoStreams() {
-    const streams = VideoStreams.find(
+    let streams = VideoStreams.find(
       { meetingId: Auth.meetingID },
       {
         fields: {
@@ -162,8 +162,11 @@ class VideoService {
       },
     ).fetch();
 
-    const connectingStream = this.getConnectingStream(streams);
+    const hideUsers = this.hideUserList();
+    const moderatorOnly = this.webcamsOnlyForModerator();
+    if (hideUsers || moderatorOnly) streams = this.filterModeratorOnly(streams);
 
+    const connectingStream = this.getConnectingStream(streams);
     if (connectingStream) streams.push(connectingStream);
 
     return streams.map(vs => ({
@@ -214,17 +217,51 @@ class VideoService {
     return streams.find(s => s.stream === stream);
   }
 
+  filterModeratorOnly(streams) {
+    const me = Users.findOne({ userId: Auth.userID });
+    const amIViewer = me.role === ROLE_VIEWER;
+
+    if (amIViewer) {
+      const moderators = Users.find(
+        {
+          meetingId: Auth.meetingID,
+          connectionStatus: 'online',
+          role: ROLE_MODERATOR,
+        },
+        { fields: { userId: 1 }}
+      ).fetch().map(user => user.userId);
+
+      return streams.reduce((result, stream) => {
+        const { userId } = stream;
+
+        const isModerator = moderators.includes(userId);
+        const isMe = me.userId === userId;
+
+        if (isModerator || isMe) result.push(stream);
+
+        return result;
+      }, []);
+    } else {
+      return streams;
+    }
+  }
+
   disableCam() {
     const m = Meetings.findOne({ meetingId: Auth.meetingID },
       { fields: { 'lockSettingsProps.disableCam': 1 } });
     return m.lockSettingsProps ? m.lockSettingsProps.disableCam : false;
   }
 
-  mirrorOwnWebcam(user) {
-    // only true if setting defined and video ids match
-    const isOwnWebcam = user ? this.userId() === user.userId : true;
-    const isEnabledMirroring = getFromUserSettings('bbb_mirror_own_webcam', MIRROR_WEBCAM);
-    return isOwnWebcam && isEnabledMirroring;
+  webcamsOnlyForModerator() {
+    const m = Meetings.findOne({ meetingId: Auth.meetingID },
+      { fields: { 'usersProp.webcamsOnlyForModerator': 1 } });
+    return m.usersProp ? m.usersProp.webcamsOnlyForModerator : false;
+  }
+
+  hideUserList() {
+    const m = Meetings.findOne({ meetingId: Auth.meetingID },
+      { fields: { 'lockSettingsProps.hideUserList': 1 } });
+    return m.lockSettingsProps ? m.lockSettingsProps.hideUserList : false;
   }
 
   getInfo() {
@@ -238,6 +275,13 @@ class VideoService {
       sessionToken: Auth.sessionToken,
       voiceBridge,
     };
+  }
+
+  mirrorOwnWebcam(user) {
+    // only true if setting defined and video ids match
+    const isOwnWebcam = user ? this.userId() === user.userId : true;
+    const isEnabledMirroring = getFromUserSettings('bbb_mirror_own_webcam', MIRROR_WEBCAM);
+    return isOwnWebcam && isEnabledMirroring;
   }
 
   getMyStream(deviceId) {
