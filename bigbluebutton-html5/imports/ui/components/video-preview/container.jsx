2be89479ba6e4ef38fe69df7d0614e0aad650962
KDSBrowne <kert.browne85@gmail.com>
2020-05-09 17:19:41 +0000

    close video preview modal if lock setting active

--- bigbluebutton-html5/imports/ui/components/video-preview/container.jsx
+++ bigbluebutton-html5/imports/ui/components/video-preview/container.jsx
@@ -3,6 +3,9 @@ import { withModalMounter } from '/imports/ui/components/modal/service';
 import { withTracker } from 'meteor/react-meteor-data';
 import deviceInfo from '/imports/utils/deviceInfo';
 import getFromUserSettings from '/imports/ui/services/users-settings';
+import Users from '/imports/api/users';
+import Meetings from '/imports/api/meetings';
+import Auth from '/imports/ui/services/auth';
 import Service from './service';
 import VideoPreview from './component';
 import VideoService from '../video-provider/service';
@@ -13,11 +16,28 @@ const SKIP_VIDEO_PREVIEW = KURENTO_CONFIG.skipVideoPreview;
 
 const VideoPreviewContainer = props => <VideoPreview {...props} />;
 
+const ROLE_MODERATOR = Meteor.settings.public.user.role_moderator;
+
+const isCamLocked = () => {
+  const meeting = Meetings.findOne({ meetingId: Auth.meetingID },
+    { fields: { 'lockSettingsProps.disableCam': 1 } });
+  const user = Users.findOne({ meetingId: Auth.meetingID, userId: Auth.userID },
+    { fields: { locked: 1, role: 1 } });
+
+  if (meeting.lockSettingsProps !== undefined) {
+    if (user.locked && user.role !== ROLE_MODERATOR) {
+      return meeting.lockSettingsProps.disableCam;
+    }
+  }
+  return false;
+};
+
 export default withModalMounter(withTracker(({ mountModal, fromInterface }) => ({
   startSharing: () => {
     mountModal(null);
     VideoService.joinVideo();
   },
+  isCamLocked: isCamLocked(),
   closeModal: () => mountModal(null),
   changeWebcam: deviceId => Service.changeWebcam(deviceId),
   webcamDeviceId: Service.webcamDeviceId(),
