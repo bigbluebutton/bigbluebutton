c0a2f5c29591af684cb8f1eb1490a663eed32262
sravani <sravani@bbb.pipitacademy.com>
2020-05-11 19:31:38 +0530

    changes in navbar

--- bigbluebutton-html5/imports/ui/components/nav-bar/component.jsx
+++ bigbluebutton-html5/imports/ui/components/nav-bar/component.jsx
@@ -11,6 +11,7 @@ import Button from '../button/component';
 import RecordingIndicator from './recording-indicator/container';
 import TalkingIndicatorContainer from '/imports/ui/components/nav-bar/talking-indicator/container';
 import SettingsDropdownContainer from './settings-dropdown/container';
+import { meetingIsBreakout } from '/imports/ui/components/app/service';
 
 const ROLE_MODERATOR = Meteor.settings.public.user.role_moderator;
 const intlMessages = defineMessages({
@@ -84,7 +85,7 @@ class NavBar extends PureComponent {
       currentUser,
     } = this.props;
 
-
+    const isBreakOutMeeting = meetingIsBreakout();
     const toggleBtnClasses = {};
     toggleBtnClasses[styles.btn] = true;
     toggleBtnClasses[styles.btnWithNotificationDot] = hasUnreadMessages;
@@ -118,7 +119,7 @@ class NavBar extends PureComponent {
           <div className={styles.right}>
  <div className={styles.both}>
    <b ><span >{currentUser.name}</span></b>
-   {(breakoutRoomName &&!amIModerator)? <p ><span >({breakoutRoomName})</span></p> :( amIModerator&& breakoutRoomName?<span>(moderator)</span>:null)}
+   {(breakoutRoomName &&!amIModerator)? <p ><span >({breakoutRoomName})</span></p> :( amIModerator&& !isBreakOutMeeting?<span>(moderator)</span>:null)}
    
     </div> 
             <SettingsDropdownContainer amIModerator={amIModerator} />
