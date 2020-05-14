7152a61e7aee6c9fd9c72feef70a4f4e15a6420d
macbook <shayan.noje@gmail.com>
2020-05-14 16:07:29 +0430

    fr

--- bigbluebutton-html5/imports/ui/components/audio/audio-modal/component.jsx
+++ bigbluebutton-html5/imports/ui/components/audio/audio-modal/component.jsx
@@ -568,7 +568,7 @@ class AudioModal extends Component {
                 {
                   isIOSChrome ? null
                     : (
-                      <h3 className={styles.title}>dsds
+                      <h3 className={styles.title}>
                         {content
                           ? intl.formatMessage(this.contents[content].title)
                           : intl.formatMessage(intlMessages.audioChoiceLabel)}
