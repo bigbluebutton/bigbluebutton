745bd69af68381bfe60d02fbebc171798caf6857
Mohamed Amine Ben Salah <damineone@gmail.com>
2020-05-15 12:06:45 -0300

    adds description comments to test specs

--- bigbluebutton-html5/tests/puppeteer/customparameters.test.js
+++ bigbluebutton-html5/tests/puppeteer/customparameters.test.js
@@ -7,51 +7,59 @@ describe('Custom parameters', () => {
     jest.setTimeout(30000);
   });
 
-  // test('Auto join', async () => {
-  //   const test = new CustomParameters();
-  //   let response;
-  //   try {
-  //     console.log('before');
-  //     response = await test.autoJoin(Page.getArgs(), undefined, c.autoJoin);
-  //     console.log('after');
-  //   } catch (e) {
-  //     console.log(e);
-  //   } finally {
-  //     await test.closePage(test.page1);
-  //   }
-  //   expect(response).toBe(true);
-  // });
+  // This test spec sets the userdata-autoJoin parameter to false 
+  // and checks if the users don't get audio modal on login
+  test('Auto join', async () => {
+    const test = new CustomParameters();
+    let response;
+    try {
+      console.log('before');
+      response = await test.autoJoin(Page.getArgs(), undefined, c.autoJoin);
+      console.log('after');
+    } catch (e) {
+      console.log(e);
+    } finally {
+      await test.closePage(test.page1);
+    }
+    expect(response).toBe(true);
+  });
 
-  // test('Listen Only Mode', async () => {
-  //   const test = new CustomParameters();
-  //   let response;
-  //   try {
-  //     console.log('before');
-  //     response = await test.listenOnlyMode(Page.getArgs(), undefined, c.listenOnlyMode);
-  //     console.log('after');
-  //   } catch (e) {
-  //     console.log(e);
-  //   } finally {
-  //     await test.close(test.page1, test.page2);
-  //   }
-  //   expect(response).toBe(true);
-  // });
+  // This test spec sets the userdata-listenOnlyMode parameter to false 
+  // and checks if the users can't see or use listen Only mode
+  test('Listen Only Mode', async () => {
+    const test = new CustomParameters();
+    let response;
+    try {
+      console.log('before');
+      response = await test.listenOnlyMode(Page.getArgs(), undefined, c.listenOnlyMode);
+      console.log('after');
+    } catch (e) {
+      console.log(e);
+    } finally {
+      await test.close(test.page1, test.page2);
+    }
+    expect(response).toBe(true);
+  });
 
-  // test('Force Listen Only', async () => {
-  //   const test = new CustomParameters();
-  //   let response;
-  //   try {
-  //     console.log('before');
-  //     response = await test.forceListenOnly(Page.getArgs(), undefined, c.forceListenOnly);
-  //     console.log('after');
-  //   } catch (e) {
-  //     console.log(e);
-  //   } finally {
-  //     await test.close(test.page1, test.page2);
-  //   }
-  //   expect(response).toBe(true);
-  // });
+  // This test spec sets the userdata-forceListenOnly parameter to false 
+  // and checks if the Viewers can only use listen only mode
+  test('Force Listen Only', async () => {
+    const test = new CustomParameters();
+    let response;
+    try {
+      console.log('before');
+      response = await test.forceListenOnly(Page.getArgs(), undefined, c.forceListenOnly);
+      console.log('after');
+    } catch (e) {
+      console.log(e);
+    } finally {
+      await test.close(test.page1, test.page2);
+    }
+    expect(response).toBe(true);
+  });
 
+  // This test spec sets the userdata-skipCheck parameter to true 
+  // and checks if the users automatically skip audio check when clicking on Microphone
   test('Skip audio check', async () => {
     const test = new CustomParameters();
     let response;
@@ -67,33 +75,37 @@ describe('Custom parameters', () => {
     expect(response).toBe(true);
   });
 
-  // test('Skip audio check', async () => {
-  //   const test = new CustomParameters();
-  //   let response;
-  //   try {
-  //     console.log('before');
-  //     response = await test.clientTitle(Page.getArgs(), undefined, c.clientTitle);
-  //     console.log('after');
-  //   } catch (e) {
-  //     console.log(e);
-  //   } finally {
-  //     await test.closePage(test.page1);
-  //   }
-  //   expect(response).toBe(true);
-  // });
+  // This test spec sets the userdata-clientTitle parameter to some value 
+  // and checks if the meeting window contains that value
+  test('Client title', async () => {
+    const test = new CustomParameters();
+    let response;
+    try {
+      console.log('before');
+      response = await test.clientTitle(Page.getArgs(), undefined, c.clientTitle);
+      console.log('after');
+    } catch (e) {
+      console.log(e);
+    } finally {
+      await test.closePage(test.page1);
+    }
+    expect(response).toBe(true);
+  });
 
-  // test('ask For Feed back On Logout', async () => {
-  //   const test = new CustomParameters();
-  //   let response;
-  //   try {
-  //     console.log('before');
-  //     response = await test.askForFeedbackOnLogout(Page.getArgs(), undefined, c.askForFeedbackOnLogout);
-  //     console.log('after');
-  //   } catch (e) {
-  //     console.log(e);
-  //   } finally {
-  //     await test.closePage(test.page1);
-  //   }
-  //   expect(response).toBe(true);
-  // });
+  // This test spec sets the userdata-askForFeedbackOnLogout parameter to true 
+  // and checks if the users automatically get asked for feedback on logout page
+  test('Ask For Feed back On Logout', async () => {
+    const test = new CustomParameters();
+    let response;
+    try {
+      console.log('before');
+      response = await test.askForFeedbackOnLogout(Page.getArgs(), undefined, c.askForFeedbackOnLogout);
+      console.log('after');
+    } catch (e) {
+      console.log(e);
+    } finally {
+      await test.closePage(test.page1);
+    }
+    expect(response).toBe(true);
+  });
 });
