45f99fed8bbff629afd921778a683331b3931ebf
Joao Siebel <joaos_desenv@imdt.com.br>
2020-06-12 13:24:11 -0300

    Prevent banned user from trying to validate auth token multiple times. close #9798

--- bigbluebutton-html5/imports/api/users/server/methods/removeUser.js
+++ bigbluebutton-html5/imports/api/users/server/methods/removeUser.js
@@ -2,6 +2,8 @@ import { Meteor } from 'meteor/meteor';
 import { check } from 'meteor/check';
 import RedisPubSub from '/imports/startup/server/redis';
 import { extractCredentials } from '/imports/api/common/server/helpers';
+import Users from '/imports/api/users';
+import BannedUsers from '/imports/api/users/server/store/bannedUsers';
 
 export default function removeUser(userId, banUser) {
   const REDIS_CONFIG = Meteor.settings.private.redis;
@@ -17,6 +19,10 @@ export default function removeUser(userId, banUser) {
     ejectedBy,
     banUser,
   };
+
+  const removedUser = Users.findOne({ meetingId, userId }, { extId: 1 });
+
+  if (banUser && removedUser) BannedUsers.add(meetingId, removedUser.extId);
 
   return RedisPubSub.publishUserMessage(CHANNEL, EVENT_NAME, meetingId, ejectedBy, payload);
 }
