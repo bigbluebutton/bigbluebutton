1fe5b4b5de15523797fcf08e4fce79c6046a3136
Tainan Felipe <tainanfelipe214@gmail.com>
2020-06-16 13:40:02 -0300

    Add a listener to voice user ejecting user from audion on socket disconnection

--- bigbluebutton-html5/imports/api/voice-users/server/publishers.js
+++ bigbluebutton-html5/imports/api/voice-users/server/publishers.js
@@ -2,15 +2,28 @@ import VoiceUsers from '/imports/api/voice-users';
 import { Meteor } from 'meteor/meteor';
 import { check } from 'meteor/check';
 import Logger from '/imports/startup/server/logger';
+import ejectUserFromVoice from './methods/ejectUserFromVoice';
 
 function voiceUser(credentials) {
   const { meetingId, requesterUserId } = credentials;
 
   check(meetingId, String);
   check(requesterUserId, String);
+  const onCloseConnection = Meteor.bindEnvironment(() => {
+    try {
+      // I used user because voiceUser is the function's name
+      const User = VoiceUsers.findOne({ meetingId, requesterUserId });
+      if (User) {
+        ejectUserFromVoice({ meetingId, requesterUserId }, requesterUserId);
+      }
+    } catch (e) {
+      Logger.error(`Exception while executing ejectUserFromVoice: ${e}`);
+    }
+  });
 
   Logger.debug(`Publishing Voice User for ${meetingId} ${requesterUserId}`);
 
+  this._session.socket.on('close', _.debounce(onCloseConnection, 100));
   return VoiceUsers.find({ meetingId });
 }
 
