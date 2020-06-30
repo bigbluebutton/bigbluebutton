85f6ef3dda27134fe3b39a7b4ef81bfa14d68ee6
Mohamed Amine Ben Salah <damineone@gmail.com>
2020-06-29 22:25:04 -0300

    fixes breakout automated test and makes it stable

--- bigbluebutton-html5/tests/puppeteer/breakout/create.js
+++ bigbluebutton-html5/tests/puppeteer/breakout/create.js
@@ -1,6 +1,7 @@
 const Page = require('../core/page');
 const params = require('../params');
 const util = require('./util');
+const be = require('./elements');
 
 class Create {
   constructor() {
@@ -11,17 +12,48 @@ class Create {
 
   // Join BigBlueButton meeting
   async init(meetingId) {
-    await this.page1.init(Page.getArgs(), meetingId, { ...params, fullName: 'Moderator1' });
-    await this.page2.init(Page.getArgs(), this.page1.meetingId, { ...params, fullName: 'Viewer1', moderatorPW: '' });
+    await this.page1.init(Page.getArgs(), meetingId, { ...params, fullName: 'Moderator1' }, undefined);
+    await this.page2.init(Page.getArgs(), this.page1.meetingId, { ...params, fullName: 'Viewer1', moderatorPW: '' }, undefined);
   }
 
-  async create() {
-    await util.waitForBreakoutElements(this.page1);
-    await util.createBreakoutRooms(this.page1, this.page2);
+  // Create Breakoutrooms
+  async create(testName) {
+    await this.page1.screenshot(`${testName}`, `01-page01-initialized-${testName}`);
+    await this.page2.screenshot(`${testName}`, `01-page01-initialized-${testName}`);
+    await this.page1.page.evaluate(util.clickTestElement, be.manageUsers);
+    await this.page1.page.evaluate(util.clickTestElement, be.createBreakoutRooms);
+    await this.page1.screenshot(`${testName}`, `02-page01-creating-breakoutrooms-${testName}`);
+    await this.page1.waitForSelector(be.randomlyAssign);
+    await this.page1.page.evaluate(util.clickTestElement, be.randomlyAssign);
+    await this.page1.screenshot(`${testName}`, `03-page01-randomly-assign-user-${testName}`);
+    await this.page1.waitForSelector(be.modalConfirmButton);
+    await this.page1.page.evaluate(util.clickTestElement, be.modalConfirmButton);
+    await this.page1.screenshot(`${testName}`, `04-page01-confirm-breakoutrooms-creation-${testName}`);
+    await this.page2.waitForSelector(be.modalConfirmButton);
+    await this.page2.page.evaluate(util.clickTestElement, be.modalConfirmButton);
+    await this.page2.screenshot(`${testName}`, `02-page02-accept-invite-breakoutrooms-${testName}`);
   }
 
-  async joinWithUser2() {
-    await this.page3.init(Page.getArgs(), this.page1.meetingId, { ...params, fullName: 'Moderator2' });
+  // Check if Breakoutrooms have been created
+  async testCreated(testName) {
+    const resp = await this.page1.page.evaluate(() => document.querySelectorAll('div[data-test="breakoutRoomsItem"]').length !== 0);
+    if (resp === true) {
+      await this.page1.screenshot(`${testName}`, `05-page01-success-${testName}`);
+      return true;
+    }
+    await this.page1.screenshot(`${testName}`, `05-page01-fail-${testName}`);
+    return false;
+  }
+
+  // Initialize a Moderator session
+  async joinWithUser3() {
+    await this.page3.init(Page.getArgs(), this.page1.meetingId, { ...params, fullName: 'Moderator3' }, undefined);
+  }
+
+  // Close pages
+  async close() {
+    await this.page1.close();
+    await this.page2.close();
   }
 }
 
--- bigbluebutton-html5/tests/puppeteer/breakout/join.js
+++ bigbluebutton-html5/tests/puppeteer/breakout/join.js
@@ -2,26 +2,39 @@ const Page = require('../core/page');
 const Create = require('./create');
 const util = require('./util');
 const e = require('./elements');
+const pe = require('../core/elements');
 
 class Join extends Create {
   constructor() {
     super('join-breakout');
-    this.page3 = new Page();
   }
 
-  async join() {
-    await this.joinWithUser2();
-    await util.joinBreakoutRooms(this.page3);
+  // Join Existing Breakoutrooms
+  async join(testName) {
+    await this.joinWithUser3();
+    await this.page3.screenshot(`${testName}`, `01-page03-initialized-${testName}`);
+    await this.page3.closeAudioModal();
+    await this.page3.screenshot(`${testName}`, `02-page03-closed-audioModal-${testName}`);
+    await this.page3.waitForSelector(e.breakoutRoomsItem);
+    await this.page3.page.evaluate(util.clickTestElement, e.breakoutRoomsItem);
+    await this.page3.screenshot(`${testName}`, `03-page03-breakoutrooms-list-visualization-${testName}`);
+    await this.page3.waitForSelector(e.breakoutJoin);
+    await this.page3.page.evaluate(util.clickTestElement, e.breakoutJoin);
+    await this.page3.screenshot(`${testName}`, `04-page03-joined-breakoutrooms-${testName}`);
   }
 
-  async test() {
-    const n = [(await this.page3.browser.pages()).length - 1];
-    const page = (await this.page3.browser.pages())[n];
-    const notificationBar = await page.evaluate(util.getTestElement, e.breakoutRemainingTime);
-    const response = notificationBar !== null;
-    return response;
+  // Check if User Joined in Breakoutrooms
+  async testJoined(testName) {
+    const resp = await this.page2.page.evaluate(util.getTestElement, e.userJoined);
+    if (resp === true) {
+      await this.page2.screenshot(`${testName}`, `05-page03-success-${testName}`);
+      return true;
+    }
+    await this.page2.screenshot(`${testName}`, `05-page03-fail-${testName}`);
+    return false;
   }
 
+  // Close pages
   async close() {
     await this.page1.close();
     await this.page2.close();
