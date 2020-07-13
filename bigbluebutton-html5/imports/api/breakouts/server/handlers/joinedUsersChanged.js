44269880a0b89a22881c62272630786cceaf250b
Martin Beckmann <m.beckmann@konkret-mafo.cloud>
2020-07-13 20:47:56 +0200

    debugging output

--- bigbluebutton-html5/imports/api/breakouts/server/handlers/joinedUsersChanged.js
+++ bigbluebutton-html5/imports/api/breakouts/server/handlers/joinedUsersChanged.js
@@ -20,6 +20,9 @@ export default function joinedUsersChanged({ body }) {
     breakoutId,
   };
 
+  Logger.info('USERSMAPPED:');
+  Logger.info(JSON.stringify(users));
+
   const usersMapped = users.map(user => ({ userId: user.id, name: user.name }));
   const modifier = {
     $set: {
