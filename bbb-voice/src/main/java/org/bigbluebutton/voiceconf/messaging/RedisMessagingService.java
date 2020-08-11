30be763998919f5ac35d59f71bb14e8011bd2122
peakstack <43585195+peakstack@users.noreply.github.com>
2020-08-11 02:39:33 +0200

    style adjustments

--- bbb-voice/src/main/java/org/bigbluebutton/voiceconf/messaging/RedisMessagingService.java
+++ bbb-voice/src/main/java/org/bigbluebutton/voiceconf/messaging/RedisMessagingService.java
@@ -15,9 +15,8 @@ import org.slf4j.Logger;
 
 public class RedisMessagingService implements IMessagingService {
 	private static Logger log = Red5LoggerFactory.getLogger(RedisMessagingService.class, "sip");
-	
-  private static final Pattern CALLERNAME_PATTERN = Pattern.compile("(.*)-bbbID-(.*)$");
-  
+	private static final Pattern CALLERNAME_PATTERN = Pattern.compile("(.*)-bbbID-(.*)$");
+
 	private MessageSender sender;
 
 	private Map<String, Object> buildEnvelope(String name, Map<String, String> routing) {
@@ -55,33 +54,32 @@ public class RedisMessagingService implements IMessagingService {
 
 	@Override
 	public void userConnectedToGlobalAudio(String voiceConf, String callerIdName) {
-		
-  	Matcher matcher = CALLERNAME_PATTERN.matcher(callerIdName);
-    if (matcher.matches()) {			
-	    String userid = matcher.group(1).trim();
-	    String name = matcher.group(2).trim();
+		Matcher matcher = CALLERNAME_PATTERN.matcher(callerIdName);
+		if (matcher.matches()) {
+			String userid = matcher.group(1).trim();
+			String name = matcher.group(2).trim();
 			String json = new UserConnectedToGlobalAudio(voiceConf, userid, name).toJson();
 			sender.send(MessagingConstants.TO_AKKA_APPS_CHANNEL, json);
-    } else {
-    	log.warn("Invalid calleridname [{}] in userConnectedToGlobalAudio as it does not match pattern (.*)-bbbID-(.*)");
+		} else {
+			log.warn("Invalid calleridname [{}] in userConnectedToGlobalAudio as it does not match pattern (.*)-bbbID-(.*)");
 			String json = new UserConnectedToGlobalAudio(voiceConf, callerIdName, callerIdName).toJson();
 			sender.send(MessagingConstants.TO_AKKA_APPS_CHANNEL, json);
-    }
+		}
 	}
 
 	@Override
 	public void userDisconnectedFromGlobalAudio(String voiceConf, String callerIdName) {
-	  	Matcher matcher = CALLERNAME_PATTERN.matcher(callerIdName);
-	    if (matcher.matches()) {
-		    String userid = matcher.group(1).trim();
-		    String name = matcher.group(2).trim();
-				String json = new UserDisconnectedFromGlobalAudio(voiceConf, userid, name).toJson();
-				sender.send(MessagingConstants.TO_AKKA_APPS_CHANNEL, json);
-	    } else {
-	    	log.warn("Invalid calleridname [{}] in userDisconnectedFromGlobalAudio as it does not match pattern (.*)-bbbID-(.*)");
-				String json = new UserDisconnectedFromGlobalAudio(voiceConf, callerIdName, callerIdName).toJson();
-				sender.send(MessagingConstants.TO_AKKA_APPS_CHANNEL, json);
-	    }
+		Matcher matcher = CALLERNAME_PATTERN.matcher(callerIdName);
+		if (matcher.matches()) {
+			String userid = matcher.group(1).trim();
+			String name = matcher.group(2).trim();
+			String json = new UserDisconnectedFromGlobalAudio(voiceConf, userid, name).toJson();
+			sender.send(MessagingConstants.TO_AKKA_APPS_CHANNEL, json);
+		} else {
+			log.warn("Invalid calleridname [{}] in userDisconnectedFromGlobalAudio as it does not match pattern (.*)-bbbID-(.*)");
+			String json = new UserDisconnectedFromGlobalAudio(voiceConf, callerIdName, callerIdName).toJson();
+			sender.send(MessagingConstants.TO_AKKA_APPS_CHANNEL, json);
+		}
 	}
 
 	public void setRedisMessageSender(MessageSender sender) {
