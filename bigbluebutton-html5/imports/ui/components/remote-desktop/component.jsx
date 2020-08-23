4ce18c619fb55ee7fcd08bacc25fe7b4d35c8e91
Brent Baccala <cosine@freesoft.org>
2020-08-23 19:10:29 -0400

    fix remote desktop styles that I had copied from external-video-player without renaming them

--- bigbluebutton-html5/imports/ui/components/remote-desktop/component.jsx
+++ bigbluebutton-html5/imports/ui/components/remote-desktop/component.jsx
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import { injectIntl } from 'react-intl';
 import VncDisplay from 'react-vnc-display';
 
+import { styles } from './styles';
+
 const propTypes = {
   remoteDesktopUrl: PropTypes.string,
 };
@@ -26,6 +28,7 @@ class RemoteDesktop extends Component {
     return (
       <div id="remote-desktop" data-test="remoteDesktop">
         <VncDisplay
+          className={styles.remoteDesktop}
           url={remoteDesktopUrl}
           forceAuthScheme={2}
           onPasswordRequired={this.passwordFunc}
