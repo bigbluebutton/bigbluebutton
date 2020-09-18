6a139aa2249deaeec6533ed8383bbb29063d08e7
Joao Siebel <joaos_desenv@imdt.com.br>
2020-09-18 18:05:31 -0300

    Fix last name sort function for save user name

--- bigbluebutton-html5/imports/ui/components/user-list/service.js
+++ bigbluebutton-html5/imports/ui/components/user-list/service.js
@@ -495,6 +495,7 @@ const sortUsersByFirstName = (a, b) => {
 };
 
 const sortUsersByLastName = (a, b) => {
+  if (!a.lastName && !b.lastName) return 0;
   if (a.lastName && !b.lastName) return -1;
   if (!a.lastName && b.lastName) return 1;
 
