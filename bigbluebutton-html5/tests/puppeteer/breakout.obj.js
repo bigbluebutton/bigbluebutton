1c40b0b8c896713ad53208232ece357f552c93fa
Mohamed Amine Ben Salah <damineone@gmail.com>
2020-09-21 15:22:11 -0300

    adds missing custom parameters specs to the custom parameters test suite

--- bigbluebutton-html5/tests/puppeteer/breakout.obj.js
+++ bigbluebutton-html5/tests/puppeteer/breakout.obj.js
@@ -10,88 +10,76 @@ const breakoutTest = () => {
     jest.setTimeout(150000);
   });
 
-  // // Create Breakout Room
-  // test('Create Breakout room', async () => {
-  //   const test = new Create();
-  //   let response;
-  //   let screenshot;
-  //   try {
-  //     const testName = 'createBreakoutrooms';
-  //     await test.init(undefined);
-  //     await test.create(testName);
-  //     response = await test.testCreatedBreakout(testName);
-  //     const page2 = await test.page2.browser.pages();
-  //     await page2[2].bringToFront();
-  //     screenshot = await page2[2].screenshot();
-  //   } catch (e) {
-  //     console.log(e);
-  //   } finally {
-  //     await test.close();
-  //   }
-  //   expect(response).toBe(true);
-  //   if (process.env.REGRESSION_TESTING === 'true') {
-  //     expect(screenshot).toMatchImageSnapshot({
-  //       failureThreshold: 8,
-  //       failureThresholdType: 'percent',
-  //     });
-  //   }
-  // });
+  // Create Breakout Room
+  test('Create Breakout room', async () => {
+    const test = new Create();
+    let response;
+    try {
+      const testName = 'createBreakoutrooms';
+      await test.init(undefined);
+      await test.create(testName);
+      response = await test.testCreatedBreakout(testName);
+    } catch (e) {
+      console.log(e);
+    } finally {
+      await test.close();
+    }
+    expect(response).toBe(true);
+  });
 
-  // // Join Breakout Room
-  // test('Join Breakout room', async () => {
-  //   const test = new Join();
-  //   let response;
-  //   let screenshot;
-  //   try {
-  //     const testName = 'joinBreakoutroomsWithoutFeatures';
-  //     await test.init(undefined);
-  //     await test.create(testName);
-  //     await test.join(testName);
-  //     response = await test.testJoined(testName);      
-  //     const page2 = await test.page2.browser.pages();
-  //     await page2[2].bringToFront();
-  //     screenshot = await page2[2].screenshot();
-  //   } catch (e) {
-  //     console.log(e);
-  //   } finally {
-  //     await test.close();
-  //   }
-  //   expect(response).toBe(true);
-  //   if (process.env.REGRESSION_TESTING === 'true') {
-  //     expect(screenshot).toMatchImageSnapshot({
-  //       failureThreshold: 4,
-  //       failureThresholdType: 'percent',
-  //     });
-  //   }
-  // });
+  // Join Breakout Room
+  test('Join Breakout room', async () => {
+    const test = new Join();
+    let response;
+    try {
+      const testName = 'joinBreakoutroomsWithoutFeatures';
+      await test.init(undefined);
+      await test.create(testName);
+      await test.join(testName);
+      response = await test.testJoined(testName);
+    } catch (e) {
+      console.log(e);
+    } finally {
+      await test.close();
+    }
+    expect(response).toBe(true);
+  });
 
-  // // Join Breakout Room with Video
-  // test('Join Breakout room with Video', async () => {
-  //   const test = new Join();
-  //   let response;
-  //   let screenshot;
-  //   try {
-  //     const testName = 'joinBreakoutroomsWithVideo';
-  //     await test.init(undefined);
-  //     await test.create(testName);
-  //     await test.join(testName);
-  //     response = await test.testJoined(testName);
-  //     const page2 = await test.page2.browser.pages();
-  //     await page2[2].bringToFront();
-  //     screenshot = await page2[2].screenshot();
-  //   } catch (e) {
-  //     console.log(e);
-  //   } finally {
-  //     await test.close();
-  //   }
-  //   expect(response).toBe(true);
-  //   if (process.env.REGRESSION_TESTING === 'true') {
-  //     expect(screenshot).toMatchImageSnapshot({
-  //       failureThreshold: 0.6,
-  //       failureThresholdType: 'percent',
-  //     });
-  //   }
-  // });
+  // Join Breakout Room with Video
+  test('Join Breakout room with Video', async () => {
+    const test = new Join();
+    let response;
+    try {
+      const testName = 'joinBreakoutroomsWithVideo';
+      await test.init(undefined);
+      await test.create(testName);
+      await test.join(testName);
+      response = await test.testJoined(testName);
+    } catch (e) {
+      console.log(e);
+    } finally {
+      await test.close();
+    }
+    expect(response).toBe(true);
+  });
+
+  // Join Breakout Room and start Screen Share
+  test('Join Breakout room and share screen', async () => {
+    const test = new Join();
+    let response;
+    try {
+      const testName = 'joinBreakoutroomsAndShareScreen';
+      await test.init(undefined);
+      await test.create(testName);
+      await test.join(testName);
+      response = await test.testJoined(testName);
+    } catch (e) {
+      console.log(e);
+    } finally {
+      await test.close();
+    }
+    expect(response).toBe(true);
+  });
 
   // Join Breakout Room and start Screen Share
   test('Join Breakout room and share screen', async () => {
