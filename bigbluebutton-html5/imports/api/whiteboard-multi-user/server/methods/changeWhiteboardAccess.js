1a10d03600bfaf680b25c5dc7e9de2a15309e22d
hiroshisuga <45039819+hiroshisuga@users.noreply.github.com>
2020-10-09 22:03:08 +0900

    Update changeWhiteboardAccess.js

--- bigbluebutton-html5/imports/api/whiteboard-multi-user/server/methods/changeWhiteboardAccess.js
+++ bigbluebutton-html5/imports/api/whiteboard-multi-user/server/methods/changeWhiteboardAccess.js
@@ -21,7 +21,7 @@ export default function changeWhiteboardAccess(multiUser, whiteboardId, userId =
     whiteboardId,
   };
   
-    const selector = {
+  const selector = {
     meetingId,
     connectionStatus: 'online',
   };
