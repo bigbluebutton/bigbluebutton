b5e5e81860255aaa2d0af8f72d7972948d4163be
hiroshisuga <45039819+hiroshisuga@users.noreply.github.com>
2020-10-09 22:23:56 +0900

    Update service.js

--- bigbluebutton-html5/imports/ui/components/presentation/cursor/service.js
+++ bigbluebutton-html5/imports/ui/components/presentation/cursor/service.js
@@ -1,5 +1,7 @@
 import Cursor from '/imports/ui/components/cursor/service';
 import Users from '/imports/api/users';
+import Auth from '/imports/ui/services/auth';
+import WhiteboardMultiUser from '/imports/api/whiteboard-multi-user/';
 
 const getCurrentCursor = (cursorId) => {
   const cursor = Cursor.findOne({ _id: cursorId });
@@ -14,6 +16,24 @@ const getCurrentCursor = (cursorId) => {
   return false;
 };
 
+const isPresenter = () => {
+  const currentUser = Users.findOne({ userId: Auth.userID }, { fields: { presenter: 1 } });
+  return currentUser ? currentUser.presenter : false;
+};
+
+const currentUserID = () => {
+  return Auth.userID ;
+};
+
+const getMultiUserStatus = (whiteboardId) => {
+  const data = WhiteboardMultiUser.findOne({ meetingId: Auth.meetingID, whiteboardId });
+  return data ? data.multiUser : 0;
+};
+
 export default {
   getCurrentCursor,
+  isPresenter,
+  getMultiUserStatus,
+  currentUserID,
 };
+
