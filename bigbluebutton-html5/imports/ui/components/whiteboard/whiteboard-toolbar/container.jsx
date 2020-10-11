cf44fd3cf86cef4ae752e013228acc8864400244
hiroshisuga <45039819+hiroshisuga@users.noreply.github.com>
2020-10-11 18:28:29 +0900

    Update container.jsx

--- bigbluebutton-html5/imports/ui/components/whiteboard/whiteboard-toolbar/container.jsx
+++ bigbluebutton-html5/imports/ui/components/whiteboard/whiteboard-toolbar/container.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { withTracker } from 'meteor/react-meteor-data';
 import WhiteboardToolbarService from './service';
 import WhiteboardToolbar from './component';
+import Users from '/imports/api/users';
+import Auth from '/imports/ui/services/auth';
 
 const WhiteboardToolbarContainer = props => (
   <WhiteboardToolbar {...props} />
@@ -9,6 +11,18 @@ const WhiteboardToolbarContainer = props => (
 
 export default withTracker((params) => {
   const { whiteboardId } = params;
+  
+  const withAccessNum = Users.find({
+    meetingId: Auth.meetingID,
+    whiteboardAccess: true,
+    connectionStatus: 'online',
+    presenter: false,
+  }, {
+    fields: {
+      id: 1,
+    },
+  }).fetch().length;
+  
   const data = {
     actions: {
       undoAnnotation: WhiteboardToolbarService.undoAnnotation,
@@ -21,12 +35,14 @@ export default withTracker((params) => {
       setThickness: WhiteboardToolbarService.setThickness,
       setColor: WhiteboardToolbarService.setColor,
       setTextShapeObject: WhiteboardToolbarService.setTextShapeObject,
+      setMultiuser: WhiteboardToolbarService.setMultiuser,
     },
     textShapeActiveId: WhiteboardToolbarService.getTextShapeActiveId(),
     multiUser: WhiteboardToolbarService.getMultiUserStatus(whiteboardId),
     isPresenter: WhiteboardToolbarService.isPresenter(),
     annotations: WhiteboardToolbarService.filterAnnotationList(),
     isMeteorConnected: Meteor.status().connected,
+    withAccessNum,
   };
 
   return data;
