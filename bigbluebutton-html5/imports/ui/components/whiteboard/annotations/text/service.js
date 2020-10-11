50327acdc59ea122ad54b0d61d476668cf02a564
hiroshisuga <45039819+hiroshisuga@users.noreply.github.com>
2020-10-11 16:50:29 +0900

    Create service.js

---
+++ bigbluebutton-html5/imports/ui/components/whiteboard/annotations/text/service.js
@@ -0,0 +1,45 @@
+import Storage from '/imports/ui/services/storage/session';
+import Users from '/imports/api/users';
+import Auth from '/imports/ui/services/auth';
+import WhiteboardMultiUser from '/imports/api/whiteboard-multi-user/';
+
+const DRAW_SETTINGS = 'drawSettings';
+
+const setTextShapeValue = (text) => {
+  const drawSettings = Storage.getItem(DRAW_SETTINGS);
+  if (drawSettings) {
+    drawSettings.textShape.textShapeValue = text;
+    Storage.setItem(DRAW_SETTINGS, drawSettings);
+  }
+};
+
+const resetTextShapeActiveId = () => {
+  const drawSettings = Storage.getItem(DRAW_SETTINGS);
+  if (drawSettings) {
+    drawSettings.textShape.textShapeActiveId = '';
+    Storage.setItem(DRAW_SETTINGS, drawSettings);
+  }
+};
+
+const isPresenter = () => {
+  const currentUser = Users.findOne({ userId: Auth.userID }, { fields: { presenter: 1 } });
+  return currentUser ? currentUser.presenter : false;
+};
+
+const getMultiUserStatus = (whiteboardId) => {
+  const data = WhiteboardMultiUser.findOne({ meetingId: Auth.meetingID, whiteboardId });
+  return data ? data.multiUser : false;
+};
+
+const activeTextShapeId = () => {
+  const drawSettings = Storage.getItem(DRAW_SETTINGS);
+  return drawSettings ? drawSettings.textShape.textShapeActiveId : '';
+};
+
+export default {
+  setTextShapeValue,
+  activeTextShapeId,
+  isPresenter,
+  resetTextShapeActiveId,
+  getMultiUserStatus,
+};
