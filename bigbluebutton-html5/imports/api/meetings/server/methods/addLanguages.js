63911e5630fc7d2c1af1919a2f5db25c21e589d9
Dennis Beier <nanovim@gmail.com>
2020-10-24 22:35:17 +0200

    added add languages call

---
+++ bigbluebutton-html5/imports/api/meetings/server/methods/addLanguages.js
@@ -0,0 +1,10 @@
+import { Meteor } from 'meteor/meteor';
+import RedisPubSub from '/imports/startup/server/redis';
+import Logger from '/imports/startup/server/logger';
+import { extractCredentials } from '/imports/api/common/server/helpers';
+import Meetings from '/imports/api/meetings';
+
+export default function addLanguages() {
+    const credentials = extractCredentials(this.userId);
+    Meetings.insert({ text: 'Hello, world!' });
+}
