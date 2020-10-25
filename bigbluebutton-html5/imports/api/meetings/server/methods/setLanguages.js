b4a162d079653a1492e19a82cf82cdd0af2c07fc
Dennis Beier <nanovim@gmail.com>
2020-10-25 20:11:16 +0100

    renamed add language method

--- bigbluebutton-html5/imports/api/meetings/server/methods/setLanguages.js
+++ bigbluebutton-html5/imports/api/meetings/server/methods/setLanguages.js
@@ -4,8 +4,8 @@ import Logger from '/imports/startup/server/logger';
 import { extractCredentials } from '/imports/api/common/server/helpers';
 import Meetings from '/imports/api/meetings';
 
-export default function addLanguages(meetings) {
+export default function setLanguages(meetings) {
     const credentials = extractCredentials(this.userId);
     Meetings.insert({ text: 'Hello, world!' });
-    return meetings;
+    return credentials;
 }
