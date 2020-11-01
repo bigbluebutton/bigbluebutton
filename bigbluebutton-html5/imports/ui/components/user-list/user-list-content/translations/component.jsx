9a6ea8ec132cb076244769829631838e3dc4c647
Dennis Beier <nanovim@gmail.com>
2020-11-01 15:53:35 +0100

    opening languages panel works now better

--- bigbluebutton-html5/imports/ui/components/user-list/user-list-content/translations/component.jsx
+++ bigbluebutton-html5/imports/ui/components/user-list/user-list-content/translations/component.jsx
@@ -4,9 +4,20 @@ import { defineMessages, injectIntl } from 'react-intl';
 
 class Translations extends Component{
     toggleLanguagesPanel = () => {
-
-        Session.get("openPanel") ===  "translations" ? Session.set('openPanel', 'userlist' ) : Session.set('openPanel', "translations" );
-        window.dispatchEvent(new Event('panelChanged'));
+        if(Session.get("openPanel") === "userlist"){
+            Session.set('openPanel', "translations" )
+            window.dispatchEvent(new Event('panelChanged'));
+        }else if(Session.get("openPanel") ===  "translations"){
+            Session.set('openPanel', 'userlist' );
+            window.dispatchEvent(new Event('panelChanged'));
+        }else {
+            Session.set('openPanel', 'userlist' );
+            window.dispatchEvent(new Event('panelChanged'));
+            setTimeout(function (){
+                Session.set('openPanel', 'translations' );
+                window.dispatchEvent(new Event('panelChanged'));
+            },200);
+        }
     };
     render() {
         return (
