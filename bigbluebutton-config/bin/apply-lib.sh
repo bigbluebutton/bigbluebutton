ebe9b3a7899d564a56d01258a0c3d8d21d85aeb0
Mario Jr <mariogasparoni@gmail.com>
2020-11-12 16:59:45 -0300

    Increase private key size for dtls-srtp.pem to 4096
    Using this larger value helps reducing ocurrences 1005/1010 errors for Chrome, avoiding an openssl's error which interrupts the dtls handshake (Chrome triggers "DTLS timeout expired" error)
    This key size is also the default value used by freeswitch on switch_core_cert.c

--- bigbluebutton-config/bin/apply-lib.sh
+++ bigbluebutton-config/bin/apply-lib.sh
@@ -124,7 +124,7 @@ enableMultipleKurentos() {
   # Step 1.  Setup shared certificate between FreeSWITCH and Kurento
 
   HOSTNAME=$(cat /etc/nginx/sites-available/bigbluebutton | grep -v '#' | sed -n '/server_name/{s/.*server_name[ ]*//;s/;//;p}' | cut -d' ' -f1 | head -n 1)
-  openssl req -x509 -new -nodes -newkey rsa:2048 -sha256 -days 3650 -subj "/C=BR/ST=Ottawa/O=BigBlueButton Inc./OU=Live/CN=$HOSTNAME" -keyout /tmp/dtls-srtp-key.pem -out /tmp/dtls-srtp-cert.pem
+  openssl req -x509 -new -nodes -newkey rsa:4096 -sha256 -days 3650 -subj "/C=BR/ST=Ottawa/O=BigBlueButton Inc./OU=Live/CN=$HOSTNAME" -keyout /tmp/dtls-srtp-key.pem -out /tmp/dtls-srtp-cert.pem
   cat /tmp/dtls-srtp-key.pem /tmp/dtls-srtp-cert.pem > /etc/kurento/dtls-srtp.pem
   cat /tmp/dtls-srtp-key.pem /tmp/dtls-srtp-cert.pem > /opt/freeswitch/etc/freeswitch/tls/dtls-srtp.pem
 
