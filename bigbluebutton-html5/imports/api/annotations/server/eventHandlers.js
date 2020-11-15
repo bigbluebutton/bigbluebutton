2e7cbd336a65126e4210f04665b40b7a0c762fd4
hiroshisuga <45039819+hiroshisuga@users.noreply.github.com>
2020-11-15 10:53:34 +0900

    Update eventHandlers.js

--- bigbluebutton-html5/imports/api/annotations/server/eventHandlers.js
+++ bigbluebutton-html5/imports/api/annotations/server/eventHandlers.js
@@ -1,11 +1,13 @@
 import RedisPubSub from '/imports/startup/server/redis';
 import { processForHTML5ServerOnly } from '/imports/api/common/server/helpers';
 import handleWhiteboardCleared from './handlers/whiteboardCleared';
+import handleWhiteboardReloaded from './handlers/whiteboardReloaded';
 import handleWhiteboardUndo from './handlers/whiteboardUndo';
 import handleWhiteboardSend from './handlers/whiteboardSend';
 import handleWhiteboardAnnotations from './handlers/whiteboardAnnotations';
 
 RedisPubSub.on('ClearWhiteboardEvtMsg', handleWhiteboardCleared);
+RedisPubSub.on('ReloadWhiteboardEvtMsg', handleWhiteboardReloaded);
 RedisPubSub.on('UndoWhiteboardEvtMsg', handleWhiteboardUndo);
 RedisPubSub.on('SendWhiteboardAnnotationEvtMsg', handleWhiteboardSend);
 RedisPubSub.on('GetWhiteboardAnnotationsRespMsg', processForHTML5ServerOnly(handleWhiteboardAnnotations));
