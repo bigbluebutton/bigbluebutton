bfe7de5207c646f39e9e27309a74f52312d7b627
hiroshisuga <45039819+hiroshisuga@users.noreply.github.com>
2020-11-15 10:58:18 +0900

    Create reloadWhiteboard.js

---
+++ bigbluebutton-html5/imports/api/annotations/server/methods/reloadWhiteboard.js
@@ -0,0 +1,20 @@
+import RedisPubSub from '/imports/startup/server/redis';
+import { Meteor } from 'meteor/meteor';
+import { check } from 'meteor/check';
+import { extractCredentials } from '/imports/api/common/server/helpers';
+
+export default function reloadWhiteboard(whiteboardId) {
+  const REDIS_CONFIG = Meteor.settings.private.redis;
+  const CHANNEL = REDIS_CONFIG.channels.toAkkaApps;
+  const EVENT_NAME = 'ReloadWhiteboardPubMsg';
+
+  const { meetingId, requesterUserId } = extractCredentials(this.userId);
+
+  check(whiteboardId, String);
+
+  const payload = {
+    whiteboardId,
+  };
+
+  return RedisPubSub.publishUserMessage(CHANNEL, EVENT_NAME, meetingId, requesterUserId, payload);
+}
