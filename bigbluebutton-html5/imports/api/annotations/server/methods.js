c75477d6769d007917975fb6d23835be2becca47
hiroshisuga <45039819+hiroshisuga@users.noreply.github.com>
2020-11-15 10:59:03 +0900

    Update methods.js

--- bigbluebutton-html5/imports/api/annotations/server/methods.js
+++ bigbluebutton-html5/imports/api/annotations/server/methods.js
@@ -1,12 +1,14 @@
 import { Meteor } from 'meteor/meteor';
 import undoAnnotation from './methods/undoAnnotation';
 import clearWhiteboard from './methods/clearWhiteboard';
+import reloadWhiteboard from './methods/reloadWhiteboard';
 import sendAnnotation from './methods/sendAnnotation';
 import sendBulkAnnotations from './methods/sendBulkAnnotations';
 
 Meteor.methods({
   undoAnnotation,
   clearWhiteboard,
+  reloadWhiteboard,
   sendAnnotation,
   sendBulkAnnotations,
 });
