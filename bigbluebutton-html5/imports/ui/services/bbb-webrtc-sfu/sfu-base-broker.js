a49aa0a1caf061acb9e4fbf0059ed569ebd71349
prlanzarin <prlanzarin@inf.ufrgs.br>
2020-12-01 18:06:50 +0000

    [html5/webrtc] Added proper error handling in bbb-webrtc-sfu base broker
    Moved bbb-webrtc-sfu utilitaries to properly named folder
    
    Logging improvements to base broker
    
    Added onerror/onstart/onended callback interfaces to base broker

--- bigbluebutton-html5/imports/ui/services/bbb-webrtc-sfu/sfu-base-broker.js
+++ bigbluebutton-html5/imports/ui/services/bbb-webrtc-sfu/sfu-base-broker.js
@@ -1,9 +1,21 @@
 import logger from '/imports/startup/client/logger';
-import { notifyStreamStateChange } from '/imports/ui/services/webrtc/stream-state-service';
+import { notifyStreamStateChange } from '/imports/ui/services/bbb-webrtc-sfu/stream-state-service';
+import SFU_BROKER_ERRORS from '/imports/ui/services/bbb-webrtc-sfu/broker-base-errors';
 
 const PING_INTERVAL_MS = 15000;
 
 class BaseBroker {
+  static assembleError(code, reason) {
+    const message = reason || SFU_BROKER_ERRORS[code];
+    const error = new Error(message);
+    error.errorCode = code;
+    // Duplicating key-vals because we can't settle on an error pattern... - prlanzarin
+    error.errorCause = error.message;
+    error.errorMessage = error.message;
+
+    return error;
+  }
+
   constructor(sfuComponent, wsUrl) {
     this.wsUrl = wsUrl;
     this.sfuComponent = sfuComponent;
@@ -11,7 +23,28 @@ class BaseBroker {
     this.webRtcPeer = null;
     this.pingInterval = null;
     this.started = false;
+    this.signallingTransportOpen = false;
     this.logCodePrefix = `${this.sfuComponent}_broker`;
+  }
+
+  set started (val) {
+    this._started = val;
+  }
+
+  get started () {
+    return this._started;
+  }
+
+  onstart () {
+    // To be implemented by inheritors
+  }
+
+  onerror (error) {
+    // To be implemented by inheritors
+  }
+
+  onended () {
+    // To be implemented by inheritors
   }
 
   openWSConnection () {
@@ -21,11 +54,8 @@ class BaseBroker {
       this.ws.onmessage = this.onWSMessage.bind(this);
 
       this.ws.onclose = () => {
-        logger.error({
-          logCode: `${this.logCodePrefix}_websocket_closed`,
-          extraInfo: { sfuComponent: this.sfuComponent },
-        }, 'WebSocket connection to SFU closed unexpectedly');
-        this.onFailure('Websocket connection closed');
+        // 1301: "WEBSOCKET_DISCONNECTED",
+        this.onerror(BaseBroker.assembleError(1301));
       };
 
       this.ws.onerror = (error) => {
@@ -35,13 +65,22 @@ class BaseBroker {
             errorMessage: error.name || error.message || 'Unknown error',
             sfuComponent: this.sfuComponent,
           }
-        }, 'Error in the WebSocket connection to SFU');
-        this.onFailure('Websocket connection error');
-        return reject(error);
+        }, 'WebSocket connection to SFU failed');
+
+        if (this.signallingTransportOpen) {
+          // 1301: "WEBSOCKET_DISCONNECTED", transport was already open
+          this.onerror(BaseBroker.assembleError(1301));
+        } else {
+          // 1302: "WEBSOCKET_CONNECTION_FAILED", transport errored before establishment
+          const normalized1302 = BaseBroker.assembleError(1302);
+          this.onerror(normalized1302);
+          return reject(normalized1302);
+        }
       };
 
       this.ws.onopen = () => {
         this.pingInterval = setInterval(this.ping.bind(this), PING_INTERVAL_MS);
+        this.signallingTransportOpen = true;
         return resolve();
       };
     });
@@ -53,18 +92,13 @@ class BaseBroker {
   }
 
   ping () {
-    const message = {
-      id: 'ping',
-    };
     this.sendMessage({ id: 'ping' });
   }
 
   processAnswer (message) {
     const { response, sdpAnswer, role, connectionId } = message;
 
-    if (response !== 'accepted') {
-      return this.handleSFUError(message);
-    }
+    if (response !== 'accepted') return this.handleSFUError(message);
 
     logger.debug({
       logCode: `${this.logCodePrefix}_start_success`,
@@ -86,8 +120,8 @@ class BaseBroker {
             sfuComponent: this.sfuComponent,
           }
         }, `Error processing SDP answer from SFU for ${this.sfuComponent}`);
-
-        return this.onFailure(error);
+        // 1305: "PEER_NEGOTIATION_FAILED",
+        return this.onerror(BaseBroker.assembleError(1305));
       }
 
       // Mark the peer as negotiated and flush the ICE queue
@@ -105,7 +139,7 @@ class BaseBroker {
     return options;
   }
 
-  handleConnectionStateChange (errorMessage, errorCode, eventIdentifier) {
+  handleConnectionStateChange (eventIdentifier) {
     if (this.webRtcPeer) {
       const { peerConnection } = this.webRtcPeer;
       const connectionState = peerConnection.connectionState;
@@ -116,13 +150,9 @@ class BaseBroker {
 
       if (connectionState === 'failed' || connectionState === 'closed') {
         this.webRtcPeer.peerConnection.onconnectionstatechange = null;
-        logger.error({
-          logCode: `${this.logCodePrefix}_connection_state_failed`,
-          extraInfo: {
-            errorCode, errorMessage, connectionState, sfuComponent: this.sfuComponent,
-          }
-        }, `RTCPeerConnection connectionState reached final state`);
-        this.onFailure({ errorMessage, errorCode });
+        // 1307: "ICE_STATE_FAILED",
+        const error = BaseBroker.assembleError(1307);
+        this.onerror(error);
       }
     }
   }
@@ -137,7 +167,9 @@ class BaseBroker {
           logCode: `${this.logCodePrefix}_addicecandidate_error`,
           extraInfo: {
             errorMessage: error.name || error.message || 'Unknown error',
+            errorCode: error.code || 'Unknown code',
             sfuComponent: this.sfuComponent,
+            started: this.started,
           }
         }, `Adding ICE candidate failed`);
       }
@@ -175,16 +207,15 @@ class BaseBroker {
   }
 
   stop () {
-    this.onSuccess = function(){};
-    this.onFailure = function(){};
-    this.started = false;
+    this.onstart = function(){};
+    this.onerror = function(){};
 
     if (this.webRtcPeer) {
       this.webRtcPeer.peerConnection.onconnectionstatechange = null;
     }
 
     if (this.ws !== null) {
-      this.ws.onclose = function () {};
+      this.ws.onclose = function (){};
       this.ws.close();
     }
 
@@ -193,11 +224,15 @@ class BaseBroker {
     }
 
     this.disposePeer();
+    this.started = false;
 
     logger.debug({
       logCode: `${this.logCodePrefix}_stop`,
       extraInfo: { sfuComponent: this.sfuComponent },
     }, `Stopped broker session for ${this.sfuComponent}`);
+
+    this.onended();
+    this.onended = function(){};
   }
 }
 
