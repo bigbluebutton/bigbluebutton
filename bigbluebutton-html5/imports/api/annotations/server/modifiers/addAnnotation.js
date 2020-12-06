2ee342133e046eed1d5bf900afa3b824f4eaed60
hiroshisuga <45039819+hiroshisuga@users.noreply.github.com>
2020-12-06 19:30:41 +0900

    Update addAnnotation.js

--- bigbluebutton-html5/imports/api/annotations/server/modifiers/addAnnotation.js
+++ bigbluebutton-html5/imports/api/annotations/server/modifiers/addAnnotation.js
@@ -2,13 +2,17 @@ import { check } from 'meteor/check';
 import Logger from '/imports/startup/server/logger';
 import Annotations from '/imports/api/annotations';
 import addAnnotationQuery from '/imports/api/annotations/addAnnotation';
+import WhiteboardMultiUser from '/imports/api/whiteboard-multi-user/';
 
 export default function addAnnotation(meetingId, whiteboardId, userId, annotation) {
   check(meetingId, String);
   check(whiteboardId, String);
   check(annotation, Object);
 
-  const query = addAnnotationQuery(meetingId, whiteboardId, userId, annotation);
+  const data = WhiteboardMultiUser.findOne({ meetingId, whiteboardId });
+  const currentMultiUser = data ? data.multiUser : 0;
+  
+  const query = addAnnotationQuery(meetingId, whiteboardId, userId, annotation, currentMultiUser);
 
   try {
     const { insertedId } = Annotations.upsert(query.selector, query.modifier);
