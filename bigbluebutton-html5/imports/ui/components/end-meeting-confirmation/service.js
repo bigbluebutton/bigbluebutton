3742746de300c0090ee5160871ce4dd494a1403c
Ramon Souza <contato@ramonsouza.com>
2021-01-20 17:49:07 -0300

    PR 9946 + suggested text changes

--- bigbluebutton-html5/imports/ui/components/end-meeting-confirmation/service.js
+++ bigbluebutton-html5/imports/ui/components/end-meeting-confirmation/service.js
@@ -1,7 +1,6 @@
 import Users from '/imports/api/users';
 import Auth from '/imports/ui/services/auth';
-
-const USERS_TO_DISPLAY = Meteor.settings.public.app.maxUsersForConfirmation;
+import Meetings from '/imports/api/meetings';
 
 const userFindSorting = {
   emojiTime: 1,
@@ -11,26 +10,25 @@ const userFindSorting = {
   userId: 1,
 };
 
+const getMeetingTitle = () => {
+  const meeting = Meetings.findOne({
+    meetingId: Auth.meetingID,
+  }, { fields: { 'meetingProp.name': 1, 'breakoutProps.sequence': 1 } });
+
+  return meeting.meetingProp.name;
+};
+
 const getUsers = () => {
-  let users = Users
+  const users = Users
     .find({
       meetingId: Auth.meetingID,
-      connectionStatus: 'online',
     }, userFindSorting)
     .fetch();
-
-  // user shouldn't see themselves in the list
-  users = users.filter(u => u.userId !== Auth.userID);
 
   return users;
 };
 
-const getUsersToDisplay = (users) => {
-  const diff = users.length - USERS_TO_DISPLAY;
-  return { displayUsers: users.slice(0, USERS_TO_DISPLAY), remainder: diff <= 0 ? false : diff };
-};
-
 export default {
   getUsers,
-  getUsersToDisplay,
+  getMeetingTitle,
 };
