0abdf00a25d2ff4a324567063c5215e2d9eecb83
KDSBrowne <kert.browne85@gmail.com>
2021-02-04 13:42:31 -0500

    Add new join param skip echo test on first join only (#11284)
    * add param to force echo test when user joins audio after init
    
    * fix UI stuck on connecting when userdata-bbb_auto_join_audio=false
    
    * fix conditions for joinFullAudioImmediately and joinFullAudioEchoTest | remove old format
    
    * remove extra param in getItem
    
    * recover audioLocked | only set getEchoTest if doesnt exist

--- bigbluebutton-html5/imports/api/users-settings/server/methods/addUserSettings.js
+++ bigbluebutton-html5/imports/api/users-settings/server/methods/addUserSettings.js
@@ -37,6 +37,7 @@ const currentParameters = [
   'bbb_force_listen_only',
   'bbb_listen_only_mode',
   'bbb_skip_check_audio',
+  'bbb_skip_check_audio_on_first_join',
   // BRANDING
   'bbb_display_branding_area',
   // SHORTCUTS
