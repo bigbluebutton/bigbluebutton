3d780453636249e4e9bd27a56acc9fa420e23538
Anton Georgiev <anto.georgiev@gmail.com>
2021-02-05 21:42:52 +0000

    Clear meeting-time-remaining collection

--- bigbluebutton-html5/imports/api/meetings/server/modifiers/meetingHasEnded.js
+++ bigbluebutton-html5/imports/api/meetings/server/modifiers/meetingHasEnded.js
@@ -20,6 +20,7 @@ import clearUserInfo from '/imports/api/users-infos/server/modifiers/clearUserIn
 import clearConnectionStatus from '/imports/api/connection-status/server/modifiers/clearConnectionStatus';
 import clearNote from '/imports/api/note/server/modifiers/clearNote';
 import clearNetworkInformation from '/imports/api/network-information/server/modifiers/clearNetworkInformation';
+import clearMeetingTimeRemaining from '/imports/api/meetings/server/modifiers/clearMeetingTimeRemaining';
 import clearLocalSettings from '/imports/api/local-settings/server/modifiers/clearLocalSettings';
 import clearRecordMeeting from './clearRecordMeeting';
 import clearVoiceCallStates from '/imports/api/voice-call-states/server/modifiers/clearVoiceCallStates';
@@ -49,6 +50,7 @@ export default function meetingHasEnded(meetingId) {
     clearNote(meetingId);
     clearNetworkInformation(meetingId);
     clearLocalSettings(meetingId);
+    clearMeetingTimeRemaining(meetingId);
     clearRecordMeeting(meetingId);
     clearVoiceCallStates(meetingId);
     clearVideoStreams(meetingId);
