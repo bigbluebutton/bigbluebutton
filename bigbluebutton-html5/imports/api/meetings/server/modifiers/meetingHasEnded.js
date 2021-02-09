811d6d3f983aed8edddf7c0f535fd910dbd87554
Joao Siebel <joaos_desenv@imdt.com.br>
2021-02-09 09:30:09 -0300

    Clear guestUsers, voiceCallStates and whiteboard-multi-user collections on meeting end

--- bigbluebutton-html5/imports/api/meetings/server/modifiers/meetingHasEnded.js
+++ bigbluebutton-html5/imports/api/meetings/server/modifiers/meetingHasEnded.js
@@ -8,6 +8,7 @@ import { removeCursorStreamer } from '/imports/api/cursor/server/streamer';
 import clearUsers from '/imports/api/users/server/modifiers/clearUsers';
 import clearUsersSettings from '/imports/api/users-settings/server/modifiers/clearUsersSettings';
 import clearGroupChat from '/imports/api/group-chat/server/modifiers/clearGroupChat';
+import clearGuestUsers from '/imports/api/guest-users/server/modifiers/clearGuestUsers';
 import clearBreakouts from '/imports/api/breakouts/server/modifiers/clearBreakouts';
 import clearAnnotations from '/imports/api/annotations/server/modifiers/clearAnnotations';
 import clearSlides from '/imports/api/slides/server/modifiers/clearSlides';
@@ -24,6 +25,7 @@ import clearRecordMeeting from './clearRecordMeeting';
 import clearVoiceCallStates from '/imports/api/voice-call-states/server/modifiers/clearVoiceCallStates';
 import clearVideoStreams from '/imports/api/video-streams/server/modifiers/clearVideoStreams';
 import clearAuthTokenValidation from '/imports/api/auth-token-validation/server/modifiers/clearAuthTokenValidation';
+import clearWhiteboardMultiUser from '/imports/api/whiteboard-multi-user/server/modifiers/clearWhiteboardMultiUser';
 import Metrics from '/imports/startup/server/metrics';
 
 export default function meetingHasEnded(meetingId) {
@@ -33,6 +35,7 @@ export default function meetingHasEnded(meetingId) {
   return Meetings.remove({ meetingId }, () => {
     clearCaptions(meetingId);
     clearGroupChat(meetingId);
+    clearGuestUsers(meetingId);
     clearPresentationPods(meetingId);
     clearBreakouts(meetingId);
     clearPolls(meetingId);
@@ -50,6 +53,7 @@ export default function meetingHasEnded(meetingId) {
     clearVoiceCallStates(meetingId);
     clearVideoStreams(meetingId);
     clearAuthTokenValidation(meetingId);
+    clearWhiteboardMultiUser(meetingId);
     BannedUsers.delete(meetingId);
     Metrics.removeMeeting(meetingId);
 
