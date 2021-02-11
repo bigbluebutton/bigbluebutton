46e230104f67ad8e6566c582eab52d53cc2ab392
hiroshisuga <45039819+hiroshisuga@users.noreply.github.com>
2021-02-11 14:28:06 +0900

    Update MeetingService.java

--- bbb-common-web/src/main/java/org/bigbluebutton/api/MeetingService.java
+++ bbb-common-web/src/main/java/org/bigbluebutton/api/MeetingService.java
@@ -49,6 +49,7 @@ import org.bigbluebutton.api.domain.Recording;
 import org.bigbluebutton.api.domain.RegisteredUser;
 import org.bigbluebutton.api.domain.User;
 import org.bigbluebutton.api.domain.UserSession;
+import org.bigbluebutton.api.domain.UploadedFile;
 import org.bigbluebutton.api.messaging.MessageListener;
 import org.bigbluebutton.api.messaging.converters.messages.DestroyMeetingMessage;
 import org.bigbluebutton.api.messaging.converters.messages.EndMeetingMessage;
@@ -66,6 +67,7 @@ import org.bigbluebutton.api.messaging.messages.MakePresentationDownloadableMsg;
 import org.bigbluebutton.api.messaging.messages.MeetingDestroyed;
 import org.bigbluebutton.api.messaging.messages.MeetingEnded;
 import org.bigbluebutton.api.messaging.messages.MeetingStarted;
+import org.bigbluebutton.api.messaging.messages.UploadRequest;
 import org.bigbluebutton.api.messaging.messages.PresentationUploadToken;
 import org.bigbluebutton.api.messaging.messages.RecordChapterBreak;
 import org.bigbluebutton.api.messaging.messages.RegisterUser;
@@ -285,6 +287,33 @@ public class MeetingService implements MessageListener {
   private void kickOffProcessingOfRecording(Meeting m) {
     if (m.isRecord() && m.getNumUsers() == 0) {
       processRecording(m);
+    }
+  }
+  
+  public Boolean isUploadRequestValid(String meetingId, String source, String filename, String userId, String token) {
+    Meeting m = getMeeting(meetingId);
+    if (m != null) {
+      return m.isUploadRequestValid(source, filename, userId, token);
+    } else {
+      return false;
+    }
+  }
+
+  public Boolean isDownloadRequestValid(String meetingId, String source, String uploadId) {
+    Meeting m = getMeeting(meetingId);
+    if (m != null) {
+      return m.hasUploadedFile(source, uploadId);
+    } else {
+      return false;
+    }
+  }
+
+  public UploadedFile getUploadedFile(String meetingId, String uploadId) {
+    Meeting m = getMeeting(meetingId);
+    if (m != null) {
+      return m.getUploadedFile(uploadId);
+    } else {
+      return null;
     }
   }
 
@@ -608,10 +637,33 @@ public class MeetingService implements MessageListener {
     }
   }
 
+  public void fileUploaded(
+      String uploadId,
+      String source,
+      String filename,
+      String contentType,
+      String extension,
+      String userId,
+      String meetingId
+    ) {
+    Meeting m = getMeeting(meetingId);
+    if (m != null) {
+      m.addUploadedFile(source, filename, contentType, extension, uploadId);
+      gw.fileUploaded(uploadId, source, filename, contentType, userId, meetingId);
+    }
+  }
+
   public void endMeeting(String meetingId) {
     handle(new EndMeeting(meetingId));
   }
 
+  private void processUploadRequest(UploadRequest message) {
+    Meeting m = getMeeting(message.meetingId);
+    if (m != null) {
+      m.addUploadRequest(message.source, message.filename, message.userId, message.token);
+    }
+  }
+  
   private void processCreateBreakoutRoom(CreateBreakoutRoom message) {
     Meeting parentMeeting = getMeeting(message.parentMeetingId);
     if (parentMeeting != null) {
@@ -1079,6 +1131,8 @@ public class MeetingService implements MessageListener {
           processRegisterUser((RegisterUser) message);
         } else if (message instanceof CreateBreakoutRoom) {
           processCreateBreakoutRoom((CreateBreakoutRoom) message);
+        } else if (message instanceof UploadRequest) {
+          processUploadRequest((UploadRequest) message);
         } else if (message instanceof PresentationUploadToken) {
           processPresentationUploadToken((PresentationUploadToken) message);
         } else if (message instanceof GuestStatusChangedEventMsg) {
