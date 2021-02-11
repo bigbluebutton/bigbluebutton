836682acc29be612ea289a2a2a468b51738cd70f
hiroshisuga <45039819+hiroshisuga@users.noreply.github.com>
2021-02-11 21:10:40 +0900

    Update archive.rb

--- record-and-playback/core/scripts/archive/archive.rb
+++ record-and-playback/core/scripts/archive/archive.rb
@@ -167,6 +167,7 @@ audio_dir = props['raw_audio_src']
 recording_dir = props['recording_dir']
 raw_archive_dir = "#{recording_dir}/raw"
 deskshare_dir = props['raw_deskshare_src']
+# Note: modify directly /usr/local/bigbluebutton/core/scripts/archive/archive.rb
 upload_dir = props['raw_upload_src']
 screenshare_dir = props['raw_screenshare_src']
 redis_host = props['redis_host']
@@ -206,6 +207,7 @@ archive_directory("#{video_dir}/#{meeting_id}", "#{target_dir}/video/#{meeting_i
 # Kurento media
 archive_directory("#{kurento_screenshare_dir}/#{meeting_id}", "#{target_dir}/deskshare")
 archive_directory("#{kurento_video_dir}/#{meeting_id}", "#{target_dir}/video/#{meeting_id}")
+# Note: modify directly /usr/local/bigbluebutton/core/scripts/archive/archive.rb
 # Uploaded media
 archive_directory("#{upload_dir}/#{meeting_id}", "#{target_dir}/upload")
 
