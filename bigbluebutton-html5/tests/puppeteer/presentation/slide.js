9fdcfb2c655f92ca96b8a33162d503c2b08212de
Mohamed Amine Ben Salah <damineone@gmail.com>
2021-02-13 00:56:07 -0300

    update presentation test suite

--- bigbluebutton-html5/tests/puppeteer/presentation/slide.js
+++ bigbluebutton-html5/tests/puppeteer/presentation/slide.js
@@ -1,6 +1,7 @@
 const Page = require('../core/page');
 const e = require('./elements');
 const we = require('../whiteboard/elements');
+const { ELEMENT_WAIT_LONGER_TIME, ELEMENT_WAIT_TIME } = require('../core/constants');
 
 class Slide extends Page {
   constructor() {
@@ -8,25 +9,24 @@ class Slide extends Page {
   }
 
   async test() {
-        
-    await this.waitForSelector(we.whiteboard);
-    await this.waitForSelector(e.presentationToolbarWrapper);
-    
-    const svg0 = await this.page.evaluate(async() => await document.querySelector('svg g g g').outerHTML.indexOf('/svg/1') !== -1);
+    await this.waitForSelector(we.whiteboard, ELEMENT_WAIT_LONGER_TIME);
+    await this.waitForSelector(e.presentationToolbarWrapper, ELEMENT_WAIT_TIME);
 
-    await this.waitForSelector(e.nextSlide);
+    const svg0 = await this.page.evaluate(async () => await document.querySelector('svg g g g').outerHTML.indexOf('/svg/1') !== -1);
+
+    await this.waitForSelector(e.nextSlide, ELEMENT_WAIT_TIME);
     await this.click(e.nextSlide, true);
-    await this.waitForSelector(we.whiteboard);
-    await this.page.waitFor(1000)
-    
-    const svg1 = await this.page.evaluate(async() => await document.querySelector('svg g g g').outerHTML.indexOf('/svg/2') !== -1);
+    await this.waitForSelector(we.whiteboard, ELEMENT_WAIT_TIME);
+    await this.page.waitFor(1000);
 
-    await this.waitForSelector(e.prevSlide);
+    const svg1 = await this.page.evaluate(async () => await document.querySelector('svg g g g').outerHTML.indexOf('/svg/2') !== -1);
+
+    await this.waitForSelector(e.prevSlide, ELEMENT_WAIT_TIME);
     await this.click(e.prevSlide, true);
-    await this.waitForSelector(we.whiteboard);
-    await this.page.waitFor(1000)
+    await this.waitForSelector(we.whiteboard, ELEMENT_WAIT_TIME);
+    await this.page.waitFor(1000);
 
-    const svg2 = await this.page.evaluate(async() => await document.querySelector('svg g g g').outerHTML.indexOf('/svg/1') !== -1);
+    const svg2 = await this.page.evaluate(async () => await document.querySelector('svg g g g').outerHTML.indexOf('/svg/1') !== -1);
 
     return svg0 === true && svg1 === true && svg2 === true;
   }
