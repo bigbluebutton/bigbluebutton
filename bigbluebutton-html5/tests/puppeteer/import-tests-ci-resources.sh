fe1fc5ded30de0a99e29f55f4344ceeb145f7a66
Mohamed Amine Ben Salah <damineone@gmail.com>
2021-02-18 10:38:41 -0300

    clone and import media and visual regressions files to the right directories

---
+++ bigbluebutton-html5/tests/puppeteer/import-tests-ci-resources.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+
+testDir=$PWD/tests/puppeteer/;
+repoDir=bbb-ci-tests-resources/;
+
+echo "Cloning bbb-ci-tests-resources repo...";
+git clone https://github.com/daminebenq/bbb-ci-tests-resources.git;
+echo "bbb-ci-tests-resources has been imported.";
+
+sleep 2;
+echo "Importing browser media files...";
+mv -f $repoDir/media $testDir;
+
+if [[ $REGRESSION_TESTING = true ]]; then
+    echo "Importing Visual Regressions Testing Files...";
+    sleep 1;
+    mv -f $repoDir/__image_snapshots__ $testDir;
+    echo "Visual Regressions Testing Files has been imported."
+fi
+rm -rf bbb-ci-tests-resources;
