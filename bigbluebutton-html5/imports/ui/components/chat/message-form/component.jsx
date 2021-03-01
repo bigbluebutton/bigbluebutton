2dedf197e008553e2a99fae8df95816b961985dd
Ramon Souza <contato@ramonsouza.com>
2021-03-01 15:36:09 -0300

    fix textarea resizing bug

--- bigbluebutton-html5/imports/ui/components/chat/message-form/component.jsx
+++ bigbluebutton-html5/imports/ui/components/chat/message-form/component.jsx
@@ -297,6 +297,7 @@ class MessageForm extends PureComponent {
             value={message}
             onChange={this.handleMessageChange}
             onKeyDown={this.handleMessageKeyDown}
+            async={true}
           />
           <Button
             hideLabel
