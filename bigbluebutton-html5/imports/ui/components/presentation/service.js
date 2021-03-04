47a21dfd98b93574f68be65c509996d0d1ef850e
Ramon Souza <contato@ramonsouza.com>
2021-03-04 13:34:23 -0300

    quick poll fix

--- bigbluebutton-html5/imports/ui/components/presentation/service.js
+++ bigbluebutton-html5/imports/ui/components/presentation/service.js
@@ -85,16 +85,15 @@ const parseCurrentSlideContent = (yesValue, noValue, abstentionValue, trueValue,
   let optionsPoll = content.match(pollRegex) || [];
   if (optionsPoll) optionsPoll = optionsPoll.map(opt => `\r${opt[0]}.`);
 
-  const excludePatt = '[^.)]';
-  const ynPollString = `(${excludePatt}${yesValue}\\s*\\/\\s*${noValue})|(${excludePatt}${noValue}\\s*\\/\\s*${yesValue})`;
+  const ynPollString = `(${yesValue}\\s*\\/\\s*${noValue})|(${noValue}\\s*\\/\\s*${yesValue})`;
   const ynOptionsRegex = new RegExp(ynPollString, 'gi');
   const ynPoll = content.match(ynOptionsRegex) || [];
 
-  const ynaPollString = `(${excludePatt}${yesValue}\\s*\\/\\s*${noValue}\\s*\\/\\s*${abstentionValue})|(${excludePatt}${yesValue}\\s*\\/\\s*${abstentionValue}\\s*\\/\\s*${noValue})|(${excludePatt}${abstentionValue}\\s*\\/\\s*${yesValue}\\s*\\/\\s*${noValue})|(${excludePatt}${abstentionValue}\\s*\\/\\s*${noValue}\\s*\\/\\s*${yesValue})|(${excludePatt}${noValue}\\s*\\/\\s*${yesValue}\\s*\\/\\s*${abstentionValue})|(${excludePatt}${noValue}\\s*\\/\\s*${abstentionValue}\\s*\\/\\s*${yesValue})`;
+  const ynaPollString = `(${yesValue}\\s*\\/\\s*${noValue}\\s*\\/\\s*${abstentionValue})|(${yesValue}\\s*\\/\\s*${abstentionValue}\\s*\\/\\s*${noValue})|(${abstentionValue}\\s*\\/\\s*${yesValue}\\s*\\/\\s*${noValue})|(${abstentionValue}\\s*\\/\\s*${noValue}\\s*\\/\\s*${yesValue})|(${noValue}\\s*\\/\\s*${yesValue}\\s*\\/\\s*${abstentionValue})|(${noValue}\\s*\\/\\s*${abstentionValue}\\s*\\/\\s*${yesValue})`;
   const ynaOptionsRegex = new RegExp(ynaPollString, 'gi');
   const ynaPoll = content.match(ynaOptionsRegex) || [];
 
-  const tfPollString = `(${excludePatt}${trueValue}\\s*\\/\\s*${falseValue})|(${excludePatt}${falseValue}\\s*\\/\\s*${trueValue})`;
+  const tfPollString = `(${trueValue}\\s*\\/\\s*${falseValue})|(${falseValue}\\s*\\/\\s*${trueValue})`;
   const tgOptionsRegex = new RegExp(tfPollString, 'gi');
   const tfPoll = content.match(tgOptionsRegex) || [];
 
