7a5312d63be217a7a451519e4fd004c2c37f2022
Dennis Beier <nanovim@gmail.com>
2021-03-10 23:10:19 +0100

    added new version string

--- bigbluebutton-html5/imports/ui/components/about/container.jsx
+++ bigbluebutton-html5/imports/ui/components/about/container.jsx
@@ -11,7 +11,7 @@ const AboutContainer = props => (
 
 const getClientBuildInfo = function () {
   return {
-    clientBuild: "Fairblue  07032021",
+    clientBuild: "Fairblue  10032021",
     copyright: "LGPL",
   };
 };
