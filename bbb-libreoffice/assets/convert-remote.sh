e430df0a31b0da6f6d9b210bd494227a59e80de2
Gustavo Trott <gustavo@trott.com.br>
2021-04-19 13:36:03 -0300

    Apply security enhancements for libreoffice conversion scripts

--- bbb-libreoffice/assets/convert-remote.sh
+++ bbb-libreoffice/assets/convert-remote.sh
@@ -1,4 +1,8 @@
-#/bin/bash
+#!/bin/bash
+set -e
+set -u
+PATH="/bin/:/usr/bin/"
+
 # This is a sample script - adjust it per your need
 # 1 - setup a server with JOD-CONVERTER-REST ( docker run --memory 512m --rm -p 8080:8080 eugenmayer/jodconverter:rest )
 # 2 - replace the HOST information in below command with your server host
@@ -8,9 +12,21 @@
 # Param 2: Output pdf file path (e.g. "/tmp/test.pdf")
 # Param 3: Destination Format (pdf default)
 
+if (( $# == 0 )); then
+	echo "Missing parameter 1 (Input office file path)";
+	exit 1
+elif (( $# == 1 )); then
+	echo "Missing parameter 2 (Output pdf file path)";
+	exit 1
+fi;
+
+
+source=${1}
+dest=${2}
+
 #If output format is missing, define PDF
 convertTo="${3:-pdf}"
 
-curl -X POST "http://127.0.0.1:8080/lool/convert-to/$convertTo" -H "accept: application/octet-stream" -H "Content-Type: multipart/form-data" -F "data=@$1" > $2
+curl -X POST "http://127.0.0.1:8080/lool/convert-to/$convertTo" -H "accept: application/octet-stream" -H "Content-Type: multipart/form-data" -F "data=@${source}" > ${dest}
 
 exit 0
