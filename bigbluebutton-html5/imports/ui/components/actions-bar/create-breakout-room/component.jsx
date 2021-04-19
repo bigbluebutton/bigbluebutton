f46f7961e383c8c3b21f7e3c4b15b7b1a86b0a38
KDSBrowne <kert.browne85@gmail.com>
2021-04-19 00:46:07 +0000

    make breakout room assignment accessible via keyboard

--- bigbluebutton-html5/imports/ui/components/actions-bar/create-breakout-room/component.jsx
+++ bigbluebutton-html5/imports/ui/components/actions-bar/create-breakout-room/component.jsx
@@ -1,5 +1,6 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
+import { findDOMNode } from 'react-dom';
 import { defineMessages, injectIntl } from 'react-intl';
 import _ from 'lodash';
 import cx from 'classnames';
@@ -181,6 +182,9 @@ class BreakoutRoom extends PureComponent {
     };
 
     this.btnLevelId = _.uniqueId('btn-set-level-');
+
+    this.handleMoveEvent = this.handleMoveEvent.bind(this);
+    this.handleShiftUser = this.handleShiftUser.bind(this);
   }
 
   componentDidMount() {
@@ -196,7 +200,38 @@ class BreakoutRoom extends PureComponent {
     }
   }
 
+  handleShiftUser(activeListSibling) {
+    const { users } = this.state;
+    if (activeListSibling) {
+      const text = activeListSibling.getElementsByTagName('p')[0].innerText;
+      const roomNumber = text.match(/\d/g).join("");
+      users.forEach(u => {
+        const { childNodes } = document.activeElement; 
+        if (!childNodes[childNodes.length - 1]) return;
+        if (u.userId === childNodes[childNodes.length - 1].id) {
+          u.room = text.substr(text.length - 1).includes(')') ? 0 : parseInt(roomNumber);
+        }
+      })
+    }
+  }
+
+  handleMoveEvent(event) {
+    if (this.listOfUsers) {
+      const { parentElement } = document.activeElement; 
+      if (event.key.includes('ArrowRight')) this.handleShiftUser(parentElement.nextSibling);
+      if (event.key.includes('ArrowLeft')) this.handleShiftUser(parentElement.previousSibling);
+      this.setRoomUsers();
+    }
+  }
+
   componentDidUpdate(prevProps, prevstate) {
+    if (this.listOfUsers) {
+      for(let i = 0; i < this.listOfUsers.children.length; i++) {
+        const roomList = this.listOfUsers.children[i].getElementsByTagName('div')[0];
+        roomList.addEventListener('keydown', this.handleMoveEvent, true);
+      }
+    }
+
     const { numberOfRooms } = this.state;
     const { users } = this.props;
     const { users: prevUsers } = prevProps;
@@ -433,12 +468,12 @@ class BreakoutRoom extends PureComponent {
     };
 
     return (
-      <div className={styles.boxContainer} key="rooms-grid-">
+      <div className={styles.boxContainer} key="rooms-grid-" ref={r => this.listOfUsers = r }>
         <div className={!valid ? styles.changeToWarn : null}>
           <p className={styles.freeJoinLabel}>
             {intl.formatMessage(intlMessages.notAssigned, { 0: this.getUserByRoom(0).length })}
           </p>
-          <div className={styles.breakoutBox} onDrop={drop(0)} onDragOver={allowDrop}>
+          <div className={styles.breakoutBox} onDrop={drop(0)} onDragOver={allowDrop} tabIndex={0}>
             {this.renderUserItemByRoom(0)}
           </div>
           <span className={valid ? styles.dontShow : styles.leastOneWarn}>
@@ -451,7 +486,7 @@ class BreakoutRoom extends PureComponent {
               <p className={styles.freeJoinLabel}>
                 {intl.formatMessage(intlMessages.roomLabel, { 0: (value) })}
               </p>
-              <div className={styles.breakoutBox} onDrop={drop(value)} onDragOver={allowDrop}>
+              <div className={styles.breakoutBox} onDrop={drop(value)} onDragOver={allowDrop} tabIndex={0}>
                 {this.renderUserItemByRoom(value)}
                 {isInvitation && this.renderJoinedUsers(value)}
               </div>
