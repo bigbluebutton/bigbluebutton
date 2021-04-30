b73a171ab424e61f9748403dd494fe5986abc955
Daniel Molkentin <daniel@molkentin.de>
2021-04-30 02:18:42 +0200

    fix(bbb-libreoffice) Do not try to install anything while apt holds the lock
    This will fail silently, with the package install still seemingly succeeding,
    resulting in unexpected behavior.

--- bbb-libreoffice/install-local.sh
+++ bbb-libreoffice/install-local.sh
@@ -6,21 +6,6 @@ fi;
 
 cd "$(dirname "$0")"
 
-if command -v docker &>/dev/null; then
-	echo "Docker already installed";
-else
-	echo "Docker not found";
-	apt update;
-	apt install apt-transport-https ca-certificates curl software-properties-common
-	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-	add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-	apt update
-	apt install docker-ce -y
-	systemctl enable docker
-	systemctl start docker
-	systemctl status docker
-fi
-
 if docker image inspect bbb-soffice &>/dev/null; then
 	echo "Docker image already exists";
 else
@@ -47,20 +32,3 @@ else
 	cp assets/zzz-bbb-docker-libreoffice /etc/sudoers.d/zzz-bbb-docker-libreoffice
 fi;
 
-aptInstalledList=$(apt list --installed "fonts*" 2>/dev/null|awk -F'/' 'NR>1{print $1}')
-fontInstalled=0
-
-for font in fonts-arkpandora fonts-crosextra-carlito fonts-crosextra-caladea fonts-noto fonts-noto-cjk fonts-liberation fonts-arkpandora
-do
-	if [[ -n $(echo "$aptInstalledList" | grep "$font") ]]; then
-		echo "Font $font already installed"
-	else
-		echo "Font $font doesn't exists, installing"
-		apt install -y --no-install-recommends $font
-		fontInstalled=1
-	fi
-done
-
-if [ $fontInstalled = "1" ]; then
-	dpkg-reconfigure fontconfig && fc-cache -f -s -v
-fi
