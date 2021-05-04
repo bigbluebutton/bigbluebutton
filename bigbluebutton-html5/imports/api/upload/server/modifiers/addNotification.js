cc2fc545cf6e6f63eddc74aecdc96d30ddae5267
hiroshisuga <45039819+hiroshisuga@users.noreply.github.com>
2021-05-05 08:23:29 +0900

    Update addNotification.js

--- bigbluebutton-html5/imports/api/upload/server/modifiers/addNotification.js
+++ bigbluebutton-html5/imports/api/upload/server/modifiers/addNotification.js
@@ -6,6 +6,8 @@ import { GroupChatMsg } from '/imports/api/group-chat-msg';
 const CHAT = Meteor.settings.public.chat;
 const SYSTEM_ID = CHAT.type_system;
 const CHAT_ID = 'MAIN-PUBLIC-GROUP-CHAT';
+const CHAT_CONFIG = Meteor.settings.public.chat;
+const SYSTEM_CHAT_TYPE = CHAT_CONFIG.type_system;
 
 export default function addUploadedFileMsg(meetingId, userId, uploadId, source, filename) {
   check(meetingId, String);
@@ -25,7 +27,7 @@ export default function addUploadedFileMsg(meetingId, userId, uploadId, source, 
   const selector = {
     meetingId,
     chatId: CHAT_ID,
-    id: `upload-msg-${now}`,
+    id: `${SYSTEM_CHAT_TYPE}-upload-msg-${now}`,
   };
 
   const modifier = {
