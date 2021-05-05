84e1656bce423a53fda10be4d849b1a80d5d0a84
Joao Siebel <joaos_desenv@imdt.com.br>
2021-05-05 10:41:56 -0300

    Add try/catch on note methods

--- bigbluebutton-html5/imports/api/note/server/methods/createNote.js
+++ bigbluebutton-html5/imports/api/note/server/methods/createNote.js
@@ -14,41 +14,45 @@ import addNote from '/imports/api/note/server/modifiers/addNote';
 import axios from 'axios';
 
 export default function createNote(meetingId, instanceId) {
-  // Avoid note creation if this feature is disabled
-  if (!isEnabled()) {
-    Logger.warn(`Shared notes are disabled`);
-    return;
-  }
-
-  check(meetingId, String);
-  check(instanceId, Number);
-
-  const noteId = withInstaceId(instanceId, generatePadId(meetingId));
-
-  const createURL = createPadURL(noteId);
-
-  axios({
-    method: 'get',
-    url: createURL,
-    responseType: 'json',
-  }).then((responseOuter) => {
-    const { status } = responseOuter;
-    if (status !== 200) {
-      Logger.error(`Could not get note info for ${meetingId} ${status}`);
+  try {
+    // Avoid note creation if this feature is disabled
+    if (!isEnabled()) {
+      Logger.warn('Shared notes are disabled');
       return;
     }
-    const readOnlyURL = getReadOnlyIdURL(noteId);
+
+    check(meetingId, String);
+    check(instanceId, Number);
+
+    const noteId = withInstaceId(instanceId, generatePadId(meetingId));
+
+    const createURL = createPadURL(noteId);
+
     axios({
       method: 'get',
-      url: readOnlyURL,
+      url: createURL,
       responseType: 'json',
-    }).then((response) => {
-      const readOnlyNoteId = getDataFromResponse(response.data, 'readOnlyID');
-      if (readOnlyNoteId) {
-        addNote(meetingId, noteId, readOnlyNoteId);
-      } else {
-        Logger.error(`Could not get note readOnlyID for ${meetingId}`);
+    }).then((responseOuter) => {
+      const { status } = responseOuter;
+      if (status !== 200) {
+        Logger.error(`Could not get note info for ${meetingId} ${status}`);
+        return;
       }
-    }).catch(error => Logger.error(`Could not get note readOnlyID for ${meetingId}: ${error}`));
-  }).catch(error => Logger.error(`Could not create note for ${meetingId}: ${error}`));
+      const readOnlyURL = getReadOnlyIdURL(noteId);
+      axios({
+        method: 'get',
+        url: readOnlyURL,
+        responseType: 'json',
+      }).then((response) => {
+        const readOnlyNoteId = getDataFromResponse(response.data, 'readOnlyID');
+        if (readOnlyNoteId) {
+          addNote(meetingId, noteId, readOnlyNoteId);
+        } else {
+          Logger.error(`Could not get note readOnlyID for ${meetingId}`);
+        }
+      }).catch((error) => Logger.error(`Could not get note readOnlyID for ${meetingId}: ${error}`));
+    }).catch((error) => Logger.error(`Could not create note for ${meetingId}: ${error}`));
+  } catch (err) {
+    Logger.error(`Exception while invoking method createNote ${err.stack}`);
+  }
 }
