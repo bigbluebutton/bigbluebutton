8da630da03ac59a04610190f2619829fea9fded2
Ramon Souza <contato@ramonsouza.com>
2021-06-14 09:55:18 -0300

    fix presentation service isPresenter

--- bigbluebutton-html5/imports/ui/components/presentation/presentation-uploader/container.jsx
+++ bigbluebutton-html5/imports/ui/components/presentation/presentation-uploader/container.jsx
@@ -4,9 +4,8 @@ import { withTracker } from 'meteor/react-meteor-data';
 import ErrorBoundary from '/imports/ui/components/error-boundary/component';
 import FallbackModal from '/imports/ui/components/fallback-errors/fallback-modal/component';
 import Service from './service';
+import PresentationService from '../service';
 import PresentationUploader from './component';
-import { withUsersConsumer } from '/imports/ui/components/components-data/users-context/context';
-import Auth from '/imports/ui/services/auth';
 
 const PRESENTATION_CONFIG = Meteor.settings.public.presentation;
 
@@ -19,15 +18,13 @@ const PresentationUploaderContainer = (props) => (
   )
 );
 
-export default withUsersConsumer(withTracker(({ users }) => {
+export default withTracker(() => {
   const currentPresentations = Service.getPresentations();
   const {
     dispatchDisableDownloadable,
     dispatchEnableDownloadable,
     dispatchTogglePresentationDownloadable,
   } = Service;
-
-  const currentUser = users[Auth.meetingID][Auth.userID];
 
   return {
     presentations: currentPresentations,
@@ -44,6 +41,6 @@ export default withUsersConsumer(withTracker(({ users }) => {
     dispatchTogglePresentationDownloadable,
     isOpen: Session.get('showUploadPresentationView') || false,
     selectedToBeNextCurrent: Session.get('selectedToBeNextCurrent') || null,
-    isPresenter: currentUser.presenter,
+    isPresenter: PresentationService.isPresenter('DEFAULT_PRESENTATION_POD'),
   };
-})(PresentationUploaderContainer));
+})(PresentationUploaderContainer);
