4b003946dc812be6cf1e4517392b6745d8343623
Martin Klampfer <martin.klampfer@fairkom.eu>
2021-06-15 12:58:39 +0200

    added rxjs to package.json
    disabled "you are muted" overlay

--- bigbluebutton-html5/imports/ui/services/audio-manager/index.js
+++ bigbluebutton-html5/imports/ui/services/audio-manager/index.js
@@ -857,6 +857,7 @@ class AudioManager {
   }
 
   async openTranslatorChannel(languageExtension, onConnected) {
+
     if( this.translatorBridge.activeSession ) {
       this.translatorChannelOpen = false;
       this.translatorBridge.exitAudio();
