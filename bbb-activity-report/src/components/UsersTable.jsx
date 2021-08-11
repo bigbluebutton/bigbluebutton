79af41c2c1bbb65d8b0f168f45771643ddb79c9a
Gustavo Trott <gustavo@trott.com.br>
2021-08-11 18:38:36 -0300

    Fix showing leftOn wrong date

--- bbb-activity-report/src/components/UsersTable.jsx
+++ bbb-activity-report/src/components/UsersTable.jsx
@@ -117,30 +117,38 @@ class UsersTable extends React.Component {
                             minute="2-digit"
                             second="2-digit"
                           />
-                          <br />
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-4 w-4 inline"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                          >
-                            <path
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              strokeWidth="2"
-                              d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
-                            />
-                          </svg>
-                          <FormattedDate
-                            value={user.leftOn}
-                            month="short"
-                            day="numeric"
-                            hour="2-digit"
-                            minute="2-digit"
-                            second="2-digit"
-                          />
                         </p>
+                        {
+                            user.leftOn > 0
+                              ? (
+                                <p className="text-xs text-gray-600 dark:text-gray-400">
+                                  <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="h-4 w-4 inline"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                  >
+                                    <path
+                                      strokeLinecap="round"
+                                      strokeLinejoin="round"
+                                      strokeWidth="2"
+                                      d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
+                                    />
+                                  </svg>
+
+                                  <FormattedDate
+                                    value={user.leftOn}
+                                    month="short"
+                                    day="numeric"
+                                    hour="2-digit"
+                                    minute="2-digit"
+                                    second="2-digit"
+                                  />
+                                </p>
+                              )
+                              : null
+                          }
                       </div>
                     </div>
                   </td>
