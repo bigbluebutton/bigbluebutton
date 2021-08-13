1c3d3031084ab80462e0358bc033c7adec497d3b
Johannes Barthel <johannes.barthel@student.kit.edu>
2021-08-13 14:07:54 +0200

    remove centos/amazon linux related files

--- build/packages-template/bbb-freeswitch-core/opts-centos7.sh
+++
@@ -1,4 +0,0 @@
-. ./opts-global.sh
-
-OPTS="$OPTS -t rpm -d speex --rpm-dist el7 --rpm-user freeswitch --rpm-group daemon --rpm-use-file-permissions"
-
--- build/packages-template/bbb-libreoffice-docker/opts-amzn2.sh
+++
@@ -1,5 +0,0 @@
-. ./opts-global.sh
-
-OPTS="$OPTS -t rpm --rpm-dist el7 -d net-tools"
-
-
