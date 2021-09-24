f9dbefe76479c1daf5ba943fee728bf7bb4a317c
Mario Jr <mariogasparoni@gmail.com>
2021-09-24 15:11:53 -0300

    feat(audio): basic funcionality for fullaudio bridge
    This commit allows user to join/leave audio using the fullaudio bridge.
    This is still under development, but to use this now we must set values of
    skipCheck to false, and defaultFullAudioBridge to fullaudio. This
    depends on newest version of bbb-webrtc-sfu

--- bigbluebutton-html5/imports/api/audio/client/bridge/fullaudio.js
+++ bigbluebutton-html5/imports/api/audio/client/bridge/fullaudio.js
@@ -1,11 +1,11 @@
 import BaseAudioBridge from './base';
 import Auth from '/imports/ui/services/auth';
 import logger from '/imports/startup/client/logger';
-import ListenOnlyBroker from '/imports/ui/services/bbb-webrtc-sfu/listenonly-broker';
+import FullAudioBroker from '/imports/ui/services/bbb-webrtc-sfu/fullaudio-broker';
 import loadAndPlayMediaStream from '/imports/ui/services/bbb-webrtc-sfu/load-play';
 import {
   fetchWebRTCMappedStunTurnServers,
-  getMappedFallbackStun
+  getMappedFallbackStun,
 } from '/imports/utils/fetchStunTurnServers';
 import getFromMeetingSettings from '/imports/ui/services/meeting-settings';
 
@@ -16,8 +16,10 @@ const MEDIA_TAG = MEDIA.mediaTag.replace(/#/g, '');
 const GLOBAL_AUDIO_PREFIX = 'GLOBAL_AUDIO_';
 const RECONNECT_TIMEOUT_MS = MEDIA.listenOnlyCallTimeout || 15000;
 const OFFERING = MEDIA.listenOnlyOffering;
+const SENDRECV_ROLE = 'sendrecv';
 const RECV_ROLE = 'recv';
-const BRIDGE_NAME = 'kurento';
+const BRIDGE_NAME = 'fullaudio';
+const AUDIO_SESSION_NUM_KEY = 'AudioSessionNumber';
 
 // SFU's base broker has distinct error codes so that it can be reused by different
 // modules. Errors that have a valid, localized counterpart in audio manager are
@@ -28,21 +30,35 @@ const errorCodeMap = {
   1302: 1002,
   1305: 1005,
   1307: 1007,
-}
+};
+
 const mapErrorCode = (error) => {
   const { errorCode } = error;
   const mappedErrorCode = errorCodeMap[errorCode];
   if (errorCode == null || mappedErrorCode == null) return error;
+  // eslint-disable-next-line no-param-reassign
   error.errorCode = mappedErrorCode;
   return error;
-}
-
-// TODO Would be interesting to move this to a service along with the error mapping
-const getMediaServerAdapter = () => {
-  return getFromMeetingSettings('media-server-listenonly', DEFAULT_LISTENONLY_MEDIA_SERVER);
 };
 
-export default class KurentoAudioBridge extends BaseAudioBridge {
+// TODO Would be interesting to move this to a service along with the error mapping
+const getMediaServerAdapter = () => getFromMeetingSettings(
+  'media-server-listenonly',
+  DEFAULT_LISTENONLY_MEDIA_SERVER,
+);
+
+const getAudioSessionNumber = () => {
+  let currItem = parseInt(sessionStorage.getItem(AUDIO_SESSION_NUM_KEY), 10);
+  if (!currItem) {
+    currItem = 0;
+  }
+
+  currItem += 1;
+  sessionStorage.setItem(AUDIO_SESSION_NUM_KEY, currItem);
+  return currItem;
+};
+
+export default class FullAudioBridge extends BaseAudioBridge {
   constructor(userData) {
     super();
     this.internalMeetingID = userData.meetingId;
@@ -53,8 +69,9 @@ export default class KurentoAudioBridge extends BaseAudioBridge {
     this.media = {
       inputDevice: {},
     };
-    this.broker;
+    this.broker = null;
     this.reconnecting = false;
+    this.iceServers = [];
   }
 
   async changeOutputDevice(value) {
@@ -65,8 +82,8 @@ export default class KurentoAudioBridge extends BaseAudioBridge {
         this.media.outputDeviceId = value;
       } catch (error) {
         logger.error({
-          logCode: 'listenonly_changeoutputdevice_error',
-          extraInfo: { error, bridge: BRIDGE_NAME }
+          logCode: 'fullaudio_changeoutputdevice_error',
+          extraInfo: { error, bridge: BRIDGE_NAME },
         }, 'Audio bridge failed to change output device');
         throw new Error(this.baseErrorCodes.MEDIA_ERROR);
       }
@@ -78,7 +95,7 @@ export default class KurentoAudioBridge extends BaseAudioBridge {
   getPeerConnection() {
     if (!this.broker) return null;
 
-    const webRtcPeer = this.broker.webRtcPeer;
+    const { webRtcPeer } = this.broker;
     if (webRtcPeer) return webRtcPeer.peerConnection;
     return null;
   }
@@ -113,18 +130,18 @@ export default class KurentoAudioBridge extends BaseAudioBridge {
       this.clearReconnectionTimeout();
     }, RECONNECT_TIMEOUT_MS);
 
-    this.joinAudio({ isListenOnly: true }, this.callback).then(() => {
-      this.clearReconnectionTimeout();
-    }).catch(error => {
+    this.joinAudio({ isListenOnly: this.isListenOnly }, this.callback).then(
+      () => this.clearReconnectionTimeout(),
+    ).catch((error) => {
       // Error handling is a no-op because it will be "handled" in handleBrokerFailure
       logger.debug({
-        logCode: 'listenonly_reconnect_failed',
+        logCode: 'fullaudio_reconnect_failed',
         extraInfo: {
           errorMessage: error.errorMessage,
           reconnecting: this.reconnecting,
-          bridge: BRIDGE_NAME
+          bridge: BRIDGE_NAME,
         },
-      }, 'Listen only reconnect failed');
+      }, 'Fullaudio reconnect failed');
     });
   }
 
@@ -135,38 +152,45 @@ export default class KurentoAudioBridge extends BaseAudioBridge {
 
       if (this.broker.started && !this.reconnecting) {
         logger.error({
-          logCode: 'listenonly_error_try_to_reconnect',
-          extraInfo: { errorMessage, errorCode, errorCause, bridge: BRIDGE_NAME },
-        }, 'Listen only failed, try to reconnect');
+          logCode: 'fullaudio_error_try_to_reconnect',
+          extraInfo: {
+            errorMessage,
+            errorCode,
+            errorCause,
+            bridge: BRIDGE_NAME,
+          },
+        }, 'Fullaudio failed, try to reconnect');
         this.reconnect();
         return resolve();
-      } else {
-        // Already tried reconnecting once OR the user handn't succesfully
-        // connected firsthand. Just finish the session and reject with error
-        logger.error({
-          logCode: 'listenonly_error',
-          extraInfo: {
-            errorMessage, errorCode, errorCause,
-            reconnecting: this.reconnecting,
-            bridge: BRIDGE_NAME
-          },
-        }, 'Listen only failed');
-        this.clearReconnectionTimeout();
-        this.broker.stop();
-        this.callback({
-          status: this.baseCallStates.failed,
-          error: errorCode,
-          bridgeError: errorMessage,
-          bridge: BRIDGE_NAME,
-        });
-        return reject(error);
       }
+
+      // Already tried reconnecting once OR the user handn't succesfully
+      // connected firsthand. Just finish the session and reject with error
+      logger.error({
+        logCode: 'fullaudio_error',
+        extraInfo: {
+          errorMessage,
+          errorCode,
+          errorCause,
+          reconnecting: this.reconnecting,
+          bridge: BRIDGE_NAME,
+        },
+      }, 'Fullaudio failed');
+      this.clearReconnectionTimeout();
+      this.broker.stop();
+      this.callback({
+        status: this.baseCallStates.failed,
+        error: errorCode,
+        bridgeError: errorMessage,
+        bridge: BRIDGE_NAME,
+      });
+      return reject(error);
     });
   }
 
   dispatchAutoplayHandlingEvent(mediaElement) {
     const tagFailedEvent = new CustomEvent('audioPlayFailed', {
-      detail: { mediaElement }
+      detail: { mediaElement },
     });
     window.dispatchEvent(tagFailedEvent);
     this.callback({ status: this.baseCallStates.autoplayBlocked, bridge: BRIDGE_NAME });
@@ -176,16 +200,18 @@ export default class KurentoAudioBridge extends BaseAudioBridge {
     const stream = this.broker.webRtcPeer.getRemoteStream();
     const mediaElement = document.getElementById(MEDIA_TAG);
 
-    return loadAndPlayMediaStream(stream, mediaElement, false).then(() => {
-      return this.callback({ status: this.baseCallStates.started, bridge: BRIDGE_NAME });
-    }).catch(error => {
+    return loadAndPlayMediaStream(stream, mediaElement, false).then(() => this
+      .callback({
+        status: this.baseCallStates.started,
+        bridge: BRIDGE_NAME,
+      })).catch((error) => {
       // NotAllowedError equals autoplay issues, fire autoplay handling event.
       // This will be handled in audio-manager.
       if (error.name === 'NotAllowedError') {
         logger.error({
-          logCode: 'listenonly_error_autoplay',
+          logCode: 'fullaudio_error_autoplay',
           extraInfo: { errorName: error.name, bridge: BRIDGE_NAME },
-        }, 'Listen only media play failed due to autoplay error');
+        }, 'Fullaudio media play failed due to autoplay error');
         this.dispatchAutoplayHandlingEvent(mediaElement);
       } else {
         const normalizedError = {
@@ -197,96 +223,90 @@ export default class KurentoAudioBridge extends BaseAudioBridge {
           error: normalizedError.errorCode,
           bridgeError: normalizedError.errorMessage,
           bridge: BRIDGE_NAME,
-        })
+        });
         throw normalizedError;
       }
     });
   }
 
-  trickleIce() {
+  async _initBrokerEventsPromise() {
     return new Promise((resolve, reject) => {
       try {
-        fetchWebRTCMappedStunTurnServers(this.sessionToken)
-          .then((iceServers) => {
-            const options = {
-              userName: this.name,
-              caleeName: `${GLOBAL_AUDIO_PREFIX}${this.voiceBridge}`,
-              iceServers,
-              offering: OFFERING,
-            };
+        if (!this.broker) resolve(null);
 
-            this.broker = new ListenOnlyBroker(
-              Auth.authenticateURL(SFU_URL),
-              this.voiceBridge,
-              this.userId,
-              this.internalMeetingID,
-              RECV_ROLE,
-              options,
-            );
+        this.broker.onended = this.handleTermination.bind(this);
 
-            this.broker.onstart = () => {
-              const { peerConnection } = this.broker.webRtcPeer;
+        this.broker.onerror = (error) => {
+          this.handleBrokerFailure(error).catch(reject);
+        };
 
-              if (!peerConnection) return resolve(null);
-
-              const selectedCandidatePair = peerConnection.getReceivers()[0]
-                .transport.iceTransport.getSelectedCandidatePair();
-
-              const validIceCandidate = [selectedCandidatePair.local];
-
-              this.broker.stop();
-              return resolve(validIceCandidate);
-            };
-
-            this.broker.listen();
-          });
-      } catch (e) {
-        reject(e);
+        this.broker.onstart = () => {
+          this.handleStart().then(resolve).catch(reject);
+        };
+      } catch (error) {
+        reject(error);
       }
     });
   }
 
-  joinAudio({ isListenOnly }, callback) {
-    return new Promise(async (resolve, reject) => {
-      if (!isListenOnly) return reject(new Error('Invalid bridge option'));
+  async _startBroker(_options) {
+    try {
+      const { isListenOnly } = _options;
+      this.isListenOnly = isListenOnly;
+
+      const callerIdName = [
+        `${this.userId}_${getAudioSessionNumber()}`,
+        'bbbID',
+        isListenOnly ? `${GLOBAL_AUDIO_PREFIX}${this.voiceBridge}` : this.name,
+      ].join('-').replace(/"/g, "'");
+
+      const options = {
+        userName: this.name,
+        caleeName: callerIdName,
+        iceServers: this.iceServers,
+        offering: OFFERING,
+        mediaServer: getMediaServerAdapter(),
+      };
+
+      this.broker = new FullAudioBroker(
+        Auth.authenticateURL(SFU_URL),
+        this.voiceBridge,
+        this.userId,
+        this.internalMeetingID,
+        isListenOnly ? RECV_ROLE : SENDRECV_ROLE,
+        options,
+      );
+
+      const initBrokerEventsPromise = this._initBrokerEventsPromise();
+
+      this.broker.listen();
+
+      return initBrokerEventsPromise;
+    } catch (error) {
+      logger.warn({ logCode: 'fullaudio_bridge_broker_init_fail' },
+        'Problem when initializing SFU broker for fullaudio bridge');
+      throw error;
+    }
+  }
+
+  async joinAudio(options, callback) {
+    try {
       this.callback = callback;
-      let iceServers = [];
 
-      try {
-        iceServers = await fetchWebRTCMappedStunTurnServers(this.sessionToken);
-      } catch (error) {
-        logger.error({ logCode: 'listenonly_stunturn_fetch_failed' },
-          'SFU audio bridge failed to fetch STUN/TURN info, using default servers');
-        iceServers = getMappedFallbackStun();
-      } finally {
-        const options = {
-          userName: this.name,
-          caleeName: `${GLOBAL_AUDIO_PREFIX}${this.voiceBridge}`,
-          iceServers,
-          offering: OFFERING,
-          mediaServer: getMediaServerAdapter(),
-        };
+      this.iceServers = await fetchWebRTCMappedStunTurnServers(this.sessionToken);
+    } catch (error) {
+      logger.error({ logCode: 'fullaudio_stun-turn_fetch_failed' },
+        'SFU audio bridge failed to fetch STUN/TURN info, using default servers');
+      this.iceServers = getMappedFallbackStun();
+    } finally {
+      await this._startBroker(options);
+    }
+  }
 
-        this.broker = new ListenOnlyBroker(
-          Auth.authenticateURL(SFU_URL),
-          this.voiceBridge,
-          this.userId,
-          this.internalMeetingID,
-          RECV_ROLE,
-          options,
-        );
-
-        this.broker.onended = this.handleTermination.bind(this);
-        this.broker.onerror = (error) => {
-          this.handleBrokerFailure(error).catch(reject);
-        }
-        this.broker.onstart = () => {
-          this.handleStart().then(resolve).catch(reject);
-        };
-
-       this.broker.listen().catch(reject);
-      }
-    });
+  // eslint-disable-next-line class-methods-use-this
+  async updateAudioConstraints() {
+    // TO BE IMPLEMENTED
+    return true;
   }
 
   exitAudio() {
@@ -304,4 +324,4 @@ export default class KurentoAudioBridge extends BaseAudioBridge {
   }
 }
 
-module.exports = KurentoAudioBridge;
+module.exports = FullAudioBridge;
