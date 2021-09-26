07a6ec150d51d8cc80467c6676f566b11bd33888
hiroshisuga <45039819+hiroshisuga@users.noreply.github.com>
2021-09-26 19:49:15 +0900

    Update service.js

--- bigbluebutton-html5/imports/ui/components/captions/service.js
+++ bigbluebutton-html5/imports/ui/components/captions/service.js
@@ -53,6 +53,18 @@ const getAvailableLocales = () => {
   return locales;
 };
 
+const getAllAvailableLocales = () => {
+  const { meetingID } = Auth;
+  const locales = [];
+  Captions.find({ meetingId: meetingID },
+    { sort: { locale: 1 } },
+    { fields: { ownerId: 1, locale: 1 } })
+    .forEach((caption) => {
+      locales.push(caption.locale);
+    });
+  return locales;
+};
+
 const getOwnedLocales = () => {
   const { meetingID } = Auth;
   const locales = [];
@@ -174,6 +186,7 @@ export default {
   CAPTIONS_TOKEN,
   getCaptionsData,
   getAvailableLocales,
+  getAllAvailableLocales,
   getOwnedLocales,
   takeOwnership,
   appendText,
