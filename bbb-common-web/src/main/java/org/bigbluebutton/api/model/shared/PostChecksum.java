a6d2dfd8bafb635eb7dd2275812b6537d99ee7f4
paultrudel <paatrudel@gmail.com>
2021-10-06 10:40:40 -0400

    removed some unused code

--- bbb-common-web/src/main/java/org/bigbluebutton/api/model/shared/PostChecksum.java
+++ bbb-common-web/src/main/java/org/bigbluebutton/api/model/shared/PostChecksum.java
@@ -21,47 +21,6 @@ public class PostChecksum extends Checksum {
         queryStringWithoutChecksum = ValidationService.buildQueryStringFromParamsMap(params);
     }
 
-    private void buildQueryStringFromParamsMap() {
-        StringBuilder queryString = new StringBuilder();
-        SortedSet<String> keys = new TreeSet<>(params.keySet());
-
-        boolean firstParam = true;
-        for(String key: keys) {
-
-            if(key.equals("checksum")) {
-                continue;
-            }
-
-            for(String value: params.get(key)) {
-                if(firstParam) {
-                    firstParam = false;
-                } else {
-                    queryString.append("&");
-                }
-
-                queryString.append(key);
-                queryString.append("=");
-
-                String encodedValue = encodeString(value);
-                queryString.append(encodedValue);
-            }
-        }
-
-        queryStringWithoutChecksum = queryString.toString();
-    }
-
-    private String encodeString(String stringToEncode) {
-        String encodedResult;
-
-        try {
-            encodedResult = URLEncoder.encode(stringToEncode, StandardCharsets.UTF_8.name());
-        } catch(UnsupportedEncodingException ex) {
-            encodedResult = stringToEncode;
-        }
-
-        return encodedResult;
-    }
-
     public Map<String, String[]> getParams() { return params; }
 
     public void setParams(Map<String, String[]> params) { this.params = params; }
