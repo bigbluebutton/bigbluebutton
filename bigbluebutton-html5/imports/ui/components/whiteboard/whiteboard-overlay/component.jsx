d25b58dff9bb0bb486c1fcbb5e9a4c2e24ee3974
hiroshisuga <45039819+hiroshisuga@users.noreply.github.com>
2021-10-16 20:43:06 +0900

    physicalSlideWidth & physicalSlideHeight
    Missing paremeters

--- bigbluebutton-html5/imports/ui/components/whiteboard/whiteboard-overlay/component.jsx
+++ bigbluebutton-html5/imports/ui/components/whiteboard/whiteboard-overlay/component.jsx
@@ -176,6 +176,8 @@ export default class WhiteboardOverlay extends Component {
             actions={actions}
             drawSettings={drawSettings}
             whiteboardId={whiteboardId}
+            physicalSlideWidth={physicalSlideWidth}
+          physicalSlideHeight={physicalSlideHeight}
           />
         );
       }
