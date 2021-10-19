85e5200d597b8d3345241b597e7e952bc50689ca
Lucas Zawacki <lfzawacki@gmail.com>
2021-10-19 19:28:26 +0000

    chore(layout): Remove unused renderSidebar and renderNavBar methods

--- bigbluebutton-html5/imports/ui/components/app/component.jsx
+++ bigbluebutton-html5/imports/ui/components/app/component.jsx
@@ -345,37 +345,6 @@ class App extends Component {
       && (isPhone || isLayeredView.matches);
   }
 
-  renderNavBar() {
-    const { navbar, isLargeFont } = this.props;
-
-    if (!navbar) return null;
-
-    const realNavbarHeight = isLargeFont ? LARGE_NAVBAR_HEIGHT : NAVBAR_HEIGHT;
-
-    return (
-      <header
-        className={styles.navbar}
-        style={{
-          height: realNavbarHeight,
-        }}
-      >
-        {navbar}
-      </header>
-    );
-  }
-
-  renderSidebar() {
-    const { sidebar } = this.props;
-
-    if (!sidebar) return null;
-
-    return (
-      <aside className={styles.sidebar}>
-        {sidebar}
-      </aside>
-    );
-  }
-
   renderCaptions() {
     const {
       captions,
