8f346b6ae42512e2feab8f018bcc3cb9d08ece21
Lucas Zawacki <lfzawacki@gmail.com>
2021-10-19 20:43:33 +0000

    feature(layout): userdata to hide navbar

--- bigbluebutton-html5/imports/api/users-settings/server/methods/addUserSettings.js
+++ bigbluebutton-html5/imports/api/users-settings/server/methods/addUserSettings.js
@@ -66,6 +66,7 @@ const currentParameters = [
   'bbb_show_participants_on_login',
   'bbb_show_public_chat_on_login',
   'bbb_hide_actions_bar',
+  'bbb_hide_nav_bar',
   // OUTSIDE COMMANDS
   'bbb_outside_toggle_self_voice',
   'bbb_outside_toggle_recording',
