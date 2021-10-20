37b4673477a4d7992f8b264bb2acc786fd29de4e
Ramón Souza <contato@ramonsouza.com>
2021-10-20 16:45:44 +0000

    convert user-list-content component

---
+++ bigbluebutton-html5/imports/ui/components/user-list/user-list-content/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+import { FlexColumn } from '/imports/ui/stylesheets/styled-components/placeholders';
+
+const Content = styled(FlexColumn)`
+  flex-grow: 1;
+  overflow: hidden;
+`;
+
+export default {
+  Content,
+};
