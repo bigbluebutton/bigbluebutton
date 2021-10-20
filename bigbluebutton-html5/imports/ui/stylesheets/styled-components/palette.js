da15431a54d0587dee2ea5176d7cd5b2fc337233
Ramón Souza <contato@ramonsouza.com>
2021-10-20 14:17:16 +0000

    convert user-list component

---
+++ bigbluebutton-html5/imports/ui/stylesheets/styled-components/palette.js
@@ -0,0 +1,12 @@
+const colorOffWhite = '#F3F6F9'
+const colorGray = '#4E5A66';
+
+const userListBg = colorOffWhite;
+const userListText = colorGray;
+
+export {
+    colorOffWhite,
+    colorGray,
+    userListBg,
+    userListText,
+}
