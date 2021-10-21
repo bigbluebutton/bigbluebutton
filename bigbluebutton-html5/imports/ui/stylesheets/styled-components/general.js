5bdd4487d2414389eb5e99b0b1726fc6e6600a00
Ramón Souza <contato@ramonsouza.com>
2021-10-21 16:56:33 +0000

    convert user-dropdown component

--- bigbluebutton-html5/imports/ui/stylesheets/styled-components/general.js
+++ bigbluebutton-html5/imports/ui/stylesheets/styled-components/general.js
@@ -1,5 +1,6 @@
 const borderSize = '2px';
 const smPaddingX = '.75rem';
+const smPaddingY = '.3rem';
 const mdPaddingY = '.45rem';
 const mdPaddingX = '1rem';
 const lgPaddingX = '1.25rem';
@@ -8,6 +9,7 @@ const lgPaddingY = '0.6rem';
 export {
   borderSize,
   smPaddingX,
+  smPaddingY,
   mdPaddingX,
   mdPaddingY,
   lgPaddingX,
