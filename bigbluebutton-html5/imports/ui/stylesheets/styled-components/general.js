351fe186a3450b412dac730e2a7677b1fc671761
Ramón Souza <contato@ramonsouza.com>
2021-10-25 13:29:29 +0000

    convert actions-bar component

---
+++ bigbluebutton-html5/imports/ui/stylesheets/styled-components/general.js
@@ -0,0 +1,17 @@
+const borderSize = '2px';
+const smPaddingX = '.75rem';
+const smPaddingY = '.3rem';
+const mdPaddingY = '.45rem';
+const mdPaddingX = '1rem';
+const lgPaddingX = '1.25rem';
+const lgPaddingY = '0.6rem';
+
+export {
+  borderSize,
+  smPaddingX,
+  smPaddingY,
+  mdPaddingX,
+  mdPaddingY,
+  lgPaddingX,
+  lgPaddingY,
+};
