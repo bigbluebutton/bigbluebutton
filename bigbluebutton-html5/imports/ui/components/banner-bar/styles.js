3d5e505b33e5fc753ecc9eb816b9fed6859c6dc6
Ramón Souza <contato@ramonsouza.com>
2021-10-26 13:37:23 +0000

    convert breakout-join-confirmation component

--- bigbluebutton-html5/imports/ui/components/banner-bar/styles.js
+++ bigbluebutton-html5/imports/ui/components/banner-bar/styles.js
@@ -1,8 +1,9 @@
 import styled from 'styled-components';
 import { TextElipsis } from '/imports/ui/stylesheets/styled-components/placeholders';
+import { colorWhite } from '/imports/ui/stylesheets/styled-components/palette';
 
 const BannerTextColor = styled(TextElipsis)`
-  color: var(--color-white);
+  color: ${colorWhite};
 `;
 
 export default {
