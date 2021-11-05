5e82448be3a1e0b44bce65e899bd1afb588b4aad
Joao Victor <joaov@imdt.com.br>
2021-11-05 11:11:55 -0300

    fixes the publication for user response polls

--- bigbluebutton-html5/imports/ui/components/whiteboard/annotations/poll/component.jsx
+++ bigbluebutton-html5/imports/ui/components/whiteboard/annotations/poll/component.jsx
@@ -14,6 +14,10 @@ const intlMessages = defineMessages({
     id: 'app.whiteboard.annotations.pollResult',
     description: 'aria label used in poll result string',
   },
+  noResponsesFromUserResponsePoll: {
+    id: 'app.whiteboard.annotations.noResponses',
+    description: 'aria label used when there is no responses',
+  },
 });
 
 const MAX_DISPLAYED_CHARS = 15;
@@ -73,6 +77,10 @@ class PollDrawComponent extends Component {
   }
 
   componentDidMount() {
+    const { annotation } = this.props;
+    const { pollType, numResponders } = annotation;
+    if (pollType === PollService.pollTypes.Response && numResponders === 0) return;
+
     const isLayoutSwapped = getSwapLayout() && shouldEnableSwapLayout();
     if (isLayoutSwapped) return;
 
@@ -619,10 +627,20 @@ class PollDrawComponent extends Component {
   }
 
   render() {
-    const { intl } = this.props;
+    const { intl, annotation } = this.props;
     const { prepareToDisplay, textArray } = this.state;
+    let ariaResultLabel;
 
-    let ariaResultLabel = `${intl.formatMessage(intlMessages.pollResultAria)}: `;
+    const { pollType, numResponders } = annotation;
+    if (pollType === PollService.pollTypes.Response && numResponders === 0) {
+      const noResponseLabel = intl.formatMessage(intlMessages.noResponsesFromUserResponsePoll);
+      ariaResultLabel = `${intl.formatMessage(intlMessages.pollResultAria)}: ${noResponseLabel}}`;
+      return (
+        <g aria-label={ariaResultLabel} data-test="pollResultAria"></g>
+      );
+    }
+
+    ariaResultLabel = `${intl.formatMessage(intlMessages.pollResultAria)}: `;
     textArray.map((t, idx) => {
       const pollLine = t.slice(0, -1);
       ariaResultLabel += `${idx > 0 ? ' |' : ''} ${pollLine.join(' | ')}`;
