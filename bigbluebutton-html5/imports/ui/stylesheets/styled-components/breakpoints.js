59057744ecbbbc263b52012b966ce3903e9560c0
Ramón Souza <contato@ramonsouza.com>
2021-11-08 11:36:44 +0000

    convert presentation default-content component

--- bigbluebutton-html5/imports/ui/stylesheets/styled-components/breakpoints.js
+++ bigbluebutton-html5/imports/ui/stylesheets/styled-components/breakpoints.js
@@ -2,10 +2,12 @@ const smallOnly = 'only screen and (max-width: 40em)';
 const mediumOnly = 'only screen and (min-width: 40.063em) and (max-width: 64em)';
 const mediumUp = 'only screen and (min-width: 40.063em)';
 const phoneLandscape = 'only screen and (max-width: 480px) and (orientation: landscape)';
+const largeUp = 'only screen and (min-width: 64.063em)';
 
 export {
   smallOnly,
   mediumOnly,
   mediumUp,
   phoneLandscape,
+  largeUp,
 };
