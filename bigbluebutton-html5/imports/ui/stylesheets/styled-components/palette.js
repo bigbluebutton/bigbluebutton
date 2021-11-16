cd928cfb8d1e5b249de18f3414d9cdefd183b53f
Ramón Souza <contato@ramonsouza.com>
2021-11-16 17:13:44 +0000

    add css variables to palette

--- bigbluebutton-html5/imports/ui/stylesheets/styled-components/palette.js
+++ bigbluebutton-html5/imports/ui/stylesheets/styled-components/palette.js
@@ -1,110 +1,110 @@
-const colorWhite = '#FFF';
-const colorOffWhite = '#F3F6F9';
+const colorWhite = 'var(--color-white, #FFF)';
+const colorOffWhite = 'var(--color-off-white, #F3F6F9)';
 
-const colorBlack = '#000000';
+const colorBlack = 'var(--color-black, #000000)';
 
-const colorGray = '#4E5A66';
-const colorGrayDark = '#06172A';
-const colorGrayLight = '#8B9AA8';
-const colorGrayLighter = '#A7B3BD';
-const colorGrayLightest = '#D4D9DF';
+const colorGray = 'var(--color-gray, #4E5A66)';
+const colorGrayDark = 'var(--color-gray-dark, #06172A)';
+const colorGrayLight = 'var(--color-gray-light, #8B9AA8)';
+const colorGrayLighter = 'var(--color-gray-lighter, #A7B3BD)';
+const colorGrayLightest = 'var(--color-gray-lightest, #D4D9DF)';
 
-const colorBlueLight = '#54a1f3';
-const colorBlueLighter = '#92BCEA';
-const colorBlueLightest = '#E4ECF2';
+const colorBlueLight = 'var(--color-blue-light, #54a1f3)';
+const colorBlueLighter = 'var(--color-blue-lighter, #92BCEA)';
+const colorBlueLightest = 'var(--color-blue-lightest, #E4ECF2)';
 
-const colorTransparent = '#ff000000';
+const colorTransparent = 'var(--color-transparent, #ff000000)';
 
-const colorPrimary = '#0F70D7';
-const colorDanger = '#DF2721';
-const colorSuccess = '#008081';
-const colorWarning = 'purple';
-const colorOffline = colorGrayLight;
-const colorMuted = '#586571';
-const colorMutedBackground = '#F3F6F9';
+const colorPrimary = 'var(--color-primary, #0F70D7)';
+const colorDanger = 'var(--color-danger, #DF2721)';
+const colorSuccess = 'var(--color-success, #008081)';
+const colorWarning = 'var(--color-warning, purple)';
+const colorOffline = `var(--color-offline, ${colorGrayLight})`;
+const colorMuted = 'var(--color-muted, #586571)';
+const colorMutedBackground = 'var(--color-muted-background, #F3F6F9)';
 
-const colorBackground = colorGrayDark;
-const userListBg = colorOffWhite;
-const userListText = colorGray;
-const unreadMessagesBg = colorDanger;
-const colorGrayLabel = colorGray;
-const colorText = colorGray;
-const colorLink = colorPrimary;
+const colorBackground = `var(--color-background, ${colorGrayDark})`;
+const userListBg = `var(--user-list-bg, ${colorOffWhite})`;
+const userListText = `var(--user-list-text, ${colorGray})`;
+const unreadMessagesBg = `var(--unread-messages-bg, ${colorDanger})`;
+const colorGrayLabel = `var(--color-gray-label, ${colorGray})`;
+const colorText = `var(--color-text, ${colorGray})`;
+const colorLink = `var(--color-link, ${colorPrimary})`;
 
-const listItemBgHover = '#DCE4ED';
-const colorTipBg = '#333333';
-const itemFocusBorder = colorBlueLighter;
+const listItemBgHover = 'var(--list-item-bg-hover, #DCE4ED)';
+const colorTipBg = 'var(--color-tip-bg, #333333)';
+const itemFocusBorder = `var(--item-focus-border, ${colorBlueLighter})`;
 
-const btnDefaultColor = colorGray;
-const btnDefaultBg = colorWhite;
-const btnDefaultBorder = colorWhite;
+const btnDefaultColor = `var(--btn-default-color, ${colorGray})`;
+const btnDefaultBg = `var(--btn-default-bg, ${colorWhite})`;
+const btnDefaultBorder = `var(--btn-default-border, ${colorWhite})`;
 
-const btnPrimaryBorder = colorPrimary;
-const btnPrimaryColor = colorWhite;
-const btnPrimaryBg = colorPrimary;
+const btnPrimaryBorder = `var(--btn-primary-border, ${colorPrimary})`;
+const btnPrimaryColor = `var(--btn-primary-color, ${colorWhite})`;
+const btnPrimaryBg = `var(--btn-primary-bg, ${colorPrimary})`;
 
-const btnSuccessBorder = colorSuccess;
-const btnSuccessColor = colorWhite;
-const btnSuccessBg = colorSuccess;
+const btnSuccessBorder = `var(--btn-success-border, ${colorSuccess})`;
+const btnSuccessColor = `var(--btn-success-color, ${colorWhite})`;
+const btnSuccessBg = `var(--btn-success-bg, ${colorSuccess})`;
 
-const btnWarningBorder = colorWarning;
-const btnWarningColor = colorWhite;
-const btnWarningBg = colorWarning;
+const btnWarningBorder = `var(--btn-warning-border, ${colorWarning})`;
+const btnWarningColor = `var(--btn-warning-color, ${colorWhite})`;
+const btnWarningBg = `var(--btn-warning-bg, ${colorWarning})`;
 
-const btnDangerBorder = colorDanger;
-const btnDangerColor = colorWhite;
-const btnDangerBg = colorDanger;
+const btnDangerBorder = `var(--btn-danger-border, ${colorDanger})`;
+const btnDangerColor = `var(--btn-danger-color, ${colorWhite})`;
+const btnDangerBg = `var(--btn-danger-bg, ${colorDanger})`;
 
-const btnDarkBorder = colorDanger;
-const btnDarkColor = colorWhite;
-const btnDarkBg = colorGrayDark;
+const btnDarkBorder = `var(--btn-dark-border, ${colorDanger})`;
+const btnDarkColor = `var(--btn-dark-color, ${colorWhite})`;
+const btnDarkBg = `var(--btn-dark-bg, ${colorGrayDark})`;
 
-const btnOfflineBorder = colorOffline;
-const btnOfflineColor = colorWhite;
-const btnOfflineBg = colorOffline;
+const btnOfflineBorder = `var(--btn-offline-border, ${colorOffline})`;
+const btnOfflineColor = `var(--btn-offline-color, ${colorWhite})`;
+const btnOfflineBg = `var(--btn-offline-bg, ${colorOffline})`;
 
-const btnMutedBorder = colorMutedBackground;
-const btnMutedColor = colorMuted;
-const btnMutedBg = colorMutedBackground;
+const btnMutedBorder = `var(--btn-muted-border, ${colorMutedBackground})`;
+const btnMutedColor = `var(--btn-muted-color, ${colorMuted})`;
+const btnMutedBg = `var(--btn-muted-bg, ${colorMutedBackground})`;
 
-const toolbarButtonColor = btnDefaultColor;
-const userThumbnailBorder = colorGrayLight;
-const loaderBg = colorGrayDark;
-const loaderBullet = colorWhite;
+const toolbarButtonColor = `var(--toolbar-button-color, ${btnDefaultColor})`;
+const userThumbnailBorder = `var(--user-thumbnail-border, ${colorGrayLight})`;
+const loaderBg = `var(--loader-bg, ${colorGrayDark})`;
+const loaderBullet = `var(--loader-bullet, ${colorWhite})`;
 
-const systemMessageBackgroundColor = '#F9FBFC';
-const systemMessageBorderColor = '#C5CDD4';
-const systemMessageFontColor = colorGrayDark;
-const colorHeading = colorGrayDark;
-const palettePlaceholderText = '#787675';
-const pollAnnotationGray = '#333333';
+const systemMessageBackgroundColor = 'var(--system-message-background-color, #F9FBFC)';
+const systemMessageBorderColor = 'var(--system-message-border-color, #C5CDD4)';
+const systemMessageFontColor = `var(--system-message-font-color, ${colorGrayDark})`;
+const colorHeading = `var(--color-heading, ${colorGrayDark})`;
+const palettePlaceholderText = 'var(--palette-placeholder-text, #787675)';
+const pollAnnotationGray = 'var(--poll-annotation-gray, #333333)';
 
-const toolbarButtonBorderColor = colorGrayLighter;
-const toolbarListColor = colorGray;
-const toolbarButtonBg = btnDefaultBg;
-const toolbarListBg = '#DDD';
-const toolbarListBgFocus = '#C6C6C6';
-const colorContentBackground = '#1B2A3A';
+const toolbarButtonBorderColor = `var(--toolbar-button-border-color, ${colorGrayLighter})`;
+const toolbarListColor = `var(--toolbar-list-color, ${colorGray})`;
+const toolbarButtonBg = `var(--toolbar-button-bg, ${btnDefaultBg})`;
+const toolbarListBg = 'var(--toolbar-list-bg, #DDD)';
+const toolbarListBgFocus = 'var(--toolbar-list-bg-focus, #C6C6C6)';
+const colorContentBackground = 'var(--color-content-background, #1B2A3A)';
 
-const dropdownBg = colorWhite;
+const dropdownBg = `var(--dropdown-bg, ${colorWhite})`;
 
-const pollStatsBorderColor = '#D4D9DF';
-const pollBlue = '#1A73D4';
+const pollStatsBorderColor = 'var(--poll-stats-border-color, #D4D9DF)';
+const pollBlue = 'var(--poll-blue, #1A73D4)';
 
-const toastDefaultColor = colorWhite;
-const toastDefaultBg = colorGray;
+const toastDefaultColor = `var(--toast-default-color, ${colorWhite})`;
+const toastDefaultBg = `var(--toast-default-bg, ${colorGray})`;
 
-const toastInfoColor = colorWhite;
-const toastInfoBg = colorPrimary;
+const toastInfoColor = `var(--toast-info-color, ${colorWhite})`;
+const toastInfoBg = `var(--toast-info-bg, ${colorPrimary})`;
 
-const toastSuccessColor = colorWhite;
-const toastSuccessBg = colorSuccess;
+const toastSuccessColor = `var(--toast-success-color, ${colorWhite})`;
+const toastSuccessBg = `var(--toast-success-bg, ${colorSuccess})`;
 
-const toastErrorColor = colorWhite;
-const toastErrorBg = colorDanger;
+const toastErrorColor = `var(--toast-error-color, ${colorWhite})`;
+const toastErrorBg = `var(--toast-error-bg, ${colorDanger})`;
 
-const toastWarningColor = colorWhite;
-const toastWarningBg = colorWarning;
+const toastWarningColor = `var(--toast-warning-color, ${colorWhite})`;
+const toastWarningBg = `var(--toast-warning-bg, ${colorWarning})`;
 
 export {
   colorWhite,
