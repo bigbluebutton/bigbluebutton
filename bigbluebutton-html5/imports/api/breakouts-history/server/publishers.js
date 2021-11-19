cf61057e9437e5e03a3ff599c14b6f88beda94d6
Gustavo Trott <gustavo@trott.com.br>
2021-11-19 17:42:33 -0300

    Adds publicationSafeGuard to breakouts-history

--- bigbluebutton-html5/imports/api/breakouts-history/server/publishers.js
+++ bigbluebutton-html5/imports/api/breakouts-history/server/publishers.js
@@ -5,6 +5,7 @@ import AuthTokenValidation, { ValidationStates } from '../../auth-token-validati
 import Logger from '/imports/startup/server/logger';
 import Meetings from '/imports/api/meetings';
 import Users from '/imports/api/users';
+import { publicationSafeGuard } from '/imports/api/common/server/helpers';
 
 const ROLE_MODERATOR = Meteor.settings.public.user.role_moderator;
 
@@ -29,6 +30,20 @@ function breakoutsHistory() {
   const selector = {
     meetingId,
   };
+
+  // Monitor this publication and stop it when user is not a moderator anymore
+  const comparisonFunc = () => {
+    const user = Users.findOne({ userId, meetingId }, { fields: { role: 1, userId: 1 } });
+    const condition = user.role === ROLE_MODERATOR;
+
+    if (!condition) {
+      Logger.info(`conditions aren't filled anymore in publication ${this._name}: 
+      user.role === ROLE_MODERATOR :${condition}, user.role: ${user.role} ROLE_MODERATOR: ${ROLE_MODERATOR}`);
+    }
+
+    return condition;
+  };
+  publicationSafeGuard(comparisonFunc, this);
 
   return BreakoutsHistory.find(selector);
 }
