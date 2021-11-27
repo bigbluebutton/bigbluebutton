e3330d0e07e9f6dc2bc93ead410bbb82901bdcca
root <root@ns382746.ip-46-105-101.eu>
2021-11-27 09:42:25 +0000

    upgrading bbb-common-web

--- bbb-common-web/project/Dependencies.scala
+++ bbb-common-web/project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
 
   object Versions {
     // Scala
-    val scala = "2.12.8"
+    val scala = "2.12.15"
     val junit = "4.12"
     val junitInterface = "0.11"
     val scalactic = "3.0.3"
