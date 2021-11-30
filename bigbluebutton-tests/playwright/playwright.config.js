4ec8916ebcfa8edb2f584ef80f2aa45b3fbfc82f
Anton <antonbsa@gmail.com>
2021-11-30 14:13:43 -0300

    add chromium project

--- bigbluebutton-tests/playwright/playwright.config.js
+++ bigbluebutton-tests/playwright/playwright.config.js
@@ -2,18 +2,26 @@ require('dotenv').config();
 const parameters = require('./core/parameters');
 
 const config = {
+  workers: 1,
   use: {
     headless: true,
-    launchOptions: {
-      args: [
-        '--no-sandbox',
-        '--use-fake-ui-for-media-stream',
-        '--use-fake-device-for-media-stream',
-        '--disable-features=IsolateOrigins,site-per-process',
-      ]
-    }
   },
-  workers: 1,
+  projects: [
+    {
+      name: 'Chromium',
+      use: {
+        browserName: 'chromium',
+        launchOptions: {
+          args: [
+            '--no-sandbox',
+            '--use-fake-ui-for-media-stream',
+            '--use-fake-device-for-media-stream',
+            '--disable-features=IsolateOrigins,site-per-process',
+          ]
+        },
+      },
+    },
+  ],
 };
 
 module.exports = config;
