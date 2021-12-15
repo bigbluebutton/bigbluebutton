65688af8b09487bee0ffe7ce6df8e881f0854765
paultrudel <paatrudel@gmail.com>
2021-12-15 14:28:37 -0500

    Added logging for passwords

--- bbb-common-web/src/main/java/org/bigbluebutton/api/model/validator/PasswordValidator.java
+++ bbb-common-web/src/main/java/org/bigbluebutton/api/model/validator/PasswordValidator.java
@@ -1,11 +1,15 @@
 package org.bigbluebutton.api.model.validator;
 
 import org.bigbluebutton.api.model.constraint.PasswordConstraint;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 public class PasswordValidator implements ConstraintValidator<PasswordConstraint, String> {
+
+    private static Logger log = LoggerFactory.getLogger(PasswordValidator.class);
 
     @Override
     public void initialize(PasswordConstraint constraintAnnotation) {
@@ -14,8 +18,18 @@ public class PasswordValidator implements ConstraintValidator<PasswordConstraint
 
     @Override
     public boolean isValid(String password, ConstraintValidatorContext context) {
-        if(password == null || password.equals("")) return true;
-        if(password.length() < 2 || password.length() > 64) return false;
+        log.info("Validating password [{}]", password);
+
+        if(password == null || password.equals("")) {
+            log.info("Provided password is either null or an empty string");
+            return true;
+        }
+
+        if(password.length() < 2 || password.length() > 64) {
+            log.info("Passwords must be between 2 and 64 characters in length");
+            return false;
+        }
+
         return true;
     }
 }
