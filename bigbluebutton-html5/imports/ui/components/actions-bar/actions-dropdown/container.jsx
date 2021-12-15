f9bbdad955e1f2ce33bdf606e5b0003fff592997
Arthurk12 <arthur_bockmann@hotmail.com>
2021-12-15 12:36:16 +0000

    feat(layout): propagate button
    Adds propagate layout button to the actions dropdown.

--- bigbluebutton-html5/imports/ui/components/actions-bar/actions-dropdown/container.jsx
+++ bigbluebutton-html5/imports/ui/components/actions-bar/actions-dropdown/container.jsx
@@ -6,6 +6,8 @@ import PresentationPodService from '/imports/ui/components/presentation-pod/serv
 import ActionsDropdown from './component';
 import LayoutContext from '../../layout/context';
 import getFromUserSettings from '/imports/ui/services/users-settings';
+import Settings from '/imports/ui/services/settings';
+import LayoutService from '/imports/ui/components/layout/service';
 
 const ActionsDropdownContainer = (props) => {
   const layoutContext = useContext(LayoutContext);
@@ -28,11 +30,15 @@ const LAYOUT_CONFIG = Meteor.settings.public.layout;
 
 export default withTracker(() => {
   const presentations = Presentations.find({ 'conversion.done': true }).fetch();
+  const AppSettings = Settings.application;
+  const { selectedLayout } = AppSettings;
   return ({
     presentations,
     isDropdownOpen: Session.get('dropdownOpen'),
     setPresentation: PresentationUploaderService.setPresentation,
     podIds: PresentationPodService.getPresentationPodIds(),
     hidePresentation: getFromUserSettings('bbb_hide_presentation', LAYOUT_CONFIG.hidePresentation),
+    settingsLayout: selectedLayout?.replace('Push', ''),
+    pushLayout: LayoutService.setMeetingLayout,
   });
 })(ActionsDropdownContainer);
