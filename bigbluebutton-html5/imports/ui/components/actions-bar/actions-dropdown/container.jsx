641fda7668b91a71a4150af97f2e0a88c9fe9785
Lucas Zawacki <lfzawacki@gmail.com>
2021-12-16 20:44:09 +0000

    fix(layout): Fix propagate layout with new code

--- bigbluebutton-html5/imports/ui/components/actions-bar/actions-dropdown/container.jsx
+++ bigbluebutton-html5/imports/ui/components/actions-bar/actions-dropdown/container.jsx
@@ -38,8 +38,8 @@ export default withTracker(() => {
     setPresentation: PresentationUploaderService.setPresentation,
     podIds: PresentationPodService.getPresentationPodIds(),
     hidePresentation: getFromUserSettings('bbb_hide_presentation', LAYOUT_CONFIG.hidePresentation),
-    settingsLayout: selectedLayout?.replace('Push', ''),
-    pushLayout: LayoutService.setMeetingLayout,
+    settingsLayout: selectedLayout,
+    setMeetingLayout: LayoutService.setMeetingLayout,
     showPushLayout: LAYOUT_CONFIG.layoutControlInActionsMenu,
   });
 })(ActionsDropdownContainer);
