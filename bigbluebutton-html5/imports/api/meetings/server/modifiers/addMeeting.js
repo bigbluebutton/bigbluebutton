3455f64d25ed2987e58bdf35613a4ee51306fb92
Lucas Zawacki <lfzawacki@gmail.com>
2021-12-20 19:03:37 +0000

    feature(layout): Enable layout to be defined via metadata at meeting creation

--- bigbluebutton-html5/imports/api/meetings/server/modifiers/addMeeting.js
+++ bigbluebutton-html5/imports/api/meetings/server/modifiers/addMeeting.js
@@ -13,6 +13,7 @@ import { initPads } from '/imports/api/common/server/etherpad';
 import { addAnnotationsStreamer } from '/imports/api/annotations/server/streamer';
 import { addCursorStreamer } from '/imports/api/cursor/server/streamer';
 import { addExternalVideoStreamer } from '/imports/api/external-videos/server/streamer';
+import { LAYOUT_TYPE } from '/imports/ui/components/layout/enums';
 
 const addExternalVideo = (meetingId) => {
   const selector = { meetingId };
@@ -172,6 +173,7 @@ export default function addMeeting(meeting) {
     $set: Object.assign({
       meetingId,
       meetingEnded,
+      layout: LAYOUT_TYPE[meeting.metadataProp.meetingLayout],
       publishedPoll: false,
       guestLobbyMessage: '',
       randomlySelectedUser: [],
