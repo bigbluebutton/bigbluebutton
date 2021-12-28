74750658896dc86ece42283aacfb0f0ff6df3a7f
Tim Schneider <tim-3.schneider@uni-ulm.de>
2021-12-28 12:26:00 +0100

    Fixed modifyWhiteboard false api call

--- bigbluebutton-html5/imports/api/annotations/server/methods/modifyWhiteboardAnnotations.js
+++ bigbluebutton-html5/imports/api/annotations/server/methods/modifyWhiteboardAnnotations.js
@@ -5,7 +5,7 @@ import { extractCredentials } from '/imports/api/common/server/helpers';
 import Logger from '/imports/startup/server/logger';
 import checkAnnotation from '../helpers/checkAnnotation';
 
-function modifyWhiteboardAnnotations(annotations, whiteBoardId, action, meetingId, userId) {
+function modifyWhiteboardAnnotations(annotations, idsToRemove, whiteBoardId, action, meetingId, userId) {
   const REDIS_CONFIG = Meteor.settings.private.redis;
   const CHANNEL = REDIS_CONFIG.channels.toAkkaApps;
   const EVENT_NAME = 'ModifyWhiteboardAnnotationPubMsg';
@@ -16,12 +16,12 @@ function modifyWhiteboardAnnotations(annotations, whiteBoardId, action, meetingI
 
     check(whiteBoardId, String);
     check(action, String);
-
+    check(idsToRemove, [String]);
     check(annotations, [Match.Any]);
     annotations.forEach((annotation) => checkAnnotation(annotation));
 
     const payload = {
-      annotations, userId, whiteBoardId, action,
+      annotations, idsToRemove, userId, whiteBoardId, action,
     };
 
     RedisPubSub.publishUserMessage(CHANNEL, EVENT_NAME, meetingId, userId, payload);
@@ -32,5 +32,5 @@ function modifyWhiteboardAnnotations(annotations, whiteBoardId, action, meetingI
 
 export default function deleteWhiteboardAnnotations(annotations, whiteboardId) {
   const { meetingId, requesterUserId } = extractCredentials(this.userId);
-  modifyWhiteboardAnnotations(annotations, whiteboardId, 'delete', meetingId, requesterUserId);
+  modifyWhiteboardAnnotations([], annotations.map( a => a.id), whiteboardId, 'delete', meetingId, requesterUserId);
 }
