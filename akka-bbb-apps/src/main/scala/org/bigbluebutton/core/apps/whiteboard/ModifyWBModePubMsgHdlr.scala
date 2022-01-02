37a043d9450479c677aeca63383361c961a28ca1
hiroshisuga <45039819+hiroshisuga@users.noreply.github.com>
2022-01-02 19:58:22 +0900

    Add files via upload

---
+++ akka-bbb-apps/src/main/scala/org/bigbluebutton/core/apps/whiteboard/ModifyWBModePubMsgHdlr.scala
@@ -0,0 +1,34 @@
+package org.bigbluebutton.core.apps.whiteboard
+
+import org.bigbluebutton.core.running.LiveMeeting
+import org.bigbluebutton.common2.msgs._
+import org.bigbluebutton.core.bus.MessageBus
+import org.bigbluebutton.core.apps.{ PermissionCheck, RightsManagementTrait }
+
+trait ModifyWBModePubMsgHdlr extends RightsManagementTrait {
+  this: WhiteboardApp2x =>
+
+  def handle(msg: ModifyWBModePubMsg, liveMeeting: LiveMeeting, bus: MessageBus): Unit = {
+
+    def broadcastEvent(msg: ModifyWBModePubMsg): Unit = {
+      val routing = Routing.addMsgToHtml5InstanceIdRouting(liveMeeting.props.meetingProp.intId, liveMeeting.props.systemProps.html5InstanceId.toString)
+      val envelope = BbbCoreEnvelope(ModifyWBModeEvtMsg.NAME, routing)
+      val header = BbbClientMsgHeader(ModifyWBModeEvtMsg.NAME, liveMeeting.props.meetingProp.intId, msg.header.userId)
+
+      val body = ModifyWBModeEvtMsgBody(msg.body.whiteboardMode)
+      val event = ModifyWBModeEvtMsg(header, body)
+      val msgEvent = BbbCommonEnvCoreMsg(envelope, event)
+      bus.outGW.send(msgEvent)
+    }
+
+    if (filterWhiteboardMessage(msg.body.meetingId, msg.header.userId, liveMeeting) && permissionFailed(PermissionCheck.GUEST_LEVEL, PermissionCheck.PRESENTER_LEVEL, liveMeeting.users2x, msg.header.userId)) {
+      val meetingId = liveMeeting.props.meetingProp.intId
+      val reason = "No permission to modify the whiteboard style."
+      PermissionCheck.ejectUserForFailedPermission(meetingId, msg.header.userId, reason, bus.outGW, liveMeeting)
+    } else {
+      println("ModifyWBModePubMsgHdlr " + msg.body.whiteboardMode)
+      modifyWBMode(msg.body.meetingId, msg.body.whiteboardMode, liveMeeting)
+      broadcastEvent(msg)
+    }
+  }
+}
