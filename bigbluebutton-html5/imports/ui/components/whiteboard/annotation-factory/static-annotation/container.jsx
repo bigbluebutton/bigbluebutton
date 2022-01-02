0a9c7381c2897fdac027d25b2db1d044a22b50e7
hiroshisuga <45039819+hiroshisuga@users.noreply.github.com>
2022-01-02 21:54:38 +0900

    Update container.jsx

--- bigbluebutton-html5/imports/ui/components/whiteboard/annotation-factory/static-annotation/container.jsx
+++ bigbluebutton-html5/imports/ui/components/whiteboard/annotation-factory/static-annotation/container.jsx
@@ -5,9 +5,6 @@ import StaticAnnotation from './component';
 import StaticAnnotationService from './service';
 import ReactiveAnnotationService from '../reactive-annotation/service';
 
-//import { Annotations } from '/imports/ui/components/whiteboard/service';
-//import { UnsentAnnotations } from '/imports/ui/components/whiteboard/service';
-
 const StaticAnnotationContainer = (props) => {
   return (
     <StaticAnnotation
@@ -21,19 +18,10 @@ export default withTracker((params) => {
     shapeId,
   } = params;
 
-//console.log("STATIC_CONT", shapeId, params.whiteboardId);
-//console.log(Annotations.find({whiteboardId: params.whiteboardId}).fetch());
   const annotation = StaticAnnotationService.getAnnotationById(shapeId);
-  //var annotation = StaticAnnotationService.getAnnotationById(shapeId);
-  if (!annotation){
-//console.log(UnsentAnnotations.find({whiteboardId: params.whiteboardId}).fetch());
-//console.log(UnsentAnnotations.findOne({_id: shapeId}));
-console.log("FACTORY no annotation found! ShapeId:", shapeId);
-    //annotation = ReactiveAnnotationService.getAnnotationById(shapeId);
-  }
 
   return {
-    // This is necessary to real-time update the movement. Otherwise the prop.version is undefined in the component.jsx
+    // This is necessary to real-time update the movement
     version: annotation.version,
   };
 })(StaticAnnotationContainer);
