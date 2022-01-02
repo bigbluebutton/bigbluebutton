3300450f62b9246b8ee09574c45a126a1d8a9be6
hiroshisuga <45039819+hiroshisuga@users.noreply.github.com>
2022-01-02 21:02:00 +0900

    Add files via upload

---
+++ bigbluebutton-html5/imports/api/meetings/server/methods/setWhiteboardMode.js
@@ -0,0 +1,30 @@
+import RedisPubSub from '/imports/startup/server/redis';
+import { Meteor } from 'meteor/meteor';
+import { check } from 'meteor/check';
+import { extractCredentials } from '/imports/api/common/server/helpers';
+import Logger from '/imports/startup/server/logger';
+
+export default function setWhiteboardMode(whiteboardMode) {
+  const REDIS_CONFIG = Meteor.settings.private.redis;
+  const CHANNEL = REDIS_CONFIG.channels.toAkkaApps;
+  const EVENT_NAME = 'ModifyWBModePubMsg';
+
+
+  try {
+    const { meetingId, requesterUserId } = extractCredentials(this.userId);
+
+    check(meetingId, String);
+    check(requesterUserId, String);
+//    check(whiteboardId, String);
+
+    //const whiteboardMode = {synchronizeWBUpdate: synchronize };
+    const payload = {
+      whiteboardMode,
+      meetingId,
+    };
+
+    return RedisPubSub.publishUserMessage(CHANNEL, EVENT_NAME, meetingId, requesterUserId, payload);
+  } catch (err) {
+    Logger.error(`Exception while invoking method setWhiteboardMode ${err.stack}`);
+  }
+}
