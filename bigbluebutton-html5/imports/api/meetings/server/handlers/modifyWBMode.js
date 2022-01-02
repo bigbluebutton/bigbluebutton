d7edd617e19adc99ef91911d3305907985480668
hiroshisuga <45039819+hiroshisuga@users.noreply.github.com>
2022-01-02 21:01:14 +0900

    Update modifyWBMode.js

--- bigbluebutton-html5/imports/api/meetings/server/handlers/modifyWBMode.js
+++ bigbluebutton-html5/imports/api/meetings/server/handlers/modifyWBMode.js
@@ -2,11 +2,9 @@ import modifyWBMode from '../modifiers/modifyWBMode';
 import { check } from 'meteor/check';
 
 export default function handleModifyWBMode({ body }, meetingId) {
-  //const whiteboardId = body.whiteboardId;
   const whiteboardMode = body.whiteboardMode;
 
   check(meetingId, String);
-  //check(whiteboardId, String);
   check(whiteboardMode, Object);
 
   return modifyWBMode(meetingId, whiteboardMode);
