625c1de1a776cee960e32359b1ec5a90efdb1d79
hiroshisuga <45039819+hiroshisuga@users.noreply.github.com>
2022-01-02 22:41:45 +0900

    Update component.jsx

--- bigbluebutton-html5/imports/ui/components/whiteboard/whiteboard-overlay/shape-draw-listener/component.jsx
+++ bigbluebutton-html5/imports/ui/components/whiteboard/whiteboard-overlay/shape-draw-listener/component.jsx
@@ -283,6 +283,7 @@ export default class ShapeDrawListener extends Component {
       userId,
       actions,
       drawSettings,
+      synchronizeWBUpdate,
     } = this.props;
 
     const {
@@ -318,7 +319,7 @@ export default class ShapeDrawListener extends Component {
       position: 0,
     };
 
-    sendAnnotation(annotation, whiteboardId);
+    sendAnnotation(annotation, synchronizeWBUpdate);
   }
 
   discardAnnotation() {
