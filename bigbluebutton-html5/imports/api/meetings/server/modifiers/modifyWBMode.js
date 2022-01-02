d5ecfd59ef74a65d189dac976da737638f6ac0e9
hiroshisuga <45039819+hiroshisuga@users.noreply.github.com>
2022-01-02 21:08:22 +0900

    Update modifyWBMode.js

--- bigbluebutton-html5/imports/api/meetings/server/modifiers/modifyWBMode.js
+++ bigbluebutton-html5/imports/api/meetings/server/modifiers/modifyWBMode.js
@@ -4,13 +4,8 @@ import Meetings from '/imports/api/meetings/';
 
 export default function modifyWBMode(meetingId, whiteboardMode) {
   check(meetingId, String);
-  //check(whiteboardId, String);
   check(whiteboardMode, Object);
 
-//console.log("API old", meetingId);
-//console.log("API old", Meetings.find().fetch());
-//console.log("API old", Meetings.find({meetingId}).fetch());
-//console.log("API new", meetingId, whiteboardMode);
   const selector = {
     meetingId,
   };
@@ -20,13 +15,6 @@ export default function modifyWBMode(meetingId, whiteboardMode) {
   };
 
   try {
-    //const { insertedId } = WhiteboardMultiUser.upsert(selector, modifier);
-    //if (insertedId) {
-    //  Logger.info(`Added whiteboard style flag=${whiteboardMode} whiteboardId=${whiteboardId}`);
-    //} else {
-    //  Logger.info(`Upserted whiteboard style flag=${whiteboardMode} whiteboardId=${whiteboardId}`);
-    //}
-    //WhiteboardMultiUser.update(selector, modifier);
     Meetings.update(selector, modifier);
     Logger.info(`Updated whiteboard style flag=${whiteboardMode} for meeting=${meetingId}`);
   } catch (err) {
