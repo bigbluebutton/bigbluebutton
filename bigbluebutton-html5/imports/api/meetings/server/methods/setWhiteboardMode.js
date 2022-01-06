a57d1cda77ba64732d3064fe6d3cae952a37c5f1
hiroshisuga <45039819+hiroshisuga@users.noreply.github.com>
2022-01-06 11:11:30 +0900

    optional credentials
    so that we can give credentials even if this.userId is unavailable, for instance in a breakout.

--- bigbluebutton-html5/imports/api/meetings/server/methods/setWhiteboardMode.js
+++ bigbluebutton-html5/imports/api/meetings/server/methods/setWhiteboardMode.js
@@ -4,14 +4,19 @@ import { check } from 'meteor/check';
 import { extractCredentials } from '/imports/api/common/server/helpers';
 import Logger from '/imports/startup/server/logger';
 
-export default function setWhiteboardMode(whiteboardMode) {
+export default function setWhiteboardMode(whiteboardMode, credentials) {
   const REDIS_CONFIG = Meteor.settings.private.redis;
   const CHANNEL = REDIS_CONFIG.channels.toAkkaApps;
   const EVENT_NAME = 'ModifyWBModePubMsg';
 
-
+  let meetingId = undefined;
+  let requesterUserId = undefined;
   try {
-    const { meetingId, requesterUserId } = extractCredentials(this.userId);
+    if (this.userId) {
+      ( { meetingId, requesterUserId } = extractCredentials(this.userId) );
+    } else {
+      ( { meetingId, requesterUserId } = credentials );
+    }
 
     check(meetingId, String);
     check(requesterUserId, String);
