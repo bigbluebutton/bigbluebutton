974d7e59f7e5ac8a8e4ffe6b19cf728017ca27d8
hiroshisuga <45039819+hiroshisuga@users.noreply.github.com>
2022-01-06 11:25:14 +0900

    throwing WB mode to akka-apps
    We need this because the whiteboard mode set at the meeting modifier API addMeeting.js has not reached to the akka-app.
    This is not a very elegant solution, but I chose this way so that the change in the code stays minimum.

--- bigbluebutton-html5/imports/ui/components/whiteboard/whiteboard-overlay/container.jsx
+++ bigbluebutton-html5/imports/ui/components/whiteboard/whiteboard-overlay/container.jsx
@@ -4,9 +4,28 @@ import PropTypes from 'prop-types';
 import WhiteboardOverlayService from './service';
 import WhiteboardOverlay from './component';
 import WhiteboardService from '../service';
+import Meetings from '/imports/api/meetings';
+import Auth from '/imports/ui/services/auth';
+import { makeCall } from '/imports/ui/services/api';
 
 const WhiteboardOverlayContainer = (props) => {
   const { drawSettings } = props;
+  
+  // Updating the whiteboard mode for the breakout room.
+  // Note that doing this when the breakout room is created is a more elegant solution,
+  //  but it would be much complicated and there is a risk to break something else.
+  const meeting = Meetings.findOne({meetingId: Auth.meetingID}, { fields: {meetingProp:1, visited:1}});
+  if ( meeting.meetingProp.isBreakout && !meeting.visited ){
+    WhiteboardService.setVisited(true);
+    // The first one who enters the breakout room has to do this.
+    // I decided to add the member "visited".
+    // Another option is to check the number of users in the room,
+    //  but there is a race condition where somebody enters before creating the whiteboard overlay.
+
+    WhiteboardService.setWhiteboardMode(WhiteboardService.getWhiteboardMode(), {meetingId: Auth.meetingID, requesterUserId: Auth.userId});
+    // We need this because the whiteboard mode set at the modifier API addMeeting.js has not reached to the akka-app.
+  }
+  
   if (Object.keys(drawSettings).length > 0) {
     return (
       <WhiteboardOverlay {...props} />
