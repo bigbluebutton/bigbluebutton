880bd9aac89bbfaef19e9cdd1d4421ca7d3cb1d8
Maximilian Weyand <weyand@in.tum.de>
2022-01-20 21:45:25 +0100

    inject mouse events on selectables with elementsFromPoint() since events got consumed by presentation / whiteboard overlay

--- bigbluebutton-html5/imports/ui/components/whiteboard/annotation-selection-modification/component.jsx
+++ bigbluebutton-html5/imports/ui/components/whiteboard/annotation-selection-modification/component.jsx
@@ -21,21 +21,67 @@ function SelectionModification(props) {
     setMoveableTargets(moveableTargets.filter((selected) => !selection.includes(selected.id)));
   }
 
+  function forwardEventOnSelectableToSelecto(eventToTarget) {
+    if (selectoRef) {
+      const newEvent = eventToTarget;
+
+      let x = null;
+      let y = null;
+
+      switch (eventToTarget.type) {
+        case 'mousedown':
+          x = eventToTarget.clientX;
+          y = eventToTarget.clientY;
+          break;
+        case 'touchstart':
+          x = eventToTarget.touches[0].pageX;
+          y = eventToTarget.touches[0].pageY;
+          break;
+        default:
+          break;
+      }
+
+      if (!x || !y) return;
+
+      const elements = document.elementsFromPoint(x, y)
+        .filter((e) => e.classList.contains('selectable'));
+      if (elements.length > 0) {
+        [newEvent.target] = elements;
+        selectoRef.current.clickTarget(newEvent, elements[0]);
+      }
+    }
+  }
+
   useEffect(() => {
     setDeselectHandle(deselect);
+  });
+
+  useEffect(() => {
+    const events = ['mousedown', 'touchstart'];
+    events.forEach(
+      (eventType) => window.addEventListener(eventType, forwardEventOnSelectableToSelecto),
+    );
+    // window.addEventListener('touchstart', forwardEventOnSelectableToSelecto);
+    return () => {
+      events.forEach(
+        (eventType) => window.removeEventListener(eventType, forwardEventOnSelectableToSelecto),
+      );
+      // window.removeEventListener('touchstart', forwardEventOnSelectableToSelecto);
+    };
   });
 
   return (
     <>
       <Moveable
         origin={false}
+        draggable={false}
         rootContainer={document.body}
         edge={false}
         ref={moveableRef}
         target={moveableTargets}
       />
       <Selecto
-        dragCondition={(_) => tool === 'selection'}
+        dragCondition={() => tool === 'selection'}
         boundContainer="#slideSVG"
         ref={selectoRef}
         selectByClick
