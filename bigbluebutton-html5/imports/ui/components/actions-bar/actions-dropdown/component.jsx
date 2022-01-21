7ed3338d6d9719c963ccc68a732e308d6b7b1e1a
Pedro Beschorner Marin <pedrobmarin@gmail.com>
2022-01-21 14:17:00 -0300

    refactor(layout): remove moderator push button

--- bigbluebutton-html5/imports/ui/components/actions-bar/actions-dropdown/component.jsx
+++ bigbluebutton-html5/imports/ui/components/actions-bar/actions-dropdown/component.jsx
@@ -222,6 +222,15 @@ class ActionsDropdown extends PureComponent {
       });
     }
 
+    if (amIPresenter) {
+      actions.push({
+        icon: 'send',
+        label: intl.formatMessage(intlMessages.layouts),
+        key: 'layouts',
+        onClick: () => mountModal(<SettingsMenuContainer highlightLayoutSubMenu />),
+      });
+    }
+
     return actions;
   }
 
