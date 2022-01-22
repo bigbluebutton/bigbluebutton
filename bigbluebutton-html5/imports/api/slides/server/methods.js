218b86585904acc65e6fbfcaaaca37ded1d8c5b6
hiroshisuga <45039819+hiroshisuga@users.noreply.github.com>
2022-01-22 22:17:31 +0900

    Update methods.js

--- bigbluebutton-html5/imports/api/slides/server/methods.js
+++ bigbluebutton-html5/imports/api/slides/server/methods.js
@@ -1,8 +1,10 @@
 import { Meteor } from 'meteor/meteor';
 import switchSlide from './methods/switchSlide';
 import zoomSlide from './methods/zoomSlide';
+import fitToWidth from './methods/fitToWidth';
 
 Meteor.methods({
   switchSlide,
   zoomSlide,
+  fitToWidth,
 });
