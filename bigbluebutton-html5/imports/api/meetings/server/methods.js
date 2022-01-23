0c252a9cea98c1d7ac59e543ef71f1b861d20dd0
hiroshisuga <45039819+hiroshisuga@users.noreply.github.com>
2022-01-23 11:46:24 +0900

    Update methods.js

--- bigbluebutton-html5/imports/api/meetings/server/methods.js
+++ bigbluebutton-html5/imports/api/meetings/server/methods.js
@@ -6,6 +6,7 @@ import toggleLockSettings from './methods/toggleLockSettings';
 import toggleWebcamsOnlyForModerator from './methods/toggleWebcamsOnlyForModerator';
 import clearRandomlySelectedUser from './methods/clearRandomlySelectedUser';
 import changeLayout from './methods/changeLayout';
+import setFitToWidth from './methods/setFitToWidth';
 
 Meteor.methods({
   endMeeting,
@@ -15,4 +16,5 @@ Meteor.methods({
   toggleWebcamsOnlyForModerator,
   clearRandomlySelectedUser,
   changeLayout,
+  setFitToWidth,
 });
