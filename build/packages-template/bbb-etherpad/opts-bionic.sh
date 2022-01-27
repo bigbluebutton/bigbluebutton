8237862117efa432703d223a9b6e752be6f1d567
Anton Georgiev <anto.georgiev@gmail.com>
2022-01-27 19:18:59 +0000

    build: resolve conflicts opts dependencies

--- build/packages-template/bbb-etherpad/opts-bionic.sh
+++ build/packages-template/bbb-etherpad/opts-bionic.sh
@@ -1,7 +1,4 @@
 . ./opts-global.sh
 
-<<<<<<< HEAD
-OPTS="$OPTS -d yq -t deb --deb-user etherpad --deb-group etherpad --deb-use-file-permissions"
-=======
-OPTS="$OPTS -d yq,tidy -t deb"
->>>>>>> upstream/v2.4.x-release
+OPTS="$OPTS -t deb"
+
--- build/packages-template/bbb-html5/opts-bionic.sh
+++ build/packages-template/bbb-html5/opts-bionic.sh
@@ -1,8 +1,4 @@
 . ./opts-global.sh
 
-<<<<<<< HEAD
-OPTS="$OPTS -d bbb-webrtc-sfu,bc,yq,bbb-pads,bbb-web -t deb --deb-user meteor --deb-group meteor --deb-use-file-permissions"
-=======
-OPTS="$OPTS -d bbb-webrtc-sfu,bc,yq,bbb-etherpad,bbb-web -t deb"
->>>>>>> upstream/v2.4.x-release
+OPTS="$OPTS -d bbb-webrtc-sfu,bc,yq,bbb-pads,bbb-web -t deb"
 
