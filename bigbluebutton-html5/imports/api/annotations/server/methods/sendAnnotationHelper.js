384eb2cd0adde0c62ecbb4e37916af0568d4eeeb
hiroshisuga <45039819+hiroshisuga@users.noreply.github.com>
2022-02-05 11:06:42 +0900

    remove drawEndOnly: true

--- bigbluebutton-html5/imports/api/annotations/server/methods/sendAnnotationHelper.js
+++ bigbluebutton-html5/imports/api/annotations/server/methods/sendAnnotationHelper.js
@@ -83,7 +83,6 @@ export default function sendAnnotationHelper(annotation, meetingId, requesterUse
 
     const payload = {
       annotation,
-      drawEndOnly: true,
     };
 
     return RedisPubSub.publishUserMessage(CHANNEL, EVENT_NAME, meetingId, requesterUserId, payload);
