41f626a943f9eba7f21488c007d096006befe143
Paul Trudel <paatrudel@gmail.com>
2022-02-09 15:10:28 +0000

    removed user and password from .env

--- bbb-common-web/hibernate-cfg.sh
+++ bbb-common-web/hibernate-cfg.sh
@@ -1,4 +1,5 @@
 . .env
+mkdir -p ./src/main/resources
 echo '<!DOCTYPE hibernate-configuration PUBLIC
         "-//Hibernate/Hibernate Configuration DTD 3.0//EN"
         "http://www.hibernate.org/dtd/hibernate-configuration-3.0.dtd">
