d1b45a1e5833a77813645b239892c5241a4ef36b
hiroshisuga <45039819+hiroshisuga@users.noreply.github.com>
2022-02-11 14:01:38 +0900

    Update methods.js

--- bigbluebutton-html5/imports/api/captions/server/methods.js
+++ bigbluebutton-html5/imports/api/captions/server/methods.js
@@ -2,9 +2,11 @@ import { Meteor } from 'meteor/meteor';
 import takeOwnership from '/imports/api/captions/server/methods/takeOwnership';
 import appendText from '/imports/api/captions/server/methods/appendText';
 import getPadId from '/imports/api/captions/server/methods/getPadId';
+import toggleAutoTranslation from '/imports/api/captions/server/methods/toggleAutoTranslation';
 
 Meteor.methods({
   takeOwnership,
   appendText,
   getPadId,
+  toggleAutoTranslation,
 });
