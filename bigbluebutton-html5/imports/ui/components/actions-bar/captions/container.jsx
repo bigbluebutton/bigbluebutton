cf9fac57256d4af3d5e7b17853dc118bf0860a6d
hiroshisuga <45039819+hiroshisuga@users.noreply.github.com>
2022-02-11 13:45:10 +0900

    Update container.jsx

--- bigbluebutton-html5/imports/ui/components/actions-bar/captions/container.jsx
+++ bigbluebutton-html5/imports/ui/components/actions-bar/captions/container.jsx
@@ -4,6 +4,9 @@ import { withModalMounter } from '/imports/ui/components/modal/service';
 import CaptionsService from '/imports/ui/components/captions/service';
 import CaptionsReaderMenuContainer from '/imports/ui/components/actions-bar/captions/reader-menu/container';
 import CaptionsButton from './component';
+import Storage from '/imports/ui/services/storage/session';
+
+const CAPTION_LOCALE = "captionLocale";
 
 const CaptionsButtonContainer = (props) => <CaptionsButton {...props} />;
 
@@ -12,4 +15,6 @@ export default withModalMounter(withTracker(({ mountModal }) => ({
   handleOnClick: () => (CaptionsService.isCaptionsActive()
     ? CaptionsService.deactivateCaptions()
     : mountModal(<CaptionsReaderMenuContainer />)),
+  ownedLocales: CaptionsService.getOwnedLocales(),
+  selectedLocale: Storage.getItem(CAPTION_LOCALE),
 }))(CaptionsButtonContainer));
