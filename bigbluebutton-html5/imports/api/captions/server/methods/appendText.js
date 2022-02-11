d6fbd2971d77183b784490d57b65212723a4f9d9
hiroshisuga <45039819+hiroshisuga@users.noreply.github.com>
2022-02-11 14:00:49 +0900

    Update appendText.js

--- bigbluebutton-html5/imports/api/captions/server/methods/appendText.js
+++ bigbluebutton-html5/imports/api/captions/server/methods/appendText.js
@@ -5,38 +5,69 @@ import Captions from '/imports/api/captions';
 import { appendTextURL } from '/imports/api/common/server/etherpad';
 import { extractCredentials } from '/imports/api/common/server/helpers';
 
-export default function appendText(text, locale) {
+const CAPTIONS_CONFIG = Meteor.settings.public.captions;
+
+function sendToPad (padId, text) {
+  axios({
+    method: 'get',
+    url: appendTextURL(padId, text),
+    responseType: 'json',
+  }).then((response) => {
+    const { status } = response;
+    if (status !== 200) {
+      Logger.error(`Could not append captions for padId=${padId}`);
+    }
+  }).catch((error) => Logger.error(`Could not append captions for padId=${padId}: ${error}`));
+}
+
+function translateText (padId, index, textOri, src, dst) {
+  const url = CAPTIONS_CONFIG.googleTranslateUrl + '/exec?' +
+              'text=' + encodeURIComponent(textOri) + '&source=' + src + '&target=' + dst;
+
+  if (index === 0) {
+      sendToPad(padId, textOri);
+  } else {
+    axios({
+      method: 'get',
+      url,
+      responseType: 'json',
+    }).then((response) => {
+      const { code, text } = response.data;
+      if (code === 200) {
+        //sendToPad(padId, text.replace(/\n+$/,''));
+        sendToPad(padId, text);
+      } else {
+        Logger.error(`Could not get translation for ${textOri}`);
+      }
+    }).catch((error) => Logger.error(`Could not get translation for ${textOri}: ${error}`));
+
+  }
+}
+
+export default function appendText(text, locales) {
   try {
     const { meetingId, requesterUserId } = extractCredentials(this.userId);
 
     check(meetingId, String);
     check(requesterUserId, String);
     check(text, String);
-    check(locale, String);
+    check(locales, Array);
 
-    const captions = Captions.findOne({
-      meetingId,
-      locale,
-      ownerId: requesterUserId,
-    });
+    locales.forEach(function(locale, index) {
+      const captions = Captions.findOne({
+        meetingId,
+        locale,
+        ownerId: requesterUserId,
+      });
 
-    if (!captions) {
-      Logger.error(`Could not find captions' pad for meetingId=${meetingId} locale=${locale}`);
-      return;
-    }
-
-    const { padId } = captions;
-
-    axios({
-      method: 'get',
-      url: appendTextURL(padId, text),
-      responseType: 'json',
-    }).then((response) => {
-      const { status } = response;
-      if (status !== 200) {
-        Logger.error(`Could not append captions for padId=${padId}`);
+      if (!captions) {
+        Logger.error(`Could not find captions' pad for meetingId=${meetingId} locale=${locale}`);
+        return;
       }
-    }).catch((error) => Logger.error(`Could not append captions for padId=${padId}: ${error}`));
+
+      const { padId } = captions;
+      translateText(padId, index, text, locales[0], locale);
+    });
   } catch (err) {
     Logger.error(`Exception while invoking method appendText ${err.stack}`);
   }
