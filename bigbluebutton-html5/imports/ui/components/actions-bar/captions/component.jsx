18bd863f94efe3a3ae95be79abeba62448843e9e
hiroshisuga <45039819+hiroshisuga@users.noreply.github.com>
2022-02-11 13:43:46 +0900

    Update component.jsx

--- bigbluebutton-html5/imports/ui/components/actions-bar/captions/component.jsx
+++ bigbluebutton-html5/imports/ui/components/actions-bar/captions/component.jsx
@@ -4,6 +4,18 @@ import cx from 'classnames';
 import { defineMessages, injectIntl } from 'react-intl';
 import { styles } from '/imports/ui/components/actions-bar/styles';
 import Button from '/imports/ui/components/button/component';
+import ButtonEmoji from '/imports/ui/components/button/button-emoji/ButtonEmoji';
+import CaptionsService from '/imports/ui/components/captions/service';
+import { Session } from 'meteor/session';
+import _ from 'lodash';
+import Dropdown from '/imports/ui/components/dropdown/component';
+import DropdownTrigger from '/imports/ui/components/dropdown/trigger/component';
+import DropdownContent from '/imports/ui/components/dropdown/content/component';
+import DropdownList from '/imports/ui/components/dropdown/list/component';
+import DropdownListItem from '/imports/ui/components/dropdown/list/item/component';
+import Storage from '/imports/ui/services/storage/session';
+
+const CAPTION_LOCALE = 'captionLocale';
 
 const propTypes = {
   intl: PropTypes.shape({
@@ -22,21 +34,65 @@ const intlMessages = defineMessages({
     id: 'app.actionsBar.captions.stop',
     description: 'Stop closed captions option',
   },
+  changeCaption: {
+    id: 'app.actionsBar.changeCaption',
+    description: 'Open change caption label',
+  },
 });
 
-const CaptionsButton = ({ intl, isActive, handleOnClick }) => (
-  <Button
-    className={cx(isActive || styles.btn)}
-    icon="closed_caption"
-    label={intl.formatMessage(isActive ? intlMessages.stop : intlMessages.start)}
-    color={isActive ? 'primary' : 'default'}
-    ghost={!isActive}
-    hideLabel
-    circle
-    size="lg"
-    onClick={handleOnClick}
-    id={isActive ? 'stop-captions-button' : 'start-captions-button'}
-  />
+const handleClickCaption = (locale, selectedLocale) => {
+  if (selectedLocale != locale) {
+    Storage.setItem(CAPTION_LOCALE, locale);
+    Session.set('activeCaptions', locale);
+    //CaptionsService.setActiveCaptions(locale);
+  }
+}
+
+const getAvailableCaptions = (captions, selected) => {
+  return(
+    captions.map(locale => (
+    <DropdownListItem
+      label={locale.name}
+      key={_.uniqueId('locale-selector')}
+      onClick={() => handleClickCaption(locale.locale, selected)}
+      iconRight={selected == locale.locale ? 'check' : null}
+      className={selected == locale.locale ? styles.selectedLocale : ''}
+    />
+    ))
+  );
+};
+
+const CaptionsButton = ({ intl, isActive, handleOnClick, ownedLocales, selectedLocale }) => (
+  <div className={styles.offsetBottom}>
+    <Button
+      className={cx(isActive || styles.btn)}
+      icon="closed_caption"
+      label={intl.formatMessage(isActive ? intlMessages.stop : intlMessages.start)}
+      color={isActive ? 'primary' : 'default'}
+      ghost={!isActive}
+      hideLabel
+      circle
+      size="lg"
+      onClick={handleOnClick}
+      id={isActive ? 'stop-captions-button' : 'start-captions-button'}
+    />
+    {isActive &&
+      <Dropdown>
+        <DropdownTrigger tabIndex={0}>
+        <ButtonEmoji
+          emoji="device_list_selector"
+          hideLabel
+          label={intl.formatMessage(intlMessages.changeCaption)}
+        />
+        </DropdownTrigger>
+        <DropdownContent placement="top left">
+          <DropdownList>
+            {getAvailableCaptions(ownedLocales, selectedLocale)}
+          </DropdownList>
+        </DropdownContent>
+      </Dropdown>
+    }
+  </div>
 );
 
 CaptionsButton.propTypes = propTypes;
