f13103ea7b2f83c2af01698a1a5160de0189eddd
Joao Victor <joaov@imdt.com.br>
2022-02-21 15:01:55 -0300

    New message for presentation item status

--- bigbluebutton-html5/imports/ui/components/presentation/presentation-uploader/component.jsx
+++ bigbluebutton-html5/imports/ui/components/presentation/presentation-uploader/component.jsx
@@ -99,6 +99,10 @@ const intlMessages = defineMessages({
     id: 'app.presentationUploder.rejectedError',
     description: 'some files rejected, please check the file mime types',
   },
+  badConnectionError: {
+    id: 'app.presentationUploder.connectionClosedError',
+    description: 'message indicating that the connection was closed',
+  },
   uploadProcess: {
     id: 'app.presentationUploder.upload.progress',
     description: 'message that indicates the percentage of the upload',
@@ -982,6 +986,11 @@ class PresentationUploader extends Component {
     if (item.upload.done && item.upload.error) {
       if (item.conversion.status === 'FILE_TOO_LARGE') {
         constraint['0'] = ((item.conversion.maxFileSize) / 1000 / 1000).toFixed(2);
+      }
+
+      if (item.upload.progress < 100) {
+        const errorMessage = intlMessages.badConnectionError;
+        return intl.formatMessage(errorMessage);
       }
 
       const errorMessage = intlMessages[item.upload.status] || intlMessages.genericError;
