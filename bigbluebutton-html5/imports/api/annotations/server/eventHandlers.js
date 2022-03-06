64fc5a965416f40cb9e519601dd10da2aede0524
hiroshisuga <45039819+hiroshisuga@users.noreply.github.com>
2022-03-06 14:04:35 +0900

    Update eventHandlers.js

--- bigbluebutton-html5/imports/api/annotations/server/eventHandlers.js
+++ bigbluebutton-html5/imports/api/annotations/server/eventHandlers.js
@@ -1,11 +1,17 @@
 import RedisPubSub from '/imports/startup/server/redis';
 import { processForHTML5ServerOnly } from '/imports/api/common/server/helpers';
 import handleWhiteboardCleared from './handlers/whiteboardCleared';
+import handleWhiteboardRemoveAnnotations from './handlers/whiteboardRemoveAnnotations';
+import handleWhiteboardMoveAnnotations from './handlers/whiteboardMoveAnnotations';
+import handleWhiteboardReorderAnnotations from './handlers/whiteboardReorderAnnotations';
 import handleWhiteboardUndo from './handlers/whiteboardUndo';
 import handleWhiteboardSend from './handlers/whiteboardSend';
 import handleWhiteboardAnnotations from './handlers/whiteboardAnnotations';
 
 RedisPubSub.on('ClearWhiteboardEvtMsg', handleWhiteboardCleared);
+RedisPubSub.on('RemoveWhiteboardAnnotationsEvtMsg', handleWhiteboardRemoveAnnotations);
+RedisPubSub.on('MoveWhiteboardAnnotationsEvtMsg', handleWhiteboardMoveAnnotations);
+RedisPubSub.on('ReorderWhiteboardAnnotationsEvtMsg', handleWhiteboardReorderAnnotations);
 RedisPubSub.on('UndoWhiteboardEvtMsg', handleWhiteboardUndo);
 RedisPubSub.on('SendWhiteboardAnnotationEvtMsg', handleWhiteboardSend);
 RedisPubSub.on('GetWhiteboardAnnotationsRespMsg', processForHTML5ServerOnly(handleWhiteboardAnnotations));
