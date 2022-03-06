207c7d2fa88041d42e1714a4a606240cb165dbee
hiroshisuga <45039819+hiroshisuga@users.noreply.github.com>
2022-03-06 14:58:53 +0900

    Create container.jsx

---
+++ bigbluebutton-html5/imports/ui/components/whiteboard/annotation-factory/static-annotation/container.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withTracker } from 'meteor/react-meteor-data';
+import WhiteboardService from '../../service';
+import StaticAnnotation from './component';
+import StaticAnnotationService from './service';
+
+const StaticAnnotationContainer = (props) => {
+  return (
+    <StaticAnnotation
+      {...props}
+    />
+  );
+};
+
+export default withTracker((params) => {
+  const {
+    whiteboardId,
+    shapeId,
+  } = params;
+
+  const annotation = StaticAnnotationService.getAnnotationById(shapeId);
+  const isPresenter = WhiteboardService.isPresenter();
+  const isMyAnnotation = WhiteboardService.currentUserID() == annotatorID;
+  const isEditable = isMyAnnotation || (isPresenter && !WhiteboardService.isMultiUserActive(whiteboardId));
+
+  return {
+    selected: annotation.selected ? annotation.selected : false,
+    isEditable,
+    // This is necessary to real-time update the movement. Otherwise the prop.version is undefined in the component.jsx
+    version: annotation.version,
+  };
+})(StaticAnnotationContainer);
+
+StaticAnnotationContainer.propTypes = {
+  whiteboardId: PropTypes.string.isRequired,
+  annotation: PropTypes.objectOf(PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number,
+    PropTypes.object,
+  ])),
+  drawObject: PropTypes.func.isRequired,
+  slideWidth: PropTypes.number.isRequired,
+  slideHeight: PropTypes.number.isRequired,
+};
