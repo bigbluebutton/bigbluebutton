2193c99d8d0fe3f488add21d6f2fedd4264c3ce9
Paulo Lanzarin <4529051+prlanzarin@users.noreply.github.com>
2022-03-08 14:46:34 -0300

    build: bump bbb-webrtc-sfu to v2.6.10

--- bbb-webrtc-sfu.placeholder.sh
+++ bbb-webrtc-sfu.placeholder.sh
@@ -1,1 +1,1 @@
-git clone --branch v2.6.9 --depth 1 https://github.com/bigbluebutton/bbb-webrtc-sfu bbb-webrtc-sfu
+git clone --branch v2.6.10 --depth 1 https://github.com/bigbluebutton/bbb-webrtc-sfu bbb-webrtc-sfu
