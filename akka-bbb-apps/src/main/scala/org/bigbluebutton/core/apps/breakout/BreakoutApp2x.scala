901c990ce4e43ad37ddd41c8d8ef1c7a898c314c
Gustavo Trott <gustavo@trott.com.br>
2022-03-10 10:11:14 -0300

    Fix merge conflicts on Akka

--- akka-bbb-apps/src/main/scala/org/bigbluebutton/core/apps/breakout/BreakoutApp2x.scala
+++ akka-bbb-apps/src/main/scala/org/bigbluebutton/core/apps/breakout/BreakoutApp2x.scala
@@ -17,12 +17,8 @@ trait BreakoutApp2x extends BreakoutRoomCreatedMsgHdlr
   with SendBreakoutUsersUpdateMsgHdlr
   with TransferUserToMeetingRequestHdlr
   with EndBreakoutRoomInternalMsgHdlr
-<<<<<<< HEAD
   with UpdateBreakoutRoomTimeInternalMsgHdlr
-=======
-  with ExtendBreakoutRoomTimeInternalMsgHdlr
   with EjectUserFromBreakoutInternalMsgHdlr
->>>>>>> upstream/v2.4.x-release
   with BreakoutRoomEndedInternalMsgHdlr {
 
   this: MeetingActor =>
