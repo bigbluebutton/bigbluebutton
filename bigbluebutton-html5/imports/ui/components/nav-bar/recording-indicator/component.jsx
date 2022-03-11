cc54f167f75646f97a69b68772aac3b49a20b4ce
Ramón Souza <contato@ramonsouza.com>
2022-03-11 17:16:39 +0000

    remove unused code

--- bigbluebutton-html5/imports/ui/components/nav-bar/recording-indicator/component.jsx
+++ bigbluebutton-html5/imports/ui/components/nav-bar/recording-indicator/component.jsx
@@ -148,9 +148,9 @@ class RecordingIndicator extends PureComponent {
               cy="10"
             />
             <circle
-              stroke="FFF"
-              fill="#FFF"
-              r={recording ? '5' : '4'}
+              stroke={recording ? '#F00' : '#FFF'}
+              fill={recording ? '#F00' : '#FFF'}
+              r="4"
               cx="10"
               cy="10"
             />
