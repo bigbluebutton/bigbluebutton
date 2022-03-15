1f0b58a87e34dcb940bd7070d5486f157d4787ca
Anton Georgiev <anto.georgiev@gmail.com>
2022-03-15 00:23:39 +0000

    build: re-add sfu as a dependency to bbb-html5

--- build/packages-template/bbb-html5/opts-focal.sh
+++ build/packages-template/bbb-html5/opts-focal.sh
@@ -1,4 +1,4 @@
 . ./opts-global.sh
 
-# TODO - add yq, bbb-webrtc-sfu
-OPTS="$OPTS -d bc,bbb-pads,bbb-web,mongodb-org -t deb"
+# TODO - add yq
+OPTS="$OPTS -d bc,bbb-pads,bbb-webrtc-sfu,bbb-web,mongodb-org -t deb"
