e25df307c164b38eed016ea7aa1ca47bce5ce8a6
KDSBrowne <kert.browne85@gmail.com>
2022-03-23 14:54:13 +0000

    use users context

--- bigbluebutton-html5/imports/ui/components/presentation/cursor/container.jsx
+++ bigbluebutton-html5/imports/ui/components/presentation/cursor/container.jsx
@@ -1,64 +1,70 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { withTracker } from 'meteor/react-meteor-data';
-import Auth from '/imports/ui/services/auth';
-import Meetings from '/imports/api/meetings';
-import Users from '/imports/api/users';
-import lockContextContainer from '/imports/ui/components/lock-viewers/context/container';
-import CursorService from './service';
-import Cursor from './component';
+import React, { Component, useContext } from "react";
+import PropTypes from "prop-types";
+import { withTracker } from "meteor/react-meteor-data";
+import Auth from "/imports/ui/services/auth";
+import lockContextContainer from "/imports/ui/components/lock-viewers/context/container";
+import { UsersContext } from "/imports/ui/components/components-data/users-context/context";
+import CursorService from "./service";
+import Cursor from "./component";
 
 const ROLE_MODERATOR = Meteor.settings.public.user.role_moderator;
 
-class CursorContainer extends Component {
-  render() {
-    const { cursorX, cursorY } = this.props;
-    if (cursorX > 0 && cursorY > 0) {
+const CursorContainer = (props) => {
+  const { cursorX, cursorY, presenter, uid, isViewersCursorLocked } = props;
+  if (cursorX > 0 && cursorY > 0) {
+    const usingUsersContext = useContext(UsersContext);
+    const { users } = usingUsersContext;
+    const role = users[Auth.meetingID][Auth.userID].role;
+    const userId = users[Auth.meetingID][Auth.userID].userId;
+    const showCursor =
+      role === ROLE_MODERATOR || presenter || (!presenter && uid === userId);
+    if (!isViewersCursorLocked || (isViewersCursorLocked && showCursor)) {
       return (
         <Cursor
           cursorX={cursorX}
           cursorY={cursorY}
           setLabelBoxDimensions={this.setLabelBoxDimensions}
-          {...this.props}
+          {...props}
         />
       );
     }
-    return null;
   }
-}
+  return null;
+};
 
-export default lockContextContainer(withTracker((params) => {
-  const { cursorId, userLocks } = params;
-  const isViewersCursorLocked = userLocks?.hideViewersCursor;
-  const cursor = CursorService.getCurrentCursor(cursorId);
-  const user = Users.findOne(
-    { meetingId: Auth.meetingID, userId: Auth.userID },
-    { fields: { role: 1, userId: 1 } },
-  );
-  if (cursor) {
-    const {
-      xPercent: cursorX, yPercent: cursorY, userName, userId,
-    } = cursor;
-    const isRTL = document.documentElement.getAttribute('dir') === 'rtl';
-    const showCursor = user.role === ROLE_MODERATOR || cursor.presenter
-      || (!cursor.presenter && cursor.userId === user.userId);
+export default lockContextContainer(
+  withTracker((params) => {
+    const { cursorId, userLocks } = params;
+    const isViewersCursorLocked = userLocks?.hideViewersCursor;
+    const cursor = CursorService.getCurrentCursor(cursorId);
+    if (cursor) {
+      const {
+        xPercent: cursorX,
+        yPercent: cursorY,
+        userName,
+        userId,
+        presenter,
+      } = cursor;
+      const isRTL = document.documentElement.getAttribute("dir") === "rtl";
 
-    if (!isViewersCursorLocked || (isViewersCursorLocked && showCursor)) {
       return {
         cursorX,
         cursorY,
         userName,
+        presenter: presenter,
+        uid: userId,
         isRTL,
+        isViewersCursorLocked,
       };
     }
-  }
 
-  return {
-    cursorX: -1,
-    cursorY: -1,
-    userName: '',
-  };
-})(CursorContainer));
+    return {
+      cursorX: -1,
+      cursorY: -1,
+      userName: "",
+    };
+  })(CursorContainer)
+);
 
 CursorContainer.propTypes = {
   // Defines the 'x' coordinate for the cursor, in percentages of the slide's width
