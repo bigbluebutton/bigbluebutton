69a39f1c8020ddb7de224c51fca491153ddf0672
Ramon Souza <contato@ramonsouza.com>
2022-06-27 10:21:40 -0300

    only remove annotations/cursors when multiUser is disabled

--- bigbluebutton-html5/imports/ui/components/presentation/component.jsx
+++ bigbluebutton-html5/imports/ui/components/presentation/component.jsx
@@ -162,9 +162,10 @@ class Presentation extends PureComponent {
     const {
       numCameras: prevNumCameras,
       presentationBounds: prevPresentationBounds,
+      multiUser: prevMultiUser,
     } = prevProps;
 
-    if (!multiUser) {
+    if (prevMultiUser && !multiUser) {
       clearFakeAnnotations();
       clearCursors();
     }
