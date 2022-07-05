a467e97dccefe83d395fd0b370b4097e414b6126
Paulo Lanzarin <4529051+prlanzarin@users.noreply.github.com>
2022-07-05 09:12:56 -0300

    build(bbb-webrtc-sfu): v2.9.0-alpha.4
    See https://github.com/bigbluebutton/bbb-webrtc-sfu/releases/tag/v2.9.0-alpha.4

--- bbb-webrtc-sfu.placeholder.sh
+++ bbb-webrtc-sfu.placeholder.sh
@@ -1,1 +1,1 @@
-git clone --branch v2.9.0-alpha.3 --depth 1 https://github.com/bigbluebutton/bbb-webrtc-sfu bbb-webrtc-sfu
+git clone --branch v2.9.0-alpha.4 --depth 1 https://github.com/bigbluebutton/bbb-webrtc-sfu bbb-webrtc-sfu
