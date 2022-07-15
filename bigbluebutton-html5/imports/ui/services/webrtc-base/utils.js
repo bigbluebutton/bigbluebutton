cd9b8ccef42462567920c3398d9aaca836f1ae1c
prlanzarin <4529051+prlanzarin@users.noreply.github.com>
2022-07-15 14:25:37 +0000

    fix: handle Firefox stream cleanup edge case (peer.js)
     Firefox doesn't fire the ended evt/onended callback for live
     MediaStreamTrack(s). We rely on that event.
    
     Manually emit the ended event which works with the onended callback
     when a track is stopped

--- bigbluebutton-html5/imports/ui/services/webrtc-base/utils.js
+++ bigbluebutton-html5/imports/ui/services/webrtc-base/utils.js
@@ -1,5 +1,11 @@
 const stopTrack = (track) => {
-  if (track && typeof track.stop === 'function') track.stop();
+  if (track && typeof track.stop === 'function' && track.readyState !== 'ended') {
+    track.stop();
+    // Manually emit the event as a safeguard; Firefox doesn't fire it when it
+    // should with live MediaStreamTracks...
+    const trackStoppedEvt = new MediaStreamTrackEvent('ended', { track });
+    track.dispatchEvent(trackStoppedEvt);
+  }
 };
 
 const stopStream = (stream) => {
