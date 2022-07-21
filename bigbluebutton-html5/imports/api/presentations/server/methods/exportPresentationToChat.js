e3062f2da7218aaf2c16f679a48487f7dc4469af
Joao Victor <joaov@imdt.com.br>
2022-07-21 16:40:52 -0300

    fix: increase exporting threshold to 1 minute

--- bigbluebutton-html5/imports/api/presentations/server/methods/exportPresentationToChat.js
+++ bigbluebutton-html5/imports/api/presentations/server/methods/exportPresentationToChat.js
@@ -5,7 +5,7 @@ import Logger from '/imports/startup/server/logger';
 import setPresentationExporting from '/imports/api/presentations/server/modifiers/setPresentationExporting';
 import Presentations from '/imports/api/presentations';
 
-const EXPORTING_THRESHOLD = 20000;
+const EXPORTING_THRESHOLD = 60000;
 
 export default function exportPresentationToChat(presentationId) {
   const REDIS_CONFIG = Meteor.settings.private.redis;
