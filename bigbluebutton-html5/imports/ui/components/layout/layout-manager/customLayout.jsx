6f139610184287197b216e354f2b157cbb3001b5
prlanzarin <4529051+prlanzarin@users.noreply.github.com>
2022-07-21 14:40:12 -0300

    fix(layout): set external-video/screen share initial states on all layouts
    Only smart layout takes screen sharing/external video states in account
    when populating its initial state. The others don't, and that causes
    some weird issues when switching back-and-forth between layout types due
    to their input states becoming inconsistent - ie having an active screen
    sharing and transitioning from Smart -> Custom would mark it as false
    (due to its absence from the initial state) and pollute its state for
    subsequent layouts.
    
    This commit guarantees those features are taken into account when
    populating initial input states for Focus On*/Custom layouts.

--- bigbluebutton-html5/imports/ui/components/layout/layout-manager/customLayout.jsx
+++ bigbluebutton-html5/imports/ui/components/layout/layout-manager/customLayout.jsx
@@ -179,6 +179,12 @@ const CustomLayout = (props) => {
           cameraDock: {
             numCameras: cameraDockInput.numCameras,
           },
+          externalVideo: {
+            hasExternalVideo: input.externalVideo.hasExternalVideo,
+          },
+          screenShare: {
+            hasScreenShare: input.screenShare.hasScreenShare,
+          },
         }, INITIAL_INPUT_STATE),
       });
     }
