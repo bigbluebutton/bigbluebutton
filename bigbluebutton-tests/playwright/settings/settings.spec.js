a2aed9f956cf9bbf52d8a24822172c18d83cc06f
Anton B <antonbsa.bck@gmail.com>
2022-07-25 16:42:48 -0300

    test: fix and update test plan links

--- bigbluebutton-tests/playwright/settings/settings.spec.js
+++ bigbluebutton-tests/playwright/settings/settings.spec.js
@@ -2,6 +2,7 @@ const { test } = require('@playwright/test');
 const { Language } = require('./language');
 
 test.describe.parallel('Settings', () => {
+  // https://docs.bigbluebutton.org/2.6/release-tests.html#sharing-screen-in-full-screen-mode-automated
   test(`Locales`, async ({ browser, page }) => {
     const language = new Language(browser, page);
     await language.init(true, true);
