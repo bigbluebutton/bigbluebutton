ecc8c2dd1b4d8ce25d2f455c2dd68f8bcbb65719
Daniel Petri Rocha <daniel.petri@tum.de>
2022-07-27 16:49:25 +0200

    feat(lint): add eslint to bbb-export-annotations

---
+++ bbb-export-annotations/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  'env': {
+    'commonjs': true,
+    'es2021': true,
+    'node': true,
+  },
+  'extends': [
+    'eslint:recommended',
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+  },
+  'rules': {
+    'require-jsdoc': 0,
+    'camelcase': 0,
+    'max-len': 0,
+  },
+};
