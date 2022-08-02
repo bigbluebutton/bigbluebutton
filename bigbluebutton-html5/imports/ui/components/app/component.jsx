28d82ba758df5eba7faecaf583436415097c7e84
Brent Baccala <cosine@freesoft.org>
2022-08-02 12:36:03 -0400

    RemoteDesktopContainer must be imported in app/component.jsx

--- bigbluebutton-html5/imports/ui/components/app/component.jsx
+++ bigbluebutton-html5/imports/ui/components/app/component.jsx
@@ -28,6 +28,7 @@ import NewWebcamContainer from '../webcam/container';
 import PresentationAreaContainer from '../presentation/presentation-area/container';
 import ScreenshareContainer from '../screenshare/container';
 import ExternalVideoContainer from '../external-video-player/container';
+import RemoteDesktopContainer from '../remote-desktop/container';
 import { styles } from './styles';
 import {
   LAYOUT_TYPE, DEVICE_TYPE, ACTIONS,
