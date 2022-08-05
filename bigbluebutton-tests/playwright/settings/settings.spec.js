34ec0ebbd6b79f25ad9df7a84192dcce9aed3e84
Anton B <antonbsa.bck@gmail.com>
2022-08-05 14:00:26 -0300

    test: fix wrong test plan link

--- bigbluebutton-tests/playwright/settings/settings.spec.js
+++ bigbluebutton-tests/playwright/settings/settings.spec.js
@@ -2,7 +2,7 @@ const { test } = require('@playwright/test');
 const { Language } = require('./language');
 
 test.describe.parallel('Settings', () => {
-  // https://docs.bigbluebutton.org/2.6/release-tests.html#sharing-screen-in-full-screen-mode-automated
+  // https://docs.bigbluebutton.org/2.6/release-tests.html#application-settings
   test(`Locales`, async ({ browser, page }) => {
     test.slow();
     const language = new Language(browser, page);
