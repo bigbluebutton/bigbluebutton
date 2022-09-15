facae4e2fb720e83e7805be17859fdb13ad927ee
Ramón Souza <contato@ramonsouza.com>
2022-09-15 16:57:20 -0300

    include server role and instance in logs

--- bigbluebutton-html5/imports/startup/server/logger.js
+++ bigbluebutton-html5/imports/startup/server/logger.js
@@ -5,12 +5,25 @@ import WinstonPromTransport from './prom-metrics/winstonPromTransport';
 const LOG_CONFIG = Meteor?.settings?.private?.serverLog || {};
 const { level } = LOG_CONFIG;
 
+const serverInfoFormat = format.printf(({ level, message, ...metadata}) => {
+  const instanceId = parseInt(process.env.INSTANCE_ID, 10);
+  const role = process.env.BBB_HTML5_ROLE;
+  const server = Meteor?.isDevelopment ? 'development' : `${role} ${instanceId}`;
+
+  let msg = `${server} [${level}] : ${message} `  
+  if(metadata) {
+	msg += JSON.stringify(metadata)
+  }
+  return msg
+});
+
 const Logger = createLogger({
   level,
   format: format.combine(
     format.colorize({ level: true }),
     format.splat(),
     format.simple(),
+    serverInfoFormat,
   ),
   transports: [
     // console logging
