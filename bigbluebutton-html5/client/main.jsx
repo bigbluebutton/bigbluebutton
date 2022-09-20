f055781db8583edaac4c583bf0353540840d1a8e
Ramón Souza <contato@ramonsouza.com>
2022-09-20 18:34:29 -0300

    disable xhr fallback on websocket connection

--- bigbluebutton-html5/client/main.jsx
+++ bigbluebutton-html5/client/main.jsx
@@ -38,6 +38,11 @@ import('/imports/api/audio/client/bridge/bridge-whitelist').catch(() => {
   // bridge loading
 });
 
+Meteor.connection._stream._sockjsProtocolsWhitelist = function () { return ['websocket'] ; }
+
+Meteor.disconnect();
+Meteor.reconnect();
+
 collectionMirrorInitializer();
 liveDataEventBrokerInitializer();
 
