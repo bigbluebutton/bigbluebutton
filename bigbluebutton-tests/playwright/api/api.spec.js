8f82c7b69577d8d2de22017b470eec66ed93fff9
Brent Baccala <cosine@freesoft.org>
2022-11-17 17:59:53 -0500

    testsuite: initial breakout room tests

--- bigbluebutton-tests/playwright/api/api.spec.js
+++ bigbluebutton-tests/playwright/api/api.spec.js
@@ -1,5 +1,5 @@
 const { test } = require('@playwright/test');
-const { API } = require('./api.js');
+const { API, APIBreakout } = require('./api.js');
 
 test.describe.parallel('API', () => {
 
@@ -11,6 +11,31 @@ test.describe.parallel('API', () => {
   test('getMeetingInfo', async ({ browser, context, page }) => {
     const api = new API(browser, context, page);
     await api.testGetMeetingInfo();
+  });
+
+  test('breakoutWithoutParent', async ({ browser, context }) => {
+    const api = new APIBreakout(browser, context);
+    await api.testBreakoutWithoutParent();
+  });
+
+  test('breakoutWithoutSequenceNumber', async ({ browser, context, page }) => {
+    const api = new APIBreakout(browser, context);
+    await api.initModPage(page);
+    await api.testBreakoutWithoutSequenceNumber();
+  });
+
+  test('breakoutWithNonexistentParent', async ({ browser, context }) => {
+    const api = new APIBreakout(browser, context);
+    await api.testBreakoutWithNonexistentParent();
+  });
+
+  test('breakoutMeetingInfo', async ({ browser, context, page }) => {
+    const api = new APIBreakout(browser, context);
+    await api.initPages(page);
+    await api.create();
+    await api.testBreakoutMeetingInfoNoJoins();
+    await api.joinRoom();
+    await api.testBreakoutMeetingInfoOneJoin();
   });
 
 });
