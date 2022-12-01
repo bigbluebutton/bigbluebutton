876d8aa940ddbee8b53a71a92faae75dbc735502
Lucas Fialho Zawacki <lfzawacki@gmail.com>
2022-12-01 12:15:26 -0300

    feat(captions): write in the pad

--- bigbluebutton-html5/imports/api/audio-captions/server/handlers/transcriptUpdated.js
+++ bigbluebutton-html5/imports/api/audio-captions/server/handlers/transcriptUpdated.js
@@ -1,12 +1,23 @@
 import setTranscript from '/imports/api/audio-captions/server/modifiers/setTranscript';
+import updatePad from '/imports/api/pads/server/methods/updatePad';
+import Users from '/imports/api/users';
 
 export default function transcriptUpdated({ header, body }) {
-  const { meetingId } = header;
+  const {
+    meetingId,
+    userId,
+  } = header;
 
   const {
     transcriptId,
     transcript,
+    locale,
   } = body;
+
+  const user = Users.findOne({ userId }, { fields: { name: 1 } });
+  const datetime = new Date(Date.now());
+  const userSpoke = `\n ${user.name} (${datetime.getHours()}:${datetime.getMinutes()}): ${transcript}`;
+  updatePad(meetingId, userId, 'en', userSpoke);
 
   setTranscript(meetingId, transcriptId, transcript);
 }
