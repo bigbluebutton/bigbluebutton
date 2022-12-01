76ecbb3085c00960301753801e0de400bc99ef2f
Gabriel Porfirio <gabrielporfirio1994@gmail.com>
2022-12-01 11:39:26 -0300

    removed only flag

--- bigbluebutton-tests/playwright/options/options.spec.js
+++ bigbluebutton-tests/playwright/options/options.spec.js
@@ -24,7 +24,7 @@ test.describe.parallel('Settings', () => {
     await language.localesTest();
   });
 
-  test.only('Dark mode', async ({ browser, page }) => {
+  test('Dark mode', async ({ browser, page }) => {
     const darkModeTest = new Options(browser, page);
     await darkModeTest.init(true, true);
     await darkModeTest.darkMode();
