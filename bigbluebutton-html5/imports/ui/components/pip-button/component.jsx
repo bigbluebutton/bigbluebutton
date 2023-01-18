0a7405fff843484f74c1c9abd176fdacabaac5dc
Joao Victor <joaov@imdt.com.br>
2023-01-18 13:33:22 -0300

    Tweak picture-in-picture button style

--- bigbluebutton-html5/imports/ui/components/pip-button/component.jsx
+++ bigbluebutton-html5/imports/ui/components/pip-button/component.jsx
@@ -63,31 +63,7 @@ const PictureInPictureButtonComponent = ({
       right={isIphone}
     >
       <Styled.PipButton
-        customIcon={(
-          isInPictureInPictureMode
-            ? (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                width="24"
-                height="24"
-              >
-                <path fill="none" d="M0 0h24v24H0z" />
-                <path d="M21 3a1 1 0 0 1 1 1v7h-2V5H4v14h6v2H3a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h18zm0 10a1 1 0 0 1 1 1v6a1 1 0 0 1-1 1h-8a1 1 0 0 1-1-1v-6a1 1 0 0 1 1-1h8zm-9.5-6L9.457 9.043l2.25 2.25-1.414 1.414-2.25-2.25L6 12.5V7h5.5z" fill="rgba(255,255,255,1)" />
-              </svg>
-            )
-            : (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                width="24"
-                height="24"
-              >
-                <path fill="none" d="M0 0h24v24H0z" />
-                <path d="M21 3a1 1 0 0 1 1 1v7h-2V5H4v14h6v2H3a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h18zm0 10a1 1 0 0 1 1 1v6a1 1 0 0 1-1 1h-8a1 1 0 0 1-1-1v-6a1 1 0 0 1 1-1h8zM6.707 6.293l2.25 2.25L11 6.5V12H5.5l2.043-2.043-2.25-2.25 1.414-1.414z" fill="rgba(255,255,255,1)" />
-              </svg>
-            )
-        )}
+        icon={isInPictureInPictureMode ? 'video_off' : 'video'}
         color={color || 'default'}
         size="sm"
         onClick={() => handleClick()}
--- bigbluebutton-html5/imports/ui/components/pip-button/styles.js
+++ bigbluebutton-html5/imports/ui/components/pip-button/styles.js
@@ -1,23 +1,30 @@
 import styled from 'styled-components';
 import Button from '/imports/ui/components/common/button/component';
+import {
+  colorWhite,
+  colorBlack,
+  colorTransparent,
+} from '/imports/ui/stylesheets/styled-components/palette';
 
 const PipButton = styled(Button)`
   display: flex;
   padding: 5px;
+
   &,
   &:active,
   &:hover,
   &:focus {
     border: none !important;
-    background-color: var(--color-transparent) !important;
-    svg {
+    background-color: ${colorTransparent} !important;
+
+    i {
       border: none !important;
-      background-color: var(--color-transparent) !important;
+      background-color: ${colorTransparent} !important;
     }
   }
-  svg {
-    width: 1rem;
-    height: 1rem;
+
+  i {
+    font-size: 1rem;
   }
 `;
 
@@ -25,33 +32,39 @@ const Wrapper = styled.div`
   position: absolute;
   right: 2rem;
   left: auto;
-  background-color: var(--color-transparent);
+  background-color: ${colorTransparent};
   cursor: pointer;
   border: 0;
   z-index: 2;
   margin: 2px;
+
   [dir="rtl"] & {
     right: auto;
     left: 2rem;
   }
+
   ${({ dark }) => dark && `
     background-color: rgba(0, 0, 0, .3);
-    & .button path:nth-child(2) {
-      fill: var(--color-white);
+    i {
+      color: ${colorWhite};
     }
   `}
+
   ${({ light }) => light && `
-    background-color: var(--color-transparent);
-    & .button path:nth-child(2) {
-      fill: var(--color-black);
+    background-color: ${colorTransparent};
+    i {
+      color: ${colorBlack};
     }
   `}
+
   ${({ bottom }) => bottom && `
     bottom: 0;
   `}
+
   ${({ top }) => top && `
     top: 0;
   `}
+
   ${({ right }) => right && `
     right: 0;
     [dir="rtl"] & {
