cc21f1598bf0f363e2d3c749ad534c2db8f25e65
Daniel Schreiber <daniel.schreiber@hrz.tu-chemnitz.de>
2023-01-23 20:31:53 +0100

    Copy in fixups from review
    thanks @prlanzarin for reviewing:
    https://github.com/bigbluebutton/bigbluebutton/pull/16107#pullrequestreview-1241137609

--- bigbluebutton-html5/imports/ui/components/audio/local-echo/service.js
+++ bigbluebutton-html5/imports/ui/components/audio/local-echo/service.js
@@ -3,12 +3,56 @@ import browserInfo from '/imports/utils/browserInfo';
 
 const MEDIA_TAG = Meteor.settings.public.media.mediaTag;
 const USE_RTC_LOOPBACK_CHR = Meteor.settings.public.media.localEchoTest.useRtcLoopbackInChromium;
+const {
+  enabled: DELAY_ENABLED = true,
+  delayTime = 0.5,
+  maxDelayTime = 2,
+} = Meteor.settings.public.media.localEchoTest.delay;
+
+let audioContext = null;
+let sourceContext = null;
+let delayNode = null;
 
 const useRTCLoopback = () => (browserInfo.isChrome || browserInfo.isEdge) && USE_RTC_LOOPBACK_CHR;
 const createAudioRTCLoopback = () => new LocalPCLoopback({ audio: true });
 
+const cleanupDelayNode = () => {
+  if (delayNode) {
+    delayNode.disconnect();
+    delayNode = null;
+  }
+
+  if (sourceContext) {
+    sourceContext.disconnect();
+    sourceContext = null;
+  }
+
+  if (audioContext) {
+    audioContext.close();
+    audioContext = null;
+  }
+};
+
+const addDelayNode = (stream) => {
+  if (stream) {
+    if (delayNode || audioContext || sourceContext) cleanupDelayNode();
+
+    audioContext = new AudioContext();
+    sourceContext = audioContext.createMediaStreamSource(stream);
+    delayNode = new DelayNode(audioContext, { delayTime, maxDelayTime });
+    sourceContext.connect(delayNode);
+    delayNode.connect(audioContext.destination);
+    delayNode.delayTime.setValueAtTime(delayTime, audioContext.currentTime);
+  }
+};
 const deattachEchoStream = () => {
   const audioElement = document.querySelector(MEDIA_TAG);
+
+  if (DELAY_ENABLED) {
+    audioElement.muted = false;
+    cleanupDelayNode();
+  }
+
   audioElement.pause();
   audioElement.srcObject = null;
 };
@@ -27,14 +71,13 @@ const playEchoStream = async (stream, loopbackAgent = null) => {
         loopbackAgent.stop();
       }
     }
-    const audioCtx = new AudioContext();
-    const source = audioCtx.createMediaStreamSource(stream);
-    const delayNode = new DelayNode(audioCtx, {delayTime: 0.5, maxDelayTime: 2});
-    delayNode.delayTime.setValueAtTime(0.5, audioCtx.currentTime);
-    const streamdest = audioCtx.createMediaStreamDestination();
-    source.connect(delayNode);
-    delayNode.connect(audioCtx.destination)
-    audioElement.srcObject = streamdest.stream;
+
+    if (DELAY_ENABLED) {
+      // Start muted to avoid weird artifacts and prevent playing the stream twice (Chromium)
+      audioElement.muted = true;
+      addDelayNode(streamToPlay);
+    }
+    audioElement.srcObject = streamToPlay;
     audioElement.play();
   }
 };
