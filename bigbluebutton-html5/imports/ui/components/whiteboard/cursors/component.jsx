8f5c6dd4d3247c8b5bf4486aa81068ca7c278f5e
hiroshisuga <45039819+hiroshisuga@users.noreply.github.com>
2023-01-28 17:16:37 +0900

    revert adding presentationWindow

--- bigbluebutton-html5/imports/ui/components/whiteboard/cursors/component.jsx
+++ bigbluebutton-html5/imports/ui/components/whiteboard/cursors/component.jsx
@@ -182,8 +182,8 @@ export default function Cursors(props) {
     const camPosition = document.getElementById('layout')?.getAttribute('data-cam-position') || null;
     const sl = document.getElementById('layout')?.getAttribute('data-layout');
     const presentationContainer = document.querySelector('[data-test="presentationContainer"]');
-    //Only this one needs to be obtained from presentationWindow
-    const presentation = presentationWindow.document.getElementById('currentSlideText')?.parentElement;
+    //Only this one needs to be obtained from presentationWindow, but when the presentation is re-attached, this will become null.. so stay without presentationWindow
+    const presentation = document.getElementById('currentSlideText')?.parentElement;
     const banners = document.querySelectorAll('[data-test="notificationBannerBar"]');
     let yOffset = 0;
     let xOffset = 0;
