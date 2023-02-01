d6ab5f6a080f4c67fbcef3b1401bc5402118620f
Anton Georgiev <antobinary@users.noreply.github.com>
2023-02-01 12:44:18 -0500

    chore: bump bbb-playback to v5.0.0-rc.1

--- bbb-playback.placeholder.sh
+++ bbb-playback.placeholder.sh
@@ -1,1 +1,1 @@
-git clone --branch v5.0.0-beta.2 --depth 1 https://github.com/bigbluebutton/bbb-playback bbb-playback
+git clone --branch v5.0.0-rc.1 --depth 1 https://github.com/bigbluebutton/bbb-playback bbb-playback
