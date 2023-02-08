3da7081264c0ad081dc0a733aba32fa52921f2fa
Anton Georgiev <anto.georgiev@gmail.com>
2023-02-08 22:33:55 +0000

    chore: bump bbb-presentation-video to 4.0.0-beta.4

---
+++ bbb-presentation-video.placeholder.sh
@@ -0,0 +1,2 @@
+https://github.com/bigbluebutton/bbb-presentation-video/releases/tag/4.0.0-beta.4
+git clone --branch 4.0.0-beta.4 --depth 1 https://github.com/bigbluebutton/bbb-presentation-video bbb-presentation-video
