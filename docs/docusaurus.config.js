e4b9637eef70dc803cea85b77ca04b4ed7b65290
timo <timo.nogueira.brockmeyer@posteo.net>
2023-02-09 11:02:57 +0100

    Use GitHub Actions and Pages to build and deploy the docs
    This commit introduces GitHub Actions to build the docs and deploy them
    to GitHub Pages with the following behavior:
    
    * The job will only be triggered for pushes to the branches 'develop' or
    those starting with 'v' and changes to the 'docs'-folder in order to
    avoid excessive triggering.
    * Jobs that are triggered concurrently on different branches are
      canceled and only the last job will be executed.
    
    The template for this action is taken from the original docusaurus
    documentation (https://docusaurus.io/docs/deployment#deploying-to-github-pages).
    
    Closes #16665

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -8,8 +8,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
     title: 'BigBlueButton',
     tagline: 'Official Documentation',
-    url: 'https://docs.bigbluebutton.org/',
-    baseUrl: '/',
+    url: 'https://bigbluebutton.github.io/',
+    baseUrl: '/bigbluebutton/',
     onBrokenLinks: 'throw',
     onBrokenMarkdownLinks: 'warn',
     favicon: 'img/favicon.ico',
@@ -17,7 +17,7 @@ const config = {
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
     organizationName: 'bigbluebutton', // Usually your GitHub org/user name.
-    projectName: 'bigbluebutton-docs', // Usually your repo name.
+    projectName: 'bigbluebutton', // Usually your repo name.
 
     // Even if you don't use internalization, you can use this field to set useful
     // metadata like html lang. For example, if your site is Chinese, you may want
