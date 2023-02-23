d2927c62fb64e49d9871388b25183d36d212e664
KDSBrowne <kert.browne85@gmail.com>
2023-02-23 23:58:27 +0000

    remove unused import

--- bigbluebutton-html5/imports/ui/components/whiteboard/component.jsx
+++ bigbluebutton-html5/imports/ui/components/whiteboard/component.jsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import ReactDOM from 'react-dom';
 import _ from "lodash";
 import styled, { createGlobalStyle } from "styled-components";
 import Cursors from "./cursors/container";
