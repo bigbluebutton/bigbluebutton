c9cb2596fbf0df83b91050fff3c3537ab3c2e27e
Anton Georgiev <antobinary@users.noreply.github.com>
2023-03-06 11:14:16 -0500

    build(bbb-presentation-video): Bump to 4.0.0-rc.1

--- bbb-presentation-video.placeholder.sh
+++ bbb-presentation-video.placeholder.sh
@@ -1,1 +1,1 @@
-git clone --branch 4.0.0-beta.5 --depth 1 https://github.com/bigbluebutton/bbb-presentation-video bbb-presentation-video
+git clone --branch 4.0.0-rc.1 --depth 1 https://github.com/bigbluebutton/bbb-presentation-video bbb-presentation-video
