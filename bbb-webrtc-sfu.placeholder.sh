ad1e70de3a8c9e7fa3cabaae3b6dc92f68a36784
prlanzarin <4529051+prlanzarin@users.noreply.github.com>
2023-03-13 13:54:47 -0300

    build(bbb-webrtc-sfu): v2.8.9
    See https://github.com/bigbluebutton/bbb-webrtc-sfu/releases/tag/v2.8.9

--- bbb-webrtc-sfu.placeholder.sh
+++ bbb-webrtc-sfu.placeholder.sh
@@ -1,1 +1,1 @@
-git clone --branch v2.8.8 --depth 1 https://github.com/bigbluebutton/bbb-webrtc-sfu bbb-webrtc-sfu
+git clone --branch v2.8.9 --depth 1 https://github.com/bigbluebutton/bbb-webrtc-sfu bbb-webrtc-sfu
