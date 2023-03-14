b3a645167d32978bed93bbe4fa13e344f2f454d3
imdt <andrezero@live.com>
2023-03-14 14:27:52 -0300

    Presentations back-end migrated to new async API.

--- bigbluebutton-html5/imports/api/presentations/server/modifiers/setCurrentPresentation.js
+++ bigbluebutton-html5/imports/api/presentations/server/modifiers/setCurrentPresentation.js
@@ -2,7 +2,7 @@ import { check } from 'meteor/check';
 import Presentations from '/imports/api/presentations';
 import Logger from '/imports/startup/server/logger';
 
-export default function setCurrentPresentation(meetingId, podId, presentationId) {
+export default async function setCurrentPresentation(meetingId, podId, presentationId) {
   check(meetingId, String);
   check(presentationId, String);
   check(podId, String);
@@ -48,23 +48,22 @@ export default function setCurrentPresentation(meetingId, podId, presentationId)
     },
   };
 
-  const oldPresentation = Presentations.findOne(oldCurrent.selector);
-  const newPresentation = Presentations.findOne(newCurrent.selector);
+  const oldPresentation = await Presentations.findOneAsync(oldCurrent.selector);
+  const newPresentation = await Presentations.findOneAsync(newCurrent.selector);
 
   if (oldPresentation) {
-    try{
-      Presentations.update(oldCurrent.selector, oldCurrent.modifier, {multi: true});
-    } catch(e){
+    try {
+      await Presentations.updateAsync(oldCurrent.selector, oldCurrent.modifier, { multi: true });
+    } catch (e) {
       oldCurrent.callback(e);
     }
   }
 
   if (newPresentation) {
-    try{
-      Presentations.update(newPresentation._id, newCurrent.modifier);
-    } catch(e){
+    try {
+      await Presentations.updateAsync(newPresentation._id, newCurrent.modifier);
+    } catch (e) {
       newCurrent.callback(e);
     }
   }
 }
- 
