a08af05b210d9f3a67e75195230264669c96f48b
timo <timo.nogueira.brockmeyer@posteo.net>
2023-03-21 17:26:57 +0100

    Link to the current end user support pages
    Currently the category 'Teaching' is still empty in the docs. So for
    now, we link to existing resource sites outside of the docs.
    
    related to #17055

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -60,7 +60,7 @@ const config = {
                     src: 'img/logo.svg',
                 },
                 items: [
-                    {to: '/teaching', label: 'Teaching', position: 'left'},
+                    {to: 'https://bigbluebutton.org/teachers/tutorials/', label: 'Teaching', position: 'left'},
                     {to: '/development/guide', label: 'Development', position: 'left'},
                     {to: '/administration/install', label: 'Administration', position: 'left'},
                     {to: '/greenlight/v2/overview', label: 'Greenlight', position: 'left'},
