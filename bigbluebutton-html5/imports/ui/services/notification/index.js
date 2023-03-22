fa5fa6844c02903e3b3297b3ac5d22809524a96d
Carlos Henrique <carloshsc1998@gmail.com>
2023-03-22 14:26:40 -0300

    feat(screenshare): add support for troubleshooting links
    Adds setting option to specify troubleshooting links to each error code
    of screenshare. When a troubleshooting link for the given error exists,
    the toast notification about the error is displayed with a 'Learn more'
    button that when clicked leads the user to the external link. When there
    is no link set for the specific error code, the button is not displayed.

--- bigbluebutton-html5/imports/ui/services/notification/index.js
+++ bigbluebutton-html5/imports/ui/services/notification/index.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { toast } from 'react-toastify';
 import { isEqual } from 'radash';
-
+import Styled from './styles';
 import Toast from '/imports/ui/components/common/toast/component';
 
 let lastToast = {
@@ -28,6 +28,20 @@ export function notify(message, type = 'default', icon, options, content, small)
   };
 
   if (!toast.isActive(lastToast.id) || !isEqual(lastToastProps, toastProps)) {
+    if (options?.helpLink != null && options?.helpLabel != null) {
+      const id = toast(
+        <div role="alert">
+          <Toast {...toastProps} />
+          <Styled.Button onClick={() => { window.open(options.helpLink); }}>
+            {options.helpLabel}
+          </Styled.Button>
+        </div>, settings,
+      );
+
+      lastToast = { id, ...toastProps };
+
+      return id;
+    }
     if (toast.isActive(lastToast.id)
       && isEqual(lastToastProps.key, toastProps.key) && options?.autoClose > 0) {
       toast.update(
---
+++ bigbluebutton-html5/imports/ui/services/notification/styles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const Button = styled.div`
+  position: relative;
+  background: none!important;
+  border: none;
+  padding: 0!important;
+  font-weight: bold;
+  color: #069;
+  text-decoration: underline;
+  cursor: pointer;
+  left: 15.7%;
+  font-size: 12px;
+  top: 2px;
+`;
+
+export default { Button };
