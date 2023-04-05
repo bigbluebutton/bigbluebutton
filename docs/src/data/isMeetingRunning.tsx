d480e10e0c840385d4f2c6414d2fb90c55017e1f
GuiLeme <leme.guilherme.p@gmail.com>
2023-04-05 11:07:51 -0300

    [issue-16930] - render tables with a similar structure as previous docs

---
+++ docs/src/data/isMeetingRunning.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const isMeetingRunningEndpointTableData = [
+  {
+    "name": "meetingID",
+    "required": true,
+    "type": "String",
+    "description": (<>The meeting ID that identifies the meeting you are attempting to check on.</>)
+  }
+];
+
+export default isMeetingRunningEndpointTableData;
