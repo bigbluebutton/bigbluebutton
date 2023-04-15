bb2f8809f322a49a92276afa5420c004d83fafd7
hiroshisuga <45039819+hiroshisuga@users.noreply.github.com>
2023-04-16 00:23:07 +0900

    Update component.jsx

--- bigbluebutton-html5/imports/ui/components/actions-bar/component.jsx
+++ bigbluebutton-html5/imports/ui/components/actions-bar/component.jsx
@@ -74,11 +74,7 @@ class ActionsBar extends PureComponent {
               <CaptionsButtonContainer {...{ intl }} />
             )
             : null}
-          { !deviceInfo.isMobile
-            ? (
-              <AudioCaptionsButtonContainer />
-            )
-            : null }
+          <AudioCaptionsButtonContainer />
         </Styled.Left>
         <Styled.Center>
           <AudioControlsContainer />
