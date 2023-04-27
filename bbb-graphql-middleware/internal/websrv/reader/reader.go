fa5337c46390bc986bcb02e435f040c33ffbe501
Tiago Daniel Jacobs <tiago.jacobs@gmail.com>
2023-04-26 22:26:55 -0300

    fix: wait before closing channels

--- bbb-graphql-middleware/internal/websrv/reader/reader.go
+++ bbb-graphql-middleware/internal/websrv/reader/reader.go
@@ -3,22 +3,29 @@ package reader
 import (
 	"context"
 	log "github.com/sirupsen/logrus"
-	"sync"
-
 	"nhooyr.io/websocket"
 	"nhooyr.io/websocket/wsjson"
+	"sync"
+	"time"
 )
 
 func WebsocketConnectionReader(browserConnectionId string, ctx context.Context, c *websocket.Conn, fromBrowserChannel1 chan interface{}, fromBrowserChannel2 chan interface{}, waitGroups []*sync.WaitGroup) {
 	log := log.WithField("_routine", "WebsocketConnectionReader").WithField("browserConnectionId", browserConnectionId)
 
 	defer func() {
+		close(fromBrowserChannel1)
+		close(fromBrowserChannel2)
+	}()
+
+	defer func() {
+		// Let other routines know this is about to die
 		for _, wg := range waitGroups {
 			wg.Done()
 		}
+		// Wait a little bit before closing the channels
+		time.Sleep(100 * time.Millisecond)
 	}()
-	defer close(fromBrowserChannel1)
-	defer close(fromBrowserChannel2)
+
 	defer log.Infof("finished")
 
 	for {
