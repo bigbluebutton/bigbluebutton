13b871f4d5adb79dbfe3490e97c8f4377a876c85
Gustavo Trott <gustavo@trott.com.br>
2023-05-02 10:00:51 -0300

    Alter graphql database name to bbb_graphql

--- akka-bbb-apps/src/main/scala/org/bigbluebutton/core/db/DatabaseConnection.scala
+++ akka-bbb-apps/src/main/scala/org/bigbluebutton/core/db/DatabaseConnection.scala
@@ -6,7 +6,7 @@ import slick.jdbc.PostgresProfile.api._
 import scala.concurrent.ExecutionContext.Implicits.global
 
 object DatabaseConnection {
-  val db = Database.forConfig("postgres")
+  val db = Database.forConfig("bbb_graphql")
   //  implicit val session: Session = db.createSession()
   val logger = LoggerFactory.getLogger(this.getClass)
 }
