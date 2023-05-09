d2c5b33eec0c9a80a2d7bec4e8d8707ba5b8db04
GuiLeme <leme.guilherme.p@gmail.com>
2023-05-09 12:01:55 -0300

    [issue-17839] - back-end part for hide_presentation_on_join issue

--- bigbluebutton-web/grails-app/controllers/org/bigbluebutton/web/controllers/ApiController.groovy
+++ bigbluebutton-web/grails-app/controllers/org/bigbluebutton/web/controllers/ApiController.groovy
@@ -1412,10 +1412,16 @@ class ApiController {
       def Boolean isCurrent = false;
       def Boolean isRemovable = true;
       def Boolean isDownloadable = false;
+      def Boolean isInitialPresentation = false;
 
       if (document.name != null && "default".equals(document.name)) {
         if (presentationService.defaultUploadedPresentation) {
-          downloadAndProcessDocument(presentationService.defaultUploadedPresentation, conf.getInternalId(), document.current /* default presentation */, '', false, true);
+          if (document.current) {
+            isInitialPresentation = true
+          }
+          downloadAndProcessDocument(presentationService.defaultUploadedPresentation, conf.getInternalId(),
+                  document.current /* default presentation */, '', false,
+                  true, isInitialPresentation);
         } else {
           log.error "Default presentation could not be read, it is (" + presentationService.defaultUploadedPresentation + ")", "error"
         }
@@ -1434,6 +1440,7 @@ class ApiController {
             isCurrent = true
           }
         } else if (index == 0 && !isFromInsertAPI) {
+          isInitialPresentation = true
           isCurrent = true
         }
 
@@ -1445,12 +1452,12 @@ class ApiController {
             fileName = document.@filename.toString();
           }
           downloadAndProcessDocument(document.@url.toString(), conf.getInternalId(), isCurrent /* default presentation */,
-                  fileName, isDownloadable, isRemovable);
+                  fileName, isDownloadable, isRemovable, isInitialPresentation);
         } else if (!StringUtils.isEmpty(document.@name.toString())) {
           def b64 = new Base64()
           def decodedBytes = b64.decode(document.text().getBytes())
           processDocumentFromRawBytes(decodedBytes, document.@name.toString(),
-                  conf.getInternalId(), isCurrent, isDownloadable, isRemovable/* default presentation */);
+                  conf.getInternalId(), isCurrent, isDownloadable, isRemovable/* default presentation */, isInitialPresentation);
         } else {
           log.debug("presentation module config found, but it did not contain url or name attributes");
         }
@@ -1513,7 +1520,7 @@ class ApiController {
     }
   }
 
-  def downloadAndProcessDocument(address, meetingId, current, fileName, isDownloadable, isRemovable) {
+  def downloadAndProcessDocument(address, meetingId, current, fileName, isDownloadable, isRemovable, isInitialPresentation) {
     log.debug("ApiController#downloadAndProcessDocument(${address}, ${meetingId}, ${fileName})");
     String presOrigFilename;
     if (StringUtils.isEmpty(fileName)) {
@@ -1575,7 +1582,8 @@ class ApiController {
               uploadFailed,
               uploadFailReasons,
               isDownloadable,
-              isRemovable
+              isRemovable,
+              isInitialPresentation
       )
     } else {
       org.bigbluebutton.presentation.Util.deleteDirectoryFromFileHandlingErrors(pres)
@@ -1585,7 +1593,7 @@ class ApiController {
 
 
   def processUploadedFile(podId, meetingId, presId, filename, presFile, current,
-                          authzToken, uploadFailed, uploadFailReasons, isDownloadable, isRemovable ) {
+                          authzToken, uploadFailed, uploadFailReasons, isDownloadable, isRemovable, isInitialPresentation ) {
     def presentationBaseUrl = presentationService.presentationBaseUrl
     // TODO add podId
     UploadedPresentation uploadedPres = new UploadedPresentation(podId,
@@ -1596,7 +1604,9 @@ class ApiController {
             current,
             authzToken,
             uploadFailed,
-            uploadFailReasons)
+            uploadFailReasons,
+            isInitialPresentation
+    )
     uploadedPres.setUploadedFile(presFile);
     if (isRemovable != null) {
       uploadedPres.setRemovable(isRemovable);
