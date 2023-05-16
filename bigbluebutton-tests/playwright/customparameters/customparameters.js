1cbec20499bd86c109972cdc1bdcb8855c222662
Anton Barboza <antonbsa.bck@gmail.com>
2023-05-16 08:43:16 -0300

    test: fix hide presentation on join

--- bigbluebutton-tests/playwright/customparameters/customparameters.js
+++ bigbluebutton-tests/playwright/customparameters/customparameters.js
@@ -127,7 +127,7 @@ class CustomParameters extends MultiUsers {
     await expect(notificationBarColor).toBe(colorToRGB);
   }
 
-  async hidePresentation() {
+  async hidePresentationOnJoin() {
     await this.modPage.waitForSelector(e.actions);
     const checkPresentationButton = await this.modPage.checkElement(e.restorePresentation);
     await expect(checkPresentationButton).toBeTruthy();
