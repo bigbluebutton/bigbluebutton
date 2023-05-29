3852520d0baaa30be453f2e7c8614eed7c70a3fe
André Castro <36093456+Scroody@users.noreply.github.com>
2023-05-29 10:21:17 -0300

    Update component.jsx

--- bigbluebutton-html5/imports/ui/components/poll/component.jsx
+++ bigbluebutton-html5/imports/ui/components/poll/component.jsx
@@ -223,13 +223,6 @@ const MAX_CUSTOM_FIELDS = POLL_SETTINGS.maxCustom;
 const MAX_INPUT_CHARS = POLL_SETTINGS.maxTypedAnswerLength;
 const MIN_OPTIONS_LENGTH = 2;
 const QUESTION_MAX_INPUT_CHARS = 1200;
-const POLL_OPTIONS_PLACEHOLDERS = [
-  { val: intlMessages.a },
-  { val: intlMessages.b },
-  { val: intlMessages.c },
-  { val: intlMessages.d },
-  { val: intlMessages.e },
-];
 
 class Poll extends Component {
   constructor(props) {
