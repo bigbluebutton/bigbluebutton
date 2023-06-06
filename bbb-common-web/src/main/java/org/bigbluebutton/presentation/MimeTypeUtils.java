bc090d71bbf3ea1b800073a244fa39b7dcbb69cc
danielpetri1 <daniel.petri@tum.de>
2023-06-06 12:08:21 +0000

    Permit .docx with .doc content

--- bbb-common-web/src/main/java/org/bigbluebutton/presentation/MimeTypeUtils.java
+++ bbb-common-web/src/main/java/org/bigbluebutton/presentation/MimeTypeUtils.java
@@ -12,6 +12,7 @@ public class MimeTypeUtils {
     private  static final String PPT = "application/vnd.ms-powerpoint";
     private  static final String PPTX = "application/vnd.openxmlformats-officedocument.presentationml.presentation";
     private  static final String TIKA_MSOFFICE = "application/x-tika-msoffice";
+    private  static final String TIKA_MSOFFICE_X = "application/x-tika-ooxml";
     private  static final String ODT = "application/vnd.oasis.opendocument.text";
     private  static final String RTF = "application/rtf";
     private  static final String TXT = "text/plain";
@@ -24,12 +25,12 @@ public class MimeTypeUtils {
 
     private static final HashMap<String, List<String>> EXTENSIONS_MIME = new HashMap<String, List<String>>(16) {
         {
-            put(FileTypeConstants.DOC, Arrays.asList(DOC, DOCX, TIKA_MSOFFICE));
-            put(FileTypeConstants.XLS, Arrays.asList(XLS, XLSX, TIKA_MSOFFICE));
-            put(FileTypeConstants.PPT, Arrays.asList(PPT, PPTX, TIKA_MSOFFICE));
-            put(FileTypeConstants.DOCX, Arrays.asList(DOCX));
-            put(FileTypeConstants.PPTX, Arrays.asList(PPTX));
-            put(FileTypeConstants.XLSX, Arrays.asList(XLSX));
+            put(FileTypeConstants.DOC, Arrays.asList(DOC, DOCX, TIKA_MSOFFICE, TIKA_MSOFFICE_X));
+            put(FileTypeConstants.XLS, Arrays.asList(XLS, XLSX, TIKA_MSOFFICE, TIKA_MSOFFICE_X));
+            put(FileTypeConstants.PPT, Arrays.asList(PPT, PPTX, TIKA_MSOFFICE, TIKA_MSOFFICE_X));
+            put(FileTypeConstants.DOCX, Arrays.asList(DOC, DOCX, TIKA_MSOFFICE, TIKA_MSOFFICE_X));
+            put(FileTypeConstants.PPTX, Arrays.asList(PPT, PPTX, TIKA_MSOFFICE, TIKA_MSOFFICE_X));
+            put(FileTypeConstants.XLSX, Arrays.asList(XLS, XLSX, TIKA_MSOFFICE, TIKA_MSOFFICE_X));
             put(FileTypeConstants.ODT, Arrays.asList(ODT));
             put(FileTypeConstants.RTF, Arrays.asList(RTF));
             put(FileTypeConstants.TXT, Arrays.asList(TXT));
@@ -60,7 +61,7 @@ public class MimeTypeUtils {
     public List<String> getValidMimeTypes() {
         List<String> validMimeTypes = Arrays.asList(XLS, XLSX,
                 DOC, DOCX, PPT, PPTX, ODT, RTF, TXT, ODS, ODP,
-                PDF, JPEG, PNG, SVG, TIKA_MSOFFICE
+                PDF, JPEG, PNG, SVG, TIKA_MSOFFICE, TIKA_MSOFFICE_X
         );
         return validMimeTypes;
     }
