f7052d503db0671f6888e5a731dab8963ec95256
Anton Georgiev <antobinary@users.noreply.github.com>
2023-06-06 08:10:30 -0400

    Update docusaurus.config.js

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -142,7 +142,7 @@ const config = {
                     {
                         type: 'doc',
                         position: 'left',
-                        docId: 'greenlight/v3/install#overview',
+                        docId: 'greenlight/v3/install',
                         label: 'Greenlight',
                     },
                     {
