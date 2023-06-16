026cc92a176a9363c9cee350016bea5bc3075840
Ramón Souza <contato@ramonsouza.com>
2023-06-16 13:04:05 -0300

    fix copy event in breakout panel

--- bigbluebutton-html5/imports/ui/components/breakout-room/component.jsx
+++ bigbluebutton-html5/imports/ui/components/breakout-room/component.jsx
@@ -555,7 +555,7 @@ class BreakoutRoom extends PureComponent {
       isRTL,
     } = this.props;
     return (
-      <Styled.Panel ref={(n) => this.panel = n}>
+      <Styled.Panel ref={(n) => this.panel = n} onCopy={(e) => { e.stopPropagation(); }}>
         <Header
           leftButtonProps={{
             'aria-label': intl.formatMessage(intlMessages.breakoutAriaTitle),
