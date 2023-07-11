4fbe23d2a4a1e645cac971b9d495edb684240287
Anton Georgiev <anto.georgiev@gmail.com>
2023-07-11 12:11:37 -0400

    build: Newer FreeSWITCH - up to merged PR 2069

--- freeswitch.placeholder.sh
+++ freeswitch.placeholder.sh
@@ -2,5 +2,8 @@ mkdir freeswitch
 cd freeswitch
 git init
 git remote add origin https://github.com/signalwire/freeswitch.git
-git fetch --depth 1 origin v1.10.9
-git checkout FETCH_HEAD
+#git fetch --depth 1 origin v1.10.9
+#git checkout FETCH_HEAD
+git fetch origin master
+git checkout 67840823c178153cb013014c4fa780fe233612cb
+
