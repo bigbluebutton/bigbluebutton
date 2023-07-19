005f911189d2fc652dabed94f21516e1dfb874e3
Anton B <antonbsa.bck@gmail.com>
2023-07-19 17:44:38 -0300

    test: fix brand logo url

--- bigbluebutton-tests/playwright/customparameters/constants.js
+++ bigbluebutton-tests/playwright/customparameters/constants.js
@@ -10,7 +10,7 @@ exports.docTitle = docTitle;
 exports.clientTitle = `userdata-bbb_client_title=${docTitle}`;
 exports.askForFeedbackOnLogout = 'userdata-bbb_ask_for_feedback_on_logout=true';
 exports.displayBrandingArea = 'userdata-bbb_display_branding_area=true';
-exports.logo = 'logo=https://bigbluebutton.org/wp-content/themes/bigbluebutton/library/images/bigbluebutton-logo.png';
+exports.logo = 'logo=https://bigbluebutton.org/wp-content/uploads/2021/01/BigBlueButton_icon.svg.png';
 exports.enableVideo = 'userdata-bbb_enable_video=false';
 exports.autoShareWebcam = 'userdata-bbb_auto_share_webcam=true';
 exports.multiUserPenOnly = 'userdata-bbb_multi_user_pen_only=true';
