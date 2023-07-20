543be9e44e5d43fec24f4ec54c06cf75709cd5b2
Anton Georgiev <anto.georgiev@gmail.com>
2023-07-19 20:31:02 -0400

    sfu after-install to not mention kurento

--- build/packages-template/bbb-graphql-server/after-install.sh
+++ build/packages-template/bbb-graphql-server/after-install.sh
@@ -6,8 +6,17 @@ case "$1" in
   fc-cache -f
 
   sudo -u postgres psql -c "alter user postgres password 'bbb_graphql'"
-  sudo -u postgres psql -c "drop database if exists bbb_graphql"
-  sudo -u postgres psql -c "create database bbb_graphql"
+
+  DATABASE_NAME="hasura_app"
+  DB_EXISTS=$(sudo -u postgres psql -U postgres -tAc "SELECT 1 FROM pg_database WHERE datname='$DATABASE_NAME'")
+  if [ "$DB_EXISTS" = '1' ]
+  then
+      echo "Database $DATABASE_NAME already exists"
+  else
+      sudo -u postgres psql -c "create database hasura_app"
+      echo "Database $DATABASE_NAME created"
+  fi
+
   sudo -u postgres psql -U postgres -d bbb_graphql -a -f /usr/share/bbb-graphql-server/bbb_schema.sql --set ON_ERROR_STOP=on
   sudo -u postgres psql -c "drop database if exists hasura_app"
 
--- build/packages-template/bbb-webrtc-sfu/after-install.sh
+++ build/packages-template/bbb-webrtc-sfu/after-install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 
 source /etc/lsb-release
 
@@ -10,7 +10,7 @@ case "$1" in
     cp /usr/local/bigbluebutton/bbb-webrtc-sfu/config/default.example.yml $TARGET
     chown bigbluebutton:bigbluebutton $TARGET
 
-      yq e -i ".kurento[0].ip = \"$IP\"" $TARGET
+      # yq e -i ".kurento[0].ip = \"$IP\"" $TARGET
 
       # https://github.com/bigbluebutton/bbb-webrtc-sfu/pull/37
       # yq w -i $TARGET kurento[0].url "ws://$SERVER_URL:8888/kurento"
@@ -52,7 +52,7 @@ case "$1" in
       chown -R bigbluebutton:bigbluebutton /usr/local/bigbluebutton/bbb-webrtc-sfu /var/log/bbb-webrtc-sfu/
     else
       echo "#"
-      echo "# Warning: Unable to assign ownership of bigbluebutton to kurento files"
+      echo "# Warning: Unable to assign ownership of bigbluebutton to sfu files"
       echo "#"
     fi
 
@@ -62,36 +62,36 @@ case "$1" in
     fi
 
     # Create a symbolic link from /var/kurento -> /var/lib/kurento if needed
-    if [ ! -d /var/kurento ]; then
-      if [ -d /var/lib/kurento ]; then
-        ln -s /var/lib/kurento /var/kurento
-      fi
-    fi
+    # if [ ! -d /var/kurento ]; then
+    #   if [ -d /var/lib/kurento ]; then
+    #     ln -s /var/lib/kurento /var/kurento
+    #   fi
+    # fi
 
     chmod 644 $TARGET
     chown bigbluebutton:bigbluebutton $TARGET
 
-    if [ ! -d /var/log/kurento-media-server ]; then
-      mkdir -p /var/log/kurento-media-server
-    fi
+    # if [ ! -d /var/log/kurento-media-server ]; then
+    #   mkdir -p /var/log/kurento-media-server
+    # fi
 
-    chown kurento:root /var/log/kurento-media-server
+    # chown kurento:root /var/log/kurento-media-server
 
     # Ensure a default port range is setup
-    if ! grep -v '^;' /etc/kurento/modules/kurento/BaseRtpEndpoint.conf.ini | grep -q minPort; then
-      cat >> /etc/kurento/modules/kurento/BaseRtpEndpoint.conf.ini << HERE
+#     if ! grep -v '^;' /etc/kurento/modules/kurento/BaseRtpEndpoint.conf.ini | grep -q minPort; then
+#       cat >> /etc/kurento/modules/kurento/BaseRtpEndpoint.conf.ini << HERE
 
-# Added by bbb-webrtc-sfu.postinst $(date)
-minPort=24577
-maxPort=32768
-HERE
-    fi
+# # Added by bbb-webrtc-sfu.postinst $(date)
+# minPort=24577
+# maxPort=32768
+# HERE
+#     fi
 
     # Check if using Kurento packages with focal
 
     reloadService nginx
     startService bbb-webrtc-sfu        || echo "bbb-webrtc-sfu could not be registered or started"
-    startService kurento-media-server  || echo "kurento-media-serve could not be registered or started"
+    # startService kurento-media-server  || echo "kurento-media-serve could not be registered or started"
 
   ;;
 
