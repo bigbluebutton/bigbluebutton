38c6da7c2924e7545d2570eca37db339f79a86bc
Ramón Souza <contato@ramonsouza.com>
2023-08-09 15:42:21 -0300

    add comment about debounce function

--- bigbluebutton-html5/imports/utils/debounce.js
+++ bigbluebutton-html5/imports/utils/debounce.js
@@ -1,3 +1,12 @@
+/**
+ * Debounce function, includes leading and trailing options (lodash-like)
+ * @param {Function} func - function to be debounced
+ * @param {Number} delay - delay in milliseconds
+ * @param {Object} options - options object
+ * @param {Boolean} options.leading - whether to invoke the function on the leading edge
+ * @param {Boolean} options.trailing - whether to invoke the function on the trailing edge
+ * @returns {Function} - debounced function
+ */
 export function debounce(func, delay, options = {}) {
   let timeoutId;
   let lastArgs;
