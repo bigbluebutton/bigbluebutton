8fcbfcf76ed1d189b35b575c19639afc98217f84
Paul Trudel <paatrudel@gmail.com>
2023-08-10 18:07:59 +0000

    Fixed upsert query

--- akka-bbb-apps/src/main/scala/org/bigbluebutton/core/db/AudioCaptionDAO.scala
+++ akka-bbb-apps/src/main/scala/org/bigbluebutton/core/db/AudioCaptionDAO.scala
@@ -21,30 +21,17 @@ class AudioCaptionTableDef(tag: Tag) extends Table[AudioCaptionDbModel](tag, Non
 object AudioCaptionDAO {
 
   def insertOrUpdateAudioCaption(transcriptId: String, meetingId: String, transcript: String) = {
-    //    DatabaseConnection.db.run(
-    //      TableQuery[AudioCaptionTableDef]
-    //        .filter(_.transcriptId === transcriptId)
-    //        .map(a => a.transcript)
-    //        .update((transcript))
-    //    ).onComplete {
-    //        case Success(rowsAffected) => rowsAffected match {
-    //          case 0 => DatabaseConnection.db.run(
-    //            TableQuery[AudioCaptionTableDef] += AudioCaptionDbModel(transcriptId, meetingId, transcript)
-    //          ).onComplete {
-    //              case Success    => DatabaseConnection.logger.debug(s"Inserted new audio caption with ID $transcriptId on AudioCaption table")
-    //              case Failure(e) => DatabaseConnection.logger.debug(s"Error inserting or updating audio caption on AudioCaption: $e")
-    //            }
-    //          case 1 => DatabaseConnection.logger.debug(s"Updated audio caption with ID $transcriptId on AudioCaption table")
-    //          case n => DatabaseConnection.logger.debug(s"Error: Expected to insert or update a single row not $n rows")
-    //        }
-    //        case Failure(e) => DatabaseConnection.logger.debug(s"Error updating audio caption on AudioCaption: $e")
-    //      }
-    TableQuery[AudioCaptionTableDef].insertOrUpdate(
-      AudioCaptionDbModel(
-        transcriptId = transcriptId,
-        meetingId = meetingId,
-        transcript = transcript
+    DatabaseConnection.db.run(
+      TableQuery[AudioCaptionTableDef].insertOrUpdate(
+        AudioCaptionDbModel(
+          transcriptId = transcriptId,
+          meetingId = meetingId,
+          transcript = transcript
+        )
       )
-    )
+    ).onComplete {
+        case Success(_) => DatabaseConnection.logger.debug(s"Upserted audio caption with ID $transcriptId on AudioCaption table")
+        case Failure(e) => DatabaseConnection.logger.debug(s"Error upserting audio caption on AudioCaption: $e")
+      }
   }
 }
