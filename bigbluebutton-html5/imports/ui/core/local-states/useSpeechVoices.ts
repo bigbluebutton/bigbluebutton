2523858e7356336ed58942de6f374ca68629c74e
Tainan Felipe <tainanfelipe214@gmail.com>
2023-08-11 08:30:09 -0300

    Remove unecessary files

--- bigbluebutton-html5/imports/ui/core/local-states/useSpeechVoices.ts
+++
@@ -1,9 +0,0 @@
-import createUseLocalState from './createUseLocalState';
-
-const initialPendingChat: string[] = [];
-const [useSpeechVoices, setSpeechVoices, localStateSpeechVoices] =
-  createUseLocalState<string[]>(initialPendingChat);
-
-export default useSpeechVoices;
-
-export { setSpeechVoices, localStateSpeechVoices };
