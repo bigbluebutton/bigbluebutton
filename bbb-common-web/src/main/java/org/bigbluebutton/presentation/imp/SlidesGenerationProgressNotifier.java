14c59a90a739dc3275f092e460cd5f6d53efb243
GuiLeme <leme.guilherme.p@gmail.com>
2023-08-15 11:22:50 -0300

    [issue-18449] - changes in review

--- bbb-common-web/src/main/java/org/bigbluebutton/presentation/imp/SlidesGenerationProgressNotifier.java
+++ bbb-common-web/src/main/java/org/bigbluebutton/presentation/imp/SlidesGenerationProgressNotifier.java
@@ -108,7 +108,6 @@ public class SlidesGenerationProgressNotifier {
       log.error("GeneratedSlidesInfoHelper was not set. Could not notify interested listeners.");
       return;
     }
-    // Completed conversion -> delete original file
     DocPageCompletedProgress progress = new DocPageCompletedProgress(pres.getPodId(), pres.getMeetingId(),
       pres.getId(), pres.getTemporaryPresentationId(), pres.getId(),
       pres.getName(), "notUsedYet", "notUsedYet",
