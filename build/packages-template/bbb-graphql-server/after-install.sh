8d3f8be6ff31bbfa758361dc7111dd906d26d386
Gustavo Trott <gustavo@trott.com.br>
2023-08-23 09:26:32 -0300

    Fix creating database hasura_app twice

--- build/packages-template/bbb-graphql-server/after-install.sh
+++ build/packages-template/bbb-graphql-server/after-install.sh
@@ -9,6 +9,7 @@ case "$1" in
   sudo -u postgres psql -c "drop database if exists bbb_graphql"
   sudo -u postgres psql -c "create database bbb_graphql"
   sudo -u postgres psql -c "alter database bbb_graphql set timezone to 'UTC'"
+  sudo -u postgres psql -U postgres -d bbb_graphql -a -f /usr/share/bbb-graphql-server/bbb_schema.sql --set ON_ERROR_STOP=on
 
   DATABASE_NAME="hasura_app"
   DB_EXISTS=$(sudo -u postgres psql -U postgres -tAc "SELECT 1 FROM pg_database WHERE datname='$DATABASE_NAME'")
@@ -20,10 +21,6 @@ case "$1" in
       echo "Database $DATABASE_NAME created"
   fi
 
-  sudo -u postgres psql -U postgres -d bbb_graphql -a -f /usr/share/bbb-graphql-server/bbb_schema.sql --set ON_ERROR_STOP=on
-  sudo -u postgres psql -c "drop database if exists hasura_app"
-
-  sudo -u postgres psql -c "create database hasura_app"
   echo "Postgresql configured"
 
   if [ ! -f /.dockerenv ]; then
