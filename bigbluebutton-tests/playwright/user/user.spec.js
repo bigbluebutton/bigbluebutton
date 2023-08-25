f4c5b7cf7760cd5fe7682a11946078b2f04ea8e6
Gabriel Luiz Porfirio <gabrielporfirio1994@gmail.com>
2023-08-25 14:58:54 -0300

    Update user.spec.js

--- bigbluebutton-tests/playwright/user/user.spec.js
+++ bigbluebutton-tests/playwright/user/user.spec.js
@@ -78,7 +78,7 @@ test.describe.parallel('User', () => {
       await multiusers.giveAndRemoveWhiteboardAccess();
     });
 
-    test.only('Remove user @ci', async ({ browser, context, page }) => {
+    test('Remove user @ci', async ({ browser, context, page }) => {
       const multiusers = new MultiUsers(browser, context);
       await multiusers.initModPage(page, true);
       await multiusers.initModPage2(true);
