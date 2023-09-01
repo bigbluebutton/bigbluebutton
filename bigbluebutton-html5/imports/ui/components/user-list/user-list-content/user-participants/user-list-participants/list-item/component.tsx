3756a83374459b43de36c2e918d9cfcfd55bdd87
Gabriel Porfirio <gabrielporfirio1994@gmail.com>
2023-09-01 14:01:05 -0300

    id for the tests

--- bigbluebutton-html5/imports/ui/components/user-list/user-list-content/user-participants/user-list-participants/list-item/component.tsx
+++ bigbluebutton-html5/imports/ui/components/user-list/user-list-content/user-participants/user-list-participants/list-item/component.tsx
@@ -93,7 +93,7 @@ const UserListItem: React.FC<UserListItemProps> = ({ user, lockSettings }) => {
 
    const avatarContent = user.lastBreakoutRoom?.currentlyInRoom ? user.lastBreakoutRoom?.sequence : iconUser
 
-  return <Styled.UserItemContents>
+  return <Styled.UserItemContents id='user'>
   <Styled.Avatar
     moderator={user.role === ROLE_MODERATOR}
     presenter={user.presenter}
