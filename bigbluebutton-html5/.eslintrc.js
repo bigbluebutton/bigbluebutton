8ea34e7e0cbd89218ec863a915b273d307fd8f7e
André Castro <36093456+Scroody@users.noreply.github.com>
2023-09-06 10:37:34 -0300

    Fix: Chat List Component TypeScript Errors, Linting, and ESLint Rules Update (#18635)
    * rules addition and ts errors fixed on chat list component
    
    * requested change
    
    * code duplicate removed
    
    * Update component.tsx

--- bigbluebutton-html5/.eslintrc.js
+++ bigbluebutton-html5/.eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: 'airbnb',
+  extends: ['plugin:@typescript-eslint/recommended', 'airbnb'],
   parserOptions: {
     ecmaVersion: 2020,
   },
@@ -11,26 +11,49 @@ module.exports = {
     meteor: true,
     jasmine: true,
   },
-  rules: {
-    'no-underscore-dangle': 0,
-    'import/extensions': [2, 'never'],
-    'import/no-absolute-path': 0,
-    'import/no-unresolved': 0,
-    'import/no-extraneous-dependencies': 1,
-    'react/prop-types': 1,
-    'jsx-a11y/no-access-key': 0,
-    'react/jsx-props-no-spreading': 'off',
-    'max-classes-per-file': ['error', 2],
-  },
-  globals: {
-    browser: 'writable',
-  },
-  overrides: [
-    {
-      files: ['*.ts', '*.tsx'],
-      extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'airbnb'],
-      parser: '@typescript-eslint/parser',
-      plugins: ['@typescript-eslint'],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
     },
-  ],
+    rules: {
+      'no-underscore-dangle': 0,
+      'import/extensions': [
+        'error',
+        'ignorePackages',
+        {
+          js: 'never',
+          jsx: 'never',
+          ts: 'never',
+          tsx: 'never',
+        },
+      ],
+      'import/no-absolute-path': 0,
+      'import/no-unresolved': 0,
+      'import/no-extraneous-dependencies': 1,
+      'react/prop-types': 1,
+      'jsx-a11y/no-access-key': 0,
+      'react/jsx-props-no-spreading': 'off',
+      'max-classes-per-file': ['error', 2],
+      'no-use-before-define': 'off',
+      '@typescript-eslint/no-use-before-define': ['error'],
+      'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    },
+    globals: {
+      browser: 'writable',
+    },
+    overrides: [
+      {
+        files: ['*.ts', '*.tsx'],
+        extends: [
+          'eslint:recommended',
+          'plugin:@typescript-eslint/recommended',
+          'airbnb',
+        ],
+        parser: '@typescript-eslint/parser',
+        plugins: ['@typescript-eslint'],
+      },
+    ],
+  },
 };
