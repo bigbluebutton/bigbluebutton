16bda0d70883a91b8cd10458ee7518c457de9ba0
Anton Georgiev <antobinary@users.noreply.github.com>
2023-09-06 15:54:40 -0400

    docs: set 2.7 as the current docs version

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -35,13 +35,16 @@ const config = {
                 docs: {
                     routeBasePath: "/",
                     sidebarPath: require.resolve('./sidebars.js'),
-                    lastVersion: '2.6',
+                    lastVersion: '2.7',
                     includeCurrentVersion: false,
                     versions: {
                         '2.5': {
                             banner: 'none'
                         },
                         '2.6': {
+                            banner: 'none'
+                        },
+                        '2.7': {
                             banner: 'none'
                         },
                     }
