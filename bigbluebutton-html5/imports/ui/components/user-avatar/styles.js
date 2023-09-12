f92d3a1ca504337250c0c7ddb146b6433ced3424
Arthurk12 <arthur_bockmann@hotmail.com>
2023-09-12 15:39:34 -0300

    fix(user-list): center icons on safari
    Centers icons indicating presenter, audio state and whiteboard access in the
    user list.

--- bigbluebutton-html5/imports/ui/components/user-avatar/styles.js
+++ bigbluebutton-html5/imports/ui/components/user-avatar/styles.js
@@ -118,7 +118,7 @@ const Avatar = styled.div`
     width: 0;
     height: 0;
     padding-top: .5rem;
-    padding-right: 0;
+    padding-right: ${({ isSafari }) => isSafari ? '.6rem' : '0'};
     padding-left: 0;
     padding-bottom: 0;
     color: inherit;
@@ -158,7 +158,6 @@ const Avatar = styled.div`
   ${({ presenter }) => presenter && `
     &:before {
       content: "\\00a0\\e90b\\00a0";
-      padding: ${mdPaddingY} !important;
       opacity: 1;
       top: ${userIndicatorsOffset};
       left: ${userIndicatorsOffset};
@@ -166,6 +165,8 @@ const Avatar = styled.div`
       right: auto;
       border-radius: 5px;
       background-color: ${colorPrimary};
+      height: 1.2rem;
+      width: 1.2rem;
 
       [dir="rtl"] & {
         left: auto;
@@ -175,18 +176,9 @@ const Avatar = styled.div`
     }
   `}
 
-  ${({
-    presenter, isChrome, isFirefox, isEdge,
-  }) => presenter && (isChrome || isFirefox || isEdge) && `
-    &:before {
-      padding: ${indicatorPadding} !important;
-    }
-  `}
-
   ${({ whiteboardAccess }) => whiteboardAccess && `
     &:before {
       content: "\\00a0\\e925\\00a0";
-      padding: ${mdPaddingY} !important;
       border-radius: 50% !important;
       opacity: 1;
       top: ${userIndicatorsOffset};
@@ -195,6 +187,8 @@ const Avatar = styled.div`
       right: auto;
       border-radius: 5px;
       background-color: ${colorPrimary};
+      height: 1.2rem;
+      width: 1.2rem;
 
       [dir="rtl"] & {
         left: auto;
@@ -202,14 +196,6 @@ const Avatar = styled.div`
         letter-spacing: -.33rem;
         transform: scale(-1, 1);
       }
-    }
-  `}
-
-  ${({
-    whiteboardAccess, isChrome, isFirefox, isEdge,
-  }) => whiteboardAccess && (isChrome || isFirefox || isEdge) && `
-    &:before {
-      padding: ${indicatorPadding};
     }
   `}
 
