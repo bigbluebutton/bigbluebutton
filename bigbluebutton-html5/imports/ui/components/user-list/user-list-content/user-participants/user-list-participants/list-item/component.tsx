73664d78617c28aaa113a6a784e91a071bf915f7
Ramón Souza <contato@ramonsouza.com>
2023-09-22 16:22:46 -0300

    eslint

--- bigbluebutton-html5/imports/ui/components/user-list/user-list-content/user-participants/user-list-participants/list-item/component.tsx
+++ bigbluebutton-html5/imports/ui/components/user-list/user-list-content/user-participants/user-list-participants/list-item/component.tsx
@@ -115,7 +115,9 @@ const UserListItem: React.FC<UserListItemProps> = ({ user, lockSettings }) => {
 
   const iconUser = getIconUser();
 
-  const avatarContent = user.lastBreakoutRoom?.currentlyInRoom && userAvatarFiltered.length === 0 ? user.lastBreakoutRoom?.sequence : iconUser;
+  const avatarContent = user.lastBreakoutRoom?.currentlyInRoom && userAvatarFiltered.length === 0
+    ? user.lastBreakoutRoom?.sequence
+    : iconUser;
 
   return (
     <Styled.UserItemContents data-test={(user.userId === Auth.userID) ? 'userListItemCurrent' : 'userListItem'}>
