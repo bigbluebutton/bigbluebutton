39ee5cea7965a8fa77ab218bfe228016dbde2dfb
Tiago Jacobs <tiago.jacobs@gmail.com>
2023-09-28 12:05:04 -0300

    Remove unused import and improves comments

--- bbb-graphql-actions-adapter-server/src/imports/redis.ts
+++ bbb-graphql-actions-adapter-server/src/imports/redis.ts
@@ -1,19 +1,18 @@
 import * as redis from 'redis';
 import { REDIS_HOST, REDIS_PORT } from '../config';
-import { promisify } from 'util';
 
 export const createRedisClient = () => {
+  // Create Redis Client with the specified options.
   const redisClient = redis.createClient({
-    url: `redis://${REDIS_HOST}:${REDIS_PORT}`,
-    disableOfflineQueue: true,
-    name: 'bbb-graphql-actions-adapter-server',
+    url: `redis://${REDIS_HOST}:${REDIS_PORT}`, // Construct URL from host and port.
+    disableOfflineQueue: true, // Disable offline queueing of commands.
+    name: 'bbb-graphql-actions-adapter-server', // Assign a name to this client.
     socket: {
-      reconnectStrategy: (times) => {
-        return 500;
-      }
+      reconnectStrategy: (times) => 500 // Reconnect strategy with a fixed delay.
     }
   });
 
+  // Log errors that occur with the Redis client.
   redisClient.on('error',  async (err: Error) => {
     console.error('Redis Error:', err.message);
   });
