10087cac3c4a09984b17644afe92338027303afc
Ramón Souza <contato@ramonsouza.com>
2023-09-27 23:26:30 -0300

    fix warnings

--- bigbluebutton-html5/imports/ui/components/whiteboard/container.jsx
+++ bigbluebutton-html5/imports/ui/components/whiteboard/container.jsx
@@ -1,11 +1,13 @@
 import React, { useContext } from 'react';
 import { useQuery, useSubscription } from '@apollo/client';
 import {
+  ColorStyle, DashStyle, SizeStyle, TDShapeType,
+} from '@tldraw/tldraw';
+import {
   CURRENT_PRESENTATION_PAGE_SUBSCRIPTION,
   CURRENT_PAGE_ANNOTATIONS_QUERY,
   CURRENT_PAGE_ANNOTATIONS_STREAM,
 } from './queries';
-import { ColorStyle, DashStyle, SizeStyle, TDShapeType } from '@tldraw/tldraw';
 import {
   initDefaultPages,
   persistShape,
@@ -39,12 +41,10 @@ let lastUpdatedAt = null;
 
 const WhiteboardContainer = (props) => {
   const {
-    whiteboardId,
     intl,
     slidePosition,
     svgUri,
     podId,
-    darkTheme,
   } = props;
 
   const { data: presentationPageData } = useSubscription(CURRENT_PRESENTATION_PAGE_SUBSCRIPTION);
@@ -57,16 +57,16 @@ const WhiteboardContainer = (props) => {
     loading: annotationsLoading,
     data: annotationsData,
   } = useQuery(CURRENT_PAGE_ANNOTATIONS_QUERY);
-  let { pres_annotation_curr: history } = (annotationsData || []);
+  const { pres_annotation_curr: history } = (annotationsData || []);
 
   const lastHistoryTime = history?.[0]?.lastUpdatedAt || null;
 
   if (!lastUpdatedAt) {
-    if(lastHistoryTime){
+    if (lastHistoryTime) {
       if (new Date(lastUpdatedAt).getTime() < new Date(lastHistoryTime).getTime()) {
         lastUpdatedAt = lastHistoryTime;
       }
-    }else{
+    } else {
       const newLastUpdatedAt = new Date();
       lastUpdatedAt = newLastUpdatedAt.toISOString();
     }
@@ -76,7 +76,7 @@ const WhiteboardContainer = (props) => {
     CURRENT_PAGE_ANNOTATIONS_STREAM,
     {
       variables: { lastUpdatedAt },
-    }
+    },
   );
   const { pres_annotation_curr_stream: streamDataItem } = (streamData || []);
 
@@ -89,7 +89,9 @@ const WhiteboardContainer = (props) => {
   let shapes = {};
 
   if (!annotationsLoading && history) {
-    const pageAnnotations = history.concat(annotations).filter((annotation) => annotation.pageId === currentPresentationPage.pageId);
+    const pageAnnotations = history
+      .concat(annotations)
+      .filter((annotation) => annotation.pageId === currentPresentationPage.pageId);
 
     shapes = formatAnnotations(pageAnnotations, intl, curPageId);
   }
@@ -109,7 +111,7 @@ const WhiteboardContainer = (props) => {
   const width = layoutSelect((i) => i?.output?.presentation?.width);
   const height = layoutSelect((i) => i?.output?.presentation?.height);
   const sidebarNavigationWidth = layoutSelect(
-    (i) => i?.output?.sidebarNavigation?.width
+    (i) => i?.output?.sidebarNavigation?.width,
   );
   const { users } = usingUsersContext;
   const currentUser = users[Auth.meetingID][Auth.userID];
@@ -117,8 +119,7 @@ const WhiteboardContainer = (props) => {
   const isModerator = currentUser.role === ROLE_MODERATOR;
   const { maxStickyNoteLength, maxNumberOfAnnotations } = WHITEBOARD_CONFIG;
   const fontFamily = WHITEBOARD_CONFIG.styles.text.family;
-  const handleToggleFullScreen = (ref) =>
-    FullscreenService.toggleFullScreen(ref);
+  const handleToggleFullScreen = (ref) => FullscreenService.toggleFullScreen(ref);
   const layoutContextDispatch = layoutDispatch();
 
   shapes['slide-background-shape'] = {
@@ -146,13 +147,12 @@ const WhiteboardContainer = (props) => {
     const owner = shapes[id]?.userId;
     const isBackgroundShape = id?.includes('slide-background');
     const isPollsResult = shapes[id]?.name?.includes('poll-result');
-    const hasAccess =
-      (!isBackgroundShape && !isPollsResult) ||
-      (isPresenter &&
-        ((owner && owner === currentUser?.userId) ||
-          !owner ||
-          isPresenter ||
-          isModerator));
+    const hasAccess = (!isBackgroundShape && !isPollsResult)
+      || (isPresenter
+        && ((owner && owner === currentUser?.userId)
+          || !owner
+          || isPresenter
+          || isModerator));
 
     return hasAccess;
   };
@@ -164,51 +164,53 @@ const WhiteboardContainer = (props) => {
     }
   });
 
-  return <Whiteboard
-  {...{
-    isPresenter,
-    isModerator,
-    currentUser,
-    isRTL,
-    width,
-    height,
-    maxStickyNoteLength,
-    maxNumberOfAnnotations,
-    fontFamily,
-    hasShapeAccess,
-    handleToggleFullScreen,
-    sidebarNavigationWidth,
-    layoutContextDispatch,
-    initDefaultPages,
-    persistShape,
-    isMultiUserActive,
-    hasMultiUserAccess,
-    changeCurrentSlide,
-    shapes,
-    assets,
-    removeShapes,
-    zoomSlide: PresentationToolbarService.zoomSlide,
-    skipToSlide: PresentationToolbarService.skipToSlide,
-    nextSlide: PresentationToolbarService.nextSlide,
-    previousSlide: PresentationToolbarService.previousSlide,
-    numberOfSlides: PresentationToolbarService.getNumberOfSlides(
-      podId,
-      presentationId
-    ),
-    notifyNotAllowedChange,
-    notifyShapeNumberExceeded,
-    whiteboardToolbarAutoHide:
+  return (
+    <Whiteboard
+      {...{
+        isPresenter,
+        isModerator,
+        currentUser,
+        isRTL,
+        width,
+        height,
+        maxStickyNoteLength,
+        maxNumberOfAnnotations,
+        fontFamily,
+        hasShapeAccess,
+        handleToggleFullScreen,
+        sidebarNavigationWidth,
+        layoutContextDispatch,
+        initDefaultPages,
+        persistShape,
+        isMultiUserActive,
+        hasMultiUserAccess,
+        changeCurrentSlide,
+        shapes,
+        assets,
+        removeShapes,
+        zoomSlide: PresentationToolbarService.zoomSlide,
+        skipToSlide: PresentationToolbarService.skipToSlide,
+        nextSlide: PresentationToolbarService.nextSlide,
+        previousSlide: PresentationToolbarService.previousSlide,
+        numberOfSlides: PresentationToolbarService.getNumberOfSlides(
+          podId,
+          presentationId,
+        ),
+        notifyNotAllowedChange,
+        notifyShapeNumberExceeded,
+        whiteboardToolbarAutoHide:
       SettingsService?.application?.whiteboardToolbarAutoHide,
-    animations: SettingsService?.application?.animations,
-    toggleToolsAnimations,
-    isIphone,
-    currentPresentationPage,
-    numberOfPages: PRESENTATION_CONFIG.mirroredFromBBBCore.uploadPagesMax,
-    presentationId,
-  }}
-  {...props}
-  meetingId={Auth.meetingID}
-  />;
-}
+        animations: SettingsService?.application?.animations,
+        toggleToolsAnimations,
+        isIphone,
+        currentPresentationPage,
+        numberOfPages: PRESENTATION_CONFIG.mirroredFromBBBCore.uploadPagesMax,
+        presentationId,
+      }}
+      {...props}
+      meetingId={Auth.meetingID}
+    />
+  );
+};
 
 export default WhiteboardContainer;
