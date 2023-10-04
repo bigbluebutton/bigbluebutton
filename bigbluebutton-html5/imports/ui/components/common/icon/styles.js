264d05c485f4c08a5d81815e4262a37d80bff63c
GuiLeme <leme.guilherme.p@gmail.com>
2023-10-04 11:43:10 -0300

    [plugin-sdk-issue-33] - implemented menu badge

--- bigbluebutton-html5/imports/ui/components/common/icon/styles.js
+++ bigbluebutton-html5/imports/ui/components/common/icon/styles.js
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
 
 const Icon = styled.i`
+  ${({ color }) => color && `
+    color: ${color};
+  `}
   ${({ $rotate }) => $rotate && `
     transform: rotate(180deg);
     margin-top: 20%;
