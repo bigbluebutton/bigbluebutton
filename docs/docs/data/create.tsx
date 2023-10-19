65b4c5d5d7ba92360286b6752196613e9b2a2a39
Anton Georgiev <anto.georgiev@gmail.com>
2023-10-19 14:51:08 -0400

    docs: preUploadedPresentation and preUploadedPresentationOverrideDefault

--- docs/docs/data/create.tsx
+++ docs/docs/data/create.tsx
@@ -384,6 +384,19 @@ const createEndpointTableData = [
     "type": "Boolean",
     "default": "false",
     "description": (<>Controls whether media (audio, cameras and screen sharing) should be captured on their full duration if the meeting's recorded property is true (<code className="language-plaintext highlighter-rouge">recorded=true</code>). Default is false: only captures media while recording is running in the meeting. (added 2.6.9)</>)
+  },
+  {
+    "name": "preUploadedPresentation",
+    "required": false,
+    "type": "String",
+    "description": (<>If passed with a valid presentation file url, this presentation will override the default presentation. To only upload but not set as default, also pass <code className="language-plaintext highlighter-rouge">preUploadedPresentationOverrideDefault=false</code> (added 2.7.2)</>)
+  },
+  {
+    "name": "preUploadedPresentationOverrideDefault",
+    "required": false,
+    "type": "Boolean",
+    "default": true,
+    "description": (<>Controls whether <code className="language-plaintext highlighter-rouge">preUploadedPresentation</code> presentation is set as default. Default is true: the presentation passed in the same CREATE via <code className="language-plaintext highlighter-rouge">preUploadedPresentation</code> will be set as the default presentation in the meeting. (added 2.7.2)</>)
   }
 ]
 
