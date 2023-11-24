30a680e959b9d302de526c32acb428a3ca67927b
Lucas Fialho Zawacki <lfzawacki@gmail.com>
2023-11-24 16:19:01 -0300

    fix(transcription): Add missing menu files

---
+++ bigbluebutton-html5/imports/ui/components/settings/submenus/transcription/styles.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+import Styled from '/imports/ui/components/settings/submenus/styles';
+
+const Title = styled(Styled.Title)``;
+
+const SubTitle = styled(Styled.SubTitle)``;
+
+const Form = styled(Styled.Form)``;
+
+const Row = styled(Styled.Row)``;
+
+const Col = styled(Styled.Col)``;
+
+const FormElement = styled(Styled.FormElement)``;
+
+const FormElementRight = styled(Styled.FormElementRight)``;
+
+const Label = styled(Styled.Label)``;
+
+export default {
+  Title,
+  SubTitle,
+  Form,
+  Row,
+  Col,
+  FormElement,
+  FormElementRight,
+  Label,
+};
