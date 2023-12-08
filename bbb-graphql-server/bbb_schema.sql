354e0b7040eac0139ba0e75d27ed96297ec1d3e8
Gustavo Trott <gustavo@trott.com.br>
2023-12-08 15:20:16 -0300

    Add graphql meeting.componentsFlags

--- bbb-graphql-server/bbb_schema.sql
+++ bbb-graphql-server/bbb_schema.sql
@@ -227,6 +227,52 @@ create index "idx_meeting_group_meetingId" on "meeting_group"("meetingId");
 create view "v_meeting_group" as select * from meeting_group;
 
 
+create view "v_meeting_componentsFlags" as
+select "meeting"."meetingId",
+        exists (
+            select 1
+            from "breakoutRoom"
+            where "breakoutRoom"."parentMeetingId" = "meeting"."meetingId"
+            and "endedAt" is null
+        ) as "hasBreakoutRoom",
+        exists (
+            select 1
+            from "poll"
+            where "poll"."meetingId" = "meeting"."meetingId"
+            and "ended" is false
+            and "published" is false
+        ) as "hasPoll",
+        exists (
+            select 1
+            from "timer"
+            where "timer"."meetingId" = "meeting"."meetingId"
+            and "active" is true
+        ) as "hasTimer",
+        exists (
+            select 1
+            from "v_screenshare"
+            where "v_screenshare"."meetingId" = "meeting"."meetingId"
+        ) as "hasScreenshare",
+        exists (
+            select 1
+            from "v_externalVideo"
+            where "v_externalVideo"."meetingId" = "meeting"."meetingId"
+        ) as "hasExternalVideo",
+        (
+            select array_agg(distinct "speechLocale")
+            from "user"
+            where "user"."meetingId" = "meeting"."meetingId"
+            and NULLIF("speechLocale",'') is not null
+        ) as "audioTranscriptionCaption",
+        (
+            select array_agg(distinct "name")
+            from "sharedNotes"
+            where "sharedNotes"."meetingId" = "meeting"."meetingId"
+            and "model" = 'captions'
+        ) as "typedCaption"
+from "meeting";
+
+
 -- ========== User tables
 
 CREATE TABLE "user" (
