ddfce55213eaad2fd14dca4144f2f80543561e4f
Ramón Souza <contato@ramonsouza.com>
2024-01-23 09:33:59 -0300

    remove clearWhiteboard action

--- bigbluebutton-html5/imports/api/annotations/server/methods.js
+++ bigbluebutton-html5/imports/api/annotations/server/methods.js
@@ -1,11 +1,9 @@
 import { Meteor } from 'meteor/meteor';
-import clearWhiteboard from './methods/clearWhiteboard';
 import sendAnnotations from './methods/sendAnnotations';
 import sendBulkAnnotations from './methods/sendBulkAnnotations';
 import deleteAnnotations from './methods/deleteAnnotations';
 
 Meteor.methods({
-  clearWhiteboard,
   sendAnnotations,
   sendBulkAnnotations,
   deleteAnnotations,
