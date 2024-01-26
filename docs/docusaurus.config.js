f1d74a38e85ffce247450540db1fbb5dedafc55f
Anton Georgiev <antobinary@users.noreply.github.com>
2024-01-26 12:18:56 -0500

    docs: Force trailingSlash

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -13,6 +13,7 @@ const config = {
     onBrokenLinks: 'throw',
     onBrokenMarkdownLinks: 'warn',
     favicon: 'img/favicon.ico',
+    trailingSlash: true,
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
