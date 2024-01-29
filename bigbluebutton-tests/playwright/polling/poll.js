ef0c48d46eec168126f65ee885cf9f9217a3c505
Gabriel Porfirio <gabrielporfirio1994@gmail.com>
2024-01-29 09:55:11 -0300

    removing polling changes, sending in another pr

--- bigbluebutton-tests/playwright/polling/poll.js
+++ bigbluebutton-tests/playwright/polling/poll.js
@@ -139,8 +139,6 @@ class Polling extends MultiUsers {
     await this.modPage.type(e.pollQuestionArea, 'Test');
     await this.modPage.waitAndClick(e.addPollItem);
     await this.modPage.type(e.pollOptionItem, 'test1');
-    await this.modPage.waitAndClick(e.addPollItem);
-    await this.modPage.type(e.pollOptionItem2, 'test2');
     await this.modPage.waitAndClick(e.startPoll);
 
     await this.userPage.hasElement(e.pollingContainer);
