6cf3cd47fbb99408415163fea1189e311a765e30
André <andrezero@live.com>
2024-02-28 15:41:51 -0300

    Change of approach for the debounce

--- bigbluebutton-html5/imports/ui/components/layout/service.js
+++ bigbluebutton-html5/imports/ui/components/layout/service.js
@@ -1,5 +1,4 @@
 import { makeCall } from '/imports/ui/services/api';
-import { debounce } from '/imports/utils/debounce';
 
 const setPushLayout = (pushLayout) => {
   makeCall('setPushLayout', { pushLayout });
@@ -11,5 +10,5 @@ const setMeetingLayout = (options) => {
 
 export default {
   setPushLayout,
-  setMeetingLayout: debounce(setMeetingLayout, 1000),
+  setMeetingLayout,
 };
