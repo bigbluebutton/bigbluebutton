eb743d380334ed2f731d2d9bc260dfa91424a010
Tainan Felipe <tainanfelipe214@gmail.com>
2024-03-12 14:37:28 -0300

    Fix: lock setting not working properly

--- bigbluebutton-html5/imports/ui/components/lock-viewers/context/container.jsx
+++ bigbluebutton-html5/imports/ui/components/lock-viewers/context/container.jsx
@@ -11,7 +11,7 @@ const lockContextContainer = (component) => withTracker(() => {
   const lockSetting = new LockStruct();
   const Meeting = Meetings.findOne({ meetingId: Auth.meetingID },
     { fields: { lockSettingsProps: 1 } });
-  const User = Users.findOne({ userId: Auth.userID, meetingId: Auth.meetingID },
+  const User = Users.findOne({ userId: Auth.userID },
     { fields: { role: 1, locked: 1 } });
   const userIsLocked = User ? User.locked && User.role !== ROLE_MODERATOR : true;
   const lockSettings = Meeting.lockSettingsProps;
