c6ad69f292cabbc29dd94293960a332043d62b30
Anton Georgiev <anto.georgiev@gmail.com>
2024-03-15 12:07:07 -0400

    fix(sec): Bump logback-classic

--- akka-bbb-fsesl/project/Dependencies.scala
+++ akka-bbb-fsesl/project/Dependencies.scala
@@ -14,7 +14,7 @@ object Dependencies {
     // Libraries
     val akkaVersion = "2.6.17"
     val akkaHttpVersion = "10.2.7"
-    val logback = "1.2.10"
+    val logback = "1.2.13"
 
     // Apache Commons
     val lang = "3.12.0"
