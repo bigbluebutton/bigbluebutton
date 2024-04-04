7093f54ad5d1dae504d0bb50158161f3d77de355
KDSBrowne <kert.browne85@gmail.com>
2024-04-04 02:14:57 +0000

    increase delay to prevent camera race condition

--- bigbluebutton-html5/imports/ui/components/whiteboard/component.jsx
+++ bigbluebutton-html5/imports/ui/components/whiteboard/component.jsx
@@ -870,7 +870,7 @@ const Whiteboard = React.memo(function Whiteboard(props) {
                   : baseZoom * (currentZoom / HUNDRED_PERCENT);
 
               setCamera(zoomToApply, camera.x, camera.y);
-            }, 25);
+            }, 50);
           } else {
             // Viewer logic
             const effectiveZoom = calculateEffectiveZoom(
