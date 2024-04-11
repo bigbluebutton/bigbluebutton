d2da9172da34c95f848d049bea369e3240e3d956
KDSBrowne <kert.browne85@gmail.com>
2024-04-11 00:53:42 +0000

    add no operation tool

---
+++ bigbluebutton-html5/imports/ui/components/whiteboard/custom-tools/noop-tool/component.ts
@@ -0,0 +1,14 @@
+import { StateNode } from 'tldraw'
+
+export class NoopTool extends StateNode {
+	static override id = 'noop'
+	static override initial = 'idle'
+
+	override onEnter = () => {
+		this.editor.setCursor({ type: 'default', rotation: 0 })
+	}
+
+	override onExit = () => {
+		this.editor.setCursor({ type: 'default', rotation: 0 })
+	}
+}
