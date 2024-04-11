e86fae9132f53d68beba2f3749f8ebe01a80483e
Ramón Souza <contato@ramonsouza.com>
2024-04-11 09:53:58 -0300

    fix users count in end meeting modal

--- bigbluebutton-html5/imports/ui/components/end-meeting-confirmation/service.js
+++ bigbluebutton-html5/imports/ui/components/end-meeting-confirmation/service.js
@@ -1,4 +1,3 @@
-import Users from '/imports/api/users';
 import Auth from '/imports/ui/services/auth';
 import Meetings from '/imports/api/meetings';
 
@@ -10,16 +9,6 @@ const getMeetingTitle = () => {
   return meeting.name;
 };
 
-const getUsers = () => {
-  const numUsers = Users.find({
-    meetingId: Auth.meetingID,
-    userId: { $ne: Auth.userID },
-  }, { fields: { _id: 1 } }).count();
-
-  return numUsers;
-};
-
 export default {
-  getUsers,
   getMeetingTitle,
 };
