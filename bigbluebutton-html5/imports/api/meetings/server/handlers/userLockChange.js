8ccaf095d10d17f52d77ce74abc6bf0f39463ece
Tainan Felipe <tainanfelipe214@gmail.com>
2024-04-24 22:50:09 -0300

    Remove: meetings event listeners

--- bigbluebutton-html5/imports/api/meetings/server/handlers/userLockChange.js
+++
@@ -1,5 +0,0 @@
-import changeUserLock from '../modifiers/changeUserLock';
-
-export default async function handleLockSettingsInMeeting({ body }, meetingId) {
-  await changeUserLock(meetingId, body);
-}
