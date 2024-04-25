335c0b8ba1ff464b7ffaf4a9c6044aadb05d2fb8
Ramón Souza <contato@ramonsouza.com>
2024-04-25 10:51:20 -0300

    remove createBreakoutRoom makecall

--- bigbluebutton-html5/imports/ui/components/actions-bar/service.js
+++ bigbluebutton-html5/imports/ui/components/actions-bar/service.js
@@ -22,7 +22,7 @@ const isMe = (intId) => intId === Auth.userID;
 
 export default {
   isMe,
-  meetingName: () => Meetings.findOne({ meetingId: Auth.meetingID},
+  meetingName: () => Meetings.findOne({ meetingId: Auth.meetingID },
     { fields: { name: 1 } }).name,
   users: () => Users.find({
     meetingId: Auth.meetingID,
@@ -32,7 +32,6 @@ export default {
     { fields: { groups: 1 } }).groups,
   isBreakoutRecordable: () => Meetings.findOne({ meetingId: Auth.meetingID },
     { fields: { 'breakoutProps.record': 1 } }).breakoutProps.record,
-  createBreakoutRoom: (rooms, durationInMinutes, record = false, captureNotes = false, captureSlides = false, sendInviteToModerators = false) => makeCall('createBreakoutRoom', rooms, durationInMinutes, record, captureNotes, captureSlides, sendInviteToModerators),
   sendInvitation: (breakoutId, userId) => makeCall('requestJoinURL', { breakoutId, userId }),
   breakoutJoinedUsers: () => Breakouts.find({
     joinedUsers: { $exists: true },
