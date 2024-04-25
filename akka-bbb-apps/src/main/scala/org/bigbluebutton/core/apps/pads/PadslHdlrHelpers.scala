eb74c81c6f2c80701fd8ec674766c20e95ee4049
Gustavo Trott <gustavo@trott.com.br>
2024-04-25 13:41:32 -0300

    akka apps will send msg to create pad

--- akka-bbb-apps/src/main/scala/org/bigbluebutton/core/apps/pads/PadslHdlrHelpers.scala
+++ akka-bbb-apps/src/main/scala/org/bigbluebutton/core/apps/pads/PadslHdlrHelpers.scala
@@ -1,6 +1,6 @@
 package org.bigbluebutton.core.apps.pads
 
-import org.bigbluebutton.common2.msgs.{ BbbCommonEnvCoreMsg, BbbCoreEnvelope, BbbCoreHeaderWithMeetingId, PadCreateGroupCmdMsg, PadCreateGroupCmdMsgBody }
+import org.bigbluebutton.common2.msgs.{ BbbCommonEnvCoreMsg, BbbCoreEnvelope, BbbCoreHeaderWithMeetingId, PadCreateCmdMsg, PadCreateCmdMsgBody, PadCreateGroupCmdMsg, PadCreateGroupCmdMsgBody }
 import org.bigbluebutton.core.running.OutMsgRouter
 
 object PadslHdlrHelpers {
@@ -11,6 +11,17 @@ object PadslHdlrHelpers {
     val header = BbbCoreHeaderWithMeetingId(PadCreateGroupCmdMsg.NAME, meetingId)
     val body = PadCreateGroupCmdMsgBody(externalId, model)
     val event = PadCreateGroupCmdMsg(header, body)
+    val msgEvent = BbbCommonEnvCoreMsg(envelope, event)
+
+    outGW.send(msgEvent)
+  }
+
+  def broadcastPadCreateCmdMsg(outGW: OutMsgRouter, meetingId: String,groupId: String, name: String): Unit = {
+    val routing = collection.immutable.HashMap("sender" -> "bbb-apps-akka")
+    val envelope = BbbCoreEnvelope(PadCreateCmdMsg.NAME, routing)
+    val header = BbbCoreHeaderWithMeetingId(PadCreateCmdMsg.NAME, meetingId)
+    val body = PadCreateCmdMsgBody(groupId, name)
+    val event = PadCreateCmdMsg(header, body)
     val msgEvent = BbbCommonEnvCoreMsg(envelope, event)
 
     outGW.send(msgEvent)
