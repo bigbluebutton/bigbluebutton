59d84f47223dc500dbc41bd860275b414f63a957
João Victor <joaovictornunes973@gmail.com>
2024-05-01 17:05:17 -0300

    fix: wrong style file import

--- bigbluebutton-html5/imports/ui/components/video-provider/video-provider-graphql/many-users-notify/styles.ts
+++ bigbluebutton-html5/imports/ui/components/video-provider/video-provider-graphql/many-users-notify/styles.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 import { colorPrimary } from '/imports/ui/stylesheets/styled-components/palette';
-import Styled from '/imports/ui/components/breakout-room/styles';
+import Styled from '/imports/ui/components/breakout-room/breakout-room/styles';
 import Button from '/imports/ui/components/common/button/component';
 
 const Info = styled.p`
