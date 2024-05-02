7512d4c60e0259c57e75bc78624c7f91358449aa
KDSBrowne <kert.browne85@gmail.com>
2024-05-02 00:32:58 +0000

    lint

--- bigbluebutton-html5/imports/ui/components/whiteboard/custom-tools/noop-tool/component.ts
+++ bigbluebutton-html5/imports/ui/components/whiteboard/custom-tools/noop-tool/component.ts
@@ -1,14 +1,14 @@
-import { StateNode } from 'tldraw'
+import { StateNode } from '@tldraw/tldraw'
 
 export class NoopTool extends StateNode {
-	static override id = 'noop'
-	static override initial = 'idle'
+   static override id = 'noop';
+   static override initial = 'idle';
 
-	override onEnter = () => {
-		this.editor.setCursor({ type: 'default', rotation: 0 });
-	}
+   override onEnter = () => {
+      this.editor.setCursor({ type: 'default', rotation: 0 });
+   }
 
-	override onExit = () => {
-		this.editor.setCursor({ type: 'default', rotation: 0 });
-	}
+   override onExit = () => {
+      this.editor.setCursor({ type: 'default', rotation: 0 });
+   }
 }
