be7fad8cbf898c9b12419fb6570ccec07e38bab3
Anton Georgiev <anto.georgiev@gmail.com>
2024-05-07 09:53:35 -0400

    build: try to remove frontend and leave only bbb-html5

--- bigbluebutton-html5/deploy_to_usr_share.sh
+++ bigbluebutton-html5/deploy_to_usr_share.sh
@@ -16,7 +16,7 @@ fi
 
 sudo rm -rf "$UPPER_DESTINATION_DIR"
 sudo mkdir -p "$UPPER_DESTINATION_DIR"
-sudo chown -R meteor:meteor "$UPPER_DESTINATION_DIR"
+sudo chown -R root:root "$UPPER_DESTINATION_DIR"
 
 # the next 5 lines may be temporarily commented out if you are sure you are not tweaking the required node_modules after first use of the script. This will save a minute or two during the run of the script
 if [ -d "node_modules" ]; then
@@ -28,9 +28,8 @@ meteor npm ci --production
 sudo chmod 777 /usr/share/meteor
 METEOR_DISABLE_OPTIMISTIC_CACHING=1 meteor build $UPPER_DESTINATION_DIR --architecture os.linux.x86_64 --allow-superuser --directory
 
-sudo chown -R meteor:meteor "$UPPER_DESTINATION_DIR"/
+sudo chown -R root:root "$UPPER_DESTINATION_DIR"/
 echo 'stage3'
-
 
 cd "$DESTINATION_DIR"/programs/server/ || exit
 sudo chmod -R 777 .
@@ -44,43 +43,15 @@ sudo cp $LOCAL_PACKAGING_DIR/mongod_start_pre.sh "$DESTINATION_DIR"/mongod_start
 echo "writing $DESTINATION_DIR/mongo-ramdisk.conf"
 sudo cp $LOCAL_PACKAGING_DIR/mongo-ramdisk.conf "$DESTINATION_DIR"/mongo-ramdisk.conf
 
-echo "writing $DESTINATION_DIR/systemd_start_frontend.sh"
-sudo cp $LOCAL_PACKAGING_DIR/systemd_start_frontend.sh "$DESTINATION_DIR"/systemd_start_frontend.sh
-
-sudo chown -R meteor:meteor "$UPPER_DESTINATION_DIR"/
+sudo chown -R root:root "$UPPER_DESTINATION_DIR"/
 sudo chmod +x "$DESTINATION_DIR"/mongod_start_pre.sh
-sudo chmod +x "$DESTINATION_DIR"/systemd_start_frontend.sh
 
 sudo cp $LOCAL_PACKAGING_DIR/workers-start.sh "$DESTINATION_DIR"/workers-start.sh
 sudo chmod +x "$DESTINATION_DIR"/workers-start.sh
 
-
-
-echo "writing $SERVICE_FILES_DIR/bbb-html5-frontend.service"
-sudo cp $LOCAL_PACKAGING_DIR/bbb-html5-frontend.service "$SERVICE_FILES_DIR"/bbb-html5-frontend.service
+echo "writing $SERVICE_FILES_DIR/bbb-html5.service"
+sudo cp $LOCAL_PACKAGING_DIR/bbb-html5.service "$SERVICE_FILES_DIR"/bbb-html5.service
 
 sudo systemctl daemon-reload
 
-echo 'before stopping bbb-html5:'
-ps -ef | grep node-
-sudo ss -netlp | grep -i node
-echo 'before stopping bbb-html5:'
-echo '_____________'
-
-sudo systemctl stop bbb-html5
-
-sleep 5s
-echo 'after stopping bbb-html5:'
-ps -ef | grep node-
-sudo ss -netlp | grep -i node
-echo 'after stopping bbb-html5:'
-echo '_____________'
-
-echo 'starting bbb-html5'
-sudo systemctl start bbb-html5
-sleep 10s
-echo 'after:...'
-ps -ef | grep node-
-sudo ss -netlp | grep -i node
-echo 'after:'
-echo '_____________'
+sudo systemctl restart bbb-html5
