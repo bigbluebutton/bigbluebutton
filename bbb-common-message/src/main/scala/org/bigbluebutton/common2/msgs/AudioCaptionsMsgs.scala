4c7405449870beeaddf93b767bfef506b2eb5f3f
Gustavo Trott <gustavo@trott.com.br>
2024-05-24 17:04:05 -0300

    revert removal of necessary message

--- bbb-common-message/src/main/scala/org/bigbluebutton/common2/msgs/AudioCaptionsMsgs.scala
+++ bbb-common-message/src/main/scala/org/bigbluebutton/common2/msgs/AudioCaptionsMsgs.scala
@@ -28,3 +28,9 @@ case class TranscriptionProviderErrorEvtMsgBody(errorCode: String, errorMessage:
 object TranscriptUpdatedEvtMsg { val NAME = "TranscriptUpdatedEvtMsg" }
 case class TranscriptUpdatedEvtMsg(header: BbbClientMsgHeader, body: TranscriptUpdatedEvtMsgBody) extends BbbCoreMsg
 case class TranscriptUpdatedEvtMsgBody(transcriptId: String, transcript: String, locale: String, result: Boolean)
+
+// For recordings
+object EditCaptionHistoryEvtMsg { val NAME = "EditCaptionHistoryEvtMsg" }
+case class EditCaptionHistoryEvtMsg(header: BbbClientMsgHeader, body: EditCaptionHistoryEvtMsgBody) extends StandardMsg
+case class EditCaptionHistoryEvtMsgBody(startIndex: Integer, endIndex: Integer, name: String, locale: String, text: String)
+
