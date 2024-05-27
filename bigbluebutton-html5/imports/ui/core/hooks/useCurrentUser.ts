5a868d7a552484371ecea490434b3d66a6f3c99d
Guilherme Leme <leme.guilherme.p@gmail.com>
2024-05-27 11:26:10 -0300

    [fix-plugin-data-consumption-hooks] - fix currentUser and useMeeting

--- bigbluebutton-html5/imports/ui/core/hooks/useCurrentUser.ts
+++ bigbluebutton-html5/imports/ui/core/hooks/useCurrentUser.ts
@@ -1,3 +1,4 @@
+import { useMemo } from 'react';
 import { User } from '../../Types/user';
 import { useCreateUseSubscription } from './createUseSubscription';
 import CURRENT_USER_SUBSCRIPTION from '../graphql/queries/currentUserSubscription';
@@ -5,11 +6,11 @@ import CURRENT_USER_SUBSCRIPTION from '../graphql/queries/currentUserSubscriptio
 const useCurrentUser = (fn: (c: Partial<User>) => Partial<User> = (u) => u) => {
   const currenUserSubscription = useCreateUseSubscription<User>(CURRENT_USER_SUBSCRIPTION, {}, true);
   const response = currenUserSubscription(fn);
-  const returnObject = {
+  const returnObject = useMemo(() => ({
     ...response,
     data: response.data ? response.data[0] : null,
     rawData: response.data ?? null,
-  };
+  }), [response]);
   return returnObject;
 };
 
