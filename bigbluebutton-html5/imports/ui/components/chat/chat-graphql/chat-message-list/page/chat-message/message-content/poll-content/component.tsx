3bfdeb9cfd215cfcfeeadb68dad57f5517294691
KDSBrowne <kert.browne85@gmail.com>
2024-06-10 17:12:40 -0400

    feat(whiteboard): Reuse poll result diagram as a whiteboard shape (#20350)
    * create poll shape to render chat poll component

--- bigbluebutton-html5/imports/ui/components/chat/chat-graphql/chat-message-list/page/chat-message/message-content/poll-content/component.tsx
+++ bigbluebutton-html5/imports/ui/components/chat/chat-graphql/chat-message-list/page/chat-message/message-content/poll-content/component.tsx
@@ -8,6 +8,7 @@ import Styled from './styles';
 
 interface ChatPollContentProps {
   metadata: string;
+  height?: number;
 }
 
 interface Metadata {
@@ -75,6 +76,7 @@ function assertAsMetadata(metadata: unknown): asserts metadata is Metadata {
 
 const ChatPollContent: React.FC<ChatPollContentProps> = ({
   metadata: string,
+  height,
 }) => {
   const intl = useIntl();
 
@@ -92,13 +94,13 @@ const ChatPollContent: React.FC<ChatPollContentProps> = ({
     };
   });
 
-  const height = translatedAnswers.length * 50;
+  const useHeight = height || translatedAnswers.length * 50;
   return (
     <div data-test="chatPollMessageText">
       <Styled.PollText>
         {pollData.questionText}
       </Styled.PollText>
-      <ResponsiveContainer width="90%" height={height}>
+      <ResponsiveContainer width="90%" height={useHeight}>
         <BarChart
           data={translatedAnswers}
           layout="vertical"
@@ -110,6 +112,10 @@ const ChatPollContent: React.FC<ChatPollContentProps> = ({
       </ResponsiveContainer>
     </div>
   );
+};
+
+ChatPollContent.defaultProps = {
+  height: undefined,
 };
 
 export default ChatPollContent;
