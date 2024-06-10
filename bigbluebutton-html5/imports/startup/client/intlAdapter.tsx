e6a748a98484e91b8e26a752ef4f939b3006ff87
germanocaumo <germanocaumo@gmail.com>
2024-06-10 13:21:27 -0300

    fix(plugin): current locale not being sent when client reloads
    If the user change the language and then refreshes the page, the plugins were still receving the original language locale.

--- bigbluebutton-html5/imports/startup/client/intlAdapter.tsx
+++ bigbluebutton-html5/imports/startup/client/intlAdapter.tsx
@@ -88,6 +88,14 @@ const IntlAdapter: React.FC<IntlAdapterProps> = ({
   const runOnCurrentLocaleUpdate = () => {
     setUp();
     sendUiDataToPlugins();
+    window.removeEventListener(
+      `${UI_DATA_LISTENER_SUBSCRIBED}-${PluginSdk.IntlLocaleUiDataNames.CURRENT_LOCALE}`,
+      sendUiDataToPlugins,
+    );
+    window.addEventListener(
+      `${UI_DATA_LISTENER_SUBSCRIBED}-${PluginSdk.IntlLocaleUiDataNames.CURRENT_LOCALE}`,
+      sendUiDataToPlugins,
+    );
   };
 
   useEffect(runOnMountAndUnmount, []);
