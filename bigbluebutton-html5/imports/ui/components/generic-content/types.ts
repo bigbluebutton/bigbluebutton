18d3ac7b737b566dd1cbbb49714f0b8bca96be6f
Arthurk12 <arthur_bockmann@hotmail.com>
2024-06-14 15:41:54 -0300

    review: fix linter "trailing space" error

--- bigbluebutton-html5/imports/ui/components/generic-content/types.ts
+++ bigbluebutton-html5/imports/ui/components/generic-content/types.ts
@@ -12,7 +12,7 @@ export interface GenericContentMainAreaProps {
     renderFunctionComponents: GenericContentMainArea[];
 }
 
-export interface GenericContentSidekickContainerProps { 
+export interface GenericContentSidekickContainerProps {
     genericSidekickContentId: string;
 }
 
