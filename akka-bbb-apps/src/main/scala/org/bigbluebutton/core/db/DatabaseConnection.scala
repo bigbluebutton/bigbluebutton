7a96787799b7b721f3f664abfeaa896409ad2cb0
Gustavo Trott <gustavo@trott.com.br>
2024-06-27 17:38:05 -0300

    tweak processBatch() to avoid lose inserts

--- akka-bbb-apps/src/main/scala/org/bigbluebutton/core/db/DatabaseConnection.scala
+++ akka-bbb-apps/src/main/scala/org/bigbluebutton/core/db/DatabaseConnection.scala
@@ -2,21 +2,25 @@ package org.bigbluebutton.core.db
 
 import org.slf4j.LoggerFactory
 import slick.jdbc.PostgresProfile.api._
+
 import scala.concurrent.duration._
 import java.util.concurrent.ConcurrentLinkedQueue
-import scala.jdk.CollectionConverters._
 import scala.concurrent.ExecutionContext.Implicits.global
+import java.util.concurrent.atomic.AtomicBoolean
+import scala.util.control.Breaks.break
 
 object DatabaseConnection {
   val db = Database.forConfig("postgres")
   val logger = LoggerFactory.getLogger(this.getClass)
 
   val queue = new ConcurrentLinkedQueue[DBIO[_]]()
+  val isProcessing = new AtomicBoolean(false)
+  val batchSizeLimit = 500
 
   // Initialize the scheduler for batch processing
   val system = org.apache.pekko.actor.ActorSystem("DBActionBatchSystem")
   system.scheduler.scheduleWithFixedDelay(50.millis, 50.millis)(new Runnable {
-    def run(): Unit = processBatch()
+    def run(): Unit = tryProcessBatch()
   })
 
   def initialize(): Unit = {
@@ -30,22 +34,41 @@ object DatabaseConnection {
 
   def enqueue(action: DBIO[_]): Unit = {
     queue.add(action)
+    tryProcessBatch()
+  }
+
+  private def tryProcessBatch(): Unit = {
+    if (isProcessing.compareAndSet(false, true)) {
+      processBatch()
+    }
   }
 
   private def processBatch(): Unit = {
-    if (!queue.isEmpty) {
-      val batch = queue.iterator().asScala.toSeq
-      queue.clear() // Clear the queue
-      val combinedAction = DBIO.sequence(batch)
-      db.run(combinedAction).onComplete {
-        case scala.util.Success(_) => {
-          logger.debug(s"${batch.size} actions executed.")
-          if (!queue.isEmpty) {
-            processBatch()
-          }
-        }
-        case scala.util.Failure(e) => logger.error(s"Error executing batch actions: $e")
+    val batch = collection.mutable.ListBuffer[DBIO[_]]()
+    var action = queue.poll()
+    while (action != null) {
+      batch += action
+      if (batch.size >= batchSizeLimit) {
+        break()
       }
+
+      action = queue.poll()
+    }
+
+    if (batch.nonEmpty) {
+      val combinedAction = DBIO.sequence(batch.toList)
+      db.run(combinedAction).onComplete {
+        case scala.util.Success(_) =>
+          logger.debug(s"${batch.size} actions executed.")
+          isProcessing.set(false)
+          if (!queue.isEmpty) tryProcessBatch()
+        case scala.util.Failure(e) =>
+          logger.error(s"Error executing batch actions: $e")
+          isProcessing.set(false)
+          if (!queue.isEmpty) tryProcessBatch()
+      }
+    } else {
+      isProcessing.set(false)
     }
   }
 }
