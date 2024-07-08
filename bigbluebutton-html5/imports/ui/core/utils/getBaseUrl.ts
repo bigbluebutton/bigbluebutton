5f0995b80f8d344e9e4f3a335bd9fa106a64cc06
Tainan Felipe <tainanfelipe214@gmail.com>
2024-07-08 14:56:42 -0300

    Remove: cluster support in util

--- bigbluebutton-html5/imports/ui/core/utils/getBaseUrl.ts
+++ bigbluebutton-html5/imports/ui/core/utils/getBaseUrl.ts
@@ -1,15 +1,6 @@
-function getBaseUrl(cluster = false) {
+function getBaseUrl() {
   // @ts-ignore
   const webApi = window.meetingClientSettings.public.app.bbbWebBase;
-
-  if (cluster) {
-    const pathMatch = window.location.pathname.match('^(.*)/html5client/join$');
-    if (pathMatch == null) {
-      throw new Error('Failed to match BBB client URI');
-    }
-    const serverPathPrefix = pathMatch[1];
-    return `${serverPathPrefix}${webApi}`;
-  }
 
   return webApi;
 }
