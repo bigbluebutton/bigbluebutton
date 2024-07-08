df7184d7316aa8f3f640b8f5edf12e2760782bad
Ramón Souza <contato@ramonsouza.com>
2024-07-08 09:13:36 -0300

    Update bigbluebutton-html5/imports/ui/components/nav-bar/nav-bar-graphql/recording-indicator/component.tsx

--- bigbluebutton-html5/imports/ui/components/nav-bar/nav-bar-graphql/recording-indicator/component.tsx
+++ bigbluebutton-html5/imports/ui/components/nav-bar/nav-bar-graphql/recording-indicator/component.tsx
@@ -147,7 +147,7 @@ const RecordingIndicator: React.FC<RecordingIndicatorProps> = ({
 
   const recordingIndicatorIcon = useMemo(() => (
     <Styled.RecordingIndicatorIcon
-      titleMargin={!isPhone}
+      titleMargin={!isPhone || recording}
       data-test="mainWhiteboard"
     >
       <svg
