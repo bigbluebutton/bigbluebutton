f70170adb49445c03bc2792dba70722f78b625b5
João Victor <joaovictornunes973@gmail.com>
2024-07-10 10:15:46 -0300

    refactor(html5): recording start/stop modal

---
+++ bigbluebutton-html5/imports/ui/components/recording/container.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { useMutation, useReactiveVar } from '@apollo/client';
+import RecordingComponent from './component';
+import { SET_RECORDING_STATUS } from './mutations';
+import { GET_RECORDINGS, GetRecordingResponse } from './queries';
+import useDeduplicatedSubscription from '/imports/ui/core/hooks/useDeduplicatedSubscription';
+import ConnectionStatus from '/imports/ui/core/graphql/singletons/connectionStatus';
+
+interface RecordingContainerProps {
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  amIModerator: boolean;
+  onRequestClose: () => void;
+  priority: string;
+  isOpen: boolean;
+}
+
+const RecordingContainer: React.FC<RecordingContainerProps> = (props) => {
+  const {
+    amIModerator, isOpen, onRequestClose, priority, setIsOpen,
+  } = props;
+  const [setRecordingStatus] = useMutation(SET_RECORDING_STATUS);
+  const connected = useReactiveVar(ConnectionStatus.getConnectedStatusVar());
+  const {
+    data: recordingData,
+    loading: recordingLoading,
+  } = useDeduplicatedSubscription<GetRecordingResponse>(GET_RECORDINGS);
+
+  const recording = recordingData?.meeting_recording[0]?.isRecording ?? false;
+  const time = recordingData?.meeting_recording[0]?.previousRecordedTimeInSeconds ?? 0;
+
+  const toggleRecording = () => {
+    setRecordingStatus({
+      variables: {
+        recording: !recording,
+      },
+    });
+    setIsOpen(false);
+  };
+
+  return (
+    <RecordingComponent
+      {...{
+        amIModerator,
+        connected,
+        isOpen,
+        recordingLoading,
+        onRequestClose,
+        priority,
+        recordingTime: time,
+        recordingStatus: recording,
+        setIsOpen,
+        toggleRecording,
+      }}
+    />
+  );
+};
+
+export default RecordingContainer;
