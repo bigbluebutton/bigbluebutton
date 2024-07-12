cfb16b61b537ec5dc586e7ff3e30d2ed82c2dffe
Gustavo Trott <gustavo@trott.com.br>
2024-07-12 13:40:11 -0300

    Upgrade Hasura and refactor sh scripts

--- bbb-graphql-server/deploy.sh
+++ bbb-graphql-server/deploy.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
-if [ "$EUID" -ne 0 ]; then
-	echo "Please run this script as root ( or with sudo )" ;
-	exit 1;
-fi;
 
 cd "$(dirname "$0")"
 
 export LANGUAGE="en_US.UTF-8"
 export LC_ALL="en_US.UTF-8"
 
-akka_apps_status=$(systemctl is-active "bbb-apps-akka")
-hasura_status=$(systemctl is-active "bbb-graphql-server")
+akka_apps_status=$(sudo systemctl is-active "bbb-apps-akka")
+hasura_status=$(sudo systemctl is-active "bbb-graphql-server")
 
 if [ "$akka_apps_status" = "active" ]; then
   echo "Stopping Akka-apps"
@@ -22,12 +18,12 @@ if [ "$hasura_status" = "active" ]; then
 fi
 
 echo "Restarting database bbb_graphql"
-runuser -u postgres -- psql -q -c "drop database if exists bbb_graphql with (force)"
-runuser -u postgres -- psql -q -c "create database bbb_graphql WITH TEMPLATE template0 LC_COLLATE 'C.UTF-8'"
-runuser -u postgres -- psql -q -c "alter database bbb_graphql set timezone to 'UTC'"
+sudo runuser -u postgres -- psql -q -c "drop database if exists bbb_graphql with (force)"
+sudo runuser -u postgres -- psql -q -c "create database bbb_graphql WITH TEMPLATE template0 LC_COLLATE 'C.UTF-8'"
+sudo runuser -u postgres -- psql -q -c "alter database bbb_graphql set timezone to 'UTC'"
 
 echo "Creating tables in bbb_graphql"
-runuser -u postgres -- psql -U postgres -d bbb_graphql -q -f bbb_schema.sql --set ON_ERROR_STOP=on
+sudo runuser -u postgres -- psql -U postgres -d bbb_graphql -q -f bbb_schema.sql --set ON_ERROR_STOP=on
 
 echo "Creating frontend in bbb_graphql"
 DATABASE_FRONTEND_USER="bbb_frontend"
@@ -50,7 +46,7 @@ sudo systemctl start bbb-graphql-server
 
 #Check if Hasura is ready before applying metadata
 HASURA_PORT=8085
-while ! netstat -tuln | grep ":$HASURA_PORT " > /dev/null; do
+while ! sudo netstat -tuln | grep ":$HASURA_PORT " > /dev/null; do
     echo "Waiting for Hasura's port ($HASURA_PORT) to be ready..."
     sleep 1
 done
@@ -61,4 +57,4 @@ if [ "$akka_apps_status" = "active" ]; then
 fi
 
 echo "Applying new metadata to Hasura"
-timeout 15s hasura metadata apply --skip-update-check
+timeout 15s sudo hasura metadata apply --skip-update-check
