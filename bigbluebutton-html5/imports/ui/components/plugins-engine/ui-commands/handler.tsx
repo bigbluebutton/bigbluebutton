b885b5790f7b21721fe30f8b61baecafa714e94a
germanocaumo <germanocaumo@gmail.com>
2024-07-19 12:52:23 +0000

    feat(plugins): add open and close presentation area command support (#20635)

--- bigbluebutton-html5/imports/ui/components/plugins-engine/ui-commands/handler.tsx
+++ bigbluebutton-html5/imports/ui/components/plugins-engine/ui-commands/handler.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import PluginChatUiCommandsHandler from './chat/handler';
 import PluginSidekickOptionsContainerUiCommandsHandler from './sidekick-options-container/handler';
+import PluginPresentationAreaUiCommandsHandler from './presentation/handler';
 
 const PluginUiCommandsHandler = () => (
   <>
     <PluginChatUiCommandsHandler />
     <PluginSidekickOptionsContainerUiCommandsHandler />
+    <PluginPresentationAreaUiCommandsHandler />
   </>
 );
 
