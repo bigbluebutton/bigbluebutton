5a00a85f07b3a4b5f4a2af48aac7f5dd70f02731
João Victor Nunes <62393923+JoVictorNunes@users.noreply.github.com>
2024-07-26 13:45:48 -0300

    fix(html5): a couple of fixes for the recording button (#20758)

--- bigbluebutton-html5/imports/ui/components/recording/container.tsx
+++ bigbluebutton-html5/imports/ui/components/recording/container.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { useMutation, useReactiveVar } from '@apollo/client';
 import RecordingComponent from './component';
 import { SET_RECORDING_STATUS } from './mutations';
-import { GET_RECORDINGS, GetRecordingResponse } from './queries';
+import { GetRecordingResponse } from './queries';
 import useDeduplicatedSubscription from '/imports/ui/core/hooks/useDeduplicatedSubscription';
 import ConnectionStatus from '/imports/ui/core/graphql/singletons/connectionStatus';
+import { GET_MEETING_RECORDING_DATA } from '/imports/ui/components/nav-bar/nav-bar-graphql/recording-indicator/queries';
 
 interface RecordingContainerProps {
   setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
@@ -22,9 +23,7 @@ const RecordingContainer: React.FC<RecordingContainerProps> = (props) => {
   const connected = useReactiveVar(ConnectionStatus.getConnectedStatusVar());
   const {
     data: recordingData,
-    loading: recordingLoading,
-    error: recordingError,
-  } = useDeduplicatedSubscription<GetRecordingResponse>(GET_RECORDINGS);
+  } = useDeduplicatedSubscription<GetRecordingResponse>(GET_MEETING_RECORDING_DATA);
 
   const recording = recordingData?.meeting_recording[0]?.isRecording ?? false;
   const time = recordingData?.meeting_recording[0]?.previousRecordedTimeInSeconds ?? 0;
@@ -44,8 +43,6 @@ const RecordingContainer: React.FC<RecordingContainerProps> = (props) => {
         amIModerator,
         connected,
         isOpen,
-        recordingError,
-        recordingLoading,
         onRequestClose,
         priority,
         recordingTime: time,
--- bigbluebutton-html5/imports/ui/components/recording/queries.ts
+++ bigbluebutton-html5/imports/ui/components/recording/queries.ts
@@ -1,21 +1,6 @@
-import { gql } from '@apollo/client';
-
 export interface GetRecordingResponse {
   meeting_recording: {
     isRecording: boolean;
     previousRecordedTimeInSeconds: number;
   }[]
 }
-
-export const GET_RECORDINGS = gql`
-  subscription getRecordingData {
-    meeting_recording {
-      isRecording
-      previousRecordedTimeInSeconds
-    }
-  }
-`;
-
-export default {
-  GET_RECORDINGS,
-};
