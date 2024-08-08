1b173de9dea755b703180b73e0a7646b3229d851
Anton Georgiev <antobinary@users.noreply.github.com>
2024-08-08 16:29:18 -0400

    build: set wss url for media

--- build/packages-template/bbb-html5/after-install.sh
+++ build/packages-template/bbb-html5/after-install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -e
 
 BIGBLUEBUTTON_USER=bigbluebutton
+BBB_HTML5_SETTINGS_FILE=/var/bigbluebutton/html5-client/private/config/settings.yml
+BBB_RELEASE_FILE=/etc/bigbluebutton/bigbluebutton-release
 
-HOST=$(cat $SERVLET_DIR/WEB-INF/classes/bigbluebutton.properties | grep -v '#' | sed -n '/^bigbluebutton.web.serverURL/{s/.*\///;p}')
-# set ownership of activity directory
+HOST=$(grep -v '#' /etc/bigbluebutton/bbb-web.properties | sed -n '/^bigbluebutton.web.serverURL/{s/.*\///;p}')
+
 chown -R $BIGBLUEBUTTON_USER:$BIGBLUEBUTTON_USER /var/bigbluebutton/html5-client/
 
 if [ ! -L /etc/nginx/sites-enabled/bigbluebutton ]; then
@@ -11,22 +13,17 @@ if [ ! -L /etc/nginx/sites-enabled/bigbluebutton ]; then
   ln -s /etc/nginx/sites-available/bigbluebutton /etc/nginx/sites-enabled/bigbluebutton
 fi
 
-TARGET=/var/bigbluebutton/html5-client/private/config/settings.yml
 
-WSURL=$(cat $SERVLET_DIR/WEB-INF/classes/bigbluebutton.properties | grep -v '#' | sed -n '/^bigbluebutton.web.serverURL/{s/.*=//;p}' | sed 's/https/wss/g' | sed s'/http/ws/g')
+WSURL=$(grep -v '#' /etc/bigbluebutton/bbb-web.properties | sed -n '/^bigbluebutton.web.serverURL/{s/.*=//;p}' | sed 's/https/wss/g' | sed s'/http/ws/g')
 
-yq e -i ".public.kurento.wsUrl = \"$WSURL/bbb-webrtc-sfu\"" $TARGET
+yq e -i ".public.kurento.wsUrl = \"$WSURL/bbb-webrtc-sfu\"" $BBB_HTML5_SETTINGS_FILE
 
-yq e -i  ".public.pads.url = \"$PROTOCOL://$HOST/pad\"" $TARGET
+yq e -i  ".public.pads.url = \"$PROTOCOL://$HOST/pad\"" $BBB_HTML5_SETTINGS_FILE
 
 sed -i "s/proxy_pass .*/proxy_pass http:\/\/$IP:5066;/g" /usr/share/bigbluebutton/nginx/sip.nginx
 sed -i "s/server_name  .*/server_name  $IP;/g" /etc/nginx/sites-available/bigbluebutton
 
-chmod 600 $TARGET
-
 # set full BBB version in settings.yml so it can be displayed in the client
-BBB_RELEASE_FILE=/etc/bigbluebutton/bigbluebutton-release
-BBB_HTML5_SETTINGS_FILE=/var/bigbluebutton/html5-client/private/config/settings.yml
 if [ -f $BBB_RELEASE_FILE ] && [ -f $BBB_HTML5_SETTINGS_FILE ]; then
   BBB_FULL_VERSION=$(cat $BBB_RELEASE_FILE | sed -n '/^BIGBLUEBUTTON_RELEASE/{s/.*=//;p}' | tail -n 1)
   echo "setting public.app.bbbServerVersion: $BBB_FULL_VERSION in $BBB_HTML5_SETTINGS_FILE "
@@ -43,16 +40,10 @@ if [ -f /etc/systemd/system/mongod.service.d/override-mongo.conf ] \
   systemctl daemon-reload
 fi
 
-# Enable Listen Only support in FreeSWITCH
-if [ -f /opt/freeswitch/etc/freeswitch/sip_profiles/external.xml ]; then
-  sed -i 's/<!--<param name="enable-3pcc" value="true"\/>-->/<param name="enable-3pcc" value="proxy"\/>/g' /opt/freeswitch/etc/freeswitch/sip_profiles/external.xml
-fi
-
 chown root:root /usr/lib/systemd/system
 
-chmod go+r $TARGET
+chmod go+r $BBB_HTML5_SETTINGS_FILE
 #
 # Restart nginx to take advantage of the updates to nginx configuration
 #
 reloadService nginx
-
