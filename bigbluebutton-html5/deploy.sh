e6875a577e2f1f3129d0eb2bdf8603f2d655e47a
Gustavo Trott <gustavo@trott.com.br>
2024-08-09 21:58:56 -0300

    remove unnecessary sudo

--- bigbluebutton-html5/deploy.sh
+++ bigbluebutton-html5/deploy.sh
@@ -6,17 +6,16 @@ for var in "$@"
 do
     if [[ $var == --reset ]] ; then
     	echo "Performing a full reset..."
-      sudo rm -rf node_modules
+      rm -rf node_modules
     fi
 done
 
 if [ ! -d ./node_modules ] ; then
-  sudo npm ci --no-progress
+  npm ci --no-progress
 fi
 
-sudo npm run build
+npm run build
 
-#sudo mv -f dist/index.js dist/bbb-graphql-actions.js
 sudo cp -rf dist/* /var/bigbluebutton/html5-client/
 
 sudo ln -sf /usr/share/bigbluebutton/nginx/bbb-html5.nginx.static /usr/share/bigbluebutton/nginx/bbb-html5.nginx
