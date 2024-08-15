prlanzarin/bigbluebutton-1

fix(audio): change unmute/unhold flow to work around FS unmute stutter
FS has an intermittent issue where unmuting a HELD channel sometimes
takes significantly (seconds) longer than usual.
conference <XYZ> unmute <WVU> simply gets stuck with no FS_API response,
which delays the unmute action whenever transparent listen only is
active.

Apparently, unholding the channel PRIOR TO unmuting works around the
issue - at least it could not be reproduced with the scenario at hand.
The unmute API already triggered an unhold in FS internally, which is
the reason why this was not done beforehand. The aforementioned issue is
way worse than an extra "redudant" API call, though.

Always unhold audio channels manually _before_ unmuting.

akka-bbb-apps/src/main/scala/org/bigbluebutton/core2/message/handlers/MuteMeetingCmdMsgHdlr.scala
@@ -7,6 +7,7 @@ import org.bigbluebutton.core.models.{ VoiceUserState, VoiceUsers }
 import org.bigbluebutton.core.running.{ MeetingActor, OutMsgRouter }
 import org.bigbluebutton.core2.MeetingStatus2x
 import org.bigbluebutton.core2.message.senders.MsgBuilder
+import org.bigbluebutton.core.apps.voice.VoiceApp
 
 trait MuteMeetingCmdMsgHdlr extends RightsManagementTrait {
   this: MeetingActor =>
@@ -32,16 +33,7 @@ trait MuteMeetingCmdMsgHdlr extends RightsManagementTrait {
       }
 
       def muteUserInVoiceConf(vu: VoiceUserState, mute: Boolean): Unit = {
-        val routing = Routing.addMsgToClientRouting(MessageTypes.BROADCAST_TO_MEETING, props.meetingProp.intId, vu.intId)
-        val envelope = BbbCoreEnvelope(MuteUserInVoiceConfSysMsg.NAME, routing)
-        val header = BbbCoreHeaderWithMeetingId(MuteUserInVoiceConfSysMsg.NAME, props.meetingProp.intId)
-
-        val body = MuteUserInVoiceConfSysMsgBody(props.voiceProp.voiceConf, vu.voiceUserId, mute)
-        val event = MuteUserInVoiceConfSysMsg(header, body)
-        val msgEvent = BbbCommonEnvCoreMsg(envelope, event)
-
-        outGW.send(msgEvent)
-
+        VoiceApp.muteUserInVoiceConf(liveMeeting, outGW, vu.intId, mute)
       }
 
       if (msg.body.mute != MeetingStatus2x.isMeetingMuted(liveMeeting.status)) {

bigbluebutton/bigbluebutton

akka-bbb-apps/src/main/scala/org/bigbluebutton/core2/message/handlers/MuteMeetingCmdMsgHdlr.scala
@@ -7,6 +7,7 @@ import org.bigbluebutton.core.models.{ VoiceUserState, VoiceUsers }
 import org.bigbluebutton.core.running.{ MeetingActor, OutMsgRouter }
 import org.bigbluebutton.core2.MeetingStatus2x
 import org.bigbluebutton.core2.message.senders.MsgBuilder
+import org.bigbluebutton.core.apps.voice.VoiceApp
 
 trait MuteMeetingCmdMsgHdlr extends RightsManagementTrait {
   this: MeetingActor =>
@@ -29,19 +30,6 @@ trait MuteMeetingCmdMsgHdlr extends RightsManagementTrait {
         val event = MeetingMutedEvtMsg(header, body)
 
         BbbCommonEnvCoreMsg(envelope, event)
-      }
-
-      def muteUserInVoiceConf(vu: VoiceUserState, mute: Boolean): Unit = {
-        val routing = Routing.addMsgToClientRouting(MessageTypes.BROADCAST_TO_MEETING, props.meetingProp.intId, vu.intId)
-        val envelope = BbbCoreEnvelope(MuteUserInVoiceConfSysMsg.NAME, routing)
-        val header = BbbCoreHeaderWithMeetingId(MuteUserInVoiceConfSysMsg.NAME, props.meetingProp.intId)
-
-        val body = MuteUserInVoiceConfSysMsgBody(props.voiceProp.voiceConf, vu.voiceUserId, mute)
-        val event = MuteUserInVoiceConfSysMsg(header, body)
-        val msgEvent = BbbCommonEnvCoreMsg(envelope, event)
-
-        outGW.send(msgEvent)
-
       }
 
       if (msg.body.mute != MeetingStatus2x.isMeetingMuted(liveMeeting.status)) {
@@ -82,7 +70,7 @@ trait MuteMeetingCmdMsgHdlr extends RightsManagementTrait {
         if (muted) {
           VoiceUsers.findAll(liveMeeting.voiceUsers) foreach { vu =>
             if (!vu.listenOnly) {
-              muteUserInVoiceConf(vu, muted)
+              VoiceApp.muteUserInVoiceConf(liveMeeting, outGW, vu.intId, muted)
             }
           }
         }