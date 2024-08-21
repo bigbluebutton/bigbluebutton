f66f07d177b50374f4234f410eab321f0c370ab8
Gabriel Luiz Porfirio <gabrielporfirio1994@gmail.com>
2024-08-21 14:56:39 -0300

    test: Backport share camera as content test to 2.7 (#20964)
    * backport camera as content test
    
    * add test to ci

--- bigbluebutton-tests/playwright/presentation/presentation.spec.js
+++ bigbluebutton-tests/playwright/presentation/presentation.spec.js
@@ -12,6 +12,12 @@ test.describe.parallel('Presentation', () => {
     await presentation.skipSlide();
   });
 
+  test('Share Camera As Content @ci', async ({ browser, context, page }) => {
+    const presentation = new Presentation(browser, context);
+    await presentation.initPages(page);
+    await presentation.shareCameraAsContent();
+  });
+
   // https://docs.bigbluebutton.org/2.7/testing/release-testing/#minimizerestore-presentation-automated
   test('Hide/Restore presentation @ci', async ({ browser, context, page }) => {
     const presentation = new Presentation(browser, context);
