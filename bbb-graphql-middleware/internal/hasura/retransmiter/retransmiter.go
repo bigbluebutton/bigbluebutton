24603b24bd52991b75a4139f006f8252de540c79
Gustavo Trott <gustavo@trott.com.br>
2024-08-27 11:28:08 -0300

    avoid retransmitting subscriptions when user lost permission

--- bbb-graphql-middleware/internal/hasura/retransmiter/retransmiter.go
+++ bbb-graphql-middleware/internal/hasura/retransmiter/retransmiter.go
@@ -9,15 +9,30 @@ func RetransmitSubscriptionStartMessages(hc *common.HasuraConnection) {
 	log := log.WithField("_routine", "RetransmitSubscriptionStartMessages").WithField("browserConnectionId", hc.BrowserConn.Id).WithField("hasuraConnectionId", hc.Id)
 
 	hc.BrowserConn.ActiveSubscriptionsMutex.RLock()
-	for _, subscription := range hc.BrowserConn.ActiveSubscriptions {
+	defer hc.BrowserConn.ActiveSubscriptionsMutex.RUnlock()
 
+	userIsInMeetingNow := false
+	if hasuraRole, exists := hc.BrowserConn.BBBWebSessionVariables["x-hasura-role"]; exists {
+		userIsInMeetingNow = hasuraRole == "bbb_client"
+	}
+
+	for _, subscription := range hc.BrowserConn.ActiveSubscriptions {
 		//Not retransmitting Mutations
 		if subscription.Type == common.Mutation {
 			continue
 		}
 
-		if subscription.LastSeenOnHasuraConnection != hc.Id {
+		//When user left the meeting, Retransmit only Presence Manager subscriptions
+		if !userIsInMeetingNow &&
+			subscription.OperationName != "getUserInfo" &&
+			subscription.OperationName != "getUserCurrent" {
+			log.Infof("Skipping retransmit %s because the user is offline", subscription.OperationName)
+			log.Debugf("Skipping retransmit %s because the user is offline", subscription.OperationName)
+			continue
+		}
 
+		if subscription.LastSeenOnHasuraConnection != hc.Id {
+			log.Infof("retransmiting subscription start: %v", string(subscription.Message))
 			log.Tracef("retransmiting subscription start: %v", string(subscription.Message))
 
 			if subscription.Type == common.Streaming && subscription.StreamCursorCurrValue != nil {
@@ -27,5 +42,5 @@ func RetransmitSubscriptionStartMessages(hc *common.HasuraConnection) {
 			}
 		}
 	}
-	hc.BrowserConn.ActiveSubscriptionsMutex.RUnlock()
+
 }
