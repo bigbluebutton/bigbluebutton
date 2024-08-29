ef9cf7e746d4f07aa8453d2faec6b93baec37dda
Gustavo Trott <gustavo@trott.com.br>
2024-08-28 22:11:16 -0300

    remove /etc/default/bbb-graphql-middleware file

--- build/packages-template/bbb-graphql-middleware/before-install.sh
+++ build/packages-template/bbb-graphql-middleware/before-install.sh
@@ -3,9 +3,9 @@
 case "$1" in
   install|upgrade|1|2)
 
-    # the config is not longer env vars at /etc/bigbluebutton/bbb-graphql-middleware
-    if [ -f /etc/bigbluebutton/bbb-graphql-middleware ]; then
-      rm /etc/bigbluebutton/bbb-graphql-middleware
+    # the config is not longer env vars at /etc/default/bbb-graphql-middleware
+    if [ -f /etc/default/bbb-graphql-middleware ]; then
+      rm /etc/default/bbb-graphql-middleware
     fi
 
   ;;
