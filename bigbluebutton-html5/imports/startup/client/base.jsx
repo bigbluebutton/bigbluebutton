e5a9a376927b2103338b6d9accde02b28daf27e6
João Victor <joaovictornunes973@gmail.com>
2024-08-29 15:48:23 -0300

    Restore layoutContextDispatch prop

--- bigbluebutton-html5/imports/startup/client/base.jsx
+++ bigbluebutton-html5/imports/startup/client/base.jsx
@@ -5,6 +5,7 @@ import DebugWindow from '/imports/ui/components/debug-window/component';
 import { ACTIONS } from '/imports/ui/components/layout/enums';
 import useSettings from '/imports/ui/services/settings/hooks/useSettings';
 import { SETTINGS } from '/imports/ui/services/settings/enums';
+import { layoutDispatch } from '/imports/ui/components/layout/context';
 
 const HTML = document.getElementsByTagName('html')[0];
 
@@ -90,11 +91,13 @@ class Base extends Component {
 
 const BaseContainer = (props) => {
   const { animations } = useSettings(SETTINGS.APPLICATION);
+  const layoutContextDispatch = layoutDispatch();
 
   return (
     <Base
       {...{
         animations,
+        layoutContextDispatch,
         ...props,
       }}
     />
