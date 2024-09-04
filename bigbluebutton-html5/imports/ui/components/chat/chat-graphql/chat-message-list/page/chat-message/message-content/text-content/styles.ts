9be6f3961c487294cea9a715821d19d7c5ab5eb1
André Möller <andrezero@live.com>
2024-09-04 10:57:15 -0300

    later changes

--- bigbluebutton-html5/imports/ui/components/chat/chat-graphql/chat-message-list/page/chat-message/message-content/text-content/styles.ts
+++ bigbluebutton-html5/imports/ui/components/chat/chat-graphql/chat-message-list/page/chat-message/message-content/text-content/styles.ts
@@ -4,6 +4,7 @@ import {
   systemMessageBorderColor,
   colorText,
 } from '/imports/ui/stylesheets/styled-components/palette';
+import { borderRadius } from 'imports/ui/stylesheets/styled-components/general';
 import { fontSizeBase, btnFontWeight } from '/imports/ui/stylesheets/styled-components/typography';
 
 interface ChatMessageProps {
@@ -21,7 +22,7 @@ export const ChatMessage = styled.div<ChatMessageProps>`
   ${({ systemMsg }) => systemMsg && `
   background: ${systemMessageBackgroundColor};
   border: 1px solid ${systemMessageBorderColor};
-  border-radius: 0.2rem;
+  border-radius: ${borderRadius};
   font-weight: ${btnFontWeight};
   padding: ${fontSizeBase};
   text-color: #1f252b;
