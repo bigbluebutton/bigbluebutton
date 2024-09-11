8dafbf867b463582cd5963380e0e73facd43e7ea
Ramón Souza <contato@ramonsouza.com>
2024-09-11 17:50:17 -0300

    fix skip video preview

--- bigbluebutton-html5/imports/ui/components/video-preview/component.jsx
+++ bigbluebutton-html5/imports/ui/components/video-preview/component.jsx
@@ -705,6 +705,7 @@ class VideoPreview extends Component {
     }
 
     this.setState({ webcamDeviceId: actualDeviceId, });
+    return actualDeviceId;
   }
 
   getInitialCameraStream(deviceId) {
@@ -802,9 +803,14 @@ class VideoPreview extends Component {
 
     try {
       // The return of doGUM is an instance of BBBVideoStream (a thin wrapper over a MediaStream)
-      const bbbVideoStream = await PreviewService.doGUM(deviceId, profile);
+      let bbbVideoStream = await PreviewService.doGUM(deviceId, profile);
       this.currentVideoStream = bbbVideoStream;
-      this.updateDeviceId(deviceId);
+      const updatedDevice = this.updateDeviceId(deviceId);
+
+      if (updatedDevice !== deviceId) {
+        bbbVideoStream = await PreviewService.doGUM(updatedDevice, profile);
+        this.currentVideoStream = bbbVideoStream;
+      }
     } catch(error) {
       // When video preview is set to skip, we need some way to bubble errors
       // up to users; so re-throw the error
