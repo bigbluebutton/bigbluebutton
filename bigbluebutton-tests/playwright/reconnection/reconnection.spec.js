8dcf4fc94e3e05d8d0528e33904fc313729f397f
Anton Barboza <antonbsa.bck@gmail.com>
2024-10-02 10:14:38 -0300

    test(ci-note): Reconnection

--- bigbluebutton-tests/playwright/reconnection/reconnection.spec.js
+++ bigbluebutton-tests/playwright/reconnection/reconnection.spec.js
@@ -2,7 +2,10 @@ const { test } = require('../fixtures');
 const { Reconnection } = require('./reconnection');
 const { checkRootPermission } = require('../core/helpers');
 
-test.describe.parallel('Reconnection', () => {
+test.describe.parallel('Reconnection', { tag: '@flaky' }, () => {
+  // @ci Note: both tests are failing due to "server closed connection" bug
+  // see issue https://github.com/bigbluebutton/bigbluebutton/issues/21147
+
   test('Chat', async ({ browser, context, page }) => {
     await checkRootPermission(); // check sudo permission before starting test
     const reconnection = new Reconnection(browser, context);
