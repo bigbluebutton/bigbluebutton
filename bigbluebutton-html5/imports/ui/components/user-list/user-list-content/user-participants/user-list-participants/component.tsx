7782d222d9206e743430fd46eaf8b07e8020ff00
Tainan Felipe <tainanfelipe214@gmail.com>
2024-10-08 09:46:44 -0300

    Fix: Wrong page size calculation

--- bigbluebutton-html5/imports/ui/components/user-list/user-list-content/user-participants/user-list-participants/component.tsx
+++ bigbluebutton-html5/imports/ui/components/user-list/user-list-content/user-participants/user-list-participants/component.tsx
@@ -115,7 +115,7 @@ const UserListParticipants: React.FC<UserListParticipantsProps> = ({
           {
             Array.from({ length: amountOfPages }).map((_, i) => {
               const isLastItem = amountOfPages === (i + 1);
-              const restOfUsers = count % 15;
+              const restOfUsers = count % 50;
               const key = i;
               return i === 0
                 ? (
