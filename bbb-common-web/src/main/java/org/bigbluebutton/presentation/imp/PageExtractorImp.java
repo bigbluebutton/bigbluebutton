30e2d767463d8d2ce12730ff70df27b92e2dd5e3
Paul Trudel <paatrudel@gmail.com>
2024-10-16 03:16:48 +0000

    Add timeout config props to bbb

--- bbb-common-web/src/main/java/org/bigbluebutton/presentation/imp/PageExtractorImp.java
+++ bbb-common-web/src/main/java/org/bigbluebutton/presentation/imp/PageExtractorImp.java
@@ -29,11 +29,16 @@ public class PageExtractorImp implements PageExtractor {
   private static Logger log = LoggerFactory.getLogger(PageExtractorImp.class);
 
   private static final String SPACE = " ";
-  private static final long extractTimeout = 10000; // 10sec
+
+  private long extractTimeout = 10000; // 10sec
 
   public boolean extractPage(File presentationFile, File output, int page) {
     String COMMAND = "pdfseparate -f " + page + " -l " + page + SPACE
         + presentationFile.getAbsolutePath() + SPACE + output.getAbsolutePath();
     return new ExternalProcessExecutor().exec(COMMAND, extractTimeout);
+  }
+
+  public void setExtractTimeout(long extractTimeout) {
+    this.extractTimeout = extractTimeout;
   }
 }
