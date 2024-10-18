22c2a371b0e874e507f07b7c054cd74f52cf091c
Arthurk12 <arthur_bockmann@hotmail.com>
2024-10-18 15:15:49 -0300

    feat(bot): increase bot transparency
    This commit removes some actions and interactions that a bot user should
    not be part:
      - No actions available when clicking over bot user in the user-list
      - Remove bot from exported user-list
      - Remove bot from available users to be assigned to breakouts

--- bigbluebutton-html5/imports/ui/core/graphql/queries/users.ts
+++ bigbluebutton-html5/imports/ui/core/graphql/queries/users.ts
@@ -30,6 +30,7 @@ subscription UserListSubscription($offset: Int!, $limit: Int!) {
     locked
     authed
     mobile
+    bot
     guest
     clientType
     disconnected
@@ -79,7 +80,7 @@ export const GET_USER_IDS = gql`
 
 export const GET_USER_NAMES = gql`
   query Users {
-    user {
+    user(where: { bot: { _eq: false } } ) {
       name
     }
   }
