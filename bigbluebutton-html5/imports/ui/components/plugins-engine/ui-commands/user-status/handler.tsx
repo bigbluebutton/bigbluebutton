250a2e46c85747ca4c0912804a3e6814a9c692ef
Arthurk12 <arthur_bockmann@hotmail.com>
2024-10-21 11:43:33 -0300

    feat(plugins): adjust the set away command
    Ensures the current away status is used to mute the microphone correctly
    when the away status is set to true. Additionaly, adds a dependency array
    for the pertinent values to be updated in the event listener callback.

--- bigbluebutton-html5/imports/ui/components/plugins-engine/ui-commands/user-status/handler.tsx
+++ bigbluebutton-html5/imports/ui/components/plugins-engine/ui-commands/user-status/handler.tsx
@@ -4,6 +4,7 @@ import { muteAway } from '/imports/ui/components/audio/audio-graphql/audio-contr
 import useToggleVoice from '/imports/ui/components/audio/audio-graphql/hooks/useToggleVoice';
 import { SET_AWAY } from '/imports/ui/components/user-list/user-list-content/user-participants/user-list-participants/user-actions/mutations';
 import useWhoIsUnmuted from '/imports/ui/core/hooks/useWhoIsUnmuted';
+import useCurrentUser from '/imports/ui/core/hooks/useCurrentUser';
 import Auth from '/imports/ui/services/auth';
 import { SetAwayStatusCommandArguments } from 'bigbluebutton-html-plugin-sdk/dist/cjs/ui-commands/user-status/types';
 import { UserStatusEnum } from 'bigbluebutton-html-plugin-sdk/dist/cjs/ui-commands/user-status/enums';
@@ -11,21 +12,29 @@ import { UserStatusEnum } from 'bigbluebutton-html-plugin-sdk/dist/cjs/ui-comman
 const PluginUserStatusUiCommandsHandler = () => {
   const [setAway] = useMutation(SET_AWAY);
   const { data: unmutedUsers } = useWhoIsUnmuted();
+  const { data: currentUserData } = useCurrentUser((user) => ({
+    away: user.away,
+  }));
   const voiceToggle = useToggleVoice();
   const muted = !unmutedUsers[Auth.userID as string];
 
   const handleUserStatusAway = (event: CustomEvent<SetAwayStatusCommandArguments>) => {
-    const { away } = event.detail;
+    const { away: targetAwayStatus } = event.detail;
+    const { away: currentAwayStatus } = currentUserData as { away: boolean };
 
-    muteAway(muted, away, voiceToggle);
+    if (targetAwayStatus === currentAwayStatus) return;
+    muteAway(muted, currentAwayStatus, voiceToggle);
     setAway({
       variables: {
-        away,
+        away: targetAwayStatus,
       },
     });
   };
 
   useEffect(() => {
+    // add event listener only when the subscription for
+    // current user information is valid
+    if (!currentUserData) return () => {};
     window.addEventListener(
       UserStatusEnum.SET_AWAY_STATUS,
       handleUserStatusAway as EventListener,
@@ -37,7 +46,7 @@ const PluginUserStatusUiCommandsHandler = () => {
         handleUserStatusAway as EventListener,
       );
     };
-  }, []);
+  }, [unmutedUsers, currentUserData]);
   return null;
 };
 
