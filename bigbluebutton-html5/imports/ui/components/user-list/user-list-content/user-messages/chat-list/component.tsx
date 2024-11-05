240e1aec61dea53a03931096d32d53e69419d6c6
AtilaU19 <atilauebel2000@gmail.com>
2024-11-05 13:16:21 +0000

    fix(chat): removed private chats from navegation bar

--- bigbluebutton-html5/imports/ui/components/user-list/user-list-content/user-messages/chat-list/component.tsx
+++ bigbluebutton-html5/imports/ui/components/user-list/user-list-content/user-messages/chat-list/component.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import { defineMessages, useIntl } from 'react-intl';
 import Styled from './styles';
 import ChatListItem from './chat-list-item/component';
 import useChat from '/imports/ui/core/hooks/useChat';
 import { Chat } from '/imports/ui/Types/chat';
-import Service from '/imports/ui/components/user-list/service';
 import { GraphqlDataHookSubscriptionResponse } from '/imports/ui/Types/hook';
 import deviceInfo from '/imports/utils/deviceInfo';
-
-const { isMobile } = deviceInfo;
 
 const intlMessages = defineMessages({
   messagesTitle: {
@@ -19,48 +15,10 @@ const intlMessages = defineMessages({
 });
 
 interface ChatListProps {
-  chats: Chat[],
+  chat: Chat,
 }
 
-const getActiveChats = (chats: Chat[], chatNodeRef: React.Ref<HTMLButtonElement>) => chats.map((chat) => (
-  <CSSTransition
-    classNames="transition"
-    appear
-    enter
-    exit={false}
-    timeout={0}
-    component="div"
-    key={chat.chatId}
-    nodeRef={chatNodeRef}
-  >
-    <Styled.ListTransition>
-      <ChatListItem
-        chat={chat}
-        chatNodeRef={chatNodeRef}
-      />
-    </Styled.ListTransition>
-  </CSSTransition>
-));
-
-const ChatList: React.FC<ChatListProps> = ({ chats }) => {
-  const messageListRef = React.useRef<HTMLDivElement | null>(null);
-  const messageItemsRef = React.useRef<HTMLDivElement | null>(null);
-  const [selectedChat, setSelectedChat] = React.useState<HTMLElement>();
-  const { roving } = Service;
-  const chatNodeRef = React.useRef<HTMLButtonElement | null>(null);
-
-  React.useEffect(() => {
-    const firstChild = (selectedChat as HTMLElement)?.firstChild;
-    if (firstChild && firstChild instanceof HTMLElement) firstChild.focus();
-  }, [selectedChat]);
-
-  const rove = (event: React.KeyboardEvent) => {
-    const msgItemsRef = messageItemsRef.current;
-    const msgItemsRefChild = msgItemsRef?.firstChild;
-    roving(event, setSelectedChat, msgItemsRefChild, selectedChat);
-    event.stopPropagation();
-  };
-
+const ChatList: React.FC<ChatListProps> = ({ chat }) => {
   const intl = useIntl();
   return (
     <Styled.Messages>
@@ -69,32 +27,19 @@ const ChatList: React.FC<ChatListProps> = ({ chats }) => {
           {intl.formatMessage(intlMessages.messagesTitle)}
         </Styled.MessagesTitle>
       </Styled.Container>
-      {!isMobile ? (
-        <Styled.ScrollableList
-          role="tabpanel"
-          tabIndex={0}
-          ref={messageListRef}
-          onKeyDown={rove}
-        >
-          <Styled.List ref={messageItemsRef}>
-            <TransitionGroup>
-              {getActiveChats(chats, chatNodeRef) ?? null}
-            </TransitionGroup>
-          </Styled.List>
-        </Styled.ScrollableList>
-      )
-        : (getActiveChats(chats, chatNodeRef) ?? null) }
+      <ChatListItem chat={chat} />
     </Styled.Messages>
   );
 };
 
 const ChatListContainer: React.FC = () => {
   const { data: chats } = useChat((chat) => chat) as GraphqlDataHookSubscriptionResponse<Chat[]>;
-  if (chats) {
-    return (
-      <ChatList chats={chats} />
-    );
-  } return <></>;
+  const publicChat = chats?.find((chat) => chat.public);
+
+  if (publicChat) {
+    return <ChatList chat={publicChat} />;
+  }
+  return <></>;
 };
 
 export default ChatListContainer;
