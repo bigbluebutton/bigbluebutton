64806f7ae92175d558d01aaf58d16b358cf4cd39
AtilaU19 <atilauebel2000@gmail.com>
2024-11-25 19:52:02 +0000

    feat(sidebar-navigation): new settings button behavior

--- bigbluebutton-html5/imports/ui/components/sidebar-navigation/settings-list-item/component.tsx
+++ bigbluebutton-html5/imports/ui/components/sidebar-navigation/settings-list-item/component.tsx
@@ -1,65 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TooltipContainer from '/imports/ui/components/common/tooltip/container';
 import { defineMessages, useIntl } from 'react-intl';
 import Icon from '/imports/ui/components/common/icon/component';
-import { PANELS, ACTIONS } from '../../layout/enums';
-import { layoutDispatch, layoutSelectInput } from '/imports/ui/components/layout/context';
-import { Input } from '/imports/ui/components/layout/layoutTypes';
 import Styled from '../styles';
+import SettingsContainer from '/imports/ui/components/settings/container';
 
 const intlMessages = defineMessages({
   settingsLabel: {
     id: 'app.userList.settingsTitle',
-    description: 'Title for the settings panel',
+    description: 'Title for the settings modal',
   },
 });
 
 const SettingsListItem = () => {
-  const CURRENT_PANEL = PANELS.SETTINGS;
   const intl = useIntl();
-  const layoutContextDispatch = layoutDispatch();
-  const sidebarContent = layoutSelectInput((i: Input) => i.sidebarContent);
-  const { sidebarContentPanel } = sidebarContent;
+  const [isSettingsModalOpen, setIsSettingsModalOpen] = useState(false);
 
-  const toggleSettingsPanel = () => {
-    layoutContextDispatch({
-      type: ACTIONS.SET_SIDEBAR_CONTENT_IS_OPEN,
-      value: sidebarContentPanel !== CURRENT_PANEL,
-    });
-    layoutContextDispatch({
-      type: ACTIONS.SET_SIDEBAR_CONTENT_PANEL,
-      value: sidebarContentPanel === CURRENT_PANEL
-        ? PANELS.NONE
-        : CURRENT_PANEL,
-    });
+  const openSettingsModal = () => {
+    setIsSettingsModalOpen(true);
   };
 
   const label = intl.formatMessage(intlMessages.settingsLabel);
 
   return (
-    <TooltipContainer
-      title={label}
-      position="right"
-    >
-      <Styled.ListItem
-        id="settings-toggle-button"
-        aria-label={label}
-        aria-describedby="settings"
-        active={sidebarContentPanel === CURRENT_PANEL}
-        role="button"
-        tabIndex={0}
-        data-test="settingsSidebarButton"
-        onClick={toggleSettingsPanel}
-        // @ts-ignore
-        onKeyDown={(e) => {
-          if (e.key === 'Enter') {
-            toggleSettingsPanel();
-          }
-        }}
-      >
-        <Icon iconName="settings" />
-      </Styled.ListItem>
-    </TooltipContainer>
+    <>
+      <TooltipContainer title={label} position="right">
+        <Styled.ListItem
+          id="settings-toggle-button"
+          aria-label={label}
+          aria-describedby="settings"
+          role="button"
+          tabIndex={0}
+          data-test="settingsSidebarButton"
+          onClick={openSettingsModal}
+          // @ts-ignore
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              openSettingsModal();
+            }
+          }}
+        >
+          <Icon iconName="settings" />
+        </Styled.ListItem>
+      </TooltipContainer>
+
+      {isSettingsModalOpen && (
+        <SettingsContainer
+          isOpen={isSettingsModalOpen}
+          setIsOpen={setIsSettingsModalOpen}
+        />
+      )}
+    </>
   );
 };
 
--- bigbluebutton-html5/imports/ui/components/sidebar-navigation/types.ts
+++ bigbluebutton-html5/imports/ui/components/sidebar-navigation/types.ts
@@ -1,4 +1,4 @@
 export interface ListItemProps {
-  active: boolean;
+  active?: boolean;
   hasNotification?: boolean;
 }
