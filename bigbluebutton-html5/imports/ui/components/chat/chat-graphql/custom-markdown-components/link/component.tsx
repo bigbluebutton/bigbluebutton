b8d3db6ba0734532b850a491b16a5cc83a82857e
João Victor <joaovictornunes973@gmail.com>
2024-11-27 15:38:03 -0300

    fix(chat): Background color for current user mentions only

--- bigbluebutton-html5/imports/ui/components/chat/chat-graphql/custom-markdown-components/link/component.tsx
+++ bigbluebutton-html5/imports/ui/components/chat/chat-graphql/custom-markdown-components/link/component.tsx
@@ -1,14 +1,22 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import { ReactMarkdownProps } from 'react-markdown/lib/complex-types';
+import Auth from '/imports/ui/services/auth';
 import Styled from './styles';
 
 type Props = Omit<React.DetailedHTMLProps<React.AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement>, 'ref'> & ReactMarkdownProps;
 
+const MENTION_PROTOCOL = 'bbb://';
+
+const extractUserIdFromMention = (href: string) => {
+  return href.replace(MENTION_PROTOCOL, '');
+};
+
 const CustomMarkdownLink: React.FC<Props> = (props) => {
   const { children, node, ...rest } = props;
-  if (typeof node.properties?.href === 'string' && node.properties.href.includes('bbb://')) {
-    return <Styled.Mention>{children}</Styled.Mention>;
+  if (typeof node.properties?.href === 'string' && node.properties.href.startsWith(MENTION_PROTOCOL)) {
+    const userId = extractUserIdFromMention(node.properties.href);
+    return <Styled.Mention $isMe={userId === Auth.userID}>{children}</Styled.Mention>;
   }
   return <a {...rest}>{children}</a>;
 };
--- bigbluebutton-html5/imports/ui/components/chat/chat-graphql/custom-markdown-components/link/styles.ts
+++ bigbluebutton-html5/imports/ui/components/chat/chat-graphql/custom-markdown-components/link/styles.ts
@@ -1,17 +1,20 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { colorPrimary, colorBlueLighterChannel } from '/imports/ui/stylesheets/styled-components/palette';
 
-const Mention = styled.strong`
+const Mention = styled.strong<{ $isMe: boolean }>`
   color: ${colorPrimary};
-  background-color: rgb(${colorBlueLighterChannel} / 0.5);
   display: inline-block;
   padding: 4px;
   border-radius: 8px;
   line-height: 1;
 
-  &:hover {
-    background-color: rgb(${colorBlueLighterChannel} / 0.75);
-  }
+  ${({ $isMe }) => $isMe && css`
+    background-color: rgb(${colorBlueLighterChannel} / 0.5);
+
+    &:hover {
+      background-color: rgb(${colorBlueLighterChannel} / 0.75);
+    }
+  `}
 `;
 
 export default {
