a1d61dad38b0f45cb94fcfa25a00a88aee0b23a0
Paul Trudel <35616208+paultrudel@users.noreply.github.com>
2024-11-27 12:07:30 -0500

    fix(bbb-web): Add check to see if mod has left (#21766)

--- bbb-common-web/src/main/java/org/bigbluebutton/api/domain/Meeting.java
+++ bbb-common-web/src/main/java/org/bigbluebutton/api/domain/Meeting.java
@@ -780,7 +780,7 @@ public class Meeting {
         int sum = 0;
         for (Map.Entry<String, User> entry : users.entrySet()) {
             User u = entry.getValue();
-            if (u.isModerator())
+            if (!u.hasLeft() && u.isModerator())
                 sum++;
         }
         return sum;
