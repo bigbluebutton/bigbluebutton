a15fa1945cbafa7f1641eb49cb8d1e94837efb14
Gustavo Trott <gustavo@trott.com.br>
2024-12-04 16:29:41 -0300

    Ensure that GraphQL variables are refreshed before updating the database whenever there is a change to a user's role, locked status, or presenter status. This is important because once the client receives the updated status, it will behave as if the new status is active, requiring its GraphQL connection to reflect the change.
    To address this, all handlers in `akka-apps` involved in this process have been refactored to align and prevent potential race conditions.

--- akka-bbb-apps/src/main/scala/org/bigbluebutton/core/apps/users/AssignPresenterReqMsgHdlr.scala
+++ akka-bbb-apps/src/main/scala/org/bigbluebutton/core/apps/users/AssignPresenterReqMsgHdlr.scala
@@ -10,7 +10,7 @@ import org.bigbluebutton.core.running.{ LiveMeeting, OutMsgRouter }
 import org.bigbluebutton.core.apps.{ PermissionCheck, RightsManagementTrait }
 import org.bigbluebutton.core.domain.MeetingState2x
 import org.bigbluebutton.core.apps.screenshare.ScreenshareApp2x.requestBroadcastStop
-import org.bigbluebutton.core.db.ChatMessageDAO
+import org.bigbluebutton.core.db.{ ChatMessageDAO, UserStateDAO }
 import org.bigbluebutton.core.graphql.GraphqlMiddleware
 import org.bigbluebutton.core2.message.senders.Sender
 
@@ -35,6 +35,57 @@ trait AssignPresenterReqMsgHdlr extends RightsManagementTrait {
 object AssignPresenterActionHandler extends RightsManagementTrait {
 
   def handleAction(liveMeeting: LiveMeeting, outGW: OutMsgRouter, assignedBy: String, newPresenterId: String): Unit = {
+    if (permissionFailed(PermissionCheck.MOD_LEVEL, PermissionCheck.VIEWER_LEVEL, liveMeeting.users2x, assignedBy)) {
+      val meetingId = liveMeeting.props.meetingProp.intId
+      val reason = "No permission to change presenter in meeting."
+      PermissionCheck.ejectUserForFailedPermission(meetingId, assignedBy, reason, outGW, liveMeeting)
+    } else {
+      for {
+        oldPres <- Users2x.findPresenter(liveMeeting.users2x)
+      } yield {
+        if (oldPres.intId != newPresenterId) {
+          // Stop external video if it's running
+          ExternalVideoModel.stop(outGW, liveMeeting)
+          // Request a screen broadcast stop (goes to SFU, comes back through
+          // ScreenshareRtmpBroadcastStoppedVoiceConfEvtMsg)
+          requestBroadcastStop(outGW, liveMeeting)
+
+          for {
+            u <- RegisteredUsers.findWithUserId(oldPres.intId, liveMeeting.registeredUsers)
+            newUserState <- Users2x.makeNotPresenter(liveMeeting.users2x, oldPres.intId)
+          } yield {
+            // Force reconnection with graphql to refresh permissions
+            GraphqlMiddleware.requestGraphqlReconnection(u.sessionToken, "assigned_presenter")
+
+            //Update dabatase
+            UserStateDAO.update(newUserState)
+
+            //Send redis Evt message
+            broadcastOldPresenterChange(oldPres)
+          }
+        }
+      }
+
+      for {
+        u <- RegisteredUsers.findWithUserId(newPresenterId, liveMeeting.registeredUsers)
+        newPres <- Users2x.findWithIntId(liveMeeting.users2x, newPresenterId)
+        newUserState <- Users2x.makePresenter(liveMeeting.users2x, newPres.intId)
+      } yield {
+        // Force reconnection with graphql to refresh permissions
+        GraphqlMiddleware.requestGraphqlReconnection(u.sessionToken, "assigned_presenter")
+
+        //Update dabatase
+        UserStateDAO.update(newUserState)
+
+        //Send redis Evt message
+        broadcastNewPresenterChange(newPres)
+
+        //Chat message to announce new presenter
+        sendChatMessageAnnouncingNewPresenter(newPres)
+
+      }
+    }
+
     def broadcastOldPresenterChange(oldPres: UserState): Unit = {
       // unassign old presenter
       val routingUnassign = Routing.addMsgToClientRouting(
@@ -67,67 +118,26 @@ object AssignPresenterActionHandler extends RightsManagementTrait {
       outGW.send(msgEventAssign)
     }
 
-    if (permissionFailed(PermissionCheck.MOD_LEVEL, PermissionCheck.VIEWER_LEVEL, liveMeeting.users2x, assignedBy)) {
-      val meetingId = liveMeeting.props.meetingProp.intId
-      val reason = "No permission to change presenter in meeting."
-      PermissionCheck.ejectUserForFailedPermission(meetingId, assignedBy, reason, outGW, liveMeeting)
-    } else {
-      for {
-        oldPres <- Users2x.findPresenter(liveMeeting.users2x)
-      } yield {
-        if (oldPres.intId != newPresenterId) {
-          // Stop external video if it's running
-          ExternalVideoModel.stop(outGW, liveMeeting)
-          // Request a screen broadcast stop (goes to SFU, comes back through
-          // ScreenshareRtmpBroadcastStoppedVoiceConfEvtMsg)
-          requestBroadcastStop(outGW, liveMeeting)
+    def sendChatMessageAnnouncingNewPresenter(newPres: UserState): Unit = {
+      val announcePresenterChangeInChat = getConfigPropertyValueByPathAsBooleanOrElse(
+        liveMeeting.clientSettings,
+        "public.chat.announcePresenterChangeInChat",
+        alternativeValue = true
+      )
 
-          Users2x.makeNotPresenter(liveMeeting.users2x, oldPres.intId)
-          broadcastOldPresenterChange(oldPres)
-
-          // Force reconnection with graphql to refresh permissions
-          for {
-            u <- RegisteredUsers.findWithUserId(oldPres.intId, liveMeeting.registeredUsers)
-          } yield {
-            GraphqlMiddleware.requestGraphqlReconnection(u.sessionToken, "assigned_presenter")
-          }
-        }
-      }
-
-      for {
-        newPres <- Users2x.findWithIntId(liveMeeting.users2x, newPresenterId)
-      } yield {
-        Users2x.makePresenter(liveMeeting.users2x, newPres.intId)
-        broadcastNewPresenterChange(newPres)
-
-        // Force reconnection with graphql to refresh permissions
-        for {
-          u <- RegisteredUsers.findWithUserId(newPres.intId, liveMeeting.registeredUsers)
-        } yield {
-          GraphqlMiddleware.requestGraphqlReconnection(u.sessionToken, "assigned_presenter")
+      if (announcePresenterChangeInChat) {
+        val assignedByName = Users2x.findWithIntId(liveMeeting.users2x, assignedBy).get match {
+          case u: UserState => u.name
+          case _            => ""
         }
 
-        //Chat message to announce new presenter
-        val announcePresenterChangeInChat = getConfigPropertyValueByPathAsBooleanOrElse(
-          liveMeeting.clientSettings,
-          "public.chat.announcePresenterChangeInChat",
-          alternativeValue = true
+        //System message
+        val msgMeta = Map(
+          "assignedBy" -> assignedByName
         )
-
-        if (announcePresenterChangeInChat) {
-          val assignedByName = Users2x.findWithIntId(liveMeeting.users2x, assignedBy).get match {
-            case u: UserState => u.name
-            case _            => ""
-          }
-
-          //System message
-          val msgMeta = Map(
-            "assignedBy" -> assignedByName
-          )
-          ChatMessageDAO.insertSystemMsg(liveMeeting.props.meetingProp.intId, GroupChatApp.MAIN_PUBLIC_CHAT, "", GroupChatMessageType.USER_IS_PRESENTER_MSG, msgMeta, newPres.name)
-        }
-
+        ChatMessageDAO.insertSystemMsg(liveMeeting.props.meetingProp.intId, GroupChatApp.MAIN_PUBLIC_CHAT, "", GroupChatMessageType.USER_IS_PRESENTER_MSG, msgMeta, newPres.name)
       }
     }
+
   }
 }
--- akka-bbb-apps/src/main/scala/org/bigbluebutton/core/graphql/GraphqlMiddleware.scala
+++ akka-bbb-apps/src/main/scala/org/bigbluebutton/core/graphql/GraphqlMiddleware.scala
@@ -1,10 +1,16 @@
 package org.bigbluebutton.core.graphql
 
 import org.bigbluebutton.SystemConfiguration
+import org.slf4j.LoggerFactory
+
 import java.net.URI
 import java.net.http.{ HttpClient, HttpRequest, HttpResponse }
+import java.time.Duration
 
 object GraphqlMiddleware extends SystemConfiguration {
+
+  val logger = LoggerFactory.getLogger(this.getClass)
+
   def requestGraphqlReconnection(sessionTokens: Vector[String], reason: String): Unit = {
     for {
       sessionToken <- sessionTokens
@@ -13,13 +19,16 @@ object GraphqlMiddleware extends SystemConfiguration {
 
       val client = HttpClient.newHttpClient()
       val request = HttpRequest.newBuilder()
+        .timeout(Duration.ofSeconds(5))
         .uri(URI.create(url))
         .GET()
         .build()
 
       val response = client.send(request, HttpResponse.BodyHandlers.ofString())
+      logger.debug(s"Graphql reconnection requested for ${sessionToken}: (${url}).")
+
       if (response.statusCode() != 200) {
-        println(s"Error on requesting graphql reconnection for ${sessionToken}. Response Code: ${response.statusCode()}")
+        logger.error(s"Error on requesting graphql reconnection for ${sessionToken}. Response Code: ${response.statusCode()}")
       }
     }
   }
