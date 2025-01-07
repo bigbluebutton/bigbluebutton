3ac5355f0627a01d8cfa6feccde3d56e01a408be
prlanzarin <4529051+prlanzarin@users.noreply.github.com>
2025-01-07 14:51:53 +0000

    refactor(livekit): rename and reduce re-runs of selective sub hooks

--- bigbluebutton-html5/imports/ui/components/livekit/selective-subscription/component.tsx
+++ bigbluebutton-html5/imports/ui/components/livekit/selective-subscription/component.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useConnectionState } from '@livekit/components-react';
-import { ConnectionState, RoomEvent } from 'livekit-client';
+import { ConnectionState } from 'livekit-client';
 import { liveKitRoom } from '/imports/ui/services/livekit';
 import { useAudioSubscriptions } from './hooks';
 
@@ -12,27 +12,9 @@ const SelectiveSubscription: React.FC = () => {
     if (connectionState !== ConnectionState.Connected) return;
 
     handleSubscriptionChanges();
-
-    const onParticipantConnected = () => handleSubscriptionChanges();
-    const onParticipantDisconnected = () => handleSubscriptionChanges();
-    const onTrackPublished = () => handleSubscriptionChanges();
-    const onTrackUnpublished = () => handleSubscriptionChanges();
-
-    liveKitRoom.on(RoomEvent.ParticipantConnected, onParticipantConnected);
-    liveKitRoom.on(RoomEvent.ParticipantDisconnected, onParticipantDisconnected);
-    liveKitRoom.on(RoomEvent.TrackPublished, onTrackPublished);
-    liveKitRoom.on(RoomEvent.TrackUnpublished, onTrackUnpublished);
-
-    // eslint-disable-next-line consistent-return
-    return () => {
-      liveKitRoom.off(RoomEvent.ParticipantConnected, onParticipantConnected);
-      liveKitRoom.off(RoomEvent.ParticipantDisconnected, onParticipantDisconnected);
-      liveKitRoom.off(RoomEvent.TrackPublished, onTrackPublished);
-      liveKitRoom.off(RoomEvent.TrackUnpublished, onTrackUnpublished);
-    };
   }, [connectionState, handleSubscriptionChanges]);
 
   return null;
 };
 
-export default SelectiveSubscription;
+export default React.memo(SelectiveSubscription);
