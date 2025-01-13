48ff18c18d517b75792c5b5029f99daac0980a7b
Tainan Felipe <tainanfelipe214@gmail.com>
2025-01-13 15:39:11 -0300

    Restore preload next slide (#21616)
    * Restore preload next slide
    
    * Provide a new field pres_page_curr.nextPagesSvg that will be used to preload next slides in the Client.
    
    * Refactor: Implements preload next slide using the new graphql field
    
    * Fix: Remove unused query
    
    * Fix: boundary error on next slides urls
    
    ---------
    
    Co-authored-by: Gustavo Trott <gustavo@trott.com.br>

--- bbb-graphql-server/bbb_schema.sql
+++ bbb-graphql-server/bbb_schema.sql
@@ -1277,6 +1277,7 @@ CREATE TABLE "pres_presentation" (
 	"filenameConverted" varchar(500),
 	"isDefault" boolean,
 	"current" boolean,
+	"preloadNextPages" integer,
 	"removable" boolean,
 	"downloadable" boolean,
 	"downloadFileExtension" varchar(25),
@@ -1294,6 +1295,20 @@ CREATE TABLE "pres_presentation" (
 CREATE INDEX "idx_pres_presentation_meetingId" ON "pres_presentation"("meetingId");
 CREATE INDEX "idx_pres_presentation_meetingId_curr" ON "pres_presentation"("meetingId") where "current" is true;
 CREATE INDEX "idx_pres_presentation_meetingId_uploadUserId" ON "pres_presentation"("meetingId","uploadUserId");
+
+--Populate preloadNextPages, which will be used to provide the SVG of next slides at pres_page_curr
+CREATE OR REPLACE FUNCTION "update_preloadNextPages"() RETURNS TRIGGER AS $$
+BEGIN
+    SELECT coalesce(("clientSettingsJson"->'public'->'app'->'preloadNextSlides')::int,0) INTO NEW."preloadNextPages"
+    from "meeting_clientSettings" mcs
+    where mcs."meetingId" = NEW."meetingId";
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER "trigger_update_preloadNextPages" BEFORE INSERT ON "pres_presentation"
+FOR EACH ROW EXECUTE FUNCTION "update_preloadNextPages"();
+
 
 CREATE TABLE "pres_page" (
 	"pageId" varchar(100) PRIMARY KEY,
@@ -1406,7 +1421,14 @@ SELECT pres_presentation."meetingId",
     (pres_page."height" * LEAST(pres_page."maxImageWidth" / pres_page."width", pres_page."maxImageHeight" / pres_page."height")) AS "scaledHeight",
     (pres_page."width" * pres_page."widthRatio" / 100 * LEAST(pres_page."maxImageWidth" / pres_page."width", pres_page."maxImageHeight" / pres_page."height")) AS "scaledViewBoxWidth",
     (pres_page."height" * pres_page."heightRatio" / 100 * LEAST(pres_page."maxImageWidth" / pres_page."width", pres_page."maxImageHeight" / pres_page."height")) AS "scaledViewBoxHeight",
-    pres_page."infiniteWhiteboard"
+    pres_page."infiniteWhiteboard",
+    (
+        select array_agg("nextPages"."urlsJson"->>'svg')
+        from pres_page "nextPages"
+        where "nextPages"."presentationId" = pres_page."presentationId"
+        and "nextPages".num > pres_page."num"
+        and "nextPages".num <= pres_page."num" + pres_presentation."preloadNextPages"
+    ) "nextPagesSvg"
 FROM pres_presentation
 JOIN pres_page ON pres_presentation."presentationId" = pres_page."presentationId" AND pres_page."current" IS TRUE
 and pres_presentation."current" IS TRUE;
