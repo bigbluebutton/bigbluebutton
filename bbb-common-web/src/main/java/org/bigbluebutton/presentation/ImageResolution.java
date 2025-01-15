c5979f58f0f7e371e606254a2ff46df3e4c46fb0
Gustavo Trott <gustavo@trott.com.br>
2025-01-15 17:32:17 -0300

    Use `identify` (ImageMagick) to get image resolution instead of ImageIO.read
    It is necessary because ImageIO.read is throwing "java.lang.OutOfMemoryError: Java heap space" when trying to read a big image

---
+++ bbb-common-web/src/main/java/org/bigbluebutton/presentation/ImageResolution.java
@@ -0,0 +1,22 @@
+package org.bigbluebutton.presentation;
+
+public class ImageResolution {
+
+	private final int width;
+	private final int height;
+
+	public ImageResolution(int width, int height) {
+		this.width = width;
+		this.height = height;
+	}
+	
+	public int getWidth() {
+		return width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+	
+	 
+}
