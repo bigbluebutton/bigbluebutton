74b5fa6dde2fa588b2e26b1880a7d447389a6c1a
Anton B <antonbsa.bck@gmail.com>
2025-01-17 17:30:46 -0300

    test: add optional chaning on settings object inner data and remove autoJoinAudioModal index wrongly added

--- bigbluebutton-tests/playwright/core/settings.js
+++ bigbluebutton-tests/playwright/core/settings.js
@@ -9,45 +9,43 @@ async function generateSettingsData(page) {
     });
 
     settings = {
-      reactionsButton: settingsData.app.reactionsButton.enabled,
-      sharedNotesEnabled: settingsData.notes.enabled,
-      directLeaveButton: settingsData.app.defaultSettings.application.directLeaveButton,
+      reactionsButton: settingsData.app?.reactionsButton?.enabled,
+      sharedNotesEnabled: settingsData.notes?.enabled,
+      directLeaveButton: settingsData.app?.defaultSettings?.application?.directLeaveButton,
       // Audio
-      autoJoinAudioModal: settingsData.app.autoJoin,
-      listenOnlyMode: settingsData.app.listenOnlyMode,
-      forceListenOnly: settingsData.app.forceListenOnly,
-      skipEchoTest: settingsData.app.skipCheck,
-      skipEchoTestOnJoin: settingsData.app.skipCheckOnJoin,
-      skipEchoTestIfPreviousDevice: settingsData.app.skipEchoTestIfPreviousDevice,
-      speechRecognitionEnabled: settingsData.app.audioCaptions.enabled,
+      autoJoinAudioModal: settingsData.app?.autoJoin,
+      listenOnlyMode: settingsData.app?.listenOnlyMode,
+      forceListenOnly: settingsData.app?.forceListenOnly,
+      skipEchoTest: settingsData.app?.skipCheck,
+      skipEchoTestOnJoin: settingsData.app?.skipCheckOnJoin,
+      skipEchoTestIfPreviousDevice: settingsData.app?.skipEchoTestIfPreviousDevice,
+      speechRecognitionEnabled: settingsData.app?.audioCaptions?.enabled,
       // Chat
-      chatEnabled: settingsData.chat.enabled,
-      publicChatOptionsEnabled: settingsData.chat.enableSaveAndCopyPublicChat,
-      maxMessageLength: settingsData.chat.max_message_length,
-      emojiPickerEnabled: settingsData.chat.emojiPicker.enable,
-      autoConvertEmojiEnabled: settingsData.chat.autoConvertEmoji,
+      chatEnabled: settingsData.chat?.enabled,
+      publicChatOptionsEnabled: settingsData.chat?.enableSaveAndCopyPublicChat,
+      maxMessageLength: settingsData.chat?.max_message_length,
+      emojiPickerEnabled: settingsData.chat?.emojiPicker?.enable,
+      autoConvertEmojiEnabled: settingsData.chat?.autoConvertEmoji,
       // Polling
-      pollEnabled: settingsData.poll.enabled,
-      pollChatMessage: settingsData.poll.chatMessage,
+      pollEnabled: settingsData.poll?.enabled,
+      pollChatMessage: settingsData.poll?.chatMessage,
       // Presentation
-      originalPresentationDownloadable: settingsData.presentation.allowDownloadOriginal,
-      presentationWithAnnotationsDownloadable: settingsData.presentation.allowDownloadWithAnnotations,
-      externalVideoPlayer: settingsData.externalVideoPlayer.enabled,
-      presentationHidden: settingsData.layout.hidePresentation,
+      originalPresentationDownloadable: settingsData.presentation?.allowDownloadOriginal,
+      presentationWithAnnotationsDownloadable: settingsData.presentation?.allowDownloadWithAnnotations,
+      externalVideoPlayer: settingsData.externalVideoPlayer?.enabled,
+      presentationHidden: settingsData.layout?.hidePresentation,
       // Screensharing
-      screensharingEnabled: settingsData.kurento.enableScreensharing,
+      screensharingEnabled: settingsData.kurento?.enableScreensharing,
       // Timeouts
-      listenOnlyCallTimeout: parseInt(settingsData.media.listenOnlyCallTimeout),
-      videoPreviewTimeout: parseInt(settingsData.kurento.gUMTimeout),
+      listenOnlyCallTimeout: parseInt(settingsData.media?.listenOnlyCallTimeout),
+      videoPreviewTimeout: parseInt(settingsData.kurento?.gUMTimeout),
       // Webcam
-      webcamSharingEnabled: settingsData.kurento.enableVideo,
-      skipVideoPreview: settingsData.kurento.skipVideoPreview,
-      skipVideoPreviewOnFirstJoin: settingsData.kurento.skipVideoPreviewOnFirstJoin,
-      skipVideoPreviewIfPreviousDevice: settingsData.kurento.skipVideoPreviewIfPreviousDevice,
-      // User
-      userStatusEnabled: settingsData.userStatus.enabled,
+      webcamSharingEnabled: settingsData.kurento?.enableVideo,
+      skipVideoPreview: settingsData.kurento?.skipVideoPreview,
+      skipVideoPreviewOnFirstJoin: settingsData.kurento?.skipVideoPreviewOnFirstJoin,
+      skipVideoPreviewIfPreviousDevice: settingsData.kurento?.skipVideoPreviewIfPreviousDevice,
       // Emoji
-      emojiRain: settingsData.app.emojiRain.enabled,
+      emojiRain: settingsData.app?.emojiRain?.enabled,
     }
 
     return settings;
