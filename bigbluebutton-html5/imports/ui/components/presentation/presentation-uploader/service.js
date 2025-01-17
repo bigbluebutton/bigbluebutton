a4dd3099d33f0c1d3343fab8bfbd70d3a1344d30
Guilherme Leme <leme.guilherme.p@gmail.com>
2025-01-17 18:03:36 -0300

    [enhance-toast-stability] - fix export shared-notes to whiteboard

--- bigbluebutton-html5/imports/ui/components/presentation/presentation-uploader/service.js
+++ bigbluebutton-html5/imports/ui/components/presentation/presentation-uploader/service.js
@@ -2,7 +2,6 @@ import Auth from '/imports/ui/services/auth';
 import logger from '/imports/startup/client/logger';
 import { partition } from '/imports/utils/array-utils';
 import update from 'immutability-helper';
-import { v4 as uuid } from 'uuid';
 import { uniqueId } from '/imports/utils/string-utils';
 import { notify } from '/imports/ui/services/notification';
 import apolloContextHolder from '/imports/ui/core/graphql/apolloContextHolder/apolloContextHolder';
