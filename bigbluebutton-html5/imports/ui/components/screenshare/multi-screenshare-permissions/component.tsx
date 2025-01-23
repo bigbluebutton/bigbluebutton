162481cf781cb4a406202d380492d03c66c9cb2b
Tainan Felipe <tainanfelipe214@gmail.com>
2025-01-23 15:57:57 -0300

    WIP: modal

--- bigbluebutton-html5/imports/ui/components/screenshare/multi-screenshare-permissions/component.tsx
+++ bigbluebutton-html5/imports/ui/components/screenshare/multi-screenshare-permissions/component.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Styled from './styles';
 import { useIntl } from 'react-intl';
+import { useSubscription } from '@apollo/client';
+import { meetingMultiScreensharePermissions } from './queries';
 
-interface MultiScreensharePermissionsModalProps {
+interface MultiScreensharePermissionsModalContainerProps {
   isOpen: boolean;
   setIsOpen: (isOpen: boolean) => void;
   onRequestClose: () => void;
   priority: string;
+}
 
+interface MultiScreensharePermissionsModalProps extends MultiScreensharePermissionsModalContainerProps {
+  screenShareBroadcastAllowedFor: string;
+  viewerScreenShareViewAllowedFor: string;
 }
 
 const MultiScreensharePermissionsModal: React.FunctionComponent<MultiScreensharePermissionsModalProps> = ({
@@ -15,8 +21,28 @@ isOpen,
 setIsOpen,
 onRequestClose,
 priority,
+screenShareBroadcastAllowedFor,
+viewerScreenShareViewAllowedFor,
 }) => {
   const intl = useIntl();
+
+  const screenSharePermissions = useRef([
+    {
+      key: 'PRESENTER',
+      text: 'Presenter',
+    },
+    {
+      key: 'MODERATORS',
+      text: 'Moderators',
+    },
+    {
+      key: 'VIEWERS',
+      text: 'Viewers',
+    },
+  ]);
+
+  const viewerPermissions = useRef([]);
+
   return (
     <Styled.MultiScreenSharePermissionsModal
       isOpen={isOpen}
@@ -29,7 +55,7 @@ priority,
         data-test="multi-screenshare-permissions-modal"
       >
         <Styled.Description>
-
+          Multi screenshare Permissions
         </Styled.Description>
         <Styled.Content>
         </Styled.Content>
@@ -38,4 +64,37 @@ priority,
   );
 };
 
-export default MultiScreensharePermissionsModal;
+const MultiScreensharePermissionsModalContainer: React.FunctionComponent<MultiScreensharePermissionsModalContainerProps> = ({
+  isOpen,
+  setIsOpen,
+  onRequestClose,
+  priority,
+}) => {
+  const {
+    data: permissionsData,
+    loading: permissionsLoading,
+    error: permissionsError,
+  } = useSubscription(meetingMultiScreensharePermissions);
+
+  if (permissionsError) {
+    console.error(permissionsError);
+    return null;
+  }
+
+  if (permissionsLoading) return null;
+  if (!permissionsData) return null;
+
+  const { screenShareBroadcastAllowedFor, viewerScreenShareViewAllowedFor } = permissionsData.meeting[0];
+  return (
+    <MultiScreensharePermissionsModal
+      isOpen={isOpen}
+      setIsOpen={setIsOpen}
+      onRequestClose={onRequestClose}
+      priority={priority}
+      screenShareBroadcastAllowedFor={screenShareBroadcastAllowedFor}
+      viewerScreenShareViewAllowedFor={viewerScreenShareViewAllowedFor}
+    />
+  );
+};
+
+export default MultiScreensharePermissionsModalContainer;
