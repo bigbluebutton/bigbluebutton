d802db2a69e62e46639814375f4a6f0044ce122d
Guilherme Leme <leme.guilherme.p@gmail.com>
2025-01-28 09:52:02 -0300

    [self-view-disabled] - changes in review

--- bigbluebutton-html5/imports/ui/components/plugins-engine/ui-commands/camera/handler.tsx
+++ bigbluebutton-html5/imports/ui/components/plugins-engine/ui-commands/camera/handler.tsx
@@ -1,39 +1,39 @@
-import { useEffect } from 'react';
+import { useCallback, useEffect } from 'react';
 import { CameraEnum } from 'bigbluebutton-html-plugin-sdk/dist/cjs/ui-commands/camera/enums';
 import { SetSelfViewDisableAllDevicesCommandArguments, SetSelfViewDisableCommandArguments } from 'bigbluebutton-html-plugin-sdk/dist/cjs/ui-commands/camera/types';
 import Session from '/imports/ui/services/storage/in-memory';
 import useCurrentUser from '/imports/ui/core/hooks/useCurrentUser';
+import { useStorageKey } from '/imports/ui/services/storage/hooks';
+
+const DISABLED_CAMS_SESSION_KEY = 'disabledCams';
 
 const PluginCameraUiCommandsHandler = () => {
   const currentUserData = useCurrentUser((user) => ({
     userId: user.userId,
     cameras: user.cameras,
   }));
-  const disabledCams = (Session.getItem('disabledCams') || []) as string[];
-  const handleChangeSelfView = (isSelfViewDisabled: boolean,
+  const disabledCams: string[] = (useStorageKey(DISABLED_CAMS_SESSION_KEY, 'session') || []) as string[];
+
+  const handleChangeSelfView = useCallback((isSelfViewDisabled: boolean,
     cameraId: string) => {
     if (isSelfViewDisabled) {
       Session.setItem('disabledCams', [...disabledCams, cameraId]);
     } else {
       Session.setItem('disabledCams', disabledCams.filter((cId: string) => cId !== cameraId));
     }
-  };
-  const handleSetSelfViewDisableAllDevices = (event: CustomEvent<SetSelfViewDisableAllDevicesCommandArguments>) => {
-    const { isSelfViewDisabledAllDevices } = event.detail;
-    const cameras = currentUserData.data?.cameras;
-    if (cameras && cameras.length > 0) {
-      cameras.forEach((camera) => {
-        handleChangeSelfView(isSelfViewDisabledAllDevices, camera.streamId);
-      });
-    }
-  };
-
-  const handleSetSelfViewDisable = (event: CustomEvent<SetSelfViewDisableCommandArguments>) => {
-    const { isSelfViewDisabled, streamId } = event.detail;
-    handleChangeSelfView(isSelfViewDisabled, streamId);
-  };
+  }, [disabledCams]);
 
   useEffect(() => {
+    const handleSetSelfViewDisableAllDevices = (event: CustomEvent<SetSelfViewDisableAllDevicesCommandArguments>) => {
+      const { isSelfViewDisabledAllDevices } = event.detail;
+      const cameras = currentUserData.data?.cameras;
+      if (cameras && cameras.length > 0) {
+        cameras.forEach((camera) => {
+          handleChangeSelfView(isSelfViewDisabledAllDevices, camera.streamId);
+        });
+      }
+    };
+
     window.addEventListener(
       CameraEnum.SET_SELF_VIEW_DISABLED_ALL_DEVICES,
       handleSetSelfViewDisableAllDevices as EventListener,
@@ -48,6 +48,11 @@ const PluginCameraUiCommandsHandler = () => {
   }, [currentUserData, disabledCams]);
 
   useEffect(() => {
+    const handleSetSelfViewDisable = (event: CustomEvent<SetSelfViewDisableCommandArguments>) => {
+      const { isSelfViewDisabled, streamId } = event.detail;
+      handleChangeSelfView(isSelfViewDisabled, streamId);
+    };
+
     window.addEventListener(
       CameraEnum.SET_SELF_VIEW_DISABLED,
       handleSetSelfViewDisable as EventListener,
