8442c978cbf879e582d1fb0d42325ef91370a8fb
Ramón Souza <contato@ramonsouza.com>
2025-02-14 09:11:57 -0300

    fix create breakout disabled after moving user with free join enabled

--- bigbluebutton-html5/imports/ui/components/breakout-room/create-breakout-room/component.tsx
+++ bigbluebutton-html5/imports/ui/components/breakout-room/create-breakout-room/component.tsx
@@ -373,7 +373,6 @@ const CreateBreakoutRoom: React.FC<CreateBreakoutRoomProps> = ({
         id: 'freeJoinCheckbox',
         onChange: checkboxCallbackFactory((e: boolean) => {
           setFreeJoin(e);
-          setLeastOneUserIsValid(true);
         }),
         label: intl.formatMessage(intlMessages.freeJoinLabel),
       },
@@ -535,7 +534,7 @@ const CreateBreakoutRoom: React.FC<CreateBreakoutRoomProps> = ({
             ? intl.formatMessage(intlMessages.updateConfirm)
             : intl.formatMessage(intlMessages.confirmButton),
           callback: isUpdate ? userUpdate : createRoom,
-          disabled: !leastOneUserIsValid || !numberOfRoomsIsValid || !durationIsValid,
+          disabled: (!leastOneUserIsValid && !freeJoin) || !numberOfRoomsIsValid || !durationIsValid,
         }
       }
       dismiss={{
