d1efbef444887ea74ee8ba409be9ddf68fc4ab6d
João Victor <joaovictornunes973@gmail.com>
2025-02-17 15:39:57 -0300

    test: Remove unecessary timeout parameters

--- bigbluebutton-tests/playwright/polling/poll.js
+++ bigbluebutton-tests/playwright/polling/poll.js
@@ -39,7 +39,7 @@ class Polling extends MultiUsers {
 
     // The slide needs to be uploaded and converted, so wait a bit longer for this step
     await this.modPage.waitAndClick(e.quickPoll, ELEMENT_WAIT_LONGER_TIME);
-    await this.modPage.waitAndClick(e.startPoll, ELEMENT_WAIT_TIME);
+    await this.modPage.waitAndClick(e.startPoll);
     await this.modPage.hasElement(e.pollMenuButton, 'should display the poll menu button');
 
     await this.userPage.hasElement(e.pollingContainer, 'should display the polling container for the attendee to answer it');
@@ -189,7 +189,7 @@ class Polling extends MultiUsers {
 
     // Type Response
     await this.modPage.waitAndClick(e.quickPoll, ELEMENT_WAIT_LONGER_TIME);
-    await this.modPage.waitAndClick(e.startPoll, ELEMENT_WAIT_TIME);
+    await this.modPage.waitAndClick(e.startPoll);
     await this.userPage.hasElement(e.responsePollQuestion, 'should display the poll question after quick poll starts');
     await this.userPage.type(e.pollAnswerOptionInput, 'test');
     await this.userPage.waitAndClick(e.pollSubmitAnswer);
@@ -203,7 +203,7 @@ class Polling extends MultiUsers {
     await sleep(500); // avoid error when the tooltip is in front of the button due to layout shift
     await skipSlide(this.modPage);
     await this.modPage.waitAndClick(e.quickPoll);
-    await this.modPage.waitAndClick(e.startPoll, ELEMENT_WAIT_TIME);
+    await this.modPage.waitAndClick(e.startPoll);
     await this.userPage.waitAndClick(e.firstPollAnswerDescOption);
     await this.userPage.waitAndClick(e.secondPollAnswerDescOption);
     await this.userPage.waitAndClick(e.submitAnswersMultiple);
@@ -217,7 +217,7 @@ class Polling extends MultiUsers {
     await sleep(500); // avoid error when the tooltip is in front of the button due to layout shift
     await skipSlide(this.modPage);
     await this.modPage.waitAndClick(e.quickPoll);
-    await this.modPage.waitAndClick(e.startPoll, ELEMENT_WAIT_TIME);
+    await this.modPage.waitAndClick(e.startPoll);
     await this.userPage.waitAndClick(e.pollAnswerOptionE);
     await this.modPage.hasText(e.userVoteLiveResult, 'E) 22222', 'should display the vote result after the poll is answered');
 
@@ -240,7 +240,7 @@ class Polling extends MultiUsers {
     await sleep(500); // avoid error when the tooltip is in front of the button due to layout shift
     await skipSlide(this.modPage);
     await this.modPage.waitAndClick(e.quickPoll);
-    await this.modPage.waitAndClick(e.startPoll, ELEMENT_WAIT_TIME);
+    await this.modPage.waitAndClick(e.startPoll);
     await this.userPage.waitAndClick(e.pollAnswerOptionBtn);
     await this.modPage.hasText(e.userVoteLiveResult, 'True', 'should display the vote result after the attendee submit the answer');
     await this.modPage.waitAndClick(e.publishPollingLabel);
