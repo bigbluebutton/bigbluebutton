553866f765126df78e2f10c4d64ef451162ecd5f
Guilherme Pereira Leme <69865537+GuiLeme@users.noreply.github.com>
2025-02-19 12:03:44 -0300

    feat(plugin): block notification displaying (#21846)
    * [sendDisplayNotification] - Added possibility for the plugin developer block the notifications, so none are displayed to the user
    
    * [sendDisplayNotification] - changed the name of the ui-command
    
    * [sendDisplayNotification] - update sdk

--- bigbluebutton-html5/imports/ui/components/plugins-engine/ui-commands/handler.tsx
+++ bigbluebutton-html5/imports/ui/components/plugins-engine/ui-commands/handler.tsx
@@ -4,7 +4,7 @@ import PluginSidekickOptionsContainerUiCommandsHandler from './sidekick-options-
 import PluginPresentationAreaUiCommandsHandler from './presentation/handler';
 import PluginUserStatusUiCommandsHandler from './user-status/handler';
 import PluginConferenceUiCommandsHandler from './conference/handler';
-import PluginNotificationUiCommandsHandler from './notification/handler';
+import { PluginNotificationUiCommandsHandler } from './notification/handler';
 import { PluginLayoutUiCommandsHandler } from './layout/handler';
 import PluginNavBarUiCommandsHandler from './nav-bar/handler';
 import PluginActionsBarUiCommandsHandler from './actions-bar/handler';
