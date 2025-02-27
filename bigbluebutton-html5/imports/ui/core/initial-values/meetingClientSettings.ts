cac3597459b5508a364056f54ac2f174e8c41c3f
Paulo Lanzarin <4529051+prlanzarin@users.noreply.github.com>
2025-02-27 11:18:04 -0300

    fix(livekit): blurry screen sharing due to borked presets (#22246)
    LiveKit's screen sharing presets aren't configurable. The default values
    are also not ideal right now, which causes the received stream to be in
    a lower quality than it should in specific scenarios due to adaptive
    streaming.
    
    Make screen sharing's presets configurable, as well as adjust the
    default encoding preset to something that makes for nicer layers
    (1080p, 15fps).

--- bigbluebutton-html5/imports/ui/core/initial-values/meetingClientSettings.ts
+++ bigbluebutton-html5/imports/ui/core/initial-values/meetingClientSettings.ts
@@ -665,6 +665,11 @@ export const meetingClientSettingsInitialValues: MeetingClientSettings = {
           maxDelayTime: 2,
         },
       },
+      networkPriorities: {
+        audio: 'high',
+        webcam: 'medium',
+        screenshare: 'medium',
+      },
       muteAudioOutputWhenAway: false,
       screenshare: {
         showButtonForNonPresenters: false,
