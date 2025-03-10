7d48271facdc8277da507fd4e5bbfa29f5dccf4d
KDSBrowne <kert.browne85@gmail.com>
2025-03-10 04:43:03 +0000

    typescript errors

--- bigbluebutton-html5/imports/ui/components/whiteboard/custom-tools/delete-all/component.ts
+++ bigbluebutton-html5/imports/ui/components/whiteboard/custom-tools/delete-all/component.ts
@@ -1,9 +1,9 @@
-import { StateNode, TLEventHandlers } from "@bigbluebutton/tldraw";
+import { StateNode, TLEventHandlers } from '@bigbluebutton/tldraw';
 
 export class DeleteAllTool extends StateNode {
-  static override id = "delete-all";
+  static override id = 'delete-all';
 
-  override onPointerDown: TLEventHandlers["onPointerDown"] = () => {
-    this.parent.transition("draw");
+  override onPointerDown: TLEventHandlers['onPointerDown'] = () => {
+    this.parent.transition('draw');
   };
 }
