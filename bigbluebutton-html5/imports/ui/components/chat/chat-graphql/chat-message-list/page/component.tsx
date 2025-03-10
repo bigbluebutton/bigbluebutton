7b8456cf928686e23675c6e21a4909ecb148f2f0
João Victor <joaovictornunes973@gmail.com>
2025-03-10 13:41:36 -0300

    fix(chat): Add prefix to the message keys

--- bigbluebutton-html5/imports/ui/components/chat/chat-graphql/chat-message-list/page/component.tsx
+++ bigbluebutton-html5/imports/ui/components/chat/chat-graphql/chat-message-list/page/component.tsx
@@ -231,7 +231,7 @@ const ChatListPage: React.FC<ChatListPageProps> = ({
         const previousMessage = messagesArray[index - 1];
         return (
           <ChatMessage
-            key={message.messageSequence}
+            key={`chat-message-${message.messageSequence}`}
             message={message}
             previousMessage={previousMessage}
             setRenderedChatMessages={setRenderedChatMessages}
