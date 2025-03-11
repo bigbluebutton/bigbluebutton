05c376b2a090a0236305f7219b5500a58103db87
Gustavo Trott <gustavo@trott.com.br>
2025-03-11 09:57:19 -0300

    fix code block style for replies

--- bigbluebutton-html5/imports/ui/components/chat/chat-graphql/chat-message-list/page/chat-message/message-content/text-content/styles.ts
+++ bigbluebutton-html5/imports/ui/components/chat/chat-graphql/chat-message-list/page/chat-message/message-content/text-content/styles.ts
@@ -1,9 +1,8 @@
 import styled from 'styled-components';
 import {
-  colorGrayDark,
-  colorGrayLighter,
-  colorGrayLightest, colorOffWhite,
-  colorText
+  colorGrayLightest,
+  colorOffWhite,
+  colorText,
 } from '/imports/ui/stylesheets/styled-components/palette';
 
 interface ChatMessageProps {
