30923f60e4719195503c414a53a6b0f43a181614
Guilherme Leme <leme.guilherme.p@gmail.com>
2025-03-17 11:26:17 -0300

    [unmount-plugin-on-meeting-end] - Wrap plugin-meeting-manager into useEffect

--- bigbluebutton-html5/imports/ui/components/plugin-top-level-manager/component.tsx
+++ bigbluebutton-html5/imports/ui/components/plugin-top-level-manager/component.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { MeetingStatusData } from 'bigbluebutton-html-plugin-sdk';
 import { UpdatedEventDetails } from 'bigbluebutton-html-plugin-sdk/dist/cjs/core/types';
 import { GENERIC_HOOK_PLUGIN, HookEvents } from 'bigbluebutton-html-plugin-sdk/dist/cjs/core/enum';
@@ -13,19 +13,21 @@ const PluginTopLevelManager: React.FC<PluginTopLevelManagerProps> = (
 ) => {
   const previousUserCurrentlyInMeeting = useStatePreviousValue(currentUserCurrentlyInMeeting);
 
-  if (previousUserCurrentlyInMeeting !== currentUserCurrentlyInMeeting) {
-    window.dispatchEvent(new CustomEvent<UpdatedEventDetails<MeetingStatusData>>(
-      HookEvents.BBB_CORE_UPDATED_MEETING_STATUS,
-      {
-        detail: {
-          data: {
-            userCurrentlyInMeeting: currentUserCurrentlyInMeeting,
+  useEffect(() => {
+    if (previousUserCurrentlyInMeeting !== currentUserCurrentlyInMeeting) {
+      window.dispatchEvent(new CustomEvent<UpdatedEventDetails<MeetingStatusData>>(
+        HookEvents.BBB_CORE_UPDATED_MEETING_STATUS,
+        {
+          detail: {
+            data: {
+              userCurrentlyInMeeting: currentUserCurrentlyInMeeting,
+            },
+            hook: GENERIC_HOOK_PLUGIN,
           },
-          hook: GENERIC_HOOK_PLUGIN,
         },
-      },
-    ));
-  }
+      ));
+    }
+  }, [previousUserCurrentlyInMeeting, currentUserCurrentlyInMeeting]);
 
   return null;
 };
