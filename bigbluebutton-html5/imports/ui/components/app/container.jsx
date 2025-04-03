25fc9cbe659b2bca80db7489763099cefa15bf0e
Ramón Souza <contato@ramonsouza.com>
2025-04-03 10:00:52 -0300

    implements disabledFeatures raiseHand

--- bigbluebutton-html5/imports/ui/components/app/container.jsx
+++ bigbluebutton-html5/imports/ui/components/app/container.jsx
@@ -2,7 +2,11 @@ import React, { useEffect } from 'react';
 import { useMutation, useReactiveVar } from '@apollo/client';
 import AudioCaptionsLiveContainer from '/imports/ui/components/audio/audio-graphql/audio-captions/live/component';
 import getFromUserSettings from '/imports/ui/services/users-settings';
-import { useIsPresentationEnabled, useIsExternalVideoEnabled } from '/imports/ui/services/features';
+import {
+  useIsPresentationEnabled,
+  useIsExternalVideoEnabled,
+  useIsRaiseHandEnabled,
+} from '/imports/ui/services/features';
 import useCurrentUser from '/imports/ui/core/hooks/useCurrentUser';
 import useMeeting from '/imports/ui/core/hooks/useMeeting';
 import {
@@ -86,6 +90,7 @@ const AppContainer = (props) => {
   const isSharedNotesPinned = sharedNotesInput?.isPinned && isSharedNotesPinnedFromGraphql;
   const isExternalVideoEnabled = useIsExternalVideoEnabled();
   const isPresentationEnabled = useIsPresentationEnabled();
+  const isRaiseHandEnabled = useIsRaiseHandEnabled();
   const [showScreenshare] = usePresentationSwap();
   const [setPresentationFitToWidth] = useMutation(SET_PRESENTATION_FIT_TO_WIDTH);
 
@@ -147,6 +152,7 @@ const AppContainer = (props) => {
           isSharedNotesPinned,
           shouldShowPresentation,
           isNotificationEnabled,
+          isRaiseHandEnabled,
           genericMainContentId: genericMainContent.genericContentId,
           audioCaptions: <AudioCaptionsLiveContainer />,
           hideNotificationToasts: hideNotificationToasts
