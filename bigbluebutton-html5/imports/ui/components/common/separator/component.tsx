bed84e2afc1ba4b595b375cc82eba60af950f558
Anton Barboza <antonbsa.bck@gmail.com>
2025-05-12 16:17:19 -0300

    test: add dataTest props for actions bar sample plugin tests

--- bigbluebutton-html5/imports/ui/components/common/separator/component.tsx
+++ bigbluebutton-html5/imports/ui/components/common/separator/component.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { SeparatorProps } from './types';
 import Styled from './styles';
 
-const Separator = ({ icon = '', actionsBar = false }: SeparatorProps) => {
-  if (!icon) return (<Styled.Separator actionsBar={actionsBar} />);
-  return (<Styled.IconContainer><Styled.Icon iconName={icon} /></Styled.IconContainer>);
+const Separator = ({ icon = '', actionsBar = false, dataTest = '' }: SeparatorProps) => {
+  if (!icon) return (<Styled.Separator actionsBar={actionsBar} data-test={dataTest} />);
+  return (<Styled.IconContainer data-test={dataTest}><Styled.Icon iconName={icon} /></Styled.IconContainer>);
 };
 
 export default Separator;
