84d5f8295651ec1ff523d7780a5f8f89bedcf147
Gabriel Ramos <gcrepho@gmail.com>
2025-05-12 13:24:03 -0300

    refac: remove url suffix from apolloClient object

--- bigbluebutton-html5/imports/ui/components/connection-manager/component.tsx
+++ bigbluebutton-html5/imports/ui/components/connection-manager/component.tsx
@@ -82,7 +82,7 @@ const errorLink = onError(({ graphQLErrors, networkError }) => {
 });
 
 const ConnectionManager: React.FC<ConnectionManagerProps> = ({ children }): React.ReactNode => {
-  const [apolloClientUrl, setApolloClientUrl] = React.useState<ApolloClient<NormalizedCacheObject> | null>(null);
+  const [apolloClient, setApolloClient] = React.useState<ApolloClient<NormalizedCacheObject> | null>(null);
   const [graphqlUrl, setGraphqlUrl] = React.useState<string>('');
   const loadingContextInfo = useContext(LoadingContext);
   const numberOfAttempts = useRef(20);
@@ -299,7 +299,7 @@ const ConnectionManager: React.FC<ConnectionManagerProps> = ({ children }): Reac
           cache: new InMemoryCache(),
           connectToDevTools: (process.env.NODE_ENV === 'development') || enableDevTools,
         });
-        setApolloClientUrl(client);
+        setApolloClient(client);
         apolloContextHolder.setClient(client);
       } catch (error) {
         loadingContextInfo.setLoading(false);
@@ -309,10 +309,10 @@ const ConnectionManager: React.FC<ConnectionManagerProps> = ({ children }): Reac
   },
   [graphqlUrl]);
   return (
-    apolloClientUrl
+    apolloClient
       ? (
         <ApolloProvider
-          client={apolloClientUrl}
+          client={apolloClient}
         >
           {children}
         </ApolloProvider>
