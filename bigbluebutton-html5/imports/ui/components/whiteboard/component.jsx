4c0d5f58963cf2f6356e23bd3e74cc1060eeadf3
Tainan Felipe <tainanfelipe214@gmail.com>
2025-05-22 10:11:56 -0300

    Fix(html5): Whiteboard cursor deteriorates near border

--- bigbluebutton-html5/imports/ui/components/whiteboard/component.jsx
+++ bigbluebutton-html5/imports/ui/components/whiteboard/component.jsx
@@ -1392,6 +1392,14 @@ const Whiteboard = React.memo((props) => {
     pollInnerWrapperDimensionsUntilStable(() => {
       adjustCameraOnMount(!isPresenterRef.current);
     });
+
+    // New cursor hint shape: circle
+    const newD = 'M 8,5 A 3,3 0 1,0 2,5 A 3,3 0 1,0 8,5';
+    // Fetch the cursor hint element and update its path
+    const cursorHint = document.getElementById('cursor_hint');
+    if (cursorHint) {
+      cursorHint.setAttribute('d', newD);
+    }
   };
 
   const syncCameraOnPresenterZoom = () => {
