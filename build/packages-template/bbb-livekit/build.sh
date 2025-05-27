989ae3c43ff191997bfe6ed70a8bed9fe2d8863b
prlanzarin <4529051+prlanzarin@users.noreply.github.com>
2025-05-27 13:31:27 +0000

    refactor: shellcheck over bbb-livekit`s build script

--- build/packages-template/bbb-livekit/build.sh
+++ build/packages-template/bbb-livekit/build.sh
@@ -1,61 +1,68 @@
 #!/bin/bash -ex
 
-TARGET=`basename $(pwd)`
+TARGET=$(basename "$(pwd)")
 
 SERVER_VERSION=1.8.4
 CLI_VERSION=2.3.1
 SIP_VERSION=0.9.0
 
-PACKAGE=$(echo $TARGET | cut -d'_' -f1)
-VERSION=$(echo $TARGET | cut -d'_' -f2)
-DISTRO=$(echo $TARGET | cut -d'_' -f3)
+PACKAGE=$(echo "$TARGET" | cut -d'_' -f1)
+VERSION=$(echo "$TARGET" | cut -d'_' -f2)
+DISTRO=$(echo "$TARGET" | cut -d'_' -f3)
 
-BUILDDIR=$PWD
-DESTDIR=$BUILDDIR/staging
+BUILDDIR="$PWD"
+DESTDIR="$BUILDDIR/staging"
 
 #
 # Clear staging directory for build
 
-rm -rf $DESTDIR
-mkdir -p $DESTDIR
+rm -rf "$DESTDIR"
+mkdir -p "$DESTDIR"
 
-. ./opts-$DISTRO.sh
+# shellcheck disable=SC1090
+. "./opts-$DISTRO.sh"
 
-mkdir -p $DESTDIR/usr/share/bigbluebutton/nginx
-cp livekit.nginx $DESTDIR/usr/share/bigbluebutton/nginx
+mkdir -p "$DESTDIR/usr/share/bigbluebutton/nginx"
+cp livekit.nginx "$DESTDIR/usr/share/bigbluebutton/nginx"
 
-mkdir -p $DESTDIR/lib/systemd/system/
-cp livekit-server.service livekit-sip.service $DESTDIR/lib/systemd/system
+mkdir -p "$DESTDIR/lib/systemd/system/"
+cp livekit-server.service livekit-sip.service "$DESTDIR/lib/systemd/system"
 
-mkdir -p $DESTDIR/usr/share/livekit-server
-cp livekit.yaml livekit-sip.yaml $DESTDIR/usr/share/livekit-server
-chmod 644 $DESTDIR/usr/share/livekit-server/livekit*.yaml
+mkdir -p "$DESTDIR/usr/share/livekit-server"
+cp livekit.yaml livekit-sip.yaml "$DESTDIR/usr/share/livekit-server"
+chmod 644 "$DESTDIR/usr/share/livekit-server/livekit"*.yaml
 
-mkdir -p $DESTDIR/usr/bin
+mkdir -p "$DESTDIR/usr/bin"
 
-curl https://github.com/livekit/livekit/releases/download/v${SERVER_VERSION}/livekit_${SERVER_VERSION}_linux_amd64.tar.gz  -Lo - | tar -C $DESTDIR/usr/bin -xzf - livekit-server
-curl https://github.com/livekit/livekit-cli/releases/download/v${CLI_VERSION}/lk_${CLI_VERSION}_linux_amd64.tar.gz -Lo - | tar -C $DESTDIR/usr/bin -xzf - lk
+curl "https://github.com/livekit/livekit/releases/download/v${SERVER_VERSION}/livekit_${SERVER_VERSION}_linux_amd64.tar.gz" -Lo - | tar -C "$DESTDIR/usr/bin" -xzf - livekit-server
+curl "https://github.com/livekit/livekit-cli/releases/download/v${CLI_VERSION}/lk_${CLI_VERSION}_linux_amd64.tar.gz" -Lo - | tar -C "$DESTDIR/usr/bin" -xzf - lk
 
 apt update && apt install -y pkg-config libopus-dev libopusfile-dev libsoxr-dev
 
 buildbase=$(mktemp -d)
-curl https://github.com/livekit/sip/archive/refs/tags/v${SIP_VERSION}.tar.gz -Lo - | tar -xzf - -C $buildbase
-pushd $buildbase/sip-${SIP_VERSION}
+curl "https://github.com/livekit/sip/archive/refs/tags/v${SIP_VERSION}.tar.gz" -Lo - | tar -xzf - -C "$buildbase"
+pushd "$buildbase/sip-${SIP_VERSION}" > /dev/null
 
-if [ "$TARGETPLATFORM" = "linux/arm64" ]; then GOARCH=arm64; else GOARCH=amd64; fi && \
-    CGO_ENABLED=1 GOOS=linux GOARCH=${GOARCH} GO111MODULE=on go build -a -o livekit-sip ./cmd/livekit-sip
+if [ "$TARGETPLATFORM" = "linux/arm64" ]; then
+    GOARCH=arm64
+else
+    GOARCH=amd64
+fi
 
-cp livekit-sip $DESTDIR/usr/bin
+CGO_ENABLED=1 GOOS=linux GOARCH="${GOARCH}" GO111MODULE=on go build -a -o livekit-sip ./cmd/livekit-sip
 
-popd
-rm -rf $buildbase
+cp livekit-sip "$DESTDIR/usr/bin"
 
-fpm -s dir -C $DESTDIR -n $PACKAGE \
-    --version $VERSION --epoch 2 \
-    --before-install before-install.sh      \
-    --after-install after-install.sh        \
-    --before-remove before-remove.sh        \
-    --after-remove after-remove.sh         \
+popd > /dev/null
+rm -rf "$buildbase"
+
+# shellcheck disable=SC2086
+fpm -s dir -C "$DESTDIR" -n "$PACKAGE" \
+    --version "$VERSION" --epoch 2 \
+    --before-install before-install.sh \
+    --after-install after-install.sh \
+    --before-remove before-remove.sh \
+    --after-remove after-remove.sh \
     --description "BigBlueButton build of LiveKit Server" \
-    $DIRECTORIES                            \
+    $DIRECTORIES \
     $OPTS
