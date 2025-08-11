18e945cc10d52fe887ef1beef89c6779948ac9a1
Guilherme Pereira Leme <69865537+GuiLeme@users.noreply.github.com>
2025-08-11 09:07:11 -0300

    feat(html5): ui-command to better manipulate generic content in sidekick area (#23606)
    * [sidekick-area-commands] added 4 new ui- commands for sidekick-area
    
    * [sidekick-area-commands] synced with sdk
    
    * [sidekick-area-commands] refactor to reduce number of states in the class
    
    * [sidekick-area-commands] added callback to optimize rendering

--- bigbluebutton-html5/imports/ui/components/user-list/user-list-content/generic-sidekick-content-button/menu-item/component.tsx
+++ bigbluebutton-html5/imports/ui/components/user-list/user-list-content/generic-sidekick-content-button/menu-item/component.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import * as PluginSdk from 'bigbluebutton-html-plugin-sdk';
 import Icon from '/imports/ui/components/common/icon/component';
 import { ACTIONS, PANELS } from '/imports/ui/components/layout/enums';
-import Styled from '../styles';
+import { SidekickAreaOptionsEnum } from 'bigbluebutton-html-plugin-sdk/dist/cjs/ui-commands/sidekick-area/options/enums';
+import { RemoveGenericContentSidekickAreaBadgeCommandArguments, RenameGenericContentSidekickAreaCommandArguments, SetGenericContentSidekickAreaBadgeCommandArguments } from 'bigbluebutton-html-plugin-sdk/dist/cjs/ui-commands/sidekick-area/options/types';
+import Styled from './styles';
 
 interface GenericContentSidekickAreaMenuItemProps{
   sidebarContentPanel: string;
@@ -18,6 +20,8 @@ const GenericContentSidekickAreaMenuItem = ({
   genericContentSidekickAreaObject,
   layoutContextDispatch,
 }: GenericContentSidekickAreaMenuItemProps) => {
+  const [nameReplacement, setNameReplacement] = useState(genericContentSidekickAreaObject.name);
+  const [badgeContent, setBadgeContent] = useState<string | null>(null);
   useEffect(() => {
     if (genericContentSidekickAreaObject.open) {
       layoutContextDispatch({
@@ -30,6 +34,66 @@ const GenericContentSidekickAreaMenuItem = ({
       });
     }
   }, []);
+
+  const handleGenericContentSetBadge = ((ev: CustomEvent<SetGenericContentSidekickAreaBadgeCommandArguments>) => {
+    const {
+      id: genericContentId,
+      badgeContent,
+    } = ev.detail;
+    if (genericContentId === genericContentSidekickAreaObject.id) {
+      setBadgeContent(badgeContent);
+    }
+  }) as EventListener;
+
+  const handleGenericContentRemoveBadge = ((ev: CustomEvent<RemoveGenericContentSidekickAreaBadgeCommandArguments>) => {
+    const {
+      id: genericContentId,
+    } = ev.detail;
+    if (genericContentId === genericContentSidekickAreaObject.id) {
+      setBadgeContent(null);
+    }
+  }) as EventListener;
+
+  const handleGenericContentRename = ((ev: CustomEvent<RenameGenericContentSidekickAreaCommandArguments>) => {
+    const {
+      id: genericContentId,
+      newName,
+    } = ev.detail;
+    if (genericContentId === genericContentSidekickAreaObject.id) {
+      setNameReplacement(newName);
+    }
+  }) as EventListener;
+
+  useEffect(() => {
+    window.addEventListener(
+      SidekickAreaOptionsEnum.RENAME_GENERIC_CONTENT_MENU,
+      handleGenericContentRename,
+    );
+    window.addEventListener(
+      SidekickAreaOptionsEnum.SET_GENERIC_CONTENT_BADGE,
+      handleGenericContentSetBadge,
+    );
+    window.addEventListener(
+      SidekickAreaOptionsEnum.REMOVE_GENERIC_CONTENT_BADGE,
+      handleGenericContentRemoveBadge,
+    );
+
+    return () => {
+      window.removeEventListener(
+        SidekickAreaOptionsEnum.RENAME_GENERIC_CONTENT_MENU,
+        handleGenericContentRename,
+      );
+      window.removeEventListener(
+        SidekickAreaOptionsEnum.SET_GENERIC_CONTENT_BADGE,
+        handleGenericContentSetBadge,
+      );
+      window.removeEventListener(
+        SidekickAreaOptionsEnum.REMOVE_GENERIC_CONTENT_BADGE,
+        handleGenericContentRemoveBadge,
+      );
+    };
+  }, []);
+
   return (
     <Styled.ScrollableList>
       <Styled.List>
@@ -52,8 +116,18 @@ const GenericContentSidekickAreaMenuItem = ({
         >
           <Icon iconName={genericContentSidekickAreaObject.buttonIcon} />
           <span>
-            {genericContentSidekickAreaObject.name}
+            {nameReplacement}
           </span>
+          {
+            badgeContent
+              && (
+                <Styled.GenericContentBadge>
+                  <Styled.GenericContentBadgeText>
+                    {badgeContent}
+                  </Styled.GenericContentBadgeText>
+                </Styled.GenericContentBadge>
+              )
+          }
         </Styled.ListItem>
       </Styled.List>
     </Styled.ScrollableList>
