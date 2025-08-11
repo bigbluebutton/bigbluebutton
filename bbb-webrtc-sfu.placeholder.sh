7a4b4e603cb5a0b71d9e4e6596ce26391f51f343
Paulo Lanzarin <4529051+prlanzarin@users.noreply.github.com>
2025-08-11 10:05:35 -0300

    build(bbb-webrtc-sfu): v2.19.0-beta.5
    v2.19.0-beta.5
    ---
    * feat(livekit): add flag to enable custom BBB permission checks
    * feat(livekit): add metric for track publish handler errors
    * fix: bbb-webrtc-recorder stop requests have no timeout
    * fix: camera and screen states in BBB become stale on broken stop calls

--- bbb-webrtc-sfu.placeholder.sh
+++ bbb-webrtc-sfu.placeholder.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-git clone --branch v2.19.0-beta.4 --depth 1 https://github.com/bigbluebutton/bbb-webrtc-sfu bbb-webrtc-sfu
+git clone --branch v2.19.0-beta.5 --depth 1 https://github.com/bigbluebutton/bbb-webrtc-sfu bbb-webrtc-sfu
