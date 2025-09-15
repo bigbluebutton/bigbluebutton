b93032176902405cc5b40b40ecf7c79d8f7836c8
hiroshisuga <45039819+hiroshisuga@users.noreply.github.com>
2025-09-15 13:04:11 +0900

    remove font load -> to style.css

--- bigbluebutton-html5/imports/ui/components/whiteboard/component.jsx
+++ bigbluebutton-html5/imports/ui/components/whiteboard/component.jsx
@@ -2138,20 +2138,21 @@ const Whiteboard = React.memo((props) => {
   // To use Kosugi-Maru fonts also in the main window, as all the viewers do.
   // To use the font, you need to install the font under public/fonts/KosugiMaru and
   //  add sentences at public/stylesheets/fonts.css as done together in this PR.
-  React.useEffect(() => {
-    const kosugi = new FontFace(
-      'tldraw_draw',
-      `url(${window.location.origin}/html5client/fonts/KosugiMaru/KosugiMaru-Regular.woff2)`,
-      { weight: '400', style: 'normal' }
-    );
-    kosugi.load()
-      .then((loaded) => {
-        document.fonts.add(loaded);
-      })
-      .catch((err) => {
-        console.error('Failed to load KosugiMaru font:', err);
-      });
-  }, []);
+  //   -> moved to styles.css
+  //React.useEffect(() => {
+  //  const kosugi = new FontFace(
+  //    'tldraw_draw',
+  //    `url(${window.location.origin}/html5client/fonts/KosugiMaru/KosugiMaru-Regular.woff2)`,
+  //    { weight: '400', style: 'normal' }
+  //  );
+  //  kosugi.load()
+  //    .then((loaded) => {
+  //      document.fonts.add(loaded);
+  //    })
+  //    .catch((err) => {
+  //      console.error('Failed to load KosugiMaru font:', err);
+  //    });
+  //, []);
 
   React.useEffect(() => {
     setTldrawIsMounting(true);
@@ -2306,5 +2307,6 @@ Whiteboard.propTypes = {
   isInfiniteWhiteboard: PropTypes.bool,
   whiteboardWriters: PropTypes.arrayOf(PropTypes.shape).isRequired,
 };
+
 
 
