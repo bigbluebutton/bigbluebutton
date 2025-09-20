7f70d98d3d599e72fdf574a355542d2882aab82b
hiroshisuga <45039819+hiroshisuga@users.noreply.github.com>
2025-09-20 11:18:18 +0900

    fix: catch the accumulated message pane bottom
    When the messages are accumulated in the message panel with a user's scroll to above, a button appears to catch the newest message on the bottom . Previously the button did not work due to the global replacement of window.requestAnimationFrame by popup.rAF (and cAF)

--- bigbluebutton-html5/imports/ui/components/chat/chat-graphql/chat-message-list/component.tsx
+++ bigbluebutton-html5/imports/ui/components/chat/chat-graphql/chat-message-list/component.tsx
@@ -41,6 +41,7 @@ import Storage from '/imports/ui/services/storage/in-memory';
 import browserInfo from '/imports/utils/browserInfo';
 import deviceInfo from '/imports/utils/deviceInfo';
 import { originalHTMLElement } from '/imports/utils/HTMLElementBackup';
+import { originalRAF, originalCAF } from '/imports/utils/animationFrameBackup';
 
 const PAGE_SIZE = 50;
 const CLEANUP_TIMEOUT = 3000;
@@ -404,7 +405,7 @@ const ChatMessageList: React.FC<ChatListProps> = ({
       const value = (timestamp - initialTimestamp) / 300;
       if (value <= 1) {
         container.scrollTop = initialPosition + (value * scrollPositionDiff);
-        requestAnimationFrame(animateScrollPosition);
+        originalRAF(animateScrollPosition);
       } else {
         container.scrollTop = container.scrollHeight - container.offsetHeight;
         setIsScrollingDisabled(false);
@@ -421,10 +422,10 @@ const ChatMessageList: React.FC<ChatListProps> = ({
       initialTimestamp = timestamp;
       initialPosition = scrollTop;
       scrollPositionDiff = scrollHeight - offsetHeight - scrollTop;
-      requestAnimationFrame(animateScrollPosition);
+      originalRAF(animateScrollPosition);
     };
 
-    requestAnimationFrame(startScrollAnimation);
+    originalRAF(startScrollAnimation);
   }, []);
 
   const renderUnreadNotification = useMemo(() => {
@@ -538,7 +539,7 @@ const ChatMessageList: React.FC<ChatListProps> = ({
     },
   ) => {
     if (currentFrame < stabilityFrames) {
-      const frameId = requestAnimationFrame(() => {
+      const frameId = originalRAF(() => {
         pollScrollEndEvent(setFrameId, onScrollEnd, {
           stabilityFrames,
           currentFrame: currentFrame + 1,
@@ -553,10 +554,10 @@ const ChatMessageList: React.FC<ChatListProps> = ({
 
   const startScrollEndEventPolling = useCallback((onScrollEnd: () => void) => {
     if (scrollEndFrameRef.current != null) {
-      cancelAnimationFrame(scrollEndFrameRef.current);
+      originalCAF(scrollEndFrameRef.current);
       scrollEndFrameRef.current = undefined;
     }
-    scrollEndFrameRef.current = requestAnimationFrame(() => {
+    scrollEndFrameRef.current = originalRAF(() => {
       pollScrollEndEvent((frameId) => {
         scrollEndFrameRef.current = frameId;
       }, onScrollEnd);
@@ -609,7 +610,7 @@ const ChatMessageList: React.FC<ChatListProps> = ({
       clearInterval(scrollActivityCheckInterval.current);
     }
     if (scrollEndFrameRef.current) {
-      cancelAnimationFrame(scrollEndFrameRef.current);
+      originalCAF(scrollEndFrameRef.current);
     }
   }, []);
 
