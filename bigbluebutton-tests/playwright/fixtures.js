28203f3c9a452f510d6e3f4a71e1537d3dd4ef49
Anton B <antonbsa.bck@gmail.com>
2025-09-22 11:47:36 -0300

    refactor(test): remove unused log management functions from fixtures - used on global-setup

--- bigbluebutton-tests/playwright/fixtures.js
+++ bigbluebutton-tests/playwright/fixtures.js
@@ -1,18 +1,4 @@
 const base = require('@playwright/test');
-const { fullyParallel } = require('./playwright.config');
-const fs = require('fs');
-const path = require('path');
-
-// Logs management functions
-function initializeLogsFolder() {
-  const logsDir = path.join(__dirname, 'logs');
-
-  if (fs.existsSync(logsDir)) {
-    fs.rmSync(logsDir, { recursive: true, force: true });
-  }
-
-  fs.mkdirSync(logsDir, { recursive: true });
-}
 
 const testWithValidation = base.test.extend({
   sharedBeforeEachTestHook: [async ({ browser }, use) => {
