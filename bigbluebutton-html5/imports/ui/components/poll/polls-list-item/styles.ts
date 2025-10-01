c4f16f21a96898fbf0861d6aed7f656c9eb75f68
Arthurk12 <arthur_bockmann@hotmail.com>
2025-10-01 18:30:18 -0300

    chore(polls): cleanup unused files

--- bigbluebutton-html5/imports/ui/components/poll/polls-list-item/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-import Styled from '/imports/ui/components/sidebar-navigation/styles';
-import { ListItemProps } from '/imports/ui/components/sidebar-navigation/types';
-
-const ListItem = styled(Styled.ListItem)<ListItemProps>``;
-
-export default {
-  ListItem,
-};
