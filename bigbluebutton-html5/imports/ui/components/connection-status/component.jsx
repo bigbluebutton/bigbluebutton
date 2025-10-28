006ea487a2fa2c6f645f643f57156d9e20ed216b
Tainan Felipe <tainanfelipe214@gmail.com>
2025-10-28 17:06:37 -0300

    Feat(html5): Move rtt-check fetch to a worker

--- bigbluebutton-html5/imports/ui/components/connection-status/component.jsx
+++ bigbluebutton-html5/imports/ui/components/connection-status/component.jsx
@@ -15,6 +15,8 @@ import getStatus from '../../core/utils/getStatus';
 import logger from '/imports/startup/client/logger';
 import useTimeSync, { setTimeSync } from '/imports/ui/core/local-states/useTimeSync';
 
+const createRttWorker = () => new Worker(new URL('./worker.ts', import.meta.url), { type: 'module' });
+
 const ConnectionStatus = ({
   user,
 }) => {
@@ -22,7 +24,6 @@ const ConnectionStatus = ({
   const STATS_TIMEOUT = window.meetingClientSettings.public.stats.timeout;
   const networkRttInMs = useRef(0); // Ref to store the last network rtt
   const applicationRttInMs = useRef(0); // Ref to store the last application rtt
-  const timeoutRef = useRef(null);
   const [timeSync] = useTimeSync();
   const timeSyncRef = useRef(timeSync);
 
@@ -83,98 +84,59 @@ const ConnectionStatus = ({
     connectionStatus.setConnectionStatus(2000, 'critical');
   };
 
-  const handleUpdateConnectionAliveAt = () => {
-    const startTime = performance.now();
-    const fetchOptions = {
-      signal: AbortSignal.timeout ? AbortSignal.timeout(STATS_TIMEOUT) : undefined,
-    };
+  const handleUpdateConnectionAliveAt = (networkRtt, serverEpochMsec) => {
+    try {
+      // calculate time sync only once
+      if (timeSyncRef.current === 0) {
+        const clientNowEpoch = Date.now();
+        const oneWay = networkRtt / 2; // aproximation NTP
+        // Not allow negative skew
+        const skew = Math.max(0, ((serverEpochMsec * 1000) + oneWay) - clientNowEpoch);
+        logger.debug(`Latency between server and client: ${skew}`);
+        setTimeSync(skew);
+        timeSyncRef.current = skew;
+      }
 
-    fetch(
-      `${getBaseUrl()}/rtt-check`,
-      fetchOptions,
-    )
-      .then((res) => {
-        if (res.ok && res.status === 200) {
-          try {
-            const nowSyncedWithServer = new Date(Date.now() + timeSyncRef.current);
-            const traceLog = user.presenter ? `traceLog@client|${nowSyncedWithServer.toISOString()}` : '';
-            const rttLevels = window.meetingClientSettings.public.stats.rtt;
-            const endTime = performance.now();
-            const networkRtt = Math.round(endTime - startTime);
-
-            // Calc and set latency between client and server
-            if (timeSyncRef.current === 0) {
-              const clientNowEpoch = Date.now();
-              const serverEpochMsec = Number(res.headers.get('X-Server-Epoch-Msec'));
-              const oneWay = networkRtt / 2; // aproximation NTP
-              const skew = ((serverEpochMsec * 1000) + oneWay) - clientNowEpoch;
-              logger.debug(`Latency between server and client: ${skew}`);
-              setTimeSync(skew);
-              timeSyncRef.current = skew;
-            }
-
-            networkRttInMs.current = networkRtt;
-
-            updateConnectionAliveAtM({
-              variables: {
-                clientSessionUUID: sessionStorage.getItem('clientSessionUUID') || '0',
-                networkRttInMs: networkRtt,
-                applicationRttInMs: applicationRttInMs.current,
-                traceLog,
-              },
-            });
-            const rttStatus = getStatus(rttLevels, networkRtt);
-            connectionStatus.setConnectionStatus(networkRtt, rttStatus);
-            connectionStatus.setLastRttRequestSuccess(true);
-
-            if (Object.keys(rttLevels).includes(rttStatus)) {
-              connectionStatus.addUserNetworkHistory(
-                user,
-                rttStatus,
-                Date.now(),
-              );
-            }
-          } catch (error) {
-            logger.error({
-              logCode: 'rtt_failed_to_register_user_history',
-              extraInfo: {
-                error,
-                errorMessage: error.message,
-                errorStack: error.stack,
-                errorCause: error.cause,
-              },
-            }, 'Error registering user network history');
-          }
-        } else {
-          const error = {
-            status: res.status,
-            statusText: res.statusText,
-            url: res.url,
-            stack: new Error().stack,
-          };
-          setErrorOnRtt(error);
-        }
-      })
-      .catch((error) => {
-        setErrorOnRtt(error);
-      })
-      .finally(() => {
-        if (timeoutRef.current) {
-          clearTimeout(timeoutRef.current);
-        }
-
-        timeoutRef.current = setTimeout(() => {
-          handleUpdateConnectionAliveAt();
-        }, STATS_INTERVAL);
+      networkRttInMs.current = networkRtt;
+      const nowSyncedWithServer = new Date(Date.now() + timeSyncRef.current);
+      const traceLog = user.presenter ? `traceLog@client|${nowSyncedWithServer.toISOString()}` : '';
+      const rttLevels = window.meetingClientSettings.public.stats.rtt;
+      updateConnectionAliveAtM({
+        variables: {
+          clientSessionUUID: sessionStorage.getItem('clientSessionUUID') || '0',
+          networkRttInMs: networkRtt,
+          applicationRttInMs: applicationRttInMs.current,
+          traceLog,
+        },
       });
+
+      const rttStatus = getStatus(rttLevels, networkRtt);
+      connectionStatus.setConnectionStatus(networkRtt, rttStatus);
+      connectionStatus.setLastRttRequestSuccess(true);
+
+      if (Object.keys(rttLevels).includes(rttStatus)) {
+        connectionStatus.addUserNetworkHistory(
+          user,
+          rttStatus,
+          Date.now(),
+        );
+      }
+    } catch (error) {
+      logger.error({
+        logCode: 'rtt_failed_to_register_user_history',
+        extraInfo: {
+          error,
+          errorMessage: error.message,
+          errorStack: error.stack,
+          errorCause: error.cause,
+        },
+      }, 'Error registering user network history');
+    }
   };
 
   useEffect(() => {
     // Delay first connectionAlive to avoid high RTT misestimation
     // due to initial subscription and mutation traffic at client render
-    timeoutRef.current = setTimeout(() => {
-      handleUpdateConnectionAliveAt();
-    }, STATS_INTERVAL / 2);
 
     const STATS_ENABLED = window.meetingClientSettings.public.stats.enabled;
 
@@ -186,10 +148,41 @@ const ConnectionStatus = ({
 
     return () => {
       window.removeEventListener('audiostats', handleAudioStatsEvent);
+    };
+  }, []);
 
-      if (timeoutRef.current) {
-        clearTimeout(timeoutRef.current);
+  useEffect(() => {
+    const rttWorker = createRttWorker();
+    rttWorker.onmessage = (ev) => {
+      const { type } = ev.data || {};
+      switch (type) {
+        case 'rtt':
+          // eslint-disable-next-line no-case-declarations
+          const { rtt, serverEpochMsec } = ev.data || {};
+          handleUpdateConnectionAliveAt(rtt, serverEpochMsec);
+          break;
+        case 'error':
+          // eslint-disable-next-line no-case-declarations
+          const { error } = ev.data || {};
+          setErrorOnRtt(error);
+          break;
+        default:
+          break;
       }
+    };
+
+    rttWorker.postMessage({
+      type: 'init',
+      payload: {
+        baseUrl: getBaseUrl(),
+        interval: STATS_INTERVAL,
+        fetchTimeout: STATS_TIMEOUT,
+      },
+    });
+
+    return () => {
+      rttWorker.postMessage({ type: 'stop' });
+      rttWorker.terminate();
     };
   }, []);
 
