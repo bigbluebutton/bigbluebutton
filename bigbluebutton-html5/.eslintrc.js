1be333c0a99c04333b01055ef8c736e52630509a
Lucas Zawacki <lfzawacki@gmail.com>
2025-10-30 15:43:51 -0300

    fix(lint): Disable formatting lint, add react hooks and security lint

--- bigbluebutton-html5/.eslintrc.js
+++ bigbluebutton-html5/.eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2020,
   },
-  plugins: ['react', 'jsx-a11y', 'import'],
+  plugins: ['react', 'react-hooks', 'jsx-a11y', 'import'],
   env: {
     es6: true,
     node: true,
@@ -11,17 +11,62 @@ module.exports = {
     meteor: true,
     jasmine: true,
   },
+  ignorePatterns: ['public/compatibility/*', 'Gruntfile.js', '**/sdpUtils.js', '**/utils/*.js'],
   rules: {
+
+    'object-shorthand': 0,
+    'prefer-rest-params': 0,
+    'implicit-arrow-linebreak': 0,
+    'nonblock-statement-body-position': 0,
+    'curly': 0,
+    'func-call-spacing': 0,
+    'padded-blocks': 0,
+    'quote-props': 0,
     'no-underscore-dangle': 0,
+    'indent': 0,
+    'space-before-function-paren': 0,
+    'operator-linebreak': 0,
+    'no-multiple-empty-lines': 0,
+    'func-names': 0,
+    'camelcase': 0,
+    'linebreak-style': 0,
+    'quotes': 0,
+    'semi': 0,
+    'comma-dangle': 0,
+    'keyword-spacing': 0,
+    'space-in-parens': 0,
+    'no-mixed-operators': 0,
+    'object-curly-spacing': 0,
+    'array-bracket-spacing': 0,
+    'object-curly-newline': 0,
+    'template-curly-spacing': 0,
+    'arrow-parens': 0,
+    'max-len': 0,
+    'arrow-spacing': 0,
+    'no-spaced-func': 0,
+    'no-multi-spaces': 0,
+    'arrow-body-style': 0,
+    'prefer-arrow-callback': 0,
+    'space-before-blocks': 0,
+    'block-spacing': 0,
+    'comma-spacing': 0,
+
     'import/extensions': [2, 'never'],
     'import/no-absolute-path': 0,
     'import/no-unresolved': 0,
     'import/no-extraneous-dependencies': 1,
+
     'react/prop-types': 1,
+
     'jsx-a11y/no-access-key': 0,
     'react/jsx-props-no-spreading': 'off',
+
     'max-classes-per-file': ['error', 2],
     'react/require-default-props': 0,
+
+    // Core hooks rules
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
   },
   globals: {
     browser: 'writable',
@@ -29,7 +74,7 @@ module.exports = {
   overrides: [
     {
       files: ['*.ts', '*.tsx'],
-      extends: ['airbnb', 'eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+      extends: ['airbnb', 'eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:security/recommended-legacy'],
       parser: '@typescript-eslint/parser',
       plugins: ['@typescript-eslint'],
       rules: {
