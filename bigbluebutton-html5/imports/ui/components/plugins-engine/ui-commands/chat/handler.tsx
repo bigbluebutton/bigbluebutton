0e64ad3454c9355574ec90d9017d043cfba04738
Guilherme Leme <leme.guilherme.p@gmail.com>
2025-11-06 17:06:52 -0300

    [private-chat-features] added command to open private chat

--- bigbluebutton-html5/imports/ui/components/plugins-engine/ui-commands/chat/handler.tsx
+++ bigbluebutton-html5/imports/ui/components/plugins-engine/ui-commands/chat/handler.tsx
@@ -2,6 +2,12 @@ import { useEffect } from 'react';
 import {
   ChatFormCommandsEnum,
 } from 'bigbluebutton-html-plugin-sdk/dist/cjs/ui-commands/chat/form/enums';
+import {
+  ChatUiCommandsEnum,
+} from 'bigbluebutton-html-plugin-sdk/dist/cjs/ui-commands/chat/enums';
+import {
+  OpenPrivateChatCommandArguments,
+} from 'bigbluebutton-html-plugin-sdk/dist/cjs/ui-commands/chat/types';
 import { layoutDispatch } from '../../../layout/context';
 import { PANELS, ACTIONS } from '../../../layout/enums';
 
@@ -19,13 +25,36 @@ const PluginChatUiCommandsHandler = () => {
     });
   };
 
+  const handleOpenPrivateChat = ((event: CustomEvent<OpenPrivateChatCommandArguments>) => {
+    const { chatId } = event.detail;
+
+    // Open the sidebar with chat panel
+    layoutContextDispatch({
+      type: ACTIONS.SET_SIDEBAR_CONTENT_IS_OPEN,
+      value: true,
+    });
+    layoutContextDispatch({
+      type: ACTIONS.SET_SIDEBAR_CONTENT_PANEL,
+      value: PANELS.CHAT,
+    });
+
+    // Set the specific chat to open
+    layoutContextDispatch({
+      type: ACTIONS.SET_ID_CHAT_OPEN,
+      value: chatId,
+    });
+  }) as EventListener;
+
   useEffect(() => {
     window.addEventListener(ChatFormCommandsEnum.OPEN, handleChatFormsOpen);
+    window.addEventListener(ChatUiCommandsEnum.OPEN_PRIVATE_CHAT, handleOpenPrivateChat);
 
     return () => {
-      window.addEventListener(ChatFormCommandsEnum.OPEN, handleChatFormsOpen);
+      window.removeEventListener(ChatFormCommandsEnum.OPEN, handleChatFormsOpen);
+      window.removeEventListener(ChatUiCommandsEnum.OPEN_PRIVATE_CHAT, handleOpenPrivateChat);
     };
   }, []);
+
   return null;
 };
 
