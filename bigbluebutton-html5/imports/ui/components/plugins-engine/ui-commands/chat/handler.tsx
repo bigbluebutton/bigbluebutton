41ff42d4b3260d89584cf8f0b5aeffbe7137ffad
Guilherme Leme <leme.guilherme.p@gmail.com>
2025-11-06 17:51:29 -0300

    [private-chat-features] change openPrivateChat to the forms.open

--- bigbluebutton-html5/imports/ui/components/plugins-engine/ui-commands/chat/handler.tsx
+++ bigbluebutton-html5/imports/ui/components/plugins-engine/ui-commands/chat/handler.tsx
@@ -3,32 +3,17 @@ import {
   ChatFormCommandsEnum,
 } from 'bigbluebutton-html-plugin-sdk/dist/cjs/ui-commands/chat/form/enums';
 import {
-  ChatUiCommandsEnum,
-} from 'bigbluebutton-html-plugin-sdk/dist/cjs/ui-commands/chat/enums';
-import {
-  OpenPrivateChatCommandArguments,
-} from 'bigbluebutton-html-plugin-sdk/dist/cjs/ui-commands/chat/types';
+  OpenChatFormCommandArguments,
+} from 'bigbluebutton-html-plugin-sdk/dist/cjs/ui-commands/chat/form/types';
 import { layoutDispatch } from '../../../layout/context';
 import { PANELS, ACTIONS } from '../../../layout/enums';
 
 const PluginChatUiCommandsHandler = () => {
   const layoutContextDispatch = layoutDispatch();
+  const PUBLIC_CHAT_ID = window.meetingClientSettings.public.chat.public_group_id;
 
-  const handleChatFormsOpen = () => {
-    layoutContextDispatch({
-      type: ACTIONS.SET_SIDEBAR_CONTENT_IS_OPEN,
-      value: true,
-    });
-    layoutContextDispatch({
-      type: ACTIONS.SET_SIDEBAR_CONTENT_PANEL,
-      value: PANELS.CHAT,
-    });
-  };
-
-  const handleOpenPrivateChat = ((event: CustomEvent<OpenPrivateChatCommandArguments>) => {
-    const { chatId } = event.detail;
-
-    // Open the sidebar with chat panel
+  const handleChatFormsOpen = ((event: CustomEvent<OpenChatFormCommandArguments>) => {
+    const { chatId } = event?.detail || { chatId: null };
     layoutContextDispatch({
       type: ACTIONS.SET_SIDEBAR_CONTENT_IS_OPEN,
       value: true,
@@ -38,20 +23,25 @@ const PluginChatUiCommandsHandler = () => {
       value: PANELS.CHAT,
     });
 
-    // Set the specific chat to open
-    layoutContextDispatch({
-      type: ACTIONS.SET_ID_CHAT_OPEN,
-      value: chatId,
-    });
+    if (chatId !== null) {
+      // Set the specific chat to open
+      layoutContextDispatch({
+        type: ACTIONS.SET_ID_CHAT_OPEN,
+        value: chatId,
+      });
+    } else {
+      layoutContextDispatch({
+        type: ACTIONS.SET_ID_CHAT_OPEN,
+        value: PUBLIC_CHAT_ID,
+      });
+    }
   }) as EventListener;
 
   useEffect(() => {
     window.addEventListener(ChatFormCommandsEnum.OPEN, handleChatFormsOpen);
-    window.addEventListener(ChatUiCommandsEnum.OPEN_PRIVATE_CHAT, handleOpenPrivateChat);
 
     return () => {
       window.removeEventListener(ChatFormCommandsEnum.OPEN, handleChatFormsOpen);
-      window.removeEventListener(ChatUiCommandsEnum.OPEN_PRIVATE_CHAT, handleOpenPrivateChat);
     };
   }, []);
 
