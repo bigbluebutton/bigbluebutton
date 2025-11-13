85d3600ce25f65e3639f3a9fca13cf3ac05b8ad3
Gabriel Ramos <gcrepho@gmail.com>
2025-11-13 13:37:30 -0300

    refac: update logic to fit typescript migration

--- bigbluebutton-tests/playwright/parameters/parameters.spec.ts
+++ bigbluebutton-tests/playwright/parameters/parameters.spec.ts
@@ -614,14 +614,21 @@ test.describe.parallel('Custom Parameters', { tag: '@ci' }, () => {
     await customParam.logoutURLTest();
   });
 
-    test('Predefined groups for Breakout Rooms', async ({ browser, context, page }, testInfo) => {
+  test('Predefined groups for Breakout Rooms', async ({ browser, context, page }, testInfo) => {
     const customParam = new CustomParameters(browser, context);
-    await customParam.initModPage(page, true, { createParameter: `${encodeCustomParams(c.groups)}`, testInfo });
-    await customParam.initUserPage(true, context, { fullName: `Attendee-1235`, joinParameter: 'userID=1235',testInfo });
-    await customParam.initUserPage2(true, context, { fullName: `Attendee-2335`, joinParameter: 'userID=2335',testInfo });
+    await customParam.initModPage(page, { createParameter: `${encodeCustomParams(c.groups)}`, testInfo });
+    await customParam.initUserPage(context, {
+      fullName: `Attendee-1235`,
+      joinParameter: 'userID=1235',
+      testInfo,
+    });
+    await customParam.initUserPage2(context, {
+      fullName: `Attendee-2335`,
+      joinParameter: 'userID=2335',
+      testInfo,
+    });
     await customParam.predefinedGroups();
   });
-
 
   test.describe.parallel('Audio', () => {
     test('Auto join', async ({ browser, context, page }, testInfo) => {
