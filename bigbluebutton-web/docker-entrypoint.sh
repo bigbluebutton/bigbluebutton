bf5e725ea85ffe5617c97e5af99e207df456d317
Samuel Weirich <4281791+SamuelWei@users.noreply.github.com>
2025-11-27 22:56:55 +0100

    Fix bbb-web start
    # Conflicts:
    #	bigbluebutton-web/docker-entrypoint.sh
    #	docker-compose.yml

--- bigbluebutton-web/docker-entrypoint.sh
+++ bigbluebutton-web/docker-entrypoint.sh
@@ -14,5 +14,7 @@ if [ -n "${TURN_DOMAIN}" ] && [ -n "${TURN_SECRET}" ]; then
     sed -i "s|{{ .Env.TURN_SECRET }}|${TURN_SECRET}|g" /etc/bigbluebutton/turn-stun-servers.xml
 fi
 
+cd /usr/share/bbb-web
+
 # Allow additional JVM options via JAVA_OPTS environment variable
 exec java ${JAVA_OPTS} -Dgrails.env=prod -Dserver.address=0.0.0.0 -Dserver.port=8090 -Dspring.main.allow-circular-references=true -Xms384m -Xmx384m -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/var/bigbluebutton/diagnostics -cp WEB-INF/lib/*:/:WEB-INF/classes/:. org.springframework.boot.loader.WarLauncher "$@"
