e775bfff76469264c906280bdc2e2b005c1536a3
Samuel Weirich <4281791+SamuelWei@users.noreply.github.com>
2025-11-27 21:31:34 +0100

    Fix bbb-graphql-server

--- bbb-graphql-server/docker-start.sh
+++ bbb-graphql-server/docker-start.sh
@@ -9,17 +9,6 @@ yq e -i ".[1].configuration.connection_info.database_url = \"$HASURA_GRAPHQL_BBB
 
 sed -i "s/^admin_secret: .*/admin_secret: $HASURA_GRAPHQL_ADMIN_SECRET/g" /app/config.yaml
 
-echo "SELECT 'CREATE DATABASE hasura_app' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'hasura_app')\gexec" | psql
-
-echo "Restarting database bbb_graphql"
-psql -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE datname = 'bbb_graphql'" > /dev/null
-psql -c "drop database if exists bbb_graphql with (force)"
-psql -c "create database bbb_graphql WITH TEMPLATE template0 LC_COLLATE 'C.UTF-8'"
-psql -c "alter database bbb_graphql set timezone to 'UTC'"
-
-echo "Creating tables in bbb_graphql"
-psql -U postgres -d bbb_graphql -q -f bbb_schema.sql --set ON_ERROR_STOP=on
-
 echo "Starting hasura-graphql-engine"
 gosu nobody graphql-engine serve &
 PID=$!
