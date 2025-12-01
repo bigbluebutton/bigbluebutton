8e065ea4f1a19efab7c8daa223a97db24b7d2f36
Samuel Weirich <4281791+SamuelWei@users.noreply.github.com>
2025-12-01 12:34:04 +0100

    Fix apps-akka logging

--- akka-bbb-apps/docker/entrypoint.sh
+++ akka-bbb-apps/docker/entrypoint.sh
@@ -6,5 +6,8 @@ cp /etc/bigbluebutton/bbb-apps-akka.conf.tmpl $TARGET
 sed -i "s/SHARED_SECRET/$SHARED_SECRET/" $TARGET
 sed -i "s/POSTGRES_PASSWORD/$POSTGRES_PASSWORD/" $TARGET
 
+# Set log level
+sed -i "s|BBB_APPS_AKKA_LOGLEVEL|${BBB_APPS_AKKA_LOGLEVEL:-INFO}|g" /bbb-apps-akka/conf/logback.xml
+
 cd /bbb-apps-akka
 /bbb-apps-akka/bin/bbb-apps-akka
