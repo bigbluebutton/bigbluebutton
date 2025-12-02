bigbluebutton/bigbluebutton

commit 6c7ff0ea4144491d8318dc89a5215c6c2cec0eae

feat: add reusable SidebarNavigationButton component and latched useUnreadChatMessages hook
Performance improvements:
- Reduce navigation sidebar component re-renders through intelligent
  chat ID memoization
- Implement latch system preventing unnecessary updates when panels are
  open
- Optimize useChat subscription to avoid re-renders on every new message
- Replace full chat objects with lightweight chat ID arrays in hook
  returns

Code quality improvements:
- Create generic SidebarNavigationButton component eliminating code
  duplication
- Add support for both panel-based buttons and modal/action buttons
- Refactor ChatListItem, AppsListItem, and UsersListItem using new
  reusable component
- Follow established useHasUnreadNotes pattern for consistency across
  codebase

bigbluebutton-html5/imports/ui/components/sidebar-navigation/user-notes-list-item/component.tsx
@@ -1,18 +1,16 @@
-import React, { useCallback, useState, memo } from 'react';
+import React, { useState, memo } from 'react';
 import { defineMessages, useIntl } from 'react-intl';
-import KEYS from '/imports/utils/keys';
 import Icon from '/imports/ui/components/common/icon/component';
 import NotesService from '/imports/ui/components/notes/service';
-import { PANELS } from '/imports/ui/components/layout/enums';
 import { notify } from '/imports/ui/services/notification';
-import { layoutSelectInput, layoutDispatch } from '/imports/ui/components/layout/context';
 import Styled from './styles';
 import usePreviousValue from '/imports/ui/hooks/usePreviousValue';
 import useHasUnreadNotes from '/imports/ui/components/notes/hooks/useHasUnreadNotes';
-import TooltipContainer from '/imports/ui/components/common/tooltip/container';
 import useMeeting from '/imports/ui/core/hooks/useMeeting';
-import { DispatcherFunction } from '/imports/ui/components/layout/layoutTypes';
-import lockContextContainer from '/imports/ui/components/lock-viewers/context/container';
+import SidebarNavigationButton from '/imports/ui/components/sidebar-navigation/sidebar-navigation-button/component';
+import { BaseSidebarButtonProps } from '../types';
+import { PANELS } from '/imports/ui/components/layout/enums';
+import useLockContext from '/imports/ui/components/lock-viewers/hooks/useLockContext';
 
 const intlMessages = defineMessages({
   title: {
@@ -49,134 +47,60 @@ const intlMessages = defineMessages({
   },
 });
 
-interface UserNotesGraphqlProps {
-  isPinned: boolean,
-  disableNotes: boolean,
-  sidebarContentPanel: string,
-  layoutContextDispatch: DispatcherFunction,
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  toggleNotesPanel: (sidebarContentPanel: any, layoutContextDispatch: any) => void,
-  isEnabled: boolean,
-}
-
-interface UserNotesContainerGraphqlProps {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  userLocks: any;
-}
-
-const UserNotesGraphql: React.FC<UserNotesGraphqlProps> = (props) => {
-  const {
-    isPinned,
-    disableNotes,
-    sidebarContentPanel,
-    layoutContextDispatch,
-    isEnabled,
-    toggleNotesPanel,
-  } = props;
-  const [pinWasNotified, setPinWasNotified] = useState(false);
+const UserNotesListItemContainerGraphql: React.FC<BaseSidebarButtonProps> = (props) => {
+  const { isOpened } = props;
   const intl = useIntl();
-  const prevIsPinned = usePreviousValue(isPinned);
-
-  const hasUnreadNotes = useHasUnreadNotes();
-
-  if (isPinned && !pinWasNotified) {
-    notify(intl.formatMessage(intlMessages.pinnedNotification), 'info', 'copy', { pauseOnFocusLoss: false });
-    setPinWasNotified(true);
-  }
-
-  const notesOpen = sidebarContentPanel === PANELS.SHARED_NOTES && !isPinned;
-
-  if (prevIsPinned && !isPinned && pinWasNotified) {
-    setPinWasNotified(false);
-  }
-
-  const onClick = useCallback(() => {
-    if (!isPinned) {
-      toggleNotesPanel(sidebarContentPanel, layoutContextDispatch);
-    }
-  }, [isPinned, sidebarContentPanel, layoutContextDispatch, toggleNotesPanel]);
-
-  const renderNotes = () => {
-    let tooltipMessage = '';
-    if (isPinned) {
-      tooltipMessage = intl.formatMessage(intlMessages.sharedNotesPinned);
-    } else {
-      tooltipMessage = intl.formatMessage(intlMessages.sharedNotes);
-    }
-    return (
-      <TooltipContainer
-        title={tooltipMessage}
-        position="right"
-      >
-        {/* @ts-ignore */}
-        <Styled.ListItem
-          id="shared-notes-toggle-button"
-          aria-label={tooltipMessage}
-          aria-describedby="lockedNotes"
-          role="button"
-          tabIndex={0}
-          active={notesOpen}
-          data-test="sharedNotesSidebarButton"
-          onClick={onClick}
-          // @ts-ignore
-          onKeyDown={(e) => {
-            if (e.key === KEYS.ENTER) {
-              onClick();
-            }
-          }}
-          hasNotification={hasUnreadNotes}
-          $disabled={isPinned}
-        >
-          {disableNotes
-            ? (
-              <Styled.NotesLock>
-                {/* @ts-ignore */}
-                <span id="lockedNotes">
-                  <Icon iconName="lock" />
-                  &nbsp;
-                  {`${intl.formatMessage(intlMessages.locked)} ${intl.formatMessage(intlMessages.byModerator)}`}
-                </span>
-              </Styled.NotesLock>
-            ) : null}
-          <Icon iconName="shared_notes" />
-        </Styled.ListItem>
-      </TooltipContainer>
-    );
-  };
-
-  if (!isEnabled) return null;
-
-  return renderNotes();
-};
-
-const UserNotesListItemContainerGraphql: React.FC<UserNotesContainerGraphqlProps> = (props) => {
-  const { userLocks } = props;
+  const { userLocks } = useLockContext();
   const disableNotes = userLocks.userNotes;
-
+  const [pinWasNotified, setPinWasNotified] = useState(false);
+  const hasUnreadNotes = useHasUnreadNotes({ isNotesPanelOpened: isOpened });
   const {
     data: currentMeeting,
   } = useMeeting((meeting) => ({
     componentsFlags: meeting.componentsFlags,
   }));
-
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const sidebarContent = layoutSelectInput((i: any) => i.sidebarContent);
-  const { sidebarContentPanel } = sidebarContent;
-  const layoutContextDispatch = layoutDispatch();
+  const isPinned = currentMeeting?.componentsFlags?.isSharedNotesPinned ?? false;
+  const prevIsPinned = usePreviousValue(isPinned);
 
   const isEnabled = NotesService.useIsEnabled();
+  if (isPinned && !pinWasNotified) {
+    notify(intl.formatMessage(intlMessages.pinnedNotification), 'info', 'copy', { pauseOnFocusLoss: false });
+    setPinWasNotified(true);
+  }
 
-  const isPinned = currentMeeting?.componentsFlags?.isSharedNotesPinned ?? false;
+  const notesOpen = isOpened && !isPinned;
+
+  if (prevIsPinned && !isPinned && pinWasNotified) {
+    setPinWasNotified(false);
+  }
+
+  const label = intl.formatMessage(isPinned ? intlMessages.sharedNotesPinned : intlMessages.sharedNotes);
+  if (!isEnabled) return null;
   return (
-    <UserNotesGraphql
-      isPinned={isPinned}
-      disableNotes={disableNotes}
-      layoutContextDispatch={layoutContextDispatch}
-      sidebarContentPanel={sidebarContentPanel}
-      toggleNotesPanel={NotesService.toggleNotesPanel}
-      isEnabled={isEnabled}
-    />
+    <SidebarNavigationButton
+      panel={PANELS.SHARED_NOTES}
+      isOpened={notesOpen}
+      iconName="shared_notes"
+      label={label}
+      id="shared-notes-toggle-button"
+      ariaDescribedBy="lockedNotes"
+      dataTest="sharedNotesSidebarButton"
+      hasNotification={hasUnreadNotes}
+      isDisabled={disableNotes}
+    >
+      {disableNotes
+        ? (
+          <Styled.NotesLock>
+            {/* @ts-ignore */}
+            <span id="lockedNotes">
+              <Icon iconName="lock" />
+              &nbsp;
+              {`${intl.formatMessage(intlMessages.locked)} ${intl.formatMessage(intlMessages.byModerator)}`}
+            </span>
+          </Styled.NotesLock>
+        ) : null}
+    </SidebarNavigationButton>
   );
 };
 
-export default lockContextContainer(memo(UserNotesListItemContainerGraphql));
+export default memo(UserNotesListItemContainerGraphql);

bigbluebutton-html5/imports/ui/components/sidebar-navigation/apps-list-item/component.tsx
@@ -1,12 +1,8 @@
-import React from 'react';
-import TooltipContainer from '/imports/ui/components/common/tooltip/container';
-import KEYS from '/imports/utils/keys';
+import React, { memo } from 'react';
 import { defineMessages, useIntl } from 'react-intl';
-import Icon from '/imports/ui/components/common/icon/component';
-import { PANELS, ACTIONS } from '../../layout/enums';
-import { layoutDispatch, layoutSelectInput } from '/imports/ui/components/layout/context';
-import { Input } from '/imports/ui/components/layout/layoutTypes';
-import Styled from '../styles';
+import { PANELS } from '/imports/ui/components/layout/enums';
+import SidebarNavigationButton from '/imports/ui/components/sidebar-navigation/sidebar-navigation-button/component';
+import { BaseSidebarButtonProps } from '../types';
 
 const intlMessages = defineMessages({
   wigetsLabel: {
@@ -15,53 +11,21 @@ const intlMessages = defineMessages({
   },
 });
 
-const AppsListItem = () => {
-  const CURRENT_PANEL = PANELS.APPS_GALLERY;
+const AppsListItem: React.FC<BaseSidebarButtonProps> = ({ isOpened }) => {
   const intl = useIntl();
-  const layoutContextDispatch = layoutDispatch();
-  const sidebarContent = layoutSelectInput((i: Input) => i.sidebarContent);
-  const { sidebarContentPanel } = sidebarContent;
-
-  const toggleAppsGalleryPanel = () => {
-    layoutContextDispatch({
-      type: ACTIONS.SET_SIDEBAR_CONTENT_IS_OPEN,
-      value: sidebarContentPanel !== CURRENT_PANEL,
-    });
-    layoutContextDispatch({
-      type: ACTIONS.SET_SIDEBAR_CONTENT_PANEL,
-      value: sidebarContentPanel === CURRENT_PANEL
-        ? PANELS.NONE
-        : CURRENT_PANEL,
-    });
-  };
-
   const label = intl.formatMessage(intlMessages.wigetsLabel);
 
   return (
-    <TooltipContainer
-      title={label}
-      position="right"
-    >
-      <Styled.ListItem
-        id="apps-gallery-toggle-button"
-        aria-label={label}
-        aria-describedby="appsGallery"
-        active={sidebarContentPanel === CURRENT_PANEL}
-        role="button"
-        tabIndex={0}
-        data-test="appsGallerySidebarButton"
-        onClick={toggleAppsGalleryPanel}
-        // @ts-ignore
-        onKeyDown={(e) => {
-          if (e.key === KEYS.ENTER) {
-            toggleAppsGalleryPanel();
-          }
-        }}
-      >
-        <Icon iconName="widgets" />
-      </Styled.ListItem>
-    </TooltipContainer>
+    <SidebarNavigationButton
+      panel={PANELS.APPS_GALLERY}
+      isOpened={isOpened}
+      iconName="widgets"
+      label={label}
+      id="apps-gallery-toggle-button"
+      ariaDescribedBy="appsGallery"
+      dataTest="appsGallerySidebarButton"
+    />
   );
 };
 
-export default AppsListItem;
+export default memo(AppsListItem);

bigbluebutton-html5/imports/ui/components/sidebar-navigation/chat-list-item/component.tsx
@@ -1,16 +1,14 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
 /* eslint-disable jsx-a11y/no-access-key */
-import React, { useEffect } from 'react';
-import KEYS from '/imports/utils/keys';
-import { layoutSelect, layoutSelectInput, layoutDispatch } from '/imports/ui/components/layout/context';
+import React, { useCallback, useEffect, memo } from 'react';
+import { layoutSelect, layoutDispatch } from '/imports/ui/components/layout/context';
 import { ACTIONS, PANELS } from '/imports/ui/components/layout/enums';
 import { defineMessages, useIntl } from 'react-intl';
-import Styled from './styles';
-import Icon from '/imports/ui/components/common/icon/component';
-import { Input, Layout } from '/imports/ui/components/layout/layoutTypes';
+import { Layout } from '/imports/ui/components/layout/layoutTypes';
 import { useShortcut } from '../../../core/hooks/useShortcut';
-import { Chat } from '/imports/ui/Types/chat';
-import TooltipContainer from '/imports/ui/components/common/tooltip/container';
+import { useIsChatEnabled } from '/imports/ui/services/features';
+import useHasUnreadChatMessages from '../../chat/hooks/useHasUnreadChatMessages';
+import SidebarNavigationButton from '/imports/ui/components/sidebar-navigation/sidebar-navigation-button/component';
+import { BaseSidebarButtonProps } from '../types';
 
 const intlMessages = defineMessages({
   messagesTitle: {
@@ -27,85 +25,58 @@ const intlMessages = defineMessages({
   },
 });
 
-interface ChatListItemProps {
-  chat: Chat,
-  chatsAggregateUnreadMessages: number,
-}
-
-const ChatListItem = (props: ChatListItemProps) => {
-  const CURRENT_PANEL = PANELS.CHAT;
+const ChatListItem: React.FC<BaseSidebarButtonProps> = ({ isOpened }) => {
   const CHAT_CONFIG = window.meetingClientSettings.public.chat;
   const PUBLIC_GROUP_CHAT_ID = CHAT_CONFIG.public_group_id;
-  const {
-    chat,
-    chatsAggregateUnreadMessages,
-  } = props;
-
-  const sidebarContent = layoutSelectInput((i: Input) => i.sidebarContent);
-  const idChatOpen = layoutSelect((i: Layout) => i.idChatOpen);
-  const layoutContextDispatch = layoutDispatch();
-
-  const { sidebarContentPanel } = sidebarContent;
-  const sidebarContentIsOpen = sidebarContent.isOpen;
-
-  const TOGGLE_CHAT_PUB_AK: string = useShortcut('togglePublicChat');
   const intl = useIntl();
 
+  const {
+    hasUnreadMessages,
+    activeChat,
+    chatIds,
+  } = useHasUnreadChatMessages({ isChatPanelOpened: isOpened });
+
+  const idChatOpen = layoutSelect((i: Layout) => i.idChatOpen);
+  const isChatEnabled = useIsChatEnabled();
+  const layoutContextDispatch = layoutDispatch();
+
+  const TOGGLE_CHAT_PUB_AK: string = useShortcut('togglePublicChat');
+
   useEffect(() => {
-    if (chat.chatId !== PUBLIC_GROUP_CHAT_ID && chat.chatId === idChatOpen) {
+    if (!activeChat) return;
+    if (activeChat.chatId !== PUBLIC_GROUP_CHAT_ID && activeChat.chatId === idChatOpen) {
       layoutContextDispatch({
         type: ACTIONS.SET_ID_CHAT_OPEN,
-        value: chat.chatId,
+        value: activeChat.chatId,
       });
     }
-  }, [idChatOpen, sidebarContentIsOpen, sidebarContentPanel, chat]);
+  }, [idChatOpen, activeChat, layoutContextDispatch, PUBLIC_GROUP_CHAT_ID]);
 
-  const handleClickToggleChat = () => {
-    // Verify if chat panel is open
-    layoutContextDispatch({
-      type: ACTIONS.SET_SIDEBAR_CONTENT_IS_OPEN,
-      value: sidebarContentPanel !== CURRENT_PANEL,
-    });
-    layoutContextDispatch({
-      type: ACTIONS.SET_SIDEBAR_CONTENT_PANEL,
-      value: sidebarContentPanel === CURRENT_PANEL
-        ? PANELS.NONE
-        : CURRENT_PANEL,
-    });
+  const handleChatToggle = useCallback(() => {
     layoutContextDispatch({
       type: ACTIONS.SET_ID_CHAT_OPEN,
       value: PUBLIC_GROUP_CHAT_ID,
     });
-  };
+  }, [layoutContextDispatch, PUBLIC_GROUP_CHAT_ID]);
+
+  if (!isChatEnabled) return null;
+  if (!chatIds?.length || !activeChat) return <></>;
+
+  const label = intl.formatMessage(intlMessages.messagesTitle);
 
   return (
-    <TooltipContainer
-      title={intl.formatMessage(intlMessages.messagesTitle)}
-      position="right"
-    >
-      <Styled.ListItem
-        data-test="messagesSidebarButton"
-        role="button"
-        aria-expanded={sidebarContentPanel === CURRENT_PANEL}
-        active={sidebarContentPanel === CURRENT_PANEL}
-        tabIndex={0}
-        accessKey={TOGGLE_CHAT_PUB_AK}
-        onClick={handleClickToggleChat}
-        id="chat-toggle-button"
-        aria-label={intl.formatMessage(intlMessages.messagesTitle)}
-        hasNotification={chatsAggregateUnreadMessages > 0}
-        // @ts-ignore
-        onKeyDown={(e) => {
-          if (e.key === KEYS.ENTER) {
-            e.preventDefault();
-            e.stopPropagation();
-          }
-        }}
-      >
-        <Icon iconName="group_chat" />
-      </Styled.ListItem>
-    </TooltipContainer>
+    <SidebarNavigationButton
+      panel={PANELS.CHAT}
+      isOpened={isOpened}
+      iconName="group_chat"
+      label={label}
+      accessKey={TOGGLE_CHAT_PUB_AK}
+      id="chat-toggle-button"
+      dataTest="messagesSidebarButton"
+      hasNotification={hasUnreadMessages}
+      onToggle={handleChatToggle}
+    />
   );
 };
 
-export default ChatListItem;
+export default memo(ChatListItem);

bigbluebutton-html5/imports/ui/components/sidebar-navigation/component.tsx
@@ -8,8 +8,7 @@ import { defineMessages, useIntl } from 'react-intl';
 import CustomLogo from './custom-logo/component';
 import ProfileListItem from './profile-list-item/component';
 import getFromUserSettings from '/imports/ui/services/users-settings';
-import { useIsChatEnabled } from '/imports/ui/services/features';
-import ChatListItemContainer from './chat-list-item/container';
+import ChatListItem from './chat-list-item/component';
 import UserNotesListItemContainer from './user-notes-list-item/component';
 import UsersListItem from './users-list-item/component';
 import AppsListItem from './apps-list-item/component';
@@ -72,7 +71,6 @@ const SidebarNavigation = ({
   sidebarContentPanel,
 }: SidebarNavigationProps) => {
   const intl = useIntl();
-  const isChatEnabled = useIsChatEnabled();
   const showBrandingArea = getFromUserSettings('bbb_display_branding_area', window.meetingClientSettings.public.app.branding.displayBrandingArea);
 
   const scrollRef = useRef<HTMLDivElement>(null);
@@ -177,14 +175,14 @@ const SidebarNavigation = ({
         >
           <Styled.Top>
             {showBrandingArea && <CustomLogo />}
-            <ProfileListItem />
-            <UsersListItem />
-            {isChatEnabled && <ChatListItemContainer />}
-            <UserNotesListItemContainer />
+            <ProfileListItem isOpened={sidebarContentPanel === PANELS.PROFILE} />
+            <UsersListItem isOpened={sidebarContentPanel === PANELS.USERLIST} />
+            <ChatListItem isOpened={sidebarContentPanel === PANELS.CHAT} />
+            <UserNotesListItemContainer isOpened={sidebarContentPanel === PANELS.SHARED_NOTES} />
           </Styled.Top>
 
           <Styled.Center>
-            <AppsListItem />
+            <AppsListItem isOpened={sidebarContentPanel === PANELS.APPS_GALLERY} />
             <PinnedApps
               sidebarNavigationInput={sidebarNavigationInput}
             />

bigbluebutton-html5/imports/ui/components/sidebar-navigation/custom-logo/component.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, memo } from 'react';
 import Styled from './styles';
 import { useStorageKey } from '/imports/ui/services/storage/hooks';
 import { isDarkThemeEnabled } from '/imports/ui/components/app/service';
@@ -34,4 +34,4 @@ const CustomLogo = () => {
   );
 };
 
-export default CustomLogo;
+export default memo(CustomLogo);

bigbluebutton-html5/imports/ui/components/sidebar-navigation/learning-dashboard-list-item/component.tsx
@@ -1,11 +1,8 @@
-import React, { useCallback } from 'react';
-import KEYS from '/imports/utils/keys';
-import TooltipContainer from '/imports/ui/components/common/tooltip/container';
+import React, { useCallback, memo } from 'react';
 import { defineMessages, useIntl } from 'react-intl';
-import Icon from '/imports/ui/components/common/icon/component';
 import LearningDashboardService from '/imports/ui/components/learning-dashboard/service';
-import Styled from '../styles';
 import useMeeting from '/imports/ui/core/hooks/useMeeting';
+import SidebarNavigationButton from '/imports/ui/components/sidebar-navigation/sidebar-navigation-button/component';
 
 const intlMessages = defineMessages({
   learningDashboardLabel: {
@@ -21,7 +18,7 @@ const LearningDashboardListItem = () => {
     isBreakout: meeting?.isBreakout,
   }));
 
-  const toggleLearningDashboardPanel = useCallback(() => {
+  const openLearningDashboardPanel = useCallback(() => {
     LearningDashboardService.openLearningDashboardUrl(intl.locale, meetingInfo?.learningDashboardAccessToken);
   }, [intl, meetingInfo]);
 
@@ -30,30 +27,15 @@ const LearningDashboardListItem = () => {
   if (meetingInfo?.isBreakout) return null;
 
   return (
-    <TooltipContainer
-      title={label}
-      position="right"
-    >
-      <Styled.ListItem
-        id="learning-dashboard-toggle-button"
-        aria-label={label}
-        aria-describedby="learningDashboard"
-        active={false}
-        role="button"
-        tabIndex={0}
-        data-test="learningDashboardSidebarButton"
-        onClick={toggleLearningDashboardPanel}
-        // @ts-ignore
-        onKeyDown={(e) => {
-          if (e.key === KEYS.ENTER) {
-            toggleLearningDashboardPanel();
-          }
-        }}
-      >
-        <Icon iconName="learning_dashboard" />
-      </Styled.ListItem>
-    </TooltipContainer>
+    <SidebarNavigationButton
+      iconName="learning_dashboard"
+      label={label}
+      id="learning-dashboard-toggle-button"
+      ariaDescribedBy="learningDashboard"
+      dataTest="learningDashboardSidebarButton"
+      onClick={openLearningDashboardPanel}
+    />
   );
 };
 
-export default LearningDashboardListItem;
+export default memo(LearningDashboardListItem);

bigbluebutton-html5/imports/ui/components/sidebar-navigation/pinned-apps/component.tsx
@@ -7,8 +7,8 @@ import {
 } from '/imports/ui/components/layout/layoutTypes';
 import PinnedAppBase from './pinned-app-list-item/component';
 import ExternalPinnedApp from './external-pinned-app-list-item/component';
-import { layoutDispatch, layoutSelectInput } from '/imports/ui/components/layout/context';
-import { ACTIONS, PANELS } from '/imports/ui/components/layout/enums';
+import { layoutSelectInput } from '/imports/ui/components/layout/context';
+import { PANELS } from '/imports/ui/components/layout/enums';
 
 interface PinnedAppsProps {
   sidebarNavigationInput: SidebarNavigation;
@@ -16,24 +16,11 @@ interface PinnedAppsProps {
 
 const PinnedApps = ({ sidebarNavigationInput }: PinnedAppsProps) => {
   const { registeredApps = {}, pinnedApps = [] } = sidebarNavigationInput;
-  const layoutContextDispatch = layoutDispatch();
   const { sidebarContentPanel } = layoutSelectInput((i: Input) => i.sidebarContent);
-  const openPanel = (pinnedAppKey: string) => {
-    layoutContextDispatch({
-      type: ACTIONS.SET_SIDEBAR_CONTENT_IS_OPEN,
-      value: sidebarContentPanel !== pinnedAppKey,
-    });
-    layoutContextDispatch({
-      type: ACTIONS.SET_SIDEBAR_CONTENT_PANEL,
-      value: sidebarContentPanel === pinnedAppKey
-        ? PANELS.NONE
-        : pinnedAppKey,
-    });
-  };
 
   return pinnedApps.map((pinnedAppKey: string) => {
     const pinnedAppInfo = registeredApps[pinnedAppKey];
-    const isActive = sidebarContentPanel === pinnedAppKey;
+    const isOpened = sidebarContentPanel === pinnedAppKey;
 
     const Component = pinnedAppKey.startsWith(PANELS.GENERIC_CONTENT_SIDEKICK)
       ? ExternalPinnedApp
@@ -44,8 +31,7 @@ const PinnedApps = ({ sidebarNavigationInput }: PinnedAppsProps) => {
         key={pinnedAppKey}
         appKey={pinnedAppKey}
         appInfo={pinnedAppInfo}
-        active={isActive}
-        onActivate={openPanel}
+        isOpened={isOpened}
       />
     );
   });

bigbluebutton-html5/imports/ui/components/sidebar-navigation/pinned-apps/external-pinned-app-list-item/component.tsx
@@ -19,8 +19,7 @@ const ExternalPinnedApp: React.FC<PinnedAppProps> = (props) => {
   const {
     appKey,
     appInfo,
-    active,
-    onActivate,
+    isOpened,
   } = props;
   const { name } = appInfo;
   const [nameReplacement, setNameReplacement] = useState<string>(name);
@@ -94,8 +93,7 @@ const ExternalPinnedApp: React.FC<PinnedAppProps> = (props) => {
     <PinnedAppBase
       appKey={appKey}
       appInfo={modifiedAppInfo}
-      active={active}
-      onActivate={onActivate}
+      isOpened={isOpened}
     >
       {badgeContent && (
         <Styled.BadgeCircle data-test={`${appKey}Badge`}>

commit e671b3b4e874357ce229afc34fe110de64abc62f

fix(shared-notes): disable sidebar button when pinned
Restores the expected behavior where the shared notes sidebar button is
disabled while pinned. This regression was introduced in
6c7ff0ea4144491d8318dc89a5215c6c2cec0eae.

bigbluebutton-html5/imports/ui/components/sidebar-navigation/user-notes-list-item/component.tsx
@@ -89,7 +89,7 @@ const UserNotesListItemContainerGraphql: React.FC<BaseSidebarButtonProps> = (pro
       ariaDescribedBy="lockedNotes"
       dataTest="sharedNotesSidebarButton"
       hasNotification={hasUnreadNotes}
-      isDisabled={disableNotes}
+      isDisabled={disableNotes || isPinned}
     >
       {disableNotes
         ? (