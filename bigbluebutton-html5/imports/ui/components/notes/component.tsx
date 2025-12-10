bigbluebutton/bigbluebutton

commit e5d3370f4d65c4470c1872044051377daccffed9

fix(shared-notes): bypass unmount delay when pinned on grid layout
Bypasses the unmount delay to correcly folow the sidebar content panel
state when the shared notes is pinned and the layout is set to grid.

bigbluebutton-html5/imports/ui/components/notes/component.tsx
@@ -125,16 +125,6 @@ const NotesGraphql: React.FC<NotesGraphqlProps> = (props) => {
   };
 
   const {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    display,
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    isPinned,
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    browserHeight,
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    browserWidth,
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    tabOrder,
     ...cssProps
   } = sharedNotesOutput;
 
@@ -244,7 +234,8 @@ const NotesContainerGraphql: React.FC<NotesContainerGraphqlProps> = (props) => {
       layoutContextDispatch={layoutContextDispatch}
       isResizing={isResizing}
       isLocalChange={isLocalChange}
-      ignoreDelayforUnmount={sidebarContentToIgnoreDelay.includes(sidebarContent.sidebarContentPanel)}
+      ignoreDelayforUnmount={sidebarContentToIgnoreDelay.includes(sidebarContent.sidebarContentPanel)
+        || (isOnMediaArea && !!isGridLayout)}
       sharedNotesOutput={sharedNotesOutput}
       amIPresenter={amIPresenter}
       isRTL={isRTL}

commit b9ec67d2f99c4be49bfe568ec581f16c629c6af9

fix(notes): incorrect CSS property types
Corrects the CSS properties passed to the shared-notes panel, which were
broken in https://github.com/bigbluebutton/bigbluebutton/commit/e5d3370f4d65c4470c1872044051377daccffed9
and have since included invalid CSS properties.

bigbluebutton-html5/imports/ui/components/notes/component.tsx
@@ -125,8 +125,20 @@ const NotesGraphql: React.FC<NotesGraphqlProps> = (props) => {
   };
 
   const {
-    ...cssProps
+    height,
+    width,
+    top,
+    left,
+    zIndex,
   } = sharedNotesOutput;
+
+  const cssProps = {
+    height,
+    width,
+    top,
+    left,
+    zIndex,
+  };
 
   return shouldRenderNotes && (
     <Styled.PanelContent