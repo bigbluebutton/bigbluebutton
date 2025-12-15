ffb20e4facf26fdf0a8e85118298edac65be3702
Samuel Weirich <4281791+SamuelWei@users.noreply.github.com>
2025-12-15 17:32:19 +0100

    Enforce time and memory limit

--- bigbluebutton-web/docker/run-in-systemd.sh
+++ bigbluebutton-web/docker/run-in-systemd.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # run-in-systemd.sh
 
-# INSECURE VERSION: Run without any restrictions!
+# INSECURE VERSION: Run without most restrictions!
 
 set -euo pipefail
 
@@ -10,9 +10,13 @@ if [[ $# -lt 2 ]]; then
   exit 1
 fi
 
-# Remove first argument (timeout)
-shift
+timeout_secs="$1"; shift
 
-"$@"
+: "${BBB_PRESENTATION_DIR:=/var/bigbluebutton/}"
+
+# Hard coded memory limits (ignoring BBB_PRESENTATION_CONVERSION_MEMORY_HIGH and BBB_PRESENTATION_CONVERSION_MEMORY_MAX)
+ulimit -S -v $((512*1024))
+ulimit -H -v $((640*1024))
+timeout "$timeout_secs" "$@"
 
 exit $?   # propagate the child’s exit status
