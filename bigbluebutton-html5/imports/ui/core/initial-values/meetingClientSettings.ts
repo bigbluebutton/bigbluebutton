98f5e95e89db2f6b7d17cf707719783726b12b6b
Ramón Souza <contato@ramonsouza.com>
2026-01-26 11:35:21 -0300

    refactor camera custom pagination settings

--- bigbluebutton-html5/imports/ui/core/initial-values/meetingClientSettings.ts
+++ bigbluebutton-html5/imports/ui/core/initial-values/meetingClientSettings.ts
@@ -364,6 +364,7 @@ export const meetingClientSettingsInitialValues: MeetingClientSettings = {
       cameraSortingModes: {
         defaultSorting: 'LOCAL_ALPHABETICAL',
         paginationSorting: 'VOICE_ACTIVITY_LOCAL',
+        partitionPrivilegedStreams: true,
       },
       cameraQualityThresholds: {
         enabled: true,
