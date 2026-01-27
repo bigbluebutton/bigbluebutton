05a95dd4189b4f711b4245e74386750665ea1679
falkTX <falktx@falktx.com>
2026-01-27 18:06:10 +0000

    audioPreset telephone

--- bigbluebutton-html5/imports/ui/core/initial-values/meetingClientSettings.ts
+++ bigbluebutton-html5/imports/ui/core/initial-values/meetingClientSettings.ts
@@ -685,7 +685,7 @@ export const meetingClientSettingsInitialValues: MeetingClientSettings = {
         },
         audio: {
           publishOptions: {
-            audioPreset: AudioPresets.musicHighQuality,
+            audioPreset: AudioPresets.telephone,
             dtx: true,
             red: false,
             forceStereo: false,
