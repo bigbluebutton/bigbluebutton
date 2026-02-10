b52ad46768d54a92a69f52d20f9febe16f07664c
Lucas Zawacki <lfzawacki@gmail.com>
2026-02-10 17:00:45 -0300

    fix(transcription): Remove old code that wrote captions to a pad

--- akka-bbb-apps/src/main/scala/org/bigbluebutton/core/apps/audiocaptions/UpdateTranscriptPubMsgHdlr.scala
+++ akka-bbb-apps/src/main/scala/org/bigbluebutton/core/apps/audiocaptions/UpdateTranscriptPubMsgHdlr.scala
@@ -4,7 +4,7 @@ import org.bigbluebutton.ClientSettings.getConfigPropertyValueByPathAsStringOrEl
 import org.bigbluebutton.common2.msgs._
 import org.bigbluebutton.core.bus.MessageBus
 import org.bigbluebutton.core.db.CaptionDAO
-import org.bigbluebutton.core.models.{AudioCaptions, UserState, Pads, Users2x}
+import org.bigbluebutton.core.models.{AudioCaptions, UserState, Users2x}
 import org.bigbluebutton.core.running.LiveMeeting
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
@@ -21,17 +21,6 @@ trait UpdateTranscriptPubMsgHdlr {
       val header = BbbClientMsgHeader(TranscriptUpdatedEvtMsg.NAME, meetingId, userId)
       val body = TranscriptUpdatedEvtMsgBody(transcriptId, transcript, locale, result)
       val event = TranscriptUpdatedEvtMsg(header, body)
-      val msgEvent = BbbCommonEnvCoreMsg(envelope, event)
-
-      bus.outGW.send(msgEvent)
-    }
-
-    def sendPadUpdateCmdMsg(groupId: String, defaultPad: String, text: String, transcript: Boolean): Unit = {
-      val routing = collection.immutable.HashMap("sender" -> "bbb-apps-akka")
-      val envelope = BbbCoreEnvelope(PadUpdateCmdMsg.NAME, routing)
-      val header = BbbCoreHeaderWithMeetingId(PadUpdateCmdMsg.NAME, liveMeeting.props.meetingProp.intId)
-      val body = PadUpdateCmdMsgBody(groupId, defaultPad, text)
-      val event = PadUpdateCmdMsg(header, body)
       val msgEvent = BbbCommonEnvCoreMsg(envelope, event)
 
       bus.outGW.send(msgEvent)
@@ -91,31 +80,6 @@ trait UpdateTranscriptPubMsgHdlr {
         msg.body.locale,
         msg.body.result,
       )
-
-      if(msg.body.result) {
-        val userName = Users2x.findWithIntId(liveMeeting.users2x, msg.header.userId).get match {
-          case u: UserState => u.name
-          case _ => "???"
-        }
-
-        val now = LocalDateTime.now()
-        val formatter = DateTimeFormatter.ofPattern("HH:mm:ss")
-        val formattedTime = now.format(formatter)
-
-        val userSpoke = s"\n $userName ($formattedTime): $transcript"
-
-        val defaultPad = getConfigPropertyValueByPathAsStringOrElse(
-          liveMeeting.clientSettings,
-          "public.captions.defaultPad",
-          alternativeValue = ""
-        )
-
-        Pads.getGroup(liveMeeting.pads, defaultPad) match {
-          case Some(group) => sendPadUpdateCmdMsg(group.groupId, defaultPad, userSpoke, transcript = true)
-          case _ =>
-        }
-      }
-
     }
   }
 }
