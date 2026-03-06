703d62eff8a50ac308ba63b6743f6723d67848ed
falkTX <falktx@falktx.com>
2026-03-06 17:00:32 +0100

    Use old script processor for low-power devices
    Signed-off-by: falkTX <falktx@falktx.com>

--- bigbluebutton-html5/imports/ui/components/audio/audio-processor/service.js
+++ bigbluebutton-html5/imports/ui/components/audio/audio-processor/service.js
@@ -16,6 +16,131 @@ let audioProcessor = null;
 // check if wasm processor is supported, assigned to undefined if not checked yet
 let wasmProcessorUnsupportedError;
 
+// create audio worklet or script processor
+// we rely on script processor because worklets must run at 128 block size, which is not possible on low-spec machines
+const createWasmProcessor = (audioContext, stream) => {
+  return new Promise((resolve, reject) => {
+    const contextSource = audioContext.createMediaStreamSource(stream);
+    const contextDestination = audioContext.createMediaStreamDestination();
+
+    if (! navigator.userAgent.match(/Android/i)) {
+      // Using Audio Worklet
+      const processorBlob = new Blob([loadedFiles.worklet], { type: 'text/javascript' });
+      const processorURL = URL.createObjectURL(processorBlob);
+
+      audioContext.audioWorklet.addModule(processorURL).then(() => {
+        const audioProcessorOptions = {
+          channelCount: 1,
+          numberOfInputs: 1,
+          numberOfOutputs: 1,
+          outputChannelCount: [1],
+        };
+        const processor = new AudioWorkletNode(audioContext, 'mapi-proc', audioProcessorOptions);
+        processor.port.onmessage = (event) => {
+          if (event.data?.type === 'loaded') {
+            processor.port.postMessage({ type: 'param', symbol: "intensity", value: 90 });
+            processor.port.postMessage({ type: 'param', symbol: "leveler_target", value: -18 });
+            processor.port.postMessage({ type: 'param', symbol: "sb_strength", value: 60 });
+            processor.port.postMessage({ type: 'param', symbol: "mb_strength", value: 60 });
+            processor.port.postMessage({ type: 'param', symbol: "pre_gain", value: 2 });
+            processor.port.postMessage({ type: 'param', symbol: "post_gain", value: 0 });
+
+            audioProcessor = processor;
+            contextSource.connect(audioProcessor);
+            audioProcessor.connect(contextDestination);
+            resolve(contextDestination.stream);
+          } else if (event.data?.type === 'error') {
+            reject(event.data.error);
+          }
+        };
+        processor.port.postMessage({ type: 'init', wasm: loadedFiles.wasmBlob, js: loadedFiles.wasmJS });
+
+      }).catch(reject);
+      return;
+    }
+
+    // fallback with createScriptProcessor follows here
+
+    // execute JS to expose the emscripten load module function
+    const jsfn_bbba = new Function(loadedFiles.wasmJS + 'return mapi_bbba;');
+    const create_module_bbba = jsfn_bbba.call();
+
+    // audio setup
+    const bufferSize = 4096;
+    const numberOfInputs = 1;
+    const numberOfOutputs = 1;
+    const processor = audioContext.createScriptProcessor(bufferSize, numberOfInputs, numberOfOutputs);
+
+    // create the wasm module and instance
+    create_module_bbba({
+      instantiateWasm: (imports, successCallback) => {
+        WebAssembly.instantiate(loadedFiles.wasmBlob, imports)
+        .then(output => {
+          successCallback(output.instance, output.module);
+        })
+        .catch(reject);
+        return {};
+      },
+      postRun: function(module) {
+        const handle = module._mapi_create(audioContext.sampleRate, bufferSize);
+
+        const audioData = module._malloc(module.HEAPF32.BYTES_PER_ELEMENT * bufferSize);
+        const audioPtrs = module._malloc(module.HEAPU32.BYTES_PER_ELEMENT);
+        module.HEAPU32[audioPtrs + (0 << 2) >> 2] = audioData;
+
+        const maxSymbolLength = 255;
+        const csymbolData = module._malloc(maxSymbolLength);
+        const csymbol = (symbol) => {
+          const len = Math.min(maxSymbolLength, module.lengthBytesUTF8(symbol) + 1);
+          module.stringToUTF8(symbol, csymbolData, len);
+          return csymbolData;
+        }
+
+        let enabled = true;
+        processor.onaudioprocess = function (e) {
+          if (! enabled) {
+            e.outputBuffer.copyToChannel(e.inputBuffer.getChannelData(0), 0);
+            return;
+          }
+
+          let buffer = e.inputBuffer.getChannelData(0);
+
+          for (let i = 0; i < bufferSize; ++i)
+            module.HEAPF32[audioData + (i << 2) >> 2] = buffer[i];
+
+          module._mapi_process(handle, audioPtrs, audioPtrs, bufferSize);
+
+          buffer = e.outputBuffer.getChannelData(0);
+          for (let i = 0; i < bufferSize; ++i)
+            buffer[i] = module.HEAPF32[audioData + (i << 2) >> 2];
+        };
+
+        // use same API as worklet for pushing changes
+        processor.port = {
+          postMessage: (data) => {
+            switch (data.type)
+            {
+              case 'enable':
+                enabled = !!data.enable;
+                break;
+              case 'param':
+                module._mapi_set_parameter(handle, csymbol(data.symbol), data.value);
+                break;
+              case 'destroy':
+                break;
+            }
+          },
+        };
+
+        audioProcessor = processor;
+        contextSource.connect(audioProcessor);
+        audioProcessor.connect(contextDestination);
+        resolve(contextDestination.stream);
+      },
+    });
+  });
+};
+
 // create an audio processor on top of a stream, trigger Promise resolve with a processed stream
 const createWasmProcessorStream = (stream) => {
   // cleanup old processor
@@ -29,38 +154,9 @@ const createWasmProcessorStream = (stream) => {
     const audioContext = new AudioContext();
 
     // function to load audio worklet, called once audio context is running
-    const loadAudioWorklet = async () => {
-      const processorBlob = new Blob([loadedFiles.worklet], { type: 'text/javascript' });
-      const processorURL = URL.createObjectURL(processorBlob);
-
-      audioContext.audioWorklet.addModule(processorURL).then(() => {
-        const contextSource = audioContext.createMediaStreamSource(stream);
-        const contextDestination = audioContext.createMediaStreamDestination();
-
-        const audioProcessorOptions = {
-          channelCount: 1,
-          numberOfInputs: 1,
-          numberOfOutputs: 1,
-          outputChannelCount: [1],
-        };
-        audioProcessor = new AudioWorkletNode(audioContext, 'mapi-proc', audioProcessorOptions);
-        audioProcessor.port.onmessage = (event) => {
-          if (event.data?.type === 'loaded') {
-            audioProcessor.port.postMessage({ type: 'param', symbol: "intensity", value: 90 });
-            audioProcessor.port.postMessage({ type: 'param', symbol: "leveler_target", value: -18 });
-            audioProcessor.port.postMessage({ type: 'param', symbol: "sb_strength", value: 60 });
-            audioProcessor.port.postMessage({ type: 'param', symbol: "mb_strength", value: 60 });
-            audioProcessor.port.postMessage({ type: 'param', symbol: "pre_gain", value: 2 });
-            audioProcessor.port.postMessage({ type: 'param', symbol: "post_gain", value: 0 });
-            resolve(contextDestination.stream);
-          } else if (event.data?.type === 'error') {
-            reject(event.data.error);
-          }
-        };
-        audioProcessor.port.postMessage({ type: 'init', wasm: loadedFiles.wasmBlob, js: loadedFiles.wasmJS });
-
-        contextSource.connect(audioProcessor);
-        audioProcessor.connect(contextDestination);
+    const loadAudioWorklet = () => {
+      createWasmProcessor(audioContext, stream).then((stream) => {
+        resolve(stream);
       }).catch(reject);
     };
 
