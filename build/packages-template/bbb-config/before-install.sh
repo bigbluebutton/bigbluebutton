3be562bfd19df98fde7cd123ac4ab43cfe71bc03
Anton Georgiev <anto.georgiev@gmail.com>
2026-03-26 13:57:54 -0400

    build(fix): add ExecPaths in redis service file

--- build/packages-template/bbb-config/before-install.sh
+++ build/packages-template/bbb-config/before-install.sh
@@ -7,6 +7,7 @@ case "$1" in
         mkdir -p /etc/systemd/system/redis-server.service.d
         cat <<HERE > /etc/systemd/system/redis-server.service.d/override.conf
 [Service]
+ExecPaths=/usr/bin/timeout /bin/sh /usr/bin/ss /usr/bin/grep /usr/bin/sleep
 ExecStartPost=/usr/bin/timeout 30 /bin/sh -c 'while ! ss -H -t -l -n sport = :6379 | grep -q "^LISTEN.*:6379"; do sleep 1; done'
 HERE
     ;;
