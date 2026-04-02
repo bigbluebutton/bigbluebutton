647cfe4b4b90399898b0b9b6faf7540e3b7b0a92
Paul Trudel <paatrudel@gmail.com>
2026-04-02 13:59:12 -0400

    Allow BBB plugin URLs to bypass local address block

--- bbb-common-web/src/main/java/org/bigbluebutton/api/service/impl/PluginRedirectValidatorService.java
+++ bbb-common-web/src/main/java/org/bigbluebutton/api/service/impl/PluginRedirectValidatorService.java
@@ -1,3 +1,18 @@
 package org.bigbluebutton.api.service.impl;
 
-public class PluginRedirectValidatorService extends BaseUrlRedirectValidator {}
+public class PluginRedirectValidatorService extends BaseUrlRedirectValidator {
+
+    private String localPluginUrlPrefix;
+
+    @Override
+    protected boolean isUrlBypassed(String redirectUrl) {
+        return localPluginUrlPrefix != null
+                && !localPluginUrlPrefix.isEmpty()
+                && redirectUrl != null
+                && redirectUrl.startsWith(localPluginUrlPrefix);
+    }
+
+    public void setLocalPluginUrlPrefix(String localPluginUrlPrefix) {
+        this.localPluginUrlPrefix = localPluginUrlPrefix;
+    }
+}
