8b571e0ed2452d9d58a65f68b23d53ca15816c46
Ramón Souza <contato@ramonsouza.com>
2026-04-07 11:27:01 -0300

    remove unused import

--- bbb-learning-dashboard/src/components/ActivityScoreHelp.jsx
+++ bbb-learning-dashboard/src/components/ActivityScoreHelp.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FormattedMessage, useIntl, injectIntl } from 'react-intl';
+import { FormattedMessage, useIntl } from 'react-intl';
 
 const ActivityScoreHelp = () => {
   const intl = useIntl();
