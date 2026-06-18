a33e4cbadcca4f0d332aa0b6b57ef11071cde19e
Ramón Souza <contato@ramonsouza.com>
2026-06-18 09:34:05 -0300

    remove font-render-hinting flag from tests

--- bigbluebutton-tests/playwright/core/setup/browsersConfig.ts
+++ bigbluebutton-tests/playwright/core/setup/browsersConfig.ts
@@ -20,7 +20,6 @@ export const chromiumConfig: Project = {
         ...chromiumBaseArgs,
         `--use-file-for-fake-video-capture=${path.join(__dirname, '../media/video.y4m')}`,
         `--use-file-for-fake-audio-capture=${path.join(__dirname, '../media/fakespeech.wav')}`,
-        '--font-render-hinting=none',
       ],
     },
   },
