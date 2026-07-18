3279789ef5bfab13b3b8435e51768d0ba3e3a6b5
hiroshisuga <45039819+hiroshisuga@users.noreply.github.com>
2026-07-18 23:16:26 +0900

    Update convert-local.sh

--- bbb-libreoffice/assets/convert-local.sh
+++ bbb-libreoffice/assets/convert-local.sh
@@ -58,8 +58,7 @@ if [[ "${inputForConversion,,}" == *.pptx && "${convertTo,,}" == "pdf" ]]; then
 
   sudo -n /usr/bin/docker run --rm --memory=1g --memory-swap=1g --network none --env="HOME=/tmp/" -w /tmp/ --user=$(printf %05d `id -u`) -v "$tempDir/":/data/ -v /usr/share/fonts/:/usr/share/fonts/:ro -v /usr/share/fontconfig/:/usr/share/fontconfig/:ro -v /etc/fonts/:/etc/fonts/:ro -v /var/cache/fontconfig/:/var/cache/fontconfig/:ro --rm bbb-soffice sh -c "timeout $(printf %03d $timeoutSecs)s /data/run-pptx-fixes-in-container.sh"
 else
-  cp "${inputForConversion}" "${tempDir}/file"
-  #cp "${source}" "$tempDir/file"
+  cp "${source}" "$tempDir/file"
   sudo /usr/bin/docker run --rm --memory=1g --memory-swap=1g --network none --env="HOME=/tmp/" -w /tmp/ --user=$(printf %05d `id -u`) -v "$tempDir/":/data/ -v /usr/share/fonts/:/usr/share/fonts/:ro -v /usr/share/fontconfig/:/usr/share/fontconfig/:ro -v /etc/fonts/:/etc/fonts/:ro -v /var/cache/fontconfig/:/var/cache/fontconfig/:ro --rm bbb-soffice sh -c "timeout $(printf %03d $timeoutSecs)s /usr/bin/soffice -env:UserInstallation=file:///tmp/ $convertToParam --outdir /data /data/file"
 fi
 
