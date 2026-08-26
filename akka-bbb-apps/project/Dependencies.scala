9f816f94e5a2bbf9812037f164e442788c1621e5
Anton Georgiev <antobinary@proton.me>
2026-08-25 20:38:03 -0400

    [3.0]chore(core, client): bumped dependencies (snyk)- #25677
    Did some tests with Snyk and resolved
    numerous warnings

--- akka-bbb-apps/project/Dependencies.scala
+++ akka-bbb-apps/project/Dependencies.scala
@@ -16,7 +16,7 @@ object Dependencies {
     val pekkoHttpVersion = "1.0.0"
     val gson = "2.8.9"
     val jackson = "2.18.9"
-    val netty = "4.1.135.Final"
+    val netty = "4.1.137.Final"
     val logback = "1.5.38"
     val slf4j = "2.0.17"
     val quicklens = "1.7.5"
