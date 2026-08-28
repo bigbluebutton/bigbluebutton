7465afeed5bad7b35e4b571b485e2f32806b748b
hiroshisuga <45039819+hiroshisuga@users.noreply.github.com>
2026-08-28 09:24:29 +0900

    Refactor useEffect dependency in container.jsx
    Remove curPageId dependency from useEffect hook.

--- bigbluebutton-html5/imports/ui/components/whiteboard/container.jsx
+++ bigbluebutton-html5/imports/ui/components/whiteboard/container.jsx
@@ -312,7 +312,7 @@ const WhiteboardContainer = (props) => {
 
   useEffect(() => () => {
     throttledPublishCursorUpdate.cancel();
-  }, [throttledPublishCursorUpdate, curPageId]);
+  }, [throttledPublishCursorUpdate]);
 
   const isMultiUserActive = whiteboardWriters.filter((u) => !u.presenter)?.length > 0;
   const cursorArray = useMergedCursorData();
