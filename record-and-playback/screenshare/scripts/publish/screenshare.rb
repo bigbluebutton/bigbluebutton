dd9fc8519ba70b784fc6ad2ed3b032ed2557c8ed
germanocaumo <germanocaumo@gmail.com>
2026-09-19 06:46:13 -0300

    fix(recording): restore symbol key access for screenshare formats
    screenshare.yml has used symbol keys since 0.9, and the shared EDL library
    indexes them as symbols: EDL.encode reads format[:extension], [:parameters]
    and [:postprocess], and EDL::Video.render requires symbol keys on the layout
    and area hashes (it raises if width/height/framerate/areas are missing).
    
    publish/screenshare.rb had been switched to format['extension'] while
    process/screenshare.rb still used format[:extension]. With Symbol permitted
    at load time, process would succeed and publish would then resolve a nil
    extension and fail with ENOENT on "screenshare-0.".

--- record-and-playback/screenshare/scripts/publish/screenshare.rb
+++ record-and-playback/screenshare/scripts/publish/screenshare.rb
@@ -37,8 +37,8 @@ if playback != 'screenshare'
 end
 
 # Load parameters and set up paths
-props = YAML::safe_load(File.read(File.expand_path('../../bigbluebutton.yml', __FILE__)))
-screenshare_props = YAML::safe_load(File.read(File.expand_path('../../screenshare.yml', __FILE__)))
+props = BigBlueButton.load_yaml(File.expand_path('../../bigbluebutton.yml', __FILE__))
+screenshare_props = BigBlueButton.load_yaml(File.expand_path('../../screenshare.yml', __FILE__))
 
 process_dir = "#{props['recording_dir']}/process/screenshare/#{meeting_id}"
 publish_dir = "#{screenshare_props['publish_dir']}/#{meeting_id}"
@@ -69,8 +69,8 @@ FileUtils.cp("#{process_dir}/index.html", "#{publish_dir}/index.html")
 
 # Copy over generated video files
 screenshare_props['formats'].each_with_index do |format, i|
-  FileUtils.cp("#{process_dir}/screenshare-#{i}.#{format['extension']}",
-               "#{publish_dir}/screenshare-#{i}.#{format['extension']}")
+  FileUtils.cp("#{process_dir}/screenshare-#{i}.#{format[:extension]}",
+               "#{publish_dir}/screenshare-#{i}.#{format[:extension]}")
 end
 
 # Captions files
