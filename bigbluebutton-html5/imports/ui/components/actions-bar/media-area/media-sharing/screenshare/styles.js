a2482e12ac235093ed67cca7a73966e981ef9be5
guiiBecker <guilhermegbecker@gmail.com>
2026-09-21 14:23:47 -0300

    refactor(media-sharing): migrate the screenshare unavailable modal to GenericModal

--- bigbluebutton-html5/imports/ui/components/actions-bar/media-area/media-sharing/screenshare/styles.js
+++ bigbluebutton-html5/imports/ui/components/actions-bar/media-area/media-sharing/screenshare/styles.js
@@ -1,31 +1,8 @@
 import styled from 'styled-components';
 import ModalSimple from '/imports/ui/components/common/modal/simple/component';
-import { colorGrayDark } from '/imports/ui/stylesheets/styled-components/palette';
-import {
-  jumboPaddingY,
-  minModalHeight,
-  mdPaddingX,
-} from '/imports/ui/stylesheets/styled-components/general';
-import {
-  fontSizeLarge,
-  headingsFontWeight,
-} from '/imports/ui/stylesheets/styled-components/typography';
 
-const ScreenShareModal = styled(ModalSimple)`
-  padding: ${jumboPaddingY};
-  min-height: ${minModalHeight};
-  text-align: center;
-`;
-
-const Title = styled.h3`
-  font-weight: ${headingsFontWeight};
-  font-size: ${fontSizeLarge};
-  color: ${colorGrayDark};
-  white-space: normal;
-  padding-bottom: ${mdPaddingX};
-`;
+const ScreenShareModal = styled(ModalSimple)``;
 
 export default {
   ScreenShareModal,
-  Title,
 };
